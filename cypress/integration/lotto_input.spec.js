df07b38e12e9a5bf9b9c9b111e5dae73f7a6e0e1
2sooy <1401660@naver.com>
2021-02-16 17:25:34 +0900

    test: 금액 입력시 로또 구매 결과 안내 메시지 출력 테스트 작성

--- cypress/integration/lotto_input.spec.js
+++ cypress/integration/lotto_input.spec.js
@@ -5,7 +5,7 @@ describe('LOTTO - 구매할 금액 입력 테스트', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('1000으로 나누어 떨이지지 않는 금액을 입력하고 확인 버튼을 클릭한 경우, alert에 나머지 금액 안내 메시지를 출력한다.', () => {
+  it('정상적인 금액을 입력하고 확인 버튼을 클릭한 경우, alert에 로또 구매 결과 안내 메시지를 출력한다.', () => {
     const alertStub = cy.stub();
     cy.on('window:alert', alertStub);
 
@@ -14,7 +14,7 @@ describe('LOTTO - 구매할 금액 입력 테스트', () => {
       .click()
       .then(() => {
         expect(alertStub.getCall(0)).to.be.calledWith(
-          `500원이 반환되었습니다.`,
+          `로또 1개 구매 완료. 거스름돈 : 500원`,
         );
       });
   });
