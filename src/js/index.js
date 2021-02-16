4f531c67e57a967ada23b2c04381c7f4ac255197
ddongule <mnk918p@gmail.com>
2021-02-16 15:55:47 +0900

    chore: prettier 세팅

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,24 @@
+<<<<<<< HEAD
 import App from './components/App.js';
 import { $ } from './lib/utils/dom.js';
 
 new App($('#app'));
+=======
+const $showResultButton = document.querySelector(".open-result-modal-button");
+const $modalClose = document.querySelector(".modal-close");
+const $modal = document.querySelector(".modal");
+const $lottoNumbersToggleButton = document.querySelector(
+  ".lotto-numbers-toggle-button"
+);
+
+const onModalShow = () => {
+  $modal.classList.add("open");
+};
+
+const onModalClose = () => {
+  $modal.classList.remove("open");
+};
+
+$showResultButton.addEventListener("click", onModalShow);
+$modalClose.addEventListener("click", onModalClose);
+>>>>>>> chore: prettier 세팅
