4b0c4a3bc8dfde2b9feb03e9f3c5151f5f070695
yujo <yujo@student.42seoul.kr>
2021-02-16 15:13:17 +0900

    test: 구입 금액에 3333원 입력시 경고 메시지가 출력되는지 확인하는 테스트 코드 작성

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -1,11 +1,17 @@
+import { ERR_MESSAGE } from '../../src/js/utils/constant.js';
+
 describe('racing-game', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('구입 금액에 3000원을 입력 시, 로또 3개가 자동으로 구입되야 한다.', () => {
-    cy.get('#purchase-price-input-form__input').type(3000);
+  const typePurchasePriceAndClickSubmitButton = (purchasePrice) => {
+    cy.get('#purchase-price-input-form__input').type(purchasePrice);
     cy.get('#purchase-price-input-form__button').click();
+  };
+
+  it('구입 금액에 3000원을 입력 시, 로또 3개가 자동으로 구입되야 한다.', () => {
+    typePurchasePriceAndClickSubmitButton(3000);
 
     cy.get('#purchase-result-section__label').should(
       'have.text',
@@ -14,5 +20,15 @@ describe('racing-game', () => {
     cy.get('.purchase-result-section__lotto-icon')
       .its(length)
       .then((lottoIconAmount) => expect(lottoIconAmount).to.equal(3));
+  });
+
+  it('구입 금액에 3333원을 입력 시, 경고 메시지가 출력되야 한다.', () => {
+    typePurchasePriceAndClickSubmitButton(3333);
+
+    cy.window().then((win) => cy.stub(win, 'alert').as('windowAlert'));
+    cy.get('@windowAlert').should(
+      'be.calledWith',
+      ERR_MESSAGE.LOTTO.INVALID_PRICE,
+    );
   });
 });
---
+++ src/js/utils/constant.js
@@ -0,0 +1,5 @@
+export const ERR_MESSAGE = {
+  LOTTO: {
+    INVALID_PRICE: '금액은 1,000원 단위로만 입력이 가능합니다.',
+  },
+};
