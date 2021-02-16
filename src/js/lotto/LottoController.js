631b00ae5d3c53105613f29db958b28805423935
HyuuunjuKim <gus7wn@gmail.com>
2021-02-16 18:32:36 +0900

    feat: 발급한 로또 보여주는 기능 구현

--- src/js/lotto/LottoController.js
+++ src/js/lotto/LottoController.js
@@ -10,14 +10,14 @@ export default function LottoController() {
     return price > 0 && price % 1000 === 0; // price는 1000원 단위의 양수여야 한다.
   };
 
-  this.onClickPurchaseButton = (price) => {
+  this.onClickPriceSubmitButton = (price) => {
     if (!this.isValidPrice(price)) {
       alert(INVALID_PRICE_ERROR);
 
       return;
     }
-
     const numOfLottoes = price / 1000;
     this.model.createLottoes(numOfLottoes);
+    this.view.showConfirmation(numOfLottoes);
   };
 }
--- src/js/lotto/LottoView.js
+++ src/js/lotto/LottoView.js
@@ -1,1 +1,61 @@
-export default function LottoView() {}
+import { showElement } from "../utils.js";
+import {
+  $confirmation,
+  $lottoList,
+  $lottoNumbersToggleButton,
+} from "../elements.js";
+
+export default function LottoView() {
+  this.getTicketTemplate = (num) => {
+    return `
+      <div class="d-flex">
+        <label class="flex-auto my-0">총 ${num}개를 구매하였습니다.</label>
+        <div class="flex-auto d-flex justify-end pr-1">
+          <label class="switch">
+            <input
+              type="checkbox"
+              class="lotto-numbers-toggle-button"
+            />
+            <span class="text-base font-normal">번호보기</span>
+          </label>
+        </div>
+      </div>
+      <div id="lotto-tickets" class="d-flex flex-wrap">
+        ${[...Array(num)]
+          .map(() => `<span class="mx-1 text-4xl">🎟️ </span>`)
+          .join("")}
+      </div>
+    `;
+  };
+  this.getTicketDetailTemplate = (num) => {
+    return `
+      <div class="d-flex">
+        <label class="flex-auto my-0">총 ${num}개를 구매하였습니다.</label>
+        <div class="flex-auto d-flex justify-end pr-1">
+          <label class="switch">
+            <input
+              type="checkbox"
+              class="lotto-numbers-toggle-button"
+            />
+            <span class="text-base font-normal">번호보기</span>
+          </label>
+        </div>
+      </div>
+      <div id="lotto-tickets" class="d-flex flex-wrap">
+
+      </div>
+    `;
+  };
+  this.paintLottoList = (num) => {
+    console.log($lottoNumbersToggleButton.checked);
+    if ($lottoNumbersToggleButton.checked) {
+      $lottoList.innerHTML = this.getTicketDetailTemplate(num);
+    } else {
+      $lottoList.innerHTML = this.getTicketTemplate(num);
+    }
+  };
+  this.showConfirmation = (num) => {
+    showElement($confirmation);
+    this.paintLottoList(num);
+  };
+}
