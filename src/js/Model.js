95fab4ae2ad1c28db2e31558e9442796c9c228b6
ChayanRin <k852012@naver.com>
2021-02-16 18:10:41 +0900

    feat: LottoModel 클래스 기본 구조 구현

---
+++ src/js/Model.js
@@ -0,0 +1,33 @@
+class LottoModel {
+  constructor() {
+    this._lotto = [];
+    this._money = 0;
+    this._count = 0;
+  }
+
+  get lootto() {
+    return this._lotto;
+  }
+
+  get money() {
+    return this._money;
+  }
+
+  get count() {
+    return this._count;
+  }
+
+  set lotto(newLotto) {
+    this._lotto = newLotto;
+  }
+
+  set money(newMoney) {
+    this._money = newMoney;
+  }
+
+  set count(newCount) {
+    this._count = newCount;
+  }
+}
+
+export default LottoModel;
