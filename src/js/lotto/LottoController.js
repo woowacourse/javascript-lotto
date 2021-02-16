88977d218806164082608fd34a739683c02f9b33
HyuuunjuKim <gus7wn@gmail.com>
2021-02-16 17:06:01 +0900

    feat: 알맞은 금액에 따른 로또를 발급한다.

--- src/js/lotto/LottoController.js
+++ src/js/lotto/LottoController.js
@@ -5,4 +5,19 @@ import { INVALID_PRICE_ERROR } from "./constants.js";
 export default function LottoController() {
   this.model = new LottoModel();
   this.view = new LottoView();
+
+  this.isValidPrice = (price) => {
+    return price > 0 && price % 1000 === 0; // price는 1000원 단위의 양수여야 한다.
+  };
+
+  this.onClickPurchaseButton = (price) => {
+    if (!this.isValidPrice(price)) {
+      alert(INVALID_PRICE_ERROR);
+
+      return;
+    }
+
+    const numOfLottoes = price / 1000;
+    this.model.createLottoes(numOfLottoes);
+  };
 }
--- src/js/lotto/LottoModel.js
+++ src/js/lotto/LottoModel.js
@@ -1,3 +1,16 @@
 export default function LottoModel() {
   this.lottoList = []; // [ { winningNumber: [0, ], bonusNumber: 0 }, ]
+
+  this.createLottoes = (numOfLottoes) => {
+    const baseNumbers = Array.from({ length: 45 }, (_, i) => i + 1);
+
+    this.lottoList = [...Array(numOfLottoes)].map(() => {
+      shuffle(baseNumbers);
+
+      return {
+        winningNumber: baseNumber.slice(0, 6),
+        bonusNumber: baseNumber[6],
+      };
+    });
+  };
 }
