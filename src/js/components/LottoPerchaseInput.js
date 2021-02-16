ff7d4d6302205b8a3fc76b616d3bae7467eb4266
zereight <fullstackmachinelearning@gmail.com>
2021-02-16 19:53:34 +0900

    feat: 1000원 미만의 금액에 대한 검증 로직 추가.

--- src/js/components/LottoPerchaseInput.js
+++ src/js/components/LottoPerchaseInput.js
@@ -1,3 +1,4 @@
+import { LOTTO_PRICE } from '../utils/constants.js';
 import { $ } from '../utils/dom.js';
 import { mod, divide } from '../utils/lotto.js';
 
@@ -25,11 +26,24 @@ export default class LottoPerchaseInput {
 
   perchaseButtonClickHandler() {
     const perchaseInputValue = this.$perchaseInput.value.trim();
+    const errorMessage = validatePerchaseInputValue(perchaseInputValue);
+    if (errorMessage) {
+      alert(errorMessage);
+      return;
+    }
     const payment = Number(perchaseInputValue);
 
-    const lottoCount = divide(payment, 1000);
-    const remainingMoney = mod(payment, 1000);
+    const lottoCount = divide(payment, LOTTO_PRICE);
+    const remainingMoney = mod(payment, LOTTO_PRICE);
 
     alert(`로또 ${lottoCount}개 구매 완료. 거스름돈 : ${remainingMoney}원`);
   }
 }
+
+const validatePerchaseInputValue = perchaseInputValue => {
+  // To do : 문자 체크
+
+  if (Number(perchaseInputValue) < LOTTO_PRICE) {
+    return `${LOTTO_PRICE}원 이상의 금액만 입력할 수 있습니다.`;
+  }
+};
