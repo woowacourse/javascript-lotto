7374357fd6d812e398e41dbda2833257a971f195
HyuuunjuKim <gus7wn@gmail.com>
2021-02-16 18:30:57 +0900

    feat: element 요소들 상수로 분리

---
+++ src/js/elements.js
@@ -0,0 +1,14 @@
+export const $confirmation = document.querySelector("#confirmation");
+export const $priceInput = document.querySelector("#price-input");
+export const $lottoList = document.querySelector("#lotto-list");
+export const $priceSubmitButton = document.querySelector(
+  "#price-submit-button"
+);
+export const $modalClose = document.querySelector(".modal-close");
+export const $modal = document.querySelector(".modal");
+export const $showResultButton = document.querySelector(
+  ".open-result-modal-button"
+);
+export const $lottoNumbersToggleButton = document.querySelector(
+  ".lotto-numbers-toggle-button"
+);
