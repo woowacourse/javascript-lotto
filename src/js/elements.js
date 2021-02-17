806966df7804336304f8bda410e25799b51e1f9f
taeeun <taeeun@taeeunui-MacBookAir.local>
2021-02-17 12:19:57 +0900

    feat: querySelector short cut 사용

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,14 +1,12 @@
-export const $confirmation = document.querySelector("#confirmation");
-export const $priceInput = document.querySelector("#price-input");
-export const $lottoList = document.querySelector("#lotto-list");
-export const $priceSubmitButton = document.querySelector(
-  "#price-submit-button"
-);
-export const $modalClose = document.querySelector(".modal-close");
-export const $modal = document.querySelector(".modal");
-export const $showResultButton = document.querySelector(
-  ".open-result-modal-button"
-);
-export const $lottoNumbersToggleButton = document.querySelector(
-  ".lotto-numbers-toggle-button"
-);
+const $ = (selector) => document.querySelector(selector);
+
+export const $confirmation = $("#confirmation");
+export const $priceInput = $("#price-input");
+export const $lottoList = $("#lotto-list");
+export const $lottoListLabel = $("#lotto-list-label");
+export const $lottoTickets = $("#lotto-tickets");
+export const $priceSubmitButton = $("#price-submit-button");
+export const $modalClose = $(".modal-close");
+export const $modal = $(".modal");
+export const $showResultButton = $(".open-result-modal-button");
+export const $lottoNumbersToggleButton = $(".lotto-numbers-toggle-button");
