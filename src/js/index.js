ba72e5cd9775edda5e7507da327d9948b0270768
bigsaigon333 <likepeppermint@gmail.com>
2021-02-17 16:50:07 +0900

    feat: 유저가 구매 금액을 입력한 경우, 유효성을 검증한다

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,38 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import { ALERT_MESSAGE, JS_SELECTOR } from "./constants/index.js";
+import { $, toDataAttributeSelector as toDAS } from "./utils/index.js";
 
-const onModalShow = () => {
-  $modal.classList.add('open')
-}
+const $cashButton = $(toDAS(JS_SELECTOR.CASH.BUTTON));
+const $cashInput = $(toDAS(JS_SELECTOR.CASH.INPUT));
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+$cashButton.addEventListener("click", () => {
+  const getAlertMessage = (userInputCash) => {
+    if (userInputCash === "") {
+      return ALERT_MESSAGE.ERROR.CASH_INPUT.NOT_A_NUMBER;
+    }
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+    const cash = Number(userInputCash);
+
+    if (cash < 0) {
+      return ALERT_MESSAGE.ERROR.CASH_INPUT.NEGATIVE;
+    }
+
+    if (!Number.isInteger(cash)) {
+      return ALERT_MESSAGE.ERROR.CASH_INPUT.DECIMAL;
+    }
+
+    return "";
+  };
+
+  const alertMessage = getAlertMessage($cashInput.value);
+
+  if (alertMessage !== "") {
+    alert(alertMessage);
+    $cashInput.value = "";
+    return;
+  }
+
+  const cash = Number($cashInput.value);
+  const lottoCount = Math.floor(cash / 1000);
+
+  // TO-DO: 구매한 로또 갯수만큼 아이콘 정보를 보여준다
+});
