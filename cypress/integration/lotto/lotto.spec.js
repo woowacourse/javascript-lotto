96ab4b574aa93c80f605d56ce01e59f2ff74a095
ddongule <mnk918p@gmail.com>
2021-02-17 18:40:46 +0900

    test: 0과 음수는 입력할 수 없다.

--- cypress/integration/lotto/lotto.spec.js
+++ cypress/integration/lotto/lotto.spec.js
@@ -18,6 +18,16 @@ context('Actions', () => {
     );
   });
 
+  it('0과 음수는 입력할 수 없다.', () => {
+    cy.window().then(window => cy.stub(window, 'alert').as('alert'));
+    cy.get('#payment-input').type('0');
+    cy.get('#payment-submit').click();
+    cy.get('@alert').should(
+      'be.calledWith',
+      '0과 음수는 입력할 수 없습니다. 1 이상의 숫자를 입력해 주세요.'
+    );
+  });
+
   it('토글 버튼을 누르면 티켓의 번호를 보여준다.', () => {
     cy.get('#payment-input').type('5000');
     cy.get('#payment-submit').click();
