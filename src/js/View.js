909089dfa101b7db305679e569c490fd2b98d3a2
ChayanRin <k852012@naver.com>
2021-02-17 11:24:47 +0900

    feat: LottoView 클래스 생성 및 render, reset 관련 메서드 구현

---
+++ src/js/View.js
@@ -0,0 +1,43 @@
+import { $ } from "./util.js";
+
+class LottoView {
+  constructor() {
+    this.template = new Template();
+  }
+
+  renderBuySection() {
+    const $buySection = $("#buy");
+    $buySection.innerHTML = this.template.buySectionTemplate();
+  }
+
+  renderPocketSection(lottos, detail) {
+    const $pocketSection = $("#pocket");
+    $pocketSection.innerHTML = this.template.pocketSectionTemplate(
+      lottos,
+      detail
+    );
+  }
+
+  renderWinningSection() {
+    const $winningSection = $("#winning");
+    $winningSection.innerHTML = this.template.winningSectionTemplate();
+  }
+
+  resetPocketSection() {
+    const $pocketSection = $("#pocket");
+    $pocketSection.innerHTML = "";
+  }
+
+  resetWinningSection() {
+    const $winningSection = $("#winning");
+    $winningSection.innerHTML = "";
+  }
+
+  reset() {
+    this.renderBuySection();
+    this.resetPocketSection();
+    this.resetWinningSection();
+  }
+}
+
+export default LottoView;
