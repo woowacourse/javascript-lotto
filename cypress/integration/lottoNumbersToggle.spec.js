7cf1169ae82c55c4c6a4bdd581f8381fc1d29ef3
seojihwan <seojihwan213@naver.com>
2021-02-17 22:41:26 +0900

    test: 번호보기 토글에 대한 테스트를 수정한다

--- cypress/integration/lottoNumbersToggle.spec.js
+++ cypress/integration/lottoNumbersToggle.spec.js
@@ -19,22 +19,24 @@ describe("번호보기 토글 테스트", () => {
       "flex-col"
     );
 
-    cy.get(toCS(CLASSNAME.LOTTO_DETAIL.TOGGLE_BUTTON)).click();
+    cy.get(toCS(CLASSNAME.LOTTO_DETAIL.TOGGLE_BUTTON)).click({ force: true });
 
     cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.ICON_WRAPPER)).should(
       "have.class",
       "flex-col"
     );
     cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.NUMBERS)).should("be.visible");
-    cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.NUMBERS))
-      .invoke("text")
-      .should((text) => {
-        expect(text.split(",").length).to.be.equal(6);
-      });
+    cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.NUMBERS)).each(($num) => {
+      cy.wrap($num)
+        .invoke("text")
+        .should((text) => {
+          expect(text.split(",").length).to.be.equal(6);
+        });
+    });
   });
 
   it("활성화된 번호보기 토글을 클릭하면, 각 로또 장수에 대한 아이콘만 표시되며 숫자 6개는 표시되지 않는다.", () => {
-    cy.get(toCS(CLASSNAME.LOTTO_DETAIL.TOGGLE_BUTTON)).click();
+    cy.get(toCS(CLASSNAME.LOTTO_DETAIL.TOGGLE_BUTTON)).click({ force: true });
 
     cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.ICON_WRAPPER)).should(
       "not.have.class",
