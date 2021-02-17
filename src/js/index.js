e3c5cc01d9f167df64b86f0bc91a5499b1da3db0
Shim MunSeong <Puterism.K@gmail.com>
2021-02-17 12:31:13 +0900

    Feat: 금액에 맞춰 로또를 자동으로 구매하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,22 @@ class LottoApp {
     this.bindEvents();
   }
 
+  generateLottoNumber() {
+    // TODO: 1, 45를 상수 처리
+    // TODO: 중복 숫자 처리
+    const lottoNumber = [];
+
+    while (lottoNumber.length < 6) {
+      const num = getRandomNumber(1, 45);
+      if (lottoNumber.includes(num)) {
+        continue;
+      }
+      lottoNumber.push(num);
+    }
+
+    return lottoNumber.sort((a, b) => a - b);
+  }
+
   bindEvents() {
     $('#money-input-form').addEventListener('submit', (event) => {
       event.preventDefault();
@@ -22,6 +38,15 @@ class LottoApp {
         alert('최소 1,000원 이상의 금액을 입력하세요.');
         return;
       }
+
+      const lottoCount = money / 1000;
+
+      const lottos = Array.from(
+        { length: lottoCount },
+        () => new Lotto(this.generateLottoNumber())
+      );
+
+      this.store.save('lottos', lottos);
     });
   }
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,2 +1,6 @@
 export const $ = (selector) => document.querySelector(selector);
 export const $all = (selector) => document.querySelectorAll(selector);
+
+export const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
