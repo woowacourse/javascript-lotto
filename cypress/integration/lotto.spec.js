d8c96dc4fdf39b4948fac4fb4e0b3e845ee7234c
권세진 <0307kwon@gmail.com>
2021-02-17 12:35:26 +0900

    test: 로또 자동 구매 시, 1 ~ 45 중 중복 없이 무작위 6개 숫자를 뽑아 저장한다.

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
-context('lottoUI', () => {
+import Lotto from "../../src/js/object/Lotto.js";
+
+context('로또 UI 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500');
   });
@@ -20,5 +22,24 @@ context('lottoUI', () => {
     cy.get('.money-input-button').click().then(() => {
       expect(alertStub.getCall(0)).to.be.calledWith('최소 1000원 이상의 금액을 입력해야 합니다.');
     });
+  });
+});
+
+context('로또 기능 테스트', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500');
+  });
+
+  it('로또 자동 구매 시, 1 ~ 45 중 중복 없이 무작위 6개 숫자를 뽑아 저장한다.', () => {
+    const money = 5000;
+    const lotto = new Lotto();
+    lotto.createNumbers();
+    const amountTestSet = new Set(lotto.getNumbers());
+    expect(amountTestSet.size === 6).to.equal(true);
+
+    for (let i = 1; i <= 45; i++) {
+      const randomNumber = lotto.getRandomNumber(1, i);
+      expect(randomNumber >= 1 && randomNumber <= i).to.equal(true);
+    }
   });
 });
