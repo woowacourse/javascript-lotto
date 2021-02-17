73c7caefed7f142c33ce8e09f7f72f0c057aa37b
ddongule <mnk918p@gmail.com>
2021-02-17 16:38:18 +0900

    test: 토글 버튼을 누르면 티켓의 번호를 보여준다.

--- cypress/integration/lotto/lotto.spec.js
+++ cypress/integration/lotto/lotto.spec.js
@@ -9,16 +9,10 @@ context('Actions', () => {
     cy.get('.ticket').should('have.length', 5);
   });
 
-  // it('토글 버튼을 누르면 티켓의 번호를 보여주는 것을 테스트한다.', () => {
-  //   cy.get('.action-email')
-  //     .type('fake@email.com')
-  //     .should('have.value', 'fake@email.com');
-  // });
-
-  // it('구입 금액을 입력받아 티켓을 생성하는 것을 테스트한다.', () => {
-  //   // https://on.cypress.io/type
-  //   cy.get('.action-email')
-  //     .type('fake@email.com')
-  //     .should('have.value', 'fake@email.com');
-  // });
+  it('토글 버튼을 누르면 티켓의 번호를 보여준다.', () => {
+    cy.get('#payment-input').type('5000');
+    cy.get('#payment-submit').click();
+    cy.get('#detail-mode-toggle').click();
+    cy.get('.lotto-numbers').should('have.length', 5);
+  });
 });
