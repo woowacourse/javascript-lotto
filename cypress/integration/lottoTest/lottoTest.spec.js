3ab7781b79a431668825594b092b59ecf7af7be1
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-17 17:05:11 +0900

    test : show lotto numbers when switch on

--- cypress/integration/lottoTest/lottoTest.spec.js
+++ cypress/integration/lottoTest/lottoTest.spec.js
@@ -3,7 +3,8 @@ describe('로또 게임 테스트', () => {
     cy.visit('http://127.0.0.1:5500/');
   });
 
-  const price = 1000;
+  const price = 10000;
+  const lottoTotalCount = price / 1000;
 
   it('프로그램을 시작하면 구입금액 입력폼만 보인다.', () => {
     cy.get('#input-price-form').should('be.visible');
@@ -22,8 +23,10 @@ describe('로또 게임 테스트', () => {
     cy.get('#input-price').type(price);
     cy.get('#input-price-btn').click();
     cy.get('#purchased-lottos').should('be.visible');
-    cy.get('#total-purchased').should('have.text', 1);
-    cy.get('#lotto-icons').children('.lotto-icon').should('have.length', 1);
+    cy.get('#total-purchased').should('have.text', lottoTotalCount);
+    cy.get('#lotto-icons')
+      .children('.lotto-wrapper')
+      .should('have.length', lottoTotalCount);
     cy.get('#input-lotto-nums').should('be.visible');
   });
 
@@ -35,5 +38,22 @@ describe('로또 게임 테스트', () => {
     cy.get('#lotto-icons').should('have.class', 'flex-col');
     cy.get('.switch').click();
     cy.get('#lotto-icons').should('not.have.class', 'flex-col');
+  });
+
+  it('번호 보기 스위치가 off이면 구매한 로또의 번호가 보이지 않고, on이면 번호가 보인다.', () => {
+    cy.get('#input-price').type(price);
+    cy.get('#input-price-btn').click();
+
+    cy.get('.switch').click();
+    cy.get('#lotto-icons').should('have.class', 'flex-col');
+    cy.get('#lotto-icons')
+      .children('.lotto-wrapper')
+      .should('have.length', lottoTotalCount);
+    cy.get('.lotto-wrapper').children('.lotto-numbers').should('exist');
+
+    cy.get('.switch').click();
+    cy.get('#lotto-icons').should('not.have.class', 'flex-col');
+    cy.get('#lotto-icons').children('.lotto-wrapper').should('not.exist');
+    cy.get('.lotto-wrapper').children('.lotto-numbers').should('not.exist');
   });
 });
