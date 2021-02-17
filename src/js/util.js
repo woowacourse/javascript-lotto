090cea71d3feca92dc811ea66ec4e2f628117a0d
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-17 21:49:15 +0900

    reafactor(util): 함수들 중괄호 제거

--- src/js/util.js
+++ src/js/util.js
@@ -1,19 +1,11 @@
-export const getRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min + 1);
-};
+export const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min + 1);
 
-export const getQuotient = (dividend, divisor) => {
-  return parseInt(dividend / divisor, 10);
-};
+export const getQuotient = (dividend, divisor) =>
+  parseInt(dividend / divisor, 10);
 
-export const sortByNumber = (array) => {
-  return array.slice().sort((a, b) => a - b);
-};
+export const sortByNumber = (array) => array.slice().sort((a, b) => a - b);
 
-export const $ = (selector) => {
-  return document.querySelector(selector);
-};
+export const $ = (selector) => document.querySelector(selector);
 
-export const $$ = (selector) => {
-  return document.querySelectorAll(selector);
-};
+export const $$ = (selector) => document.querySelectorAll(selector);
