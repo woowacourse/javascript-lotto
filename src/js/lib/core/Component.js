1d837d30d4a10712531afa8b08c0ad251fc5260e
ddongule <mnk918p@gmail.com>
2021-02-17 12:20:58 +0900

    refactor: Component class에 default parameter 적용

--- src/js/lib/core/Component.js
+++ src/js/lib/core/Component.js
@@ -2,7 +2,7 @@ export default class Component {
   $target;
   props;
 
-  constructor($target, props) {
+  constructor($target, props = {}) {
     this.$target = $target;
     this.props = props;
     this.initStates();
