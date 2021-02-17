1ef548b442277dc615dffe292a4cf6d3445388a6
ddongule <mnk918p@gmail.com>
2021-02-17 18:33:43 +0900

    test: 공백은 입력할 수 없다.

--- cypress/integration/lotto/lotto.spec.js
+++ cypress/integration/lotto/lotto.spec.js
@@ -9,6 +9,15 @@ context('Actions', () => {
     cy.get('.ticket').should('have.length', 5);
   });
 
+  it('공백은 입력할 수 없다.', () => {
+    cy.window().then(window => cy.stub(window, 'alert').as('alert'));
+    cy.get('#payment-submit').click();
+    cy.get('@alert').should(
+      'be.calledWith',
+      '공백은 입력할 수 없습니다. 숫자를 입력해 주세요.'
+    );
+  });
+
   it('토글 버튼을 누르면 티켓의 번호를 보여준다.', () => {
     cy.get('#payment-input').type('5000');
     cy.get('#payment-submit').click();
