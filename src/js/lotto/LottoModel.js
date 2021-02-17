1a24c2af70da42df235e5fedc9d019f84063d77b
taeeun <taeeun@taeeunui-MacBookAir.local>
2021-02-17 12:24:07 +0900

    fix: bonusNumber 제거

--- src/js/lotto/LottoModel.js
+++ src/js/lotto/LottoModel.js
@@ -1,7 +1,7 @@
 import { shuffle } from "../utils.js";
 
 export default function LottoModel() {
-  this.lottoList = []; // [ { winningNumber: [0, ], bonusNumber: 0 }, ]
+  this.lottoList = []; // [ { winningNumber: [0, ] } ]
 
   this.createLottoes = (numOfLottoes) => {
     const baseNumbers = Array.from({ length: 45 }, (_, i) => i + 1);
@@ -11,7 +11,6 @@ export default function LottoModel() {
 
       return {
         winningNumber: baseNumbers.slice(0, 6),
-        bonusNumber: baseNumbers[6],
       };
     });
   };
