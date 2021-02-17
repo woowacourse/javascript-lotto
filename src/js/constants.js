ef8aa1c866f6004f2d25ae9f34a7814484c32771
SunnyK <sunnyk5780@gmail.com>
2021-02-17 13:04:03 +0900

    chore: 가독성을 위해 alert 메세지에 개행 추가

--- src/js/constants.js
+++ src/js/constants.js
@@ -8,8 +8,8 @@ export const PURCHASED_QUANTITY_MESSAGE = (numOfLotto) =>
   `총 ${numOfLotto}개를 구매하였습니다.`;
 
 export const ALERT_MESSAGE = {
-  PURCHASE_AMOUNT_IS_INVALID_MONEY: `화폐단위 미만의 자릿수가 포함된 금액입니다. ${LOTTO_PRICE}원 단위로 입력해주세요`,
-  PURCHASE_AMOUNT_IS_TOO_LOW: `입력된 금액이 로또 한 장의 가격보다 작습니다. ${LOTTO_PRICE}원 이상의 금액을 입력해주세요`,
+  PURCHASE_AMOUNT_IS_INVALID_MONEY: `화폐단위 미만의 자릿수가 포함된 금액입니다.\n${LOTTO_PRICE}원 단위로 입력해주세요`,
+  PURCHASE_AMOUNT_IS_TOO_LOW: `입력된 금액이 로또 한 장의 가격보다 작습니다.\n${LOTTO_PRICE}원 이상의 금액을 입력해주세요`,
   PURCHASE_AMOUNT_HAS_CHANGE: (change) =>
-    `입력된 금액에서 ${change}원을 제외한 금액으로 로또를 구매했습니다. 거스름돈 챙겨가세요.`,
+    `입력된 금액에서 ${change}원을 제외한 금액으로 로또를 구매했습니다.\n거스름돈 챙겨가세요.`,
 };
