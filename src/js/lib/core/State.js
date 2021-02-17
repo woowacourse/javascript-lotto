dbcb10d2804759d8c498306dfd0e79218e6464ba
ddongule <mnk918p@gmail.com>
2021-02-17 16:01:45 +0900

    feat: State 클래스, 반응성 구현

--- src/js/lib/core/State.js
+++ src/js/lib/core/State.js
@@ -1,8 +1,10 @@
 export default class State {
   _value;
+  handlers;
 
   constructor(initialValue = null) {
     this._value = initialValue;
+    this.handlers = new Set();
   }
 
   get() {
@@ -11,5 +13,16 @@ export default class State {
 
   set(newValue) {
     this._value = newValue;
+    this.notify();
+  }
+
+  subscribe(handler) {
+    if (typeof handler === 'function') {
+      this.handlers.add(handler);
+    }
+  }
+
+  notify() {
+    this.handlers.forEach(handler => handler());
   }
 }
