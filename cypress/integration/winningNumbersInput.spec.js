7c0870e9e82de58dd9db35d127974d656bc6f486
365kim <365listener@gmail.com>
2021-02-18 16:46:36 +0900

    test: 입력된 당첨번호 & 보너스번호가 1 ~ 45 사이의 값이 아닌 경우 재입력 요청 메세지 표시

--- cypress/integration/winningNumbersInput.spec.js
+++ cypress/integration/winningNumbersInput.spec.js
@@ -1,3 +1,5 @@
+import { WINNING_NUMBER_CHECK_MESSAGE } from '../../src/js/constants';
+
 describe('당첨번호 입력 검사', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
@@ -14,5 +16,31 @@ describe('당첨번호 입력 검사', () => {
     });
     cy.get('.bonus-number').type(bonusNumber);
     cy.get('.open-result-modal-button').should('not.be.disabled');
+  });
+
+  it('번호를 모두 입력했을 때 6개의 당첨번호 중 1 ~ 45 범위가 아닌 번호가 있으면, 입력칸 하단에 재입력 요청 메세지를 표시한다.', () => {
+    const invalidWinningNumbers = [0, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    const { OUT_OF_RANGE } = WINNING_NUMBER_CHECK_MESSAGE;
+
+    cy.get('.winning-number-check-message').should('not.be.visible');
+    cy.get('.winning-number').each(($el, index) => {
+      cy.wrap($el).type(invalidWinningNumbers[index]);
+    });
+    cy.get('.bonus-number').type(bonusNumber);
+    cy.get('.winning-number-check-message').should('be.visible').should('have.text', OUT_OF_RANGE);
+  });
+
+  it('번호를 모두 입력했을 때 보너스번호가 1 ~ 45 범위가 아니면, 입력칸 하단에 재입력 요청 메세지를 표시한다.', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const invalidBonusNumber = 77;
+    const { OUT_OF_RANGE } = WINNING_NUMBER_CHECK_MESSAGE;
+
+    cy.get('.winning-number-check-message').should('not.be.visible');
+    cy.get('.winning-number').each(($el, index) => {
+      cy.wrap($el).type(winningNumbers[index]);
+    });
+    cy.get('.bonus-number').type(invalidBonusNumber);
+    cy.get('.winning-number-check-message').should('be.visible').should('have.text', OUT_OF_RANGE);
   });
 });
--- src/js/constants.js
+++ src/js/constants.js
@@ -4,12 +4,16 @@ export const LOTTO_MIN_NUMBER = 1;
 export const LOTTO_MAX_NUMBER = 45;
 export const LOTTO_NUMBERS_LENGTH = 6;
 export const LOTTO_NUMBER_SEPARATOR = ', ';
-export const PURCHASED_QUANTITY_MESSAGE = (numOfLotto) =>
-  `총 ${numOfLotto}개를 구매하였습니다.`;
+export const PURCHASED_QUANTITY_MESSAGE = (numOfLotto) => `총 ${numOfLotto}개를 구매하였습니다.`;
 
 export const ALERT_MESSAGE = {
   PURCHASE_AMOUNT_IS_INVALID_MONEY: `화폐단위 미만의 자릿수가 포함된 금액입니다.\n${LOTTO_PRICE}원 단위로 입력해주세요`,
   PURCHASE_AMOUNT_IS_TOO_LOW: `입력된 금액이 로또 한 장의 가격보다 작습니다.\n${LOTTO_PRICE}원 이상의 금액을 입력해주세요`,
   PURCHASE_AMOUNT_HAS_CHANGE: (change) =>
     `입력된 금액에서 ${change}원을 제외한 금액으로 로또를 구매했습니다.\n거스름돈 챙겨가세요.`,
+};
+
+export const WINNING_NUMBER_CHECK_MESSAGE = {
+  OUT_OF_RANGE: `${LOTTO_MIN_NUMBER} ~ ${LOTTO_MAX_NUMBER} 범위를 벗어난 값이 있습니다. 확인 후 다시 입력해주세요.`,
+  DUPLICATED: '중복된 값이 있습니다. 확인 후 다시 입력해주세요.',
 };
