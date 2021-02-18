5b5e0f6f397e519472910520287ceb9912cc435a
365kim <365listener@gmail.com>
2021-02-18 17:12:53 +0900

    test: 로또 구매를 완료했을 때 당첨번호 입력창 표시

--- cypress/integration/winningNumbersInput.spec.js
+++ cypress/integration/winningNumbersInput.spec.js
@@ -1,8 +1,13 @@
-import { WINNING_NUMBER_CHECK_MESSAGE } from '../../src/js/constants';
+import { LOTTO_PRICE, WINNING_NUMBER_CHECK_MESSAGE } from '../../src/js/constants';
 
 describe('당첨번호 입력 검사', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
+    cy.get('.purchase-amount-input').type(LOTTO_PRICE).type('{enter}');
+  });
+
+  it('로또를 구매하면 당첨번호를 입력할 수 있는 창이 표시된다.', () => {
+    cy.get('.winning-number-form').should('be.visible');
   });
 
   it('6개의 당첨번호와 1개의 보너스번호가 모두 정상입력되기 전까지 결과확인하기 버튼이 비활성화 되어 있다.', () => {
