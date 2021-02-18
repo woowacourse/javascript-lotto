b4d837510ce6a7a5f82b2fdde8009bb36fdc4833
yujo <yujo@student.42seoul.kr>
2021-02-18 21:28:35 +0900

    refactor: 불필요한 return 제거

--- src/js/utils/setProperty.js
+++ src/js/utils/setProperty.js
@@ -1,15 +1,15 @@
 export const showElement = (element) => {
-  return element.classList.remove('d-none');
+  element.classList.remove('d-none');
 };
 
 export const hideElement = (element) => {
-  return element.classList.add('d-none');
+  element.classList.add('d-none');
 };
 
 export const disabledElement = (element) => {
-  return element.setAttribute('disabled', true);
+  element.setAttribute('disabled', true);
 };
 
 export const enabledElement = (element) => {
-  return element.removeAttribute('disabled');
+  element.removeAttribute('disabled');
 };
