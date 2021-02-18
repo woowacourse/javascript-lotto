ec92a77262575c4b3abf2faff6944bd3480218e6
0imbean0 <tjsqls2003@gmail.com>
2021-02-18 11:32:30 +0900

    fix: 게임 시작 후, 구매 버튼이 동작하지 않도록 수정

--- src/js/utils/setProperty.js
+++ src/js/utils/setProperty.js
@@ -5,3 +5,11 @@ export const showElement = (element) => {
 export const hideElement = (element) => {
   return element.classList.add('d-none');
 };
+
+export const disabledElement = (element) => {
+  return element.setAttribute('disabled', true);
+};
+
+export const enabledElement = (element) => {
+  return element.removeAttribute('disabled');
+};
--- src/js/view/viewPurchaseResultSection.js
+++ src/js/view/viewPurchaseResultSection.js
@@ -1,5 +1,9 @@
 import { $ } from '../utils/querySelector.js';
-import { showElement, hideElement } from '../utils/setProperty.js';
+import {
+  showElement,
+  hideElement,
+  disabledElement,
+} from '../utils/setProperty.js';
 
 const $purchaseResultSectionRowAlign = $('#purchase-result-section__row-align');
 const $purchaseResultSectionColAlign = $('#purchase-result-section__col-align');
@@ -21,6 +25,7 @@ export const renderPurchaseResultSection = (
   amountOfLottoTicket,
   lottoNumbers,
 ) => {
+  const $purchasePriceInputFormButton = $('#purchase-price-input-form__button');
   const $purchaseResultSection = $('#purchase-result-section');
   const $purchaseResultSectionLabel = $('#purchase-result-section__label');
 
@@ -33,6 +38,7 @@ export const renderPurchaseResultSection = (
     .map((lottoNumber) => lottoTicketDetailTemplate(lottoNumber.join(', ')))
     .join('');
 
+  disabledElement($purchasePriceInputFormButton);
   showElement($purchaseResultSection);
 };
 
