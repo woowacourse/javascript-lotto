e0114b6c0f8f6bcfed51c9e916eed2dace47bad8
365kim <365listener@gmail.com>
2021-02-18 18:39:01 +0900

    test: 구매한 로또의 당첨번호 일치 개수 판별

--- cypress/integration/resultModalUI.spec.js
+++ cypress/integration/resultModalUI.spec.js
@@ -1,13 +1,18 @@
-import { LOTTO_PRICE } from '../../src/js/constants.js';
+import { BOUNS, LOTTO_PRICE } from '../../src/js/constants.js';
+import LottoTicket from '../../src/js/model/LottoTicket.js';
 
 describe('당첨 결과 모달 UI 검사', () => {
   before(() => {
     cy.visit('http://localhost:5500/');
   });
 
+  const winningNumber = {
+    winningNumbers: [1, 2, 3, 4, 5, 6],
+    bonusNumber: 7,
+  };
+
   it('로또 구매 및 당첨번호 입력을 마치고 결과확인 버튼을 클릭하면 당첨 결과 모달이 표시된다.', () => {
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 7;
+    const { winningNumbers, bonusNumber } = winningNumber;
 
     cy.get('.purchase-amount-input').type(LOTTO_PRICE).type('{enter}');
     cy.get('.winning-number').each(($el, index) => {
@@ -15,5 +20,25 @@ describe('당첨 결과 모달 UI 검사', () => {
     });
     cy.get('.bonus-number').type(bonusNumber);
     cy.get('.modal').should('be.visible');
+  });
+
+  it('구매한 로또의 당첨번호 일치 개수를 판별한다.', () => {
+    const lottoTicket1 = new LottoTicket([1, 2, 3, 4, 5, 6]);
+    const lottoTicket2 = new LottoTicket([1, 2, 3, 4, 5, 7]);
+    const lottoTicket3 = new LottoTicket([1, 2, 3, 4, 5, 8]);
+    const lottoTicket4 = new LottoTicket([1, 2, 3, 4, 8, 7]);
+    const lottoTicket5 = new LottoTicket([1, 2, 3, 9, 8, 7]);
+    const lottoTicket6 = new LottoTicket([1, 2, 10, 9, 8, 7]);
+    const lottoTicket7 = new LottoTicket([1, 11, 10, 9, 8, 7]);
+    const lottoTicket8 = new LottoTicket([12, 11, 10, 9, 8, 7]);
+
+    expect(lottoTicket1.getMatchCount(winningNumber)).to.equal(6);
+    expect(lottoTicket2.getMatchCount(winningNumber)).to.equal(5.5);
+    expect(lottoTicket3.getMatchCount(winningNumber)).to.equal(5);
+    expect(lottoTicket4.getMatchCount(winningNumber)).to.equal(4);
+    expect(lottoTicket5.getMatchCount(winningNumber)).to.equal(3);
+    expect(lottoTicket6.getMatchCount(winningNumber)).to.equal(2);
+    expect(lottoTicket7.getMatchCount(winningNumber)).to.equal(1);
+    expect(lottoTicket8.getMatchCount(winningNumber)).to.equal(0);
   });
 });
