23c386a9b135864490da9be646dd6ae7c3c81102
zigsong <wldms5764@gmail.com>
2021-02-18 14:21:04 +0900

    feat : check price input in range validation

--- src/js/utils/priceValidator.js
+++ src/js/utils/priceValidator.js
@@ -1,10 +1,19 @@
 import { NUMBERS, ALERT_MESSAGES } from './constants.js';
 
+const isInRange = input => {
+  return (
+    NUMBERS.MIN_PURCHASE_LIMIT <= input && input <= NUMBERS.MAX_PURCHASE_LIMIT
+  );
+};
+
 const isCorrectUnit = input => {
   return Math.floor(input / NUMBERS.LOTTO_UNIT) === input / NUMBERS.LOTTO_UNIT;
 };
 
 export default function priceValidator(price) {
+  if (!isInRange(price)) {
+    return ALERT_MESSAGES.OUT_OF_RANGE;
+  }
   if (!isCorrectUnit(price)) {
     return ALERT_MESSAGES.INCORRECT_UNIT;
   }
