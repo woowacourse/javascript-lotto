60b99a2e5b35dc9c26ee4633d9364315f19819e7
yujo <yujo@student.42seoul.kr>
2021-02-18 16:44:54 +0900

    test: 당첨 번호에 Bad Case가 입력됐을 때 경고메세지가 출력되는지 확인하는 테스트 작성

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -3,35 +3,35 @@ import { ERR_MESSAGE } from '../../src/js/utils/constant.js';
 describe('racing-game', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5500/');
+    cy.window().then((win) => cy.stub(win, 'alert').as('windowAlert'));
   });
 
-  const typePurchasePriceAndClickSubmitButton = (purchasePrice = 3000) => {
+  const typePurchasePriceAndClickSubmitButton = (purchasePrice = 5000) => {
     cy.get('#purchase-price-input-form__input').type(purchasePrice);
     cy.get('#purchase-price-input-form__button').click();
   };
 
-  const typeWinningNumbersAndClickShowResultButton = () => {
-    let inputNumber = 5;
-
+  const typeWinningNumbersAndClickShowResultButton = (
+    inputNumbers = [5, 10, 15, 20, 25, 30, 35],
+  ) => {
     cy.get('.winning-number').then((winningNumbers) => {
       [...winningNumbers].forEach((winningNumber) => {
-        cy.wrap(winningNumber).type(inputNumber);
-        inputNumber += 5;
+        cy.wrap(winningNumber).type(inputNumbers.shift());
       });
     });
-    cy.get('.bonus-number').type(inputNumber);
+    cy.get('.bonus-number').type(inputNumbers.shift());
 
     cy.get('.open-result-modal-button').click();
     cy.get('.modal').should('be.visible');
   };
 
-  const playLottoGame = (purchasePrice) => {
-    typePurchasePriceAndClickSubmitButton(purchasePrice);
-    typeWinningNumbersAndClickShowResultButton();
+  const playLottoGame = (inputNumbers) => {
+    typePurchasePriceAndClickSubmitButton();
+    typeWinningNumbersAndClickShowResultButton(inputNumbers);
   };
 
   it('구입 금액에 3000원을 입력 시, 로또 3개가 자동으로 구입되야 한다.', () => {
-    typePurchasePriceAndClickSubmitButton();
+    typePurchasePriceAndClickSubmitButton(3000);
 
     cy.get('#purchase-result-section__label').should(
       'have.text',
@@ -46,8 +46,6 @@ describe('racing-game', () => {
   });
 
   it('구입 금액에 1000원 이하의 값을 입력 시, 경고 메시지가 출력되야 한다.', () => {
-    cy.window().then((win) => cy.stub(win, 'alert').as('windowAlert'));
-
     typePurchasePriceAndClickSubmitButton(-1);
 
     cy.get('@windowAlert').should(
@@ -57,7 +55,7 @@ describe('racing-game', () => {
   });
 
   it('"번호보기" 토글 버튼 클릭시 구매한 로또의 번호를 볼 수 있어야 한다.', () => {
-    typePurchasePriceAndClickSubmitButton(3000);
+    typePurchasePriceAndClickSubmitButton();
 
     cy.get('#purchase-result-section__toggle').click({ force: true });
     cy.get('#purchase-result-section__row-align').should('not.be.visible');
@@ -75,6 +73,24 @@ describe('racing-game', () => {
     cy.get('.modal').should('not.be.visible');
   });
 
+  it('당첨 번호에 중복된 번호을 입력시, 경고 메시지가 출력되야 한다.', () => {
+    playLottoGame([5, 5, 10, 15, 25, 30, 35]);
+
+    cy.get('@windowAlert').should(
+      'be.calledWith',
+      ERR_MESSAGE.WINNING_NUMBER.DUPLICATE,
+    );
+  });
+
+  it('"1 ~ 45"가 아닌 번호를 입력시, 경고 메시지가 출력되야 한다.', () => {
+    playLottoGame([5, 10, 15, 25, 30, 35, 50]);
+
+    cy.get('@windowAlert').should(
+      'be.calledWith',
+      ERR_MESSAGE.WINNING_NUMBER.OUT_OF_RANGE,
+    );
+  });
+
   it('"다시 시작하기" 버튼 클릭시 행운의 로또 초기 화면으로 돌아가야 한다.', () => {
     playLottoGame();
 
@@ -83,6 +99,6 @@ describe('racing-game', () => {
     cy.get('#purchase-result-section').should('not.be.visible');
     cy.get('#winning-number-input-form').should('not.be.visible');
 
-    playLottoGame(5000);
+    playLottoGame();
   });
 });
