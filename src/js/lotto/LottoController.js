bb7a13b435dca480e8ab6503be59b548e530243f
taeeun <taeeun@taeeunui-MacBookAir.local>
2021-02-19 14:56:05 +0900

    refactor: 멤버 변수 이름 수정

--- src/js/lotto/LottoController.js
+++ src/js/lotto/LottoController.js
@@ -3,8 +3,8 @@ import LottoView from "./LottoView.js";
 import { INVALID_PRICE_ERROR } from "./constants.js";
 
 export default function LottoController() {
-  this.model = new LottoModel();
-  this.view = new LottoView();
+  this.lottoModel = new LottoModel();
+  this.lottoView = new LottoView();
 
   this.isValidPrice = (price) => {
     return price > 0 && price % 1000 === 0; // price는 1000원 단위의 양수여야 한다.
@@ -13,16 +13,15 @@ export default function LottoController() {
   this.onClickPriceSubmitButton = (price) => {
     if (!this.isValidPrice(price)) {
       alert(INVALID_PRICE_ERROR);
-      this.view.resetLottoView();
+      this.lottoView.resetLottoView();
 
       return;
     }
-    const numOfLottoes = price / 1000;
-    this.model.createLottoes(numOfLottoes);
-    this.view.showConfirmation(this.model.lottoList);
+    this.lottoModel.buy(price);
+    this.lottoView.showConfirmation(this.lottoModel.lottoList);
   };
 
   this.onChangeLottoNumbersToggleButton = (e) => {
-    this.view.paintLottoList(this.model.lottoList, e.target.checked);
+    this.lottoView.paintLottoList(this.lottoModel.lottoList, e.target.checked);
   };
 }
