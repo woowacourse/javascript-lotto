de75720b5676edb04a0e007f8588c02a07816746
ddongule <mnk918p@gmail.com>
2021-02-19 17:19:31 +0900

    fix: 중복된 숫자가 티켓에 나오는 현상

--- src/js/lib/utils/ticket.js
+++ src/js/lib/utils/ticket.js
@@ -6,10 +6,15 @@ import {
   TICKET_MAX_NUMBER,
 } from '../constants/ticket.js';
 
-export const createTicket = () =>
-  [...Array(TICKET_NUMBERS_LENGTH)].map(() =>
-    createRandomNumber(TICKET_MIN_NUMBER, TICKET_MAX_NUMBER)
-  );
+export const createTicket = () => {
+  const ticket = new Set();
+
+  while (ticket.length < TICKET_NUMBERS_LENGTH) {
+    ticket.add(createRandomNumber(TICKET_MIN_NUMBER, TICKET_MAX_NUMBER));
+  }
+
+  return [...ticket];
+};
 
 export const getNumberOfTickets = value =>
   Math.floor(Number(value) / TICKET_PRICE);
