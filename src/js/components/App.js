155e0845554646f7a2f8c33efbe0ae22aecd121e
jho2301 <jho2301@gmail.com>
2021-02-19 20:25:00 +0900

    feat: 컴포넌트 추가
    - WinningNumberInput
    - ResultModal

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,7 +2,9 @@ import Component from '../lib/core/Component.js';
 import State from '../lib/core/State.js';
 import { $ } from '../lib/utils/dom.js';
 import PaymentInput from './PaymentInput.js';
+import ResultModal from './ResultModal.js';
 import TicketList from './TicketList.js';
+import WinningNumberInput from './WinningNumberInput.js';
 
 class App extends Component {
   initStates() {
@@ -10,20 +12,24 @@ class App extends Component {
   }
 
   mountTemplate() {
-    this.$target.innerHTML = ` 
+    this.$target.innerHTML = `
       <div class="d-flex justify-center mt-5">
         <div class="w-100">
           <h1 class="text-center">🎱 행운의 로또</h1>
           <form id="payment-input-wrapper" class="mt-5"></form>
           <section id="ticket-view-wrapper" class="mt-9"></section>
+          <form id="winning-number-form-wrapper" class="mt-9"></form>
         </div>
       </div>
+      <div class="modal"></div>
     `;
   }
 
   mountChildComponents() {
     new PaymentInput($('#payment-input-wrapper'), { tickets: this.tickets });
     new TicketList($('#ticket-view-wrapper'), { tickets: this.tickets });
+    new WinningNumberInput($('#winning-number-form-wrapper'));
+    new ResultModal($('.modal'));
   }
 }
 
