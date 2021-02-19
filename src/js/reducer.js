cd8b3aa80ecfffe49d0fb39dc48612bd6521e6be
bigsaigon333 <likepeppermint@gmail.com>
2021-02-19 18:48:35 +0900

    feat: 로또 추가 구매기능을 추가한다
    - 구매 금액을 연속해서 입력한 경우, 계속해서 로또를 구입할 수 있으며 이는 기존의 로또 장수에 더해져서 표시된다

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -3,7 +3,7 @@ import { ACTION_TYPE } from "./constants/index.js";
 const reducer = (state, action) => {
   switch (action.type) {
     case ACTION_TYPE.LOTTOS:
-      return { ...state, lottos: [...action.payload] };
+      return { ...state, lottos: [...state.lottos, ...action.payload] };
     default:
       return state;
   }
