424bb769db4884d6be8428181bde8acf99bd47eb
yungo1846 <yungo1468@naver.com>
2021-02-21 11:14:57 +0900

    refactor: change file name with camelCase

--- src/js/util/index.js
+++ src/js/util/index.js
@@ -1,3 +1,3 @@
 export { $ } from './DOM.js';
 export { validator } from './validator.js';
-export { getRandomNumber } from './get_random_number.js';
+export { getRandomNumber } from './getRandomNumber.js';
