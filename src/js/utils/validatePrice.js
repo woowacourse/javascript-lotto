6abd8e8b4c139375e10c363a9202ff27153c83e2
zigsong <wldms5764@gmail.com>
2021-02-22 00:29:07 +0900

    refactor : - change unused private property to public property - redefine Lotto Length property to Lotto class static property - move initNumbers method to constructor - rename constant name in specific way - install eslint-babel to fix eslint syntax

--- src/js/utils/validatePrice.js
+++ src/js/utils/validatePrice.js
@@ -1,5 +1,8 @@
-import { NUMBERS } from './constants.js';
+import { LOTTO_NUMBERS } from './constants.js';
 
 export const isCorrectPurchaseUnit = input => {
-  return Math.floor(input / NUMBERS.LOTTO_UNIT) === input / NUMBERS.LOTTO_UNIT;
+  return (
+    Math.floor(input / LOTTO_NUMBERS.LOTTO_UNIT) ===
+    input / LOTTO_NUMBERS.LOTTO_UNIT
+  );
 };
