168e387e605108a8a9036a3f7d9cdb4ddb55d088
Shim MunSeong <Puterism.K@gmail.com>
2021-02-21 15:16:37 +0900

    Test: 당첨 번호에 2자리 숫자 입력 시, 다음 폼에 autoFocus되는지 확인하는 테스트 케이스 추가

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -138,4 +138,18 @@ describe('LOTTO 테스트', () => {
 
     cy.get('.modal').should('not.be.visible');
   });
+
+  it('당첨 번호를 입력할 때, 2자리 숫자가 입력되면, 자동으로 다음 폼으로 focus되는지 확인한다.', () => {
+    const winningNumbers = [10, 11, 30, 25, 21, 20];
+    cy.get('#money-input').type('10000');
+    cy.get('#money-submit-button').click();
+
+    cy.get('.winning-number').each((winningNumberInput, index) => {
+      cy.wrap(winningNumberInput)
+        .should('have.value', '')
+        .and('be.focused')
+        .type(winningNumbers[index]);
+    });
+    cy.get('.bonus-number').should('have.value', '').and('be.focused').type(45);
+  });
 });
