8c3ed4bd2c29c8d3a7d6264655cb1da6080533c6
SunnyK <sunnyk5780@gmail.com>
2021-02-21 16:02:18 +0900

    feat: 입력된 당첨번호가 1 ~ 45가 아닌 경우 재입력 요청 메세지 표시

--- src/js/components/WinningNumberInput.js
+++ src/js/components/WinningNumberInput.js
@@ -1,3 +1,4 @@
+import { LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, WINNING_NUMBER_CHECK_MESSAGE } from '../constants.js';
 import { $, $$, show, hide } from '../utils/DOM.js';
 
 export default class WinningNumberInput {
@@ -5,8 +6,40 @@ export default class WinningNumberInput {
     this.$winningNumberForm = $('.winning-number-form');
     this.$winningNumberInputs = $$('.winning-number');
     this.$bonusNumberInput = $('.bonus-number');
+    this.$winningNumberCheckMessage = $('.winning-number-check-message');
 
     this.isVisible = isVisible;
+
+    this.attachEvents();
+  }
+
+  attachEvents() {
+    this.$winningNumberForm.addEventListener('keyup', this.onChangeWinningNumberInput.bind(this));
+  }
+
+  onChangeWinningNumberInput(e) {
+    if (e.target.type !== 'number' || e.target.value === '') {
+      return;
+    }
+
+    const inputValue = Number(e.target.value);
+    const errorMessage = this.validateInput(inputValue);
+
+    if (errorMessage !== '') {
+      this.$winningNumberCheckMessage.innerText = errorMessage;
+    }
+  }
+
+  validateInput(number) {
+    if (this.isOutOfRange(number)) {
+      return WINNING_NUMBER_CHECK_MESSAGE.OUT_OF_RANGE;
+    }
+
+    return '';
+  }
+
+  isOutOfRange(number) {
+    return number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER;
   }
 
   setState({ isVisible }) {
@@ -16,5 +49,6 @@ export default class WinningNumberInput {
 
   render() {
     this.isVisible ? show(this.$winningNumberForm) : hide(this.$winningNumberForm);
+    this.$winningNumberInputs[0].focus();
   }
 }
