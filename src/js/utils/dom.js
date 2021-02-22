60632add96925de7ba29d59525b800e2e5245819
2sooy <1401660@naver.com>
2021-02-22 17:44:00 +0900

    feat: 당첨 번호 입력에 대한 예외 상황 처리 기능 구현

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,5 +1,5 @@
 export const $ = selector => document.querySelector(selector);
-export const $$ = selector => document.querySelectorAll(selector);
+export const $$ = selector => Array.from(document.querySelectorAll(selector));
 
 export const clearInput = $input => {
   $input.value = '';
