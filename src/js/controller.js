4c2ffb44fd9e146a1273d03f727234dcd225252d
Chayan <chayans999@gmail.com>
2021-02-22 16:40:15 +0900

    feat: 모달 창 더미데이터 구현

--- src/js/controller.js
+++ src/js/controller.js
@@ -49,6 +49,7 @@ class LottoController {
     this.handlePocket()
     this.renderPocketLottos()
     this.view.renderWinningSection()
+    this.manageModal()
   }
 
   manageLotto() {
@@ -69,6 +70,28 @@ class LottoController {
   manageDetail() {
     this.model.toggleDetail()
     this.renderPocketLottos()
+  }
+
+  manageModal() {
+    const $showResultButton = document.querySelector(
+      ".open-result-modal-button"
+    )
+    const $modalClose = document.querySelector(".modal-close")
+    const $modal = document.querySelector(".modal")
+    const $lottoNumbersToggleButton = document.querySelector(
+      ".lotto-numbers-toggle-button"
+    )
+
+    const onModalShow = () => {
+      $modal.classList.add("open")
+    }
+
+    const onModalClose = () => {
+      $modal.classList.remove("open")
+    }
+
+    $showResultButton.addEventListener("click", onModalShow)
+    $modalClose.addEventListener("click", onModalClose)
   }
 
   handlePrice() {
