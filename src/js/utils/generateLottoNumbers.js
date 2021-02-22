3aa1e079b7cc9dec6da2491d263e9f0c4c7dc9bc
bigsaigon333 <likepeppermint@gmail.com>
2021-02-22 15:17:06 +0900

    refactor: 로또번호 갯수를 상수화하고, Set 자료형을 활용하여 로또번호를 생성한다

--- src/js/utils/generateLottoNumbers.js
+++ src/js/utils/generateLottoNumbers.js
@@ -1,14 +1,14 @@
 const MIN = 1;
 const MAX = 45;
+const LOTTO_NUMBERS_LENGTH = 6;
 
 const getRandomNumber = () => Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
 
 export const generateLottoNumbers = () => {
-  const randomNumbers = [];
-  while (randomNumbers.length < 6) {
-    const randomNumber = getRandomNumber();
-    if (!randomNumbers.includes(randomNumber)) randomNumbers.push(randomNumber);
+  const randomNumbers = new Set();
+  while (randomNumbers.size < LOTTO_NUMBERS_LENGTH) {
+    randomNumbers.add(getRandomNumber());
   }
 
-  return randomNumbers;
+  return [...randomNumbers];
 };
