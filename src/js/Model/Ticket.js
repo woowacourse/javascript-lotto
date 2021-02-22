b7829a245be02454eb8c720b4a01c831a96c3f64
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-22 14:37:18 +0900

    refactor : randomNumbers 변수명 numbers로 수정

--- src/js/Model/Ticket.js
+++ src/js/Model/Ticket.js
@@ -2,15 +2,17 @@ import { Number } from "../Util/constants.js";
 
 export default class Ticket {
   constructor() {
-    this.numbers = this.generateRandomNumbers()
+    this.numbers = this.generateRandomNumbers();
   }
 
-
   generateRandomNumbers() {
-    const randomNumbers = Array.from({ length: Number.LOTTO_MAX_NUMBER }, (_, i) => i + 1);
+    const numbers = Array.from(
+      { length: Number.LOTTO_MAX_NUMBER },
+      (_, i) => i + 1
+    );
 
-    randomNumbers.sort(() => Math.random() - Math.random());
+    numbers.sort(() => Math.random() - Math.random());
 
-    return randomNumbers.slice(0, Number.TICKET_NUMBER_LENGTH).sort((a, b) => a - b);
+    return numbers.slice(0, Number.TICKET_NUMBER_LENGTH).sort((a, b) => a - b);
   }
 }
