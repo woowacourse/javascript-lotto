d308efc3d94298380b3d2bd8e86bc91c4c08dfe2
zereight <fullstackmachinelearning@gmail.com>
2021-02-22 21:10:38 +0900

    test: 다시 시작하기 버튼 누를때, 초기화를 확인하는 테스트 작성

--- cypress/integration/lotto_modal.spec.js
+++ cypress/integration/lotto_modal.spec.js
@@ -40,4 +40,27 @@ describe('LOTTO - 모달 동작 테스트', () => {
     cy.get('.modal-close').click();
     cy.get('.modal').should('not.be.visible');
   });
+
+  it('다시 시작하기 버튼을 누르면 초기화 되서 다시 구매를 시작할 수 있다.', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 40;
+
+    cy.get('#lotto-purchase-input').type('4500');
+    cy.get('#lotto-purchase-btn').click();
+    cy.get('.winning-number')
+      .each((elem, index) => {
+        cy.wrap(elem).type(winningNumbers[index]);
+      })
+      .then(() => {
+        cy.get('.bonus-number').type(bonusNumber);
+      });
+
+    cy.get('.open-result-modal-button').click();
+    cy.get('#restart-btn').click();
+
+    cy.get('#lotto-purchase-input').should('have.text', '');
+    cy.get('#lotto-display-container').should('not.be.visible');
+    cy.get('#lotto-winning-number-input-container').should('not.be.visible');
+    cy.get('.modal').should('not.be.visible');
+  });
 });
