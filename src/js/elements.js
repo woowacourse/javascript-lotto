de968dcff2c5e3dce16377447730884286ea97d8
taeeun <taeeun@taeeunui-MacBookAir.local>
2021-02-22 16:20:24 +0900

    feat:  추가

--- src/js/elements.js
+++ src/js/elements.js
@@ -10,3 +10,4 @@ export const $modalClose = $("#modal-close");
 export const $modal = $("#modal");
 export const $showResultButton = $("#open-result-modal-button");
 export const $lottoNumbersToggleButton = $("#lotto-numbers-toggle-button");
+export const $winningNumberForm = $("#winning-number-form");
