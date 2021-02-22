eaedc2a00db75098f6a01420015b1dcc7f2c6e21
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-22 17:11:10 +0900

    feat : create modal open event in WinningNumberInput

--- src/js/views/WinningNumberInput.js
+++ src/js/views/WinningNumberInput.js
@@ -1,3 +1,28 @@
 import View from './View.js';
+import { $ } from '../utils/dom.js';
 
-export default class WinningNumberInput extends View {}
+export default class WinningNumberInput extends View {
+  constructor($element) {
+    super($element);
+    this.$showResultButton = $('.open-result-modal-button');
+    this.$modalClose = $('.modal-close');
+    this.$modal = $('.modal');
+    this.bindModalEvent();
+  }
+
+  bindModalEvent() {
+    this.$showResultButton.addEventListener(
+      'click',
+      this.onModalShow.bind(this)
+    );
+    this.$modalClose.addEventListener('click', this.onModalClose.bind(this));
+  }
+
+  onModalShow() {
+    this.$modal.classList.add('open');
+  }
+
+  onModalClose() {
+    this.$modal.classList.remove('open');
+  }
+}
