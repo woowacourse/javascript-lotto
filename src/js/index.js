c586583fdbe94da9533d3fc248bd996461189622
Chayan <chayans999@gmail.com>
2021-02-22 16:40:15 +0900

    feat: 모달 창 더미데이터 구현

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,3 @@ import LottoController from "./controller.js"
 
 const lotto = new LottoController()
 lotto.init()
-
-/* const $showResultButton = document.querySelector(".open-result-modal-button");
-const $modalClose = document.querySelector(".modal-close");
-const $modal = document.querySelector(".modal");
-const $lottoNumbersToggleButton = document.querySelector(
-  ".lotto-numbers-toggle-button"
-);
-
-const onModalShow = () => {
-  $modal.classList.add("open");
-};
-
-const onModalClose = () => {
-  $modal.classList.remove("open");
-};
-
-$showResultButton.addEventListener("click", onModalShow);
-$modalClose.addEventListener("click", onModalClose); */
