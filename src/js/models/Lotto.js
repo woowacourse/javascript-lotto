5da7a0cc1aab38db91b18600bbbb25e7db4fe6fa
seojihwan <seojihwan213@naver.com>
2021-02-22 22:56:36 +0900

    refactor: 로또 관련 상수를 로또 클래스의 static 메서드로 구성한다

--- src/js/models/Lotto.js
+++ src/js/models/Lotto.js
@@ -1,4 +1,14 @@
 export default class Lotto {
+  static get MIN_NUMBER() {
+    return 1;
+  }
+  static get MAX_NUMBER() {
+    return 45;
+  }
+  static get NUMBERS_LENGTH() {
+    return 6;
+  }
+
   constructor(numbers) {
     this.numbers = [...numbers].sort((a, b) => a - b);
   }
