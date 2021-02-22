6859d2d3efff66e43ba97391f4e6db2b05dfe885
HyuuunjuKim <gus7wn@gmail.com>
2021-02-22 17:38:50 +0900

    feat: 필요한 id 추가

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,4 +1,8 @@
-const $ = (selector) => document.querySelector(selector);
+const $ = (selector) => {
+  const selected = document.querySelectorAll(selector);
+
+  return selected.length === 1 ? selected[0] : selected;
+};
 
 export const $confirmation = $("#confirmation");
 export const $priceInput = $("#price-input");
@@ -11,3 +15,8 @@ export const $modal = $("#modal");
 export const $showResultButton = $("#open-result-modal-button");
 export const $lottoNumbersToggleButton = $("#lotto-numbers-toggle-button");
 export const $winningNumberForm = $("#winning-number-form");
+export const $prizeTable = $("#prize-table");
+export const $earningRate = $("#earning-rate");
+export const $restartButton = $("#restart-button");
+export const $winningNumberInputs = $(".winning-number");
+export const $bonusNumberInput = $("#bonus-number");
