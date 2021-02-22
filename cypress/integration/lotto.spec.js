c27489408ccfe4196b633bd1822322b48653df46
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-22 17:32:46 +0900

    test: 당첨 번호에 중복되는 숫자가 있으면 alert가 발생해야 한다 테스트 구현

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -152,11 +152,26 @@ describe("ui-exception", () => {
     cy.get(".winning-number").each(($winningNumber, i) => {
       i % 2 !== 0 && cy.wrap($winningNumber).type(i + 1)
     })
-
     cy.get("#winning-result-button").click()
     cy.get("@alertStub").should(
       "be.calledWith",
       "당첨 번호를 모두 입력해주세요."
+    )
+  })
+
+  it("당첨 번호에 중복되는 숫자가 있으면 alert가 발생해야 한다.", () => {
+    cy.get("#buy-input").type("5000")
+    cy.get("#buy-button").click()
+
+    cy.get(".winning-number").each(($winningNumber, i) => {
+      cy.wrap($winningNumber).type((i % 3) + 1)
+    })
+
+    cy.get(".bonus-number").type(2)
+    cy.get("#winning-result-button").click()
+    cy.get("@alertStub").should(
+      "be.calledWith",
+      "당첨 번호는 중복되면 안됩니다."
     )
   })
 })
