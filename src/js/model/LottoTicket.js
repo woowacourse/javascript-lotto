5e114635f5364614ce4b6f8d31ae6504f4e9290b
365kim <365listener@gmail.com>
2021-02-22 22:12:15 +0900

    feat: 구매한 로또의 당첨번호 일치 개수 반환 함수 추가

--- src/js/model/LottoTicket.js
+++ src/js/model/LottoTicket.js
@@ -1,9 +1,15 @@
 import { getRandomNumber } from '../utils/general.js';
-import { LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBERS_LENGTH } from '../constants.js';
+import {
+  LOTTO_MIN_NUMBER,
+  LOTTO_MAX_NUMBER,
+  LOTTO_NUMBERS_LENGTH,
+  BOUNS_COUNT,
+  BONUS_CHECK_REQUIRED_COUNT,
+} from '../constants.js';
 
 export default class LottoTicket {
-  constructor() {
-    this.numbers = this.createLottoNumbers().sort((a, b) => a - b);
+  constructor(numbers) {
+    this.numbers = (numbers ?? this.createLottoNumbers()).sort((a, b) => a - b);
   }
 
   createLottoNumbers(array = []) {
@@ -18,5 +24,22 @@ export default class LottoTicket {
     }
 
     return this.createLottoNumbers(array);
+  }
+
+  getTotalMatchCount(winningNumber) {
+    const { winningNumbers, bonusNumber } = winningNumber;
+    const matchCount = this.getWinningNumbersMatchCount(winningNumbers);
+
+    return matchCount === BONUS_CHECK_REQUIRED_COUNT
+      ? matchCount + this.getBonusNumberMatchCount(bonusNumber)
+      : matchCount;
+  }
+
+  getWinningNumbersMatchCount(winningNumbers) {
+    return this.numbers.reduce((acc, num) => acc + Number(winningNumbers.includes(num)), 0);
+  }
+
+  getBonusNumberMatchCount(bonusNumber) {
+    return this.numbers.includes(bonusNumber) ? BOUNS_COUNT : 0;
   }
 }
