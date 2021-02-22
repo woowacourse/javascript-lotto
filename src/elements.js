813cee3327964ccabf77b0598c94449302380386
swon3210 <swon3210@gmail.com>
2021-02-22 15:13:16 +0900

    refactor: export 일괄 적용 => 개별 적용

--- src/elements.js
+++ src/elements.js
@@ -1,26 +1,13 @@
 import { $ } from './utils/querySelector.js';
 
-const $costSubmitForm = $('#cost-submit-form');
-const $costInput = $('#cost-input');
-const $costSubmitButton = $('#cost-submit-button');
-const $purchaseResult = $('#purchase-result');
-const $purchaseItemCount = $('#purchase-item-count');
-const $lottoNumbersToggleButton = $('#lotto-numbers-toggle-button');
-const $purchaseItemList = $('#purchase-item-list');
+export const $costSubmitForm = $('#cost-submit-form');
+export const $costInput = $('#cost-input');
+export const $costSubmitButton = $('#cost-submit-button');
+export const $purchaseResult = $('#purchase-result');
+export const $purchaseItemCount = $('#purchase-item-count');
+export const $lottoNumbersToggleButton = $('#lotto-numbers-toggle-button');
+export const $purchaseItemList = $('#purchase-item-list');
 
-const $showResultButton = $('.open-result-modal-button');
-const $modalClose = $('.modal-close');
-const $modal = $('.modal');
-
-export {
-  $costSubmitForm,
-  $costInput,
-  $costSubmitButton,
-  $purchaseResult,
-  $purchaseItemCount,
-  $lottoNumbersToggleButton,
-  $purchaseItemList,
-  $showResultButton,
-  $modalClose,
-  $modal,
-};
+export const $showResultButton = $('.open-result-modal-button');
+export const $modalClose = $('.modal-close');
+export const $modal = $('.modal');
--- src/templates.js
+++ src/templates.js
@@ -1,26 +1,22 @@
-const getResultItemCountTemplate = (lottoItemCount) => {
+export const getResultItemCountTemplate = (lottoItemCount) => {
   return `
     총 <span id="lotto-count">${lottoItemCount}</span>개를 구매하였습니다.
   `;
 };
 
-const getNumberListTemplate = (numberList) => {
+export const getNumberListTemplate = (numberList) => {
   return numberList.join(', ');
 };
 
-const getResultItemListTemplate = (lottoItemList) => {
+export const getResultItemListTemplate = (lottoItemList) => {
   return lottoItemList
-    .map(
-      (lottoItem) =>
-        ` 
-      <div class="mx-1 text-4xl lotto-item">
-        <span class="lotto-icon">🎟️</span> <span class="lotto-numbers">${getNumberListTemplate(
-          lottoItem
-        )}</span>
-      </div>
-    `
+    .map((lottoItem) =>
+      ` 
+        <div class="mx-1 text-4xl lotto-item">
+          <span class="lotto-icon">🎟️</span>
+          <span class="lotto-numbers">${getNumberListTemplate(lottoItem)}</span>
+        </div>
+      `
     )
     .join('');
 };
-
-export { getResultItemCountTemplate, getResultItemListTemplate };
