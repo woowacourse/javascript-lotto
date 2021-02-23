b05287f389e2844275bf37b48582d060848e1dae
yujo <yujo@student.42seoul.kr>
2021-02-23 22:59:30 +0900

    refactor: validator 모듈 분리

--- src/js/utils/validator.js
+++ src/js/utils/validator.js
@@ -7,8 +7,14 @@ export const isDuplicate = (nums) => {
   return new Set(nums).size !== nums.length;
 };
 
+const isValidMinNumber = (num) => {
+  return VALUE.LOTTO.MIN_NUM <= num;
+};
+
+const isValidMaxNumber = (num) => {
+  return num <= VALUE.LOTTO.MAX_NUM;
+};
+
 export const isValidRange = (nums) => {
-  return nums.every(
-    (num) => VALUE.LOTTO.MIN_NUM <= num && num <= VALUE.LOTTO.MAX_NUM,
-  );
+  return nums.every((num) => isValidMinNumber(num) && isValidMaxNumber(num));
 };
