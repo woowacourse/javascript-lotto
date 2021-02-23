96cc527fb95c84a77ec87a92f6911eec3bc01558
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-23 18:15:36 +0900

    feat : create compare winning numbers logic

--- src/js/Lotto.js
+++ src/js/Lotto.js
@@ -4,17 +4,49 @@ export default class Lotto {
   static LOTT0_LENGTH = 6;
 
   constructor() {
-    this.numbers = new Set();
+    this._numbers = new Set();
+    this.matchingNumbers = 0;
+    this.isMatchBonus = false;
+    // this.rank = Infinity;
     this.initNumbers();
   }
 
   initNumbers() {
-    while (this.numbers.size < Lotto.LOTT0_LENGTH) {
-      this.numbers.add(getRandomNumber());
+    while (this._numbers.size < Lotto.LOTT0_LENGTH) {
+      this._numbers.add(getRandomNumber());
     }
   }
 
-  get numberDetail() {
-    return [...this.numbers.values()].join(', ');
+  get numbers() {
+    return this._numbers;
   }
+
+  // Test를 위한 setter 생성
+  set numbers(numsArray) {
+    this._numbers = numsArray;
+  }
+
+  get numberDetail() {
+    return [...this._numbers.values()].join(', ');
+  }
+
+  addMatchNumbers() {
+    this.matchingNumbers++;
+  }
+
+  setMatchBonus() {
+    this.isMatchBonus = true;
+  }
+
+  // updateRank(rank) {
+  //   switch (this.matchingNumbers) {
+  //     case '6':
+  //       this.rank = 1;
+  //     case '5': //
+  //       this.rank = 2; // 2등
+  //     case '4':
+
+  //   }
+  //   this.rank = rank;
+  // }
 }
