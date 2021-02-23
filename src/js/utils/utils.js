c59832cb6d6d1397e994b57161bbf71aceb31d7e
zigsong <wldms5764@gmail.com>
2021-02-23 14:22:34 +0900

    refactor : update default export to  export

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,5 +1,5 @@
 import { LOTTO_NUMBERS } from './constants.js';
 
-export default function getRandomNumber() {
+export function getRandomNumber() {
   return Math.floor(Math.random() * LOTTO_NUMBERS.LOTTO_MAX_NUM) + 1;
 }
