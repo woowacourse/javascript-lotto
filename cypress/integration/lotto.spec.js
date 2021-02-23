d1ebfc76a6d33e91f656066919f5ba14bc1fd121
권세진 <0307kwon@gmail.com>
2021-02-23 17:31:35 +0900

    test: 당첨 번호와 보너스 번호는 1~45의 숫자를 가진다.

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -97,23 +97,38 @@ context('로또 UI 테스트', () => {
     });
   });
 
-  context('로또 기능 테스트', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:5500');
-    });
+  it('당첨 번호와 보너스 번호는 1~45의 숫자를 가진다.', () => {
+    const alertStub = cy.stub();
+    cy.on('window:alert', alertStub);
 
-    it('로또 자동 구매 시, 1 ~ 45 중 중복 없이 무작위 6개 숫자를 뽑아 저장한다.', () => {
-      const money = 5000;
-      const lotto = new Lotto();
-      lotto.createNumbers();
-      const amountTestSet = new Set(lotto.getNumbers());
-      expect(amountTestSet.size === LOTTO_SETTINGS.LOTTO_NUMBER_SIZE).to.equal(true);
+    const money = 3000;
+    cy.get(`.${DOM_CLASSES.MONEY_FORM_INPUT}`).type(money);
+    cy.get(`.${DOM_CLASSES.MONEY_FORM_SUBMIT}`).click();
 
-      for (let i = LOTTO_SETTINGS.MIN_LOTTO_NUMBER; i <= LOTTO_SETTINGS.MAX_LOTTO_NUMBER; i++) {
-        const randomNumber = lotto.getRandomNumber(LOTTO_SETTINGS.MIN_LOTTO_NUMBER, i);
-        expect(randomNumber >= LOTTO_SETTINGS.MIN_LOTTO_NUMBER && randomNumber <= i).to.equal(true);
-      }
+    typeLottoNumbers([55, 65, 75, 85, 95, 105, 115]);
+
+    cy.get(`.${DOM_CLASSES.RESULT_INPUT_SUBMIT}`).click().then(() => {
+      expect(alertStub.getCall(0)).to.be.calledWith(ALERT_MESSAGES.NUMBERS_OUT_OF_RANGE);
     });
+  });
+});
+
+context('로또 기능 테스트', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500');
+  });
+
+  it('로또 자동 구매 시, 1 ~ 45 중 중복 없이 무작위 6개 숫자를 뽑아 저장한다.', () => {
+    const money = 5000;
+    const lotto = new Lotto();
+    lotto.createNumbers();
+    const amountTestSet = new Set(lotto.getNumbers());
+    expect(amountTestSet.size === LOTTO_SETTINGS.LOTTO_NUMBER_SIZE).to.equal(true);
+
+    for (let i = LOTTO_SETTINGS.MIN_LOTTO_NUMBER; i <= LOTTO_SETTINGS.MAX_LOTTO_NUMBER; i++) {
+      const randomNumber = lotto.getRandomNumber(LOTTO_SETTINGS.MIN_LOTTO_NUMBER, i);
+      expect(randomNumber >= LOTTO_SETTINGS.MIN_LOTTO_NUMBER && randomNumber <= i).to.equal(true);
+    }
   });
 });
 
