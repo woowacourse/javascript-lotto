436b1ab469e151c9275d84cdaa17a80d3affb68d
jho2301 <jho2301@gmail.com>
2021-02-23 17:03:20 +0900

    docs: 기능 구현 목록 작성

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
-import App from './components/App.js';
-import { $ } from './lib/utils/dom.js';
-
-new App($('#app'));
-=======
-const $showResultButton = document.querySelector(".open-result-modal-button");
-const $modalClose = document.querySelector(".modal-close");
-const $modal = document.querySelector(".modal");
+const $showResultButton = document.querySelector('.open-result-modal-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
 const $lottoNumbersToggleButton = document.querySelector(
-  ".lotto-numbers-toggle-button"
+  '.lotto-numbers-toggle-button'
 );
 
 const onModalShow = () => {
-  $modal.classList.add("open");
+  $modal.classList.add('open');
 };
 
 const onModalClose = () => {
-  $modal.classList.remove("open");
+  $modal.classList.remove('open');
 };
 
-$showResultButton.addEventListener("click", onModalShow);
-$modalClose.addEventListener("click", onModalClose);
->>>>>>> chore: prettier 세팅
+$showResultButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
