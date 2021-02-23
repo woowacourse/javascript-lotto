1b5267dd161c1b36f9c5812015d5cce7224a75ec
ddongule <mnk918p@gmail.com>
2021-02-23 13:56:06 +0900

    feat: 결과 확인 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,25 @@
+/* eslint-disable no-continue */
 /* eslint-disable no-use-before-define */
 /* eslint-disable spaced-comment */
 
+import { lottoData } from './model.js';
 import $ from './utils/dom.js';
 
-// const $showResultButton = $('.open-result-modal-button');
-// const $modalClose = $('.modal-close');
-// const $modal = $('.modal');
-// const $lottoNumbersToggleButton = $(
-//   '.lotto-numbers-toggle-button'
-// );
+const $showResultButton = $('.open-result-modal-button');
+const $modalClose = $('.modal-close');
+const $modal = $('.modal');
+const $lottoNumbersToggleButton = $('.lotto-numbers-toggle-button');
 
-// const onModalShow = () => {
-//   $modal.classList.add('open');
-// };
+const onModalShow = () => {
+  $modal.classList.add('open');
+};
 
-// const onModalClose = () => {
-//   $modal.classList.remove('open');
-// };
+const onModalClose = () => {
+  $modal.classList.remove('open');
+};
 
-// $showResultButton.addEventListener('click', onModalShow);
-// $modalClose.addEventListener('click', onModalClose);
+$showResultButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
 
 ///////// 구입 금액
 const TICKET_PRICE = 1000;
@@ -30,15 +30,19 @@ paymentForm.addEventListener('submit', event => {
   const paymentInput = event.target.elements['payment-input'].value;
 
   ///////// 티켓 생성
-  const ticketAmount = Math.floor(Number(paymentInput) / TICKET_PRICE);
-  const ticketNumbers = [...Array(ticketAmount)].map(() => getTicketNumber());
+  lottoData.ticketAmount = Math.floor(Number(paymentInput) / TICKET_PRICE);
+  lottoData.tickets = [...Array(lottoData.ticketAmount)].map(() =>
+    getTicketNumber()
+  );
 
-  const ticketsHTML = ticketNumbers
+  const ticketsHTML = lottoData.tickets
     .map(ticketNumber => createTicktHTML(ticketNumber))
     .join('');
 
   $('#ticket-list').innerHTML = ticketsHTML;
-  $('#ticket-count').innerHTML = `총 ${ticketAmount}개를 구매하였습니다.`;
+  $(
+    '#ticket-count'
+  ).innerHTML = `총 ${lottoData.ticketAmount}개를 구매하였습니다.`;
   $('#toggle-detail-mode').classList.remove('hide');
 });
 
@@ -52,7 +56,7 @@ function createTicktHTML(ticketNumber) {
 function getTicketNumber() {
   const ticketNumber = new Set();
 
-  while (ticketNumber.size <= 6) {
+  while (ticketNumber.size < 6) {
     ticketNumber.add(getRandomNumber(1, 45));
   }
 
@@ -73,4 +77,103 @@ $('#toggle-detail-mode').addEventListener('change', event => {
     $('#ticket-list').classList.remove('detail-mode');
     $('#ticket-list').classList.add('d-flex');
   }
+});
+
+// 결과 확인하기
+
+$('#lotto-number-form').addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth,
+    bonus,
+  } = event.target.elements;
+  const winningNumber = {
+    main: [first, second, third, fourth, fifth, sixth].map(({ value }) =>
+      Number(value)
+    ),
+    bonus: Number(bonus.value),
+  };
+
+  const winners = getWinners(lottoData.tickets, winningNumber);
+  const profitPercent = getProfitPercent(winners, lottoData.ticketAmount);
+  setResultModal(winners, profitPercent);
+});
+
+function getWinners(tickets, winningNumber) {
+  const winners = {
+    first: 0,
+    second: 0,
+    third: 0,
+    fourth: 0,
+    fifth: 0,
+  };
+
+  tickets.forEach(ticket => {
+    const rank = getRank(ticket, winningNumber);
+    if (rank === 'loser') return;
+
+    winners[rank] += 1;
+  });
+
+  return winners;
+}
+
+function getRank(ticket, winningNumber) {
+  const score = ticket
+    .map(number => winningNumber.main.includes(number))
+    .filter(isTrue => isTrue).length;
+
+  if (score === 5 && ticket.includes(winningNumber.bonus)) {
+    return 'second';
+  }
+
+  if (score < 3) {
+    return 'loser';
+  }
+
+  return ['fifth', 'fourth', 'third', 'first'][score - 3];
+}
+
+function getProfitPercent(winners, ticketAmount) {
+  const ticketPrize = {
+    first: 2000000000,
+    second: 30000000,
+    third: 1500000,
+    fourth: 50000,
+    fifth: 5000,
+  };
+  let totalProfit = 0;
+  const paymentAmount = ticketAmount * 1000;
+
+  for (const winner in winners) {
+    if (Object.hasOwnProperty.call(winners, winner)) {
+      totalProfit += winners[winner] * ticketPrize[winner];
+    }
+  }
+
+  return ((totalProfit - paymentAmount) / paymentAmount) * 100;
+}
+
+function setResultModal(winners, profitPercent) {
+  $('#fifth').innerText = `${winners.fifth}개`;
+  $('#fourth').innerText = `${winners.fourth}개`;
+  $('#third').innerText = `${winners.third}개`;
+  $('#second').innerText = `${winners.second}개`;
+  $('#first').innerText = `${winners.first}개`;
+  $('#profit').innerText = `당신의 총 수익률은 ${profitPercent}%입니다.`;
+}
+
+$('#reset').addEventListener('click', () => {
+  onModalClose();
+  $('#ticket-list').innerHTML = '';
+  lottoData.ticketAmount = 0;
+  lottoData.tickets = [];
+  $('#ticket-count').innerHTML = ``;
+  $('#toggle-detail-mode').classList.add('hide');
+  $('input[name=payment-input]').value = '';
 });
