3ecccba855fee4ff3ced89d7f3098821e49e6912
권세진 <0307kwon@gmail.com>
2021-02-23 17:24:32 +0900

    feat: 당첨 번호와 보너스 번호는 중복이 있어서는 안된다.

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,5 +1,7 @@
 export const LOTTO_SETTINGS = {
   LOTTO_NUMBER_SIZE: 6,
+  BONUS_NUMBER_SIZE: 1,
+
   MIN_LOTTO_NUMBER: 1,
   MAX_LOTTO_NUMBER: 45,
   LOTTO_PRICE: 1000,
@@ -12,6 +14,9 @@ export const UI_SETTINGS = {
 export const ALERT_MESSAGES = {
   UNDER_MIN_PRICE: `최소 ${LOTTO_SETTINGS.LOTTO_PRICE}원 이상의 금액을 입력해야 합니다.`,
   NOT_INTEGER_PRICE: `구입 금액은 정수로 입력해야합니다.`,
+
+  EMPTY_RESULT_INPUT: '당첨 번호와 보너스 번호를 입력해주세요.',
+  DUPLICATED_NUMBERS_EXIST: '로또 번호에 중복이 있습니다.',
 }
 
 export const DOM_CLASSES = {
--- src/js/utils/validation.js
+++ src/js/utils/validation.js
@@ -5,3 +5,8 @@ export function isMoneyNotInteger(money) {
 export function isResultInputsEmpty(numbers) {
   return numbers.some(number => number === 0);
 }
+
+export function isNumbersDuplicated(numbers) {
+  const set = new Set(numbers);
+  return numbers.length !== set.size;
+}
