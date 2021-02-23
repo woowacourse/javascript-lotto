6d60dc613dc1fc55b6ecbd06e9bba9e030662a97
ChayanRin <k852012@naver.com>
2021-02-23 15:08:45 +0900

    refactor: detail 관리를 모델에서 뷰로 변경

--- src/js/models/lotto_model.js
+++ src/js/models/lotto_model.js
@@ -1,36 +1,18 @@
 class LottoModel {
   constructor() {
     this._lottos = []
-    this._detail = false
   }
 
   get lottos() {
     return this._lottos
   }
 
-  set lottos(newLottos) {
-    this._lottos = newLottos
-  }
-
   addLotto(newLotto) {
     this._lottos.push(newLotto)
   }
 
-  get detail() {
-    return this._detail
-  }
-
-  toggleDetail() {
-    this._detail = !this._detail
-  }
-
-  get amount() {
-    return this._lottos.length
-  }
-
   init() {
     this._lottos = []
-    this._detail = false
   }
 }
 export default LottoModel
