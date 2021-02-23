b1182c66c5028cfc31ffad7645ea1c25fa952426
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-23 15:22:15 +0900

    feat :  show modal and check unique validation

--- src/js/utils/lottoValidation.js
+++ src/js/utils/lottoValidation.js
@@ -6,3 +6,8 @@ export const isCorrectPurchaseUnit = input => {
     input / LOTTO_NUMBERS.LOTTO_UNIT
   );
 };
+
+export const isUniqueWinningNumber = input => {
+  const uniqueNums = new Set(Object.values(input));
+  return uniqueNums.size === LOTTO_NUMBERS.WINNING_NUMBER_COUNT; //
+};
