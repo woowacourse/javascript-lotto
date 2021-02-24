6844a0e1650454a4054175b4f11a3e7e646d3a83
ChayanRin <k852012@naver.com>
2021-02-24 17:14:31 +0900

    feat: lottoResult 구현

--- src/js/models/lotto_model.js
+++ src/js/models/lotto_model.js
@@ -50,6 +50,14 @@ class LottoModel {
       numbers: null,
       bonus: null,
     }
+
+    this._winnerLotto = {
+      "1st": { price: 2000000000, count: 0 },
+      "2nd": { price: 30000000, count: 0 },
+      "3rd": { price: 1500000, count: 0 },
+      "4th": { price: 50000, count: 0 },
+      "5th": { price: 5000, count: 0 },
+    }
   }
 
   get lottos() {
@@ -69,6 +77,44 @@ class LottoModel {
       numbers,
       bonus,
     }
+  }
+
+  lottoResult() {
+    this._lottos.forEach((lotto) => {
+      const match = lotto.numbers.filter((x) =>
+        this._answerLotto.numbers.includes(x)
+      ).length
+      const bonusMatch = lotto.numbers.includes(this._answerLotto.bonus)
+
+      if (match === 6) {
+        this._winnerLotto["1st"] = {
+          price: 2000000000,
+          count: this._winnerLotto["1st"].countcount + 1,
+        }
+      } else if (match === 5 && bonusMatch) {
+        this._winnerLotto["2nd"] = {
+          price: 30000000,
+          count: this._winnerLotto["2nd"].count + 1,
+        }
+      } else if (match === 5 || (match === 4 && bonusMatch)) {
+        this._winnerLotto["3rd"] = {
+          price: 1500000,
+          count: this._winnerLotto["3rd"].count + 1,
+        }
+      } else if (match === 4 || (match === 3 && bonusMatch)) {
+        this._winnerLotto["4th"] = {
+          price: 50000,
+          count: this._winnerLotto["4th"].count + 1,
+        }
+      } else if (match === 3 || (match === 2 && bonusMatch)) {
+        this._winnerLotto["5th"] = {
+          price: 5000,
+          count: this._winnerLotto["5th"].count + 1,
+        }
+      }
+    })
+
+    console.log(this._winnerLotto)
   }
 
   init() {
