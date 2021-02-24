0c84ddd69a8ab749186088ad3a5e376a86327321
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-24 19:17:14 +0900

    refactor : winning-number, bonus-number을 correct-number로 합침

--- src/lotto/domReader.js
+++ src/lotto/domReader.js
@@ -2,14 +2,7 @@ import { $correctNumberWrapper } from '../elements.js';
 import { $ } from '../utils/querySelector.js';
 
 export const getCorrectNumbers = () => {
-  const winningNumbers = $('.winning-number', $correctNumberWrapper)
+  return $('.correct-number', $correctNumberWrapper)
     .filter(({ value }) => value !== '')
     .map(({ value }) => Number(value));
-  const bonusNumberInput = $('.bonus-number', $correctNumberWrapper).value;
-  const correctNumbers =
-    bonusNumberInput === ''
-      ? [...winningNumbers]
-      : [...winningNumbers, Number(bonusNumberInput)];
-
-  return correctNumbers;
 };
