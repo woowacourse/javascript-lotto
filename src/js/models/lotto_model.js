2e7537fb2321f639ccbdf21eff7647705914ba22
ChayanRin <k852012@naver.com>
2021-02-24 16:29:56 +0900

    feat: 당첨번호 모델에 저장, 불러오기 구현

--- src/js/models/lotto_model.js
+++ src/js/models/lotto_model.js
@@ -46,14 +46,29 @@ import { INIT } from "../constants/constant.js"
 class LottoModel {
   constructor() {
     this._lottos = []
+    this._answerLotto = {
+      numbers: null,
+      bonus: null,
+    }
   }
 
   get lottos() {
     return this._lottos
   }
 
+  get answerLotto() {
+    return this._answerLotto
+  }
+
   addLotto(newLotto) {
     this._lottos.push(newLotto)
+  }
+
+  addAnswerLotto(numbers, bonus) {
+    this._answerLotto = {
+      numbers,
+      bonus,
+    }
   }
 
   init() {
