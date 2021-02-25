41479ee648727c0a0a2011bdc60c667071887ee1
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-25 15:35:59 +0900

    refactor : $함수에서 인덱스 대신 명시적으로 변경

--- src/utils/querySelector.js
+++ src/utils/querySelector.js
@@ -1,6 +1,6 @@
 const $ = (selector, target = document) => {
   const all = target.querySelectorAll(selector);
-  return all.length > 1 ? [...all] : all[0];
+  return all.length > 1 ? [...all] : document.querySelector(selector);
 };
 
 export { $ };
