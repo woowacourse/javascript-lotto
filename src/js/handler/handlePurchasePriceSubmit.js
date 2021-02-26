37a63fc7c7fb47fa51a5fab29965d7aa923ad8db
yujo <yujo@student.42seoul.kr>
2021-02-26 23:35:25 +0900

    refactor: 사용하지 않는 부분 제거

--- src/js/handler/handlePurchasePriceSubmit.js
+++ src/js/handler/handlePurchasePriceSubmit.js
@@ -1,19 +1,8 @@
-import Ticket from '../model/Ticket.js';
 import { $ } from '../utils/querySelector.js';
-import { ERR_MESSAGE, VALUE } from '../utils/constant.js';
-import { renderPurchaseResultSection } from '../view/viewPurchaseResultSection.js';
-import { showWinningNumberInputForm } from '../view/viewWinningNumberInputForm.js';
+import { disabledElement } from '../utils/setProperty.js';
+import { ERR_MESSAGE, MESSAGE, VALUE } from '../utils/constant.js';
 import { renderPurchaseSection } from '../view/viewPurchaseSection.js';
-
-const setLotto = (lotto, amountOfLottoTicket) => {
-  const purchasePrice = amountOfLottoTicket * VALUE.LOTTO.TICKET_PRICE;
-
-  lotto.setPurchasePrice(purchasePrice);
-
-  while (amountOfLottoTicket--) {
-    lotto.addTicket(new Ticket());
-  }
-};
+import { showWinningNumberInputForm } from '../view/viewWinningNumberInputForm.js';
 
 const isValidPrice = (price) => {
   return price >= VALUE.LOTTO.TICKET_PRICE;
@@ -28,24 +17,15 @@ export const handlePurchasePriceSubmit = (lotto) => {
     return;
   }
 
-  const amountOfLottoTicket = Math.floor(
-    purchasePrice / VALUE.LOTTO.TICKET_PRICE,
-  );
   if (smallChange) {
     alert(MESSAGE.LOTTO.SMALL_CHANGE(smallChange));
   }
+
   lotto.setPurchasePrice(purchasePrice - smallChange);
   lotto.setPurchaseBudget(purchasePrice - smallChange);
 
-  setLotto(lotto, amountOfLottoTicket);
-
-  const lottoTicketNumbers = lotto
-    .getTickets()
-    .map((ticket) => ticket.getNumbers());
-
-  renderPurchaseResultSection(amountOfLottoTicket, lottoTicketNumbers);
   showWinningNumberInputForm();
-  $('.winning-number').focus();
-  console.log(lotto);
   renderPurchaseSection(lotto);
+  disabledElement($('#purchase-price-input-form__button'));
+  $('#auto-purchase-input-form__input').focus();
 };
