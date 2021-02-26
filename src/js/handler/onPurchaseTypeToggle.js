8d34e38ad69de6192a2cd4a623da81263c2d9b8d
yujo <yujo@student.42seoul.kr>
2021-02-26 23:39:17 +0900

    feat: 접근성을 위한 input form focus 기능

--- src/js/handler/onPurchaseTypeToggle.js
+++ src/js/handler/onPurchaseTypeToggle.js
@@ -1,8 +1,19 @@
+import { $ } from '../utils/querySelector.js';
 import {
   renderAutoPurchaseForm,
   renderManualPurchaseForm,
 } from '../view/viewPurchaseSection.js';
 
+const setAutoManualPurchaseForm = () => {
+  renderAutoPurchaseForm();
+  $('#auto-purchase-input-form__input').focus();
+};
+
+const setManualPurchaseForm = () => {
+  renderManualPurchaseForm();
+  $('.choose-number').focus();
+};
+
 export const onPurchaseTypeToggle = ({ target }) => {
-  target.checked ? renderManualPurchaseForm() : renderAutoPurchaseForm();
+  target.checked ? setManualPurchaseForm() : setAutoManualPurchaseForm();
 };
