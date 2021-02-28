6e284950c4ea6b5a1b865d0f160584e430b21887
yungo1846 <yungo1468@naver.com>
2021-02-28 22:13:36 +0900

    feat: add manual purchase

--- src/js/constants/alertMessage.js
+++ src/js/constants/alertMessage.js
@@ -4,3 +4,4 @@ export const MSG_INVALID_PURCHASE_AMOUNT = `금액을 ${UNIT_AMOUNT}원 단위�
 export const MSG_DUPLICATED_LOTTO_NUMBERS = `중복된 숫자를 입력하셨습니다. 다시 입력해주세요!`;
 export const MSG_OUT_RANGED_LOTTO_NUMBERS = `범위 밖의 숫자를 입력하셨습니다. 1부터 45 사이의 숫자를 입력해주세요!`;
 export const MSG_BLANK_INPUT = `빈 칸을 입력하셨습니다. 다시 입력해주세요!`;
+export const MSG_NOT_ENOUGH_MONEY = `남은 잔액이 부족합니다. 잔액을 확인해주세요`;
--- src/js/view/LottoView.js
+++ src/js/view/LottoView.js
@@ -17,7 +17,7 @@ export class LottoView {
         return (html += ` 
           <div class="lotto-wrapper d-flex items-start">
             <span class="lotto mx-1 text-4xl">🎟️ </span>
-            <span data-lotto-numbers=${idx} class="mx-1 text-2xl d-none">
+            <span class="lotto-numbers mx-1 text-2xl d-none">
               ${lotto.numbers.join(', ')}
             </span>
           </div>
