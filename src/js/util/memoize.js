55a6f1386bfaea2915a04e5171ea90755d612968
Kiyung Kim <yungo1468@naver.com>
2021-02-28 15:40:58 +0900

    refactor: delete console.log

--- src/js/util/memoize.js
+++ src/js/util/memoize.js
@@ -2,7 +2,6 @@ export function memoize(callback) {
   const cache = new Map();
 
   return function (arg) {
-    console.log(cache.get(arg));
     if (!cache.get(arg)) {
       cache.set(arg, new callback(arg));
     }
