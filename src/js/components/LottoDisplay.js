35d33ed3c5c25c9b51e4e28c27d1fa74e2be57c7
zereight <fullstackmachinelearning@gmail.com>
2021-02-28 12:56:58 +0900

    fix: 페이지 넘버 초기화 안되는 이슈 수정

--- src/js/components/LottoDisplay.js
+++ src/js/components/LottoDisplay.js
@@ -123,6 +123,8 @@ export default class LottoDisplay extends Component {
     this.$target.classList.add('d-none');
     this.$lottoDisplayArea.innerHTML = '';
     this.$toggleButton.checked = false;
+    this.onToggleSwitch();
+    this.$pageNumber.textContent = 1;
   }
 
   toggleActivationPageMoveButton(pageIndex) {
