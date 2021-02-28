0f686f4c6297646c167cdcc80b7be63ef8457ad2
Kyle <mkitigy@gmail.com>
2021-02-28 09:46:39 +0900

    chores: init configurations - eslint - prettier - cypress

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-new': 'off',
+    'newline-before-return': 'error',
+    'class-methods-use-this': 'off',
+    'import/no-named-as-default': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'no-prototype-builtins': 'off',
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', 15],
+    'linebreak-style': [
+      'error',
+      require('os').EOL === '\r\n' ? 'windows' : 'unix',
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
