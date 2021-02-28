6bd2662afcf8107b50bca2a6cd071b979ff1c48e
ChayanRin <k852012@naver.com>
2021-02-28 14:15:15 +0900

    test: 자동구매 과정 메서드화 및 적용

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -31,8 +31,10 @@ class CypressWrapper {
 }
 const cw = new CypressWrapper()
 
-const submitBuy = (value) => {
-  cw.type(SELECTOR.BUY_INPUT, value).click(SELECTOR.BUY_BUTTON)
+const submitAutoBuy = (value) => {
+  cw.type(SELECTOR.BUY_INPUT, value)
+    .click(SELECTOR.BUY_BUTTON)
+    .click("auto-button")
 }
 
 const testDetail = () => {
@@ -65,28 +67,30 @@ describe("ui-play", () => {
 
   it("사이트 접속시에 제목과 구입 금액 영역만 보여진다.", () => {
     cy.get(SELECTOR.BUY_SECTION).children().should("exist")
+    cw.get("buy-method").children().should("not.exist")
     cy.get(SELECTOR.POCKET_SECTION).children().should("not.exist")
     cy.get(SELECTOR.WINNING_SECTION).children().should("not.exist")
   })
 
   it("금액을 입력하고 버튼을 클릭하면 수동 구매 영역이 보여진다", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     cw.get("buy-method").children().should("exist")
   })
 
-  it("금액을 입력하고 버튼을 클릭하면 구매 내역 영역과 당첨 번호 확인 영역이 보여진다", () => {
-    submitBuy(5000)
+  it("금액을 입력하고 확인 버튼, 나머지 자동구매 버튼을 클릭하면 수동 구매 영역 감춰지고 구매 내역 영역과 당첨 번호 확인 영역이 보여진다", () => {
+    submitAutoBuy(5000)
+    cw.get("buy-method").children().should("not.exist")
     cy.get(SELECTOR.POCKET_SECTION).children().should("exist")
     cy.get(SELECTOR.WINNING_SECTION).children().should("exist")
   })
 
   it("금액을 입력하고 버튼을 클릭하면 입력 금액/1000 개의 로또 이모지가 보여진다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     cy.get(SELECTOR.POCKET_LOTTOS).children().its("length").should("eq", 5)
   })
 
   it("번호보기 토글 버튼을 클릭하면 각 로또 이모지와 로또 번호 6자리가 보여진다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     testDetail()
     cy.get(SELECTOR.POCKET_LOTTO_NUMBERS).each(($numbers) => {
       expect($numbers.text().split(" ").length).to.eq(6)
@@ -94,7 +98,7 @@ describe("ui-play", () => {
   })
 
   it("번호보기 토클 버튼을 클릭했을 때 나오는 각 로또 번호들은 서로 달라야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     testDetail()
     cy.get(SELECTOR.POCKET_LOTTO_NUMBERS).each(($numbers) => {
       expect(new Set($numbers.text().split(" ")).size).to.eq(6)
@@ -102,7 +106,7 @@ describe("ui-play", () => {
   })
 
   it("번호보기 토클 버튼을 클릭했을 때 나오는 각 로또 번호들은 1이상 45이하 사이여야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     testDetail()
     cy.get(SELECTOR.POCKET_LOTTO_NUMBERS).each(($numbers) => {
       cy.get($numbers.text().split(" ")).each(($number) => {
@@ -112,13 +116,13 @@ describe("ui-play", () => {
   })
 
   it("결과 확인하기 버튼을 누르면 모달 창이 보여진다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, 3, 4, 5, 6], 7)
     cw.should(SELECTOR.MODAL_SECTION, "have.class", "open")
   })
 
   it("당첨된 로또의 총 개수는 구매한 로또의 총 개수보다 작거나 같아야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, 3, 4, 5, 6], 7)
 
     let winner = 0
@@ -132,13 +136,13 @@ describe("ui-play", () => {
   })
 
   it("수익률은 -100% 미만일 수 없다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, 3, 4, 5, 6], 7)
     testProfit("be.gte", -100)
   })
 
   it("다시 시작하기 버튼을 누르면 제목과 구입 금액 영역만 보여진다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, 3, 4, 5, 6], 7)
     cw.click(SELECTOR.RESET_BUTTON)
     cy.get(SELECTOR.BUY_SECTION).children().should("exist")
@@ -147,7 +151,7 @@ describe("ui-play", () => {
   })
 
   it("구매한 번호와 당첨 번호가 6개 일치할 때 당첨 개수와 수익률이 기대한 대로 출력된다.", () => {
-    submitBuy(1000)
+    submitAutoBuy(1000)
     cy.get(SELECTOR.POCKET_LOTTO_NUMBERS).then(($numbers) => {
       const numbers = $numbers.text().split(" ")
       let bonusNum = 1
@@ -177,7 +181,7 @@ describe("ui-exception", () => {
   })
 
   it("금액에 소수점을 입력했을때 alert가 발생한다", () => {
-    submitBuy(432.13)
+    submitAutoBuy(432.13)
     cw.should(
       `@${ALERT_STUB}`,
       "be.calledWith",
@@ -186,7 +190,7 @@ describe("ui-exception", () => {
   })
 
   it("금액에 음수를 입력했을때 alert가 발생한다", () => {
-    submitBuy(-1000)
+    submitAutoBuy(-1000)
     cw.should(
       `@${ALERT_STUB}`,
       "be.calledWith",
@@ -195,7 +199,7 @@ describe("ui-exception", () => {
   })
 
   it("금액에 1000원 미만을 입력했을때 alert가 발생한다", () => {
-    submitBuy(500)
+    submitAutoBuy(500)
 
     cw.should(
       `@${ALERT_STUB}`,
@@ -205,7 +209,7 @@ describe("ui-exception", () => {
   })
 
   it("당첨 번호를 전부 입력하지 않은 상태로 결과 확인하기 버튼을 누르면 alert가 발생한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, "", 4, "", 6], 7)
     cw.should(
       `@${ALERT_STUB}`,
@@ -215,7 +219,7 @@ describe("ui-exception", () => {
   })
 
   it("당첨 번호에 중복되는 숫자가 있으면 alert가 발생해야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2, 3, 4, 4, 26], 17)
     cw.should(
       `@${ALERT_STUB}`,
@@ -225,7 +229,7 @@ describe("ui-exception", () => {
   })
 
   it("당첨 번호가 1이상 45이하가 아닌 숫자가 있을때 alert가 발생해야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([40, 41, 42, 43, 44, 46], -1)
     cw.should(
       `@${ALERT_STUB}`,
@@ -235,7 +239,7 @@ describe("ui-exception", () => {
   })
 
   it("당첨 번호에 소수가 있으면 alert가 발생해야 한다.", () => {
-    submitBuy(5000)
+    submitAutoBuy(5000)
     submitAnswer([1, 2.3, 4.5, 6, 34, 43], 3.14)
     cw.should(
       `@${ALERT_STUB}`,
