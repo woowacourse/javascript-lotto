18d57c19152dd9fba543607c85377bc23526a1d4
zigsong <wldms5764@gmail.com>
2021-03-01 15:59:53 +0900

    refactor: update array loop logic in manualInputView

--- src/js/views/ManualInputView.js
+++ src/js/views/ManualInputView.js
@@ -17,40 +17,39 @@ export default class ManualInputView extends View {
   }
 
   createManualLottos() {
-    const lottoTicekts = [...Array(this.count)]
-      .map(
-        (_, idx) => `
-          <li id="manual-wrapper-${idx}" class="mx-1 my-2 text-4xl manual-wrapper">
-            <form class="d-flex items-center justify-between manual-input-form">
-              <span class="lotto-icon">🎟️ </span>
-              ${this.createManualInput()}
-              <button type="submit" class="btn btn-cyan btn-small manual-input-btn">확정</button>
-            </form>
-          </li>
-        `
-      )
-      .join('');
+    // const lottoTickets = Array.from({length: this.count}, (_, idx) => `<li>....</li>`).join('')
 
+    const lottoTicekts = Array.from(
+      { length: this.count },
+      (_, idx) =>
+        `<li id="manual-wrapper-${idx}" class="mx-1 my-2 text-4xl manual-wrapper">
+          <form class="d-flex items-center justify-between manual-input-form">
+            <span class="lotto-icon">🎟️ </span>
+            ${this.createManualInput()}
+            <button type="submit" class="btn btn-cyan btn-small manual-input-btn">확정</button>
+          </form>
+        </li>
+      `
+    ).join('');
     this.$element.innerHTML = lottoTicekts;
     $('#input-price-form').insertAdjacentElement('afterend', this.$element);
   }
 
   createManualInput() {
-    return [...Array(LOTTO_NUMBERS.LOTTO_MANUAL_COUNT)]
-      .map(
-        (_, i) => `
-          <input
-            type="number"
-            class="manual-number mx-1 text-center"
-            aria-label="manual-number-${i}"
-            data-manual-index="${i}"
-            required
-            min="1"
-            max="45"
-          />
-        `
-      )
-      .join('');
+    return Array.from(
+      { length: LOTTO_NUMBERS.LOTTO_MANUAL_COUNT },
+      (_, i) =>
+        `<input
+          type="number"
+          class="manual-number mx-1 text-center"
+          aria-label="manual-number-${i}"
+          data-manual-index="${i}"
+          required
+          min="1"
+          max="45"
+        />
+      `
+    ).join('');
   }
 
   bindManualInputEvent() {
