e5a25406efbaeda3886681bb301eaab35e4d0042
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-03-01 19:12:32 +0900

    style : 파일 명 변경 view => View

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import LottoGame from './lotto/LottoGame.js';
 import LottoController from './lotto/controller.js';
-import View from './lotto/views/view.js';
+import View from './lotto/views/View.js';
 
 const init = () => {
   const lottoController = new LottoController(new LottoGame(), new View());
