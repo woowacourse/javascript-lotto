ed53b479c7d635060d27a1ccd30c7a33f114cf35
ChayanRin <k852012@naver.com>
2021-03-01 14:39:47 +0900

    fix: 수동 입력 번호 공백에 대한 NaN 처리

--- src/js/components/manual.js
+++ src/js/components/manual.js
@@ -2,7 +2,7 @@ import { $$ } from "../util.js"
 
 export const getManualInput = () => {
   const numbers = [...$$(".manual-number")].map(({ value }) => {
-    return Number(value)
+    return value === "" ? NaN : Number(value)
   })
 
   return numbers
