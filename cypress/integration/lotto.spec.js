8caf3406aadecf3b55a4e323280b0971b2b1856e
yungo1846 <yungo1468@naver.com>
2021-03-01 16:05:24 +0900

    feat: add tests for step3 - purchase type toggle - manual purchase validation - winning statisctics

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -25,18 +25,49 @@ describe('Lotto test', () => {
     testInputValue(button);
   });
 
-  it('구입 금액 입력 뒤 버튼을 클릭했을 때, 알맞은 개수의 로또를 렌더링한다.', () => {
+  it('자동 구매가 정상적으로 동작하는지 확인한다.', () => {
+    typeInputValue('#auto-amount-input', 1);
+    testInputValue('#auto-amount-submit');
+  });
+
+  it('자동, 수동 토글 버튼이 정상적으로 동작하는지 확인한다.', () => {
+    cy.get('#auto-amount-purchase-form').should('be.visible');
+    cy.get('#purchase-type-switch').click();
+    cy.get('#manual-purchase-form').should('be.visible');
+  });
+
+  it('수동구매 시 부적절한 로또 번호를 검사한다. (중복)', () => {
+    testManualNumbers(['1', '2', '3', '4', '5', '5'], MSG_DUPLICATED_LOTTO_NUMBERS);
+  });
+
+  it('수동구매 시 부적절한 로또 번호를 검사한다. (범위 밖)', () => {
+    testManualNumbers(['0', '2', '3', '4', '5', '46'], MSG_OUT_RANGED_LOTTO_NUMBERS);
+  });
+
+  it('수동구매 시 부적절한 로또 번호를 검사한다. (미입력)', () => {
+    testManualNumbers(['1', '2', '3', '', '5', '6'], MSG_BLANK_INPUT);
+  });
+
+  it('수동구매 시 적절한 번호를 입력하여 구매한다.', () => {
+    testManualNumbers(['1', '2', '3', '4', '5', '6']);
+  });
+
+  it('로또 구매 시 남은 잔액이 정상적으로 뜨는지 확인한다.', () => {
+    cy.get('#current-money').contains('3000');
+  });
+
+  it('구매 개수만큼 로또 이미지를 렌더링한다.', () => {
     cy.get('#lotto-container') //
       .find('.lotto-wrapper')
-      .should('have.length', 5);
+      .should('have.length', 2);
   });
 
   it('토글 버튼을 클릭했을 때, 모든 복권 번호를 렌더링한다.', () => {
-    testLottoToggle(5, 'on');
-    testLottoToggle(5, 'off');
+    testLottoToggle(2, 'on');
+    testLottoToggle(2, 'off');
 
     function testLottoToggle(count, toggleOption) {
-      cy.get('.switch').click();
+      cy.get('#lotto-numbers-switch').click();
       for (let idx = 0; idx < count; idx++) {
         cy.get('.lotto-wrapper') //
           .eq(idx)
@@ -63,6 +94,13 @@ describe('Lotto test', () => {
     cy.get('#modal').should('be.visible');
   });
 
+  it('다시 시작하기 버튼을 누르면 초기화 된다.', () => {
+    cy.get('#reset-button').click();
+    typeInputValue('#purchase-amount-input', '5000');
+    testInputValue('#purchase-amount-submit');
+    testWinnigNumbers(['1', '2', '3', '4', '5', '6', '45']);
+  });
+
   function testWinnigNumbers(numbers, alertMessage = '') {
     numbers.forEach((number, idx) => {
       typeInputValue(`[data-winning-number=${idx}]`, number);
@@ -70,10 +108,12 @@ describe('Lotto test', () => {
     testInputValue('#result-submit', alertMessage);
   }
 
-  it('다시 시작하기 버튼을 누르면 초기화 된다.', () => {
-    cy.get('#reset-button').click();
-    typeInputValue('#purchase-amount-input', '5000');
-    testInputValue('#purchase-amount-submit');
-    testWinnigNumbers(['1', '2', '3', '4', '5', '6', '45']);
-  });
+  function testManualNumbers(numbers, alertMessage = '') {
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] !== '') {
+        cy.get('.lotto-number').eq(i).type(numbers[i]);
+      }
+    }
+    testInputValue('#manual-purchase-submit', alertMessage);
+  }
 });
