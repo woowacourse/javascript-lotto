abb6bf2458c85e9c71ac1e326887e2b79632bd6f
bigsaigon333 <likepeppermint@gmail.com>
2021-03-01 22:20:53 +0900

    test: HTML InputElement 속성 추가에 따라 테스트를 삭제한다
    - HTML InputElement에 required 속성 및 min, max 속성을 추가하여 Cypress에서는 빈 문자열을
    입력한 경우와 min, max를 벗어나는 값에 대한 테스트가 불가능하게 되었으므로, 해당 테스트를 삭제한다

--- cypress/integration/cashInput.spec.js
+++ cypress/integration/cashInput.spec.js
@@ -1,4 +1,4 @@
-import { ALERT_MESSAGE, JS_SELECTOR } from "../../src/js/constants/index.js";
+import { JS_SELECTOR } from "../../src/js/constants/index.js";
 import { Lotto } from "../../src/js/models/index.js";
 import { toDataAttributeSelector as toDAS } from "../../src/js/utils/querySelector.js";
 
@@ -11,19 +11,6 @@ describe("구입 금액 입력 테스트", () => {
     cy.window().then((win) => cy.stub(win, "alert").as("windowAlert"));
   });
 
-  const testAlertMessage = (wrongUserInput, alertMessage, callCount = 1) => {
-    if (wrongUserInput !== "") {
-      cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).type(wrongUserInput);
-    }
-
-    cy.get(toDAS(JS_SELECTOR.CASH.BUTTON)).click();
-
-    cy.get("@windowAlert")
-      .should("have.callCount", callCount++)
-      .its("lastCall")
-      .should("be.calledWith", alertMessage);
-  };
-
   it("초기화면에 구입 입력 Form이 보여진다.", () => {
     cy.get(toDAS(JS_SELECTOR.CASH.CONTAINER)).should("be.visible");
     cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).should("be.visible");
@@ -33,28 +20,6 @@ describe("구입 금액 입력 테스트", () => {
       "not.be.visible"
     );
     cy.get(".modal").should("not.be.visible");
-  });
-
-  it("유저가 숫자로 변환될 수 없는 문자를 입력한 경우, 에러메시지를 alert로 출력한다", () => {
-    ["", "김동희", "+", "abcd", "🎟️"].forEach((wrongUserInput, index) => {
-      testAlertMessage(
-        wrongUserInput,
-        ALERT_MESSAGE.ERROR.CASH_INPUT.NOT_A_NUMBER,
-        index + 1
-      );
-      cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).should("have.value", "");
-    });
-  });
-
-  it("유저가 1000원 미만의 금액을 입력한 경우, 에러메시지를 alert로 출력한다", () => {
-    [-300, 500].forEach((wrongUserInput, index) => {
-      testAlertMessage(
-        wrongUserInput,
-        ALERT_MESSAGE.ERROR.CASH_INPUT.UNDER_LOTTO_PRICE,
-        index + 1
-      );
-      cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).should("have.value", "");
-    });
   });
 
   it("유저가 유효한 금액을 입력한 경우, 구매한 로또 갯수만큼 아이콘 정보와 당첨번호 입력란을 보여준다", () => {
--- cypress/integration/winningNumberInput.spec.js
+++ cypress/integration/winningNumberInput.spec.js
@@ -17,27 +17,6 @@ describe("당첨 번호 입력 테스트", () => {
     cy.get(toDAS(JS_SELECTOR.CASH.BUTTON)).click();
   });
 
-  it("유저가 정상적으로 로또를 구매한 후 당첨번호를 모두 입력하지 않았을 때 에러메시지를 alert로 출력한다", () => {
-    const WINNING_NUMBERS = [" ", 5, 8, 34, 44, 45];
-    const BONUS_NUMBER = 7;
-
-    WINNING_NUMBERS.forEach((number, index) => {
-      const inputClassName = `${CLASSNAME.WINNING_NUMBER.INPUT}:nth-child(${
-        index + 1
-      })`;
-      cy.get(toCS(inputClassName)).type(number);
-    });
-    cy.get(toCS(CLASSNAME.WINNING_NUMBER.BONUS_INPUT)).type(BONUS_NUMBER);
-
-    cy.get(toCS(CLASSNAME.MODAL)).should("not.be.visible");
-    cy.get(toCS(CLASSNAME.WINNING_NUMBER.OPEN_RESULT_MODAL_BUTTON)).click();
-    cy.get("@windowAlert").should(
-      "be.calledWith",
-      ALERT_MESSAGE.ERROR.WINNING_NUMBERS_INPUT.EMPTY
-    );
-    cy.get(toCS(CLASSNAME.MODAL)).should("not.be.visible");
-  });
-
   it("유저가 정상적으로 로또를 구매한 후 중복된 당첨번호를 입력했을 때 에러메시지를 alert로 출력한다", () => {
     const WINNING_NUMBERS = [45, 5, 8, 34, 44, 45];
     const BONUS_NUMBER = 7;
@@ -54,7 +33,11 @@ describe("당첨 번호 입력 테스트", () => {
     cy.get(toCS(CLASSNAME.WINNING_NUMBER.OPEN_RESULT_MODAL_BUTTON)).click();
     cy.get("@windowAlert").should(
       "be.calledWith",
-      ALERT_MESSAGE.ERROR.WINNING_NUMBERS_INPUT.DUPLICATED
+      `'${[...WINNING_NUMBERS, BONUS_NUMBER].sort(
+        (a, b) => a - b
+      )}'중에 중복된 숫자가 있습니다. ${
+        ALERT_MESSAGE.ERROR.WINNING_NUMBERS_INPUT.DUPLICATED
+      }`
     );
     cy.get(toCS(CLASSNAME.MODAL)).should("not.be.visible");
   });
@@ -75,7 +58,11 @@ describe("당첨 번호 입력 테스트", () => {
     cy.get(toCS(CLASSNAME.WINNING_NUMBER.OPEN_RESULT_MODAL_BUTTON)).click();
     cy.get("@windowAlert").should(
       "be.calledWith",
-      ALERT_MESSAGE.ERROR.WINNING_NUMBERS_INPUT.DUPLICATED
+      `'${[...WINNING_NUMBERS, BONUS_NUMBER].sort(
+        (a, b) => a - b
+      )}'중에 중복된 숫자가 있습니다. ${
+        ALERT_MESSAGE.ERROR.WINNING_NUMBERS_INPUT.DUPLICATED
+      }`
     );
     cy.get(toCS(CLASSNAME.MODAL)).should("not.be.visible");
   });
