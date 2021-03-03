6ce92fddcf05a6e61857135bf389909676065260
0imbean0 <tjsqls2003@gmail.com>
2021-03-04 01:16:49 +0900

    refactor: 수동 입력 번호 데이터를 로또 모델에서 구매 모달 전역으로 이동

--- src/js/handler/handlePurchaseModal.js
+++ src/js/handler/handlePurchaseModal.js
@@ -1,21 +1,23 @@
 import Ticket from '../model/Ticket.js';
 import { $, $$ } from '../utils/querySelector.js';
 import { isDuplicate, isValidRange, isEqual } from '../utils/validator.js';
-import { ERR_MESSAGE } from '../utils/constant.js';
+import { ERROR_MESSAGE } from '../utils/constant.js';
 import { closeModal } from '../utils/setProperty.js';
 import { renderSelfResultTable } from '../view/viewPurchaseModal.js';
 import { renderPurchaseResultSection } from '../view/viewPurchaseResultSection.js';
 import { showWinningNumberInputForm } from '../view/viewWinningNumberInputForm.js';
 
+const selfTicketsNumbers = [];
+
 const setTickets = (lotto) => {
-  const selfTicketsNumbers = lotto.getSelves()
-  const amountOfRestTicket = lotto.getAmount() - selfTicketsNumbers.length;
+  const amountOfSelfTicket = selfTicketsNumbers.length;
+  const amountOfAutoTicket = lotto.getAmount() - amountOfSelfTicket;
 
-  selfTicketsNumbers.forEach((selfTicketNumbers) => {
-    lotto.addTicket(new Ticket(selfTicketNumbers));
-  });
+  for (let i = 0; i < amountOfSelfTicket; i++) {
+    lotto.addTicket(new Ticket(selfTicketsNumbers.pop()));
+  }
 
-  for (let i = 0; i < amountOfRestTicket; i++) {
+  for (let i = 0; i < amountOfAutoTicket; i++) {
     lotto.addTicket(new Ticket());
   }
 };
@@ -30,8 +32,8 @@ export const handlePurchaseModal = ({ target }, lotto) => {
   }
 
   if (target.id === 'purchase-modal__self-input-form') {
-    if (isEqual(lotto.getAmount(), lotto.getSelves().length)) {
-      return alert(ERR_MESSAGE.LOTTO.OVER_PURCHASE);
+    if (isEqual(lotto.getAmount(), selfTicketsNumbers.length)) {
+      return alert(ERROR_MESSAGE.LOTTO.OVER_PURCHASE);
     }
 
     const selfNumbers = [...$$('.self-number')]
@@ -39,15 +41,15 @@ export const handlePurchaseModal = ({ target }, lotto) => {
       .sort((a, b) => a - b);
 
     if (!isValidRange(selfNumbers)) {
-      return alert(ERR_MESSAGE.WINNING_NUMBER.OUT_OF_RANGE);
+      return alert(ERROR_MESSAGE.WINNING_NUMBER.OUT_OF_RANGE);
     }
 
     if (isDuplicate(selfNumbers)) {
-      return alert(ERR_MESSAGE.WINNING_NUMBER.DUPLICATE);
+      return alert(ERROR_MESSAGE.WINNING_NUMBER.DUPLICATE);
     }
 
-    lotto.addSelfNumbers(selfNumbers);
-    renderSelfResultTable(lotto.getSelves().length, selfNumbers);
+    selfTicketsNumbers.push(selfNumbers);
+    renderSelfResultTable(selfTicketsNumbers.length, selfNumbers);
 
     $$('.self-number').forEach((selfNumber) => {
       selfNumber.value = '';
--- src/js/model/Lotto.js
+++ src/js/model/Lotto.js
@@ -2,7 +2,6 @@ export default class Lotto {
   constructor() {
     this.amount = 0;
     this.tickets = [];
-    this.selves = []
   }
   setAmount(amount) {
     this.amount = amount;
@@ -20,17 +19,8 @@ export default class Lotto {
     return this.tickets;
   }
 
-  addSelfNumbers(selfNumbers) {
-    this.selves.push(selfNumbers);
-  }
-
-  getSelves() {
-    return this.selves
-  }
-
   initialize() {
     this.amount = 0;
     this.tickets = [];
-    this.selves = []
   }
 }
