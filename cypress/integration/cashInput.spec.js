3d4f22ed39a04474b63d3ad41c33c65a929ebec7
bigsaigon333 <likepeppermint@gmail.com>
2021-03-11 23:26:38 +0900

    test: 구입 금액 입력 테스트를 수정한다

--- cypress/integration/cashInput.spec.js
+++ cypress/integration/cashInput.spec.js
@@ -15,6 +15,7 @@ describe("구입 금액 입력 테스트", () => {
     cy.get(toDAS(JS_SELECTOR.CASH.CONTAINER)).should("be.visible");
     cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).should("be.visible");
     cy.get(toDAS(JS_SELECTOR.CASH.BUTTON)).should("be.visible");
+    cy.get(toDAS(JS_SELECTOR.ISSUE_MANAGER.CONTAINER)).should("not.be.visible");
     cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.CONTAINER)).should("not.be.visible");
     cy.get(toDAS(JS_SELECTOR.WINNING_NUMBER.CONTAINER)).should(
       "not.be.visible"
@@ -22,23 +23,23 @@ describe("구입 금액 입력 테스트", () => {
     cy.get(".modal").should("not.be.visible");
   });
 
-  it("유저가 유효한 금액을 입력한 경우, 구매한 로또 갯수만큼 아이콘 정보와 당첨번호 입력란을 보여준다", () => {
+  it("유저가 유효한 금액을 입력한 경우, 입력된 금액으로 살 수 있는 로또 장수 만큼에 대한 로또 번호 입력칸이 표시된다", () => {
     const userInput = 4500;
     const lottoCount = Math.floor(userInput / Lotto.UNIT_PRICE);
+
     cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).type(userInput);
     cy.get(toDAS(JS_SELECTOR.CASH.BUTTON)).click();
 
-    cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.CONTAINER)).should("be.visible");
-    cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.LABEL)).should(
+    cy.get(toDAS(JS_SELECTOR.ISSUE_MANAGER.CONTAINER)).should("be.visible");
+    cy.get(toDAS(JS_SELECTOR.ISSUE_MANAGER.TITLE)).should(
       "have.text",
-      `총 ${lottoCount}개를 구매하였습니다.`
-    );
-    cy.get(toDAS(JS_SELECTOR.LOTTO_DETAIL.ICON)).should(
-      "have.length",
-      lottoCount
+      `총 ${lottoCount}개의 로또를 발급가능합니다.`
     );
 
-    cy.get(toDAS(JS_SELECTOR.WINNING_NUMBER.CONTAINER)).should("be.visible");
+    cy.get(toDAS(JS_SELECTOR.ISSUE_MANAGER.ENTRY_LIST))
+      .children()
+      .should("have.length", lottoCount);
+
     cy.get(toDAS(JS_SELECTOR.CASH.INPUT)).should("have.value", "");
   });
 });
