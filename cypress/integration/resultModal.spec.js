e0e3c802113553e075a6af8050279769a592f944
bigsaigon333 <likepeppermint@gmail.com>
2021-03-13 15:10:36 +0900

    test: 당첨 결과 모달 테스트를 리팩토링한다

--- cypress/integration/resultModal.spec.js
+++ cypress/integration/resultModal.spec.js
@@ -37,39 +37,38 @@ describe("당첨 결과 모달 테스트: 당첨 결과에 대한 모달이 표�
   it("유저가 구매한 로또 정보에 대한 수익률이 결과모달에 표시된다", () => {
     let winningCounts = [];
     let winningMoneyUnits = [];
-    cy.get(toDAS(JS_SELECTOR.MODAL.WINNING_MONEY_UNIT))
-      .each(($winningMoneyUnit) => {
+    cy.get(toDAS(JS_SELECTOR.MODAL.WINNING_MONEY_UNIT)).each(
+      ($winningMoneyUnit) => {
         winningMoneyUnits.push(
           Number($winningMoneyUnit.text().trim().replace(/,/g, ""))
         );
-      })
-      .then(() => {
-        cy.get(toDAS(JS_SELECTOR.MODAL.WINNING_COUNT)).each(($winningCount) => {
-          winningCounts.push(Number($winningCount.text().trim().slice(0, -1)));
-        });
-      })
-      .then(() => {
-        const totalWinningMoney = Array.from({
-          length: winningMoneyUnits.length,
-        }).reduce((total, _, index) => {
-          return total + winningCounts[index] * winningMoneyUnits[index];
-        }, 0);
+      }
+    );
 
-        const investment = userInput - (userInput % Lotto.UNIT_PRICE);
-        const profitRate = totalWinningMoney / investment - 1;
-        const profitRateParagraph = `당신의 총 수익률은 ${profitRate.toLocaleString(
-          "en-US",
-          {
-            style: "percent",
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          }
-        )}입니다.`;
+    cy.get(toDAS(JS_SELECTOR.MODAL.WINNING_COUNT)).each(($winningCount) => {
+      winningCounts.push(Number($winningCount.text().trim().slice(0, -1)));
+    });
 
-        cy.get(toDAS(JS_SELECTOR.MODAL.PROFIT_RATE_PARAGRAPH)).should(
-          "have.text",
-          profitRateParagraph
-        );
-      });
+    const totalWinningMoney = Array.from({
+      length: winningMoneyUnits.length,
+    }).reduce((total, _, index) => {
+      return total + winningCounts[index] * winningMoneyUnits[index];
+    }, 0);
+
+    const investment = userInput - (userInput % Lotto.UNIT_PRICE);
+    const profitRate = totalWinningMoney / investment - 1;
+    const profitRateParagraph = `당신의 총 수익률은 ${profitRate.toLocaleString(
+      "en-US",
+      {
+        style: "percent",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      }
+    )}입니다.`;
+
+    cy.get(toDAS(JS_SELECTOR.MODAL.PROFIT_RATE_PARAGRAPH)).should(
+      "have.text",
+      profitRateParagraph
+    );
   });
 });
