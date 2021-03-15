8755d8860c1f037219b8f2ea0cdfc912e4b21ae0
jum0 <wnsah052@naver.com>
2021-03-16 01:05:15 +0900

    feat: 구입 금액을 잔액에서 확인하는 기능을 추가한다

--- src/js/PurchaseOption/PurchaseOption.js
+++ src/js/PurchaseOption/PurchaseOption.js
@@ -10,6 +10,7 @@ class PurchaseOption {
       this.manualNumberTicketCount = 0;
 
       this.showPurchaseOption();
+      this.render();
     });
   }
 
@@ -17,6 +18,22 @@ class PurchaseOption {
     $(ELEMENT.PURCHASE_OPTION_CONTAINER).classList.remove(ELEMENT.HIDDEN);
   }
 
+  render() {
+    this.renderBlance();
+    this.renderPurchaseStatus();
+  }
+
+  renderBlance() {
+    $(ELEMENT.PURCHASE_BALANCE_LABEL).innerText = `잔액 : ${Number(
+      this.money
+    ).toLocaleString()}원`;
+  }
+
+  renderPurchaseStatus() {
+    $(
+      ELEMENT.PURCHASE_STATUS_LABEL
+    ).innerText = `구매 현황: 자동 ${this.autoNumberTicketCount}장, 수동 ${this.manualNumberTicketCount} 장`;
+  }
 }
 
 export default PurchaseOption;
