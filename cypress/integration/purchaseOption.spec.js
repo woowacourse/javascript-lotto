8b4e4ecd0885b6f9e15ff99ff21459e58f626a31
jum0 <wnsah052@naver.com>
2021-03-16 04:12:07 +0900

    test: 수동 구매 버튼 누르는 상황을 테스트한다

--- cypress/integration/purchaseOption.spec.js
+++ cypress/integration/purchaseOption.spec.js
@@ -1,4 +1,4 @@
-import { ELEMENT } from "../../src/js/Util/constants.js";
+import { ALERT_MESSAGE, ELEMENT } from "../../src/js/Util/constants.js";
 
 describe("purchaseOption container를 테스트한다", () => {
   const money = "5000";
@@ -20,6 +20,33 @@ describe("purchaseOption container를 테스트한다", () => {
     cy.get(ELEMENT.AUTO_NUMBER_PURCHASE_BUTTON).click();
     cy.get(ELEMENT.PURCHASE_STATUS_LABEL)
       .invoke("text")
-      .should("eq", `구매 현황: 자동 1장, 수동 0 장`);
+      .should("eq", `구매 현황: 자동 1 장, 수동 0 장`);
+  });
+
+  it("올바른 번호 입력 후, 수동 구매 추가 버튼을 누르면 수동 1장이 추가된다.", () => {
+    cy.get(ELEMENT.MANUAL_NUMBER).then((els) => {
+      [...els].forEach((el, i) => cy.wrap(el).type(i + 1));
+    });
+    cy.get(ELEMENT.MANUAL_NUMBER_PURCHASE_BUTTON).click();
+    cy.get(ELEMENT.PURCHASE_STATUS_LABEL)
+      .invoke("text")
+      .should("eq", `구매 현황: 자동 0 장, 수동 1 장`);
+  });
+
+  it.only("올바르지 않은 번호 입력 후, 수동 구매 추가 버튼을 누르면 alert가 나타난다.", () => {
+    cy.get(ELEMENT.MANUAL_NUMBER).then((els) => {
+      [...els].forEach((el, i) => cy.wrap(el).type(i));
+    });
+
+    const stub = cy.stub();
+    cy.on("window:alert", stub);
+
+    cy.get(ELEMENT.MANUAL_NUMBER_PURCHASE_BUTTON)
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          ALERT_MESSAGE.INVALID_NUMBER_RANGE
+        );
+      });
   });
 });
