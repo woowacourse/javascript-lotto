1b7711412b39574c7391827c11115246e66087e7
bigsaigon333 <likepeppermint@gmail.com>
2021-03-27 22:54:04 +0900

    feat: 예상하지 못한 에러가 발생한 경우의 예외처리를 추가한다

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,15 @@ import {
   WinningNumberContainer,
   Modal,
 } from "./components/index.js";
+import { notify } from "./utils/index.js";
 
-CashContainer.init();
-IssueManagerContainer.init();
-LottoDetailContainer.init();
-WinningNumberContainer.init();
-Modal.init();
+try {
+  CashContainer.init();
+  IssueManagerContainer.init();
+  LottoDetailContainer.init();
+  WinningNumberContainer.init();
+  Modal.init();
+} catch (error) {
+  console.error(error.message);
+  notify("예상하지 못한 에러가 발생하였습니다. 관리자에게 문의해주세요. ");
+}
