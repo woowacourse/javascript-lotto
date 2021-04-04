0279a704cda2ceb5e494922271b4534aa182fb23
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-04-05 02:37:05 +0900

    refactor: printWidth 변경에 따른 코드 재정렬

--- src/js/lottoValidation.js
+++ src/js/lottoValidation.js
@@ -2,10 +2,7 @@ import { LOTTO_NUMBERS } from './utils/constants.js';
 import { changeObjectToSet } from './utils/utils.js';
 
 export const isCorrectPurchaseUnit = input => {
-  return (
-    Math.floor(input / LOTTO_NUMBERS.LOTTO_UNIT) ===
-    input / LOTTO_NUMBERS.LOTTO_UNIT
-  );
+  return Math.floor(input / LOTTO_NUMBERS.LOTTO_UNIT) === input / LOTTO_NUMBERS.LOTTO_UNIT;
 };
 
 export const isUniqueWinningNumber = input => {
