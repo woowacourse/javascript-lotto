9831ea9a781626eac0a456a72741f155cde41d0a
woowapark <shinyoung.park@woowahan.com>
2022-02-21 16:05:13 +0900

    feat: add configs for webpack build & jest test

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,5 @@
+describe("테스트 그룹 ", () => {
+  it("테스트 명세", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  resolve: {
+    extensions: [".js", ".css"],
+  },
+  devServer: {
+    port: 9000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
