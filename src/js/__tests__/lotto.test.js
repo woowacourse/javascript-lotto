3c0e13c3def08aab4257d3253bc48e7a254e4fd5
prefer2 <sunho6200@gmail.com>
2022-02-22 17:42:16 +0900

    test: 테스트 케이스 작성 - 구입 금액이 1000으로 나누었을 때 나머지가 0인지 확인하는 실패/성공 테스트 작성

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,4 +1,4 @@
-import { isPositiveInteger } from '../app.js';
+import { isPositiveInteger, isDivisibleBy } from '../app.js';
 
 test('구입 금액은 양의 정수이다. 실패 케이스', () => {
   const payment = -1;
@@ -20,4 +20,20 @@ test('구입 금액은 양의 정수이다. 성공 케이스', () => {
   const payment = 3;
 
   expect(isPositiveInteger(payment)).toBe(true);
+});
+
+test('구입 금액이 1000으로 나누어 떨어진다. 실패 케이스', () => {
+  const payment = 33;
+  const quotient = 1000;
+
+  expect(() => {
+    isDivisibleBy(payment, quotient);
+  }).toThrowError();
+});
+
+test('구입 금액이 1000으로 나누어 떨어진다. 성공 케이스', () => {
+  const payment = 3000;
+  const quotient = 1000;
+
+  expect(isDivisibleBy(payment, quotient)).toBe(3);
 });
