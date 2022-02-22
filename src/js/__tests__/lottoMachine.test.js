e00c8b076266f36f7a44f0566cdb9162920cd058
cks3066 <cks3066@naver.com>
2022-02-22 20:30:31 +0900

    feat: 로또 기계가 투입금액에서 로또 가격을 나눈 개수만큼의 로또를 발급하도록 구현
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- src/js/__tests__/lottoMachine.test.js
+++ src/js/__tests__/lottoMachine.test.js
@@ -3,22 +3,16 @@
 import LottoMachine from "../domains/LottoMachine.js";
 import { LOTTO } from "../constants/constants.js";
 
-const validInputMoney = "6000";
-
 function isValidInputMoney(money) {
   return money % LOTTO_PRICE === 0;
 }
 
 describe("로또 기계 단위 테스트", () => {
   test("로또 기계는 투입금액에서 로또 가격을 나눈 개수만큼의 로또를 발급해야 한다.", () => {
-    const lottos = [
-      [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6],
-      [1, 2, 3, 4, 5, 6],
-    ];
-    expect(lottos).toHaveLength(validInputMoney / LOTTO.PRICE);
+    const validInputMoney = "6000";
+    const lottoMachine = new LottoMachine();
+    lottoMachine.inputMoney = validInputMoney;
+    lottoMachine.operateLottoMachine();
+    expect(lottoMachine.lottos).toHaveLength(validInputMoney / LOTTO.PRICE);
   });
 });
--- src/js/domains/LottoMachine.js
+++ src/js/domains/LottoMachine.js
@@ -1,17 +1,41 @@
 import { LOTTO } from "../constants/constants.js";
-
+import Lotto from "../domains/Lotto.js";
 export default class LottoMachine {
-  #inputPrice = 0;
+  #inputMoney = 0;
+  #lottos = [];
 
-  get inputPrice() {
-    return this.#inputPrice;
+  get inputMoney() {
+    return this.#inputMoney;
   }
 
-  set inputPrice(money) {
-    this.#inputPrice = money;
+  set inputMoney(money) {
+    this.#inputMoney = money;
+  }
+
+  get lottos() {
+    return this.#lottos;
+  }
+
+  set lottos(lottos) {
+    this.#lottos = lottos;
+  }
+
+  operateLottoMachine() {
+    this.lottos = this.generateLottos();
+    this.inputMoney = 0;
+  }
+
+  generateLottos() {
+    return Array(this.calculateLottoQuantity())
+      .fill()
+      .map(() => {
+        const lotto = new Lotto();
+        lotto.numbers = lotto.generateNumbers(lotto.generateRandomNumber);
+        return lotto;
+      });
   }
 
   calculateLottoQuantity() {
-    return this.#inputPrice / LOTTO.PRICE;
+    return this.#inputMoney / LOTTO.PRICE;
   }
 }
