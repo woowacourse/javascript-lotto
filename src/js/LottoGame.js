d0e6e1d2d548813a0d29b24dd8c7b9b626e2b3df
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-02-22 16:54:29 +0900

    feat: add check validation
    details below
    - check money is negative
    - check money is integer

--- src/js/LottoGame.js
+++ src/js/LottoGame.js
@@ -2,7 +2,30 @@ export class LottoGame {
   constructor() {
     this.moneyInput;
   }
+
   insertMoney(moneyInput) {
-    this.moneyInput = moneyInput;
+    if (this.isInputValid(moneyInput)) {
+      this.moneyInput = moneyInput;
+    }
+  }
+
+  isInputValid(input) {
+    if (!this.isMoneyPositive(input)) {
+      alert('is money negative');
+      throw new Error('negative error');
+    }
+    if (!this.isMoneyInteger(input)) {
+      alert('is money not a integer');
+      throw new Error('not a integer error');
+    }
+    return true;
+  }
+
+  isMoneyPositive(input) {
+    return input > 0;
+  }
+
+  isMoneyInteger(input) {
+    return Number.isInteger(input);
   }
 }
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,21 @@
 import { LottoGame } from '../LottoGame.js';
 
-test(' 로또 구매 금액을 입력할 수 있다.', () => {
+test('로또 구매 금액을 입력할 수 있다.', () => {
   const lottoGame = new LottoGame();
   lottoGame.insertMoney(1000);
   expect(lottoGame.moneyInput).toBe(1000);
+});
+
+test('금액은 자연수이어야 한다.', () => {
+  const lottoGame = new LottoGame();
+  expect(() => {
+    lottoGame.insertMoney(-10);
+  }).toThrow();
+});
+
+test('금액은 정수이어야 한다.', () => {
+  const lottoGame = new LottoGame();
+  expect(() => {
+    lottoGame.insertMoney('abc');
+  }).toThrow();
 });
