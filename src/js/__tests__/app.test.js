64ab303fe4cf71524d31c7fb28cfafe60fee1edb
nan-noo <uni613@naver.com>
2022-02-22 18:05:25 +0900

    test: write cash input validation test
    공백, 1000원 단위, 범위 확인
    
    Co-Authored-By: Yo Wook Kim <uk960214@gmail.com>

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,32 @@
-describe("테스트 그룹 ", () => {
-  it("테스트 명세", () => {
-    expect(true).toBe(true);
+import LottoManager from '../model/lottoManager.js';
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    }
+    return {
+      message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
+      pass: false,
+    };
+  },
+});
+
+describe('구입 금액 검증 테스트 ', () => {
+  test('입력 값이 빈 칸이 아니어야 한다.', () => {
+    const cashInput = '';
+    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
+  });
+  test('입력 값이 1000원 단위인지 검증한다.', () => {
+    const cashInput = '1500';
+    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
+  });
+  test('입력 값의 범위가 1000 - 50000인지 검증한다.', () => {
+    const cashInput = '55000';
+    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
   });
 });
