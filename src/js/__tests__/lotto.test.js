c499f96999b542896fbd3c67647fbebb0f8d1fc5
cks3066 <cks3066@naver.com>
2022-02-22 17:15:53 +0900

    feat: 로또 숫자 생성 함수 구현
    - 로또는 1부터 45번까지의 숫자들을 가진다.
    - 로또는 총 6개의 숫자를 가진다.
    
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Lotto from "../domains/Lotto.js";
 import { LOTTO } from "../constants/constants.js";
 import { isInRange, isDuplicated } from "../validations/utils.js";
@@ -8,6 +9,25 @@ describe("로또 단위 테스트 ", () => {
 
   test("로또는 1부터 45번까지의 숫자들을 가진다.", () => {
     expect(isInRange(lotto.numbers, LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER)).toBe(
+=======
+// - [ ] 로또는 1부터 45번까지의 총 6개의 숫자를 가진다.
+// - [ ] 로또의 각 숫자들은 중복되지 않는다.
+import Lotto from "../domains/Lotto";
+
+const LOTTO_MIN_NUMBER = 1;
+const LOTTO_MAX_NUMBER = 45;
+
+function isInRange(arr, min, max) {
+  return arr.every((item) => item >= min && item <= max);
+}
+
+describe("로또 단위 테스트 ", () => {
+  const lotto = new Lotto();
+  lotto.generateNumbers(lotto.generateRandomNumber);
+
+  test("로또는 1부터 45번까지의 숫자들을 가진다.", () => {
+    expect(isInRange(lotto.numbers, LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)).toBe(
+>>>>>>> f98c3df (feat: 로또 숫자 생성 함수 구현)
       true
     );
   });
@@ -15,8 +35,11 @@ describe("로또 단위 테스트 ", () => {
   test("로또는 총 6개의 숫자를 가진다.", () => {
     expect(lotto.numbers).toHaveLength(6);
   });
+<<<<<<< HEAD
 
   test("로또의 각 숫자들은 중복되지 않는다.", () => {
     expect(isDuplicated(lotto.numbers)).toBe(true);
   });
+=======
+>>>>>>> f98c3df (feat: 로또 숫자 생성 함수 구현)
 });
--- src/js/domains/Lotto.js
+++ src/js/domains/Lotto.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { LOTTO } from "../constants/constants.js";
 export default class Lotto {
   #numbers = [];
@@ -17,6 +18,29 @@ export default class Lotto {
   #generateRandomNumber() {
     return Math.floor(
       Math.random() * (LOTTO.MAX_NUMBER - LOTTO.MIN_NUMBER) + LOTTO.MIN_NUMBER
+=======
+const LOTTO_MAX_NUMBER = 45;
+const LOTTO_MIN_NUMBER = 1;
+
+export default class Lotto {
+  #numbers = [];
+
+  // 로또에 숫자를 생성
+  // 랜덤
+  generateNumbers(generateRandomNumber) {
+    this.#numbers = Array(6)
+      .fill()
+      .map(() => generateRandomNumber());
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  generateRandomNumber() {
+    return (
+      Math.random() * (LOTTO_MAX_NUMBER - LOTTO_MIN_NUMBER) + LOTTO_MIN_NUMBER
+>>>>>>> f98c3df (feat: 로또 숫자 생성 함수 구현)
     );
   }
 }
