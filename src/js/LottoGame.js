653dd0ce6e85da43692ceb987383665e428835ca
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-02-22 16:24:02 +0900

    feat: can enter money

---
+++ src/js/LottoGame.js
@@ -0,0 +1,8 @@
+export class LottoGame {
+  constructor() {
+    this.moneyInput;
+  }
+  insertMoney(moneyInput) {
+    this.moneyInput = moneyInput;
+  }
+}
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,7 @@
-describe("테스트 그룹 ", () => {
-  it("테스트 명세", () => {
-    expect(true).toBe(true);
-  });
+import { LottoGame } from '../LottoGame.js';
+
+test(' 로또 구매 금액을 입력할 수 있다.', () => {
+  const lottoGame = new LottoGame();
+  lottoGame.insertMoney(1000);
+  expect(lottoGame.moneyInput).toBe(1000);
 });
