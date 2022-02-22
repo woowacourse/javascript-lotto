f5fba642999518cdc4285ec0e9640084ac96c833
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-22 17:19:59 +0900

    feat: 로또번호 인스턴스 추가

---
+++ src/js/model/Lotto.js
@@ -0,0 +1,27 @@
+import { getRandomNumber } from '../utils/data-manager.js';
+
+export default class Lotto {
+  #pickedNumber = [];
+
+  pushNumberIntoPickedNumber(number) {
+    if (this.#pickedNumber.includes(number) === true) {
+      return;
+    }
+
+    if (this.#pickedNumber.length >= 6) {
+      return;
+    }
+
+    this.#pickedNumber.push(number);
+  }
+
+  generate() {
+    while (this.#pickedNumber.length !== 6) {
+      this.pushNumberIntoPickedNumber(getRandomNumber(1, 45));
+    }
+  }
+
+  get pickedNumber() {
+    return this.#pickedNumber;
+  }
+}
