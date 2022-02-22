b3cee40b3fbd614a0f3727ed3e9376b8bd6c0037
kamwoo <rkadndud06@naver.com>
2022-02-22 17:09:24 +0900

    refactor: 입력된 금액의 유효성을 검사하는 메소드 분리
    - 숫자인지 체크하는 메소드 분리
    - 1000으로 나누어 떨어지는지 체크하는 메소드 분리
    - 1000에서 10000사이의 수인지 체크하는 메소드 분리
    
    Co-authored-by: greenblues1190 <greenblues1190@gmail.com>

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -10,22 +10,7 @@
 // - [ ] 계산된 로또개수만큼 로또를 자동 구매할 수 있어야 한다.
 
 import LottoApp from '../LottoApp.js';
-
-function checkIsValidChargeAmount(chargeAmount) {
-  if (typeof chargeAmount !== 'number') {
-    throw new Error('입력된 금액이 숫자가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.');
-  }
-  if (chargeAmount % 1000) {
-    throw new Error(
-      '입력된 금액이 1000으로 나누어 떨어지지 않습니다. 1000으로 나누어 떨어지는 금액을 입력해주세요.'
-    );
-  }
-  if (chargeAmount < 1000 || chargeAmount > 10000) {
-    throw new Error(
-      '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
-    );
-  }
-}
+import validator from '../utils/validator.js';
 
 describe('1단계 기능목록', () => {
   test('금액이 주어지면 발급할 로또 개수를 구할 수 있어야 한다.', () => {
@@ -38,7 +23,7 @@ describe('1단계 기능목록', () => {
     const chargeAmount = '만원';
 
     expect(() => {
-      checkIsValidChargeAmount(chargeAmount);
+      validator.checkChargeAmount(chargeAmount);
     }).toThrowError('입력된 금액이 숫자가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.');
   });
 
@@ -46,7 +31,7 @@ describe('1단계 기능목록', () => {
     const chargeAmount = 1001;
 
     expect(() => {
-      checkIsValidChargeAmount(chargeAmount);
+      validator.checkChargeAmount(chargeAmount);
     }).toThrowError(
       '입력된 금액이 1000으로 나누어 떨어지지 않습니다. 1000으로 나누어 떨어지는 금액을 입력해주세요.'
     );
@@ -57,13 +42,13 @@ describe('1단계 기능목록', () => {
     const secondChargeAmount = 11000;
 
     expect(() => {
-      checkIsValidChargeAmount(firstChargeAmount);
+      validator.checkChargeAmount(firstChargeAmount);
     }).toThrowError(
       '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
     );
 
     expect(() => {
-      checkIsValidChargeAmount(secondChargeAmount);
+      validator.checkChargeAmount(secondChargeAmount);
     }).toThrowError(
       '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
     );
---
+++ src/js/utils/validator.js
@@ -0,0 +1,33 @@
+function isNumber(value) {
+  return !isNaN(value);
+}
+
+function isDividedByThousand(value) {
+  return value % 1000 === 0;
+}
+
+function isValidChargeAmountRange(chargeAmount) {
+  return chargeAmount >= 1000 && chargeAmount <= 10000;
+}
+
+const validator = {
+  checkChargeAmount: (chargeAmount) => {
+    if (!isNumber(chargeAmount)) {
+      throw new Error('입력된 금액이 숫자가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.');
+    }
+
+    if (!isDividedByThousand(chargeAmount)) {
+      throw new Error(
+        '입력된 금액이 1000으로 나누어 떨어지지 않습니다. 1000으로 나누어 떨어지는 금액을 입력해주세요.'
+      );
+    }
+
+    if (!isValidChargeAmountRange(chargeAmount)) {
+      throw new Error(
+        '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
+      );
+    }
+  },
+};
+
+export default validator;
