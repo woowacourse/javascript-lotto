6377767c35a77b08d4c12d7fa041a4e6ba0eade4
kamwoo <rkadndud06@naver.com>
2022-02-22 16:13:33 +0900

    test: 입력 금액 유효성 검증 테스트 실패 케이스 작성
    Co-authored-by: greenblues1190 <greenblues1190@gmail.com>

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,17 +1,69 @@
 // ### 1단계 기능목록
 
-// - [ ] 금액이 주어지면 발급할 로또 개수를 구할 수 있어야한다.
+// - [x] 금액이 주어지면 발급할 로또 개수를 구할 수 있어야한다.
+// - [ ] 입력된 금액이 공백으로 이루어져 있으면 에러를 throw한다.
+// - [ ] 입력된 금액이 숫자가 아니면 에러를 throw한다.
 // - [ ] 1000으로 나눠서 안떨어지는 금액이 입려되면 에러를 throw한다.
+// - [ ] 입력된 금액이 1000부터 10000 사이가 아니면 에러를 throw한다.
 // - [ ] 랜덤한 번호를 6개 생성할 수 있어야 한다.
 // - [ ] 번호 6개를 가진 로또를 발급할 수 있다.
 // - [ ] 계산된 로또개수만큼 로또를 자동 구매할 수 있어야 한다.
 
 import LottoApp from '../LottoApp.js';
 
+function checkIsValidChargeAmount(chargeAmount) {
+  return undefined;
+}
+
 describe('1단계 기능목록', () => {
   test('금액이 주어지면 발급할 로또 개수를 구할 수 있어야 한다.', () => {
     const chargeAmount = 2000;
 
     expect(LottoApp.getNumberOfLotto(chargeAmount)).toBe(2);
+  });
+
+  test('입력된 금액이 공백으로 이루어져 있으면 에러를 throw한다.', () => {
+    const chargeAmount = ' ';
+
+    expect(() => {
+      checkIsValidChargeAmount(chargeAmount);
+    }).toThrowError(
+      '입력된 금액이 공백으로 이루어져 있습니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
+    );
+  });
+
+  test('입력된 금액이 숫자가 아니면 에러를 throw한다.', () => {
+    const chargeAmount = '만원';
+
+    expect(() => {
+      checkIsValidChargeAmount(chargeAmount);
+    }).toThrowError('입력된 금액이 숫자가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.');
+  });
+
+  test('1000으로 나눠서 안떨어지는 금액이 입려되면 에러를 throw한다.', () => {
+    const chargeAmount = '1001';
+
+    expect(() => {
+      checkIsValidChargeAmount(chargeAmount);
+    }).toThrowError(
+      '입력된 금액이 1000으로 나누어 떨어지지 않습니다. 1000으로 나누어 떨어지는 금액을 입력해주세요.'
+    );
+  });
+
+  test('입력된 금액이 1000부터 10000 사이가 아니면 에러를 throw한다.', () => {
+    const firstChargeAmount = '999';
+    const secondChargeAmount = '10001';
+
+    expect(() => {
+      checkIsValidChargeAmount(firstChargeAmount);
+    }).toThrowError(
+      '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
+    );
+
+    expect(() => {
+      checkIsValidChargeAmount(secondChargeAmount);
+    }).toThrowError(
+      '입력된 금액이 1000부터 10000 사이가 아닙니다. 1000 이상 10000 이하의 금액을 입력해주세요.'
+    );
   });
 });
