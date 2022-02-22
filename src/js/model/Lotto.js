96696ae11fcaf66334ec062ffa3c9db162790457
Ahn <jin7969@gmail.com>
2022-02-22 23:57:16 +0900

    feat: 각 로또 티켓 객체를 만드는 기능

---
+++ src/js/model/Lotto.js
@@ -0,0 +1,17 @@
+import { LOTTO_NUMBER } from "../utils/constants.js";
+import { getRandomNumber } from "../utils/general.js";
+
+export default class Lotto {
+  constructor() {
+    this.numbers = [];
+  }
+
+  makeRandomNumber() {
+    while (this.numbers.length < LOTTO_NUMBER.LENGTH) {
+      const randomNumber = getRandomNumber(LOTTO_NUMBER.RANGE_MIN, LOTTO_NUMBER.RANGH_MAX);
+      if (!this.numbers.includes(randomNumber)) {
+        this.numbers.push(randomNumber);
+      }
+    }
+  }
+}
--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -2,11 +2,18 @@ export const SELECTOR = {
   PURCHASE_FORM: ".purchase-form",
   PURCHASE_INPUT: ".purchase-input",
   PURCHASE_BUTTON: ".purchase-button",
+  PURCHASE_INFOMATION: ".purchase-infomation",
 };
 
 export const MINIMUM_AMOUNT = 1000;
 
 export const AMOUNT_UNIT = 1000;
+
+export const LOTTO_NUMBER = {
+  LENGTH: 6,
+  RANGE_MIN: 1,
+  RANGH_MAX: 45,
+};
 
 export const ERROR_MESSAGES = {
   INVALID_MINIMUM_AMOUNT: `금액은 ${MINIMUM_AMOUNT}원 이상으로 입력해주세요.`,
