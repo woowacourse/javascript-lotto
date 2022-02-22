86adc12f0bf2e1ca612b8527ba44f65a40825cee
Ahn <jin7969@gmail.com>
2022-02-22 15:22:45 +0900

    chore: eslint, prettier 환경 설정

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-alert": "off",
+    "import/extensions": ["error", "always"],
+    "max-depth": ["error", 2],
+    "prettier/prettier": "error",
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "no-new": "off",
+    "no-undef": "off",
+    "lines-between-class-members": "off",
+  },
+  plugins: ["prettier"],
+};
