0064eb71c45a405ba071bf885fdfa0ec5c2301ce
prefer2 <sunho6200@gmail.com>
2022-02-22 20:31:30 +0900

    test: 테스트 케이스 작성 - 구입한 로또의 각각의 번호가 1~45 사이의 숫자이도록 한다. - 구입한 로또 번호는 서로 다른 랜덤한 6개의 숫자이다.

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -37,3 +37,34 @@ test('구입 금액이 1000으로 나누어 떨어진다. 성공 케이스', () 
 
   expect(isDivisibleBy(payment, lottoPrice)).toBe(3);
 });
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      };
+    }
+  },
+});
+
+test('구입한 로또의 각각의 번호가 1~45 사이의 숫자이도록 한다. 성공 케이스', () => {
+  const randomNumber = createRandomNumber(1, 45);
+
+  expect(randomNumber).toBeWithinRange(1, 45);
+});
+
+test('구입한 로또 번호는 서로 다른 랜덤한 숫자 6개로 이루어진 값이다, 성공 케이스', () => {
+  const randomNumberList = createRandomNumberList(6);
+
+  expect(randomNumberList.length).toBe(new Set(randomNumberList).size);
+});
