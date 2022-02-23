cc05ee1b3c23b7aecf32a4fe6bddd5978c9e2583
nan-noo <uni613@naver.com>
2022-02-23 12:13:20 +0900

    test: refactor & add test code

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -17,16 +17,21 @@ expect.extend({
 });
 
 describe('구입 금액 검증 테스트 ', () => {
+  const lottoManger = new LottoManager();
   test('입력 값이 빈 칸이 아니어야 한다.', () => {
     const cashInput = '';
-    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
+    expect(() => lottoManger.buyLotto(cashInput)).toThrow();
   });
   test('입력 값이 1000원 단위인지 검증한다.', () => {
     const cashInput = '1500';
-    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
+    expect(() => lottoManger.buyLotto(cashInput)).toThrow();
   });
   test('입력 값의 범위가 1000 - 50000인지 검증한다.', () => {
     const cashInput = '55000';
-    expect(() => LottoManager.validateCashInput(cashInput)).toThrow();
+    expect(() => lottoManger.buyLotto(cashInput)).toThrow();
+  });
+  test('올바른 입력 값을 입력하면 오류가 발생하지 않는다.', () => {
+    const cashInput = '2000';
+    expect(() => lottoManger.buyLotto(cashInput)).not.toThrow();
   });
 });
