908abb56c4c4a1b428201f210ae269c5f8697a0b
nan-noo <uni613@naver.com>
2022-02-23 15:32:51 +0900

    feat: render purchased lottos
    로또 뷰에 렌더링함수 추가
    index.html 수정

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -2,3 +2,8 @@ export const selectDom = (selector, parent = document) => parent.querySelector(s
 export const isNumberInRange = ({ number, min, max }) => number >= min && number <= max;
 export const generateRandomNumberInRange = (min, max) =>
   Math.floor(Math.random() * (max - min) + min); // eslint-disable-line
+export const createElementWithClassName = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+};
--- src/js/view/lottoView.js
+++ src/js/view/lottoView.js
@@ -1,4 +1,4 @@
-import { selectDom } from '../utils/utils';
+import { createElementWithClassName, selectDom } from '../utils/utils';
 
 class LottoView {
   constructor() {
@@ -8,11 +8,33 @@ class LottoView {
   initDom() {
     this.purchasedLottoSection = selectDom('.purchased-lotto-section');
     this.winnerNumberSection = selectDom('.winner-number-section');
+    this.lottoShowContainer = selectDom('.lotto-show-container');
+    this.lottoNumberContainer = selectDom('.lotto-number-container', this.purchasedLottoSection);
   }
 
   renderLottos(lottos) {
     this.purchasedLottoSection.classList.remove('hide');
     this.winnerNumberSection.classList.remove('hide');
+
+    const labelElement = document.createElement('label');
+    labelElement.textContent = `총 ${lottos.length}개를 구매하였습니다.`;
+    this.lottoShowContainer.prepend(labelElement);
+    this.lottoNumberContainer.append(...LottoView.generateLottoElementTemplate(lottos));
+  }
+
+  static generateLottoElementTemplate(lottos) {
+    return lottos.map((lotto) => LottoView.generateLottoElement(lotto));
+  }
+
+  static generateLottoElement(lotto) {
+    const lottoElement = createElementWithClassName('div', 'lotto');
+    const lottoImage = createElementWithClassName('p', 'lotto-image');
+    lottoImage.textContent = '🎟️';
+    const lottoNumbers = createElementWithClassName('p', 'lotto-numbers');
+    lottoNumbers.classList.add('hide');
+    lottoNumbers.textContent = Array.from(lotto.lottoNumbers).join(', ');
+    lottoElement.append(lottoImage, lottoNumbers);
+    return lottoElement;
   }
 }
 export default LottoView;
