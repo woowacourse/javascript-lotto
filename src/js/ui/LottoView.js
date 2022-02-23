b31c32475aff524c2a82139e0caa28eddd5459d3
cks3066 <cks3066@naver.com>
2022-02-23 15:25:09 +0900

    refactor: LottoView 셀럭터 상수 분리
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- src/js/ui/LottoView.js
+++ src/js/ui/LottoView.js
@@ -14,7 +14,11 @@ import {
 import { DOM } from "../constants/constants.js";
 =======
 } from './template.js';
+<<<<<<< HEAD
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
+=======
+import { SELECTOR } from '../constants/constants.js';
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
 
 export default class LottoView {
   constructor() {
@@ -34,11 +38,11 @@ export default class LottoView {
   }
 
   bindEvents() {
-    $('purchase-money-form').addEventListener(
+    $(SELECTOR.ID.PURCHASE_MONEY_FORM).addEventListener(
       'submit',
       this.handlePurchaseForm.bind(this)
     );
-    $('lotto-result-toggle').addEventListener(
+    $(SELECTOR.ID.LOTTO_RESULT_TOGGLE).addEventListener(
       'click',
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
       this.handleResultToggle.bind(this)
@@ -81,19 +85,25 @@ export default class LottoView {
     $(DOM.ID.TOGGLE_CHECKBOX).checked
 =======
   userInputMoney() {
-    this.machine.inputMoney = Number($('purchase-money-input').value);
+    this.machine.inputMoney = Number($(SELECTOR.ID.PURCHASE_MONEY_INPUT).value);
   }
 
   renderLotto() {
+<<<<<<< HEAD
     $('lotto-result-container').replaceChildren();
     $('lotto-result-toggle-checkbox').checked
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
+=======
+    $(SELECTOR.ID.LOTTO_RESULT_CONTAINER).replaceChildren();
+    $(SELECTOR.ID.TOGGLE_CHECKBOX).checked
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
       ? this.renderLottoNumbers()
       : this.renderLottoImgs();
   }
 
   renderLottoImgs() {
     this.machine.lottos.map(() => {
+<<<<<<< HEAD
 <<<<<<< HEAD
       $(DOM.ID.LOTTO_RESULT_CONTAINER).insertAdjacentHTML(
         "beforeEnd",
@@ -102,17 +112,27 @@ export default class LottoView {
 =======
       $('lotto-result-container').insertAdjacentHTML('beforeEnd', ticketImg);
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
+=======
+      $(SELECTOR.ID.LOTTO_RESULT_CONTAINER).insertAdjacentHTML(
+        'beforeEnd',
+        ticketImg
+      );
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
     });
   }
 
   renderLottoNumbers() {
     this.machine.lottos.map((lotto) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
       $(DOM.ID.LOTTO_RESULT_CONTAINER).insertAdjacentHTML(
         "beforeEnd",
         lottoNumberTemplate(lotto.numbers.join(", "))
 =======
       $('lotto-result-container').insertAdjacentHTML(
+=======
+      $(SELECTOR.ID.LOTTO_RESULT_CONTAINER).insertAdjacentHTML(
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
         'beforeEnd',
         lottoNumberTemplate(lotto.numbers.join(', '))
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
@@ -122,15 +142,20 @@ export default class LottoView {
 
   renderLottoAmount() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     $(DOM.ID.LOTTO_RESULT_SPAN).textContent = purchaseMessageTemplate(
 =======
     $('lotto-result-span').textContent = purchaseMessageTemplate(
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
+=======
+    $(SELECTOR.ID.LOTTO_RESULT_SPAN).textContent = purchaseMessageTemplate(
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
       this.machine.lottos
     );
   }
 
   disablePurchase() {
+<<<<<<< HEAD
 <<<<<<< HEAD
     $(DOM.ID.PURCHASE_MONEY_INPUT).disabled = true;
     $(DOM.ID.PURCHASE_MONEY_BUTTON).disabled = true;
@@ -148,5 +173,18 @@ export default class LottoView {
     $('lotto-result-section').hidden = !$('lotto-result-section').hidden;
     $('winning-number-form').hidden = !$('winning-number-form').hidden;
 >>>>>>> 96fc414 (refactor: 함수 분리, 함수명 변경)
+=======
+    $(SELECTOR.ID.PURCHASE_MONEY_INPUT).disabled = true;
+    $(SELECTOR.ID.PURCHASE_MONEY_BUTTON).disabled = true;
+  }
+
+  controlLottoContainers() {
+    $(SELECTOR.ID.LOTTO_RESULT_SECTION).hidden = !$(
+      SELECTOR.ID.LOTTO_RESULT_SECTION
+    ).hidden;
+    $(SELECTOR.ID.WINNING_NUMBER_FORM).hidden = !$(
+      SELECTOR.ID.WINNING_NUMBER_FORM
+    ).hidden;
+>>>>>>> 978f6e4 (refactor: LottoView 셀럭터 상수 분리)
   }
 }
