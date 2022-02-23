92a6a32d6cafb526ce8f85df93235aa6c687b08e
prefer2 <sunho6200@gmail.com>
2022-02-23 16:49:14 +0900

    test: 에러 테스트 작성 - 구입할 금액이 1000원 이상 10000원 이하가 아닌 경우 에러메시지를 보여준다. - 구입할 금액을 1000원 단위로 입력하지 않았을 경우 에러메시지를 보여준다.

--- cypress/integration/lotto.test.js
+++ cypress/integration/lotto.test.js
@@ -39,3 +39,37 @@ it('번호 보기 버튼을 비활성화시키면 로또 번호가 가려진다'
       cy.get('.lotto-number').should('be.not.visible');
     });
 });
+
+it('구입할 금액이 1000원 이상 10000원 이하가 아닌 경우 에러메시지를 보여준다.', () => {
+  cy.visit('/index.html');
+  const input = -1;
+  const alertStub = cy.stub();
+  cy.on('window:alert', alertStub);
+
+  cy.get('#payment-input').type(input);
+  cy.get('#payment-button')
+    .click()
+    .then(() => {
+      expect(alertStub).to.be.calledWith(
+        '구입할 금액은 1000원 이상 10000원 이하를 입력해주셔야 합니다.'
+      );
+      cy.get('#payment-input').should('text', '').should('have.foucs');
+    });
+});
+
+it('구입할 금액을 1000원 단위로 입력하지 않았을 경우 에러메시지를 보여준다', () => {
+  cy.visit('/index.html');
+  const input = 1001;
+  const alertStub = cy.stub();
+  cy.on('window:alert', alertStub);
+
+  cy.get('#payment-input').type(input);
+  cy.get('#payment-button')
+    .click()
+    .then(() => {
+      expect(alertStub).to.be.calledWith(
+        '구입할 금액은 1000원 단위로 입력해주셔야 합니다'
+      );
+      cy.get('#payment-input').should('text', '').should('have.foucs');
+    });
+});
