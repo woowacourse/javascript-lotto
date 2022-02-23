46bb21e8b21985ccd8a19afdfdc3dfe49219bc73
cks3066 <cks3066@naver.com>
2022-02-23 15:04:23 +0900

    refactor: valiate 로직 파일 분리
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- src/js/domains/LottoMachine.js
+++ src/js/domains/LottoMachine.js
@@ -46,7 +46,11 @@ import Lotto from "../domains/Lotto.js";
 =======
 import { LOTTO } from '../constants/constants.js';
 import Lotto from '../domains/Lotto.js';
+<<<<<<< HEAD
 >>>>>>> a161b95 (feat: 로또 금액을 입력받아 저장하도록 구현)
+=======
+import validateMoney from '../validations/LottoMachine.js';
+>>>>>>> df9aee0 (refactor: valiate 로직 파일 분리)
 export default class LottoMachine {
   #inputMoney = 0;
   #lottos = [];
@@ -56,7 +60,7 @@ export default class LottoMachine {
   }
 
   set inputMoney(money) {
-    this.validateMoney(money);
+    validateMoney(money);
     this.#inputMoney = money;
   }
 
@@ -66,23 +70,6 @@ export default class LottoMachine {
 
   set lottos(lottos) {
     this.#lottos = lottos;
-  }
-
-  validateMoney(money) {
-    if (!this.isValidUnit(money)) {
-      throw new Error(`${LOTTO.PRICE}단위로 입력해주세요`);
-    }
-    if (!this.isOverZero(money)) {
-      throw new Error('0원보다 큰 금액을 입력해주세요.');
-    }
-  }
-
-  isOverZero(money) {
-    return money > 0;
-  }
-
-  isValidUnit(money) {
-    return money % LOTTO.PRICE === 0;
   }
 
   operateLottoMachine() {
--- src/js/validations/LottoMachine.js
+++ src/js/validations/LottoMachine.js
@@ -1,18 +1,34 @@
+<<<<<<< HEAD
 import { LOTTO } from "../constants/constants.js";
+=======
+import { LOTTO } from '../constants/constants.js';
+>>>>>>> df9aee0 (refactor: valiate 로직 파일 분리)
 
 function isOverZero(money) {
   return money > 0;
 }
 
+<<<<<<< HEAD
 function isDividedByPrice(money) {
+=======
+function isValidUnit(money) {
+>>>>>>> df9aee0 (refactor: valiate 로직 파일 분리)
   return money % LOTTO.PRICE === 0;
 }
 
 export default function validateMoney(money) {
+<<<<<<< HEAD
   if (!isDividedByPrice(money)) {
     throw new Error(`${LOTTO.PRICE}단위로 입력해주세요`);
   }
   if (!isOverZero(money)) {
     throw new Error("0원보다 큰 금액을 입력해주세요.");
+=======
+  if (!isValidUnit(money)) {
+    throw new Error(`${LOTTO.PRICE}단위로 입력해주세요`);
+  }
+  if (!isOverZero(money)) {
+    throw new Error('0원보다 큰 금액을 입력해주세요.');
+>>>>>>> df9aee0 (refactor: valiate 로직 파일 분리)
   }
 }
