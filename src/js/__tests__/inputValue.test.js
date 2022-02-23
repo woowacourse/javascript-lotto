5da172cb41611e0fc01ef23cb8075185a361e0dc
JASUN LEE <liswktjs@gmail.com>
2022-02-23 12:16:52 +0900

    refactor: test코드 내에 있던 함수 분리

--- src/js/__tests__/inputValue.test.js
+++ src/js/__tests__/inputValue.test.js
@@ -1,16 +1,8 @@
-import { LOTTO_PRICE } from '../constants/constant.js';
-
-export const isValueTypeNumber = value => {
-  return typeof value === Number;
-};
-
-export const isValuePositiveNumber = value => {
-  return value > 0;
-};
-
-export const isValueDivideThousand = value => {
-  return value % LOTTO_PRICE === 0;
-};
+import {
+  isValueTypeNumber,
+  isValuePositiveNumber,
+  isValueDivideThousand,
+} from '../core/checkInputValue.js';
 
 describe('사용자의 입력값을 테스트', () => {
   test('사용자가 입력한 로또 금액값이 Number 타입인지 확인한다', () => {
