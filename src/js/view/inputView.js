413cb2bd1c674f8135e4fedb0b4db79b7e15156b
Hwang Jung Min <hwangstar156@gmail.com>
2022-02-24 01:46:57 +0900

    refactor: view의 template부분을 분리하여 관리
    Co-authored-by: rladpwl0512 <rladpwl0512@naver.com>

--- src/js/view/inputView.js
+++ src/js/view/inputView.js
@@ -1,3 +1,5 @@
+import makeTemplate from './template';
+
 export default class InputView {
   constructor() {
     this.$result = document.querySelector('#result');
@@ -5,33 +7,7 @@ export default class InputView {
   }
 
   renderWinningNumbersInput() {
-    this.$result.insertAdjacentHTML('beforeend', this.makeWinningNumbersTemplate());
-  }
-
-  makeWinningNumbersTemplate() {
-    return `
-      <div>
-        <p>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>
-        <div class="numbers-input">
-          <div class="winning-numbers">
-            <div class="winning-numbers-header">당첨 번호</div>
-            <div class="winning-numbers-input">
-              <input type="number" class="winning-number-input"/>
-              <input type="number" class="winning-number-input"/>
-              <input type="number" class="winning-number-input"/>
-              <input type="number" class="winning-number-input"/>
-              <input type="number" class="winning-number-input"/>
-              <input type="number" class="winning-number-input"/>
-            </div>
-          </div>
-          <div class="bonus-number">
-            <div class="bonus-number-header">보너스 번호</div> 
-            <input type="number" class="bonus-number-input"/>
-          </div>
-        </div>
-        <button id="check-result-button" class="btn">결과 확인하기</button>
-      </div>
-    `;
+    this.$result.insertAdjacentHTML('beforeend', makeTemplate.makeWinningNumbersTemplate());
   }
 
   initLottoPriceInput() {
--- src/js/view/resultView.js
+++ src/js/view/resultView.js
@@ -1,34 +1,13 @@
+import makeTemplate from './template';
+
 export default class ResultView {
   constructor() {
     this.$result = document.querySelector('#result');
   }
 
   renderResult(count) {
-    const template = this.makeResultTemplate(count);
     this.$result.replaceChildren();
-    this.$result.insertAdjacentHTML('beforeend', template);
-  }
-
-  makeResultTemplate(count) {
-    return `
-      <div id="result-container">
-        <div id="purchase-result">
-          <div id="result-header">총 ${count}개를 구매하였습니다.</div>
-          <div id="result-lotto">
-            ${'<div class="lotto"><div class="lotto-icon">🎟️</div></div>'.repeat(count)}
-          </div>
-        </div>
-        <div id="view-number">
-            <div>번호 보기</div>
-          <div class="toggle-switch">
-            <input type="checkbox" id="view-checkbox" />
-            <label for="view-checkbox">
-              <span class="toggle-track"></span>
-            </label>
-          </div>
-        </div>
-      </div>
-    `;
+    this.$result.insertAdjacentHTML('beforeend', makeTemplate.makeResultTemplate(count));
   }
 
   renderLottos(lottos) {
