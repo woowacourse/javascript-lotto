0c6252afdb97f739ee176521a1d7d36d691c2bcc
junzzi <jjh01027717502@gmail.com>
2022-02-23 16:23:55 +0900

    test: 모델의 변경사항을 테스트 파일에 적용 및 상수화 진행

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,33 +1,18 @@
-import Lotto from "../models/Lotto";
+import { ERROR_MESSAGE } from '../constants/errorMessage';
+import Lotto from '../models/Lotto';
 
-describe("로또 모델 테스트", () => {
-  it("로또 모델에 숫자 배열을 인자로 넣어서 인스턴스를 생성할 수 있다.", () => {
+describe('로또 모델 테스트', () => {
+  it('로또 모델에 숫자 배열을 인자로 넣어서 인스턴스를 생성할 수 있다.', () => {
     const lotto = Lotto.create([1, 2, 3, 4, 5, 6]);
     expect(lotto.lottoNumbers).toContain(1, 2, 3, 4, 5, 6);
   });
 
-  it("로또 모델의 배열에 들어갈 값은 1이상 45이하의 숫자여야 한다.", () => {
+  it('로또 모델의 배열에 들어갈 값은 1이상 45이하의 숫자여야 한다.', () => {
     const invalidInput = [1, 2, 3, 4, 5, 46];
-
     try {
-      const invalidLotto = Lotto.create(invalidInput);
+      Lotto.create(invalidInput);
     } catch ({ message }) {
-      expect(message).toEqual("잘못된 입력 값");
+      expect(message).toEqual(ERROR_MESSAGE.LOTTO_NUMBER_IS_INVALIDATE);
     }
   });
-
-  it("로또 모델에 들어갈 배열의 길이는 6이어야 한다.", () => {
-    const invalidInput = [1, 2, 3, 4, 5, 6, 7];
-    try {
-      const invalidLotto = Lotto.create(invalidInput);
-    } catch ({ message }) {
-      expect(message).toEqual("잘못된 입력 값");
-    }
-  });
-
-  // it("로또 모델의 번호와 당첨 번호를 비교하여 등수를 반환할 수 있어야 한다.", () => {
-  //   const winNumber = [1, 2, 3, 4, 5, 6, 7];
-  //   const validLotto = Lotto.create([1, 2, 3, 4, 5, 6]);
-  //   expect(validLotto.lottoScore(winNumber)).toBe();
-  // });
 });
--- src/js/__tests__/lottoGame.test.js
+++ src/js/__tests__/lottoGame.test.js
@@ -1,27 +1,31 @@
-import LottoGame from "../models/LottoGame";
+import { ERROR_MESSAGE } from '../constants/errorMessage';
+import LottoGame from '../models/LottoGame';
 
-describe("로또 게임 모델 테스트", () => {
-  it("로또 게임 모델에 금액이 정상적으로 입력되면, 구매할 수 있는 로또의 수를 반환할 수 있어야 한다.", () => {
+describe('로또 게임 모델 테스트', () => {
+  it('로또 게임 모델에 금액이 정상적으로 입력되면, 구매할 수 있는 로또의 수를 반환할 수 있어야 한다.', () => {
     const lottoGame = new LottoGame();
-
-    const availableLottoAmount = lottoGame.inputCharge(5000);
-    expect(availableLottoAmount).toBe(5);
+    const charge = 5000;
+    const expectedAvailableLottoAmount = 5;
+    const availableLottoAmount = lottoGame.exchangeChargeToLottoAmount(charge);
+    expect(availableLottoAmount).toBe(expectedAvailableLottoAmount);
   });
 
-  it("금액은 1000이상의 숫자여야한다.", () => {
+  it('금액은 1000이상의 숫자여야한다.', () => {
     const lottoGame = new LottoGame();
+    const lessThanLottoPriceCharge = 500;
     try {
-      const availableLottoAmount = lottoGame.inputCharge(500);
+      lottoGame.exchangeChargeToLottoAmount(lessThanLottoPriceCharge);
     } catch ({ message }) {
-      expect(message).toEqual("금액은 1000원 이상이어야합니다.");
+      expect(message).toEqual(ERROR_MESSAGE.CHARGE_IS_INVALIDATE);
     }
   });
 
-  it("로또 번호 배열들을 입력하여 로또 모델을 생성하고 관리할 수 있어야 한다.", () => {
+  it('로또 번호 배열들을 입력하여 로또 모델을 생성하고 관리할 수 있어야 한다.', () => {
     const lottoGame = new LottoGame();
-    const availableLottoAmount = lottoGame.inputCharge(5000);
+    const charge = 5000;
+    const availableLottoAmount = lottoGame.exchangeChargeToLottoAmount(charge);
 
-    lottoGame.createLottoList(availableLottoAmount);
+    lottoGame.createLottoList(charge);
 
     expect(lottoGame.lottoList.length).toBe(availableLottoAmount);
   });
