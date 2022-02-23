b09e767acd866226056690a305bb718b47c23871
prefer2 <sunho6200@gmail.com>
2022-02-23 22:17:38 +0900

    test: 테스트 케이스 작성 - 구입할 금액을 조건에 맞게 입력 후 구입 버튼을 누르면, 지난주 당첨 번호 영역이 보여진다. - 구입할 금액을 조건에 맞게 입력 후 구입 버튼을 누르면, 결과 확인하기 버튼이 보여진다.

--- cypress/integration/lotto.test.js
+++ cypress/integration/lotto.test.js
@@ -1,4 +1,4 @@
-it('사용자가 구매한 티켓을 확인할 수 있다.', () => {
+it('구입할 금액을 조건에 맞게 입력 후 구입 버튼을 누르면, 구입한 로또 갯수를 확인할 수 있다.', () => {
   cy.visit('/index.html');
   const input = 3000;
 
@@ -10,6 +10,30 @@ it('사용자가 구매한 티켓을 확인할 수 있다.', () => {
         'text',
         '총 3개를 구매하였습니다.'
       );
+    });
+});
+
+it('구입할 금액을 조건에 맞게 입력 후 구입 버튼을 누르면, 지난주 당첨 번호 영역이 보여진다.', () => {
+  cy.visit('/index.html');
+  const input = 3000;
+
+  cy.get('#payment-input').type(input);
+  cy.get('#payment-button')
+    .click()
+    .then(() => {
+      cy.get('#last-week-winning-number-section').should('be.visible');
+    });
+});
+
+it('구입할 금액을 조건에 맞게 입력 후 구입 버튼을 누르면, 결과 확인하기 버튼이 보여진다', () => {
+  cy.visit('/index.html');
+  const input = 3000;
+
+  cy.get('#payment-input').type(input);
+  cy.get('#payment-button')
+    .click()
+    .then(() => {
+      cy.get('#result-checking-button').should('be.visible');
     });
 });
 
