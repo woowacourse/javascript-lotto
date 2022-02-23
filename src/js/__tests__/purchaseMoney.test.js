3e7504909ea236850ac7d9942e7d2f8c2f09dddb
kimsanglok <evertree6031@gmail.com>
2022-02-24 00:21:12 +0900

    style: toggle 스타일 분리
    Co-authored-by: intae92 <intae92@users.noreply.github.com>
    Co-authored-by: LAH1203 <LAH1203@users.noreply.github.com>

--- src/js/__tests__/purchaseMoney.test.js
+++ src/js/__tests__/purchaseMoney.test.js
@@ -1,52 +1,52 @@
 import { ERROR_MESSAGE } from '../constants/index.js';
-import { invalidPurchaseMoney } from '../util/validator.js';
+import { validatePurchaseMoney } from '../util/validator.js';
 
 describe('구입할 금액 입력 기능 테스트', () => {
   it('구입할 금액으로 0을 입력할 수 없다.', () => {
     const purchaseMoney = 0;
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.ZERO_MONEY,
     );
   });
 
   it('구입할 금액으로 공백을 입력하면 안된다.', () => {
     const purchaseMoney = '';
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NOT_NUMBER_TYPE,
     );
   });
 
   it('구입할 금액으로 숫자 타입을 입력해야 한다.', () => {
     const purchaseMoney = 'abc';
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NOT_NUMBER_TYPE,
     );
   });
 
   it('구입할 금액으로 천원 단위만 입력해야 한다.', () => {
     const purchaseMoney = 1250;
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NOT_UNIT_OF_THOUSAND,
     );
   });
 
   it('구입할 금액으로 소수점은 입력할 수 없다.', () => {
     const purchaseMoney = 1000.5;
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NOT_UNIT_OF_THOUSAND,
     );
   });
 
   it('구입할 금액으로 최소 금액은 천원이상 이여야 한다.', () => {
     const purchaseMoney = 900;
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NOT_UNIT_OF_THOUSAND,
     );
   });
 
   it('구입할 금액으로 양의 정수여야 한다.', () => {
     const purchaseMoney = -1;
-    expect(() => invalidPurchaseMoney(purchaseMoney)).toThrow(
+    expect(() => validatePurchaseMoney(purchaseMoney)).toThrow(
       ERROR_MESSAGE.NEGATIVE_NUMBER,
     );
   });
