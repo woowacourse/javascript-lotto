473f8a741c03e219f788ecd5eb023d2555fd46ff
prefer2 <sunho6200@gmail.com>
2022-02-23 15:10:01 +0900

    test: E2E 테스트 코드 작성 및 파일 삭제제 - 번호 보기 버튼을 누르면 사용자가 구매한 로또 번호를 확인할 수 있다 - app.test.js 삭제

--- cypress/integration/lotto.test.js
+++ cypress/integration/lotto.test.js
@@ -1,6 +1,7 @@
 it('사용자가 구매한 티켓을 확인할 수 있다.', () => {
-  cy.visit('./index.html');
+  cy.visit('/index.html');
   const input = 3000;
+
   cy.get('#payment-input').type(input);
   cy.get('#payment-button')
     .click()
@@ -9,5 +10,18 @@ it('사용자가 구매한 티켓을 확인할 수 있다.', () => {
         'text',
         '총 3개를 구매하였습니다.'
       );
+    });
+});
+
+it('번호 보기 버튼을 누르면 사용자가 구매한 로또 번호를 확인할 수 있다.', () => {
+  cy.visit('/index.html');
+  const input = 3000;
+
+  cy.get('#payment-input').type(input);
+  cy.get('#payment-button').click();
+  cy.get('#lotto-list-toggle-button')
+    .click()
+    .then(() => {
+      cy.get('.lotto-number').should('be.visible');
     });
 });
