43b0a7bd9a8fa405e664c60121b26e12a2772667
wonsss <bigdatajang@gmail.com>
2022-02-23 14:25:21 +0900

    :lipstick: style(css): use CSS custom properties
    Co-authored-by: YoungHwa Byun <rlaalsdl111@naver.com>

--- src/js/layouts/template.js
+++ src/js/layouts/template.js
@@ -1,6 +1,6 @@
 // 티켓 한 장에 대해서만 템플릿
 export const ticketTemplate = (numbers) => {
-  return `<div class="ticket-container">
+  return `<div class="ticket-icon-container">
             <span class="ticket-icon">🎟️</span>
             <span class="ticket-numbers hidden">${numbers.join(', ')}</span>
           </div>`;
