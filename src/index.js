943c3119d49f60cc2df56a4d5a12cb0566672380
junzzi <jjh01027717502@gmail.com>
2022-02-25 22:35:42 +0900

    refactor: deepcopy 메소드를 유틸 함수로 분리

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/index.css';
 import './css/converter.css';
 import './css/nanumbarungothic.css';
-import './js/utils/customPrototypeMethod';
 import RacingGameManager from './js/app';
 
 export default new RacingGameManager().start();
