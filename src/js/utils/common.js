d60ea6a755120142878338bc63c7e6231d2fd01d
kimsanglok <evertree6031@gmail.com>
2022-02-26 15:41:15 +0900

    refactor: 로또번호를 n개 생성하는 함수(pickLottoNumber(n)) 접근 방식 변경 -refactor: shuffleArray(배열의 값을 무작위로 정렬하는 함수) 추가

--- src/js/utils/common.js
+++ src/js/utils/common.js
@@ -29,22 +29,17 @@ export const pickLottoNumber = count => {
   const lottoNumbers = [...Array(RULES.MAX_LOTTO_NUMBER)].map(
     (_, index) => index + 1,
   );
+  shuffleArray(lottoNumbers);
+
   const numbers = [];
 
-  //로또번호 전체 갯수
-  let totalNumber = RULES.MAX_LOTTO_NUMBER;
-
   for (let i = 0; i < count; i++) {
-    const index = getRandomIndexNumber(0, totalNumber - 1);
-
-    numbers.push(lottoNumbers[index]);
-    lottoNumbers.splice(index, 1);
-    totalNumber--;
+    numbers.push(lottoNumbers.pop());
   }
 
   return numbers;
 };
 
-const getRandomIndexNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+function shuffleArray(inputArray) {
+  inputArray.sort(() => Math.random() - 0.5);
+}
