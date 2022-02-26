0596f36c0ee24e604257d78cf69749b07f76861c
DomMorello <zuzudnf@gmail.com>
2022-02-26 13:32:53 +0900

    refactor: isUnderMillion->isUnderTenThousand 로 의미에 맞게 수정

--- src/js/controller/validator.js
+++ src/js/controller/validator.js
@@ -4,13 +4,11 @@ export const isThousandMultiple = money => money % MONEY_INPUT.MIN_PRICE === 0;
 
 export const isOverThouand = money => money >= MONEY_INPUT.MIN_PRICE;
 
-export const isUnderMillion = money => money <= MONEY_INPUT.MAX_PRICE;
+export const isUnderTenThousand = money => money <= MONEY_INPUT.MAX_PRICE;
 
-export const isValidMoneyRange = money => isOverThouand(money) && isUnderMillion(money);
+export const isValidMoneyRange = money => isOverThouand(money) && isUnderTenThousand(money);
 
 export const isValidMoneyInput = money => {
   return isThousandMultiple(money) && 
-         isOverThouand(money) && 
-         isUnderMillion(money) && 
          isValidMoneyRange(money);
 }
