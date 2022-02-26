19a1c3f3827598d401631709f97f3dd4f62e3e61
JASUN LEE <liswktjs@gmail.com>
2022-02-26 22:55:37 +0900

    refactor: test코드 수정 앞선 함수 변경에따른 test코드 변경 + 로또 번호 생성시 중복값 확인하는 곳에서 input변수 분리

--- src/js/__tests__/inputValue.test.js
+++ src/js/__tests__/inputValue.test.js
@@ -1,6 +1,6 @@
 import {
   isValueTypeNumber,
-  isValuePositiveNumber,
+  isPositiveNumber,
   isValueDivideThousand,
 } from '../core/checkInputValue.js';
 
@@ -14,7 +14,7 @@ describe('사용자의 입력값을 테스트', () => {
   test('사용자가 입력한 로또 금액값이 양수 값인지 확인한다', () => {
     const value = [0, -1000];
     value.forEach(item => {
-      expect(isValuePositiveNumber(item)).toBe(false);
+      expect(isPositiveNumber(item)).toBe(false);
     });
   });
   test('사용자가 입력한 로또 금액값이 1000원 단위의 금액인지 확인한다', () => {
--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,17 +1,29 @@
-import { numberOfLotto, isOverlapLottoNumber } from '../core/drawLotto.js';
+import {
+  calculateGameCount,
+  isOverlapLottoNumber,
+} from '../core/makeLottoList.js';
 
 describe('로또가 알맞게 생성이 되는지 테스트', () => {
   test('입력 금액에 맞게 로또를 생성되는지 확인한다', () => {
     const input = 2000;
-    expect(numberOfLotto(input)).toBe(2);
+    expect(calculateGameCount(input)).toBe(2);
   });
 
   test('로또 번호를 생성할 때에는 중복값이 없도록 한다.', () => {
-    const input = [
-      [1, 2, 3, 4, 5, 6, 7],
-      [1, 2, 33, 33, 4, 5, 6],
+    const overLapInputNumberList = [
+      [1, 2, 3, 4, 4, 5],
+      [1, 2, 33, 33, 4, 5],
     ];
-    expect(isOverlapLottoNumber(input[0])).toBe(true);
-    expect(isOverlapLottoNumber(input[1])).toBe(false);
+    overLapInputNumberList.forEach(lottoNumbers => {
+      expect(isOverlapLottoNumber(lottoNumbers)).toBe(false);
+    });
+
+    const positiveInputNumberList = [
+      [1, 2, 3, 4, 5, 6],
+      [2, 15, 36, 42, 1, 23],
+    ];
+    positiveInputNumberList.forEach(lottoNumbers => {
+      expect(isOverlapLottoNumber(lottoNumbers)).toBe(true);
+    });
   });
 });
