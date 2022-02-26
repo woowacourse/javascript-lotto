f59d15633f0f4e782d02fd775c9b4e25a19df127
JUDONGHYEOK <judonghyeok@gmail.com>
2022-02-26 16:01:13 +0900

    test: lotto 리팩터링에 따른 테스트코드 변경

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Lotto from "../domains/Lotto.js";
 import { LOTTO } from "../constants/constants.js";
 import { isInRange, isDuplicated } from "../validations/utils.js";
@@ -29,6 +30,10 @@ import { LOTTO } from "../constants/constants.js";
 import Lotto from '../domains/Lotto.js';
 import { LOTTO } from '../constants/constants.js';
 >>>>>>> a161b95 (feat: 로또 금액을 입력받아 저장하도록 구현)
+=======
+import Lotto from "../domains/Lotto.js";
+import { LOTTO } from "../constants/constants.js";
+>>>>>>> d542d56 (test: lotto 리팩터링에 따른 테스트코드 변경)
 
 function isInRange(arr, min, max) {
   return arr.every((item) => item >= min && item <= max);
@@ -39,10 +44,11 @@ function isDuplicated(arr) {
   return set.size === arr.length;
 }
 
-describe('로또 단위 테스트 ', () => {
+describe("로또 단위 테스트 ", () => {
   const lotto = new Lotto();
-  lotto.numbers = lotto.pickNumbers(lotto.generateRandomNumber);
+  lotto.pickNumbers();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   test("로또는 1부터 45번까지의 숫자들을 가진다.", () => {
 <<<<<<< HEAD
@@ -52,13 +58,16 @@ describe('로또 단위 테스트 ', () => {
 =======
   test('로또는 1부터 45번까지의 숫자들을 가진다.', () => {
 >>>>>>> a161b95 (feat: 로또 금액을 입력받아 저장하도록 구현)
+=======
+  test("로또는 1부터 45번까지의 숫자들을 가진다.", () => {
+>>>>>>> d542d56 (test: lotto 리팩터링에 따른 테스트코드 변경)
     expect(isInRange(lotto.numbers, LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER)).toBe(
 >>>>>>> 0d4f4f9 (refactor: 로또 관련 상수 결합)
       true
     );
   });
 
-  test('로또는 총 6개의 숫자를 가진다.', () => {
+  test("로또는 총 6개의 숫자를 가진다.", () => {
     expect(lotto.numbers).toHaveLength(6);
   });
 <<<<<<< HEAD
@@ -66,7 +75,7 @@ describe('로또 단위 테스트 ', () => {
 =======
 >>>>>>> 57f94ab (feat: 로또의 각 숫자들은 중복되지 않도록 구현)
 
-  test('로또의 각 숫자들은 중복되지 않는다.', () => {
+  test("로또의 각 숫자들은 중복되지 않는다.", () => {
     expect(isDuplicated(lotto.numbers)).toBe(true);
   });
 <<<<<<< HEAD
