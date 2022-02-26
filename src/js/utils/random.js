8f90592fff4593477b0e0951e72d4a97d85eaf10
Marco <bigdatajang@gmail.com>
2022-02-26 15:29:12 +0900

    :recycle: refactor(random.js): merge dependent code on function 'generatedRandomNumbers'

--- src/js/utils/random.js
+++ src/js/utils/random.js
@@ -1,20 +1,16 @@
 const getRandomNumber = (max, min) =>
   Math.floor(Math.random() * (max + 1 - min)) + min;
 
-const isAlreadyInsertedNumber = (randomNumbers, number) =>
-  randomNumbers.includes(number);
-
-const pushNotYetInsertedNumber = (randomNumbers, number) => {
-  if (!isAlreadyInsertedNumber(randomNumbers, number)) {
-    randomNumbers.push(number);
-  }
-};
+const isExcludedNumber = (numbers, number) => !numbers.includes(number);
 
 const generateRandomNumbers = ({ count, max, min }) => {
   const randomNumbers = [];
 
   while (randomNumbers.length < count) {
-    pushNotYetInsertedNumber(randomNumbers, getRandomNumber(max, min));
+    const randomNumber = getRandomNumber(max, min);
+    if (isExcludedNumber(randomNumbers, randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
   }
 
   return randomNumbers;
