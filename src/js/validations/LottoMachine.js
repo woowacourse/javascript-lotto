7ea7bab4db8f451101691eb5d284ae09f351aa59
JUDONGHYEOK <judonghyeok@gmail.com>
2022-02-26 16:55:53 +0900

    refactor: Validation LottoMachine isValidUnit -> isDividedByPrice

--- src/js/validations/LottoMachine.js
+++ src/js/validations/LottoMachine.js
@@ -1,18 +1,18 @@
-import { LOTTO } from '../constants/constants.js';
+import { LOTTO } from "../constants/constants.js";
 
 function isOverZero(money) {
   return money > 0;
 }
 
-function isValidUnit(money) {
+function isDividedByPrice(money) {
   return money % LOTTO.PRICE === 0;
 }
 
 export default function validateMoney(money) {
-  if (!isValidUnit(money)) {
+  if (!isDividedByPrice(money)) {
     throw new Error(`${LOTTO.PRICE}단위로 입력해주세요`);
   }
   if (!isOverZero(money)) {
-    throw new Error('0원보다 큰 금액을 입력해주세요.');
+    throw new Error("0원보다 큰 금액을 입력해주세요.");
   }
 }
