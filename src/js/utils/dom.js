80e67722cda0ce1c6c3ccc531074de1c7ef173b5
Ahn <jin7969@gmail.com>
2022-02-27 02:51:37 +0900

    refactor: disabled 속성 추가, 삭제 하는 함수 분리

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,1 +1,9 @@
 export const $ = (select) => document.querySelector(select);
+
+export const disableElement = (element) => {
+  element.setAttribute("disabled", true);
+};
+
+export const enableElement = (element) => {
+  element.removeAttribute("disabled");
+};
