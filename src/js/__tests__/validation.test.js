4fcfd2604607e2477755153e2366b72f19a8a154
2019015414 <2019015414@hanyang.ac.kr>
2022-02-26 16:20:20 +0900

    test: 테스트 명 수정
    테스트명에 상황과 결과에 대한 내용 추가

--- src/js/__tests__/validation.test.js
+++ src/js/__tests__/validation.test.js
@@ -2,12 +2,12 @@ import validateCharge from '../validation';
 import { ERROR_MESSAGE } from '../constants/constants';
 
 describe('로또 구입 금액 유효성 검사 테스트', () => {
-  it('구입 금액은 정수인 숫자로 입력해야한다.', () => {
+  it('구입 금액이 정수인 숫자가 아닌 경우 에러 메세지를 띄워준다.', () => {
     const notIntegerValue = 'abc';
     expect(() => validateCharge(notIntegerValue)).toThrow(ERROR_MESSAGE.INTEGER_CHARGE_INPUT);
   });
 
-  it('구입 금액은 1000원 이상이여야 한다.', () => {
+  it('구입 금액이 티켓 금액보다 적은 경우 에러 메세지를 띄워준다.', () => {
     const lessThanMinimumValue = 500;
     expect(() => validateCharge(lessThanMinimumValue)).toThrow(ERROR_MESSAGE.MIN_CHARGE_INPUT);
   });
