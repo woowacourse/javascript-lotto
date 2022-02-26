93e4b613c4eb08f12d48057f3c5c2a8236d1aaab
uk960214 <uk960214@gmail.com>
2022-02-26 11:42:44 +0900

    test: update and refactor cash input tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -3,22 +3,6 @@ import Lotto from '../model/lotto.js';
 import { generateRandomNumberInRange } from '../utils/utils';
 import { LOTTO_NUMBER_RANGE, LOTTO_PRICE } from '../constants/constants';
 
-expect.extend({
-  toBeWithinRange(received, floor, ceiling) {
-    const pass = received >= floor && received <= ceiling;
-    if (pass) {
-      return {
-        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
-        pass: true,
-      };
-    }
-    return {
-      message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
-      pass: false,
-    };
-  },
-});
-
 describe('구입 금액 검증 테스트 ', () => {
   const lottoManger = new LottoManager();
   test('입력 값이 빈 칸이 아니어야 한다.', () => {
@@ -29,9 +13,11 @@ describe('구입 금액 검증 테스트 ', () => {
     const cashInput = '1500';
     expect(() => lottoManger.buyLotto(cashInput)).toThrow();
   });
-  test('입력 값의 범위가 1000 - 50000인지 검증한다.', () => {
-    const cashInput = '55000';
-    expect(() => lottoManger.buyLotto(cashInput)).toThrow();
+  test('입력 값의 범위가 1000원 - 50000원인지 검증한다.', () => {
+    const lowCashInput = '900';
+    expect(() => lottoManger.buyLotto(lowCashInput)).toThrow();
+    const highCashInput = '51000';
+    expect(() => lottoManger.buyLotto(highCashInput)).toThrow();
   });
   test('올바른 입력 값을 입력하면 오류가 발생하지 않는다.', () => {
     const cashInput = '2000';
@@ -48,6 +34,7 @@ describe('로또 번호 생성 테스트', () => {
 
   test('범위가 1 - 45인 고유한 숫자 6개가 생성되는지 확인한다.', () => {
     const lotto = new Lotto();
+    expect(lotto.lottoNumberSet.size).toEqual(6);
     lotto.lottoNumberSet.forEach((number) =>
       expect(number).toBeWithinRange(LOTTO_NUMBER_RANGE.MIN, LOTTO_NUMBER_RANGE.MAX)
     );
@@ -59,4 +46,20 @@ describe('로또 번호 생성 테스트', () => {
     lottoManager.buyLotto(cashInput);
     expect(lottoManager.lottos).toHaveLength(Number(cashInput) / LOTTO_PRICE);
   });
+});
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    }
+    return {
+      message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
+      pass: false,
+    };
+  },
 });
