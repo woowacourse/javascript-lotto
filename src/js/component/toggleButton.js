ce8073794c89f976a87a6b33f3d7a24567a420bc
JASUN LEE <liswktjs@gmail.com>
2022-02-26 21:22:03 +0900

    refactor: 버튼이 diable되었을때의 스타일링과 toggle애니메이션 고침

--- src/js/component/toggleButton.js
+++ src/js/component/toggleButton.js
@@ -2,4 +2,9 @@ import { $ } from '../utils/dom.js';
 
 export const toggleButton = () => {
   $('.purchased-lotto-main').classList.toggle('is-active');
+  if ($('.checkbox').checked) {
+    $('.checkbox').checked = false;
+    return;
+  }
+  $('.checkbox').checked = true;
 };
