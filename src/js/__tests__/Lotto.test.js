febfaec6d29af197d8630ff12a04845f4939e1fa
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-27 00:11:46 +0900

    test: 로또 인스턴스 테스트 검증 추가

--- src/js/__tests__/Lotto.test.js
+++ src/js/__tests__/Lotto.test.js
@@ -1,3 +1,4 @@
+import { LOTTO_SETTING } from '../constants/setting';
 import Lotto from '../models/Lotto';
 
 it('로또 번호는 중복된 숫자들을 가질 수 없다.', () => {
@@ -23,4 +24,15 @@ it('로또 번호를 자동으로 생성할 시 숫자 6개를 가져야 한다.
   lotto.generateNumberList();
 
   expect(lotto.pickedNumber.length).toBe(6);
+});
+
+it('로또 번호를 자동으로 생성할 시 1 ~ 45 사이의 숫자만 생성되어야 한다.', () => {
+  const lotto = new Lotto();
+  lotto.generateNumberList();
+
+  lotto.pickedNumber.forEach((number) => {
+    expect(typeof number).toEqual('number');
+    expect(number).toBeGreaterThanOrEqual(LOTTO_SETTING.MIN_RANDOM_NUMBER);
+    expect(number).toBeLessThan(LOTTO_SETTING.MAX_RANDOM_NUMBER);
+  });
 });
