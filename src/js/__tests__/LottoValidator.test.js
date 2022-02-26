8a46668683353082e74752a54c711f5b81df71c4
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-27 00:43:18 +0900

    test: 로또 구매 금액 입력 오류 테스트 분리

--- src/js/__tests__/LottoValidator.test.js
+++ src/js/__tests__/LottoValidator.test.js
@@ -24,19 +24,17 @@ it('로또 구매 금액은 천원 단위로 입력할 수 있다.', () => {
 });
 
 it('로또 구매 금액을 정상적으로 입력할 수 있다.', () => {
-  // 정상 값 시도
   let userInput = 2000;
   expect(checkValidMoneyInput(userInput)).toBe(undefined);
+});
 
-  // -- 이후 비정상 값 시도
-  // 천원 단위가 아닐 때
-  userInput = 1100;
+it('로또 구매 금액을 정상적으로 입력하지 않았을 때 오류를 출력한다', () => {
+  let userInput = 1100;
   expect(() => {
     checkValidMoneyInput(userInput);
   }).toThrow(ERROR_MESSAGE.WRONG_LOTTO_PRICE_UNIT_INPUT);
 
-  // 숫자가 아닐 때
-  userInput = '일억원';
+  userInput = '일억 콤피코인';
   expect(() => {
     checkValidMoneyInput(userInput);
   }).toThrow(ERROR_MESSAGE.NOT_POSITIVE_NUMBER_INPUT);
