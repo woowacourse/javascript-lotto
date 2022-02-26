5b5240b4d54db0b67ac6f751c950b3964620d89c
Marco <bigdatajang@gmail.com>
2022-02-26 15:42:19 +0900

    :recycle: refactor: check for duplicates using data structure

--- src/js/utils/random.js
+++ src/js/utils/random.js
@@ -1,7 +1,10 @@
 const getRandomNumber = (max, min) =>
   Math.floor(Math.random() * (max + 1 - min)) + min;
 
-const isExcludedNumber = (numbers, number) => !numbers.includes(number);
+const isExcludedNumber = (numbers, number) => {
+  const numbersSet = new Set(numbers);
+  return !numbersSet.has(number);
+};
 
 const generateRandomNumbers = ({ count, max, min }) => {
   const randomNumbers = [];
