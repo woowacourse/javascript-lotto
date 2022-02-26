be6cfc157d5507b8c6913c8384a06c6e740ddac0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-26 22:11:00 +0900

    test: 테스트 코드 설명 명확하게 수정

--- src/js/__tests__/chargedMoneyUtils.test.js
+++ src/js/__tests__/chargedMoneyUtils.test.js
@@ -1,27 +1,27 @@
 import { isPositiveInteger, isDivisible } from '../utils/validator';
 import { LOTTO_SETTING } from '../constants/setting';
 
-it('입력란이 빈칸일 수 없다.', () => {
-  const input = '';
+it('로또 구매 금액은 입력란이 빈칸일 수 없다.', () => {
+  const userInput = '';
 
-  expect(isPositiveInteger(input)).toBe(false);
+  expect(isPositiveInteger(userInput)).toBe(false);
 });
 
-it('숫자만 입력할 수 있다.', () => {
-  const input = '천원';
+it('로또 구매 금액은 숫자만 입력할 수 있다.', () => {
+  const userInput = '천원';
 
-  expect(isPositiveInteger(input)).toBe(false);
+  expect(isPositiveInteger(userInput)).toBe(false);
 });
 
-it('천원 단위로 입력할 수 있다.', () => {
-  const input = 1100;
+it('로또 구매 금액은 천원 단위로 입력할 수 있다.', () => {
+  const userInput = 1100;
 
-  expect(isDivisible(input, LOTTO_SETTING.PRICE)).toBe(false);
+  expect(isDivisible(userInput, LOTTO_SETTING.PRICE)).toBe(false);
 });
 
-it('로또 금액을 입력할 수 있다.', () => {
-  const input = 2000;
+it('로또 구매 금액을 입력할 수 있다.', () => {
+  const userInput = 2000;
 
-  expect(isPositiveInteger(input)).toBe(true);
-  expect(isDivisible(input, LOTTO_SETTING.PRICE)).toBe(true);
+  expect(isPositiveInteger(userInput)).toBe(true);
+  expect(isDivisible(userInput, LOTTO_SETTING.PRICE)).toBe(true);
 });
