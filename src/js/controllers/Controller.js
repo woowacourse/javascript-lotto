f4fff73139571f06a135c6b26210aa6a49d71b3a
jhy979 <jhy979@gmail.com>
2022-02-26 16:32:48 +0900

    refactor: 이벤트 핸들러 함수의 콜백함수를 분리하여 작성

--- src/js/controllers/Controller.js
+++ src/js/controllers/Controller.js
@@ -6,16 +6,28 @@ export class Controller {
   constructor() {
     this.view = new View();
     this.lottoGame = new LottoGame();
-    this.purchaseLotto();
+    this.bindPurchaseEventHandler();
   }
 
-  purchaseLotto() {
-    this.view.purchaseBtn.addEventListener('click', (e) => {
-      e.preventDefault();
+  bindPurchaseEventHandler() {
+    this.view.purchaseBtn.addEventListener(
+      'click',
+      this.#purchaseLotto.bind(this)
+    );
+  }
 
-      if (this.detectInvalidInput()) {
-        return;
-      }
+  bindToggleEvent() {
+    this.view.toggleBtn.addEventListener(
+      'click',
+      this.#controllToggleBtn.bind(this)
+    );
+  }
+
+  #purchaseLotto(e) {
+    try {
+      e.preventDefault();
+      validator.isInputValid(Number(this.view.moneyInput.value));
+
       this.lottoGame.insertMoney(Number(this.view.moneyInput.value));
       this.lottoGame.buyLotto();
 
@@ -26,26 +38,17 @@ export class Controller {
       this.view.showPurchasedLottos(this.lottoGame.lottoWallet);
 
       this.bindToggleEvent();
-    });
-  }
-
-  bindToggleEvent() {
-    this.view.toggleBtn.addEventListener('click', () => {
-      if (this.view.toggleBtn.checked) {
-        this.view.lottosToggleOn(this.lottoGame.lottoWallet);
-        return;
-      }
-      this.view.lottosToggleOff(this.lottoGame.lottoWallet);
-    });
-  }
-
-  detectInvalidInput() {
-    try {
-      validator.isInputValid(Number(this.view.moneyInput.value));
     } catch (err) {
       this.view.clearMoneyInput();
-      return true;
+      alert(err);
     }
-    return false;
+  }
+
+  #controllToggleBtn() {
+    if (this.view.toggleBtn.checked) {
+      this.view.lottosToggleOn(this.lottoGame.lottoWallet);
+      return;
+    }
+    this.view.lottosToggleOff(this.lottoGame.lottoWallet);
   }
 }
