8287193ca9fffc4cc40dbfce7adf441754949692
airman5573 <rpf5573@kakao.com>
2022-02-26 10:22:39 +0900

    :bug: fix(util): 무한 루프 방지
    랜덤 배열을 구하는 함수안에서, 같은 랜덤값이 계속 나오는 경우 무한 루프를 돌수가 있다.
    이 문제를 수정했다.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,25 @@
+export const range = (startInclusive, endInclusive) => {
+  const size = Math.abs(endInclusive - startInclusive) + 1;
+  if (endInclusive >= startInclusive) {
+    return [...Array(size).keys()].map((i) => startInclusive + i);
+  }
+  return [...Array(size).keys()].map((i) => startInclusive - i);
+};
+
+export const removeValByIndexInArr = (arr, index) => {
+  const newArr = [...arr];
+  newArr[index] = newArr[newArr.length - 1];
+  newArr.pop();
+  return newArr;
+};
+
 export const pickNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const pickUniqueNumbersInRange = (startInclusive, endInclusive, count) => {
-  const numSet = new Set([pickNumberInRange(startInclusive, endInclusive)]);
-
-  while (numSet.size < count) {
-    numSet.add(pickNumberInRange(startInclusive, endInclusive));
+  let candidates = range(startInclusive, endInclusive);
+  const until = endInclusive - startInclusive + 1 - count;
+  for (let i = 0; i < until; i += 1) {
+    candidates = removeValByIndexInArr(candidates, pickNumberInRange(0, candidates.length - 1));
   }
-
-  return [...numSet.values()];
+  return candidates;
 };
