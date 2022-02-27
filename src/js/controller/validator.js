fc5651a70629fefce818006774b08e06fe2688e5
jswith <9geul@naver.com>
2022-02-27 22:00:05 +0900

    refactor: 추상화 수준 조정

--- src/js/controller/validator.js
+++ src/js/controller/validator.js
@@ -1,12 +1,7 @@
 import { MONEY_INPUT } from '../constants/constants';
 
 const isThousandMultiple = (money) => money % MONEY_INPUT.MIN_PRICE === 0;
-const isOverThouand = (money) => money >= MONEY_INPUT.MIN_PRICE;
-const isUnderMillion = (money) => money <= MONEY_INPUT.MAX_PRICE;
-const isValidMoneyRange = (money) => isOverThouand(money) && isUnderMillion(money);
+const isValidMoneyRange = (money) =>
+  money >= MONEY_INPUT.MIN_PRICE && money <= MONEY_INPUT.MAX_PRICE;
 
-export const isValidMoneyInput = (money) =>
-  isThousandMultiple(money) &&
-  isOverThouand(money) &&
-  isUnderMillion(money) &&
-  isValidMoneyRange(money);
+export const isValidMoneyInput = (money) => isThousandMultiple(money) && isValidMoneyRange(money);
