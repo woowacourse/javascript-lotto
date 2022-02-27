f2a57d60fbc18792609cdd1c3d846712637ea520
kamwoo <rkadndud06@naver.com>
2022-02-27 16:13:12 +0900

    refactor: toggle view를 띄우는 메소드로 이름 변경

--- src/js/views/subViews/TicketView.js
+++ src/js/views/subViews/TicketView.js
@@ -11,7 +11,7 @@ export default class TicketView {
   render() {
     this.mountTemplate();
     this.afterMounted();
-    this.updateShowNumberToggle();
+    this.mountShowNumberToggleView();
   }
 
   mountTemplate() {
@@ -23,7 +23,7 @@ export default class TicketView {
     this.$showNumberToggleArea = $(SELECTOR.SHOW_NUMBER_TOGGLE_AREA);
   }
 
-  updateShowNumberToggle() {
+  mountShowNumberToggleView() {
     this.$showNumberToggleArea.innerHTML = template.showNumberToggleArea(
       this.isShowNumber
     );
