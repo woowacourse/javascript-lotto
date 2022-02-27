ca22ce04b1c09d899c20e65377141ba30f8af94c
kamwoo <rkadndud06@naver.com>
2022-02-27 17:22:43 +0900

    test: 생성된 로또의 번호들이 중복되지 않는지 검증 기능 추가

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,66 +1,66 @@
-import { getLottoNumber } from '../utils/lottoUtils.js';
 import {
   validator,
   isValidLottoNumberRange,
   isValidLottoList,
   isValidLotto,
+  isValidDuplicatedLottoNumber,
 } from '../utils/validator.js';
-import { ERROR_MESSAGE } from '../configs/contants.js';
+import { getRandomInt } from '../utils/utils.js';
+
+import { ERROR_MESSAGE, LOTTO } from '../configs/contants.js';
 import LottoModel from '../models/LottoModel.js';
 
 describe('금액이 주어지면', () => {
   test('발급할 로또 개수를 구할 수 있어야 한다.', () => {
     const purchaseAmount = 2000;
 
-    expect(LottoModel.getNumberOfLotto(purchaseAmount)).toBe(2);
+    expect(LottoModel.getCountOfLotto(purchaseAmount)).toBe(2);
+  });
+});
+
+describe('입력된 금액의 유효성을 검증하여', () => {
+  test('숫자가 아니면 에러를 throw한다.', () => {
+    const purchaseAmount = '만원';
+
+    expect(() => {
+      validator.checkPurchaseAmount(purchaseAmount);
+    }).toThrowError(ERROR_MESSAGE.NOT_A_NUMBER);
   });
 
-  describe('유효성을 검증하여', () => {
-    test('숫자가 아니면 에러를 throw한다.', () => {
-      const purchaseAmount = '만원';
+  test('로또의 금액인 1000보다 작으면 에러를 throw한다.', () => {
+    const purchaseAmount = 999;
 
-      expect(() => {
-        validator.checkPurchaseAmount(purchaseAmount);
-      }).toThrowError(ERROR_MESSAGE.NOT_A_NUMBER);
-    });
+    expect(() => {
+      validator.checkPurchaseAmount(purchaseAmount);
+    }).toThrowError(ERROR_MESSAGE.OUT_OF_PURCHASE_AMOUNT_RANGE);
+  });
 
-    test('1000으로 나눠서 안떨어지는 금액이 입력되면 에러를 throw한다.', () => {
-      const purchaseAmount = 1001;
+  test('최대 입력 금액인 100000보다 크면 아니면 에러를 throw한다.', () => {
+    const purchaseAmount = 100001;
 
-      expect(() => {
-        validator.checkPurchaseAmount(purchaseAmount);
-      }).toThrowError(ERROR_MESSAGE.NOT_DIVIDED_BY_THOUSAND);
-    });
+    expect(() => {
+      validator.checkPurchaseAmount(purchaseAmount);
+    }).toThrowError(ERROR_MESSAGE.OUT_OF_PURCHASE_AMOUNT_RANGE);
+  });
+});
 
-    test('1000부터 10000 사이가 아니면 에러를 throw한다.', () => {
-      const firstPurchaseAmount = 0;
-      const secondPurchaseAmount = 11000;
+describe('로또 번호를 생성하여', () => {
+  const lottoNumber = getRandomInt(
+    LOTTO.NUMBER_RANGE.MIN,
+    LOTTO.NUMBER_RANGE.MAX
+  );
 
-      expect(() => {
-        validator.checkPurchaseAmount(firstPurchaseAmount);
-      }).toThrowError(ERROR_MESSAGE.OUT_OF_PURCHASE_AMOUNT_RANGE);
+  test('생성된 로또 번호가 정수여야한다.', () => {
+    expect(Number.isInteger(lottoNumber)).toBe(true);
+  });
 
-      expect(() => {
-        validator.checkPurchaseAmount(secondPurchaseAmount);
-      }).toThrowError(ERROR_MESSAGE.OUT_OF_PURCHASE_AMOUNT_RANGE);
-    });
+  test('생성된 로또 번호가 1부터 45 사이여야 한다.', () => {
+    expect(isValidLottoNumberRange(lottoNumber)).toBe(true);
   });
 });
 
 describe('LottoModel은', () => {
-  const lottoAppModel = new LottoModel();
-
-  describe('로또 번호를 생성하여', () => {
-    const lottoNumber = getLottoNumber();
-
-    test('생성된 로또 번호가 정수여야한다.', () => {
-      expect(Number.isInteger(lottoNumber)).toBe(true);
-    });
-
-    test('생성된 로또 번호가 1부터 45 사이여야 한다.', () => {
-      expect(isValidLottoNumberRange(lottoNumber)).toBe(true);
-    });
-  });
+  const lottoModel = new LottoModel();
 
   test('6개의 로또번호를 가진 로또를 생성할 수 있어야 한다.', () => {
     const lotto = LottoModel.issueLotto();
@@ -68,13 +68,16 @@ describe('LottoModel은', () => {
     expect(isValidLotto(lotto)).toBe(true);
   });
 
+  test('6개의 로또번호에 중복이 없어야 한다.', () => {
+    const lotto = LottoModel.issueLotto();
+
+    expect(isValidDuplicatedLottoNumber(lotto)).toBe(true);
+  });
+
   test('주어진 개수만큼 로또를 자동 구매할 수 있어야 한다.', () => {
     const lottoCount = 6;
+    const lottoList = lottoModel.issueLottosWithCount(lottoCount);
 
-    lottoAppModel.issueLottoWithCount(lottoCount);
-
-    expect(isValidLottoList(lottoAppModel.state.lottoList, lottoCount)).toBe(
-      true
-    );
+    expect(isValidLottoList(lottoList, lottoCount)).toBe(true);
   });
 });
