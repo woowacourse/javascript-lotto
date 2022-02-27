3361989b68d7171b6c81d56036a28719e3db41e8
kamwoo <rkadndud06@naver.com>
2022-02-27 18:55:04 +0900

    refactor: 로또 숫자 리스트 생성 메소드 이름 변경

--- src/js/models/Lotto/Lotto.js
+++ src/js/models/Lotto/Lotto.js
@@ -3,14 +3,14 @@ import { LOTTO } from '../../configs/contants.js';
 
 export default class Lotto {
   constructor() {
-    this.numbers = this.getLottoNumberList();
+    this.numbers = this.getLottoNumbers();
   }
 
   getNumbers() {
     return this.numbers;
   }
 
-  getLottoNumberList() {
+  getLottoNumbers() {
     return getRandomList(
       LOTTO.NUMBER_LENGTH,
       LOTTO.NUMBER_RANGE.MIN,
