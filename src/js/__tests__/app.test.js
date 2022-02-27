010e22c681e7535d3397c8f3a7746a32ae5dca95
sanaandmomo <qajjang46@naver.com>
2022-02-27 16:31:43 +0900

    test: 1000원 이상 투입 테스트명을 유저 친화적으로 바꾸고 경계값을 테스트

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -5,16 +5,24 @@ import { createRandomNumbers } from '../utils/index.js';
 import { LOTTO_RULES } from '../constant/index.js';
 
 describe('요금을 1000원 이상 투입해야 한다.', () => {
-  test('500원을 입력하면 false가 반환돼야 한다.', () => {
-    const fare = 500;
+  test('1000원 미만은 게임을 실행할 수 없다.', () => {
+    const fare1 = 500;
+    const fare2 = -500;
+    const fare3 = 100.1;
+    const fare4 = 999;
 
-    expect(isEnoughFare(fare)).toBe(false);
+    expect(isEnoughFare(fare1)).toBe(false);
+    expect(isEnoughFare(fare2)).toBe(false);
+    expect(isEnoughFare(fare3)).toBe(false);
+    expect(isEnoughFare(fare4)).toBe(false);
   });
 
-  test('5000원을 입력하면 true가 반환돼야 한다.', () => {
-    const fare = 5000;
+  test('1000원 이상으로 게임 실행이 가능하다.', () => {
+    const fare1 = 1000;
+    const fare2 = 1000.01;
 
-    expect(isEnoughFare(fare)).toBe(true);
+    expect(isEnoughFare(fare1)).toBe(true);
+    expect(isEnoughFare(fare2)).toBe(true);
   });
 });
 
