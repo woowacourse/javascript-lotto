54cc885ef7bf334b6ad52a3f892b53189dbf12ca
jswith <9geul@naver.com>
2022-02-28 00:31:33 +0900

    refactor: validator 수정

--- src/js/controller/validator.js
+++ src/js/controller/validator.js
@@ -1,7 +1,9 @@
 import { MONEY_INPUT } from '../constants/constants';
 
-const isThousandMultiple = (money) => money % MONEY_INPUT.MIN_PRICE === 0;
-const isValidMoneyRange = (money) =>
-  money >= MONEY_INPUT.MIN_PRICE && money <= MONEY_INPUT.MAX_PRICE;
-
-export const isValidMoneyInput = (money) => isThousandMultiple(money) && isValidMoneyRange(money);
+export const isValidMoneyInput = (money) => {
+  return (
+    money % MONEY_INPUT.MIN_PRICE === 0 &&
+    money >= MONEY_INPUT.MIN_PRICE &&
+    money <= MONEY_INPUT.MAX_PRICE
+  );
+};
