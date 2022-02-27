03691fbd30263c2bfdbce4839b5d87b87eeae96f
jhy979 <jhy979@gmail.com>
2022-02-27 13:09:16 +0900

    fix: 유효성 검증에 대한 케이스 추가

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,8 +1,8 @@
 export const ERROR_MESSAGE = {
-  NEGATIVE_INPUT_ERROR: '입력한 금액이 양수가 아닙니다.',
-  NOT_INTEGER_INPUT_ERROR: '입력한 금액이 자연수가 아닙니다.',
-  NOT_MUTIPLE_THOUSAND: '입력한 금액이 1000원 단위의 금액이 아닙니다.',
-  NOT_AVAILABLE_MONEY: '100개 이상의 로또를 한 번에 구매할 수는 없습니다.',
+  NEGATIVE_INPUT: '입력한 금액이 양수가 아닙니다.',
+  NOT_INTEGER_INPUT: '입력한 금액이 자연수가 아닙니다.',
+  TOO_BIG_INPUT: '100개 이상의 로또를 한 번에 구매할 수는 없습니다.',
+  TOO_SMALL_INPUT: '로또 한 장의 가격은 1000원입니다.',
 };
 
 export const CONDITIONS = {
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,18 @@
-import { ERROR_MESSAGE } from './constants/constants';
+import { CONDITIONS, ERROR_MESSAGE } from './constants/constants';
 
 export const validator = {
   isInputValid(input) {
     if (!this.isMoneyPositive(input)) {
-      throw new Error(ERROR_MESSAGE.NEGATIVE_INPUT_ERROR);
+      throw new Error(ERROR_MESSAGE.NEGATIVE_INPUT);
     }
     if (!this.isMoneyInteger(input)) {
-      throw new Error(ERROR_MESSAGE.NOT_INTEGER_INPUT_ERROR);
+      throw new Error(ERROR_MESSAGE.NOT_INTEGER_INPUT);
     }
-    if (!this.isMoneyTooBig(input)) {
-      throw new Error(ERROR_MESSAGE.NOT_AVAILABLE_MONEY);
+    if (this.isMoneyTooBig(input)) {
+      throw new Error(ERROR_MESSAGE.TOO_BIG_INPUT);
+    }
+    if (this.isMoneyTooSmall(input)) {
+      throw new Error(ERROR_MESSAGE.TOO_SMALL_INPUT);
     }
   },
 
@@ -22,10 +25,13 @@ export const validator = {
   },
 
   isMoneyTooBig(input) {
-    return input < 100000;
+    return input >= CONDITIONS.LOTTO_PRICE * 100;
+  },
+
+  isMoneyTooSmall(input) {
+    return input < CONDITIONS.LOTTO_PRICE;
   },
 };
-
 export const getValues = {
   randomInt(min, max) {
     min = Math.ceil(min);
