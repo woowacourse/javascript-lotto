93d17085af02f0a732d1b6f4b464fa8fde86d8b0
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-02-28 17:42:18 +0900

    :recycle: refactor: use 'set' for deduplication

--- src/js/utils/random.js
+++ src/js/utils/random.js
@@ -1,23 +1,14 @@
 const getRandomNumber = (max, min) =>
   Math.floor(Math.random() * (max + 1 - min)) + min;
 
-const isAlreadyInsertedNumber = (randomNumbers, number) =>
-  randomNumbers.includes(number);
-
-const pushNotYetInsertedNumber = (randomNumbers, number) => {
-  if (!isAlreadyInsertedNumber(randomNumbers, number)) {
-    randomNumbers.push(number);
-  }
-};
-
 const generateRandomNumbers = ({ count, max, min }) => {
-  const randomNumbers = [];
+  const randomNumbers = new Set();
 
-  while (randomNumbers.length < count) {
-    pushNotYetInsertedNumber(randomNumbers, getRandomNumber(max, min));
+  while (randomNumbers.size < count) {
+    randomNumbers.add(getRandomNumber(max, min));
   }
 
-  return randomNumbers;
+  return Array.from(randomNumbers);
 };
 
 export default generateRandomNumbers;
