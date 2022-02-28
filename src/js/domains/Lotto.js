8cc6d91aad9fb228004ea33856863fc916c63c34
junzzi <jjh01027717502@gmail.com>
2022-02-28 17:44:33 +0900

    feat: 로또 도메인은 당첨 정보를 계산할 수 있다

--- src/js/domains/Lotto.js
+++ src/js/domains/Lotto.js
@@ -1,4 +1,5 @@
 import { NUMBER } from '../constants/number';
+import { RANK } from '../constants/win';
 import { lottoNumberClosure } from '../utils/gameUtil';
 
 class Lotto {
@@ -8,12 +9,24 @@ class Lotto {
     this.lottoNumbers = this.createLottoNumbers();
   }
 
+  getLottoNumbers() {
+    return this.lottoNumbers;
+  }
+
   /** 기존 로직 : Set을 활용하여, 중복이 발생한 경우 숫자를 또 받는 로직으로 구현 */
   /** 변경 로직 : 1~45 까지의 숫자가 있는 배열에서 하나하나 뽑아온다 */
   /** 변경 이유 : 중복이 여러번 반복해서 발생하는 경우, 로직의 반복이 너무 많이 될 것 같다. */
   createLottoNumbers() {
     const getLottoNumber = lottoNumberClosure();
     return [...new Array(NUMBER.LOTTO_NUMBER_LENGTH)].map(() => getLottoNumber());
+  }
+
+  computeWinResult(winNumbers, bonusNumber) {
+    const { length: numberMatchCount } = this.lottoNumbers.filter((number) =>
+      winNumbers.includes(number)
+    );
+    const isMatchBonus = this.lottoNumbers.includes(bonusNumber);
+    return RANK[`${numberMatchCount}${isMatchBonus}`] ?? RANK.UNRANK;
   }
 }
 
