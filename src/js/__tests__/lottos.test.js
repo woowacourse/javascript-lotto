89d42eea600e27c3b4ab4f3c28298581c32fb154
이아현 <lah1203@naver.com>
2022-02-28 11:32:59 +0900

    test: 테스트 내역 변경

--- src/js/__tests__/lottos.test.js
+++ src/js/__tests__/lottos.test.js
@@ -1,18 +1,28 @@
 import Lottos from '../model/Lottos.js';
 
-describe('Lottos 인스턴스 객체 생성 테스트', () => {
-  it('Lottos 인스턴스 객체 생성 시, 데이터가 초기화되어야 한다.', () => {
+describe('Lottos 인스턴스 테스트', () => {
+  it('생성 시 내부 데이터가 초기화된다.', () => {
     const lottos = new Lottos();
 
     expect(lottos.getLottos()).toBeNull();
   });
 
-  it('전달 매개변수 값에 따른 원하는 로또 개수가 생성되어야 한다.', () => {
+  it('로또가 원하는 개수만큼 생성된다.', () => {
     const lottos = new Lottos();
     const count = 10;
 
     lottos.makeLottos(count);
 
     expect(lottos.getLottos()).toHaveLength(count);
+  });
+
+  it('로또를 초기화한다.', () => {
+    const lottos = new Lottos();
+    const count = 10;
+
+    lottos.makeLottos(count);
+    lottos.reset();
+
+    expect(lottos.getLottos()).toBeNull();
   });
 });
--- src/js/__tests__/util.test.js
+++ src/js/__tests__/util.test.js
@@ -1,24 +1,30 @@
 import { RULES } from '../constants/index.js';
 import { convertToNumber, pickLottoNumber } from '../util/common.js';
 
-describe('로또 번호 랜덤 생성 함수 기능 테스트', () => {
-  it('로또 랜덤 번호는 총 6개를 가져야 한다.', () => {
+describe('로또 번호 생성 테스트', () => {
+  it(`한 로또에는 ${RULES.LOTTO_NUMS}개의 숫자가 있다.`, () => {
     const numberList = pickLottoNumber(RULES.LOTTO_NUMS);
 
     expect(numberList).toHaveLength(RULES.LOTTO_NUMS);
   });
 
-  it('로또 랜덤 번호는 중복이 있어서는 안된다.', () => {
+  it('로또 번호에는 중복이 없다.', () => {
     const numberList = pickLottoNumber(RULES.LOTTO_NUMS);
 
     expect(numberList).toHaveLength(new Set(numberList).size);
   });
+
+  it(`모든 로또 번호는 ${RULES.MIN_LOTTO_NUMBER}~${RULES.MAX_LOTTO_NUMBER} 사이에 존재한다.`, () => {
+    const numberList = pickLottoNumber(RULES.LOTTO_NUMS);
+
+    expect(numberList.filter(number => number >= RULES.MIN_LOTTO_NUMBER && number <= RULES.MAX_LOTTO_NUMBER)).toHaveLength(RULES.LOTTO_NUMS);
+  })
 });
 
-describe('숫자 타입으로 변환해주는 함수 기능 테스트', () => {
-  it('문자열을 숫자 타입으로 변환해야 한다.', () => {
+describe('문자열에서 숫자로의 변환 테스트', () => {
+  it('문자열을 숫자로 변환한다.', () => {
     const string = '11000';
 
-    expect(convertToNumber(string)).toEqual(expect.any(Number));
+    expect(convertToNumber(string)).toEqual(Number(string));
   });
 });
