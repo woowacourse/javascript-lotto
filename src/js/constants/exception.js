99dec1ba45af5a70c8906995732edbcb30d8aff2
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-02-28 17:24:49 +0900

    :recycle: refactor: use constants

--- src/js/constants/exception.js
+++ src/js/constants/exception.js
@@ -1,10 +1,13 @@
+import LOTTO from './lotto.js';
+import autoComma from '../utils/autoComma.js';
+
 const EXCEPTION = Object.freeze({
   INVALID_RANGE: {
-    MINIMUM: '1000원 이상의 금액이 투입되어야 합니다.',
+    MINIMUM: `${autoComma(LOTTO.PRICE_PER_TICKET)}원 이상의 금액이 투입되어야 합니다.`,
     MAXIMUM:
-      '너무 큰 돈을 입력하셨습니다. 1,000,000원 이하의 금액이 투입되어야 합니다.',
+      `너무 큰 돈을 입력하셨습니다. ${autoComma(LOTTO.PRICE_PER_TICKET * LOTTO.INVENTORY)}원 이하의 금액이 투입되어야 합니다.`,
   },
-  INVALID_UNIT: '1000원 단위의 금액이 투입되어야 합니다.',
+  INVALID_UNIT: `${autoComma(LOTTO.PRICE_PER_TICKET)}원 단위의 금액이 투입되어야 합니다.`,
 });
 
 export default EXCEPTION;
---
+++ src/js/utils/autoComma.js
@@ -0,0 +1,3 @@
+const autoComma = text => String(text).replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+
+export default autoComma;
