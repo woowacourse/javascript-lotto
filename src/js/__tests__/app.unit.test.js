c7a58af347bb0254345e8f07516eaf9d89003d02
sangminsong <ssmin940606@gmail.com>
2022-03-01 23:35:54 +0900

    test: unit 테스트 추가
    - 입력한 당첨번호와 구매한 로또의 매칭 결과를 비교할 수 있어야한다

--- src/js/__tests__/app.unit.test.js
+++ src/js/__tests__/app.unit.test.js
@@ -5,37 +5,70 @@ import {
 } from '../utils/validator.js';
 import LottoModel from '../lottoModel.js';
 
-test('금액은 천 단위로 입력해야 한다', () => {
-  const purchaseMoney = 3000;
+describe('구매 금액 유효성 검사 유틸 테스트', () => {
+  test('금액이 천 단위인지 판단 할 수 있어야한다.', () => {
+    const purchaseMoney = 3000;
 
-  expect(isDividedByThousand(purchaseMoney)).toBe(true);
+    expect(isDividedByThousand(purchaseMoney)).toBe(true);
+  });
+
+  test('금액이 빈값인지 판단 할 수 있어야한다. ', () => {
+    const purchaseMoney = '';
+
+    expect(isEmptyValue(purchaseMoney)).toBe(true);
+  });
+
+  test('금액이 양의 정수인지 판단 할 수 있어야한다', () => {
+    let purchaseMoney = -1000;
+
+    expect(isPositiveValue(purchaseMoney)).toBe(false);
+
+    purchaseMoney = 0;
+
+    expect(isPositiveValue(purchaseMoney)).toBe(false);
+  });
 });
 
-test('금액은 빈값으로 입력할 수 없다 ', () => {
-  const purchaseMoney = '';
+describe('로또 모델 테스트', () => {
+  test('구입한 로또 금액만큼 로또가 구매되어야 한다.', () => {
+    const lottoModel = new LottoModel();
+    const lottoCount = 4;
 
-  expect(isEmptyValue(purchaseMoney)).toBe(true);
-});
+    lottoModel.createLottoList(lottoCount);
 
-test('금액은 양의 정수를 입력해야한다', () => {
-  let purchaseMoney = -1000;
+    const lottoResult = lottoModel.lottoList;
+    const isCorrectLottoLength = lottoResult.every(
+      (result) => result.size === 6
+    );
 
-  expect(isPositiveValue(purchaseMoney)).toBe(false);
+    expect(lottoResult).toHaveLength(lottoCount);
+    expect(isCorrectLottoLength).toBe(true);
+  });
 
-  purchaseMoney = 0;
+  test('입력한 당첨번호와 구매한 로또의 매칭 결과를 비교할 수 있어야한다.', () => {
+    const lottoModel = new LottoModel();
 
-  expect(isPositiveValue(purchaseMoney)).toBe(false);
-});
+    const winningLottoNumbers = [1, 2, 3, 4, 5, 6];
+    const winningLottoBonusNumber = [7];
 
-test('구입한 로또 금액만큼 로또 개수를 확인할 수 있어야 한다', () => {
-  const lottoModel = new LottoModel();
-  const lottoCount = 4;
+    const lottoList = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [19, 10, 20, 21, 9, 6],
+    ];
 
-  lottoModel.createLottoList(lottoCount);
+    const lottoMatchingResult = lottoModel.calcLottoMachingResult(
+      winningLottoNumbers,
+      winningLottoBonusNumber,
+      lottoList
+    );
 
-  const lottoResult = lottoModel.lottoList;
-  const isCorrectLottoLength = lottoResult.every((result) => result.size === 6);
-
-  expect(lottoResult).toHaveLength(lottoCount);
-  expect(isCorrectLottoLength).toBe(true);
+    expect(lottoMatchingResult).toBe({
+      '3개': 0,
+      '4개': 0,
+      '5개': 0,
+      '5개+보너스볼': 1,
+      '6개': 1,
+    });
+  });
 });
