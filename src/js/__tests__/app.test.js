39c9e842514891b6929bbc2411cd2eeec86cf003
woose28 <dldntmdsla@naver.com>
2022-03-01 19:20:59 +0900

    test: 지난 주 당첨 번호의 개수를 검증하는 테스트 코드 작성

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -82,11 +82,15 @@ describe(`중복되지 않는 1 ~ 45 사이의 숫자를 6개 생성한다.`, ()
 describe('지난 주 당첨 번호는 중복되지 않는 1 ~ 45 사이의 6개의 숫자여야 한다.', () => {
   const previousWinningNumber = [7, 15, 30, 37, 39, 44];
 
-  test('지난 주 당첨 번호 숫자들은 1 ~ 45 사이의 숫자여야 한다.', () => {
+  test('지난주 당첨 번호 숫자들은 1 ~ 45 사이의 숫자여야 한다.', () => {
     expect(isValidRange(previousWinningNumber)).toBeTruthy();
   });
 
-  test('지난 주 당첨 번호에는 중복된 숫자가 있으면 안된다.', () => {
+  test('지난주 당첨 번호에는 중복된 숫자가 있으면 안된다.', () => {
     expect(isNotOverlapped(previousWinningNumber)).toBeTruthy();
+  });
+
+  test('지난주 당첨 번호의 숫자 개수는 6개여야 한다.', () => {
+    expect(isValidCount(previousWinningNumber)).toBeTruthy();
   });
 });
