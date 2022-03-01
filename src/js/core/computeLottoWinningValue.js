7e8182c88b4c82ed5d5fb4a665752354abe6e564
JASUN LEE <liswktjs@gmail.com>
2022-03-01 22:28:10 +0900

    ⚡️ feat: 일치 개수에 따라 몇등인지 알아내는 기능 구현 1,2,3,4,5 중 몇 등인지 확인할 수 있는 함수를 구현

--- src/js/core/computeLottoWinningValue.js
+++ src/js/core/computeLottoWinningValue.js
@@ -2,6 +2,7 @@ import {
   LOTTO_PRICE,
   MATCH_NUMBER_3,
   MATCH_NUMBER_3_PRIZE,
+  MATCH_NUMBER_4,
   MATCH_NUMBER_4_PRIZE,
   MATCH_NUMBER_5,
   MATCH_NUMBER_5_PRIZE,
@@ -34,12 +35,12 @@ export function computeLottoRateOfReturn(
   currentLottoNumbers,
   lastLottoNumberList,
 ) {
-  let lastLottoNumbers = lastLottoNumberList;
-  const bounsNumber = lastLottoNumbers.pop();
+  let lastLottoNumbers = lastLottoNumberList[0];
+  const bounsNumber = lastLottoNumberList[1];
   let totalPrizeMoney = 0;
   currentLottoNumbers.forEach(lotto => {
     const corretCount = lotto.numbers.filter(num => {
-      if (lastLottoNumberList.includes(num)) return num;
+      if (lastLottoNumbers.includes(num)) return num;
     }).length;
     if (corretCount === MATCH_NUMBER_5 && lotto.numbers.includes(bounsNumber)) {
       totalPrizeMoney = totalPrizeMoney + MATCH_NUMBER_BONUS_PRIZE;
@@ -51,4 +52,43 @@ export function computeLottoRateOfReturn(
     (totalPrizeMoney / (currentLottoNumbers.length * LOTTO_PRICE)) *
     PERCENT_RATE
   );
+}
+
+export function computeLottoRankList(currentLottoNumbers, lastLottoNumberList) {
+  let lastLottoNumbers = lastLottoNumberList[0];
+  const bonusNumber = lastLottoNumberList[1];
+  let lottoRankObj = {
+    rank_5: 0,
+    rank_4: 0,
+    rank_3: 0,
+    rank_2: 0,
+    rank_1: 0,
+  };
+  currentLottoNumbers.forEach(lotto => {
+    const correctCount = lotto.numbers.filter(num => {
+      if (lastLottoNumbers.includes(num)) return num;
+    }).length;
+    if (
+      correctCount === MATCH_NUMBER_5 &&
+      lotto.numbers.includes(bonusNumber)
+    ) {
+      lottoRankObj.rank_2++;
+    }
+    switch (correctCount) {
+      case MATCH_NUMBER_3:
+        lottoRankObj.rank_5++;
+        break;
+      case MATCH_NUMBER_4:
+        lottoRankObj.rank_4++;
+        break;
+      case MATCH_NUMBER_5:
+        lottoRankObj.rank_3++;
+        break;
+      case MATCH_NUMBER_6:
+        lottoRankObj.rank_1++;
+        break;
+    }
+  });
+  const lottoRankList = Object.values(lottoRankObj);
+  return lottoRankList;
 }
