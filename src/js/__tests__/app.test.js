16371d731ee9d173faff58d3c3e77b214c8d5c28
woose28 <dldntmdsla@naver.com>
2022-03-01 19:38:18 +0900

    test: 보너스 당첨 번호가 지난 중 당첨 번호에 포함되는지 검증하는 테스트 작성

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -94,3 +94,12 @@ describe('지난 주 당첨 번호는 중복되지 않는 1 ~ 45 사이의 6개�
     expect(isValidCount(previousWinningNumber)).toBeTruthy();
   });
 });
+
+describe('보너스 당첨 번호는 지난주 당첨 번호에 속해있지 않는 1 ~ 45 사이의 숫자여야 한다.', () => {
+  test('보너스 당첨 번호는 지난주 당첨 번호에 속해있으면 안된다.', () => {
+    const previousWinningNumber = [7, 15, 30, 37, 39, 44];
+    const bonumsNumber = 18;
+
+    expect(isNotContainedWinningNumber(previousWinningNumber, bonumsNumber)).toBeTruthy();
+  });
+});
