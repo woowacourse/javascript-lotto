ea5daf480d45bc28af9759ebd0f9b2f8ca9b2dd4
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-01 22:17:35 +0900

    feat: get winning result
    details below:
    - get winning results(bonus is 0.5)
    - calculate earn rate(two decimal places of percentage)

--- src/js/model/LottoGame.js
+++ src/js/model/LottoGame.js
@@ -7,6 +7,15 @@ export class LottoGame {
     this.lottoWallet = [];
     this.winningNumbers = [];
     this.bonusNumber;
+    this.result = {
+      matchSix: 0,
+      matchFiveBonus: 0,
+      matchFive: 0,
+      matchFour: 0,
+      matchThree: 0,
+      matchUnderThree: 0,
+    };
+    this.earnRate;
   }
 
   insertMoney(moneyInput) {
@@ -25,5 +34,50 @@ export class LottoGame {
 
   enterBonusNumber(bonusNumber) {
     this.bonusNumber = bonusNumber;
+  }
+
+  findResult() {
+    const result = [];
+    this.lottoWallet.forEach((lotto) => {
+      let temp = this.winningNumbers.filter((num) => lotto.numbers.includes(num)).length;
+      if (lotto.numbers.includes(this.bonusNumber)) {
+        temp += 0.5;
+      }
+      result.push(temp);
+    });
+
+    this.saveResult(result);
+  }
+
+  saveResult(result) {
+    let count = result.filter((element) => 6 === element).length;
+    this.result.matchSix = count;
+
+    count = result.filter((element) => 5.5 === element).length;
+    this.result.matchFiveBonus = count;
+
+    count = result.filter((element) => 5 === element).length;
+    this.result.matchFive = count;
+
+    count = result.filter((element) => 4 === element).length;
+    this.result.matchFour = count;
+
+    count = result.filter((element) => 3 === element).length;
+    this.result.matchThree = count;
+
+    count = result.filter((element) => 3 > element).length;
+    this.result.matchUnderThree = count;
+  }
+
+  calculateEarnRate() {
+    let earnMoney = 0;
+    earnMoney += this.result.matchSix * 2000000000;
+    earnMoney += this.result.matchFiveBonus * 30000000;
+    earnMoney += this.result.matchFive * 1500000;
+    earnMoney += this.result.matchFour * 50000;
+    earnMoney += this.result.matchThree * 5000;
+
+    this.earnRate = Number(((earnMoney / (this.lottoWallet.length * 1000)) * 100 - 100).toFixed(2));
+    console.log(this.earnRate);
   }
 }
