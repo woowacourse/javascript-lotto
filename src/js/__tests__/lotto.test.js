d644b2cda95d7df4188d1db5d2e3982448917b5e
JASUN LEE <liswktjs@gmail.com>
2022-03-01 13:54:11 +0900

    🐛 fix: 수익율 test코드 수정 computeLottoRateofRetturn에서 함수를 변경함에 따라 lotto.test.js에서도 변경사항 적용

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,50 +1,9 @@
-import {
-  MATCH_NUMBER_3_PRIZE,
-  MATCH_NUMBER_4_PRIZE,
-  MATCH_NUMBER_5_PRIZE,
-  MATCH_NUMBER_6_PRIZE,
-  MATCH_NUMBER_BONUS_PRIZE,
-} from '../constants/constant.js';
+import { computeLottoRateOfReturn } from '../core/computeLottoRateOfReturn.js';
+import Lotto from '../core/lotto.js';
 import {
   calculateGameCount,
   isOverlapLottoNumber,
 } from '../core/makeLottoList.js';
-
-function getLottoPrize(correctCount) {
-  let prizeMoney = 0;
-  switch (correctCount) {
-    case 3:
-      prizeMoney = MATCH_NUMBER_3_PRIZE;
-      break;
-    case 4:
-      prizeMoney = MATCH_NUMBER_4_PRIZE;
-      break;
-    case 5:
-      prizeMoney = MATCH_NUMBER_5_PRIZE;
-      break;
-    case 6:
-      prizeMoney = MATCH_NUMBER_6_PRIZE;
-      break;
-  }
-  return prizeMoney;
-}
-
-function computeRateOfReturn(lottoInfo, lastLottoInfo) {
-  let totalPrizeMoney = 0;
-  lottoInfo.currentLottoNumbers.forEach(lottoNumbers => {
-    const correctCount = lottoNumbers.filter(num =>
-      lastLottoInfo.lastLottoNumbers.includes(num),
-    ).length;
-    if (
-      correctCount === 5 &&
-      lottoNumbers.includes(lastLottoInfo.bounsNumber)
-    ) {
-      totalPrizeMoney = totalPrizeMoney + MATCH_NUMBER_BONUS_PRIZE;
-    }
-    totalPrizeMoney = totalPrizeMoney + getLottoPrize(correctCount);
-  });
-  return (totalPrizeMoney / (lottoInfo.lottoCount * 1000)) * 100;
-}
 
 describe('로또와 관련된 테스트를 진행하는 곳', () => {
   test('입력 금액에 맞게 로또를 생성되는지 확인한다', () => {
@@ -70,21 +29,17 @@ describe('로또와 관련된 테스트를 진행하는 곳', () => {
     });
   });
 
-  test('로또 수익률이 올바르게 계산이 되는지 확인한다.', () => {
-    const lastLottoInfo = {
-      lastLottoNumbers: [1, 2, 3, 4, 5, 6],
-      bounsNumber: 7,
-    };
-    const lottoInfo = {
-      lottoCount: 5,
-      currentLottoNumbers: [
-        [1, 2, 3, 7, 8, 9],
-        [1, 2, 3, 7, 8, 9],
-        [1, 2, 3, 7, 8, 9],
-        [1, 2, 3, 7, 8, 9],
-        [1, 2, 3, 7, 8, 9],
-      ],
-    };
-    expect(computeRateOfReturn(lottoInfo, lastLottoInfo)).toBe(500);
+  test('로또 수익율이 올바르게 계산이 되는지 확인한다.', () => {
+    const lastLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+    const currentLottoNumbers = [
+      new Lotto([1, 2, 3, 8, 9, 10]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+    ];
+    expect(
+      computeLottoRateOfReturn(currentLottoNumbers, lastLottoNumbers),
+    ).toBe(500);
   });
 });
