74081f409a7871532cdffc29d63fd1caef64e746
sanaandmomo <qajjang46@naver.com>
2022-03-02 20:53:44 +0900

    refactor: 변수명 변경

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -3,11 +3,11 @@ import LottoCountCalcultor from '../CalculatorImpl/LottoCountCalculator.js';
 
 describe('입력한 요금만큼 로또를 생성할 수 있다.', () => {
   test('5000원을 입력하면 5개의 로또가 생성돼야 한다.', () => {
-    const lottoManager = new LottoCollectionImpl();
+    const lottoCollection = new LottoCollectionImpl();
     const fare = 5000;
 
-    lottoManager.createLottos(new LottoCountCalcultor(fare).execute());
+    lottoCollection.createLottos(new LottoCountCalcultor(fare).execute());
 
-    expect(lottoManager.getLottos().length).toBe(5);
+    expect(lottoCollection.getLottos().length).toBe(5);
   });
 });
