955bf643cb85ef56387a53663715531d04869075
wonsss <bigdatajang@gmail.com>
2022-03-03 02:39:21 +0900

    :sparkles: feat: add shape and color style to purchased lottery numbers

--- src/js/layouts/template.js
+++ src/js/layouts/template.js
@@ -1,7 +1,22 @@
+const getRange = (number) => {
+  if (number < 11) return 'yellow';
+  if (number < 21) return 'blue';
+  if (number < 31) return 'red';
+  if (number < 41) return 'black';
+  return 'green';
+};
+
 const ticketTemplate = (numbers) => {
   return `<div class="ticket-icon-div">
             <span class="ticket-icon">🎟️</span>
-            <span class="ticket-numbers hidden">${numbers.join(', ')}</span>
+            <div class="ticket-numbers hidden">
+              ${numbers
+                .map((number) => {
+                  const range = getRange(number);
+                  return `<span data-number=${range} class="ball">${number}</span>`;
+                })
+                .join('')}
+            </div>
           </div>`;
 };
 
