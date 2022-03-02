9bdeaa1920428253c520d4f187ec4c0510e28d42
kamwoo <rkadndud06@naver.com>
2022-03-03 01:18:34 +0900

    feat: 로또 리스트의 각 로또와 당첨 번호의 일치 개수를 담은 리스트를 반환

--- src/js/controllers/subController/WinningNumberController.js
+++ src/js/controllers/subController/WinningNumberController.js
@@ -2,6 +2,17 @@ import WinningNumberView from '../../views/subViews/WinningNumberView.js';
 import { SELECTOR } from '../../configs/contants.js';
 
 export default class WinningNumberController {
+  static createCoincideCountList(winningNumbers, lottoNumbersList) {
+    const countList = lottoNumbersList.map((lottoNumbers) => {
+      return WinningNumberController.calculateCoincideCount(
+        winningNumbers,
+        lottoNumbers
+      );
+    });
+
+    return countList;
+  }
+
   static calculateCoincideCount(winningNumbers, lottoNumbers) {
     const coincideList = winningNumbers.filter(
       (winningNumber, index) => winningNumber === lottoNumbers[index]
