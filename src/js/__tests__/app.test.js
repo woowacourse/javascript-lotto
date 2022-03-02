d366f09872e30a93e50a99ac126938cfaea42d20
jhy979 <jhy979@gmail.com>
2022-03-02 15:34:40 +0900

    test: 테스트별로 묶기

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -51,61 +51,65 @@ describe('구입한 금액에 맞게 로또가 구매되는지 확인', () => {
   });
 });
 
-test('당첨 번호(6개)와 보너스 번호(1개)를 입력받는다.', () => {
-  const lottoGame = new LottoGame();
-  lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE);
-  lottoGame.buyLotto();
-  lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
-  expect(lottoGame.winningNumbers.size).toBe(6);
-  expect(lottoGame.bonusNumber).toBe(7);
+describe('당첨 번호를 입력 받을 수 있다.', () => {
+  test('당첨 번호(6개)와 보너스 번호(1개)를 입력받는다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE);
+    lottoGame.buyLotto();
+    lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
+    expect(lottoGame.winningNumbers.size).toBe(6);
+    expect(lottoGame.bonusNumber).toBe(7);
+  });
+
+  test('당첨 번호와 보너스 번호가 중복되면 에러를 throw한다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE);
+    lottoGame.buyLotto();
+    expect(() => {
+      validator.isWinningInputValid({ win1: 1, win2: 1, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
+    }).toThrowError(ERROR_MESSAGE.WINNGINGS_NO_OVERLAPPED);
+  });
 });
 
-test('당첨 번호와 보너스 번호가 중복되면 에러를 throw한다.', () => {
-  const lottoGame = new LottoGame();
-  lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE);
-  lottoGame.buyLotto();
-  expect(() => {
-    validator.isWinningInputValid({ win1: 1, win2: 1, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
-  }).toThrowError(ERROR_MESSAGE.WINNGINGS_NO_OVERLAPPED);
-});
+describe('당첨 통계를 낼 수 있다.', () => {
+  test('당첨 통계를 위한 배열을 낼 수 있다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
+    lottoGame.buyLotto();
+    lottoGame.lottoWallet[0].numbers = [1, 2, 3, 4, 5, 6];
+    lottoGame.lottoWallet[1].numbers = [1, 2, 3, 14, 15, 16];
+    lottoGame.lottoWallet[2].numbers = [1, 2, 3, 4, 5, 7];
+    lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
+    lottoGame.compareLottos();
+    expect(lottoGame.winningStatus).toEqual([1, 0, 0, 1, 1]);
+  });
 
-test('당첨 통계를 위한 배열을 낼 수 있다.', () => {
-  const lottoGame = new LottoGame();
-  lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
-  lottoGame.buyLotto();
-  lottoGame.lottoWallet[0].numbers = [1, 2, 3, 4, 5, 6];
-  lottoGame.lottoWallet[1].numbers = [1, 2, 3, 14, 15, 16];
-  lottoGame.lottoWallet[2].numbers = [1, 2, 3, 4, 5, 7];
-  lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
-  lottoGame.compareLottos();
-  expect(lottoGame.winningStatus).toEqual([1, 0, 0, 1, 1]);
-});
+  test('수익률을 계산할 수 있다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
+    lottoGame.buyLotto();
+    lottoGame.lottoWallet[0].numbers = [1, 2, 3, 4, 5, 6];
+    lottoGame.lottoWallet[1].numbers = [1, 2, 3, 14, 15, 16];
+    lottoGame.lottoWallet[2].numbers = [1, 2, 3, 4, 5, 7];
+    lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
+    lottoGame.compareLottos();
+    lottoGame.calculateYield();
+    expect(lottoGame.yield).toBe(666685);
+  });
 
-test('수익률을 계산할 수 있다.', () => {
-  const lottoGame = new LottoGame();
-  lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
-  lottoGame.buyLotto();
-  lottoGame.lottoWallet[0].numbers = [1, 2, 3, 4, 5, 6];
-  lottoGame.lottoWallet[1].numbers = [1, 2, 3, 14, 15, 16];
-  lottoGame.lottoWallet[2].numbers = [1, 2, 3, 4, 5, 7];
-  lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
-  lottoGame.compareLottos();
-  lottoGame.calculateYield();
-  expect(lottoGame.yield).toBe(666685);
-});
-
-test('다시 시작을 위해 초기화가 가능하다.', () => {
-  const lottoGame = new LottoGame();
-  lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
-  lottoGame.buyLotto();
-  lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
-  lottoGame.compareLottos();
-  lottoGame.calculateYield();
-  lottoGame.reStartLottos();
-  expect(lottoGame.moneyInput).toBe(0);
-  expect(lottoGame.lottoWallet.length).toBe(0);
-  expect(lottoGame.winningNumbers.size).toBe(0);
-  expect(lottoGame.bonusNumber).toBe(0);
-  expect(lottoGame.winningStatus.every((winStatus) => winStatus === 0)).toBe(true);
-  expect(lottoGame.yield).toBe(0);
+  test('다시 시작을 위해 초기화가 가능하다.', () => {
+    const lottoGame = new LottoGame();
+    lottoGame.insertMoney(CONDITIONS.LOTTO_PRICE * 3);
+    lottoGame.buyLotto();
+    lottoGame.getWinningNumbers({ win1: 1, win2: 2, win3: 3, win4: 4, win5: 5, win6: 6 }, 7);
+    lottoGame.compareLottos();
+    lottoGame.calculateYield();
+    lottoGame.reStartLottos();
+    expect(lottoGame.moneyInput).toBe(0);
+    expect(lottoGame.lottoWallet.length).toBe(0);
+    expect(lottoGame.winningNumbers.size).toBe(0);
+    expect(lottoGame.bonusNumber).toBe(0);
+    expect(lottoGame.winningStatus.every((winStatus) => winStatus === 0)).toBe(true);
+    expect(lottoGame.yield).toBe(0);
+  });
 });
