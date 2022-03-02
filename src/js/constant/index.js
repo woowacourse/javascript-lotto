47926c36899d49be6a04cded64cf7a4a880454c1
sanaandmomo <qajjang46@naver.com>
2022-03-02 16:58:00 +0900

    refactor: freeze로 일관성 맞추기

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -1,11 +1,11 @@
-export const ERROR_MESSAGE = Object.freeze({
-  LACK_OF_FARE: '요금은 1000원 이상 투입해주세요!',
-});
-
 export const LOTTO_PRICE = 1000;
 
-export const LOTTO_RULES = {
+export const LOTTO_RULES = Object.freeze({
   MIN_RANGE: 1,
   MAX_RANGE: 45,
   BALL_COUNT: 6,
-};
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  LACK_OF_FARE: '요금은 1000원 이상 투입해주세요!',
+});
