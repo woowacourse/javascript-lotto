c31981705cf95219c6cc2832cf519f83e09cb913
sanaandmomo <qajjang46@naver.com>
2022-03-02 20:44:34 +0900

    feat: 로또 당첨 번호 유효성 검사 구현

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -29,6 +29,8 @@ describe('로또 당첨 번호가 유효해야 한다.', () => {
   const { isNotNumber, overlappedNumber, outedOfLottoNumberRange } = new ValidatorImpl()
     .checkFunctions;
 
+  const isEmpty = (value) => value === '';
+
   test('로또 당첨 번호가 값이 비어있으면 결과를 확인할 수 없다.', () => {
     const winningNumber = [45, '1', '', 3, 4, 5];
 
