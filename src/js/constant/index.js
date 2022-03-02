1e1e97f404c46dd1f48723e6c1f2e33fc81d8acc
sanaandmomo <qajjang46@naver.com>
2022-03-02 21:22:33 +0900

    fix: 상수 메세지 수정

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -9,4 +9,8 @@ export const LOTTO_RULES = Object.freeze({
 
 export const ERROR_MESSAGE = Object.freeze({
   LACK_OF_FARE: `요금은 ${LOTTO_PRICE}원 이상 투입해주세요!`,
+  WINNING_NUMBER_IS_NOT_NUMBER: '당첨 번호는 숫자만 입력해주세요!',
+  OUT_OF_RANGE_WINNING_NUMBER: `당첨 번호는 ${LOTTO_RULES.MIN_RANGE}이상 ${LOTTO_RULES.MAX_RANGE}이하로 입력해주세요!`,
+  OVERLAPPED_WINNING_NUMBER: '당첨 번호는 중복된 숫자 없이 입력해주세요!',
+  EMPTY_OF_LOTTO: '로또를 먼저 구매해주세요!',
 });
