d27910918df6df637ff981fff1ad29c7c1df59f9
jhy979 <jhy979@gmail.com>
2022-03-02 14:52:33 +0900

    refactor: View를 공통View와 모달View로 분리

--- src/js/controllers/Controller.js
+++ src/js/controllers/Controller.js
@@ -1,12 +1,15 @@
-import { View } from '../view/View.js';
+import { CommonView } from '../view/CommonView.js';
 import { LottoGame } from '../model/LottoGame.js';
 import { validator } from '../utils.js';
 import { CONDITIONS } from '../constants/constants.js';
+import { ModalView } from '../view/ModalView.js';
 
 export class Controller {
   constructor() {
-    this.view = new View();
+    this.commonView = new CommonView();
+    this.modalView = new ModalView();
     this.lottoGame = new LottoGame();
+
     this.bindPurchaseEvent();
     this.bindToggleEvent();
     this.bindShowResultEvent();
@@ -15,77 +18,78 @@ export class Controller {
   }
 
   bindPurchaseEvent() {
-    this.view.purchaseBtn.addEventListener('click', this.#purchaseLotto.bind(this));
+    this.commonView.purchaseBtn.addEventListener('click', this.#purchaseLotto.bind(this));
   }
 
   bindToggleEvent() {
-    this.view.toggleBtn.addEventListener('click', this.#controllToggleBtn.bind(this));
+    this.commonView.toggleBtn.addEventListener('click', this.#controllToggleBtn.bind(this));
   }
 
   bindShowResultEvent() {
-    this.view.showResultBtn.addEventListener('click', this.#showLottoResult.bind(this));
+    this.commonView.showResultBtn.addEventListener('click', this.#showLottoResult.bind(this));
   }
 
   bindCloseModalEvent() {
-    this.view.closeModalBtn.addEventListener('click', this.#closeModal.bind(this));
+    this.modalView.closeModalBtn.addEventListener('click', this.#closeModal.bind(this));
   }
 
   bindRestartEvent() {
-    this.view.restartBtn.addEventListener('click', this.#restartLotto.bind(this));
+    this.modalView.restartBtn.addEventListener('click', this.#restartLotto.bind(this));
   }
 
   #purchaseLotto(e) {
     try {
       e.preventDefault();
-      validator.isInputValid(Number(this.view.moneyInput.value));
-      this.lottoGame.insertMoney(Number(this.view.moneyInput.value));
+      validator.isInputValid(Number(this.commonView.moneyInput.value));
+      this.lottoGame.insertMoney(Number(this.commonView.moneyInput.value));
       this.lottoGame.buyLotto();
 
-      this.view.uncheckToggleSwitch();
-      this.view.lottosQuantityTemplate(this.lottoGame.lottoWallet);
-      this.view.lottosInfoTemplate(this.lottoGame.lottoWallet);
-      this.view.showLottoStatusContainer();
-      this.view.showWinningLottoContainer();
-      this.view.showPurchasedLottos(this.lottoGame.lottoWallet);
+      this.commonView.uncheckToggleSwitch();
+      this.commonView.lottosQuantityTemplate(this.lottoGame.lottoWallet);
+      this.commonView.lottosInfoTemplate(this.lottoGame.lottoWallet);
+      this.commonView.showLottoStatusContainer();
+      this.commonView.showWinningLottoContainer();
+      this.commonView.showPurchasedLottos(this.lottoGame.lottoWallet);
     } catch (err) {
       alert(err.message);
     }
-    this.view.clearMoneyInput(this.lottoGame.moneyInput % CONDITIONS.LOTTO_PRICE);
+    this.commonView.clearMoneyInput(this.lottoGame.moneyInput % CONDITIONS.LOTTO_PRICE);
   }
 
   #controllToggleBtn() {
-    if (this.view.toggleBtn.checked) {
-      this.view.toggleOn();
+    if (this.commonView.toggleBtn.checked) {
+      this.commonView.toggleOn();
       return;
     }
-    this.view.toggleOff();
+    this.commonView.toggleOff();
   }
 
   #showLottoResult(e) {
     e.preventDefault();
     try {
       validator.isWinningInputValid(
-        this.view.getWinningNumbersInput(),
-        Number(this.view.bonusNumber.value)
+        this.modalView.getWinningNumbersInput(),
+        Number(this.modalView.bonusNumber.value)
       );
       this.lottoGame.getWinningNumbers(
-        this.view.getWinningNumbersInput(),
-        Number(this.view.bonusNumber.value)
+        this.modalView.getWinningNumbersInput(),
+        Number(this.modalView.bonusNumber.value)
       );
       this.lottoGame.compareLottos();
       this.lottoGame.calculateYield();
-      this.view.showResultModal(this.lottoGame.winningStatus, this.lottoGame.yield);
+      this.modalView.showResultModal(this.lottoGame.winningStatus, this.lottoGame.yield);
     } catch (err) {
       alert(err.message);
     }
   }
 
   #closeModal() {
-    this.view.closeModal();
+    this.modalView.closeModal();
   }
 
   #restartLotto() {
     this.lottoGame.reStartLottos();
-    this.view.initView();
+    this.commonView.initView();
+    this.modalView.initModalView();
   }
 }
