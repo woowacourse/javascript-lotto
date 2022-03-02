79eeb0096e182bee338fd2e1702b3bfa467717f7
wonsss <bigdatajang@gmail.com>
2022-03-02 17:04:53 +0900

    :sparkles: feat: increase usability when entering winning numbers

--- src/js/view/WinningNumbersView.js
+++ src/js/view/WinningNumbersView.js
@@ -1,18 +1,22 @@
 import EVENT from '../constants/event';
 import ID from '../constants/selector';
 import { emit, on } from '../utils/event';
-import { $ } from '../utils/selector';
+import { $, $$ } from '../utils/selector';
 
 export default class WinningNumbersView {
   constructor(lottoResult) {
     this.lottoResult = lottoResult;
     this.$winningNumbersForm = $(ID.WINNING_NUMBERS_FORM);
-    this.$winningNumbersInput = $(ID.WINNING_NUMBERS_RESET);
+    this.$winningNumbersReset = $(ID.WINNING_NUMBERS_RESET);
+    this.$$winningNumberInputs = $$('.basic-input');
+    this.$resultButton = $(ID.RESULT_BUTTON);
     this.#bindEvents();
   }
 
   #bindEvents() {
     on(this.$winningNumbersForm, 'submit', (e) => this.#handleSubmit(e));
+
+    this.#enableAutoFocus();
   }
 
   #handleSubmit(e) {
@@ -27,6 +31,29 @@ export default class WinningNumbersView {
     });
   }
 
+  #enableAutoFocus() {
+    this.$$winningNumberInputs.forEach(($input, index) => {
+      $input.addEventListener('keyup', (e) => {
+        if (e.target.valueAsNumber <= 45) {
+          this.#handleAutoFocus(e, index);
+          return;
+        }
+        e.target.value = e.target.value.substr(0, 2);
+        this.$resultButton.click();
+      });
+    });
+  }
+
+  #handleAutoFocus(e, index) {
+    if (index === 6 && e.target.value.length >= 2) {
+      this.$resultButton.focus();
+      return;
+    }
+    if (e.target.value.length >= 2) {
+      this.$$winningNumberInputs[index + 1].focus();
+    }
+  }
+
   removeInputValue() {
     console.log(this.$winningNumbersForm.event);
     [0, 1, 2, 3, 4, 5, 6].forEach((i) => {
@@ -35,6 +62,6 @@ export default class WinningNumbersView {
   }
 
   resetInput() {
-    this.$winningNumbersInput.click();
+    this.$winningNumbersReset.click();
   }
 }
