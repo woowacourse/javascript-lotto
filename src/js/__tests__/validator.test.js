bfb7785b8a6c40915b48ec82241ea7767651a4a2
sanaandmomo <qajjang46@naver.com>
2022-03-02 19:12:23 +0900

    test: 로또 당첨 번호에 대한 테스트 케이스 작성

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,4 +1,5 @@
 import ValidatorImpl from '../ValidatorImpl/index.js';
+import { LOTTO_RULES } from '../constant/index.js';
 
 describe('요금을 1000원 이상 투입해야 한다.', () => {
   const { isLackFare } = new ValidatorImpl().checkFunctions;
@@ -21,5 +22,43 @@ describe('요금을 1000원 이상 투입해야 한다.', () => {
 
     expect(isLackFare(fare1)).toBe(false);
     expect(isLackFare(fare2)).toBe(false);
+  });
+});
+
+describe('로또 당첨 번호가 유효해야 한다.', () => {
+  test('로또 당첨 번호가 숫자가 아니면 결과를 확인할 수 없다.', () => {
+    const winningNumber = ['ab', '1', '2', 3, 4, 5];
+
+    expect(isNotNumber(winningNumber)).toBe(true);
+  });
+
+  test('로또 당첨 번호가 숫자면 결과를 확인할 수 있다.', () => {
+    const winningNumber = ['1', '2', 3, 4, 44, 45];
+
+    expect(isNotNumber(winningNumber)).toBe(false);
+  });
+
+  test('로또 당첨 번호에 중복된 숫자가 있으면 결과를 확인할 수 없다.', () => {
+    const winningNumber = [1, 2, 1, 2, 3, 4];
+
+    expect(overlappedNumber(winningNumber)).toBe(true);
+  });
+
+  test(`로또 당첨 번호가 ${LOTTO_RULES.MIN_RANGE}이상이고 ${LOTTO_RULES.MAX_RANGE}이하라면 결과를 확인할 수 있다.`, () => {
+    const winningNumber = [1, 2, 45, 43, 32, 21];
+
+    expect(outedOfLottoNumberRange(winningNumber)).toBe(false);
+  });
+
+  test(`로또 당첨 번호가 ${LOTTO_RULES.MIN_RANGE}미만이면 결과를 확인할 수 없다.`, () => {
+    const winningNumber = [-1, 0, -2, -3, -4, -5];
+
+    expect(outedOfLottoNumberRange(winningNumber)).toBe(true);
+  });
+
+  test(`로또 당첨 번호가 ${LOTTO_RULES.MAX_RANGE}초과라면 결과를 확인할 수 없다.`, () => {
+    const winningNumber = [46, 47, 48, 49, 50, 51];
+
+    expect(outedOfLottoNumberRange(winningNumber)).toBe(true);
   });
 });
