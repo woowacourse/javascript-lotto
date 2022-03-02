f06d48e0735501ecb153bed8cddca9e0d9da191e
kamwoo <rkadndud06@naver.com>
2022-03-03 00:54:47 +0900

    feat: 당첨 번호와 로또 한 개를 비교하여 일치하는 개수를 구하는 기능

--- src/js/controllers/subController/WinningNumberController.js
+++ src/js/controllers/subController/WinningNumberController.js
@@ -2,6 +2,14 @@ import WinningNumberView from '../../views/subViews/WinningNumberView.js';
 import { SELECTOR } from '../../configs/contants.js';
 
 export default class WinningNumberController {
+  static calculateCoincideCount(winningNumbers, lottoNumbers) {
+    const coincideList = winningNumbers.filter(
+      (winningNumber, index) => winningNumber === lottoNumbers[index]
+    );
+
+    return coincideList.length;
+  }
+
   constructor() {
     this.winningNumberView = new WinningNumberView(
       SELECTOR.WINNING_NUMBER_SECTION
