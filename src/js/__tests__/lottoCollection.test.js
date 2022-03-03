88e8765aada3cb6a2c6497fa6c6a9de4a3f15d54
sanaandmomo <qajjang46@naver.com>
2022-03-03 18:41:26 +0900

    feat: 수익률 계산 구현

--- src/js/__tests__/lottoCollection.test.js
+++ src/js/__tests__/lottoCollection.test.js
@@ -1,4 +1,6 @@
 describe('수익률을 계산할 수 있어야한다.', () => {
+  const calculateRateOfReturn = (buyCount, profits) => (profits / (buyCount * 1000)) * 100;
+
   test('로또를 1개 사고, 수익금이 5,000이면, 500%여야 한다.', () => {
     const buyCount = 1;
     const profits = 5000;
