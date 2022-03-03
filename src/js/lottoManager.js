b35c3c92eb60fa24e92887dabf41caa9c33a4bf0
woose28 <dldntmdsla@naver.com>
2022-03-03 17:01:37 +0900

    refactor: lottoManager 객체 생성 방법 수정
    - 객체 리터럴 방식에서 class 방식으로 수정

--- src/js/lottoManager.js
+++ src/js/lottoManager.js
@@ -1,23 +1,32 @@
 import { LOTTO_RULES, LOTTO_PRICE } from './constant/index.js';
 import { createRandomNumbers } from './utils/index.js';
 
-const lottoManager = {
-  lottos: [],
+class LottoManager {
+  #lottos = [];
+
   createLottos(lottoCount) {
     for (let i = 0; i < lottoCount; i += 1) {
-      this.lottos.push(
+      this.#lottos.push(
         createRandomNumbers(LOTTO_RULES.MIN_RANGE, LOTTO_RULES.MAX_RANGE, LOTTO_RULES.BALL_COUNT),
       );
     }
 
-    return this.lottos;
-  },
+    return this.#lottos;
+  }
+
+  getLottos() {
+    return this.#lottos;
+  }
+
   calculateLottoCount(fare) {
     return Math.floor(fare / LOTTO_PRICE);
-  },
+  }
+
   calculateRemainFare(fare) {
     return fare % LOTTO_PRICE;
-  },
-};
+  }
+}
+
+const lottoManager = new LottoManager();
 
 export default lottoManager;
