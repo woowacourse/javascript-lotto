e86a97c132e8b6495daa4354b5ac94a01838a880
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-03 19:38:05 +0900

    feat: 검증 유틸리티 숫자 검증 기능 추가

--- src/js/utils/validator.js
+++ src/js/utils/validator.js
@@ -1,3 +1,5 @@
-export const isPositiveInteger = (value) => /^[0-9]*$/g.test(value) && value > 0;
+export const isNumber = (value) => /^[0-9]*$/g.test(value);
+
+export const isPositiveInteger = (value) => isNumber(value) && value > 0;
 
 export const isDivisible = (value, number) => value % number === 0;
