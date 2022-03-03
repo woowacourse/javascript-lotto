f94c7aa9eced44c53242c0ec0635c8ee91028001
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-04 02:56:53 +0900

    feat: 구매 로또 번호와 당첨 번호 비교 기능 추가

--- src/js/models/LottosModel.js
+++ src/js/models/LottosModel.js
@@ -1,8 +1,12 @@
-import Lotto from './Lotto';
 import { LOTTO_SETTING } from '../constants/setting';
+import { getListDuplicateCount } from '../utils/data-manager';
+
+import Lotto from './Lotto';
 
 export default class LottosModel {
   #lottos = [];
+  #winningNumberList = [];
+  #bonusNumber = 0;
 
   buy(inputMoney) {
     const lottoCount = inputMoney / LOTTO_SETTING.PRICE;
@@ -16,5 +20,45 @@ export default class LottosModel {
 
   get list() {
     return this.#lottos.map((value) => value.pickedNumber.join(', '));
+  }
+
+  get winningNumberList() {
+    return this.#winningNumberList;
+  }
+
+  set winningNumberList(numberList) {
+    const arrayToNumberList = numberList.map((value) => Number(value));
+    const { LOTTO_NUMBER_LENGTH, BONUS_NUMBER_LENGTH } = LOTTO_SETTING;
+
+    this.#winningNumberList = arrayToNumberList.slice(0, LOTTO_NUMBER_LENGTH);
+    this.#bonusNumber = arrayToNumberList
+      .slice(LOTTO_NUMBER_LENGTH, LOTTO_NUMBER_LENGTH + BONUS_NUMBER_LENGTH)
+      .shift();
+  }
+
+  #isBonusWinner(lottoNumberList, matchCount) {
+    const bonusWinnerRange = LOTTO_SETTING.LOTTO_NUMBER_LENGTH - LOTTO_SETTING.BONUS_NUMBER_LENGTH;
+    return matchCount === bonusWinnerRange && lottoNumberList.includes(this.#bonusNumber);
+  }
+
+  getRankNumber(lottoNumberList) {
+    const { RACKING_START_NUMBER, WINNING_MIN_MATCH_NUMBER } = LOTTO_SETTING;
+
+    const matchCount = getListDuplicateCount(lottoNumberList, this.#winningNumberList);
+    const rankNumber = RACKING_START_NUMBER + WINNING_MIN_MATCH_NUMBER - matchCount;
+
+    if (this.#isBonusWinner(lottoNumberList, matchCount)) {
+      return 2;
+    }
+
+    if (LOTTO_SETTING.LOTTO_NUMBER_LENGTH === matchCount) {
+      return 1;
+    }
+
+    return rankNumber;
+  }
+
+  get result() {
+    return '';
   }
 }
--- src/js/utils/data-manager.js
+++ src/js/utils/data-manager.js
@@ -1,2 +1,5 @@
 export const getRandomNumber = (minNumber, maxNumber) =>
   Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+
+export const getListDuplicateCount = (currentList, compareList) =>
+  currentList.filter((value) => compareList.includes(value)).length;
