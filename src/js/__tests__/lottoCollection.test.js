f68bd26787abb4e85c54c4645e3fdb28c9a7159a
sanaandmomo <qajjang46@naver.com>
2022-03-03 18:40:53 +0900

    test: 수익률 계산 테스트

---
+++ src/js/__tests__/lottoCollection.test.js
@@ -0,0 +1,15 @@
+describe('수익률을 계산할 수 있어야한다.', () => {
+  test('로또를 1개 사고, 수익금이 5,000이면, 500%여야 한다.', () => {
+    const buyCount = 1;
+    const profits = 5000;
+
+    expect(calculateRateOfReturn(buyCount, profits)).toBe(500);
+  });
+
+  test('로또를 5개 사고, 수익금이 2,000,000,000라면 40000000%여야 한다.', () => {
+    const buyCount = 5;
+    const profits = 2000000000;
+
+    expect(calculateRateOfReturn(buyCount, profits)).toBe(40000000);
+  });
+});
