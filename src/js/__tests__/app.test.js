8621061062b4ce48023b261ee4e4aa3fcd05e463
woose28 <dldntmdsla@naver.com>
2022-03-03 15:25:21 +0900

    test: 당첨된 로또 개수를 계산한 후 검증하는 테스트 코드 작성

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -87,18 +87,18 @@ describe(`중복되지 않는 1 ~ 45 사이의 숫자를 6개 생성한다.`, ()
 });
 
 describe('지난 주 당첨 번호는 중복되지 않는 1 ~ 45 사이의 6개의 숫자여야 한다.', () => {
-  const previousWinningNumber = [7, 15, 30, 37, 39, 44];
+  const winningNumber = [7, 15, 30, 37, 39, 44];
 
   test('지난주 당첨 번호 숫자들은 1 ~ 45 사이의 숫자여야 한다.', () => {
-    expect(isValidRangeNumbers(previousWinningNumber)).toBeTruthy();
+    expect(isValidRangeNumbers(winningNumber)).toBeTruthy();
   });
 
   test('지난주 당첨 번호에는 중복된 숫자가 있으면 안된다.', () => {
-    expect(isNotOverlapped(previousWinningNumber)).toBeTruthy();
+    expect(isNotOverlapped(winningNumber)).toBeTruthy();
   });
 
   test('지난주 당첨 번호의 숫자 개수는 6개여야 한다.', () => {
-    expect(isValidCount(previousWinningNumber)).toBeTruthy();
+    expect(isValidCount(winningNumber)).toBeTruthy();
   });
 });
 
@@ -106,12 +106,26 @@ describe('보너스 당첨 번호는 지난주 당첨 번호에 속해있지 않
   const bonumsNumber = 18;
 
   test('보너스 당첨 번호는 지난주 당첨 번호에 속해있으면 안된다.', () => {
-    const previousWinningNumber = [7, 15, 30, 37, 39, 44];
+    const winningNumber = [7, 15, 30, 37, 39, 44];
 
-    expect(isNotIncludeWinningNumbers(previousWinningNumber, bonumsNumber)).toBeTruthy();
+    expect(isNotIncludeWinningNumbers(winningNumber, bonumsNumber)).toBeTruthy();
   });
 
   test('보너스 당첨 번호는 1 ~ 45 사이의 숫자여야 한다.', () => {
     expect(isValidRangeNumber(bonumsNumber)).toBeTruthy();
+  });
+});
+
+describe('구매한 로또 번호와 지난주 당첨 번호, 보너스 번호를 이용해서 당첨 결과를 확인할 수 있어야 한다.', () => {
+  const lottos = [
+    [7, 15, 30, 37, 39, 44],
+    [7, 15, 30, 37, 39, 18],
+    [7, 15, 30, 37, 39, 45],
+    [7, 15, 30, 37, 40, 45],
+  ];
+  const winningNumbers = [7, 15, 30, 37, 39, 44];
+  const bonusNumber = 18;
+  test('구매한 로또 중 당첨된 로또를 개수를 등수 별로 계산할 수 있어야 한다.', () => {
+    expect(calculateWinningCounts(lottos, winningNumbers, bonusNumber)).tobe([0, 1, 1, 1, 1]);
   });
 });
