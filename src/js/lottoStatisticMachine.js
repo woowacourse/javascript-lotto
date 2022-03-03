78f71ff93f99ebc86c4450327e37ab64b76526ed
woose28 <dldntmdsla@naver.com>
2022-03-03 16:40:40 +0900

    refactor: 당첨된 로또 개수를 계산하는 함수 위치 이동

---
+++ src/js/lottoStatisticMachine.js
@@ -0,0 +1,68 @@
+import { LOTTO_RULES } from './constant/index.js';
+
+class LottoStatisticMachine {
+  #winningNumbers = new Array(LOTTO_RULES.BALL_COUNT).fill(null);
+
+  #bonusNumber = null;
+
+  #winningCounts = new Array(5).fill(null);
+
+  calculateWinningCounts(lottos, winningNumbers, bonumsNumber) {
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonumsNumber;
+
+    const winningCounts = new Array(5).fill(0);
+
+    lottos.forEach((lotto) => {
+      const hitCount = this.#calculateHitCount(lotto);
+      const isHitBonusNumber = this.#checkHitBonusNumber(lotto);
+
+      if (hitCount >= 3) {
+        const rank = this.#convertHitCountToRank(hitCount, isHitBonusNumber);
+
+        winningCounts[rank - 1] += 1;
+      }
+    });
+
+    this.#winningCounts = winningCounts.reverse();
+    return this.#winningCounts;
+  }
+
+  #calculateHitCount(lotto) {
+    return lotto.reduce((hitCount, lottoNumber) => {
+      if (this.#winningNumbers.includes(lottoNumber)) {
+        return hitCount + 1;
+      }
+
+      return hitCount;
+    }, 0);
+  }
+
+  #checkHitBonusNumber(lotto) {
+    return lotto.includes(this.#bonusNumber);
+  }
+
+  #convertHitCountToRank(hitCount, isHitBonusNumber) {
+    if (hitCount === 3) {
+      return 5;
+    }
+
+    if (hitCount === 4) {
+      return 4;
+    }
+
+    if (hitCount === 5) {
+      if (isHitBonusNumber) {
+        return 2;
+      }
+
+      return 3;
+    }
+
+    return 1;
+  }
+}
+
+const lottoStatisticMachine = new LottoStatisticMachine();
+
+export default lottoStatisticMachine;
