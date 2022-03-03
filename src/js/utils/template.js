daa46c96803ebf480d9e0dc3a3c1dd9c318b7ef5
sangminsong <ssmin940606@gmail.com>
2022-03-04 02:33:30 +0900

    design: 스타일링
    - 부분적으로 tailwind css 방식을 도입해서 진행 - 따로 id를 만들 필요가 없어서 간단한 css를 첨부하는데 편리하게 쓸 수 있을 것 같음

--- src/js/utils/template.js
+++ src/js/utils/template.js
@@ -8,7 +8,7 @@ export const lottoPurchaseCountTemplate = (count) => {
 
 export const lottoTemplate = (numbers) => {
   return `
-    <li class="lotto-wrap">
+    <li class="flex">
       <img src=${lotto} />
       <p class="lotto-numbers hidden">${numbers.join(', ')}</p>
     </li>
@@ -25,17 +25,23 @@ export const lottoWinningNumberInputTemplate = () => {
   return `
     <section id="lotto-winning-number-input-section">
       <form id="lotto-match-result-form">
-        <p>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요</p>
-        <div>
-          <label for="lotto-winning-number">당첨 번호</label>
-          <input id="lotto-winning-number" class="lotto-winning-number-container" maxlength="2" />
-          ${'<input class="lotto-winning-number-container" maxlength="2" />'.repeat(5)}
+        <p class="mt-28">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요</p>
+        <div class="flex justify-content-space-between mt-8">
+          <div>
+            <label for="lotto-winning-number">당첨 번호</label>
+            <div class="mt-8 flex column-gap-8">
+              <input id="lotto-winning-number" class="lotto-winning-number-container" maxlength="2" />
+              ${'<input class="lotto-winning-number-container" maxlength="2" />'.repeat(5)}
+            </div>
+          </div>
+          <div>
+            <label for="lotto-winning-bonus-number">보너스 번호</label>
+            <div class="flex justify-content-end mt-8">
+              <input id="lotto-winning-bonus-number" class="lotto-winning-number-container" maxlength="2" />
+            </div>
+          </div>
         </div>
-        <div>
-          <label for="lotto-winning-bonus-number">보너스 번호</label>
-          <input id="lotto-winning-bonus-number" class="lotto-winning-number-container" maxlength="2" />
-        </div>
-        <button id="lotto-match-result-button" type="submit">결과 확인하기</button>
+        <button id="lotto-match-result-button" class="base-button mt-28" type="submit">결과 확인하기</button>
       </form>
     </section>
   `;
