877c711a889181449781d0248961971e57c8719e
junzzi <jjh01027717502@gmail.com>
2022-03-03 20:04:15 +0900

    refactor: 도메인 클래스에서 클래스 내부 변수들은 private화

--- src/js/domains/Lotto.js
+++ src/js/domains/Lotto.js
@@ -3,24 +3,27 @@ import { RANK } from '../constants/rank';
 import { shuffle } from '../utils/gameUtil';
 
 class Lotto {
+  #lottoNumbers = null;
+
   constructor() {
-    this.lottoNumbers = this.#createLottoNumbers();
+    this.#createLottoNumbers();
   }
 
   getLottoNumbers() {
-    return this.lottoNumbers;
+    return this.#lottoNumbers;
   }
 
   #createLottoNumbers() {
     const lottoNumbers = shuffle([...new Array(NUMBER.LOTTO_MAX_NUMBER)].map((_, idx) => idx + 1));
-    return lottoNumbers.slice(0, NUMBER.LOTTO_NUMBER_LENGTH);
+
+    this.#lottoNumbers = lottoNumbers.slice(0, NUMBER.LOTTO_NUMBER_LENGTH);
   }
 
   computeWinResult(winningNumbers, bonusNumber) {
-    const { length: numberMatchCount } = this.lottoNumbers.filter((number) =>
+    const { length: numberMatchCount } = this.#lottoNumbers.filter((number) =>
       winningNumbers.includes(number)
     );
-    const isMatchBonus = this.lottoNumbers.includes(bonusNumber);
+    const isMatchBonus = this.#lottoNumbers.includes(bonusNumber);
     return RANK[`${numberMatchCount}${isMatchBonus}`] ?? RANK.UNRANK;
   }
 }
--- src/js/domains/LottoList.js
+++ src/js/domains/LottoList.js
@@ -5,19 +5,17 @@ import { isValidCharge, isValidWinningNumber } from '../utils/validator';
 import Lotto from './Lotto';
 
 class LottoList {
-  constructor() {
-    this.lottoList = null;
-  }
+  #lottoList = null;
 
   getLottoList() {
-    return this.lottoList;
+    return this.#lottoList;
   }
 
   createLottoList(chargeInput) {
     /** 정상적이지 않은 로또가 하나라도 존재한다면, 멤버는 빈 값이고 사용자는 금액을 다시 입력하여야 한다. */
     const availableLottoAmount = this.exchangeChargeToLottoAmount(chargeInput);
     const lottoList = [...new Array(availableLottoAmount)].map(() => new Lotto());
-    this.lottoList = lottoList;
+    this.#lottoList = lottoList;
   }
 
   exchangeChargeToLottoAmount(charge) {
@@ -39,7 +37,7 @@ class LottoList {
   }
 
   #computeStatistics(winningNumbers, bonusNumber) {
-    const statisticsArray = this.lottoList.map((lotto) =>
+    const statisticsArray = this.#lottoList.map((lotto) =>
       lotto.computeWinResult(winningNumbers, bonusNumber)
     );
 
@@ -49,7 +47,7 @@ class LottoList {
   }
 
   #computeProfitRatio(statistics) {
-    const lottoPurchaseAmount = this.lottoList.length * NUMBER.LOTTO_PRICE;
+    const lottoPurchaseAmount = this.#lottoList.length * NUMBER.LOTTO_PRICE;
 
     const profitAmount = Object.keys(statistics).reduce((prev, currentKey) => {
       const count = statistics[currentKey];
