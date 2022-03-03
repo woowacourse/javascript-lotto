a4a7baaf5ce70f597d09a1c1981d372b43b7d2f0
junzzi <jjh01027717502@gmail.com>
2022-03-03 16:58:27 +0900

    test: util 테스트 삭제

--- src/js/__tests__/util.test.js
+++
@@ -1,9 +0,0 @@
-import { deepCopy } from '../utils/copy';
-
-describe('유틸 함수 테스트', () => {
-  it('deepCopy 함수는 배열을 인자로 받으면 깊은 복사를 수행한 배열을 반환한다.', () => {
-    const array = [1, 2, 3, 4, 5];
-
-    expect(array === deepCopy(array)).toBe(false);
-  });
-});
