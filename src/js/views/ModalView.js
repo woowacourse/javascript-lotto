b16d31c5e25b5706713a814f21dbb299d18c756c
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-03 22:58:42 +0900

    feat: 모달 전용 처리 뷰 추가

---
+++ src/js/views/ModalView.js
@@ -0,0 +1,47 @@
+import { $ } from '../utils/element-manager';
+import { setDelay } from '../utils/event-manager';
+
+export default class ModalView {
+  #modalContainer;
+  #modalContent;
+
+  constructor(modalContentSelector) {
+    this.#modalContainer = $('.modal-container');
+    this.#modalContent = $(modalContentSelector);
+
+    this.#bindEvents();
+  }
+
+  #bindEvents() {
+    this.#modalContainer.addEventListener('click', this.#handleModalClose.bind(this));
+  }
+
+  #handleModalClose({ target: $target }) {
+    if (
+      $target.classList.contains('modal-container') === false &&
+      $target.classList.contains('modal-close') === false
+    ) {
+      return;
+    }
+
+    this.hide();
+  }
+
+  show() {
+    this.#modalContent.classList.add('focus');
+    this.#modalContainer.classList.remove('hide');
+
+    document.documentElement.setAttribute('data-modal', this.#modalContent.id);
+  }
+
+  async hide() {
+    this.#modalContainer.classList.add('disappear');
+    await setDelay(301);
+
+    this.#modalContainer.classList.add('hide');
+    this.#modalContainer.classList.remove('disappear');
+
+    this.#modalContent.classList.remove('focus');
+    document.documentElement.removeAttribute('data-modal');
+  }
+}
