843de11cbb7dfa762b96e599474c7fcd5d7b0cad
sangminsong <ssmin940606@gmail.com>
2022-03-03 15:18:58 +0900

    design: 결과창 UI 수정

--- src/js/views/lottoResultView.js
+++ src/js/views/lottoResultView.js
@@ -1,14 +1,43 @@
 import { $ } from '../utils/helper.js';
 import { lottoResultModalTemplate } from '../utils/template.js';
+import { LOTTO_MATCHING_RESULT_KEY } from '../utils/constants.js';
 
 export default class LottoResultView {
   #app;
 
+  #lottoResultDialog;
+
+  #threeMatchedNumber;
+
+  #fourMatchedNumber;
+
+  #fiveMatchedNumber;
+
+  #fiveWithBonusMatchedNumber;
+
+  #sixMatchedNumber;
+
   constructor() {
     this.#app = $('#app');
+
+    this.#app.insertAdjacentHTML('beforeend', lottoResultModalTemplate());
+
+    this.#lottoResultDialog = $('#lotto-result-dialog');
+    this.#threeMatchedNumber = $('#three-matched-number');
+    this.#fourMatchedNumber = $('#four-matched-number');
+    this.#fiveMatchedNumber = $('#five-matched-number');
+    this.#fiveWithBonusMatchedNumber = $('#five-with-bonus-matched-number');
+    this.#sixMatchedNumber = $('#six-matched-number');
   }
 
-  render() {
-    this.#app.insertAdjacentHTML('afterend', lottoResultModalTemplate());
+  render(lottoMatchingResult) {
+    this.#lottoResultDialog.showModal();
+
+    this.#threeMatchedNumber.textContent = lottoMatchingResult[LOTTO_MATCHING_RESULT_KEY.THREE];
+    this.#fourMatchedNumber.textContent = lottoMatchingResult[LOTTO_MATCHING_RESULT_KEY.FOUR];
+    this.#fiveMatchedNumber.textContent = lottoMatchingResult[LOTTO_MATCHING_RESULT_KEY.FIVE];
+    this.#fiveWithBonusMatchedNumber.textContent =
+      lottoMatchingResult[LOTTO_MATCHING_RESULT_KEY.FIVE_PLUS_BONUS];
+    this.#sixMatchedNumber.textContent = lottoMatchingResult[LOTTO_MATCHING_RESULT_KEY.SIX];
   }
 }
