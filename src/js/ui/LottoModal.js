1c6e2081dd1bdb305ce6a893176b59ac5866314c
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-04 21:59:42 +0900

    reafctor: lottoModal table 템플릿 분리

--- src/js/ui/LottoModal.js
+++ src/js/ui/LottoModal.js
@@ -1,5 +1,6 @@
 import LottoMachine from '../domains/LottoMachine';
 import { validateArrayNumber } from '../validations/utils';
+import { lottoResultTableTemplate } from './template';
 import $ from './utils';
 
 export class LottoModal {
@@ -17,23 +18,7 @@ export class LottoModal {
       $('lotto-result-table').replaceChildren();
       $('lotto-result-table').insertAdjacentHTML(
         'beforeend',
-        `  <div class="grid table-title"><span>일치 갯수</span><span>당첨금</span><span>당첨 갯수</span></div>
-    <div class="grid"><span>3개</span><span>5,000</span><span>${machine.getNumberOfGrade(
-      'fifth'
-    )}개</span></div>
-    <div class="grid"><span>4개</span><span>50,000</span><span>${machine.getNumberOfGrade(
-      'fourth'
-    )}개</span></div>
-    <div class="grid"><span>5개</span><span>1,500,000</span><span>${machine.getNumberOfGrade(
-      'third'
-    )}개</span></div>
-    <div class="grid"><span>5개+보너스볼</span><span>30,000,000</span><span>${machine.getNumberOfGrade(
-      'second'
-    )}개</span></div>
-    <div class="grid"><span>6개</span><span>2,000,000,000</span><span>${machine.getNumberOfGrade(
-      'first'
-    )}개</span></div>
- `
+        lottoResultTableTemplate(machine)
       );
       $(
         'lotto-result-rate'
--- src/js/ui/template.js
+++ src/js/ui/template.js
@@ -10,3 +10,24 @@ export function lottoNumberTemplate(lottoNumber) {
 export function purchaseMessageTemplate({ length }) {
   return `총 ${length}개를 구매하였습니다.`;
 }
+
+export function lottoResultTableTemplate(machine) {
+  console.log(machine);
+  return `  <div class="grid table-title"><span>일치 갯수</span><span>당첨금</span><span>당첨 갯수</span></div>
+    <div class="grid"><span>3개</span><span>5,000</span><span>${machine.getNumberOfGrade(
+      'fifth'
+    )}개</span></div>
+    <div class="grid"><span>4개</span><span>50,000</span><span>${machine.getNumberOfGrade(
+      'fourth'
+    )}개</span></div>
+    <div class="grid"><span>5개</span><span>1,500,000</span><span>${machine.getNumberOfGrade(
+      'third'
+    )}개</span></div>
+    <div class="grid"><span>5개+보너스볼</span><span>30,000,000</span><span>${machine.getNumberOfGrade(
+      'second'
+    )}개</span></div>
+    <div class="grid"><span>6개</span><span>2,000,000,000</span><span>${machine.getNumberOfGrade(
+      'first'
+    )}개</span></div>
+ `;
+}
