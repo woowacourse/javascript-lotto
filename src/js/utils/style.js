1717f01094ffbe65e68b79a8f831d07dc5ab15b3
jswith <9geul@naver.com>
2022-03-04 21:59:13 +0900

    style: 버튼 투명 처리 기능 추가

--- src/js/utils/style.js
+++ src/js/utils/style.js
@@ -2,6 +2,10 @@ import { $ } from './dom';
 
 export const deactivateForm = (enable, selectorArray) => {
   selectorArray.forEach((selector) => {
-    $(selector).setAttribute('disabled', enable);
+    const element = $(selector);
+    element.setAttribute('disabled', enable);
+    element.tagName === 'BUTTON' && enable
+      ? element.classList.add('button-dimmed')
+      : element.classList.remove('button-dimmed');
   });
 };
