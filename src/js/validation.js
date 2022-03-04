f5fb1044587d22c3db32ea52c4fde096fd50be65
Soyi Jeon <soyi.dev@gmail.com>
2022-03-04 18:10:49 +0900

    refactor: 유효성 검사 관련 함수 추가, 수정 및 이름 변경

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,25 +1,32 @@
-import { MIN_CHARGE_INPUT, ERROR_MESSAGE, MAX_CHARGE_INPUT } from './constants/constants';
+import { MIN_CHARGE_INPUT, ERROR_MESSAGE, MAX_CHARGE_INPUT, LOTTERY_TICKET_NUMBER } from './constants/constants';
 
-const isChargeWithinValidRange = (charge) => MIN_CHARGE_INPUT <= charge && charge <= MAX_CHARGE_INPUT;
+const hasDuplicates = arr => new Set(arr).size !== arr.length;
+const isNotInteger = value => !Number.isInteger(value);
 
-const isWinningNumberWithinValidRange = (winningNumber) => winningNumber >= 1 && winningNumber <= 45;
-
-const hasDuplicates = (arr) => new Set(arr).size !== arr.length
+const isChargeOutOfRange = (charge) => charge < MIN_CHARGE_INPUT || charge > MAX_CHARGE_INPUT;
 
 export const validateCharge = (charge) => {
-  if (!Number.isInteger(charge))
+  if (isNotInteger(charge))
     throw new Error(ERROR_MESSAGE.CHARGE_INPUT_NOT_INTEGER);
-  if (!isChargeWithinValidRange(charge))
+  if (isChargeOutOfRange(charge))
     throw new Error(ERROR_MESSAGE.CHARGE_INPUT_NOT_IN_RANGE);
 }
 
+const isWinningNumberOutOfRange = (winningNumber) => 
+  winningNumber < LOTTERY_TICKET_NUMBER.MIN || winningNumber > LOTTERY_TICKET_NUMBER.MAX;
+
+const isInsufficientInputNumber = (winningNumbers) => winningNumbers.length !== 7;
+const someWinningNumber = (validateFunc) => (winningNumbers) => winningNumbers.some(validateFunc);
+const hasNotIntegerNumber = someWinningNumber(number => isNotInteger(number));
+const hasNumberOutOfRange = someWinningNumber(number => isWinningNumberOutOfRange(number));
+
 export const validateWinningNumbers = (winningNumbers) => {
-  if (winningNumbers.length !== 7)
+  if (isInsufficientInputNumber(winningNumbers))
     throw new Error(ERROR_MESSAGE.WINNING_NUMBERS_INSUFFICIENT)
   if (hasDuplicates(winningNumbers))
     throw new Error(ERROR_MESSAGE.WINNING_NUMBERS_DUPLICATED);
-  if (winningNumbers.some((number) => !Number.isInteger(number)))
+  if (hasNotIntegerNumber(winningNumbers))
     throw new Error(ERROR_MESSAGE.WINNING_NUMBERS_NOT_INTEGER);
-  if (winningNumbers.some((number) => !isWinningNumberWithinValidRange(number)))
+  if (hasNumberOutOfRange(winningNumbers))
     throw new Error(ERROR_MESSAGE.WINNING_NUMBERS_NOT_IN_RANGE);
 }
