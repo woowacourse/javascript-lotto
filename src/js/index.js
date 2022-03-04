c6f88312d8a28d52f48b495e6b476a42a9927113
kamwoo <rkadndud06@naver.com>
2022-03-04 23:11:18 +0900

    refactor: 섹션별 css 파일 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 import '../css/index.css';
+import '../css/lotto_app.css';
+import '../css/payment_section.css';
+import '../css/ticket_section.css';
+import '../css/statistic_section.css';
+import '../css/winning_number_section.css';
 import App from './App.js';
 
 const app = new App();
