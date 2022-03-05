09c96aa4861108640ebf27cf4b810ba86a960c0a
jswith <9geul@naver.com>
2022-03-05 20:47:08 +0900

    feat: 결과 확인 모달창 구현

--- src/js/handler/WinningResultModal.js
+++ src/js/handler/WinningResultModal.js
@@ -18,6 +18,61 @@ export default class WinningResultModal {
     return userInputWinningNumber;
   }
 
+  openWinningResultModal() {
+    const template = `
+    <div id="modal" class="modal-overlay modal-display-none">
+      <div class="modal-window">
+        <span class="modal-close-button"></span>
+        <h2 class="modal-winning-statistics-text">🏆 당첨 통계 🏆</h2>
+        <table class="modal-table">
+          <thead>
+            <tr>
+              <th>일치 갯수</th>
+              <th>당첨금</th>
+              <th>당첨 갯수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>3개</td>
+              <td>5,000</td>
+              <td class="match-3">n개</td>
+            </tr>
+            <tr>
+              <td>4개</td>
+              <td>5,0000</td>
+              <td class="match-4">n개</td>
+            </tr>
+            <tr>
+              <td>5개</td>
+              <td>1,500,000</td>
+              <td class="match-5">n개</td>
+            </tr>
+            <tr>
+              <td>5개 + 보너스볼</td>
+              <td>30,000,000</td>
+              <td class="match-5-bonus-ball">n개</td>
+            </tr>
+            <tr>
+              <td>6개</td>
+              <td>2,000,000,000</td>
+              <td class="match-6">n개</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="modal-profit-rate-text">당신의 총 수익률은 %입니다.</div>
+        <button class="modal-restart-button">다시 시작하기</button>
+      </div>
+    </div>`;
+    $('#app').insertAdjacentHTML('afterend', template);
+    $('#modal').classList.remove('modal-display-none');
+    $('.modal-close-button').addEventListener('click', this.closeWinningResultModal);
+  }
+
+  closeWinningResultModal() {
+    $('#modal').classList.add('modal-display-none');
+  }
+
   handleWinningResultModal = (e) => {
     e.preventDefault();
     const userInputWinningNumber = this.getUserInputWinningNumber();
@@ -32,6 +87,7 @@ export default class WinningResultModal {
     winningStatistics.calculateLottoRank(winningNumber.getWinningNumber(), winningNumber.getBonusNumber());
 
     // 모달 창 띄우기
+    this.openWinningResultModal();
 
     // 다시 시작하기 버튼 이벤트 바인딩
   };
