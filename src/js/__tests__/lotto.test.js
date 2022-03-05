de2f6610a73b22741e1d9f6712e8e6364619d7f9
sanaandmomo <qajjang46@naver.com>
2022-03-06 05:10:22 +0900

    refactor: 서로 연관되는 테스트 묶기

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -4,56 +4,29 @@ describe('로또 번호가 얼마나 일치하는지 계산할 수 있다.', () 
   const winningNumber = [1, 32, 24, 19, 43, 3];
   const bonusNumber = 45;
 
-  test('[1, 2, 3, 4, 5, 6] 일 때, 2개가 일치해야 한다.', () => {
+  test('보너스 번호가 일치하지 않을 때 몇 개가 일치하는지 알 수 있어야 한다.', () => {
     const lotto = new Lotto();
-    const lottoNumber = [1, 2, 3, 4, 5, 6];
 
-    lotto.numbers = lottoNumber;
-
+    lotto.numbers = [1, 2, 3, 4, 5, 6];
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(2);
-  });
 
-  test('[32, 24, 19, 7, 8, 2] 일 때, 3개가 일치해야 한다.', () => {
-    const lotto = new Lotto();
-    const lottoNumber = [32, 24, 19, 7, 8, 2];
-
-    lotto.numbers = lottoNumber;
-
+    lotto.numbers = [32, 24, 19, 7, 8, 2];
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(3);
-  });
 
-  test('[32, 24, 19, 7, 8, 3] 일 때, 4개가 일치해야 한다.', () => {
-    const lotto = new Lotto();
-    const lottoNumber = [32, 24, 19, 7, 8, 3];
-
-    lotto.numbers = lottoNumber;
-
+    lotto.numbers = [32, 24, 19, 7, 8, 3];
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(4);
-  });
 
-  test('[32, 24, 19, 1, 8, 3] 일 때, 5개가 일치해야 한다.', () => {
-    const lotto = new Lotto();
-    const lottoNumber = [32, 24, 19, 1, 8, 3];
-
-    lotto.numbers = lottoNumber;
-
+    lotto.numbers = [32, 24, 19, 1, 8, 3];
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(5);
-  });
 
-  test('[32, 24, 19, 1, 43, 3] 일 때, 6개가 일치해야 한다.', () => {
-    const lotto = new Lotto();
-    const lottoNumber = [32, 24, 19, 1, 43, 3];
-
-    lotto.numbers = lottoNumber;
-
+    lotto.numbers = [32, 24, 19, 1, 43, 3];
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(6);
   });
 
-  test('[6, 24, 19, 1, 43, 3] 일 때, 5개+보너스볼이란 의미로 7이 리턴되어야 한다.', () => {
+  test('보너스 번호가 일치할 때, 몇 개가 일치하는지 알 수 있어야 한다.', () => {
     const lotto = new Lotto();
-    const lottoNumber = [45, 24, 19, 1, 43, 3];
 
-    lotto.numbers = lottoNumber;
+    lotto.numbers = [45, 24, 19, 1, 43, 3];
 
     expect(lotto.match(new Set(winningNumber), bonusNumber)).toBe(7);
   });
