bf7afceded29cd2a96ce43ef394b7cb0b504a618
Jeongmin Woo <greenblues1190@gmail.com>
2022-03-05 18:23:08 +0900

    feat: Lotto.matchWinningNumbers 구현
    주어진 당첨 번호로 로또 한장의 당첨 결과를 구하는 기능

--- src/js/models/Lotto/Lotto.js
+++ src/js/models/Lotto/Lotto.js
@@ -21,9 +21,41 @@ export default class Lotto {
     }).sort(ascendingOrder);
   }
 
-  constructor() {
-    this.numbers = Lotto.getLottoNumberList();
+  constructor(manualNumbers) {
+    this.numbers = manualNumbers || Lotto.getLottoNumberList();
 
     Object.freeze(this);
+  }
+
+  countMatchedNumbers(winningNumbers) {
+    let matchedCount = 0;
+    let isBonusNumberMatched = false;
+
+    winningNumbers.main.forEach((num) => {
+      if (this.numbers.includes(num)) matchedCount += 1;
+    });
+
+    if (this.numbers.includes(winningNumbers.bonus))
+      isBonusNumberMatched = true;
+
+    return { matchedCount, isBonusNumberMatched };
+  }
+
+  matchWinningNumbers(winningNumbers) {
+    const { matchedCount, isBonusNumberMatched } =
+      this.countMatchedNumbers(winningNumbers);
+
+    switch (matchedCount) {
+      case 6:
+        return 'first';
+      case 5:
+        return isBonusNumberMatched ? 'second' : 'third';
+      case 4:
+        return 'forth';
+      case 3:
+        return 'fifth';
+      default:
+        return 'none';
+    }
   }
 }
