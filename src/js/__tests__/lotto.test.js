b15d81ae8eba2012586a2031a9cb572c8627c0c9
Ahn <jin7969@gmail.com>
2022-03-05 15:58:33 +0900

    test: 당첨 번호 유효성 테스트 추가

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,11 +1,11 @@
-import Lotto from "../model/Lotto.js";
+import LottoModel from "../model/LottoModel.js";
 import { LOTTO_NUMBER } from "../utils/constants.js";
 import { isValidNumberRange } from "../utils/general.js";
 
 /* eslint-disable no-undef */
 describe("로또 번호 생성 테스트", () => {
   test("로또 번호는 1부터 45 범위 안에 있어야 한다.", () => {
-    const lotto = new Lotto();
+    const lotto = new LottoModel();
     const lottoNumbers = lotto.generateLottoNumber();
     expect(
       lottoNumbers.some((number) =>
@@ -16,13 +16,13 @@ describe("로또 번호 생성 테스트", () => {
 
   test("로또 번호는 총 6개 가지고 있다.", () => {
     const lottoNumberLength = 6;
-    const lotto = new Lotto();
+    const lotto = new LottoModel();
     expect(lotto.generateLottoNumber()).toHaveLength(lottoNumberLength);
   });
 
   test("구입한 개수 만큼 로또가 구매된다.", () => {
     const lottoCount = 5;
-    const lotto = new Lotto();
+    const lotto = new LottoModel();
     lotto.generateLottoTicket(lottoCount);
     expect(lotto.getLottoList()).toHaveLength(lottoCount);
   });
--- src/js/__tests__/winningNumber.test.js
+++ src/js/__tests__/winningNumber.test.js
@@ -4,6 +4,8 @@ import {
   isValidNumberRange,
   isValidOverlap,
 } from "../utils/general.js";
+import { validateWinningNumbers } from "../utils/validation.js";
+import { ERROR_MESSAGES } from "../utils/constants.js";
 
 /* eslint-disable no-undef */
 describe("당첨 번호 유효성 테스트", () => {
@@ -12,8 +14,18 @@ describe("당첨 번호 유효성 테스트", () => {
     expect(numbers.some((number) => isValidTypeNumber(number))).toBeFalsy();
   });
 
+  test("숫자가 아니면 에러 메시지를 보여준다.", () => {
+    const value = ["문자", 2, 3, 4, 5, 6];
+    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.NOT_A_NUMBER));
+  });
+
   test("6개 전부 입력을 해야 한다.", () => {
     expect(numbers.some((number) => isValidEmptyValue(number))).toBeFalsy();
+  });
+
+  test("빈값이 입력이 되면 에러 메시지를 보여준다.", () => {
+    const value = ["", 2, 3, 4, 5, 6];
+    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.EMPTY_CAN_NOT_ENTERED));
   });
 
   test("1~45 범위 안으로 입력할 수 있다.", () => {
@@ -22,7 +34,17 @@ describe("당첨 번호 유효성 테스트", () => {
     expect(numbers.some((number) => isValidNumberRange(number, min, max))).toBeFalsy();
   });
 
+  test("1~45 범위를 벗어나면 에러 메시지를 보여준다.", () => {
+    const value = [1, 2, 3, 4, 5, 46];
+    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.OUT_OF_RANGE));
+  });
+
   test("중복없이 입력해야 한다.", () => {
     expect(isValidOverlap(numbers)).toBeFalsy();
+  });
+
+  test("중복 입력을 하면 에러 메시지를 보여준다.", () => {
+    const value = [2, 2, 3, 4, 5, 6];
+    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.CAN_NOT_OVERLAP));
   });
 });
