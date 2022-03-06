306fffc64a50620f4df38655471f21139fb90e01
Soyi Jeon <soyi.dev@gmail.com>
2022-03-06 22:07:52 +0900

    test: 테스트 데이터 정의 이동
    공통으로 사용되는 테스트 데이터를 describe 아래로 이동

--- src/js/__tests__/checkResult.test.js
+++ src/js/__tests__/checkResult.test.js
@@ -2,37 +2,36 @@ import { countMatchNumber, calculateMatchResult, calculateProfitRatio } from '..
 import LotteryTicket from '../LotteryTicket';
 
 describe('결과 확인 테스트', () => {
+  const winningNumber = [12, 28, 22, 37, 19, 23];
+  const ticketNumber = [
+    [12, 28, 22, 37, 19, 23], // 6개 일치
+    [12, 28, 22, 37, 19, 21], // 5개 일치 + 보너스 일치
+    [12, 28, 22, 37, 19, 45], // 5개 일치
+    [12, 28, 22, 37, 14, 20], // 4개 일치
+    [12, 28, 22, 31, 34, 45], // 3개 일치
+    [12, 28, 30, 31, 34, 45], // 2개 일치
+  ]
 
   it('주어진 로또 숫자와 당첨 번호가 일치하는 숫자의 개수를 확인한다.', () => {
-    const winningNumber = [12, 28, 22, 37, 19, 23];
-    const lottos = [
-      [12, 28, 22, 37, 19, 23], // 6개 일치
-      [12, 28, 22, 37, 19, 21], // 5개 일치
-      [12, 28, 22, 37, 14, 20], // 4개 일치
-      [12, 28, 22, 31, 34, 45], // 3개 일치
-      [12, 28, 30, 31, 34, 45], // 2개 일치
-    ]
-
-    expect(countMatchNumber(lottos[0], winningNumber)).toEqual(6);
-    expect(countMatchNumber(lottos[1], winningNumber)).toEqual(5);
-    expect(countMatchNumber(lottos[2], winningNumber)).toEqual(4);
-    expect(countMatchNumber(lottos[3], winningNumber)).toEqual(3);
-    expect(countMatchNumber(lottos[4], winningNumber)).toEqual(2);
+    expect(countMatchNumber(ticketNumber[0], winningNumber)).toEqual(6);
+    expect(countMatchNumber(ticketNumber[2], winningNumber)).toEqual(5);
+    expect(countMatchNumber(ticketNumber[3], winningNumber)).toEqual(4);
+    expect(countMatchNumber(ticketNumber[4], winningNumber)).toEqual(3);
+    expect(countMatchNumber(ticketNumber[5], winningNumber)).toEqual(2);
   });
 
   it('주어진 로또 숫자와, 당첨 번호, 보너스 숫자의 최종 일치 결과를 확인한다.', () => {
-    const winningNumber = [12, 28, 22, 37, 19, 23];
     const bonusNumber = 21;
-    const lottos = [
-      new LotteryTicket([12, 28, 22, 37, 19, 23]), // 6개 일치
-      new LotteryTicket([12, 28, 22, 37, 19, 21]), // 5개 일치 + 보너스 일치
-      new LotteryTicket([12, 28, 22, 37, 19, 45]), // 5개 일치
-      new LotteryTicket([12, 28, 22, 37, 14, 20]), // 4개 일치
-      new LotteryTicket([12, 28, 22, 31, 34, 45]), // 3개 일치
+    const lottoTicket = [
+      new LotteryTicket(ticketNumber[0]), // 6개 일치
+      new LotteryTicket(ticketNumber[1]), // 5개 일치 + 보너스 일치
+      new LotteryTicket(ticketNumber[2]), // 5개 일치
+      new LotteryTicket(ticketNumber[3]), // 4개 일치
+      new LotteryTicket(ticketNumber[4]), // 3개 일치
     ];
 
     const expectedResult = [1, 1, 1, 1, 1];
-    expect(calculateMatchResult(lottos, winningNumber, bonusNumber)).toEqual(expectedResult);
+    expect(calculateMatchResult(lottoTicket, winningNumber, bonusNumber)).toEqual(expectedResult);
   });
 
   it('주어진 로또 티켓 수와 최종 일치 결과로 수익률을 계산한다.', () => {
