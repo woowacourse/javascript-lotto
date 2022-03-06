7ab6fec7b85b03e17fb204ab1d344d4309d83f3f
Daye Lee <508yeah@gmail.com>
2022-03-07 00:59:19 +0900

    :white_check_mark: test(unit): 단위 테스트에 불필요한 테스트 제거

--- src/js/__tests__/lotto.test.js
+++ src/js/__tests__/lotto.test.js
@@ -1,8 +1,7 @@
 import Store from '../flux/store';
 import initialState from '../flux/initialState';
-
-import MoneyForm from '../components/MoneyForm';
-import WinningNumberForm from '../components/WinningNumberForm';
+import createAction from '../flux/actionCreator';
+import ACTION from '../flux/actions';
 
 import { LOTTO, PRIZE_MONEY } from '../constants';
 import { calculateEarningsRate } from '../utils';
@@ -15,8 +14,7 @@ describe('로또 게임을 진행한다.', () => {
   test('로또를 구입할 금액을 입력할 수 있다.', () => {
     const money = '5000';
 
-    const moneyFormComponent = new MoneyForm();
-    moneyFormComponent.updateMoney(money);
+    window.store.dispatch(createAction(ACTION.PURCHASE_LOTTO, money));
 
     expect(window.store.getState().money).toBe(money);
   });
@@ -24,8 +22,7 @@ describe('로또 게임을 진행한다.', () => {
   test('입력한 금액만큼의 로또를 자동 구매할 수 있다.', () => {
     const money = '5000';
 
-    const moneyFormComponent = new MoneyForm();
-    moneyFormComponent.updateMoney(money);
+    window.store.dispatch(createAction(ACTION.PURCHASE_LOTTO, money));
 
     expect(window.store.getState().lottoList.length).toBe(Number(money) / LOTTO.PRICE);
   });
@@ -34,8 +31,7 @@ describe('로또 게임을 진행한다.', () => {
     const winningNumber = ['1', '4', '29', '39', '43', '45', '31'];
     const winningNumbers = ['1', '4', '29', '39', '43', '45'];
 
-    const winningNumberFormComponent = new WinningNumberForm();
-    winningNumberFormComponent.checkResult(winningNumber);
+    window.store.dispatch(createAction(ACTION.UPDATE_RESULT, winningNumber));
 
     expect(window.store.getState().winningNumbers).toStrictEqual(winningNumbers);
   });
@@ -44,17 +40,15 @@ describe('로또 게임을 진행한다.', () => {
     const winningNumber = ['1', '4', '29', '39', '43', '45', '31'];
     const bonusNumber = '31';
 
-    const winningNumberFormComponent = new WinningNumberForm();
-    winningNumberFormComponent.checkResult(winningNumber);
+    window.store.dispatch(createAction(ACTION.UPDATE_RESULT, winningNumber));
 
     expect(window.store.getState().bonusNumber).toStrictEqual(bonusNumber);
   });
 
   test('로또 당첨 결과 모달을 확인할 수 있다.', () => {
-    const winningNumber = ['1', '4', '29', '39', '43', '45', '31'];
+    const resultModalVisibility = true;
 
-    const winningNumberFormComponent = new WinningNumberForm();
-    winningNumberFormComponent.checkResult(winningNumber);
+    window.store.dispatch(createAction(ACTION.TOGGLE_RESULT_MODAL, resultModalVisibility));
 
     expect(window.store.getState().resultModalVisibility).toBe(true);
   });
