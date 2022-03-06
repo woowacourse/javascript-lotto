d2655d2e933119a57902f9365635a6db58079b59
gytks4 <gytks4@naver.com>
2022-03-07 00:42:24 +0900

    fix: 한글 정규식에 가-힣을 추가하여 NaN 문제 해결

--- src/js/constants/regexp.js
+++ src/js/constants/regexp.js
@@ -2,5 +2,5 @@ export const REGEXP = {
   NUMBER: /^[0-9]/,
   NOT_NUMBER_IN_NUMBER_INPUT: /[e.\-+]{,1}/,
   NOT_NUMBER: /[a-zA-z|\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/,
-  KOREAN: /[ㄱ-ㅎ|ㅏ-ㅣ]/,
+  KOREAN: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
 };
