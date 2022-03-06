ee725ed95538ae42251f25de56ef0c7c03ec9030
jswith <9geul@naver.com>
2022-03-06 22:39:19 +0900

    fix: 오타 수정

--- src/js/utils/array.js
+++ src/js/utils/array.js
@@ -1,8 +1,8 @@
 export const shuffleNumber = (array) => [...array].sort(() => Math.random() - 0.5);
 
 export const findEqualElementsLength = (array1, array2) => {
-  const meregedArray = array1.concat(array2);
-  const equalElements = meregedArray.filter((item) => array1.includes(item) && array2.includes(item));
+  const mergedArray = array1.concat(array2);
+  const equalElements = mergedArray.filter((item) => array1.includes(item) && array2.includes(item));
   const deduplicatedEqualElements = [...new Set(equalElements)];
 
   return deduplicatedEqualElements.length;
