a359e70555148cad7eaefd03e2ed69faffc4492e
sangminsong <ssmin940606@gmail.com>
2022-03-06 19:35:17 +0900

    refactor: views 클래스 시작점 생성
    - 가독성을 위해 view에 해당하는 클래스들을 index.js를 만들어주어 관리해주도록 변경
    
    - 관련 피드백: https://github.com/woowacourse/javascript-lotto/pull/136#discussion_r820087835

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,14 @@ import LottoController from './lottoController.js';
 import LottoCreator from './model/lottoCreator.js';
 import LottoResultManager from './model/lottoResultManager.js';
 
-import LottoPurchaseInputView from './views/lottoPurchaseInputView.js';
-import LottoPurchaseResultView from './views/lottoPurchaseResultView.js';
-import LottoWinningNumberInputView from './views/lottoWinningNumberInputView.js';
-import LottoMatchResultModalView from './views/lottoMatchResultModalView.js';
+import views from './views/index.js';
 
 import '../css/index.css';
 
-// eslint-disable-next-line max-lines-per-function
 const startLotto = () => {
   const models = {
     lottoCreator: new LottoCreator(),
     LottoResultManager,
-  };
-
-  const views = {
-    lottoPurchaseInputView: new LottoPurchaseInputView(),
-    lottoPurchaseResultView: new LottoPurchaseResultView(),
-    lottoWinningNumberInputView: new LottoWinningNumberInputView(),
-    lottoMatchResultModalView: new LottoMatchResultModalView(),
   };
 
   new LottoController(models, views);
