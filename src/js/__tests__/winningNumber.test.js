82ceabb88aa11d9e442b65ab711f0991b79c1816
jswith <9geul@naver.com>
2022-03-06 22:28:22 +0900

    test: 테스트 코드 수정

--- src/js/__tests__/winningNumber.test.js
+++ src/js/__tests__/winningNumber.test.js
@@ -8,10 +8,14 @@ describe('당첨 번호 테스트', () => {
   });
 
   test('당첨 번호 중 1등 당첨 번호를 가져온다.', () => {
+    winningNumber.initializeWinningNumber();
+    winningNumber.setWinningNumber([8, 13, 18, 24, 27, 29, 17]);
     expect(winningNumber.getWinningNumber().join(',')).toBe('8,13,18,24,27,29');
   });
 
   test('당첨 번호 중 보너스 번호를 가져온다.', () => {
+    winningNumber.initializeWinningNumber();
+    winningNumber.setWinningNumber([8, 13, 18, 24, 27, 29, 17]);
     expect(winningNumber.getBonusNumber()).toBe(17);
   });
 
