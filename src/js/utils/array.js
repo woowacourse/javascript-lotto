19a2f2e91577f13135bac18950fc8e15bcb892af
jswith <9geul@naver.com>
2022-03-06 22:37:40 +0900

    refactor: findEqualElementsLength 함수 내부 변수 네이밍 수정

--- src/js/utils/array.js
+++ src/js/utils/array.js
@@ -3,7 +3,7 @@ export const shuffleNumber = (array) => [...array].sort(() => Math.random() - 0.
 export const findEqualElementsLength = (array1, array2) => {
   const meregedArray = array1.concat(array2);
   const equalElements = meregedArray.filter((item) => array1.includes(item) && array2.includes(item));
-  const deduplicatedArray = [...new Set(equalElements)];
+  const deduplicatedEqualElements = [...new Set(equalElements)];
 
-  return deduplicatedArray.length;
+  return deduplicatedEqualElements.length;
 };
