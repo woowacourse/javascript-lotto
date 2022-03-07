6824a6340345b80145981b02f31216b048c23b57
Ahn <jin7969@gmail.com>
2022-03-07 15:32:15 +0900

    test: 당첨 번호 유효성 검사 테스트 코드 수정

--- src/js/__tests__/winningNumber.test.js
+++ src/js/__tests__/winningNumber.test.js
@@ -1,40 +1,11 @@
-import { isNotNumber, isEmpty, isOutOfRange, isOverlap } from "../utils/general.js";
+import { isOverlap } from "../utils/general.js";
 import { validateWinningNumbers } from "../utils/validation.js";
 import { ERROR_MESSAGES } from "../utils/constants.js";
 
 /* eslint-disable no-undef */
 describe("당첨 번호 유효성 테스트", () => {
-  const numbers = [1, 2, 3, 4, 5, 6];
-  test("숫자만 입력할 수 있다.", () => {
-    expect(numbers.some((number) => isNotNumber(number))).toBeFalsy();
-  });
-
-  test("숫자가 아니면 에러 메시지를 보여준다.", () => {
-    const value = ["문자", 2, 3, 4, 5, 6];
-    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.NOT_A_NUMBER));
-  });
-
-  test("6개 전부 입력을 해야 한다.", () => {
-    expect(numbers.some((number) => isEmpty(number))).toBeFalsy();
-  });
-
-  test("빈값이 입력이 되면 에러 메시지를 보여준다.", () => {
-    const value = ["", 2, 3, 4, 5, 6];
-    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.EMPTY_CAN_NOT_ENTERED));
-  });
-
-  test("1~45 범위 안으로 입력할 수 있다.", () => {
-    const min = 1;
-    const max = 45;
-    expect(numbers.some((number) => isOutOfRange(number, min, max))).toBeFalsy();
-  });
-
-  test("1~45 범위를 벗어나면 에러 메시지를 보여준다.", () => {
-    const value = [1, 2, 3, 4, 5, 46];
-    expect(() => validateWinningNumbers(value).toThrow(ERROR_MESSAGES.OUT_OF_RANGE));
-  });
-
   test("중복없이 입력해야 한다.", () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
     expect(isOverlap(numbers)).toBeFalsy();
   });
 
