8288865b40e2c3959056af413cc454a006db3fef
InKyoJeong <viapolar@naver.com>
2022-03-07 19:47:59 +0900

    refactor: validation 함수명 및 로직 수정

--- src/js/utils/validation.js
+++ src/js/utils/validation.js
@@ -1,44 +1,44 @@
 import { AMOUNT, ERROR_MESSAGES, LOTTO_NUMBER } from "./constants.js";
 
-export const isValidMinimumAmount = (amount) => {
-  return amount >= AMOUNT.MINIMUM;
+export const isUnderMinimumAmount = (amount) => {
+  return amount < AMOUNT.MINIMUM;
 };
 
-export const isValidMaximumAmount = (amount) => {
-  return amount <= AMOUNT.MAXIMUM;
+export const isOverMaximumAmount = (amount) => {
+  return amount > AMOUNT.MAXIMUM;
 };
 
-export const isValidAmountUnit = (amount) => {
-  return amount % AMOUNT.UNIT === 0;
+export const isUnderAmountUnit = (amount) => {
+  return amount % AMOUNT.UNIT !== 0;
 };
 
-export const isValidWinningNumbers = (numbers) => {
-  return numbers.every(
-    (number) => number >= LOTTO_NUMBER.RANGE_MIN && number <= LOTTO_NUMBER.RANGE_MAX,
+export const hasOverRangeWinningNumber = (numbers) => {
+  return numbers.some(
+    (number) => number < LOTTO_NUMBER.RANGE_MIN || number > LOTTO_NUMBER.RANGE_MAX,
   );
 };
 
-export const isDuplicatedNumbers = (numbers) => {
+export const hasDuplicatedNumber = (numbers) => {
   return numbers.length !== new Set(numbers).size;
 };
 
 export const verifyPurchaseAmount = (amount) => {
-  if (!isValidMinimumAmount(amount)) {
+  if (isUnderMinimumAmount(amount)) {
     throw new Error(ERROR_MESSAGES.INVALID_MINIMUM_AMOUNT);
   }
-  if (!isValidMaximumAmount(amount)) {
+  if (isOverMaximumAmount(amount)) {
     throw new Error(ERROR_MESSAGES.INVALID_MAXIMUM_AMOUNT);
   }
-  if (!isValidAmountUnit(amount)) {
+  if (isUnderAmountUnit(amount)) {
     throw new Error(ERROR_MESSAGES.INVALID_AMOUNT_UNIT);
   }
 };
 
 export const verifyWinningNumbers = (numbers) => {
-  if (!isValidWinningNumbers(numbers)) {
+  if (hasOverRangeWinningNumber(numbers)) {
     throw new Error(ERROR_MESSAGES.INVALID_LOTTO_RANGE);
   }
-  if (isDuplicatedNumbers(numbers)) {
+  if (hasDuplicatedNumber(numbers)) {
     throw new Error(ERROR_MESSAGES.DUPLICATED_LOTTO_NUMBER);
   }
 };
