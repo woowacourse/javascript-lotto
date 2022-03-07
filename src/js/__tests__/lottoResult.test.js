a08163da95fa7111f884e938f8df4b7d3c36e2d7
wonsss <bigdatajang@gmail.com>
2022-03-07 19:52:24 +0900

    :white_check_mark: test: add another test case

--- src/js/__tests__/lottoResult.test.js
+++ src/js/__tests__/lottoResult.test.js
@@ -1,7 +1,7 @@
 import LottoVendor from '../model/LottoVendor';
 import LottoResult from '../model/LottoResult';
 
-describe('당첨 결과를 확인할 수 있어야 한다.', () => {
+describe('[케이스 1]당첨 결과를 확인할 수 있어야 한다.', () => {
   // given
   const lottoVendor = new LottoVendor();
   lottoVendor.lottos = [
@@ -12,16 +12,15 @@ describe('당첨 결과를 확인할 수 있어야 한다.', () => {
     { numbers: [5, 1, 2, 3, 4, 8] },
     { numbers: [4, 3, 2, 1, 9, 15] },
   ];
+  lottoVendor.paidMoney = 6000;
 
   const lottoResult = new LottoResult(lottoVendor);
-  lottoResult.winningNumbers = [1, 2, 3, 4, 5, 6];
-  lottoResult.bonusNumber = 7;
 
   test('당첨번호와 구입한 로또 번호를 비교하여 당첨 금액별 당첨 번호 개수를 계산할 수 있어야 한다.', () => {
-    // when
+    lottoResult.winningNumbers = [1, 2, 3, 4, 5, 6];
+    lottoResult.bonusNumber = 7;
     lottoResult.calculateWinningCounts();
 
-    // then
     expect(lottoResult.winningCounts).toStrictEqual({
       fifthPlace: 1,
       fourthPlace: 1,
@@ -32,13 +31,46 @@ describe('당첨 결과를 확인할 수 있어야 한다.', () => {
   });
 
   test('구입 금액과 당첨된 금액을 비교하여 수익률을 계산할 수 있어야 한다.', () => {
-    // given
-    lottoVendor.paidMoney = 1000000;
-
     // when
     lottoResult.calculateLottoYield();
 
     // then
-    expect(lottoResult.lottoYield).toBe(206155);
+    expect(lottoResult.lottoYield).toBe(34359250);
+  });
+});
+
+describe('[케이스 2] 당첨 결과를 확인할 수 있어야 한다.', () => {
+  // given
+  const lottoVendor = new LottoVendor();
+  lottoVendor.lottos = [
+    { numbers: [2, 1, 3, 4, 5, 6] },
+    { numbers: [1, 2, 3, 24, 25, 17] },
+    { numbers: [7, 1, 3, 2, 4, 5] },
+    { numbers: [7, 1, 3, 2, 4, 5] },
+    { numbers: [5, 1, 2, 3, 4, 8] },
+    { numbers: [4, 3, 2, 1, 9, 15] },
+  ];
+  lottoVendor.paidMoney = 6000;
+
+  const lottoResult = new LottoResult(lottoVendor);
+
+  test('당첨번호와 구입한 로또 번호를 비교하여 당첨 금액별 당첨 번호 개수를 계산할 수 있어야 한다.', () => {
+    lottoResult.winningNumbers = [1, 2, 5, 15, 24, 25];
+    lottoResult.bonusNumber = 8;
+    lottoResult.calculateWinningCounts();
+
+    expect(lottoResult.winningCounts).toStrictEqual({
+      fifthPlace: 5,
+      fourthPlace: 1,
+      thirdPlace: 0,
+      secondPlace: 0,
+      firstPlace: 0,
+    });
+  });
+
+  test('구입 금액과 당첨된 금액을 비교하여 수익률을 계산할 수 있어야 한다.', () => {
+    lottoResult.calculateLottoYield();
+
+    expect(lottoResult.lottoYield).toBe(1250);
   });
 });
