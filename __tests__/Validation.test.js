ec3956faacf1287c98c639228c1e0fef5411b1ee
ashleysyheo <ashleysyheo@gmail.com>
2023-02-14 16:48:05 +0900

    test: check minimum money input

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -13,7 +13,7 @@ describe("Validation 테스트", () => {
       });
     });
   });
-  
+
   test("값이 천원 단위가 아닌 경우 예외 발생", () => {
     const PURCHASE_AMOUNT = [100, 2200, 3330, 4444];
 
@@ -22,5 +22,15 @@ describe("Validation 테스트", () => {
         ErrorMessage.MONEY_VALUE
       );
     });
+  });
+
+  test('값이 숫자가 아닌 문자를 포함하는 경우 예외 발생', () => {
+    const PURCHASE_AMOUNT = ['A1', '2B', ' ', '', 'ABC', '1 2 3'];
+
+    PURCHASE_AMOUNT.forEach((amount) => {
+      expect(() => Validation.checkMoneyInputType(amount)).toThrow(
+        ErrorMessage.MONEY_INPUT_TYPE
+      )
+    })
   });
 });
