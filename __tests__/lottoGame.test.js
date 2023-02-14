35026b35f6d1d297e2393f62c382c05215ef9166
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-02-14 19:25:52 +0900

    test: 로또 상금과 구매 금액을 기반으로 한 수익률 계산 테스트 작성

--- __tests__/lottoGame.test.js
+++ __tests__/lottoGame.test.js
@@ -4,14 +4,24 @@ test('당첨된 로또 상금을 계산한다.', () => {
   // given
   const ranks = [3, 4, 0, 0, 0, 0, 0, 0];
   const lottoGame = new LottoGame();
+
   // when
   const totalAmount = lottoGame.calculateTotalPrize(ranks);
+
   //then
   expect(totalAmount).toBe(1550000);
 });
 
-test('반환된 개수를 토대로 수익률을 계산한다.', () => {
-  // give
+test('로또 상금과 구매 금액을 토대로 수익률을 계산한다.', () => {
+  // given
+  const price = 8000;
+  const ranks = [3, 4, 0, 0, 0, 0, 0, 0];
+  const lottoGame = new LottoGame();
+  const totalAmount = lottoGame.calculateTotalPrize(ranks);
+
   // when
+  const earningRate = lottoGame.calculateEarningRate(price, totalAmount);
+
   // then
+  expect(earningRate).toBe('193.75');
 });
