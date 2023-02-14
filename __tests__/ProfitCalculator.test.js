4dbfb51b84415b98a9d58cc8ca84f6da29e9e653
shackstack <kms1061@naver.com>
2023-02-14 18:17:52 +0900

    test: 전체 당첨 금액과 구입 금액으로 수익률을 계산한다.

--- __tests__/ProfitCalculator.test.js
+++ __tests__/ProfitCalculator.test.js
@@ -1,13 +1,26 @@
 const ProfitCalculator = require('../src/domain/ProfitCaculator');
 
-test('전체 당첨 금액의 합을 구한다.', () => {
+describe('', () => {
   //given
   const ranking = { 1: 1, 2: 0, 3: 0, 4: 0, 5: 2 };
-
-  //when
   const profitCalculator = new ProfitCalculator(ranking);
-  const winningAmount = profitCalculator.getWinningAmount();
 
-  //then
-  expect(winningAmount).toEqual(2_000_010_000);
+  test('전체 당첨 금액의 합을 구한다.', () => {
+    //when
+    const winningAmount = profitCalculator.getWinningAmount();
+
+    //then
+    expect(winningAmount).toEqual(2_000_010_000);
+  });
+
+  test('전체 당첨 금액과 구입 금액으로 수익률을 계산한다.', () => {
+    //given
+    const money = 10_000;
+
+    //when
+    const profitRate = profitCalculator.getProfitRate(money);
+
+    //then
+    expect(profitRate).toEqual(20_000_100);
+  });
 });
