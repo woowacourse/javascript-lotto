d09d6b74271c5375fdd09d6074a417ff497c6e42
yeopto <gunyeop530@gmail.com>
2023-02-14 15:55:00 +0900

    chore: lint, prettier를 설정하다

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
+    'no-alert': 'off',
+    'no-undef': 'off',
+    'no-new': 'off',
+    'lines-between-class-members': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+};
