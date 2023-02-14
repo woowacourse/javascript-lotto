1df76b66240a18119d3402da004b169926433a51
yeopto <gunyeop530@gmail.com>
2023-02-14 17:25:24 +0900

    test: 로또 배열 길이 유효성 검사 테스트 코드 작성하다

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,13 +1,17 @@
 /* eslint-disable */
+import Lotto from '../src/model/Lotto';
 
 describe('Lotto 단일 객체 테스트', () => {
+  const lotto = new Lotto();
   test('로또 숫자가 1~45 사이의 숫자인지 확인', () => {
-    const lotto = new Lotto();
-
     const lottoNum = lotto.lottoNum;
 
-    expect(() => {
-      lottoNum.every(num => num >= 1 && num <= 45);
-    }).toEqual(true);
+    expect(lottoNum.every(num => num >= 1 && num <= 45)).toBe(true);
+  });
+
+  test('로또 배열 길이가 6개인지 확인', () => {
+    const lottoNum = lotto.lottoNum;
+
+    expect(lottoNum.length === 6).toBe(true);
   });
 });
