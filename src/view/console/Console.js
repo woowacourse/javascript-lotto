e8edb1af8dc0442a97822dc9804d4be48c28dee8
cruelladevil <dev.timetravel@gmail.com>
2023-02-14 18:44:58 +0900

    feat: 콘솔 인터페이스 구현
    Co-authored-by: 2yunseong<dbsdltjd3701@naver.com>

---
+++ src/view/console/Console.js
@@ -0,0 +1,21 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const Console = {
+  async read(query) {
+    const answer = await rl.question(`> ${query}`);
+    return answer;
+  },
+
+  print(text) {
+    console.log(text);
+  },
+
+  close() {
+    rl.close();
+  },
+};
+
+export default Console;
