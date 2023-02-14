48fee4ef3df71e05a31e7d60497ed673de410aca
shackstack <kms1061@naver.com>
2023-02-14 17:56:26 +0900

    feat: 각 등수에 맞게 당첨된 로또의 수를 구한다.

--- src/domain/Comparer.js
+++ src/domain/Comparer.js
@@ -15,6 +15,31 @@ class Comparer {
   checkIncludesBonus() {
     return this.lottos.map((lotto) => lotto.includes(this.bonus));
   }
+
+  getRanking() {
+    const matchCount = this.countMatchesOfWinningNumber();
+    const bonus = this.checkIncludesBonus();
+    return matchCount.reduce(
+      (acc, cur, index) => {
+        const rank = this.getRank(cur, bonus[index]);
+        if (rank) acc[rank] += 1;
+        return acc;
+      },
+      { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
+    );
+  }
+
+  getRank(matchCount, hasBonus) {
+    if (matchCount === 5 && hasBonus) return 2;
+    return (
+      {
+        3: 5,
+        4: 4,
+        5: 3,
+        6: 1,
+      }[matchCount] ?? undefined
+    );
+  }
 }
 
 module.exports = Comparer;
