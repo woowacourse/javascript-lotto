b78ef7aca91067ba0abf1f487a5ce3dac43c175b
shackstack <kms1061@naver.com>
2023-02-14 16:09:31 +0900

    test: 구입 금액에 해당하는 만큼 구매갯수를 출력한다.
    Co-authored-by: Leejin Yang <ejin1996@gmail.com>

---
+++ __tests__/LottoMachine.test.js
@@ -0,0 +1,12 @@
+const LottoMachine = require('../src/domain/LottoMachine');
+
+test('구매 금액에 해당하는 만큼 로또를 발행한다.', () => {
+  // given
+  const money = 10_000;
+
+  // when
+  const lottoMachine = new LottoMachine(money);
+
+  // then
+  expect(lottoMachine.getQuantity()).toBe(10);
+});
