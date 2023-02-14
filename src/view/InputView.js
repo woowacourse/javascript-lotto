e77d26e40a3de346c6c21b0d29fc1fa084cee711
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-14 17:36:20 +0900

    feat: 로또 보너스 번호를 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -8,4 +8,8 @@ const InputView = {
   readLottoWinningNumber(callback) {
     Console.read('당첨 번호를 입력해 주세요.', callback);
   },
+
+  readLottoBonusNumber(callback) {
+    Console.read('보너스 번호를 입력해 주세요.', callback);
+  },
 };
