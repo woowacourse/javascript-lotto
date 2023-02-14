5b66d3aa9ce528201582bb6c3d9f987eba6a87ad
shackstack <kms1061@naver.com>
2023-02-14 16:24:59 +0900

    feat: 1~45의 중복되지 않은 로또를 생성한다.

--- src/domain/LottoMachine.js
+++ src/domain/LottoMachine.js
@@ -6,6 +6,20 @@ class LottoMachine {
   getQuantity() {
     return this.money / 1_000;
   }
+
+  issueLotto() {
+    const pickNumberInRange = (start, end) => {
+      return Math.floor(Math.random() * (end + 1 - start)) + start;
+    };
+
+    const lotto = new Set();
+
+    while (lotto.size < 6) {
+      lotto.add(pickNumberInRange(1, 45));
+    }
+
+    return [...lotto];
+  }
 }
 
 module.exports = LottoMachine;
