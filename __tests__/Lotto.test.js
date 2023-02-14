310afe9db2b15a187ae9e30be6be0d12afc6018c
solo5star <solo_5@naver.com>
2023-02-14 17:21:04 +0900

    test: 로또번호 당첨번호 비교 테스트 케이스 추가
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,13 +1,30 @@
 import Lotto from '../src/domain/Lotto';
 
 describe('로또 클래스 테스트', () => {
-  test('로또번호와 당첨번호를 비교하여 당첨 개수를 계산할 수 있다.', () => {
-    //given
-    const lottoNumbers = [1, 2, 3, 4, 5, 6];
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
-    //when
-    const lotto = new Lotto(lottoNumbers);
-    //then
-    expect(lotto.countMatchingNumbers(winningNumbers)).toBe(6);
-  });
+  test.each([
+    {
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      winningNumbers: [1, 2, 3, 4, 5, 6],
+      matchNumbers: 6,
+    },
+    {
+      lottoNumbers: [10, 11, 40, 41, 42, 43],
+      winningNumbers: [1, 2, 3, 4, 10, 11],
+      matchNumbers: 2,
+    },
+    {
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      winningNumbers: [40, 41, 42, 43, 44, 45],
+      matchNumbers: 0,
+    },
+  ])(
+    '로또번호와 당첨번호를 비교하여 당첨 개수를 계산할 수 있다.',
+    ({ lottoNumbers, winningNumbers, matchNumbers }) => {
+      // when
+      const lotto = new Lotto(lottoNumbers);
+
+      // then
+      expect(lotto.countMatchingNumbers(winningNumbers)).toBe(matchNumbers);
+    },
+  );
 });
