3538177c35247a9e2093f367cf2f9a0d1cf41486
chsua <csj1919@naver.com>
2023-02-14 18:22:28 +0900

    Test: 로또에 넣는 중복되지 않는 랜덤숫자 생성 테스트 작성

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,4 +1,5 @@
-const LottoGame = require("../src/LottoGame");
+const LottoGame = require("../src/domain/LottoGame");
+const Random = require("../src/utils/Random");
 
 describe("구매 금액 입력 시 복권 생성", () => {
   test("구매 금액 입력 시 복권 생성되는 케이스", () => {
@@ -13,5 +14,18 @@ describe("구매 금액 입력 시 복권 생성", () => {
     lottoGame.generateLotteries(7);
     const lottoSize = lottoGame.lotteries.length;
     expect(lottoSize).toEqual(7);
+  });
+});
+
+describe("로또에 넣을 랜덤숫자 생성되는지 확인", () => {
+  const randomNumbers = Random.generateRandomNumbers();
+  test("로또에 넣을 랜덤숫자가 여섯 개면 통과", () => {
+    const randomNumberLength = randomNumbers.length;
+    expect(randomNumberLength).toEqual(6);
+  });
+
+  test("로또에 넣을 랜덤숫자 내 중복이 없으면 통과", () => {
+    const randomNumberSet = new Set(randomNumbers);
+    expect(randomNumberSet.size).toEqual(6);
   });
 });
