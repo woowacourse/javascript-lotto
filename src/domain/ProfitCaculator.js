6ba2ae79b0194db8893c83339dcf63b8525829fe
shackstack <kms1061@naver.com>
2023-02-14 18:12:56 +0900

    feat: 전체 당첨 금액의 합을 구한다.

---
+++ src/domain/ProfitCaculator.js
@@ -0,0 +1,17 @@
+class ProfitCalculator {
+  constructor(ranking) {
+    this.ranking = ranking;
+  }
+
+  getWinningAmount() {
+    return (
+      this.ranking[1] * 2_000_000_000 +
+      this.ranking[2] * 30_000_000 +
+      this.ranking[3] * 1_500_000 +
+      this.ranking[4] * 50_000 +
+      this.ranking[5] * 5_000
+    );
+  }
+}
+
+module.exports = ProfitCalculator;
