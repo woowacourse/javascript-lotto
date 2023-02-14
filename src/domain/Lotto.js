f76610a79fe0a145f1fbbe46708450fc91acfa43
unknown <tkdrb15@gmail.com>
2023-02-14 17:57:49 +0900

    feat: validateLotto() 로또 번호 유효성 검사

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -8,6 +8,15 @@ class Lotto {
   get numbers() {
     return this.#numbers;
   }
+
+  validateLotto(numbers) {
+    if (
+      !numbers.every((num) => {
+        1 < num && num < 45;
+      })
+    )
+      throw new Error();
+  }
 }
 
 module.exports = Lotto;
