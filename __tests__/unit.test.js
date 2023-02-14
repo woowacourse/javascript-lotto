85ef252e4c06317293c4cae519a48dbcebf972bd
nlom0218 <nlom0218@naver.com>
2023-02-14 16:16:41 +0900

    test: 랜덤 숫자 범위 테스트 작성

---
+++ __tests__/unit.test.js
@@ -0,0 +1,8 @@
+describe("로또를 뽑는 기능", () => {
+  test("랜덤한 숫자가 범위 안에 있다.", () => {
+    const number = generateRandomNumber();
+
+    expect(number).toBeLessThan(46);
+    expect(number).toBeGreaterThan(0);
+  });
+});
