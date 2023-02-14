43343f47bea6c05f7a080406453060539b94e48d
gyeongza <gyeongza@naver.com>
2023-02-14 16:42:39 +0900

    feat : 각 로또의 객체를 생성하는 기능

---
+++ src/Lotto.js
@@ -0,0 +1,12 @@
+class Lotto {
+  #lottoNumbers;
+  constructor(lottoNumbers) {
+    this.#lottoNumbers = lottoNumbers;
+  }
+
+  getLottoNumbers() {
+    return this.#lottoNumbers;
+  }
+}
+
+export default Lotto;
