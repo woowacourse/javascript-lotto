8801c480a92a0e79826689af627174f32716fe39
solo5star <solo_5@naver.com>
2023-02-14 16:39:16 +0900

    test: 로또번호와 당첨번호를 비교하는 테스트 케이스 작성
    Co-authored-by: xodms0309 <xodms0309@naver.com>

---
+++ __tests__/Lotto.test.js
@@ -0,0 +1,11 @@
+describe('로또 클래스 테스트', () => {
+  test('로또번호와 당첨번호를 비교하여 당첨 개수를 계산할 수 있다.', () => {
+    //given
+    const lottoNumbers = [1, 2, 3, 4, 5, 6];
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    //when
+    const lotto = new Lotto(lottoNumbers);
+    //then
+    expect(lotto.countMatchingNumbers(winningNumbers)).toBe(6);
+  });
+});
