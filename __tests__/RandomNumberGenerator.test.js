14586fc373cf045d02493560b6473660bdf7fc52
ashleysyheo <ashleysyheo@gmail.com>
2023-02-14 17:50:09 +0900

    test: generate unique random numbers in range test

---
+++ __tests__/RandomNumberGenerator.test.js
@@ -0,0 +1,10 @@
+import generateRandomNumbersInRange from '../src/utils/RandomNumberGenerator'
+
+describe('Random Number Generator 테스트', () =>{
+  test('입력 갯수 만큼 중복되지 않는 랜덤 숫자 생성', () => {
+    const RANDOM_NUMBER = generateRandomNumbersInRange(1, 45, 6);
+
+    expect(RANDOM_NUMBER.length === new Set(RANDOM_NUMBER).size).toBe(true);
+
+  })
+})
