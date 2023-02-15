9fe171e024e17628bfdf47c8076558b8fbb4c037
shackstack <kms1061@naver.com>
2023-02-15 15:05:48 +0900

    feat: 당첨 번호를 입력받는다.

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,6 +5,11 @@ const InputView = {
     const purchaseAmount = await Console.readLine('구입금액을 입력해 주세요.');
     return purchaseAmount;
   },
+
+  async readWinnigNumber() {
+    const winningNumber = await Console.readLine('당첨 번호를 입력해 주세요.');
+    return winningNumber;
+  },
 };
 
 module.exports = InputView;
