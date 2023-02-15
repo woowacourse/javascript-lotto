dedf5b641ba7c0b6173530d89160c62622617ff5
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 16:36:41 +0900

    refactor: change from async/await to callback

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -7,59 +7,59 @@ import OutputView from "../view/OutputView.js";
 class LottoGameController {
   #lottoGame = new LottoGame();
 
-  async setupGame() {
-    await this.#handlePurchaseAmount();
-    this.#handleUserLottos();
-    await this.#handleWinningNumbers();
-    await this.#handleBonusNumber()
+  startGame() {
+    this.#handlePurchaseAmount();
   }
 
-  async #handlePurchaseAmount() {
-    const PURCHASE_AMOUNT = await InputView.readPurchaseAmount();
-
-    try {
-      Validation.checkPurchaseAmount(PURCHASE_AMOUNT);
-      const PURCHASE_COUNT = Number(PURCHASE_AMOUNT) / 1000;
-      this.#lottoGame.generateUserLottos(PURCHASE_COUNT);
-      OutputView.print(ConsoleMessage.purchaseCount(PURCHASE_COUNT));
-    } catch (error) {
-      this.#rerequestInput(error.message, this.#handlePurchaseAmount);
-    }
+  #handlePurchaseAmount() {
+    InputView.readPurchaseAmount((input) => {
+      try {
+        Validation.checkPurchaseAmount(input);
+        const PURCHASE_COUNT = Number(input) / 1000;
+        this.#lottoGame.generateUserLottos(PURCHASE_COUNT);
+        OutputView.print(ConsoleMessage.purchaseCount(PURCHASE_COUNT));
+        this.#handleUserLottos();
+      } catch (error) {
+        OutputView.print(error.message);
+        this.#handlePurchaseAmount();
+      }
+    });
   }
 
   #handleUserLottos() {
     const USER_LOTTOS = this.#lottoGame.getUserLottos();
 
     USER_LOTTOS.forEach(OutputView.printUserLottos);
+
+    this.#handleWinningNumbers();
   }
 
-  async #handleWinningNumbers() {
-    const WINNING_NUMBERS_INPUT = await InputView.readWinningNumbers();
-    const WINNING_NUMBERS = WINNING_NUMBERS_INPUT.split(",").map(Number);
+  #handleWinningNumbers() {
+    InputView.readWinningNumbers((input) => {
+      const WINNING_NUMBERS = input.split(",").map(Number);
 
-    try {
-      Validation.checkLottoNumber(WINNING_NUMBERS);
-      this.#lottoGame.setWinningNumbers(WINNING_NUMBERS);
-    } catch (error) {
-      this.#rerequestInput(error.message, this.#handleWinningNumbers);
-    }
+      try {
+        Validation.checkLottoNumber(WINNING_NUMBERS);
+        this.#handleBonusNumber(WINNING_NUMBERS);
+      } catch (error) {
+        OutputView.print(error.message);
+        this.#handleWinningNumbers();
+      }
+    });
   }
 
-  async #handleBonusNumber() {
-    const BONUS_NUMBER_INPUT = await InputView.readBonusNumber();
-    const BONUS_NUMBER = Number(BONUS_NUMBER_INPUT);
+  #handleBonusNumber(winningNumbers) {
+    InputView.readBonusNumber((input) => {
+      const BONUS_NUMBER = Number(input);
 
-    try {
-      Validation.checkBonusNumber(BONUS_NUMBER);
-      this.#lottoGame.setBonusNumber(BONUS_NUMBER);
-    } catch (error) {
-      this.#rerequestInput(error.message, this.#handleBonusNumber)
-    }
-  }
-
-  #rerequestInput(errorMessage, itself) {
-    OutputView.print(errorMessage);
-    return itself();
+      try {
+        Validation.checkBonusNumber(winningNumbers, BONUS_NUMBER);
+        this.#lottoGame.setGameLottos(winningNumbers, BONUS_NUMBER);
+      } catch (error) {
+        OutputView.print(error.message);
+        this.#handleBonusNumber(winningNumbers);
+      }
+    });
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,16 +2,22 @@ import { ConsoleMessage } from "../constants/Constants.js";
 import Console from "../utils/Console.js";
 
 const InputView = {
-  readPurchaseAmount() {
-    return Console.question(ConsoleMessage.PURCHASE_AMOUNT);
+  readPurchaseAmount(callback) {
+    return Console.question(ConsoleMessage.PURCHASE_AMOUNT, (input) =>
+      callback(input)
+    );
   },
 
-  readWinningNumbers() {
-    return Console.question(ConsoleMessage.WINNING_NUMBER);
+  readWinningNumbers(callback) {
+    return Console.question(ConsoleMessage.WINNING_NUMBER, (input) =>
+      callback(input)
+    );
   },
 
-  readBonusNumber() {
-    return Console.question(ConsoleMessage.BONUS_NUMBER);
+  readBonusNumber(callback) {
+    return Console.question(ConsoleMessage.BONUS_NUMBER, (input) =>
+      callback(input)
+    );
   },
 };
 
