5978d3f05cc61111403aed6eda7009d96cc8cd14
shackstack <kms1061@naver.com>
2023-02-15 15:14:41 +0900

    feat: 당첨 내역 및 수익률을 출력한다.

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -10,6 +10,20 @@ const OutputView = {
       Console.print(lotto);
     });
   },
+
+  printRanking(ranking) {
+    Console.print('당첨 통계');
+    Console.print('--------------------');
+    Console.print(`3개 일치 (5,000원) - ${ranking[5]}개`);
+    Console.print(`4개 일치 (50,000원) - ${ranking[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${ranking[3]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${ranking[2]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${ranking[1]}개`);
+  },
+
+  printProfitRate(rate) {
+    Console.print(`총 수익률은 ${rate.toFixed(1)}%입니다.`);
+  },
 };
 
 module.exports = OutputView;
