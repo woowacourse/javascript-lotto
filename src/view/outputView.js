5a459bb6d752a150e66843f07b787d9d06cd81f4
unknown <tkdrb15@gmail.com>
2023-02-15 16:47:49 +0900

    feat:printPurchaselottos() 발행 로또 출력

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -1,3 +1,13 @@
-const OutputView = {};
+const OutputView = {
+  printLottoCount(lottoCount) {
+    console.log(`${lottoCount}개를 구매했습니다.`);
+  },
+
+  printPurchaseLottos(lottos) {
+    const result = lottos.map((lotto) => `${lotto.numbers}\n`).join("");
+
+    console.log(result);
+  },
+};
 
 module.exports = OutputView;
