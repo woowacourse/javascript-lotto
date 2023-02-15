90f46fa8117375a1413e2b8c79481a5a47726875
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 19:15:31 +0900

    refactor: change purchase amount input message

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -4,7 +4,7 @@ const StaticValue = {
 };
 
 const ConsoleMessage = {
-  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.",
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요. ",
   purchaseCount: (count) => `${count}개를 구매했습니다.`,
   WINNING_NUMBER: "당첨 번호를 입력해 주세요. ",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요. ",
