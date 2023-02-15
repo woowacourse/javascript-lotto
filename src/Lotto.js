217d16c2915bb6cd41eafd8dbccd85d35ed51a17
gyeongza <gyeongza@naver.com>
2023-02-15 14:20:39 +0900

    feat : 당첨번호와 각 로또 객체 번호를 비교하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,31 @@
 class Lotto {
   #lottoNumbers;
+  #score;
   constructor(lottoNumbers) {
     this.#lottoNumbers = lottoNumbers;
+    this.#score = 0;
   }
 
   getLottoNumbers() {
     return this.#lottoNumbers;
   }
 
+  getScore() {
+    return this.#score;
+  }
+
   sortLottoNumbers() {
     this.#lottoNumbers.sort((a, b) => a - b);
-    console.log(this.#lottoNumbers);
+  }
+
+  compareNumbers(winningNumbers) {
+    winningNumbers.forEach((winningNumbers) => {
+      this.#lottoNumbers.includes(winningNumbers) && this.addScore();
+    });
+  }
+
+  addScore() {
+    this.#score += 1;
   }
 }
 
