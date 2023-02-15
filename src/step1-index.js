03b94dfcf8fa4a1d208c9e3e019d92d5d022b57b
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 14:35:36 +0900

    feat: create App class and add play method

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,4 +1,12 @@
-/**
- * step 1의 시작점이 되는 파일입니다.
- * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import LottoGameController from "./controller/LottoGameController.js";
+
+class App {
+  #lottoGameController = new LottoGameController();
+
+  play() {
+    this.#lottoGameController.setupGame();
+  }
+}
+
+const app = new App();
+app.play();
