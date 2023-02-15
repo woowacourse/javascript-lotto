5b8a4b90bf791101b689e53b4c4f4391eefc7a29
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 15:58:06 +0900

    feat: 로또 번호는 1~45의 자연수

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -9,7 +9,14 @@ const InputValidator = {
     if (input % unit !== 0) {
       throw new Error(`입력 값이 ${unit}원 단위가 아닙니다.`);
     }
-  }
+  },
+  checkLottoNumber(number) {
+    this.checkNaturalNumber(number);
+    if (number < 1 || number > 45) {
+      throw new Error('입력값이 1~45범위의 숫자가 아닙니다.');
+    }
+  },
+
 };
 
 export default InputValidator;
