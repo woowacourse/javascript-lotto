33a22220abc9be27c0554bd3ee3c6865be084716
yeopto <gunyeop530@gmail.com>
2023-02-15 14:55:07 +0900

    feat: 맞은 갯수를 당첨 금액으로 치환해주는 객체를 만들다

--- src/constants/values.js
+++ src/constants/values.js
@@ -1,6 +1,18 @@
+import deepFreeze from '../utils/deepFreeze';
+
 const values = Object.freeze({
   LOWER_BOUND: 1,
   UPPER_BOUND: 45,
 });
 
-export default values;
+const correctCountsToMoney = deepFreeze({
+  6: 2000000000,
+  5: [30000000, 1500000],
+  4: 1500000,
+  3: 5000,
+  2: 0,
+  1: 0,
+  0: 0,
+});
+
+export default { values, correctCountsToMoney };
