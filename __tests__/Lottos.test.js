a1f04ee4612acb7921237894b93e17a900d78d4b
gyeongza <gyeongza@naver.com>
2023-02-15 17:16:56 +0900

    test : 총 수익률을 계산하는 함수 테스트

--- __tests__/Lottos.test.js
+++ __tests__/Lottos.test.js
@@ -51,4 +51,21 @@ describe("Lotto 객체 단위테스트", () => {
 
     expect(lottos.getTotalBenefit()).toBe(2001550000);
   });
+
+  test("총 수익률을 계산하는 함수 테스트", () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto1.compareNumbers(winningNumbers);
+    const lotto2 = new Lotto([2, 3, 4, 5, 6, 7]);
+    lotto2.compareNumbers(winningNumbers);
+    const lotto3 = new Lotto([3, 4, 5, 6, 8, 9]);
+    lotto3.compareNumbers(winningNumbers);
+    const lottos = new Lottos([lotto1, lotto2, lotto3]);
+
+    lottos.compareLottosScore();
+    lottos.calculateBenefit();
+
+    expect(lottos.getBenefitRate(1000)).toBe(2001550);
+  });
 });
