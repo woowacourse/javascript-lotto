268b7ca0b7e139080c5c780a6c344cd07989e1be
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 21:11:02 +0900

    chore: wrap constants with Object.freeze

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,26 +1,7 @@
-const StaticValue = {
+const StaticValue = Object.freeze({
   PURCHASE_AMOUNT_UNIT: 1000,
   REGEX_NON_DIGIT: /\D|^$/,
-};
-
-const ConsoleMessage = {
-  PURCHASE_AMOUNT: "구입금액을 입력해 주세요. ",
-  purchaseCount: (count) => `${count}개를 구매했습니다.`,
-  WINNING_NUMBER: "당첨 번호를 입력해 주세요. ",
-  BONUS_NUMBER: "보너스 번호를 입력해 주세요. ",
-};
-
-const ErrorMessage = {
-  MONEY_VALUE: "[ERROR] 로또 구입 금액은 1000원 단위로 입력해 주세요.",
-  MINMUM_VALUE: "[ERROR] 로또 구입 금액은 1000원 이상이여야 합니다.",
-  MONEY_INPUT_TYPE: "[ERROR] 로또 구입 금액은 숫자만 입력해 주세요.",
-  LOTTO_VALUE: "[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.",
-  LOTTO_LENGTH: "[ERROR] 로또 당첨 번호 6개를 입력해주세요.",
-  LOTTO_DUPLICATE: "[ERROR] 로또 번호는 중복되지 않게 입력해주세요.",
-  BONUS_NUMBER_DUPLICATE:
-    "[ERROR] 보너스 번호는 로또 번호와 중복되지 않게 입력해주세요.",
-  BONUS_NUMBER_VALUE: "[ERROR] 보너스 번호는 1~45 사이의 숫자를 입력해주세요.",
-};
+});
 
 const Rank = Object.freeze({
   0: 6,
@@ -42,4 +23,23 @@ const Prize = Object.freeze({
   6: 0,
 });
 
-export { StaticValue, ConsoleMessage, ErrorMessage, Rank, Prize };
+const ConsoleMessage = Object.freeze({
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요. ",
+  purchaseCount: (count) => `${count}개를 구매했습니다.`,
+  WINNING_NUMBER: "당첨 번호를 입력해 주세요. ",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요. ",
+});
+
+const ErrorMessage = Object.freeze({
+  MONEY_VALUE: "[ERROR] 로또 구입 금액은 1000원 단위로 입력해 주세요.",
+  MINMUM_VALUE: "[ERROR] 로또 구입 금액은 1000원 이상이여야 합니다.",
+  MONEY_INPUT_TYPE: "[ERROR] 로또 구입 금액은 숫자만 입력해 주세요.",
+  LOTTO_VALUE: "[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.",
+  LOTTO_LENGTH: "[ERROR] 로또 당첨 번호 6개를 입력해주세요.",
+  LOTTO_DUPLICATE: "[ERROR] 로또 번호는 중복되지 않게 입력해주세요.",
+  BONUS_NUMBER_DUPLICATE:
+    "[ERROR] 보너스 번호는 로또 번호와 중복되지 않게 입력해주세요.",
+  BONUS_NUMBER_VALUE: "[ERROR] 보너스 번호는 1~45 사이의 숫자를 입력해주세요.",
+});
+
+export { StaticValue, Rank, Prize, ConsoleMessage, ErrorMessage };
