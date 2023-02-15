e92f13f30873fb1c0f4d77c34b7fc8b48ace1338
shackstack <kms1061@naver.com>
2023-02-15 14:47:03 +0900

    feat: 구매수량 출력기능

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,10 @@
 const Console = require('../util/Console');
 
 const OutputView = {
+  printPurchaseQuantity(quantity) {
+    Console.print(`${quantity}개를 구매했습니다.`);
+  },
+
   printLottos(lottos) {
     lottos.forEach((lotto) => {
       Console.print(lotto);
