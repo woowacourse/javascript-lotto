943f7edfdbed622b2a525c8708ef5aef17da4724
inyeong-kang <tomatorkd@naver.com>
2023-02-15 16:44:48 +0900

    feat(constants): 등수에 따라 수익을 보여주는 배열 상수 분리

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,11 +2,26 @@ const PRICE_UNIT = 1_000;
 const BLANK_REGEXP = /\s/;
 const EMPTY_STRING = '';
 
-const PROFIT = Object.freeze({
+const profit = Object.freeze({
   FIRST_RANK: 2_000_000_000,
   SECOND_RANK: 30_000_000,
   THIRD_RANK: 1_500_000,
   FOURTH_RANK: 50_000,
   FIFTH_RANK: 5_000,
 });
-module.exports = { PRICE_UNIT, BLANK_REGEXP, EMPTY_STRING, PROFIT };
+
+const profitByRank = Object.freeze([
+  profit.FIRST_RANK,
+  profit.SECOND_RANK,
+  profit.THIRD_RANK,
+  profit.FOURTH_RANK,
+  profit.FIFTH_RANK,
+]);
+
+module.exports = {
+  PRICE_UNIT,
+  BLANK_REGEXP,
+  EMPTY_STRING,
+  profit,
+  profitByRank,
+};
