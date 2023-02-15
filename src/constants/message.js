2abd9132af6c7bc26c6401b0c7419d7b4a4a91d0
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 16:20:59 +0900

    feat(message): 에러메세지 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -5,4 +5,10 @@ const REQUEST_MESSAGE = Object.freeze({
   restartCommand: `다시 시작하시겠습니까? (y/n)\n`,
 });
 
-export { REQUEST_MESSAGE };
+const ERROR_MESSAGE = Object.freeze({
+  invalidInputType: '[ERROR] 숫자 외의 문자를 입력할 수 없습니다.',
+  lowerThanLottoPrice: '[ERROR] 로또 1장 가격 미만의 금액을 입력 하셨습니다.',
+  indivisibleByLottoPrice: '[ERROR] 입력값이 로또 가격으로 나누어 떨어지지 않습니다.',
+});
+
+export { REQUEST_MESSAGE, ERROR_MESSAGE };
