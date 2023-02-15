c450b1811d7a279d357c56ea638a81b6ae98c978
Patrick <kuyt3122@naver.com>
2023-02-15 19:06:39 +0900

    ✨ feat: 일치된 숫자가 몇 등인지 확인하는 기능 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,8 +1,8 @@
 class Lotto {
-  constructor(money) {
-    if (money%1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
+  constructor(money, number) {
+    if (money % 1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
     this.lottoMoney = money;
-    this.lottoNumber = [];
+    this.lottoNumber = number;
   }
 
   countLotto(money) {
@@ -15,18 +15,31 @@ class Lotto {
   }
 
   sortedNumber(number) {
-    return number.sort((a, b)=>a-b)
+    return number.sort((a, b) => a - b);
   }
 
   randomNumberLotto() {
-    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46)
-    );
-    if (this.compareSameNumberInLotto(randomNumber)) return this.sortedNumber(randomNumber);
+    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46));
+    if (this.checkRepeatedNumber(randomNumber)) return this.sortedNumber(randomNumber);
     this.randomNumberLotto();
   }
 
-  compareSameNumberInLotto(randomNumber) {
-    return [...new Set(randomNumber)].length === 6
+  checkRepeatedNumber(randomNumber) {
+    return [...new Set(randomNumber)].length === 6;
+  }
+
+  compareNumber(winningNumber, bonusNumber) {
+    const ranks = [];
+    this.lottoNumber.forEach(numbers => {
+      const matchedNumber = numbers.filter(number => winningNumber.includes(number)).length;
+      if (matchedNumber === 5) return ranks.push(this.bouseNumberChecked(numbers, bonusNumber));
+      if (matchedNumber >= 3) ranks.push(matchedNumber);
+    });
+    return ranks;
+  }
+  bouseNumberChecked(numbers, bonusNumber) {
+    if (numbers.includes(bonusNumber)) return 7;
+    return 5;
   }
 }
 
