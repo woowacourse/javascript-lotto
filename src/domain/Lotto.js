e4a206c77d58af0a3c409e7bed256d623ec295b7
Patrick <kuyt3122@naver.com>
2023-02-15 16:32:21 +0900

    ✨ feat: 한 장당 부여된 번호의 중복 여부를 확인하는 기능 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,16 +1,19 @@
 class Lotto {
   constructor(money, number) {
-    if (money%1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
+    if (money % 1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
     this.lottoMoney = money;
     this.lottoNumber = number;
   }
 
   randomNumberLotto() {
-    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46)
-    );
-    return randomNumber.every((number)=>number>=1 && number<=45)
+    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46));
+    if (this.compareSameNumberInLotto()) return randomNumber;
+    this.randomNumberLotto();
   }
 
+  compareSameNumberInLotto(randomNumber) {
+    return [...new Set(randomNumber)].length === 6;
+  }
 }
 
 export default Lotto;
