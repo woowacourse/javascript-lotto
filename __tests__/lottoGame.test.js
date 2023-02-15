a1336e3e05938ce652da6f7070d43f161d47de5f
Q Kim <qoli0703@gmail.com>
2023-02-15 17:04:26 +0900

    test: 거스름돈 계산 테스트

---
+++ __tests__/lottoGame.test.js
@@ -0,0 +1,11 @@
+import LottoGame from '../src/domain/LottoGame';
+
+describe('거스름돈 계산하기', () => {
+  test('구입 금액 8700인 경우', () => {
+    expect(LottoGame.calculateTheChange(8700)).toBe(700);
+  });
+
+  test('구입 금액 8000인 경우', () => {
+    expect(LottoGame.calculateTheChange(8000)).toBe(0);
+  });
+});
