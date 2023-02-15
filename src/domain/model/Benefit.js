17f1417b14f0c431ef125b7d23057e8c5e32b63e
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 18:13:51 +0900

    ✨ feat(Benefit): 수익률을 계산한다

--- src/domain/model/Benefit.js
+++ src/domain/model/Benefit.js
@@ -1,8 +1,21 @@
+const { MAGIC_NUMBER } = require('../../constant');
+
 class Benefit {
   #rate;
 
   getRate() {
     return this.#rate;
+  }
+
+  calculateRate(money, ranks) {
+    const rewards = Object.values(MAGIC_NUMBER.reward);
+
+    const total = ranks.reduce(
+      (accumulator, rank, index) => accumulator + rank * rewards[index],
+      0
+    );
+
+    this.#rate = (total / money) * 100;
   }
 }
 
