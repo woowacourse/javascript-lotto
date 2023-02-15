7e7fd597608720cee31a2daab613db2179a177b2
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 22:45:53 +0900

    feat(message): 에러 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,3 +1,5 @@
+import { LOTTO_CONDITION } from './condition.js';
+
 const REQUEST_MESSAGE = Object.freeze({
   purchaseAmount: '구입금액을 입력해 주세요.\n',
   winningNumbers: '당첨 번호를 입력해 주세요.\n',
@@ -9,6 +11,8 @@ const ERROR_MESSAGE = Object.freeze({
   invalidInputType: '[ERROR] 숫자 외의 문자를 입력할 수 없습니다.',
   lowerThanLottoPrice: '[ERROR] 로또 1장 가격 미만의 금액을 입력 하셨습니다.',
   indivisibleByLottoPrice: '[ERROR] 입력값이 로또 가격으로 나누어 떨어지지 않습니다.',
+  invalidLottoNumberLength: `[ERROR] 로또 번호는 ${LOTTO_CONDITION.lottoDigits}개여야 합니다.`,
+  invalidLottoNumberRange: `[ERROR] 로또 번호의 범위는 ${LOTTO_CONDITION.lottoNumberMinRange} ~ ${LOTTO_CONDITION.lottoNumberMaxRange} 이여야 합니다.`,
 });
 
 export { REQUEST_MESSAGE, ERROR_MESSAGE };
