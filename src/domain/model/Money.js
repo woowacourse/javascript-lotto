c9c5f4d657ed133c1bcca2376dc6b46cfbf3208f
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 17:38:56 +0900

    ✨ feat(Money): 구매 금액이 유효한지 확인한다
    양수인 정수인지, 1000원 단위인지, 10만원을 넘지 않는지 확인한다

--- src/domain/model/Money.js
+++ src/domain/model/Money.js
@@ -1,12 +1,28 @@
+const utilFn = require('../../utils');
+
 class Money {
   #amount;
 
   constructor(amount) {
+    this.#validateAmount(amount);
     this.#amount = amount;
   }
 
   getAmount() {
     return this.#amount;
+  }
+
+  #validateAmount(amount) {
+    if (amount > 100000 || amount < 1000) {
+      throw new Error('[ERROR]');
+    }
+    if (!utilFn.isNumber(amount)) {
+      throw new Error('[ERROR]');
+    }
+
+    if (amount % 1000 !== 0) {
+      throw new Error('[ERROR]');
+    }
   }
 }
 
