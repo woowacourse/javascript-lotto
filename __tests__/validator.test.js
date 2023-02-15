a43fdd1dcd58e326dc48b973a3e99d1ab917ddc9
Q Kim <qoli0703@gmail.com>
2023-02-15 14:20:53 +0900

    test: 자연수 확인 기능 테스트 추가

---
+++ __tests__/validator.test.js
@@ -0,0 +1,18 @@
+import Validator from '../src/util/Validator';
+
+describe('숫자가 자연수가 맞는지 확인하는 기능', () => {
+  test.each(['1004', '1', '+3000'])('%s은(는) true를 반환한다', (input) => {
+    expect(Validator.isPositiveInteger(input)).toBeTruthy();
+  });
+
+  test.each([
+    ['0x123', '십진법이 아님'],
+    ['1000.4', 'floating point임'],
+    ['-1000', '음수임'],
+    ['타우린', '문자열임'],
+    ['1234567890987654321', 'Number 자료형 범위 초과'],
+    ['0', '자연수가 아님'],
+  ])('%s은(는) false를 반환한다(%s)', (input) => {
+    expect(Validator.isPositiveInteger(input)).toBeFalsy();
+  });
+});
