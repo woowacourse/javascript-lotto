df486cc07e73a749dbe8a785398f10cb28ab6659
solo5star <solo_5@naver.com>
2023-02-15 09:13:37 +0000

    test: Winning Lotto 객체를 새로 만듦에 따라 테스트 수정
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- __tests__/Reward.test.js
+++ __tests__/Reward.test.js
@@ -1,6 +1,6 @@
 import Lotto from '../src/domain/Lotto';
-import LottoResult from '../src/domain/LottoResult';
 import Reward from '../src/domain/Reward';
+import WinningLotto from '../src/domain/WinningLotto';
 
 describe('Reward 클래스에 대한 테스트 작성', () => {
   test('로또가 주어졌을 때 Reward에 자격이 있는 지 검사한다.', () => {
@@ -8,14 +8,14 @@ describe('Reward 클래스에 대한 테스트 작성', () => {
     const lotto = new Lotto([1, 2, 3, 4, 7, 45]);
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
-    const lottoResult = new LottoResult(winningNumbers, bonusNumber);
+    const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
 
     const matchingNumbers = 4;
     const money = 30_000;
     const reward = new Reward(matchingNumbers, money);
 
     // when
-    const canReceiveReward = lottoResult.canReceiveReward(lotto, reward);
+    const canReceiveReward = reward.canReceive(lotto, winningLotto);
 
     // then
     expect(canReceiveReward).toBeTruthy();
