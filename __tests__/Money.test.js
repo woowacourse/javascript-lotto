1b32f8fee60f5d6f578a627e347ceedd301d0f1d
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 18:20:20 +0900

    ✅ test(Money): 구매 금액을 반환하고, 유효성을 확인한다

---
+++ __tests__/Money.test.js
@@ -0,0 +1,41 @@
+const Money = require('../src/domain/model/Money');
+
+describe('Money class 기능 테스트', () => {
+  test('구매 금액을 반환한다.', () => {
+    const AMOUNT = 10000;
+    const money = new Money(AMOUNT);
+
+    const result = money.getAmount();
+
+    expect(result).toBe(AMOUNT);
+  });
+});
+
+describe('Money class 유효성 테스트', () => {
+  test.each([[10000.5], [-1000], [0], ['라잇']])(
+    '구매 금액이 양수인 정수가 아닐 경우 에러를 발생시킨다',
+    (amount) => {
+      expect(() => {
+        const money = new Money(amount);
+      }).toThrow('[ERROR]');
+    }
+  );
+
+  test.each([[10006], [1500]])(
+    '구매 금액이 천원 단위가 아닐 경우 에러를 발생시킨다',
+    (amount) => {
+      expect(() => {
+        const money = new Money(amount);
+      }).toThrow('[ERROR]');
+    }
+  );
+
+  test.each([[101000], [1000000]])(
+    '구매 금액이 십만원을 넘을 경우 에러를 발생시킨다',
+    (amount) => {
+      expect(() => {
+        const money = new Money(amount);
+      }).toThrow('[ERROR]');
+    }
+  );
+});
