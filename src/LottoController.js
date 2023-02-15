7e7c1201ed465e3f3e5eaa712716ee1a7be4d261
cruelladevil <dev.timetravel@gmail.com>
2023-02-15 20:48:14 +0900

    feat: LottoController 클래스 구현
    Co-authored-by: 2yunseong <dbsdltjd3701@naver.com>

---
+++ src/LottoController.js
@@ -0,0 +1,49 @@
+import LottoMachine from './domain/LottoMachine';
+import InputView from './view/console/InputView';
+import OutputView from './view/console/OutputView';
+
+class LottoController {
+  #lottoMachine;
+
+  async readBuyMoney() {
+    try {
+      const money = await InputView.readBuyMoney();
+      this.#lottoMachine = new LottoMachine(money);
+    } catch (error) {
+      console.error(error.message);
+      await this.readBuyMoney();
+    }
+  }
+
+  printBuyLottos() {
+    const lottoNumbers = this.#lottoMachine.getLottoNumbers();
+    OutputView.printBuyLottos(lottoNumbers);
+  }
+
+  async readWinNumbers() {
+    try {
+      const winNumbers = await InputView.readWinNumbers();
+      this.#lottoMachine.setWinningLotto(winNumbers);
+    } catch (error) {
+      console.log(error.message);
+      await this.readWinNumbers();
+    }
+  }
+
+  async readBonusNumber() {
+    try {
+      const bonusNumber = await InputView.readBonusNumber();
+      this.#lottoMachine.setBonusNumber(bonusNumber);
+    } catch (error) {
+      console.error(error.message);
+      await this.readBonusNumber();
+    }
+  }
+
+  printWinStatistics() {
+    const prizes = this.#lottoMachine.getPrizes();
+    OutputView.printWinStatistics(prizes);
+  }
+}
+
+export default LottoController;
