69be13fc3d2bd784203d8ebbe65e53a17ae56fda
Q Kim <qoli0703@gmail.com>
2023-02-15 14:45:50 +0900

    test: number 범위 초과 테스트 추가

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -10,9 +10,16 @@ describe('숫자가 자연수가 맞는지 확인하는 기능', () => {
     ['1000.4', 'floating point임'],
     ['-1000', '음수임'],
     ['타우린', '문자열임'],
-    ['1234567890987654321', 'Number 자료형 범위 초과'],
     ['0', '자연수가 아님'],
   ])('%s은(는) false를 반환한다(%s)', (input) => {
     expect(Validator.isPositiveInteger(input)).toBeFalsy();
+  });
+
+  test('Number 자료형 범위 초과', () => {
+    expect(Validator.isSafeNumber('123456789011121314')).toBeFalsy();
+  });
+
+  test('Number 자료형 범위 안', () => {
+    expect(Validator.isSafeNumber('1234')).toBeTruthy();
   });
 });
