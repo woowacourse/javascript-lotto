d81b4118f5db3f6589c7e2f11257c2d007397d7d
gyeongza <gyeongza@naver.com>
2023-02-15 16:19:27 +0900

    feat : 일치하는 개수에 따른 등수를 산정하는 기능 구현

--- src/Lottos.js
+++ src/Lottos.js
@@ -1,11 +1,39 @@
 class Lottos {
   #lottos;
+  #lottoRanking;
   constructor(lottos) {
     this.#lottos = lottos;
+    this.#lottoRanking = {
+      3: 0,
+      4: 0,
+      5: 0,
+      "5 bonus": 0,
+      6: 0,
+    };
   }
 
   getLottos() {
     return this.#lottos;
+  }
+
+  getLottoRanking() {
+    return this.#lottoRanking;
+  }
+
+  compareLottosScore() {
+    this.#lottos.forEach((lotto) => {
+      lotto.getScore() === 5
+        ? (lotto.getIsContainBonusNumber()
+          ? this.addScoreBoard("5 bonus")
+          : this.addScoreBoard("5"))
+        : this.addScoreBoard(lotto.getScore());
+    });
+  }
+
+
+
+  addScoreBoard(score) {
+    this.#lottoRanking[score] += 1;
   }
 }
 
