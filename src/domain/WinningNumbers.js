aa34468d004fdf49e94087c57380bd2ff90cef2c
turtle601 <turtle601@naver.com>
2023-02-15 13:42:50 +0900

    refactor(WinningNumbers): WinningNumbers를 객체 리터럴 -> 클래스 변경

--- src/domain/WinningNumbers.js
+++ src/domain/WinningNumbers.js
@@ -1,22 +1,54 @@
-const WinningNumbers = {
-  validateWinningNumbers(winningNumbers) {
-    if (!this.isLottoNumbersLength(winningNumbers)) {
+class WinningNumbers {
+  #winningNumbers;
+
+  constructor(winningNumbers) {
+    this.#validateWinningNumbers(winningNumbers);
+    this.#winningNumbers = winningNumbers.split(',');
+  }
+
+  #validateWinningNumbers(winningNumbers) {
+    if (!this.#isLottoNumbersLength(winningNumbers)) {
       throw new Error(
         '[ERROR] 로또 당첨 번호는 1 ~ 45 범위 내에서만 가능합니다.'
       );
     }
-  },
+  }
 
-  isLottoNumber(number) {
+  #isLottoNumber(number) {
     return number >= 1 && number <= 45;
-  },
+  }
 
-  isLottoNumbersLength(winningNumbers) {
+  #isLottoNumbersLength(winningNumbers) {
     return (
-      winningNumbers.split(',').filter((number) => this.isLottoNumber(number))
+      winningNumbers.split(',').filter((number) => this.#isLottoNumber(number))
         .length === 6
     );
-  },
-};
+  }
+}
 
 module.exports = WinningNumbers;
+
+// const WinningNumbers = {
+//   // 필드: 당첨번호
+
+//   validateWinningNumbers(winningNumbers) {
+//     if (!this.isLottoNumbersLength(winningNumbers)) {
+//       throw new Error(
+//         '[ERROR] 로또 당첨 번호는 1 ~ 45 범위 내에서만 가능합니다.'
+//       );
+//     }
+//   },
+
+//   isLottoNumber(number) {
+//     return number >= 1 && number <= 45;
+//   },
+
+//   isLottoNumbersLength(winningNumbers) {
+//     return (
+//       winningNumbers.split(',').filter((number) => this.isLottoNumber(number))
+//         .length === 6
+//     );
+//   },
+// };
+
+// module.exports = WinningNumbers;
