7c324a1c1555e4cd01d589190a96d8db0f5fd9e1
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 16:50:03 +0900

    feat(InputView): 입력 요청 기능 일괄 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,3 +1,22 @@
-const InputView = {};
+import Console from '../utils/Console.js';
+import { REQUEST_MESSAGE } from '../constants/message.js';
+
+const InputView = {
+  readPurchaseAmount() {
+    return Console.question(REQUEST_MESSAGE.readPurchaseAmount);
+  },
+
+  readWinningNumbers() {
+    return Console.question(REQUEST_MESSAGE.winningNumbers);
+  },
+
+  readBonusNumber() {
+    return Console.question(REQUEST_MESSAGE.bonusNumber);
+  },
+
+  readRestartCommand() {
+    return Console.question(REQUEST_MESSAGE.restartCommand);
+  },
+};
 
 export default InputView;
