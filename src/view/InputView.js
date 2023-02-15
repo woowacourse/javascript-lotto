43ebf3cde43c2c77570ee170f0e1f6818eeee3cc
shackstack <kms1061@naver.com>
2023-02-15 15:06:56 +0900

    feat: 보너스 번호를 입력받는다.

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -10,6 +10,11 @@ const InputView = {
     const winningNumber = await Console.readLine('당첨 번호를 입력해 주세요.');
     return winningNumber;
   },
+
+  async readBonusNumber() {
+    const bonusNumber = await Console.readLine('보너스 번호를 입력해 주세요.');
+    return bonusNumber;
+  },
 };
 
 module.exports = InputView;
