bcd69ad47af931ac71697b41a153524b005225bc
solo5star <solo_5@naver.com>
2023-02-15 05:38:51 +0000

    fix: 빠진 import문 추가
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- __tests__/LottoFactory.test.js
+++ __tests__/LottoFactory.test.js
@@ -1,4 +1,5 @@
 import Lotto from '../src/domain/Lotto';
+import LottoFactory from '../src/domain/LottoFactory';
 
 describe('LottoFactory 클래스 테스트', () => {
   test('랜덤한 로또를 한 장 생성할 수 있어야 한다.', () => {
