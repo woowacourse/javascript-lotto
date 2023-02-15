707ba2315bf26652b0efb08659dc1047f140b8bd
turtle601 <turtle601@naver.com>
2023-02-15 14:09:26 +0900

    feat(BonusNumber): 보너스 번호 유효성 검사 기능 구현

---
+++ src/domain/BonusNumber.js
@@ -0,0 +1,20 @@
+class BonusNumber {
+  #bonusNumber = 0;
+
+  constructor(bonusNumberInput) {
+    this.#validate(parseInt(bonusNumberInput, 10));
+    this.#bonusNumber = parseInt(bonusNumberInput, 10);
+  }
+
+  #validate(bonusNumber) {
+    if (!this.#isValidBonusNumber(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 1 ~ 45 이내의 숫자여야 합니다.');
+    }
+  }
+
+  #isValidBonusNumber(bonusNumber) {
+    return bonusNumber >= 1 && bonusNumber <= 45;
+  }
+}
+
+module.exports = BonusNumber;
