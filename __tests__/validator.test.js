d70aa39b723779ce4475f032866a86b2e29bff29
Dain Lee <feb.dain@gmail.com>
2023-02-15 18:00:04 +0900

    feat: 로또 당첨 번호 입력 예외 처리 기능 추가

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,4 +1,10 @@
-import { checkPurchaseAmount, checkIsInteger } from "../src/step1-index";
+import {
+  checkPurchaseAmount,
+  checkIsInteger,
+  checkIsDuplicated,
+  checkIsBetween1And45,
+  checkListLengthIsSix,
+} from "../src/step1-index";
 
 test.each([
   [1000, true],
@@ -29,4 +35,37 @@ test.each([
   expected
     ? expect(() => checkIsInteger(purchaseAmount)).not.toThrow()
     : expect(() => checkIsInteger(purchaseAmount)).toThrow();
+});
+
+test.each([
+  [[1, 1, 2, 3, 4, 5], false],
+  [[1, 2, 3, 4, 5, 2], false],
+  [[1, 6, 2, 3, 4, 5], true],
+])("로또 번호(%p)에 중복이 있을 경우 에러를 반환한다.", (winningLottoNumbers, expected) => {
+  expected
+    ? expect(() => checkIsDuplicated(winningLottoNumbers)).not.toThrow()
+    : expect(() => checkIsDuplicated(winningLottoNumbers)).toThrow();
+});
+
+test.each([
+  [[1, 1, 2, 3, 4, 46], false],
+  [[0, 2, 3, 4, 5, 2], false],
+  [[1, 6, 2, 3, 4, 5], true],
+])(
+  "로또 번호(%p) 중 1 ~ 45 사이가 아닌 숫자가 있을 경우 에러를 반환한다.",
+  (winningLottoNumbers, expected) => {
+    expected
+      ? expect(() => checkIsBetween1And45(winningLottoNumbers)).not.toThrow()
+      : expect(() => checkIsBetween1And45(winningLottoNumbers)).toThrow();
+  }
+);
+
+test.each([
+  [[1, 2, 3, 4, 5], false],
+  [[1, 2, 3, 4, 5, 20, 30], false],
+  [[1, 6, 2, 3, 4, 5], true],
+])("로또 번호(%p)가 6개가 아닐 경우 에러를 반환한다.", (winningLottoNumbers, expected) => {
+  expected
+    ? expect(() => checkListLengthIsSix(winningLottoNumbers)).not.toThrow()
+    : expect(() => checkListLengthIsSix(winningLottoNumbers)).toThrow();
 });
