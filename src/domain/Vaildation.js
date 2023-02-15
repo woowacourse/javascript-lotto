d71a082807f39672fa8b81268dd8c4b4f0fdad4b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 15:47:58 +0900

    feat(Validation): 구입금액이 로또 1장 가격 이상인지 검증하는 기능 추가

--- src/domain/Vaildation.js
+++ src/domain/Vaildation.js
@@ -1,6 +1,12 @@
+import { LOTTO_CONDITION } from '../constants/condition.js';
+
 const Validation = {
   isNumber(purchaseAmount) {
     return Number.isInteger(Number(purchaseAmount));
+  },
+
+  isHigherThanLottoPrice(purchaseAmount) {
+    return purchaseAmount >= LOTTO_CONDITION.lottoPrice;
   },
 };
 
