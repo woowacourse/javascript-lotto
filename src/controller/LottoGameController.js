697a024dc1514b1e70d181ff94743feb82bad5be
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 14:32:18 +0900

    feat: setup game

---
+++ src/controller/LottoGameController.js
@@ -0,0 +1,20 @@
+import LottoGame from "../domain/LottoGame.js";
+import Validation from "../utils/Validation.js";
+import InputView from "../view/InputView.js";
+import Outputview from "../view/OutputView.js";
+
+class LottoGameController {
+  #lottoGame = new LottoGame();
+
+  async setupGame() {
+    const PURCHASE_AMOUNT = await InputView.readPurchaseAmount();
+    try {
+      Validation.checkPurchaseAmount(PURCHASE_AMOUNT);
+      this.#lottoGame.generateUserLottos(PURCHASE_AMOUNT / 1000);
+    } catch (error) {
+      Outputview.print(error.message);
+      return this.setupGame();
+    }
+  }
+}
+export default LottoGameController;
