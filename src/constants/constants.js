310cf7593d6a5cfaf50d4f5329fb49321140b121
inyeong-kang <tomatorkd@naver.com>
2023-02-15 14:46:05 +0900

    feat(constants): 로또 게임 관련 상수 분리

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 const PRICE_UNIT = 1000;
 const BLANK_REGEXP = /\s/;
+const EMPTY_STRING = '';
 
-module.exports = {PRICE_UNIT, BLANK_REGEXP};
+module.exports = { PRICE_UNIT, BLANK_REGEXP, EMPTY_STRING };
