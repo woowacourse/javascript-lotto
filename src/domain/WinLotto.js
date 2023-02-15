819110ce6ff7084e00427a91905d86ade04f6894
unknown <tkdrb15@gmail.com>
2023-02-15 15:27:37 +0900

    feat: calculateWinLotto() 당첨 등수 계산

--- src/domain/WinLotto.js
+++ src/domain/WinLotto.js
@@ -5,6 +5,7 @@ class WinLotto extends Lotto {
 
   constructor(numbers, bonusNumber) {
     super(numbers);
+    this.validateBonusNumber(bonusNumber);
     this.#bonusNumber = bonusNumber;
   }
 
@@ -13,9 +14,32 @@ class WinLotto extends Lotto {
     if (super.numbers.includes(bonusNumber)) throw new Error();
   }
 
+  //calculateWinLotto(lottos){
+  // const sameNumbers = lottos.map((lotto)=> {
+  //     sameNumber = lotto.numbers.filter(num => super.numbers.includes(num));
+
+  //     return sameNumber.length;
+  // });
+  // }
+  //}
+
+  calculateWinLotto(lotto) {
+    const sameNumbers = lotto.numbers.filter((num) =>
+      super.numbers.includes(num)
+    );
+    if (sameNumbers.length === 6) return 1;
+    if (sameNumbers.length === 5)
+      return lotto.numbers.includes(this.bonusNumber) ? 2 : 3;
+    if (sameNumbers.length === 4) return 4;
+    if (sameNumbers.length === 3) return 5;
+    if (sameNumbers.length < 3) return 0;
+  }
+
   get bonusNumber() {
     return this.#bonusNumber;
   }
 }
+
+[];
 
 module.exports = WinLotto;
