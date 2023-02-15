5c5376eec330ca5a02caf970b51da01a7b7ead33
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 17:00:17 +0900

    feat: 구입금액 입력 요청 기능 구현

--- src/domain/LottoGameController.js
+++ src/domain/LottoGameController.js
@@ -1,3 +1,13 @@
+import InputView from '../view/InputView.js';
+import Validation from './Vaildation.js';
+
 export default class LottoGameController {
-  play() {}
+  play() {
+    this.#requestPurchaseAmount();
+  }
+
+  async #requestPurchaseAmount() {
+    const purchaseAmount = await InputView.readPurchaseAmount();
+    Validation.isValidPurchaseAmount(purchaseAmount);
+  }
 }
