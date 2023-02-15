cf9f8c64224280f6c7459aeeee31e5618ef59d95
chsua <csj1919@naver.com>
2023-02-15 17:17:03 +0900

    feat: 컨드롤러 생성

--- src/Controller.js
+++ src/Controller.js
@@ -4,6 +4,8 @@ const LottoGame = require("./domain/LottoGame");
 const Validation = require("./Validation");
 
 class Controller {
+  #lottoNumbers;
+
   constructor() {
     this.inputPurchaseAmount();
   }
@@ -15,11 +17,46 @@ class Controller {
   purchaseAmountHandler(amount) {
     try {
       Validation.purchaseAmount(+amount);
-      const lottoGame = new LottoGame(amount);
+      const lottoGame = new LottoGame(+amount);
       OutputView.printLotteries(lottoGame.getLotteries());
+      this.inputLottoNumbers();
     } catch (error) {
       OutputView.printError(error);
       this.inputPurchaseAmount();
+    }
+  }
+
+  inputLottoNumbers() {
+    InputView.readLottoNumbers(this.lottoNumbersHandler.bind(this));
+  }
+
+  lottoNumbersHandler(lottoNumbers) {
+    try {
+      this.convertLotto(lottoNumbers);
+      Validation.lottoNumbers(this.#lottoNumbers);
+      this.inputBonusNumber();
+    } catch (error) {
+      OutputView.printError(error);
+      this.inputLottoNumbers();
+    }
+  }
+
+  convertLotto(lottoNumbers) {
+    this.#lottoNumbers = lottoNumbers
+      .split(",")
+      .map((number) => +number.trim());
+  }
+
+  inputBonusNumber() {
+    InputView.readBonusNumber(this.bonusNumberHandler.bind(this));
+  }
+
+  bonusNumberHandler(bonusNumber) {
+    try {
+      Validation.bonusNumber(this.#lottoNumbers, +bonusNumber);
+    } catch (error) {
+      OutputView.printError(error);
+      this.inputBonusNumber();
     }
   }
 }
