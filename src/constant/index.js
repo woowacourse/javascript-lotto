f353af4f062df849eda0b9d3869a17eb3918d1d9
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 18:22:24 +0900

    ✨ feat(constant): 상금 금액 상수화

---
+++ src/constant/index.js
@@ -0,0 +1,13 @@
+const MAGIC_NUMBER = {
+  reward: {
+    first: 2_000_000_000,
+    second: 30_000_000,
+    third: 1_500_000,
+    fourth: 50_000,
+    fifth: 5_000,
+  },
+};
+
+module.exports = {
+  MAGIC_NUMBER,
+};
