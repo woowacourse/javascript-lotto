80856a579a3202deacd8c7defd9e4d085ec32fff
Jungwoo <evencoding@gmail.com>
2023-02-15 14:56:11 +0900

    feat: 입력받은 당첨 번호에 대한 유효성 검사 구현

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -19,3 +19,21 @@ describe('입력받은 로또 구매 비용에 대한 유효성 검사 테스트
     expect(() => validator.throwErrorIfNotDivisiable(number, divisor)).toThrow();
   });
 });
+
+describe('당첨 번호에 대한 유효성 검사', () => {
+  test('로또 번호가 ,로 구분되는 6개의 1 이상 45 이하의 정수가 아닐 경우 에러가 발생한다.', () => {
+    // Given
+    const winningLotto = '1,45,102,63,9,8';
+
+    // Then
+    expect(() => validator.throwErrorIfWinningLottoInvalid(winningLotto)).toThrow();
+  });
+
+  test('중복되는 로또 번호가 있다면 에러가 발생한다.', () => {
+    // Given
+    const winningLotto = '1,1,2,3,4,5';
+
+    // Then
+    expect(() => validator.throwErrorIfHaveDuplicates(winningLotto)).toThrow();
+  });
+});
--- src/domain/validator.js
+++ src/domain/validator.js
@@ -12,6 +12,23 @@ const validator = {
       throw new Error(`[ERROR] 구입 금액은 ${divisor}원 단위여야 합니다`);
     }
   },
+
+  throwErrorIfWinningLottoInvalid(winningLotto) {
+    const isWinningLottoValid = /^(([1-9]|[1-3]\d|4[0-5]),){5}([1-9]|[1-3]\d|4[0-5])$/;
+
+    if (!isWinningLottoValid.test(winningLotto)) {
+      throw new Error('[ERROR] 로또는 콤마(,)로 구분되는 6개의 1 이상 45 이하의 정수여야 합니다.');
+    }
+  },
+
+  throwErrorIfHaveDuplicates(winningLotto) {
+    const numbers = winningLotto.split(',');
+    const haveDuplicates = numbers.length !== new Set(numbers).size;
+
+    if (haveDuplicates) {
+      throw new Error('[ERROR] 로또 번호는 중복되는 수가 없어야 합니다');
+    }
+  },
 };
 
 export default validator;
