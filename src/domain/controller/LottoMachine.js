f07e10ff5da9e3f10308d5c23f22a66c4c8eadc2
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 20:10:51 +0900

    ✨ feat(LottoMachine): 구매 금액만큼 로또들을 생성한다

--- src/domain/controller/LottoMachine.js
+++ src/domain/controller/LottoMachine.js
@@ -11,6 +11,14 @@ class LottoMachine {
     inputHandler('구입금액을 입력해 주세요.', this.#afterReadMoney);
   }
 
+  generateLottos(amount) {
+    const lottoCount = amount / 1000;
+
+    this.#lottos = Array.from({ length: lottoCount }).map(() =>
+      this.makeLottoNumbers()
+    );
+  }
+
   makeLottoNumbers() {
     const lottoNumbers = Array.from({ length: 6 }).map(() =>
       pickRandomNumberInRange(1, 45)
