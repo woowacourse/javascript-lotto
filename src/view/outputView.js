e0d9bd106c8b512ec1dc15e8c794b64e93181c6f
unknown <tkdrb15@gmail.com>
2023-02-15 18:44:39 +0900

    feat:유효성 테스트 위치 변경

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -4,9 +4,21 @@ const OutputView = {
   },
 
   printPurchaseLottos(lottos) {
-    const result = lottos.map((lotto) => `${lotto.numbers}\n`).join("");
+    const result = lottos
+      .map((lotto) => `[${lotto.numbers.join(", ")}]\n`)
+      .join("");
 
     console.log(result);
+  },
+
+  printRevenueResult(lottoResult) {
+    console.log("당첨 통계");
+    console.log("-------------------");
+    console.log(`3개 일치 (5,000원) -${}개`);
+    console.log(`4개 일치 (50,000원)) -${}개`);
+    console.log(`5개 일치 (1,500,000원) -${}개`);
+    console.log(`5개 일치, 보너스 볼 일치 (30,000,000원) -  -${}개`);
+    console.log(`6개 일치 (2,000,000,000원) -  -${}개`);
   },
 };
 
