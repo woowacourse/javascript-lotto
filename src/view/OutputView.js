763b1a1f96042361dd8a2f4f787b90b7550cc978
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 18:01:20 +0900

    fix: 로또 당첨 결과를 제대로 표시 못하는 문제를 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -8,20 +8,23 @@ const OutputView = {
     Console.print(numbers);
   },
   // eslint-disable-next-line max-lines-per-function
-  printResult() {
+  printResult(matchResult) {
     Console.print(`
 당첨 통계
 --------------------
-3개 일치 (5,000원) - 1개
-4개 일치 (50,000원) - 0개
-5개 일치 (1,500,000원) - 0개
-5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-6개 일치 (2,000,000,000원) - 0개
+3개 일치 (5,000원) - ${matchResult[2]}개
+4개 일치 (50,000원) - ${matchResult[3]}개
+5개 일치 (1,500,000원) - ${matchResult[4]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchResult[6]}개
+6개 일치 (2,000,000,000원) - ${matchResult[5]}개
 총 수익률은 62.5%입니다.
 `);
   },
   printNewLine() {
     Console.print('');
+  },
+  printError(error) {
+    Console.print(error.message);
   }
 };
 export default OutputView;
