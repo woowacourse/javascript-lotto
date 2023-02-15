d0fd1b7eb08c8c2f3354d5ded79e322c4ff8bcc0
Patrick <kuyt3122@naver.com>
2023-02-15 15:29:17 +0900

    ✨ feat: 로또 한 장당 6개의 번호를 부여하는 기능 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,8 +1,9 @@
 class Lotto {
-    constructor(money){
-        if(moeny%1000 !== 0) throw new Error("로또는 1000원 단위로 입력을 해주셔야 됩니다.")
-        this.lottoMoney = money;
-    }
+  constructor(money, number) {
+    if (money%1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
+    this.lottoMoney = money;
+    this.lottoNumber = number;
+  }
 }
 
-export default Lotto
+export default Lotto;
