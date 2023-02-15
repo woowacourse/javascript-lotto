80dc365c2d6777fcddb14a665a5dc843f6858633
turtle601 <turtle601@naver.com>
2023-02-15 16:11:51 +0900

    test(WinningNumbers): 당첨번호가 중복일 경우 예외 처리 테스트 작성

--- __tests__/WinningNumbers.test.js
+++ __tests__/WinningNumbers.test.js
@@ -15,3 +15,11 @@ test('당첨번호를 쉼표로 구분했을 때 6개안 경우 정상 동작한
     new WinningNumbers(winningNumbersInput);
   }).not.toThrow();
 });
+
+test('당첨번호가 중복일 경우 예외처리한다.', () => {
+  const winningNumbersInput = '1,2,3,4,5,5';
+
+  expect(() => {
+    new WinningNumbers(winningNumbersInput);
+  }).toThrow();
+});
