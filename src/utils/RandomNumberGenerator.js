9b01e6e91b8aa8980ea8a01a84ff2c86e2d761be
inyeong-kang <tomatorkd@naver.com>
2023-02-15 18:04:16 +0900

    feat(utils): 무작위의 로또 번호 생성 기능 구현

---
+++ src/utils/RandomNumberGenerator.js
@@ -0,0 +1,38 @@
+const { lottoNumberRange } = require('../constants/constants');
+
+const randomNumberGenerator = {
+  generateRandomNumber() {
+    return Math.floor(
+      Math.random() *
+        (lottoNumberRange.MAX_LOTTO_NUMBER -
+          lottoNumberRange.MIN_LOTTO_NUMBER) +
+        lottoNumberRange.MIN_LOTTO_NUMBER
+    );
+  },
+
+  generateLottoNumbers() {
+    const lottoNumbers = [];
+
+    while (lottoNumbers.length < 6) {
+      const randomNumber = this.generateRandomNumber();
+      this.fillLottoNumbers(lottoNumbers, randomNumber);
+    }
+
+    return this.sortLottoNumbers(lottoNumbers);
+  },
+
+  fillLottoNumbers(lottoNumbers, randomNumber) {
+    if (!this.isNumberDuplicate(lottoNumbers, randomNumber))
+      lottoNumbers.push(randomNumber);
+  },
+
+  isNumberDuplicate(lottoNumbers, randomNumber) {
+    return lottoNumbers.includes(randomNumber);
+  },
+
+  sortLottoNumbers(lottoNumbers) {
+    return lottoNumbers.sort((a, b) => a - b);
+  },
+};
+
+module.exports = { randomNumberGenerator };
