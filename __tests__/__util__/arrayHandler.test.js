fd5b4c9716bd1e2b4219023cb39dbc19bc14ed4c
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-02-15 16:21:37 +0900

    test: 소수점 둘째 자리에서 반올림하는 기능 테스트 추가

--- __tests__/__util__/arrayHandler.test.js
+++ __tests__/__util__/arrayHandler.test.js
@@ -1,7 +1,6 @@
 import ArrayHandler from "../../src/util/ArrayHandler.js";
 
 describe("ArrayHandler테스트", () => {
-  const ascendingOrderSortedNumbers = [1, 2, 3, 4, 5];
   test.each([
     [
       [5, 4, 3, 2, 1],
@@ -9,8 +8,29 @@ describe("ArrayHandler테스트", () => {
       [1, 2, 3, 4, 5],
     ],
   ])("배열 오름차순 정렬 테스트", (numbers) => {
+    const ascendingOrderSortedNumbers = [1, 2, 3, 4, 5];
+
     expect(ArrayHandler.sortAscendingOrder(numbers)).toEqual(
       ascendingOrderSortedNumbers
     );
   });
+
+  test.each([
+    [[1, 2, 3, 4, 5, 6], 6],
+    [[1, 2, 3, 4, 5, 7], 5],
+    [[1, 2, 3, 4, 8, 7], 4],
+    [[1, 2, 3, 9, 8, 7], 3],
+    [[1, 2, 10, 9, 8, 7], 2],
+    [[1, 11, 10, 9, 8, 7], 1],
+    [[12, 11, 10, 9, 8, 7], 0],
+  ])(
+    "getMatchCount 테스트) 두 배열의 동일한 숫자 개수를 리턴한다. ",
+    (lottoNumbers, matchCount) => {
+      const targetNumbers = [1, 2, 3, 4, 5, 6];
+
+      expect(ArrayHandler.getMatchCount(lottoNumbers, targetNumbers)).toBe(
+        matchCount
+      );
+    }
+  );
 });
--- __tests__/__util__/numberHandler.test.js
+++ __tests__/__util__/numberHandler.test.js
@@ -1,22 +1,16 @@
 import NumberHandler from "../../src/util/NumberHandler.js";
+/**
+    - [ ]  범위 내의 랜덤 숫자값 리턴하기 - 범위 테스트하기
+ */
 
 describe("NumberHandler 테스트", () => {
-  const targetNumbers = [1, 2, 3, 4, 5, 6];
-
   test.each([
-    [[1, 2, 3, 4, 5, 6], 6],
-    [[1, 2, 3, 4, 5, 7], 5],
-    [[1, 2, 3, 4, 8, 7], 4],
-    [[1, 2, 3, 9, 8, 7], 3],
-    [[1, 2, 10, 9, 8, 7], 2],
-    [[1, 11, 10, 9, 8, 7], 1],
-    [[12, 11, 10, 9, 8, 7], 0],
-  ])(
-    "getMatchCount 테스트) 두 배열의 동일한 숫자 개수를 리턴한다. ",
-    (lottoNumbers, matchCount) => {
-      expect(NumberHandler.getMatchCount(lottoNumbers, targetNumbers)).toBe(
-        matchCount
-      );
-    }
-  );
+    [1.123, 1.1],
+    [1.56, 1.6],
+    [1.0, 1],
+    [100.05, 100.1],
+    [100, 100],
+  ])("소수점 둘째자리에서 반올림한다.", (floatNumber, roundedOffNumber) => {
+    expect(NumberHandler.roundOffNumber(floatNumber)).toBe(roundedOffNumber);
+  });
 });
