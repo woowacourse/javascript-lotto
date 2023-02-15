6b8e79646bf3cc8f6b58d427db5f78a73b805190
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 15:37:53 +0900

    feat: 로또게임 조건, 메세지 상수화

---
+++ src/constants/condition.js
@@ -0,0 +1,8 @@
+const LOTTO_CONDITION = {
+  lottoPrice: 1000,
+  lottoNumberMinRange: 1,
+  lottoNumberMaxRange: 45,
+  lottoDigits: 6,
+};
+
+export { LOTTO_CONDITION };
---
+++ src/constants/message.js
@@ -0,0 +1,8 @@
+const REQUEST_MESSAGE = Object.freeze({
+  purchaseAmount: '구입금액을 입력해 주세요.\n',
+  winningNumbers: '당첨 번호를 입력해 주세요.\n',
+  bonusNumber: '보너스 번호를 입력해 주세요.\n',
+  restartCommand: `다시 시작하시겠습니까? (y/n)\n`,
+});
+
+export { REQUEST_MESSAGE };
