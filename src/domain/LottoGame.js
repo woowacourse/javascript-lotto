238d877a32c6474dbf1df6b941c41932b84116dd
Q Kim <qoli0703@gmail.com>
2023-02-15 17:42:09 +0900

    feat: 금액만큼 lotto 생성

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -1,6 +1,27 @@
+import Random from '../util/Random.js';
+import Lotto from './Lotto.js';
+
 const LOTTO_PRICE = 1000;
+const LOTTO_SIZE = 6;
+const LOTTO_MIN_NUMBER = 1;
+const LOTTO_MAX_NUMBER = 45;
 
 class LottoGame {
+  #lottos;
+
+  constructor(money) {
+    const count = Math.floor(money / LOTTO_PRICE);
+    this.#lottos = Array.from({ length: count }, () => {
+      const randomNumbers = Random.generateUniqueNumbersInRange(
+        LOTTO_SIZE,
+        LOTTO_MIN_NUMBER,
+        LOTTO_MAX_NUMBER,
+      );
+
+      return new Lotto(randomNumbers);
+    });
+  }
+
   static calculateTheChange(money) {
     return money % LOTTO_PRICE;
   }
