95c14add3b4e80b5f3b60deeeabf88749aa528f1
ashleysyheo <ashleysyheo@gmail.com>
2023-02-15 16:22:23 +0900

    refactor: change from readline promise to readline

--- src/utils/Console.js
+++ src/utils/Console.js
@@ -1,6 +1,8 @@
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline';
 
-const Console = readline.createInterface({ input, output });
+const Console = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 export default Console;
