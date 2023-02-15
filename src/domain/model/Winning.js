48d6bd0e371d12c4fdcaa202e8e73c766e771344
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-15 14:36:37 +0900

    ✨ feat(Winning): 당첨 번호와 보너스 번호를 가지고 반환한다

---
+++ src/domain/model/Winning.js
@@ -0,0 +1,16 @@
+class Winning {
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor() {}
+
+  getWinningNumbers() {
+    return this.#winningNumbers;
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
+export default Winning;
