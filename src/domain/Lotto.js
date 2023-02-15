ffdab820b699fa77f9cd4266c4c95bf86dc04154
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-15 17:33:00 +0900

    feat: 로또 결과 계산 구현

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -2,6 +2,16 @@ class Lotto {
   constructor(numbers) {
     this.numbers = numbers;
   }
+
+  getNumbers() {
+    return this.numbers;
+  }
+
+  matcher(winningNumber, bonusNumber) {
+    const bonus = this.numbers.includes(bonusNumber);
+    const match = this.numbers.filter((number) => winningNumber.includes(number)).length;
+    return { match, bonus };
+  }
 }
 
 export default Lotto;
