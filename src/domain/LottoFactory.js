8114589f537325fc87b01ca68f1e2f364a85432f
solo5star <solo_5@naver.com>
2023-02-15 07:34:14 +0000

    feat: LottoFactory에서 로또를 판매하는 기능 추가
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- src/domain/LottoFactory.js
+++ src/domain/LottoFactory.js
@@ -9,6 +9,16 @@ class LottoFactory {
     const lottoNumbers = shuffle(allLottoNumbers).slice(0, 6);
     return new Lotto(lottoNumbers);
   }
+
+  sellLottos(money) {
+    if (money % 1000 !== 0) throw new Error('1000원 단위로 금액을 주어야 합니다.');
+    const amount = money / 1000;
+    if (amount <= 0) throw new Error('1개 이상 구매할 수 있는 금액을 주어야 합니다.');
+
+    return Array(amount)
+      .fill()
+      .map(() => this.createRandomLotto());
+  }
 }
 
 export default LottoFactory;
