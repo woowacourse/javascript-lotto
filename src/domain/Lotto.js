13b6c0e615ac7c1fdadb6b77f08dd9c663d9c2f3
Patrick <kuyt3122@naver.com>
2023-02-15 17:02:24 +0900

    ✨ feat: 구매한 로또가 갯수만큼 발행되는 기능 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,18 +1,29 @@
 class Lotto {
-  constructor(money, number) {
-    if (money % 1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
+  constructor(money) {
+    if (money%1000) throw new Error('로또는 1000원 단위로 입력을 해주셔야 됩니다.');
     this.lottoMoney = money;
-    this.lottoNumber = number;
+    this.lottoNumber = [];
+  }
+
+  countLotto(money) {
+    return Number(money) / 1000;
+  }
+
+  makeLotto() {
+    const number = this.countLotto(this.lottoMoney);
+    this.lottoNumber = Array.from({ length: number }, () => this.randomNumberLotto());
+
   }
 
   randomNumberLotto() {
-    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46));
-    if (this.compareSameNumberInLotto()) return randomNumber;
+    const randomNumber = Array.from({ length: 6 }, () => Math.floor(Math.random() * 46)
+    );
+    if (this.compareSameNumberInLotto(randomNumber)) return randomNumber;
     this.randomNumberLotto();
   }
 
   compareSameNumberInLotto(randomNumber) {
-    return [...new Set(randomNumber)].length === 6;
+    return [...new Set(randomNumber)].length === 6
   }
 }
 
