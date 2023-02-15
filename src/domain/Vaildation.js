182216acc0c0bb0c6b0b83e7bdb4cbd0bc09e8c7
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 16:38:43 +0900

    feat(Validation): 구입금액 유효성 검사 기능 구현

--- src/domain/Vaildation.js
+++ src/domain/Vaildation.js
@@ -1,6 +1,21 @@
 import { LOTTO_CONDITION } from '../constants/condition.js';
+import { ERROR_MESSAGE } from '../constants/message.js';
 
 const Validation = {
+  isValidPurchaseAmount(purchaseAmount) {
+    if (!this.isNumber(purchaseAmount)) {
+      throw new Error(ERROR_MESSAGE.invalidInputType);
+    }
+
+    if (!this.isHigherThanLottoPrice(purchaseAmount)) {
+      throw new Error(ERROR_MESSAGE.lowerThanLottoPrice);
+    }
+
+    if (!this.isDivisibleByLottoPrice(purchaseAmount)) {
+      throw new Error(ERROR_MESSAGE.indivisibleByLottoPrice);
+    }
+  },
+
   isNumber(purchaseAmount) {
     return Number.isInteger(Number(purchaseAmount));
   },
