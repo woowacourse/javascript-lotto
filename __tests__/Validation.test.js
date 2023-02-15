cecb4b4c0c7b73b39281842012dd1815cc836344
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-15 16:19:47 +0900

    test(Validation): describe 추가로 메서드 별 테스트케이스 분리

--- __tests__/Validation.test.js
+++ __tests__/Validation.test.js
@@ -1,51 +1,57 @@
 import Validation from '../src/domain/Vaildation.js';
 import { LOTTO_CONDITION } from '../src/constants/condition.js';
 
-test.each(['abc', '8천원', '8$'])(
-  '구입금액 입력이 숫자가 아닌 경우 false를 반환한다.',
-  (purchaseAmount) => {
+describe('Validation.isNumber', () => {
+  test.each(['abc', '8천원', '8$'])(
+    '구입금액 입력이 숫자가 아닌 경우 false를 반환한다.',
+    (purchaseAmount) => {
+      const result = Validation.isNumber(purchaseAmount);
+
+      expect(result).toBe(false);
+    }
+  );
+
+  test('구입금액 입력이 숫자인 경우 true를 반환한다.', () => {
+    const purchaseAmount = '1000';
+
     const result = Validation.isNumber(purchaseAmount);
 
+    expect(result).toBe(true);
+  });
+});
+
+describe('Validation.isHigherThanLottoPrice', () => {
+  test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원) 미만인 경우, false를 반환한다.`, () => {
+    const purchaseAmount = `${LOTTO_CONDITION.lottoPrice - 1}`;
+
+    const result = Validation.isHigherThanLottoPrice(purchaseAmount);
+
     expect(result).toBe(false);
-  }
-);
+  });
 
-test('구입금액 입력이 숫자인 경우 true를 반환한다.', () => {
-  const purchaseAmount = '1000';
+  test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원) 이상인 경우, true를 반환한다.`, () => {
+    const purchaseAmount = `${LOTTO_CONDITION.lottoPrice + 1}`;
 
-  const result = Validation.isNumber(purchaseAmount);
+    const result = Validation.isHigherThanLottoPrice(purchaseAmount);
 
-  expect(result).toBe(true);
+    expect(result).toBe(true);
+  });
 });
 
-test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원) 미만인 경우, false를 반환한다.`, () => {
-  const purchaseAmount = `${LOTTO_CONDITION.lottoPrice - 1}`;
+describe('Validation.isDivisibleByLottoPrice', () => {
+  test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원)으로 나누어 떨어지지 않는 경우, false를 반환한다.`, () => {
+    const purchaseAmount = `${LOTTO_CONDITION.lottoPrice + 1}`;
 
-  const result = Validation.isHigherThanLottoPrice(purchaseAmount);
+    const result = Validation.isDivisibleByLottoPrice(purchaseAmount);
 
-  expect(result).toBe(false);
-});
+    expect(result).toBe(false);
+  });
 
-test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원) 이상인 경우, true를 반환한다.`, () => {
-  const purchaseAmount = `${LOTTO_CONDITION.lottoPrice + 1}`;
+  test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원)으로 나누어 떨어지는 경우, true를 반환한다.`, () => {
+    const purchaseAmount = `${LOTTO_CONDITION.lottoPrice * 2}`;
 
-  const result = Validation.isHigherThanLottoPrice(purchaseAmount);
+    const result = Validation.isDivisibleByLottoPrice(purchaseAmount);
 
-  expect(result).toBe(true);
-});
-
-test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원)으로 나누어 떨어지지 않는 경우, false를 반환한다.`, () => {
-  const purchaseAmount = `${LOTTO_CONDITION.lottoPrice + 1}`;
-
-  const result = Validation.isDivisibleByLottoPrice(purchaseAmount);
-
-  expect(result).toBe(false);
-});
-
-test(`구입금액 입력이 로또 1장 가격(${LOTTO_CONDITION.lottoPrice}원)으로 나누어 떨어지는 경우, true를 반환한다.`, () => {
-  const purchaseAmount = `${LOTTO_CONDITION.lottoPrice * 2}`;
-
-  const result = Validation.isDivisibleByLottoPrice(purchaseAmount);
-
-  expect(result).toBe(true);
+    expect(result).toBe(true);
+  });
 });
