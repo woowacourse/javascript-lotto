fdc97f05d967929583b1cb92a284995a6f1fdf40
solo5star <solo_5@naver.com>
2023-02-15 05:39:49 +0000

    feat: 랜덤한 숫자 배열을 받아 로또를 발행하는 함수 작성
    Co-authored-by: xodms0309 <xodms0309@naver.com>

---
+++ src/domain/LottoFactory.js
@@ -0,0 +1,14 @@
+import { shuffle } from '../utils/Random';
+import Lotto from './Lotto';
+
+class LottoFactory {
+  constructor() {}
+
+  createRandomLotto() {
+    const allLottoNumbers = Array.from({ length: 45 }, (_, i) => i + 1);
+    const lottoNumbers = shuffle(allLottoNumbers).slice(0, 6);
+    return new Lotto(lottoNumbers);
+  }
+}
+
+export default LottoFactory;
