c9ccfbd5e8666b94d392fae66dcd81c085746ea8
gyeongza <gyeongza@naver.com>
2023-02-16 11:39:04 +0900

    test : 당첨번호가 1~45의 범위인지 판별하는 함수 테스트

--- __tests__/Validations.test.js
+++ __tests__/Validations.test.js
@@ -1,11 +1,11 @@
 import Validations from "../src/Validations";
 
-describe("Validations 단위 테스트", () => {
+describe("구매금액 Validations 단위 테스트", () => {
   
   test("구입금액이 숫자인지 판별하는 함수 테스트", () => {
     const buyMoney = 'aiden'
     
-    expect(Validations.isBuyMoneyNumber(buyMoney)).toBeFalsy();
+    expect(Validations.isNumber(buyMoney)).toBeFalsy();
   });
 
   test("구입금액이 양의 정수인지 판별하는 함수 테스트", () => {
@@ -21,3 +21,35 @@ describe("Validations 단위 테스트", () => {
   });
 
 });
+
+describe("당첨번호 Validations 단위 테스트", () => {
+
+  test("당첨번호가 1~45의 범위인지 판별하는 함수 테스트", () => {
+    const winningNumber = 0
+    
+    expect(Validations.isCorrectRange(winningNumber)).toBeFalsy();
+  });
+
+});
+
+// describe("보너스 번호 Validations 단위 테스트", () => {
+  
+//   test("구입금액이 숫자인지 판별하는 함수 테스트", () => {
+//     const buyMoney = 'aiden'
+    
+//     expect(Validations.isBuyMoneyNumber(buyMoney)).toBeFalsy();
+//   });
+
+//   test("구입금액이 양의 정수인지 판별하는 함수 테스트", () => {
+//     const buyMoney = -2000
+    
+//     expect(Validations.isPositiveInteger(buyMoney)).toBeFalsy();
+//   });
+
+//   test("구입금액이 1000으로 나누어 떨어지는지 판별하는 함수 테스트", () => {
+//     const buyMoney = 200
+    
+//     expect(Validations.isDevidedByThousand(buyMoney)).toBeFalsy();
+//   });
+
+// });
