7c2f441f1cac979381f16d6d3b52638b42bf1f97
turtle601 <turtle601@naver.com>
2023-02-16 11:41:25 +0900

    test(LottoStatistics): 모든 로또의 당첨 결과를 배열로 반환하는 기능 테스트 작성

--- __tests__/LottoStatistics.test.js
+++ __tests__/LottoStatistics.test.js
@@ -1,7 +1,7 @@
-const BonusNumber = require('../src/domain/BonusNumber');
 const Lotto = require('../src/domain/Lotto');
 const LottoStatistics = require('../src/domain/LottoStatistics');
 const WinningNumbers = require('../src/domain/WinningNumbers');
+const BonusNumber = require('../src/domain/BonusNumber');
 
 test('각 로또의 등수를 결정한다.', () => {
   const statistics = new LottoStatistics(
@@ -12,4 +12,18 @@ test('각 로또의 등수를 결정한다.', () => {
   const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
 
   expect(rank).toBe(1);
+});
+
+test('모든 로또의 당첨 결과를 배열로 반환한다.', () => {
+  const statistics = new LottoStatistics(
+    new WinningNumbers('1,2,3,4,5,6'),
+    new BonusNumber('7')
+  );
+
+  const result = statistics.determineAllLottosRank([
+    new Lotto([1, 2, 3, 4, 5, 6]),
+    new Lotto([1, 2, 3, 4, 5, 7]),
+  ]);
+
+  expect(result).toEqual([1, 1, 0, 0, 0, 0]);
 });
