62b902692f06236de1a9ed673f2338a04f15d03c
yeopto <gunyeop530@gmail.com>
2023-02-16 14:52:14 +0900

    refactor: validateWithCondition 메서드를 적용하다

--- src/validator/InputValidator.js
+++ src/validator/InputValidator.js
@@ -9,13 +9,8 @@ const InputValidator = {
   },
 
   validateMoneyInput(number) {
-    if (!ValidatorUtils.isPositiveInteger(+number)) {
-      throw new Error(messages.ERROR.POSITIVE_INTEGER);
-    }
-
-    if (!ValidatorUtils.isThousandsOfWon(+number)) {
-      throw new Error(messages.ERROR.THOUSANDS_WON);
-    }
+    this.validateWithCondition(ValidatorUtils.isPositiveInteger(+number), messages.ERROR.POSITIVE_INTEGER);
+    this.validateWithCondition(ValidatorUtils.isThousandsOfWon(+number), messages.ERROR.THOUSANDS_WON);
   },
 
   validateWinningNumberInput(winningNumber) {
