aa9918c79c0574754255ba4f94e7737b9818eaa9
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-16 17:15:52 +0900

    feat: 보너스 번호를 가지고 있는지 확인하는 기능 구현

--- src/domain/LottoTicket.js
+++ src/domain/LottoTicket.js
@@ -8,8 +8,16 @@ class LottoTicket {
     this.#numbers = numbers;
   }
 
+  hasBonusNumber(number) {
+    return this.#numbers.includes(number);
+  }
+
   countMatchedNumbers(winningNumbers) {
     return this.#numbers.filter((number) => winningNumbers.includes(number)).length;
+  }
+
+  getNumbers() {
+    return this.#numbers;
   }
 }
 
