97dd34dda44d7b1e560335c8f15bc361811be78e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-16 13:23:27 +0900

    feat(Lotto): 당첨번호 일치 확인 기능 구현

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,9 +1,21 @@
+import { LOTTO_CONDITION } from '../constants/condition';
+
 export default class Lotto {
   #numbers;
 
   constructor(lottoNumbers) {
     this.#numbers = lottoNumbers;
   }
+
+  getCompareResult(winningNumbers, bonusNumber) {}
+
+  getMatchCount(winningNumbers) {
+    const duplicateCheck = new Set([...winningNumbers, ...this.#numbers]);
+
+    return LOTTO_CONDITION.lottoDigits * 2 - duplicateCheck.size;
+  }
+
+  hasBonusNumber(bonusNumber) {}
 
   getNumbers() {
     return this.#numbers;
