fff532bb62f22f0099f0adffbc66f96855ba9e06
turtle601 <turtle601@naver.com>
2023-02-16 15:58:13 +0900

    test(LottoMachine): LottoMachine 테스트 각각을 test.each()로 수정

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -1,16 +1,22 @@
 const LottoMachine = require('../src/domain/LottoMachine');
 
-test('구입 금액이 1000으로 나누어 떨어지지 않는 경우 예외 처리한다.', () => {
-  const purchasePrice = 8001;
+describe('LottoMachine 클래스 테스트', () => {
+  test.each([[0], [8001], ['']])(
+    '구입 금액이 1000으로 나누어 떨어지지 않는 경우 예외 처리한다.',
+    (input) => {
+      expect(() => {
+        new LottoMachine(input);
+      }).toThrow();
+    }
+  );
 
-  expect(() => {
-    new LottoMachine().validatePurchasePrice(purchasePrice);
-  }).toThrow();
-});
-
-test('구입 금액을 1000으로 나눈 개수만큼의 로또를 발행한다.', () => {
-  const purchasePrice = 8000;
-  const lottoGame = new LottoMachine(purchasePrice);
-
-  expect(lottoGame.lottos).toHaveLength(8);
+  test.each([
+    [1000, 1],
+    [8000, 8],
+  ])(
+    '구입 금액을 1000으로 나눈 개수만큼의 로또를 발행한다.',
+    (input, expected) => {
+      expect(new LottoMachine(input).lottos).toHaveLength(expected);
+    }
+  );
 });
