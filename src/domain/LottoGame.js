67b6748d96ba96e3e0defd9cb3590d4d122993dd
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-16 17:17:59 +0900

    feat: 당첨기준에 해당하는 등수를 가져오는 기능 구현

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -1,5 +1,6 @@
 const generateLottoNumbers = require('./LottoNumbersGenerator');
 const LottoTicket = require('./LottoTicket');
+const Converter = require('../util/Converter');
 
 class LottoGame {
   #userBudget;
@@ -11,6 +12,13 @@ class LottoGame {
       { length: userBudget / 1000 },
       () => new LottoTicket(Converter.sortAscending(generateLottoNumbers()))
     );
+  }
+
+  getLottoRank(matchedNumberCount, hasBonusNumber) {
+    const rank = Rank[matchedNumberCount];
+    if (matchedNumberCount === 5 && hasBonusNumber) return 2;
+    if (rank === undefined) return 0;
+    return rank;
   }
 }
 
