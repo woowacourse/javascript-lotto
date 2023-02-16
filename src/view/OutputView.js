42769946e20fe4ecca73f0349b888a3d2eaac4bb
yeopto <gunyeop530@gmail.com>
2023-02-16 13:15:28 +0900

    feat: 로또 출력 로직을 구현한다

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,6 +4,12 @@ const OutputView = {
   printMessage(message) {
     Console.print(message);
   },
+
+  printLottos(lottos) {
+    lottos.forEach(lotto => {
+      Console.print(lotto.lottoNum);
+    });
+  },
 };
 
 export default OutputView;
