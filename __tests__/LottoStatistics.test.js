bd53ef3c4e24bb6025ca5e234e8dfc61b4cf82c1
turtle601 <turtle601@naver.com>
2023-02-16 16:07:14 +0900

    refactor(LottoStatistics): LottoStatistics 테스트를 test.each()로 수정

--- __tests__/LottoStatistics.test.js
+++ __tests__/LottoStatistics.test.js
@@ -3,40 +3,57 @@ const LottoStatistics = require('../src/domain/LottoStatistics');
 const WinningNumbers = require('../src/domain/WinningNumbers');
 const BonusNumber = require('../src/domain/BonusNumber');
 
-test('각 로또의 등수를 결정한다.', () => {
-  const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
-  const bonusNumber = new BonusNumber('7', winningNumbers);
+const LottoMachine = require('../src/domain/LottoMachine');
 
-  const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+describe('LottoStatistics 클래스 테스트', () => {
+  test.each([
+    ['1,2,3,4,5,6', '7', 1],
+    ['1,2,3,4,5,45', '6', 2],
+    ['1,2,3,4,5,45', '44', 3],
+    ['1,2,3,4,44,45', '33', 4],
+    ['1,2,3,43,44,45', '33', 5],
+    ['1,2,42,43,44,45', '33', 6],
+  ])(
+    '각 로또의 등수를 결정한다.',
+    (winningNumbersInput, bonusNumberInput, expected) => {
+      const winningNumbers = new WinningNumbers(winningNumbersInput);
+      const bonusNumber = new BonusNumber(bonusNumberInput, winningNumbers);
+      const statistics = new LottoStatistics(winningNumbers, bonusNumber);
 
-  const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+      const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
 
-  expect(rank).toBe(1);
-});
+      expect(rank).toBe(expected);
+    }
+  );
 
-test('모든 로또의 당첨 결과를 배열로 반환한다.', () => {
-  const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
-  const bonusNumber = new BonusNumber('7', winningNumbers);
+  test('모든 로또의 당첨 결과를 배열로 반환한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
+    const bonusNumber = new BonusNumber('7', winningNumbers);
 
-  const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
 
-  const result = statistics.determineAllLottosRank([
-    new Lotto([1, 2, 3, 4, 5, 6]),
-    new Lotto([1, 2, 3, 4, 5, 7]),
-  ]);
+    const result = statistics.determineAllLottosRank([
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 4, 5, 7]),
+      new Lotto([1, 2, 3, 4, 5, 8]),
+      new Lotto([1, 2, 3, 4, 44, 45]),
+      new Lotto([1, 2, 3, 43, 44, 45]),
+      new Lotto([1, 2, 42, 43, 44, 45]),
+    ]);
 
-  expect(result).toEqual([1, 1, 0, 0, 0, 0]);
-});
+    expect(result).toEqual([1, 1, 1, 1, 1, 1]);
+  });
 
-test('총 수익률을 계산한다.', () => {
-  const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
-  const bonusNumber = new BonusNumber('7', winningNumbers);
+  test('총 수익률을 계산한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
+    const bonusNumber = new BonusNumber('7', winningNumbers);
 
-  const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
 
-  const winningLottos = statistics.determineAllLottosRank([
-    new Lotto([1, 2, 3, 40, 41, 42]),
-  ]);
+    const winningLottos = statistics.determineAllLottosRank([
+      new Lotto([1, 2, 3, 40, 41, 42]),
+    ]);
 
-  expect(statistics.calculateProfitRate(winningLottos, 8000)).toBe('62.5');
+    expect(statistics.calculateProfitRate(winningLottos, 8000)).toBe('62.5');
+  });
 });
