dd584c29ae86d089315d84a78116bbc703abba09
ashleysyheo <ashleysyheo@gmail.com>
2023-02-16 10:43:37 +0900

    refactor: add initial ranks to constructor

--- src/domain/LottoCalculator.js
+++ src/domain/LottoCalculator.js
@@ -1,29 +1,37 @@
-import { Rank } from "../constants/Constants.js";
+import { Prize, Rank, StaticValue } from "../constants/Constants.js";
 
 class LottoCalculator {
   constructor(matchStates) {
     this.matchStates = matchStates;
+    this.ranks = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+    };
   }
 
   calculateRank() {
     return this.matchStates.reduce((ranks, currentState) => {
       const CURRENT_RANK = Rank[currentState];
-      ranks[CURRENT_RANK] = ranks[CURRENT_RANK] + 1 || 1;
+      ranks[CURRENT_RANK] += 1;
 
       return ranks;
-    }, {});
+    }, this.ranks);
   }
 
-  calculateProfitRate(ranks) {
+  calculateProfitRate() {
     return (
-      (this.#calculateProfit(ranks) /
+      (this.#calculateProfit() /
         (this.matchStates.length * StaticValue.PURCHASE_AMOUNT_UNIT)) *
       100
     ).toFixed(1);
   }
 
-  #calculateProfit(ranks) {
-    return Object.entries(ranks).reduce((totalProfit, [rank, count]) => {
+  #calculateProfit() {
+    return Object.entries(this.ranks).reduce((totalProfit, [rank, count]) => {
       return totalProfit + Prize[rank] * count;
     }, 0);
   }
