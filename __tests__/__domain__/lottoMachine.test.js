9af12b78d4d6f6a8d7eb05d11aac6ada7acc18cf
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-02-16 10:48:38 +0900

    test: lottoMachine 기능 테스트 추가

--- __tests__/__domain__/lottoMachine.test.js
+++ __tests__/__domain__/lottoMachine.test.js
@@ -1,5 +1,22 @@
 import LottoMachine from '../../src/domain/LottoMachine.js';
 
 describe('LottoMachine 테스트', () => {
-  test('', () => {});
+  test.each([
+    [LottoMachine.generateLottoNumbers()],
+    [LottoMachine.generateLottoNumbers()],
+    [LottoMachine.generateLottoNumbers()],
+  ])(
+    'generateLottoNumbers가 6개의 오름차순으로 정렬된 숫자를 반환한다.',
+    lottoNumbers => {
+      const length = 6;
+      const sortedLottoNumbers = lottoNumbers.sort((a, b) => a - b);
+      const lottoNumberType = 'number';
+
+      expect(lottoNumbers.length).toBe(length);
+      lottoNumbers.forEach(lottoNumber =>
+        expect(typeof lottoNumber).toBe(lottoNumberType)
+      );
+      expect(lottoNumbers).toEqual(sortedLottoNumbers);
+    }
+  );
 });
