36bd7087ce941c102b25e04c01380ed13eceb813
gyeongza <gyeongza@naver.com>
2023-02-16 15:53:00 +0900

    feat : 로또번호 출력 및 당첨 내역 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import Console from "./Console.js";
 import Validations from "./Validations.js";
 import InputView from "./view/InputView.js";
+import Lotto from "./Lotto.js";
+import Lottos from "./Lottos.js";
+import Random from "./Random.js";
 
 class App {
   #winningLotto;
   #bonusNumber;
+  #lottoArray;
 
   constructor() {
     this.#winningLotto = [];
     this.#bonusNumber = 0;
+    this.#lottoArray = [];
   }
 
   async play() {
     await this.getBuyMoney();
-    Console.close();
+    await this.getWinningNumbers();
+    await this.getBonusNumber();
+    await this.getRetryInput();
   }
 
   async getBuyMoney() {
     const buyMoney = await InputView.inputMoney("구입금액을 입력해 주세요.");
     try {
       this.validateBuyMoney(buyMoney);
+      this.createLotto(parseInt(buyMoney / 1000));
+      this.printLottos(buyMoney / 1000);
+
     } catch (e) {
       Console.print(e);
       await this.getBuyMoney();
     }
+  }
+
+  createLotto(lottoAmount) {
+    for (let i = 0; i < lottoAmount; i++) {
+      const lotto = new Lotto(Random.getnerateRandomNumbers());
+      this.#lottoArray.push(lotto);
+    }
+  }
+
+  printLottos(lottoAmount) {
+    Console.print(`${lottoAmount}개를 구매했습니다.`);
+    this.#lottoArray.forEach((lotto) => {
+      lotto.sortLottoNumbers();
+      Console.print(lotto.getLottoNumbers());
+    });
   }
 
   validateBuyMoney(buyMoney) {
@@ -42,13 +67,21 @@ class App {
     const winningNumbers = await InputView.inputWinningNumbers(
       "당첨 번호를 입력해 주세요."
     );
-    this.#winningLotto = winningNumbers.split(",");
+    this.#winningLotto = this.convertStringToNumber(winningNumbers.split(","))
+    // this.#winningLotto = winningNumbers.split(",");
     try {
       this.validateWinningNumbers();
     } catch (e) {
       Console.print(e);
-      this.getWinningNumbers();
+      await this.getWinningNumbers();
     }
+  }
+
+  convertStringToNumber(strings) {
+    const numberArr = strings.map((str) => {
+      return Number(str)
+    })
+    return numberArr
   }
 
   validateWinningNumbers() {
@@ -75,18 +108,40 @@ class App {
     );
     this.#bonusNumber = Number(bonusNumber);
     try {
-      this.validateBonusNumber()
-      this.checkEachNumber(this.#bonusNumber)
-    } catch(e) {
-      Console.print(e)
-      this.getBonusNumber()
+      this.validateBonusNumber();
+      this.checkEachNumber(this.#bonusNumber);
+      this.compareLottos()
+      // this.printResult()
+    } catch (e) {
+      Console.print(e);
+      await this.getBonusNumber();
     }
   }
 
   validateBonusNumber() {
-    if (!Validations.hasBonusNumber(this.#bonusNumber, this.#winningLotto)) {
+    if (Validations.hasBonusNumber(this.#bonusNumber, this.#winningLotto)) {
       throw new Error("보너스 번호는 당첨번호와 중복되지 않아야합니다.");
     }
+  }
+
+  compareLottos() {
+    const lottos = new Lottos(this.#lottoArray);
+    lottos.getLottos().forEach((lotto) => {
+      lotto.compareNumbers(this.#winningLotto)
+      lotto.checkBonusNumber(this.#bonusNumber)
+    })
+    lottos.compareLottosScore()
+    this.printResult(lottos)
+  }
+
+  printResult(lottos) {
+    Console.print("당첨통계");
+    Console.print("--------------------");
+    Console.print(`3개 일치 (${lottos.getBenefitBoard()[3]}원) - ${lottos.getLottoRanking()[3]}개`);
+    Console.print(`4개 일치 (${lottos.getBenefitBoard()[4]}원) - ${lottos.getLottoRanking()[4]}개`);
+    Console.print(`5개 일치 (${lottos.getBenefitBoard()[5]}원) - ${lottos.getLottoRanking()[5]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치  (${lottos.getBenefitBoard()["5 bonus"]}원) - ${lottos.getLottoRanking()["5 bonus"]}개`);
+    Console.print(`6개 일치 (${lottos.getBenefitBoard()[6]}원) - ${lottos.getLottoRanking()[6]}개`);
   }
 
   async getRetryInput() {
@@ -94,19 +149,28 @@ class App {
       "다시 시작하시겠습니까? (y/n)."
     );
     try {
-      this.validateRetryInput(retryInput)
-    } catch(e) {
-      Console.print(e)
-      this.getRetryInput()
-    }
-  }
-  
-  validateRetryInput(retryInput) {
-    if(!Validations.isCorrectRetryInput(retryInput)) {
-      throw new Error("재시작은 y, 종료는 n을 입력해주세요.")
+      this.validateRetryInput(retryInput);
+      this.retryLottoGame(retryInput);
+    } catch (e) {
+      Console.print(e);
+      await this.getRetryInput();
     }
   }
 
+  retryLottoGame(retryInput) {
+    if (retryInput === "y" || "y") {
+      this.play();
+    }
+    if (retryInput === "n" || "N") {
+      Console.close();
+    }
+  }
+
+  validateRetryInput(retryInput) {
+    if (!Validations.isCorrectRetryInput(retryInput)) {
+      throw new Error("재시작은 y, 종료는 n을 입력해주세요.");
+    }
+  }
 }
 
 export default App;
