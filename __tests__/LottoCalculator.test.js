ce0a423d3fdee8dbc02bbdc1507d9cb649ea0bed
ashleysyheo <ashleysyheo@gmail.com>
2023-02-16 09:53:57 +0900

    test: check user lottos profit rate

--- __tests__/LottoCalculator.test.js
+++ __tests__/LottoCalculator.test.js
@@ -1,20 +1,29 @@
 import LottoCalculator from "../src/domain/LottoCalculator.js";
 
 describe("LottoCalculator 클래스 테스트", () => {
-  test("matchStates에 해당하는 순위 객체를 반환한다", () => {
-    const MATCH_STATES = [
-      "FIVE",
-      "SIX",
-      "SIX",
-      "ONE",
-      "FIVE_AND_BONUS",
-      "FIVE",
-    ];
+  const MATCH_STATES = [
+    "FIVE",
+    "SIX",
+    "FOUR",
+    "ONE",
+    "FIVE_AND_BONUS",
+    "THREE",
+  ];
+  const lottoCalculator = new LottoCalculator(MATCH_STATES);
 
-    const EXPECTED = { 1: 2, 2: 1, 3: 2, 6: 1 };
-
-    const lottoCalculator = new LottoCalculator(MATCH_STATES);
+  test("matchStates에 해당하는 순위 객체를 반환한다.", () => {
+    const EXPECTED = { 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1 };
 
     expect(lottoCalculator.calculateRank()).toEqual(EXPECTED);
+  });
+
+  test("로또 게임 결과 수익률을 반환한다.", () => {
+    const USER_RANKS = { 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1 };
+    const EXPECTED_PROFIT_RATE = "33859250.0";
+    console.log(lottoCalculator.calculateProfitRate(USER_RANKS));
+
+    expect(lottoCalculator.calculateProfitRate(USER_RANKS)).toBe(
+      EXPECTED_PROFIT_RATE
+    );
   });
 });
