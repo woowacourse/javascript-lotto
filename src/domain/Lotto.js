17bd6fc798c5ad2e3ba69f3f5053a5577cd2e36f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-16 11:47:02 +0900

    refactor: Lotto.matcher 함수의 반환 값이 등수가 되도록 변경
    Co-authored-by: Minjae Kim <smallkdb@gmail.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -7,10 +7,12 @@ class Lotto {
     return this.numbers;
   }
 
+  // 개명 필요
   matcher(winningNumber, bonusNumber) {
     const bonus = this.numbers.includes(bonusNumber);
     const match = this.numbers.filter((number) => winningNumber.includes(number)).length;
-    return { match, bonus };
+    const lottoResult = [null, null, null, 5, 4, bonus ? 2 : 3, 1];
+    return lottoResult[match];
   }
 }
 
--- src/domain/lottoGenerator.js
+++ src/domain/lottoGenerator.js
@@ -3,7 +3,7 @@ import randomNumberInRange from "../utils/randomNumberInRange.js";
 const lottoGenerator = () => {
   const numbers = new Set();
   while (numbers.size < 6) {
-    numbers.add(randomNumberInRange(1, 45));
+    numbers.add(String(randomNumberInRange(1, 45)));
   }
 
   return [...numbers].sort((current, next) => current - next);
