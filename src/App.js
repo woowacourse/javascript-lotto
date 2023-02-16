50e0b471686af659b33b812fe44aa913313918d1
cruelladevil <dev.timetravel@gmail.com>
2023-02-16 14:59:07 +0900

    refactor: 커맨드 상수화
    Co-authored-by: 2yunseong <dbsdltjd3701@naver.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Console from './view/console/Console';
 import InputView from './view/console/InputView';
 
 class App {
+  // 고민해볼 것 : 해당 커맨드를 웹 에서는 어떻게 사용할지?, 위치가 컨트롤러로 가야할 지?
+  static RESTART_COMMAND = 'y';
+  static EXIT_COMMAND = 'n';
+
   #lottoController;
 
   constructor() {
@@ -20,10 +24,14 @@ class App {
 
   async readRestartCommand() {
     const command = await InputView.readRestartCommand();
-    if (command === 'y') {
+    this.actionByCommand(command);
+  }
+
+  actionByCommand(command) {
+    if (command === App.RESTART_COMMAND) {
       this.play();
     }
-    if (command === 'n') {
+    if (command === App.EXIT_COMMAND) {
       Console.close();
     }
   }
