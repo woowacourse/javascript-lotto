fb4e31f68b2aee65c7234e5493fc61a1ada3805e
turtle601 <turtle601@naver.com>
2023-02-16 16:17:47 +0900

    refactor(WinningNumbers): WinningNumbers 클래스 테스트를 test.each()로 변경

--- __tests__/WinningNumbers.test.js
+++ __tests__/WinningNumbers.test.js
@@ -1,25 +1,29 @@
 const WinningNumbers = require('../src/domain/WinningNumbers');
 
-test('당첨번호를 쉼표로 구분했을 때 6개가 아닌 경우 예외처리한다.', () => {
-  const winningNumbersInput = '1.2.3.4.5.6';
+describe('WinningNumbers 클래스 테스트', () => {
+  test.each([
+    ['1.2.3.4.5.6'],
+    [''],
+    ['1,2,3,4,5'],
+    ['1,2,3,4,5.6'],
+    ['1,2,3,4,5.46'],
+    ['1,2,3,4,5,5'],
+    ['a,b,c,d,e,f'],
+  ])(
+    '당첨번호를 쉼표로 구분했을 때 1 ~ 45 범위에 속하는 숫자 6개가 아닌 경우 예외처리한다.',
+    (input) => {
+      expect(() => {
+        new WinningNumbers(input);
+      }).toThrow();
+    }
+  );
 
-  expect(() => {
-    new WinningNumbers(winningNumbersInput);
-  }).toThrow();
-});
-
-test('당첨번호를 쉼표로 구분했을 때 6개안 경우 정상 동작한다.', () => {
-  const winningNumbersInput = '1,2,3,4,5,6';
-
-  expect(() => {
-    new WinningNumbers(winningNumbersInput);
-  }).not.toThrow();
-});
-
-test('당첨번호가 중복일 경우 예외처리한다.', () => {
-  const winningNumbersInput = '1,2,3,4,5,5';
-
-  expect(() => {
-    new WinningNumbers(winningNumbersInput);
-  }).toThrow();
+  test.each([['1,2,3,4,5,6'], ['1,2,3,4,5,45'], ['40,41,42,43,44,45']])(
+    '당첨번호를 쉼표로 구분했을 때 1 ~ 45 범위에 속하는 숫자 6개인 경우 정상동작.',
+    (input) => {
+      expect(() => {
+        new WinningNumbers(input);
+      }).not.toThrow();
+    }
+  );
 });
