6ec9738544fa241056888bbc028b498636a103ba
cruelladevil <dev.timetravel@gmail.com>
2023-02-16 11:25:56 +0900

    refactor: validation 메서드 추출
    Co-authored-by: 2yunseong <dbsdltjd3701@naver.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,18 +1,31 @@
-import { isValidLottoNumber } from '../validation';
-
 class Lotto {
   #numbers = [];
 
   constructor(numbers) {
-    if (!numbers.every(isValidLottoNumber)) {
+    if (!this.isValidLottoNumbers(numbers)) {
       throw new Error('[ERROR]: 잘못된 입력입니다.');
     }
-    if (new Set(numbers).size !== 6 || numbers.length !== 6) {
+    if (this.isDuplicateNumbers(numbers)) {
       throw new Error('[ERROR]: 중복된 입력입니다.');
     }
 
     this.#numbers = numbers.sort((a, b) => a - b);
   }
+
+  isDuplicateNumbers(numbers) {
+    return new Set(numbers).size !== 6 || numbers.length !== 6;
+  }
+
+  isValidLottoNumbers(numbers) {
+    return numbers.every(this.isValidLottoNumber);
+  }
+
+  isValidLottoNumber = (number) => {
+    const MIN_NUMBER = 1;
+    const MAX_NUMBER = 45;
+
+    return number >= MIN_NUMBER && number <= MAX_NUMBER;
+  };
 
   getNumbers() {
     return this.#numbers;
--- src/validation.js
+++ src/validation.js
@@ -3,11 +3,4 @@ const isPositiveNumber = (number) => number > 0;
 
 export const isPositiveInteger = (value) => isInteger(value) && isPositiveNumber(value);
 
-export const isValidLottoNumber = (number) => {
-  const MIN_NUMBER = 1;
-  const MAX_NUMBER = 45;
-
-  return number >= MIN_NUMBER && number <= MAX_NUMBER;
-};
-
 export const isValidRestartCommand = (command) => command === 'y' || command === 'n';
