0efb95dde58575be93ecd73c1515520c416670e4
Patrick <kuyt3122@naver.com>
2023-02-16 11:16:32 +0900

    ✨ feat: 구매한 로또의 번호를 출력하는 기능 추가

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -4,6 +4,12 @@ const outputView = {
   printCountLotto(number) {
     Console.print(`${number}개를 구매하셨습니다.`);
   },
+
+  printLottoNumber(lottos) {
+    lottos.forEach(lotto => {
+      Console.print(lotto);
+    });
+  },
 };
 
 export default outputView;
