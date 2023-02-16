c3405caa098d9d2e19908fbfb7c16cdf8a91dae4
solo5star <solo_5@naver.com>
2023-02-16 04:13:22 +0000

    feat: 사용자가 입력한 돈을 검증하는 기능 구현
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- src/Validation.js
+++ src/Validation.js
@@ -1,4 +1,12 @@
 const Validation = {
+  REGEX_NUMERIC: /^\d+$/,
+
+  validateMoney(number) {
+    if (!Validation.REGEX_NUMERIC.test(number) || number < 0) {
+      throw new Error('로또 구매 금액은 0 이상의 정수를 입력해야 한다.');
+    }
+  },
+
   validateDistinctNumbers(lottoNumbers) {
     const lottoSet = new Set(lottoNumbers);
     if (lottoNumbers.length !== lottoSet.size) {
@@ -8,7 +16,7 @@ const Validation = {
 
   validateNumberArray(lottoNumbers) {
     lottoNumbers.forEach((lottoNumber) => {
-      if (!/^\d+$/g.test(lottoNumber)) {
+      if (!Validation.REGEX_NUMERIC.test(lottoNumber)) {
         throw new Error('로또 번호는 정수여야 합니다.');
       }
     });
