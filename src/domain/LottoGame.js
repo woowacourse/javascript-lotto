430170f6dee591efc401e3359a827b3bfd75d2a2
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-16 17:20:25 +0900

    feat: 총 상금을 계산하는 기능 구현

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -44,6 +44,15 @@ class LottoGame {
     if (rank === undefined) return 0;
     return rank;
   }
+
+  calculateTotalPrize(lottoRanksCount) {
+    const prizeMoneys = [0, 2000000000, 30000000, 1500000, 50000, 5000];
+
+    return lottoRanksCount.reduce(
+      (totalPrize, rankCount, rank) => totalPrize + rankCount * prizeMoneys[rank],
+      0
+    );
+  }
 }
 
 module.exports = LottoGame;
