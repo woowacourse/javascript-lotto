f49e1f5c5ed40e5b34d6eca7458763e4ab3dec96
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-16 13:25:48 +0900

    test(Lotto): 테스트 케이스 설명 메시지 수정

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,6 +1,6 @@
 import Lotto from '../src/domain/Lotto.js';
 
-test('getMatchCount', () => {
+test('getMatchCount메서드 실행 시 일치하는 번호의 갯수를 반환한다.', () => {
   const lottoNumbers = [1, 2, 3, 4, 5, 6];
   const lotto = new Lotto(lottoNumbers);
 
