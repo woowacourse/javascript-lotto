a5453d8d55b2b61bc40421d052b1c08b5b78c755
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-16 17:49:51 +0900

    test: 불필요한 주석 제거

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -10,19 +10,6 @@ test('숫자 6개를 받아 로또를 발행한다.', () => {
   expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-// test('구입 금액에 해당하는 만큼 로또를 발행', () => {
-//   const money = 3000;
-//   const TICKET_PRICE = 1000;
-//   const lottoNumbers = [
-//     [1, 2, 3, 4, 5, 6],
-//     [8, 9, 10, 11, 12, 13],
-//     [14, 15, 16, 17, 18, 19],
-//   ];
-//   const lottos = lottoNumbers.map((numbers) => new Lotto(numbers));
-
-//   expect(lottos).toBe(lottoIssuer(money / TICKET_PRICE));
-// });
-
 test('로또 갯수는 6개다.', () => {
   const lotto = [1, 2, 3, 4, 5];
   expect(() => InputValidator.checkArrayLength(lotto, LOTTO.length)).toThrow();
