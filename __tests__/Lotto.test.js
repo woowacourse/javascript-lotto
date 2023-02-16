1619d7d38b8de4edeeba1eabbd60a85c1c703548
turtle601 <turtle601@naver.com>
2023-02-16 10:51:27 +0900

    test(Lotto): LottoGame에서 분리한 메서드들에 대한 테스트 작성

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,15 +1,21 @@
 const Lotto = require('../src/domain/Lotto');
 
 describe('Lotto 클래스 테스트', () => {
-  test('로또에 1 ~ 45까지의 숫자가 아닌 숫자가 포함되면 예외 처리 한다. ', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 66]);
-    }).toThrow();
-  });
-
-  test('로또의 모든 숫자가 1 ~ 45에 포함되면 로또를 생성한다.', () => {
+  test('로또 번호 6개를 인자로 받아 로또를 생성한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
 
     expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('로또 번호와 당첨 번호의 일치 개수를 계산한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.calculateMatchCount([1, 2, 3, 4, 5, 6])).toBe(6);
+  });
+
+  test('보너스 번호가 로또 번호에 존재하는지 판단한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.isBonus(6)).toBe(true);
   });
 });
