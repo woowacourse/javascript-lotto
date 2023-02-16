db749b35fd1b06a17d4555c38e560b2814252e4f
chsua <csj1919@naver.com>
2023-02-16 14:03:37 +0900

    feat: 순위별 당첨자 인원과 수익률 결과 출력 구현

--- src/constants.js
+++ src/constants.js
@@ -11,8 +11,8 @@ const AMOUNT_OF_PURCHASE = (amount) => {
 const ERROR = {
   NUMBER: "[ERROR] 숫자를 입력해주세요.",
   OVER_UNIT: "[ERROR] 천 원 이상 입력해주세요.",
-  SIZE: "[ERROR] 6개를 입력해주세요.",
-  RANGE: "[ERROR] 1부터 45 사이의 값을 입력하세요.",
+  SIZE: "[ERROR] 1 ~ 45 사이의 서로 다른 숫자 6개를 입력해주세요.",
+  RANGE: "[ERROR] 1 ~ 45 사이의 값을 입력하세요.",
   DUPLICATED: "[ERROR] 중복된 숫자가 있습니다.",
   DIVIDE: "[ERROR] 로또 1매 가격으로 나누어 떨어지는 금액을 입력하세요.",
   RESTART: "[ERROR] y 혹은 n으로 입력해주세요.",
@@ -23,6 +23,7 @@ const NUMBER = {
   MIN_RANGE: 1,
   MAX_RANGE: 45,
   SIZE: 6,
+  RANK: 5,
 };
 
 const COMMAND = {
@@ -30,10 +31,13 @@ const COMMAND = {
   NO: "n",
 };
 
+const PRIZE = [5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000];
+
 module.exports = {
   NUMBER,
   MESSAGE,
   ERROR,
   AMOUNT_OF_PURCHASE,
   COMMAND,
+  PRIZE,
 };
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,12 +3,18 @@ const { AMOUNT_OF_PURCHASE } = require("../constants");
 
 const OutputView = {
   printError(error) {
-    Console.print(error.message);
+    Console.print(error);
   },
 
   printLotteries(lotteries) {
     Console.print(AMOUNT_OF_PURCHASE(lotteries.length));
     lotteries.forEach((lotto) => Console.print(`[${lotto}]`));
+  },
+
+  printResult(lottoResult) {
+    Console.print(
+      `\n당첨 통계\n--------------------\n3개 일치 (5,000원) - ${lottoResult[0]}개\n4개 일치 (50,000원) - ${lottoResult[1]}개\n5개 일치 (1,500,000원) - ${lottoResult[2]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult[3]}개\n6개 일치 (2,000,000,000원) - ${lottoResult[4]}개\n총 수익률은 ${lottoResult[5]}%입니다.`
+    );
   },
 };
 
