bab308052a4e6a2fbdbe0906f04c10ed0b337c23
solo5star <solo_5@naver.com>
2023-02-16 06:07:18 +0000

    refactor: LottoFactory 에서 사용하지 않는 constructor 제거
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- src/domain/LottoFactory.js
+++ src/domain/LottoFactory.js
@@ -2,8 +2,6 @@ import { shuffle } from '../utils/Random';
 import Lotto from './Lotto';
 
 class LottoFactory {
-  constructor() {}
-
   createRandomLotto() {
     const allLottoNumbers = Array.from({ length: 45 }, (_, i) => i + 1);
     const lottoNumbers = shuffle(allLottoNumbers).slice(0, 6);
