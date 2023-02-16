a6df61f3ce3279fb50f2404ac8bc99397d61f46e
turtle601 <turtle601@naver.com>
2023-02-16 11:56:27 +0900

    refactor(LottoStatistics): 당첨번호와 보너스 번호를 비교하여 유효성 검사 기능 메서드 분리

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -10,18 +10,6 @@ class LottoGame {
 
   #winningLottos = [0, 0, 0, 0, 0, 0];
 
-  validateBonusNumber(winningNumbers, bonusNumber) {
-    if (this.#isDuplicateFor(winningNumbers, bonusNumber)) {
-      throw new Error(
-        '[ERROR] 당첨 번호와 보너스 번호에 중복이 존재할 수 없습니다.'
-      );
-    }
-  }
-
-  #isDuplicateFor(winningNumbers, bonusNumber) {
-    return winningNumbers.includes(bonusNumber);
-  }
-
   async play() {
     await this.inputPurchasePrice();
     this.showPurchasedLottos();
