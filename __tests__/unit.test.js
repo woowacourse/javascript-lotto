a59c5ab0aa6fc18e41fd3d3803ebb31b74b842cf
chsua <csj1919@naver.com>
2023-02-16 10:41:42 +0900

    test: 보너스번호가 복권에 포함되는지 확인하는 함수 테스트 작성

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -29,17 +29,26 @@ describe("로또에 넣을 랜덤숫자 생성되는지 확인", () => {
   });
 });
 
-describe("복권과 당첨번호(유저입력)이 일치하는 갯수 반환 테스트", () => {
+describe("복권이 당첨번호, 보너스번호와 일치하는지 테스트", () => {
   const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-  test("3개 일치하는 경우", () => {
+  test("복권과 당첨번호(유저입력)이 일치하는 개수가 3개인 경우 9 반환", () => {
     expect(lotto.matchNumbers([1, 2, 3, 7, 8, 9])).toEqual(9);
   });
 
-  test("일치하는 번호가 없는 경우", () => {
+  test("복권과 당첨번호(유저입력)이 일치하는 개수가 없는 경우 12 반환", () => {
     expect(lotto.matchNumbers([7, 8, 9, 10, 11, 12])).toEqual(12);
   });
 
-  test("전부 일치하는 경우", () => {
+  test("복권과 당첨번호(유저입력)이 일치하는 개수가 6개인 경우 6 반환", () => {
     expect(lotto.matchNumbers([1, 2, 3, 4, 5, 6])).toEqual(6);
+  });
+
+  test("보너스번호가 복권에 있으면 true", () => {
+    console.log(lotto.matchBonus(1));
+    expect(lotto.matchBonus(1)).toBeTruthy();
+  });
+
+  test("보너스번호가 복권에 없으면 false", () => {
+    expect(lotto.matchBonus(7)).toBeFalsy();
   });
 });
