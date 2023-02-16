9d77be11d1cfef3c69c6c09a1672d6fc1382f5e7
solo5star <solo_5@naver.com>
2023-02-16 06:05:28 +0000

    test: Validation 테스트 추가
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -1,15 +1,6 @@
 import WinningLotto from '../src/domain/WinningLotto';
 
 describe('WinningLotto 클래스에 대한 테스트', () => {
-  test.each([0, 46, -3])(
-    '보너스 번호는 1에서 45 사이의 숫자가 아닌 경우 예외를 던져야 한다',
-    (bonusNumber) => {
-      const lottoNumbers = [1, 2, 3, 4, 5, 6];
-
-      expect(() => new WinningLotto(lottoNumbers, bonusNumber)).toThrowError();
-    },
-  );
-
   test.each([
     [[1, 2, 3, 4, 5, 6], 5],
     [[1, 2, 3, 4, 5, 6], 1],
