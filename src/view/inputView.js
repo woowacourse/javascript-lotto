93cca407a052c1ea37ab62462247229b716ba2d6
Patrick <kuyt3122@naver.com>
2023-02-16 11:21:04 +0900

    ✨ feat: 당첨 번호 입력 기능 추가

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -3,9 +3,15 @@ import Console from '../utils/Console.js';
 const inputView = {
   readMoney() {
     Console.readLine('구입금액을 입력해 주세요.', input => {
-      Console.print(input);
+      return input
     });
   },
+
+  readWinningNumber() {
+    Console.readLine('당첨 번호를 입력해 주세요. ', input => {
+        return input
+    })
+  }
 };
 
 export default inputView;
