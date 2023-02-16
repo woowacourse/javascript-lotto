201b756a824e5736d867a4a4a352a822ccffd748
cruelladevil <dev.timetravel@gmail.com>
2023-02-16 14:53:59 +0900

    refactor: OutputView 리터럴 상수화
    Co-authored-by: 2yunseong <dbsdltjd3701@naver.com>

--- src/view/console/OutputView.js
+++ src/view/console/OutputView.js
@@ -1,40 +1,61 @@
 import Console from './Console';
 
 const OutputView = {
-  // winCondition, winMoney, winCount는 상수화(맵)되어야 하며,
-  // OutputView 바깥에서 받아야함.
-  winCondition: {
+  WIN_TITLE: '당첨 통계\n--------------------',
+  WIN_CONDITION: {
     1: '6개 일치',
     2: '5개 일치, 보너스 볼 일치',
     3: '5개 일치',
     4: '4개 일치',
     5: '3개 일치',
   },
-  winMoney: { 1: 2000000000, 2: 30000000, 3: 1500000, 4: 50000, 5: 5000 },
+
+  generateNumbersMessage(lotto) {
+    return `[${lotto.join(', ')}]`;
+  },
+
+  generateBuyAmountMessage(amount) {
+    return `${amount}개를 구매했습니다.`;
+  },
+
+  generateProfitRateMessage(profitRate) {
+    return `총 수익률은 ${profitRate.toFixed(2)}%입니다.`;
+  },
   // to-do : 메서드명 나중에 변경하기
   printBuyLottos(lottos) {
-    Console.print(`${lottos.length}개를 구매했습니다.`);
+    Console.print(this.generateBuyAmountMessage(lottos.length));
     lottos.forEach((lotto) => {
-      Console.print(`[${lotto.join(', ')}]`);
+      Console.print(OutputView.generateNumbersMessage(lotto));
     });
   },
 
   printWinTitle() {
-    Console.print('당첨 통계');
-    Console.print('--------------------');
+    Console.print(OutputView.WIN_TITLE);
+  },
+
+  generateWinPrizeMoneyMessage(winPrizeMoney, rank) {
+    return `(${winPrizeMoney[rank].toLocaleString('ko-KR')}원)`;
+  },
+
+  generateWinCountMessage(winCount, rank) {
+    return `${winCount[rank]}개`;
   },
 
   printWinStatistics({ winCount, winPrizeMoney, profitRate }) {
-    const results = Array.from({ length: 5 }, (_, i) => {
-      return `${winCount[i + 1]} ${this.winCondition[i + 1]} ${winPrizeMoney[i + 1]}`;
+    this.printWinTitle();
+    const rankLength = 5;
+    const results = Array.from({ length: rankLength }, (_, i) => {
+      return `${this.WIN_CONDITION[rankLength - i]} ${this.generateWinPrizeMoneyMessage(
+        winPrizeMoney,
+        rankLength - i,
+      )} - ${this.generateWinCountMessage(winCount, rankLength - i)}`;
     });
-
     results.forEach((result) => Console.print(result));
     this.printProfitRate(profitRate);
   },
 
   printProfitRate(profitRate) {
-    Console.print(profitRate.toFixed(2));
+    Console.print(this.generateProfitRateMessage(profitRate));
   },
 
   printEmptyLine() {
