ecac14fa69199719920c68f36c39b17df8866d40
gyeongza <gyeongza@naver.com>
2023-02-16 13:52:29 +0900

    test : 보너스 번호가 중복된 숫자인지 판별하는 함수 테스트

--- __tests__/Validations.test.js
+++ __tests__/Validations.test.js
@@ -35,3 +35,11 @@ describe("당첨번호 Validations 단위 테스트", () => {
     expect(Validations.isCorrectRange(winningNumber)).toBeFalsy();
   });
 });
+
+describe("보너스 번호 Validations 단위 테스트", () => {
+  test("보너스 번호가 중복된 숫자인지 판별하는 함수 테스트", () => {
+    const winningLotto = [1,2,3,4,5,6];
+    const bonusNumber = 1;
+    expect(Validations.hasBonusNumber(bonusNumber, winningLotto)).toBeTruthy();
+  });
+});
