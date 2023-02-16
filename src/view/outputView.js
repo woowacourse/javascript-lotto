ac65065042423ec7a0ebc91e60bd057676b450e9
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-16 10:52:51 +0900

    ✨ feat(outputView): 로또 당첨 결과 문장을 반환한다

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -13,6 +13,23 @@ const outputView = {
   printLotto(lotto) {
     Console.print(`[${lotto.join(MAGIC_LITERAL.separator)}]`);
   },
+
+  printResultTitle() {
+    Console.print('당첨 통계\n--------------------\n');
+  },
+
+  getResultLine(rank, rankCount) {
+    if (rank === MAGIC_NUMBER.secondRankIndex) {
+      return `${
+        RANK_INFORMATIONS[rank].matchedCount
+      }개 일치, 보너스 볼 일치 (${RANK_INFORMATIONS[
+        rank
+      ].reward.toLocaleString()}원) - ${rankCount}개`;
+    }
+    return `${RANK_INFORMATIONS[rank].matchedCount}개 일치 (${RANK_INFORMATIONS[
+      rank
+    ].reward.toLocaleString()}원) - ${rankCount}개`;
+  },
 };
 
 module.exports = outputView;
