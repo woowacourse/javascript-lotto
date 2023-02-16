0d12854441460d5edb6ae7e2fe939c70f189a65b
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-16 17:29:04 +0900

    feat: 도메인과 뷰를 연결하는 컨트롤러 작성

---
+++ src/controller/LottoGameController.js
@@ -0,0 +1,66 @@
+const InputView = require('../view/InputView');
+const OutputView = require('../view/OutputView');
+const LottoGame = require('../domain/LottoGame');
+const Console = require('../util/Console');
+const InputValidator = require('../validators/InputValidator');
+const LottoTicket = require('../domain/LottoTicket');
+
+class LottoGameController {
+  play() {
+    InputView.readUserBudget(this.#onSubmitUserBudget.bind(this));
+  }
+
+  #onSubmitUserBudget(userBudget) {
+    try {
+      InputValidator.checkUserBudget(userBudget);
+      this.lottoGame = new LottoGame(Number(userBudget));
+      const lottoTickets = this.lottoGame.getLottoTickets();
+      OutputView.printLottoTicketCount(lottoTickets.length);
+      OutputView.printLottoTickets(lottoTickets);
+      InputView.readLottoWinningNumber(this.#onSubmitLottoWinningNumber.bind(this));
+    } catch (error) {
+      Console.print(error.message);
+      this.play();
+    }
+  }
+
+  #onSubmitLottoWinningNumber(winningNumber) {
+    try {
+      InputValidator.checkWinningNumber(winningNumber);
+      this.winningNumber = new LottoTicket(winningNumber.split(',').map(Number));
+      InputView.readLottoBonusNumber(this.#onSubmitLottoBonusNumber.bind(this));
+    } catch (error) {
+      Console.print(error.message);
+      InputView.readLottoWinningNumber(this.#onSubmitLottoWinningNumber.bind(this));
+    }
+  }
+
+  #onSubmitLottoBonusNumber(bonusNumber) {
+    try {
+      InputValidator.checkBonusNumber(this.winningNumber.getNumbers(), bonusNumber);
+      const lottoRanksCount = this.lottoGame.countLottoRanks(
+        this.winningNumber.getNumbers(),
+        bonusNumber
+      );
+      OutputView.printResultTitle();
+      OutputView.printLottoRanksResult(lottoRanksCount);
+      OutputView.printProfitRate(
+        this.lottoGame.calculateProfitRate(this.lottoGame.calculateTotalPrize(lottoRanksCount))
+      );
+      InputView.readRestartCommand(this.#onSubimtRestartCommand.bind(this));
+    } catch (error) {
+      Console.print(error.message);
+      InputView.readLottoBonusNumber(this.#onSubmitLottoBonusNumber.bind(this));
+    }
+  }
+
+  #onSubimtRestartCommand(command) {
+    if (command === 'y') {
+      this.play();
+      return;
+    }
+    Console.close();
+  }
+}
+
+module.exports = LottoGameController;
