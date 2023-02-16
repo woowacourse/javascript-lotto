4ca450e1fb503c17b42879f7f50835899f90f61e
unknown <tkdrb15@gmail.com>
2023-02-16 16:08:16 +0900

    test:도메인 로직 기능 테스트 작성

--- __tests__/errorhandler.test.js
+++
@@ -1,3 +0,0 @@
-const Errorhandler = require("../src/domain/Errorhandler");
-
-describe("입력 값 유효성 테스트", () => {});
--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,6 +1,6 @@
 const Lotto = require("../src/domain/Lotto");
 
-describe("로또 기능 테스트", () => {
+describe("로또 클래스 테스트", () => {
   test("배열이 전달되면 로또가 생성되는지 테스트", () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
 
@@ -11,13 +11,5 @@ describe("로또 기능 테스트", () => {
     const lotto = new Lotto([2, 1, 3, 6, 5, 4]);
 
     expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
-  });
-});
-
-describe("로또 유효성 테스트", () => {
-  test("로또 번호들의 범위가 1-45가 아니면 에러를 발생시킨다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 47]);
-    }).toThrow();
   });
 });
