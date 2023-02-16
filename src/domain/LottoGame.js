f2e8cd223fd4aa88cd99a2d6446fdf6e727eab39
unknown <tkdrb15@gmail.com>
2023-02-16 11:25:09 +0900

    feat: LottoGame 클래스로 변경 및 메서드 위치 이동

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -2,9 +2,21 @@ const Lotto = require("./Lotto");
 const Random = require("../util/Random");
 const WinLotto = require("../domain/WinLotto");
 
-const LottoGame = {
-  lottos: [],
-  rankResult: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
+const RANK_RESULT = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+const PRIZE = { 1: 2000000000, 2: 30000000, 3: 1500000, 4: 50000, 5: 5000 };
+
+class LottoGame {
+  #lottos;
+  #rankResult;
+  #winLottos;
+
+  constructor(rankResult) {
+    this.#rankResult = { ...RANK_RESULT };
+  }
+
+  get rankResult() {
+    return this.#rankResult;
+  }
 
   LottoNumberGenerator() {
     const lottoNumbers = new Set();
@@ -12,26 +24,53 @@ const LottoGame = {
       lottoNumbers.add(Random.RandomMinMax(1, 45));
     }
     return Array.from(lottoNumbers);
-  },
+  }
 
   makeLottos(lottoCount) {
-    this.lottos = [];
     Array.from({ length: lottoCount }, () => {
       const lottoOne = new Lotto(this.LottoNumberGenerator());
       this.lottos.push(lottoOne);
     });
-  },
+  }
 
   makeWinLotto(winningNumbers, bonusNumber) {
     const winlotto = new WinLotto(winningNumbers, bonusNumber);
-    this.rankResult = winlotto.calculateWinLotto(this.lottos, this.rankResult);
+  }
 
-    const revenue = winlotto.calculateRevenue(
-      this.rankResult,
-      this.lottos.length
+  calculateRevenueRate() {
+    this.calculateWinLotto();
+    const revenue = this.calculateRevenueRate();
+
+    return revenue;
+  }
+
+  calculateWinLotto() {
+    this.#lottos.forEach((lotto) => {
+      const rank = this.calculateRank(lotto);
+      this.#rankResult[rank] += 1;
+    });
+  }
+
+  calculateRank(lotto) {
+    const winNumbers = this.#winLottos.numbers;
+    const sameNumbers = lotto.numbers.filter((num) => winNumbers.includes(num));
+
+    if (sameNumbers.length === 6) return 1;
+    if (sameNumbers.length === 5)
+      return lotto.numbers.includes(this.#winLottos.bonusNumber) ? 2 : 3;
+    if (sameNumbers.length === 4) return 4;
+    if (sameNumbers.length === 3) return 5;
+    if (sameNumbers.length < 3) return 0;
+  }
+
+  calculateRevenueRate() {
+    const revenue = Object.keys(PRIZE).reduce(
+      (result, current) => result + PRIZE[current] * this.#rankResult[current],
+      0
     );
-    return [revenue, this.rankResult];
-  },
-};
+
+    return (revenue / (this.#lottos.length * 10)).toFixed(1);
+  }
+}
 
 module.exports = LottoGame;
