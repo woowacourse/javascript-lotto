c1a717a572978ae03aebb28d444ca59a4306eaf3
solo5star <solo_5@naver.com>
2023-02-16 05:37:47 +0000

    feat: 에러 발생시 재입력을 받을 수 있게 InputView 수정
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,38 +1,51 @@
+import Lotto from '../domain/Lotto';
 import WinningLotto from '../domain/WinningLotto';
 import Console from '../utils/Console';
 import Validation from '../Validation';
 
 const InputView = {
   async readMoney() {
-    const money = await Console.readLine('구입금액을 입력해 주세요.');
-    Validation.validateMoney(money);
-    return Number(money);
+    return Console.repeatWhile(async () => {
+      const money = await Console.readLine('구입금액을 입력해 주세요.');
+      Validation.validateMoney(money);
+      return Number(money);
+    });
   },
 
   async readWinningLotto() {
-    const winningNumbers = await this.readWinningNumbers();
-    const bonusNumber = await this.readBonusNumber();
+    return Console.repeatWhile(async () => {
+      const lotto = await this.readLotto();
+      const bonusNumber = await this.readBonusNumber();
 
-    return new WinningLotto(winningNumbers, bonusNumber);
+      return new WinningLotto(lotto.getLottoNumbers(), bonusNumber);
+    });
   },
 
-  async readWinningNumbers() {
-    const readWinningNumbers = await Console.readLine('당첨 번호를 입력해 주세요. ');
-    return readWinningNumbers
-      .split(',')
-      .map((number) => number.trim())
-      .map(Number);
+  async readLotto() {
+    return Console.repeatWhile(async () => {
+      const readWinningNumbers = await Console.readLine('당첨 번호를 입력해 주세요. ');
+      const lottoNumbers = readWinningNumbers
+        .split(',')
+        .map((number) => number.trim())
+        .map(Number);
+      return new Lotto(lottoNumbers);
+    });
   },
 
   async readBonusNumber() {
-    const bonusNumber = await Console.readLine('보너스 번호를 입력해 주세요.');
-    return Number(bonusNumber);
+    return Console.repeatWhile(async () => {
+      const bonusNumber = await Console.readLine('보너스 번호를 입력해 주세요.');
+      Validation.validateBonusNumber(bonusNumber);
+      return Number(bonusNumber);
+    });
   },
 
   async readRestartCommand() {
-    const restartCommand = await Console.readLine('다시 시작하시겠습니까? (y/n) ');
-    Validation.validateRestartCommand(restartCommand);
-    return restartCommand;
+    return Console.repeatWhile(async () => {
+      const restartCommand = await Console.readLine('다시 시작하시겠습니까? (y/n) ');
+      Validation.validateRestartCommand(restartCommand);
+      return restartCommand;
+    });
   },
 };
 
