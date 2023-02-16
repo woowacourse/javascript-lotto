8d5cfb711f02e5c8eafe0cc878ee95cb731cf0a1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-16 12:23:34 +0900

    refactor: printResult 인자값 변경으로 인한 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -8,16 +8,16 @@ const OutputView = {
     Console.print(numbers);
   },
   // eslint-disable-next-line max-lines-per-function
-  printResult(matchResult) {
+  printResult(matchResult, benefit) {
     Console.print(`
 당첨 통계
 --------------------
-3개 일치 (5,000원) - ${matchResult[2]}개
-4개 일치 (50,000원) - ${matchResult[3]}개
-5개 일치 (1,500,000원) - ${matchResult[4]}개
-5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchResult[6]}개
-6개 일치 (2,000,000,000원) - ${matchResult[5]}개
-총 수익률은 62.5%입니다.
+3개 일치 (5,000원) - ${matchResult[5]}개
+4개 일치 (50,000원) - ${matchResult[4]}개
+5개 일치 (1,500,000원) - ${matchResult[3]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchResult[2]}개
+6개 일치 (2,000,000,000원) - ${matchResult[1]}개
+총 수익률은 ${benefit}%입니다.
 `);
   },
   printNewLine() {
