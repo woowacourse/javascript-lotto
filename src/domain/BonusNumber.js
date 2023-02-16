30880c7d3cfbf75fb1a9b9c897dd1daab1bf47ee
turtle601 <turtle601@naver.com>
2023-02-16 14:11:43 +0900

    refactor(BonusNumber): 당첨번호와 보너스 번호 중복 검사 기능 구현

--- src/domain/BonusNumber.js
+++ src/domain/BonusNumber.js
@@ -1,19 +1,28 @@
 class BonusNumber {
   #bonusNumber = 0;
 
-  constructor(bonusNumberInput) {
-    this.#validate(parseInt(bonusNumberInput, 10));
+  constructor(bonusNumberInput, winningNumbers) {
+    this.#validate(parseInt(bonusNumberInput, 10), winningNumbers);
     this.#bonusNumber = parseInt(bonusNumberInput, 10);
   }
 
-  #validate(bonusNumber) {
+  #validate(bonusNumber, winningNumbers) {
     if (!this.#isValidBonusNumber(bonusNumber)) {
       throw new Error('[ERROR] 보너스 번호는 1 ~ 45 이내의 숫자여야 합니다.');
+    }
+    if (this.#isDuplicateFor(bonusNumber, winningNumbers)) {
+      throw new Error(
+        '[ERROR] 보너스 번호와 당첨번호가 중복이 되어서는 안됩니다.'
+      );
     }
   }
 
   #isValidBonusNumber(bonusNumber) {
     return bonusNumber >= 1 && bonusNumber <= 45;
+  }
+
+  #isDuplicateFor(bonusNumber, winningNumbers) {
+    return winningNumbers.winningNumbers.includes(bonusNumber);
   }
 
   get bonusNumber() {
