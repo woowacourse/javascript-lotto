50e5e9c51d6f918b3dbc9bb3c97cef80ad01b2f5
gyeongza <gyeongza@naver.com>
2023-02-16 10:58:13 +0900

    feat : 구매금액 Error처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ class App {
 
   async inputBuyMoney() {
     const buyMoney = await InputView.userInput("구입금액을 입력해 주세요.");
+    try {
+      this.validateBuyMoney(buyMoney);
+    } catch (e) {
+      Console.print(e);
+      await this.inputBuyMoney();
+    }
+  }
+
+  validateBuyMoney(buyMoney) {
+    if (!Validations.isBuyMoneyNumber(buyMoney)) {
+      throw new Error("숫자만 입력할 수 있습니다.");
+    }
+    if (!Validations.isDevidedByThousand(buyMoney)) {
+      throw new Error("1000원 단위로 입력해주세요.");
+    }
+    if (!Validations.isPositiveInteger(buyMoney)) {
+      throw new Error("구매 금액은 양의 정수여야 합니다.");
+    }
   }
 }
+
 export default App;
