69ee50f2226089a1126649aec69a20d5e8457a73
solo5star <solo_5@naver.com>
2023-02-16 01:57:40 +0000

    test: 사용자가 로또에 대한 보상을 받은 후 수익률을 계산하는 테스트 추가
    Co-authored-by: xodms0309 <xodms0309@naver.com>

--- __tests__/Buyer.test.js
+++ __tests__/Buyer.test.js
@@ -1,4 +1,17 @@
 import Buyer from '../src/domain/Buyer';
+import Lotto from '../src/domain/Lotto';
+import LottoFactory from '../src/domain/LottoFactory';
+import LottoResult from '../src/domain/LottoResult';
+
+const mockLottoFactory = (lottos) => {
+  const remainLottos = [...lottos];
+
+  return new (class extends LottoFactory() {
+    createRandomLotto() {
+      return remainLottos.shift();
+    }
+  })();
+};
 
 describe('Buyer 클래스 테스트', () => {
   test('사용자는 로또를 구매할 수 있다.', () => {
@@ -12,6 +25,28 @@ describe('Buyer 클래스 테스트', () => {
 
     // then
     expect(buyer.getLottos()).toHaveLength(amount);
+  });
+
+  test('사용자는 로또에 대한 보상을 받은 후 수익률을 계산할 수 있다.', () => {
+    // given
+    const money = 5000;
+    const buyer = new Buyer(money);
+    const lottoFactory = mockLottoFactory([
+      new Lotto([1, 2, 3, 10, 11, 12]), // 3개
+      new Lotto([20, 21, 22, 23, 24, 25]), // 0개
+      new Lotto([1, 2, 3, 4, 5, 7]), // 5개+보너스 번호
+      new Lotto([7, 8, 9, 10, 11, 12]), // 0개
+      new Lotto([30, 31, 32, 33, 34, 35]), // 0개
+    ]);
+    const lottoResult = new LottoResult([1, 2, 3, 4, 5, 6], 7);
+
+    // when
+    buyer.buyLottos(lottoFactory);
+    buyer.receiveRewards(lottoResult);
+    const profitRate = buyer.getProfitRate();
+
+    // then
+    expect(profitRate).toBe(6001);
   });
 });
 
