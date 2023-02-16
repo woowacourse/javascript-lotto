5030d015ab8f98bc2b0165e971737379da12b79c
gyeongza <gyeongza@naver.com>
2023-02-16 14:05:42 +0900

    test : 보너스번호가 당첨번호와 중복될 경우 Error처리 테스트

--- __tests__/Error.test.js
+++ __tests__/Error.test.js
@@ -26,8 +26,8 @@ describe("구입금액 Error처리 테스트", () => {
   });
 });
 
-describe("당첨번호 Error처리 테스트", () => {
-    test("당첨번호가 숫자가 아닐 경우 Error처리 테스트", () => {
+describe("당첨로또 Error처리 테스트", () => {
+    test("당첨번호와 보너스번호가 숫자가 아닐 경우 Error처리 테스트", () => {
       const eachNumber = 'a'
       const app = new App();
       expect(() => {
@@ -35,7 +35,7 @@ describe("당첨번호 Error처리 테스트", () => {
       }).toThrowError("숫자만 입력할 수 있습니다.");
     });
   
-    test("당첨번호가 해당 범위를 벗어날 경우 Error처리 테스트", () => {
+    test("당첨번호와 보너스번호가 해당 범위를 벗어날 경우 Error처리 테스트", () => {
       const eachNumber = 47;
       const app = new App();
       expect(() => {
@@ -43,11 +43,20 @@ describe("당첨번호 Error처리 테스트", () => {
       }).toThrowError("당첨번호는 1~45까지의 범위입니다.");
     });
   
-    test("당첨번호가 양의 정수가 아닐 경우 Error처리 테스트", () => {
+    test("당첨번호와 보너스번호가 양의 정수가 아닐 경우 Error처리 테스트", () => {
       const eachNumber = 44.5;
       const app = new App();
       expect(() => {
         app.checkEachNumber(eachNumber);
       }).toThrowError("당첨번호는 양의 정수여야 합니다.");
     });
+
+    test("보너스번호가 당첨번호와 중복될 경우 Error처리 테스트", () => {
+        const bonusNumber = 9;
+        const winningLotto = [1,2,3,4,5,9]
+        const app = new App();
+        expect(() => {
+          app.validateBonusNumber(bonusNumber,winningLotto);
+        }).toThrowError("보너스 번호는 당첨번호와 중복되지 않아야합니다.");
+      });
   });
