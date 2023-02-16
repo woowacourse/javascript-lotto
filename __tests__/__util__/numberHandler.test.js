5707c131e1705fe43f073a5a101a4066ce41e856
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-02-16 13:30:57 +0900

    chore: 개발 환경 수정

--- __tests__/__util__/numberHandler.test.js
+++ __tests__/__util__/numberHandler.test.js
@@ -1,13 +1,13 @@
-import NumberHandler from "../../src/util/NumberHandler.js";
+import NumberHandler from '../../src/util/NumberHandler.js';
 
-describe("NumberHandler 테스트", () => {
+describe('NumberHandler 테스트', () => {
   test.each([
     [1.123, 1.1],
     [1.56, 1.6],
     [1.0, 1],
     [100.05, 100.1],
     [100, 100],
-  ])("소수점 둘째자리에서 반올림한다.", (floatNumber, roundedOffNumber) => {
+  ])('소수점 둘째자리에서 반올림한다.', (floatNumber, roundedOffNumber) => {
     expect(NumberHandler.roundOffNumber(floatNumber)).toBe(roundedOffNumber);
   });
 
@@ -27,10 +27,9 @@ describe("NumberHandler 테스트", () => {
     [NumberHandler.generateRandomNumber(1, 45)],
     [NumberHandler.generateRandomNumber(1, 45)],
     [NumberHandler.generateRandomNumber(1, 45)],
-  ])("generateRandomNumber가 1~45사이의 숫자를 반환한다.", (randomNumber) => {
+  ])('generateRandomNumber가 1~45사이의 숫자를 반환한다.', randomNumber => {
     const rangeNumbers = Array.from({ length: 45 }, (_, index) => index + 1);
 
-    console.log(randomNumber);
     expect(rangeNumbers.includes(randomNumber)).toBeTruthy();
   });
 
@@ -40,17 +39,17 @@ describe("NumberHandler 테스트", () => {
    */
 
   test.each([
-    [1, "1"],
-    [10, "10"],
-    [100, "100"],
-    [1000, "1,000"],
-    [10000, "10,000"],
-    [100000, "100,000"],
-    [1000000, "1,000,000"],
-  ])("천의 자리마다 콤마를 찍어 문자열로 반환한다.", (number, string) => {
+    [1, '1'],
+    [10, '10'],
+    [100, '100'],
+    [1000, '1,000'],
+    [10000, '10,000'],
+    [100000, '100,000'],
+    [1000000, '1,000,000'],
+  ])('천의 자리마다 콤마를 찍어 문자열로 반환한다.', (number, string) => {
     const numberString = NumberHandler.addComma(number);
 
     expect(numberString).toBe(string);
-    expect(typeof numberString).toBe("string");
+    expect(typeof numberString).toBe('string');
   });
 });
