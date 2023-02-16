d532f6b7bfa22c9dbe7e34d8aa9a825feecefcf6
Patrick <kuyt3122@naver.com>
2023-02-16 11:07:43 +0900

    ✨ feat: 구매한 로또의 갯수 출력 기능 추가

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,12 +1,11 @@
 import Console from '../utils/Console.js';
 
-class InputView {
+const inputView = {
   readMoney() {
     Console.readLine('구입금액을 입력해 주세요.', input => {
       Console.print(input);
     });
-  }
-}
+  },
+};
 
-const inputView = new InputView();
-inputView.readMoney();
+export default inputView;
---
+++ src/view/outputView.js
@@ -0,0 +1,9 @@
+import Console from '../utils/Console.js';
+
+const outputView = {
+  printCountLotto(number) {
+    Console.print(`${number}개를 구매하셨습니다.`);
+  },
+};
+
+export default outputView;
