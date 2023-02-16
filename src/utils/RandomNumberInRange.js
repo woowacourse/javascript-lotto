c7f8aa71d7a9f10de3ca865217f6cbd1984a367a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-16 14:44:59 +0900

    refactor: randomNumberInRange 로직 개선

--- src/utils/RandomNumberInRange.js
+++ src/utils/RandomNumberInRange.js
@@ -1,5 +1,5 @@
 const randomNumberInRange = (min, max) => {
-  return Math.floor(Math.random() * max + 1);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 export default randomNumberInRange;
