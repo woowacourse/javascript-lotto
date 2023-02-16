e2743f136df96fde77a2245b12213ca023a962c0
solo5star <solo_5@naver.com>
2023-02-16 02:28:11 +0000

    feat: 사용자의 입력을 받아오는 InputView 생성
    Co-authored-by: xodms0309 <xodms0309@naver.com>

---
+++ src/view/InputView.js
@@ -0,0 +1,25 @@
+import Console from '../utils/Console';
+
+const InputView = {
+  async readMoney() {
+    const money = await Console.readLine('구입금액을 입력해 주세요.');
+    return Number(money);
+  },
+
+  async readWinningNumbers() {
+    const readWinningNumbers = await Console.readLine('당첨 번호를 입력해 주세요. ');
+    return readWinningNumbers.split(',').map(Number);
+  },
+
+  async readBonusNumber() {
+    const bonusNumber = await Console.readLine('보너스 번호를 입력해 주세요.');
+    return Number(bonusNumber);
+  },
+
+  async readRestartConfirm() {
+    const restartConfirm = await Console.readLine('다시 시작하시겠습니까? (y/n) ');
+    return restartConfirm;
+  },
+};
+
+export default InputView;
