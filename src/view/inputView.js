015eeeeee7c0970d692dad17c35387ee06fb20b2
Patrick <kuyt3122@naver.com>
2023-02-16 11:24:15 +0900

    ✨ feat: 보너스 번호 입력 기능 추가

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -3,15 +3,23 @@ import Console from '../utils/Console.js';
 const inputView = {
   readMoney() {
     Console.readLine('구입금액을 입력해 주세요.', input => {
-      return input
+      return input;
     });
   },
 
   readWinningNumber() {
     Console.readLine('당첨 번호를 입력해 주세요. ', input => {
-        return input
-    })
-  }
+      return input;
+    });
+  },
+
+  readBonusNumber() {
+    Console.readLine('보너스 번호를 입력해 주세요. ', input => {
+      Console.print(input);
+    });
+  },
 };
+
+inputView.readBonusNumber();
 
 export default inputView;
