5da5f65caf98b6ada96f669f29ee854bdec3a5d4
shackstack <kms1061@naver.com>
2023-02-16 13:53:58 +0900

    refactor: 에러메세지 상수화

--- src/constant/message.js
+++ src/constant/message.js
@@ -21,6 +21,16 @@ const OUTPUT_MESSAGE_METHOD = {
     `총 수익률은 ${rate.toLocaleString(undefined, { minimumFractionDigits: 1 })}%입니다.`,
 };
 
-const ERROR_MESSAGE = {};
+const ERROR_MESSAGE = {
+  LESS_THAN_MINIMUM: '[ERROR] 구입 금액은 1,000원 이상이어야 합니다.',
+  HAS_CHANGE: '[ERROR] 구입 금액은 1,000원 단위이어야 합니다.',
+  OUT_OF_RANGE: '[ERROR] 1~45사이의 숫자이어야 합니다.',
+  INVALID_WINNING_NUMBER_LENGTH: '[ERROR] 당첨 번호는 6개의 숫자로 이루어져야 합니다.',
+  DUPLICATED_NUMBER: '[ERROR] 중복된 번호입니다.',
+  INVALID_RESTART_COMMAND: '[ERROR] 재시작 명령어는 y또는 n이어야 합니다.',
+  NULL: '[ERROR] 빈값을 입력할 수 없습니다.',
+  HAS_BLANK: '[ERROR] 공백이 포함될 수 없습니다.',
+  NOT_A_NUMBER: '[ERROR] 입력한 값이 숫자가 아닙니다.',
+};
 
 module.exports = { INPUT_MESSAGE, OUTPUT_MESSAGE, OUTPUT_MESSAGE_METHOD, ERROR_MESSAGE };
--- src/domain/Validator.js
+++ src/domain/Validator.js
@@ -1,12 +1,14 @@
+const { ERROR_MESSAGE } = require('../constant/Message');
+
 const Validator = {
   purchaseAmount(money) {
     this.number(money);
 
     if (this.isLessThanMinimum(+money)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.LESS_THAN_MINIMUM);
     }
     if (this.hasChange(+money)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.HAS_CHANGE);
     }
   },
 
@@ -25,26 +27,26 @@ const Validator = {
       this.number(number);
 
       if (this.isOutOfRange(number)) {
-        throw new Error();
+        throw new Error(ERROR_MESSAGE.OUT_OF_RANGE);
       }
     });
 
     if (!this.isValidLength(winningNumbers)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.INVALID_WINNING_NUMBER_LENGTH);
     }
     if (this.hasDuplicatedNumber(winningNumbers)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.DUPLICATED_NUMBER);
     }
   },
 
   bonusNumber(bonusNumber, winningNumber) {
     this.number(bonusNumber);
 
-    if (this.isOutOfRange(bonusNumber)) {
-      throw new Error();
+    if (this.isOutOfRange(+bonusNumber)) {
+      throw new Error(ERROR_MESSAGE.OUT_OF_RANGE);
     }
-    if (this.isDuplicatedNumber(bonusNumber, winningNumber)) {
-      throw new Error();
+    if (this.isDuplicatedNumber(+bonusNumber, winningNumber)) {
+      throw new Error(ERROR_MESSAGE.DUPLICATED_NUMBER);
     }
   },
 
@@ -54,7 +56,7 @@ const Validator = {
 
   restartCommand(command) {
     if (!this.isValidRestartCommand(command)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.INVALID_RESTART_COMMAND);
     }
   },
 
@@ -76,13 +78,13 @@ const Validator = {
 
   number(number) {
     if (this.isNull(number)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.NULL);
     }
     if (this.hasBlank(number)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.HAS_BLANK);
     }
     if (isNaN(number)) {
-      throw new Error();
+      throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
     }
   },
 
