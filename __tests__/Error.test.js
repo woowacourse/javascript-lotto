96957c8c461c706e82c2b07c5b8c7df5c5588cce
gyeongza <gyeongza@naver.com>
2023-02-16 13:32:27 +0900

    test : 당첨번호 Error처리 테스트

--- __tests__/Error.test.js
+++ __tests__/Error.test.js
@@ -1,6 +1,6 @@
 import App from "../src/App.js";
 
-describe("Error처리 테스트", () => {
+describe("구입금액 Error처리 테스트", () => {
   test("구입금액이 숫자가 아닐 경우 Error처리 테스트", () => {
     const buyMoney = "aiden";
     const app = new App();
@@ -17,7 +17,7 @@ describe("Error처리 테스트", () => {
     }).toThrowError("1000원 단위로 입력해주세요.");
   });
 
-  test("구입금액이 1000원 단위가 아닐 경우 Error처리 테스트", () => {
+  test("구매금액이 양의 정수가 아닐 경우 Error처리 테스트", () => {
     const buyMoney = -2000;
     const app = new App();
     expect(() => {
@@ -25,3 +25,29 @@ describe("Error처리 테스트", () => {
     }).toThrowError("구매 금액은 양의 정수여야 합니다.");
   });
 });
+
+describe("당첨번호 Error처리 테스트", () => {
+    test("당첨번호가 숫자가 아닐 경우 Error처리 테스트", () => {
+      const eachNumber = 'a'
+      const app = new App();
+      expect(() => {
+        app.checkEachNumber(eachNumber);
+      }).toThrowError("숫자만 입력할 수 있습니다.");
+    });
+  
+    test("당첨번호가 해당 범위를 벗어날 경우 Error처리 테스트", () => {
+      const eachNumber = 47;
+      const app = new App();
+      expect(() => {
+        app.checkEachNumber(eachNumber);
+      }).toThrowError("당첨번호는 1~45까지의 범위입니다.");
+    });
+  
+    test("당첨번호가 양의 정수가 아닐 경우 Error처리 테스트", () => {
+      const eachNumber = 44.5;
+      const app = new App();
+      expect(() => {
+        app.checkEachNumber(eachNumber);
+      }).toThrowError("당첨번호는 양의 정수여야 합니다.");
+    });
+  });
