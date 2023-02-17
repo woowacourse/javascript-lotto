2247619bceeb86cc1a625f26947a9dbe2f52dda3
jeongwusi <jeongwusi@gmail.com>
2023-02-17 20:10:19 +0900

    ♻️ refactor: Console 객체로 수정

--- src/utils/Console.js
+++ src/utils/Console.js
@@ -1,24 +1,20 @@
 const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-class Console {
-  static readLine(query) {
+const Console = {
+  readLine(query) {
     return new Promise(resolve => {
       rl.question(query, answer => resolve(answer));
     });
-  }
+  },
 
-  static print(input) {
+  print(input) {
     console.log(input);
-  }
+  },
 
-  static close() {
+  close() {
     rl.close();
-  }
-}
+  },
+};
 
 module.exports = Console;
