fcad6a46840127b6c7c76a9109f60ce684f137fb
chsua <csj1919@naver.com>
2023-02-18 19:12:55 +0900

    test: 각 testCase를 test.each로 수정

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -2,23 +2,28 @@ const Lotto = require("../src/domain/Lotto");
 
 describe("복권이 당첨번호, 보너스번호와 일치하는지 테스트", () => {
   const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-  test("복권과 당첨번호(유저입력)이 일치하는 개수가 3개인 경우 9 반환", () => {
-    expect(lotto.matchNumbers([1, 2, 3, 7, 8, 9])).toEqual(9);
-  });
 
-  test("복권과 당첨번호(유저입력)이 일치하는 개수가 없는 경우 12 반환", () => {
-    expect(lotto.matchNumbers([7, 8, 9, 10, 11, 12])).toEqual(12);
-  });
+  test.each([
+    [
+      [[1, 2, 3, 7, 8, 9], 9],
+      [[7, 8, 9, 10, 11, 12], 12],
+      [[1, 2, 3, 4, 5, 6], 6],
+    ],
+  ])(
+    "복권과 당첨번호(유저입력)의 합에서 중복값을 뺀 개수 반환 %#",
+    (testCase) => {
+      const result = lotto.matchNumbers(testCase[0]);
+      expect(result).toEqual(testCase[1]);
+    }
+  );
 
-  test("복권과 당첨번호(유저입력)이 일치하는 개수가 6개인 경우 6 반환", () => {
-    expect(lotto.matchNumbers([1, 2, 3, 4, 5, 6])).toEqual(6);
-  });
-
-  test("보너스번호가 복권에 있으면 true", () => {
-    expect(lotto.matchBonus(1)).toBeTruthy();
-  });
-
-  test("보너스번호가 복권에 없으면 false", () => {
-    expect(lotto.matchBonus(7)).toBeFalsy();
+  test.each([
+    [
+      [1, true],
+      [7, false],
+    ],
+  ])("보너스번호가 복권에 있는지 여부에 따라 불린값 반환 %#", (testCase) => {
+    const result = lotto.matchBonus(testCase[0]);
+    expect(result).toEqual(testCase[1]);
   });
 });
