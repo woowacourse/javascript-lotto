3d935fda5a6e0f5368ea2721b11b59fbb4665cbc
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-19 01:19:09 +0900

    refactor: 출력문 상수화

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import Trimmer from '../util/Trimmer.js';
+
 const GAME_VALUE = Object.freeze({
   LOTTO_PRICE: 1_000,
   LOTTO_SIZE: 6,
@@ -18,12 +20,27 @@ const MESSAGE = Object.freeze({
   ASK_BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요. ',
   ASK_RESTART: '\n> 다시 시작하시겠습니까? (y/n) ',
 
+  BUY_RESULT: (buyCount) => `${buyCount}개를 구매했습니다.`,
+  PRIZE_RESULT: (gameData) =>
+    Trimmer.trimTemplate(
+      `
+        당첨 통계
+        ${'-'.repeat(20)}
+        3개 일치 (${GAME_VALUE.PRIZE[4].toLocaleString()}원) - ${gameData.fifth}개
+        4개 일치 (${GAME_VALUE.PRIZE[3].toLocaleString()}원) - ${gameData.fourth}개
+        5개 일치 (${GAME_VALUE.PRIZE[2].toLocaleString()}원) - ${gameData.third}개
+        5개 일치, 보너스 볼 일치 (${GAME_VALUE.PRIZE[1].toLocaleString()}원) - ${gameData.second}개
+        6개 일치 (${GAME_VALUE.PRIZE[0].toLocaleString()}원) - ${gameData.first}개`
+    ),
+  PROFIT_RATE_RESULT: (formattedRate) => `총 수익률은 ${formattedRate}%입니다.`,
+
   ERROR_DECIMAL: '[ERROR] 구입 금액은 정수여야 합니다.',
   ERROR_INVALID_LOTTO_FORMAT:
     '[ERROR] 로또는 콤마(,)로 구분되는 6개의 1 이상 45 이하의 정수여야 합니다.',
   ERROR_LOTTO_DUPLICATES: '[ERROR] 로또 번호는 중복되는 수가 없어야 합니다',
   ERROR_INVALID_BONUS_NUMBER: '[ERROR] 보너스 번호는 1이상 45이하의 정수여야 합니다',
   ERROR_INVALID_RETRY_COMMAND: '[ERROR] 재시작 여부는 y 또는 n이어야 합니다.',
+  ERROR_BUDGET_NOT_DIVISIBLE: (divisor) => `[ERROR] 구입 금액은 ${divisor}원 단위여야 합니다`,
 });
 
 const REGEX = Object.freeze({
@@ -37,12 +54,12 @@ const COMMAND = Object.freeze({
   EXIT: 'n',
 });
 
-const RANK_FORMAT = {
+const RANK_FORMAT = Object.freeze({
   1: 'first',
   2: 'second',
   3: 'third',
   4: 'fourth',
   5: 'fifth',
-};
+});
 
 export { FORMATTING_TYPE, MESSAGE, REGEX, COMMAND, GAME_VALUE, RANK_FORMAT };
--- src/view/output.js
+++ src/view/output.js
@@ -1,5 +1,5 @@
 import Trimmer from '../util/Trimmer.js';
-import { FORMATTING_TYPE, GAME_VALUE } from '../constants/index.js';
+import { FORMATTING_TYPE, GAME_VALUE, MESSAGE } from '../constants/index.js';
 
 const output = (data, formattingType = '') => {
   if (formattingType === '') {
@@ -15,21 +15,12 @@ const output = (data, formattingType = '') => {
   }
 
   if (formattingType === FORMATTING_TYPE.WINNING_STATUS) {
-    console.log(
-      Trimmer.trimTemplate(`
-        \n당첨 통계\n${'-'.repeat(20)}
-        3개 일치 (${GAME_VALUE.PRIZE[4].toLocaleString()}원) - ${data.fifth}개
-        4개 일치 (${GAME_VALUE.PRIZE[3].toLocaleString()}원) - ${data.fourth}개
-        5개 일치 (${GAME_VALUE.PRIZE[2].toLocaleString()}원) - ${data.third}개
-        5개 일치, 보너스 볼 일치 (${GAME_VALUE.PRIZE[1].toLocaleString()}원) - ${data.second}개
-        6개 일치 (${GAME_VALUE.PRIZE[0].toLocaleString()}원) - ${data.first}개
-      `)
-    );
+    console.log(MESSAGE.PRIZE_RESULT(data));
   }
 
   if (formattingType === FORMATTING_TYPE.PROFIT_RATE) {
     const formattedRate = data.toLocaleString();
-    console.log(`총 수익률은 ${formattedRate}%입니다.`);
+    console.log(MESSAGE.PROFIT_RATE_RESULT(formattedRate));
   }
 };
 
