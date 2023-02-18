6b3f668f670714d9218177788649f709223e3152
Leejin-Yang <ejin1996@gmail.com>
2023-02-18 21:42:03 +0900

    refactor: 메세지에서 변경될 수 있는 값 처리

--- src/constant/message.js
+++ src/constant/message.js
@@ -1,10 +1,10 @@
-const { RANK } = require('./setting');
+const { LOTTO, RANK, COMMAND } = require('./setting');
 
 const INPUT_MESSAGE = {
   PURCHASE_AMOUNT: '> 구입금액을 입력해 주세요.',
   WINNING_NUMBER: '> 당첨 번호를 입력해 주세요.',
   BONUS_NUMBER: '> 보너스 번호를 입력해 주세요.',
-  RESTART_COMMAND: '> 다시 시작하시겠습니까? (y/n)',
+  RESTART_COMMAND: `> 다시 시작하시겠습니까? (${COMMAND.YES}/${COMMAND.NO})`,
 };
 
 const OUTPUT_MESSAGE = {
@@ -22,12 +22,12 @@ const OUTPUT_MESSAGE_METHOD = {
 };
 
 const ERROR_MESSAGE = {
-  LESS_THAN_MINIMUM: '[ERROR] 구입 금액은 1,000원 이상이어야 합니다.',
-  HAS_CHANGE: '[ERROR] 구입 금액은 1,000원 단위이어야 합니다.',
-  OUT_OF_RANGE: '[ERROR] 1~45사이의 숫자이어야 합니다.',
-  INVALID_WINNING_NUMBER_LENGTH: '[ERROR] 당첨 번호는 6개의 숫자로 이루어져야 합니다.',
+  LESS_THAN_MINIMUM: `[ERROR] 구입 금액은 ${LOTTO.UNIT.toLocaleString()}원 이상이어야 합니다.`,
+  HAS_CHANGE: `[ERROR] 구입 금액은 ${LOTTO.UNIT.toLocaleString()}원 단위이어야 합니다.`,
+  OUT_OF_RANGE: `[ERROR] ${LOTTO.MIN_NUMBER_RANGE}~${LOTTO.MAX_NUMBER_RANGE}사이의 숫자이어야 합니다.`,
+  INVALID_WINNING_NUMBER_LENGTH: `[ERROR] 당첨 번호는 ${LOTTO.LENGTH}개의 숫자로 이루어져야 합니다.`,
   DUPLICATED_NUMBER: '[ERROR] 중복된 번호입니다.',
-  INVALID_RESTART_COMMAND: '[ERROR] 재시작 명령어는 y또는 n이어야 합니다.',
+  INVALID_RESTART_COMMAND: `[ERROR] 재시작 명령어는 ${COMMAND.YES}또는 ${COMMAND.NO}이어야 합니다.`,
   NULL: '[ERROR] 빈값을 입력할 수 없습니다.',
   HAS_BLANK: '[ERROR] 공백이 포함될 수 없습니다.',
   NOT_A_NUMBER: '[ERROR] 입력한 값이 숫자가 아닙니다.',
