be5f667cb79e7f4a41c99b69fd50975ce0d28f94
yeopto <gunyeop530@gmail.com>
2023-02-18 20:28:17 +0900

    refactor: LottoMachine에서 등수 구하는 로직을 WinningLotto에 분리시키다

--- src/model/LottoMachine.js
+++ src/model/LottoMachine.js
@@ -1,21 +1,11 @@
 import generateRandomNumber from '../utils/generateRandomNumber';
-import { values, correctCountsToMoney, prize } from '../constants/values';
 import Lotto from './Lotto';
+import WinningLotto from './WinningLotto';
+import { values, prize } from '../constants/values';
 
 class LottoMachine {
-  #lottos;
-  #statistics = {
-    ranks: new Array(6).fill(0),
-    rateOfProfit: null,
-  };
-
-  constructor() {
-    this.init();
-  }
-
-  init() {
-    this.#lottos = [];
-  }
+  #lottos = [];
+  #winnnigLotto;
 
   get lottos() {
     return this.#lottos;
@@ -39,70 +29,39 @@ class LottoMachine {
     return randomLottoNumbers.sort((prev, next) => prev - next);
   }
 
-  calculateStatistics(winningNumber, bonusNumber) {
-    const targetNumber = {
-      winningNumber,
-      bonusNumber,
-    };
-
-    const totalRanks = this.calculateRanks(targetNumber);
-    totalRanks.forEach(rank => {
-      this.#statistics.ranks[rank - 1]++;
-    });
-
-    this.#statistics.rateOfProfit = this.rateOfProfit(this.calcaulateTotalSum(totalRanks), totalRanks.length);
-
-    return this.#statistics;
+  initWinningLotto(winningNumber, bonusNumber) {
+    this.#winnnigLotto = new WinningLotto(winningNumber, bonusNumber);
   }
 
-  calcaulateTotalSum = ranks => ranks.reduce((acc, curr) => acc + prize[curr - 1], 0);
-
-  calculateRanks(targetNumber) {
-    return this.#lottos.map(lotto => this.checkLotteryWinnings(lotto.lottoNum, targetNumber));
+  calculateRanks() {
+    return this.#lottos.map(lotto => this.#winnnigLotto.checkLotteryWinningsRank(lotto.lottoNum));
   }
 
-  computeCorrectCounts(winningNumber, lottoNumber) {
-    const correctCounts = lottoNumber.reduce((acc, cur) => {
-      if (winningNumber.includes(cur)) return ++acc;
-      return acc;
-    }, 0);
-
-    return correctCounts;
-  }
-
-  checkLotteryWinnings(lottoNumber, targetNumber) {
-    const correctCounts = this.computeCorrectCounts(targetNumber.winningNumber, lottoNumber);
-
-    if (this.isFive(correctCounts)) {
-      return correctCountsToMoney[`${correctCounts}`][
-        `${this.isSecond(targetNumber.bonusNumber, lottoNumber) ? 0 : 1}`
-      ].rank;
-    }
-    return correctCountsToMoney[correctCounts].rank;
-  }
-
-  isFive(correctCounts) {
-    return correctCounts === 5;
-  }
-
-  isSecond(bonusNumber, lottoNumber) {
-    return lottoNumber.includes(bonusNumber);
-  }
-
-  lotteryWinningsSum(lottos, targetNumber) {
-    const lotteryWinningsSum = lottos.reduce(
-      (acc, lotto) => acc + this.checkLotteryWinnings(lotto, targetNumber),
-      0,
-    );
-
-    return lotteryWinningsSum;
+  calculateTotalSum(ranks) {
+    return ranks.reduce((acc, curr) => acc + prize[curr - 1], 0);
   }
 
   rateOfProfit(lotteryWinningsSum, lottosLength) {
     const { LOTTO_PRICE } = values;
     const spentMoney = lottosLength * LOTTO_PRICE;
 
-    return ((lotteryWinningsSum - spentMoney) / spentMoney) * 100;
+    return ((lotteryWinningsSum / spentMoney) * 100).toFixed(1);
+  }
+
+  calculateStatistics() {
+    const statistics = {
+      countOfRanks: new Array(6).fill(0),
+      rateOfProfit: null,
+    };
+    const totalRanks = this.calculateRanks();
+
+    totalRanks.forEach(rank => {
+      statistics.countOfRanks[rank - 1] += 1;
+    });
+
+    statistics.rateOfProfit = this.rateOfProfit(this.calculateTotalSum(totalRanks), totalRanks.length);
+
+    return statistics;
   }
 }
 
