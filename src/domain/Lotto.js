ef408c8f2cb32cc3c273fb5e3de11fb66005cdc9
ukkodeveloper <ukkodeveloper@gmail.com>
2023-02-18 18:47:53 +0900

    feat: lotto 객체 다른 lotto객체와 비교하는 기능 제거

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,9 +1,25 @@
-import { LOTTO_RULE } from '../constant/constants.js';
+import { LOTTO_COUNT_STANDARD, LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER } from './constant/constants.js';
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    if (numbers.every((number) => typeof number !== number || Number.isNaN(number))) {
+      throw new Error('[ERROR] ');
+    }
+
+    if (numbers.every((number) => number > LOTTO_MAX_NUMBER || number < LOTTO_MIN_NUMBER)) {
+      throw new Error('[ERROR] ');
+    }
+
+    if (numbers.length !== LOTTO_COUNT_STANDARD) {
+      throw new Error('[ERROR] ');
+    }
+
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error('[ERROR] ');
+    }
+
     this.#numbers = new Set(numbers);
   }
 
