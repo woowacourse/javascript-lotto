71c2e655b4838382f62b87e0049d4493c40f29f6
yeopto <gunyeop530@gmail.com>
2023-02-18 20:29:46 +0900

    refactor: statistics의 key명 변경한 것을 OutputView에 적용하다

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -14,11 +14,11 @@ const OutputView = {
 
   printStatistics(statistics) {
     Console.print(messages.STATISTICS_MESSAGE.OPENING);
-    Console.print(messages.STATISTICS_MESSAGE.FIFTH + statistics.ranks[4]);
-    Console.print(messages.STATISTICS_MESSAGE.FOURTH + statistics.ranks[3]);
-    Console.print(messages.STATISTICS_MESSAGE.THIRD + statistics.ranks[2]);
-    Console.print(messages.STATISTICS_MESSAGE.SECOND + statistics.ranks[1]);
-    Console.print(messages.STATISTICS_MESSAGE.FIRST + statistics.ranks[0]);
+    Console.print(messages.STATISTICS_MESSAGE.FIFTH + statistics.countOfRanks[4]);
+    Console.print(messages.STATISTICS_MESSAGE.FOURTH + statistics.countOfRanks[3]);
+    Console.print(messages.STATISTICS_MESSAGE.THIRD + statistics.countOfRanks[2]);
+    Console.print(messages.STATISTICS_MESSAGE.SECOND + statistics.countOfRanks[1]);
+    Console.print(messages.STATISTICS_MESSAGE.FIRST + statistics.countOfRanks[0]);
 
     Console.print(`총 수익률은 ${statistics.rateOfProfit}% 입니다.`);
   },
