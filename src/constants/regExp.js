c31aa21d9de6860685b24fc39cf26799b25fa13f
n0eyes <doll4586633@naver.com>
2023-02-20 02:06:48 +0900

    fix: 당첨 번호 유효성 검사 추가
    중복 번호 검사
    숫자 개수 검사

--- src/constants/regExp.js
+++ src/constants/regExp.js
@@ -1,2 +1,2 @@
-export const ONLY_NUMBERS_WITH_COMMA = /^\d+([,]\d+)*$/;
+export const ONLY_NUMBERS_WITH_COMMA = /^(?!.*\b(\d+)\b.*\b\1\b)\d+,\d+,\d+,\d+,\d+,\d+$/;
 export const ONLY_NUMBER = /^-?\d+$/;
