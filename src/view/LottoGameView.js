afcaca2f9a2e75e195deeaaafe45d90a960bef74
ashleysyheo <ashleysyheo@gmail.com>
2023-02-19 17:19:48 +0900

    feat: hide error on input

--- src/view/LottoGameView.js
+++ src/view/LottoGameView.js
@@ -2,14 +2,27 @@ import $ from '../utils/DomSelector.js';
 
 class LottoGameView {
   constructor() {
+    this.#initDom();
+  }
+
+  #initDom() {
     this.purchaseForm = $('#purchase-form');
     this.purchaseInput = $('#purchase-input');
+
+    this.addPurchaseInputEvent();
   }
 
   addPurchaseSubmitEvent(callback) {
     this.purchaseForm.addEventListener('submit', (event) => {
       event.preventDefault();
       callback(event.target['purchase-amount'].value);
+    });
+  }
+
+  addPurchaseInputEvent() {
+    this.purchaseInput.addEventListener('input', () => {
+      this.purchaseInput.classList.remove('error-input');
+      this.hideErrorMessage('purchase');
     });
   }
 
