8b5eb318f6c804de5feb2d0f5049ecfe85fee2ef
gyeongza <gyeongza@naver.com>
2023-02-19 15:44:25 +0900

    refactor : 수익률 계산 테스트코드 수정

--- __tests__/Lottos.test.js
+++ __tests__/Lottos.test.js
@@ -39,12 +39,12 @@ describe("Lotto 객체 단위테스트", () => {
   test("총 수익을 계산하는 함수 테스트", () => {
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
-    lotto1.compareNumbers(winningNumbers);
     const lotto2 = new Lotto([2, 3, 4, 5, 6, 7]);
-    lotto2.compareNumbers(winningNumbers);
     const lotto3 = new Lotto([3, 4, 5, 6, 8, 9]);
-    lotto3.compareNumbers(winningNumbers);
     const lottos = new Lottos([lotto1, lotto2, lotto3]);
+    lotto1.compareNumbers(winningNumbers);
+    lotto2.compareNumbers(winningNumbers);
+    lotto3.compareNumbers(winningNumbers);
     lottos.compareLottosScore();
 
     lottos.calculateBenefit();
@@ -54,16 +54,15 @@ describe("Lotto 객체 단위테스트", () => {
 
   test("총 수익률을 계산하는 함수 테스트", () => {
     const winningNumbers = [1, 2, 3, 4, 5, 6];
-
     const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
-    lotto1.compareNumbers(winningNumbers);
     const lotto2 = new Lotto([2, 3, 4, 5, 6, 7]);
-    lotto2.compareNumbers(winningNumbers);
     const lotto3 = new Lotto([3, 4, 5, 6, 8, 9]);
-    lotto3.compareNumbers(winningNumbers);
     const lottos = new Lottos([lotto1, lotto2, lotto3]);
-
+    lotto1.compareNumbers(winningNumbers);
+    lotto2.compareNumbers(winningNumbers);
+    lotto3.compareNumbers(winningNumbers);
     lottos.compareLottosScore();
+
     lottos.calculateBenefit();
 
     expect(lottos.getBenefitRate(1000)).toBe(2001550);
