0fdd317e5eda9b055e296adfba6e99cb07f5f34c
gyeongza <gyeongza@naver.com>
2023-02-19 16:16:37 +0900

    refactor : 각 랜덤숫자를 반환하는 함수로 변경

--- src/Random.js
+++ src/Random.js
@@ -10,7 +10,7 @@ const Random = {
   makeRandomNumbers(randomNumbers) {
     const number = parseInt(Math.random() * 45 + 1, 10);
     if (randomNumbers.indexOf(number) < 0) {
-      randomNumbers.push(number);
+      return randomNumbers.push(number);
     }
   },
 };
