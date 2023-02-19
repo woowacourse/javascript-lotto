867f006bbe888dac81ec4210a5a97e13732841ef
sh981013s <sh981013s@gmail.com>
2023-02-19 22:47:20 +0900

    refactor(Lotto.test): 테스트코드 매직넘버에 상수 적용

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import Lotto from '../src/model/Lotto';
+import { values } from '../src/constants/values';
 
 describe('Lotto 단일 객체 테스트', () => {
   let lotto;
@@ -10,19 +11,19 @@ describe('Lotto 단일 객체 테스트', () => {
   test('로또 숫자가 1~45 사이의 숫자여야 한다.', () => {
     const lottoNum = lotto.lottoNum;
 
-    expect(lottoNum.every(num => num >= 1 && num <= 45)).toBe(true);
+    expect(lottoNum.every(num => num >= values.LOWER_BOUND && values.UPPER_BOUND <= 45)).toBe(true);
   });
 
   test('로또 배열 길이가 6개이어야 한다.', () => {
     const lottoNum = lotto.lottoNum;
 
-    expect(lottoNum.length === 6).toBe(true);
+    expect(lottoNum.length === values.LOTTO_LENGTH).toBe(true);
   });
 
   test('로또 배열에 겹치는 숫자가 없어야 한다.', () => {
     const lottoNum = lotto.lottoNum;
 
-    expect(new Set(lottoNum).size === 6);
+    expect(new Set(lottoNum).size === values.LOTTO_LENGTH);
   });
 
   test('로또 배열이 오름차순으로 정렬되어야 한다.', () => {
