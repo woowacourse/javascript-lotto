989d41c06c3002551207ff1838beb5350d2114f0
sh981013s <sh981013s@gmail.com>
2023-02-19 23:06:19 +0900

    refactor(inputValidator): 지속해서 검증 로직 호출하지 않도록 구조 개선

--- src/validator/InputValidator.js
+++ src/validator/InputValidator.js
@@ -1,31 +1,43 @@
 import messages from '../constants/messages';
 import ValidatorUtils from './ValidatorUtils';
 
-const InputValidator = {
-  validateWithCondition(condition, errorMessage) {
-    if (!condition) {
-      throw new Error(errorMessage);
-    }
+const inputValidator = {
+  validateWithCondition(conditions) {
+    conditions.forEach(condition => {
+      if (!condition.condition) {
+        throw new Error(condition.errorMessage);
+      }
+    });
   },
 
   validateMoneyInput(number) {
-    this.validateWithCondition(ValidatorUtils.isPositiveInteger(+number), messages.ERROR.POSITIVE_INTEGER);
-    this.validateWithCondition(ValidatorUtils.isThousandsOfWon(+number), messages.ERROR.THOUSANDS_WON);
+    this.validateWithCondition([
+      { condition: ValidatorUtils.isPositiveInteger(+number), errorMessage: messages.ERROR.POSITIVE_INTEGER },
+      { condition: ValidatorUtils.isThousandsOfWon(+number), errorMessage: messages.ERROR.THOUSANDS_WON },
+    ]);
   },
 
   validateWinningNumberInput(winningNumber) {
     winningNumber.split(',').forEach(number => {
-      this.validateWithCondition(ValidatorUtils.isPositiveInteger(+number), messages.ERROR.VALID_SIX_NUMBER);
-      this.validateWithCondition(ValidatorUtils.isInRange(+number), messages.ERROR.VALID_SIX_NUMBER);
+      this.validateWithCondition([
+        {
+          condition: ValidatorUtils.isPositiveInteger(+number),
+          errorMessage: messages.ERROR.VALID_SIX_NUMBER,
+        },
+        {
+          condition: ValidatorUtils.isInRange(+number),
+          errorMessage: messages.ERROR.VALID_SIX_NUMBER,
+        },
+      ]);
     });
-
-    this.validateWithCondition(ValidatorUtils.hasNoBlank(winningNumber.split('')), messages.ERROR.HAS_BLANK);
   },
 
   validateBonusNumberInput(number) {
-    this.validateWithCondition(ValidatorUtils.isPositiveInteger(+number), messages.ERROR.POSITIVE_INTEGER);
-    this.validateWithCondition(ValidatorUtils.isInRange(+number), messages.ERROR.IN_RANGE);
-  }
+    this.validateWithCondition([
+      { condition: ValidatorUtils.isPositiveInteger(+number), errorMessage: messages.ERROR.POSITIVE_INTEGER },
+      { condition: ValidatorUtils.isInRange(+number), errorMessage: messages.ERROR.IN_RANGE },
+    ]);
+  },
 };
 
-export default InputValidator;
+export default inputValidator;
