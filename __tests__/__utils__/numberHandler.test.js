2ed844dc8d650157b6f3a05fc0689fa14e8091e8
chex <chex417@naver.com>
2023-02-19 16:47:05 +0900

    test: generateRandomNumber테스트에서 Array.from을 사용하여 중복코드 제거

--- __tests__/__utils__/numberHandler.test.js
+++ __tests__/__utils__/numberHandler.test.js
@@ -7,36 +7,18 @@ describe('numberHandler 테스트', () => {
     [1.0, 1],
     [100.05, 100.1],
     [100, 100],
-  ])('소수점 둘째자리에서 반올림한다.', (floatNumber, roundedOffNumber) => {
+  ])('소수점 둘째자리에서 반올림한 값을 반환한다.', (floatNumber, roundedOffNumber) => {
     expect(numberHandler.roundOffNumber(floatNumber)).toBe(roundedOffNumber);
   });
 
-  test.each([
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-    [numberHandler.generateRandomNumber(1, 45)],
-  ])('generateRandomNumber가 1~45사이의 숫자를 반환한다.', randomNumber => {
-    const rangeNumbers = Array.from({ length: 45 }, (_, index) => index + 1);
+  test.each(Array.from({ length: 10 }, () => [numberHandler.generateRandomNumber(1, 45)]))(
+    '1~45사이의 랜덤한 숫자를 반환한다.',
+    randomNumber => {
+      const rangeNumbers = Array.from({ length: 45 }, (_, index) => index + 1);
 
-    expect(rangeNumbers.includes(randomNumber)).toBeTruthy();
-  });
-
-  /**
-   * TODO
-   * - [ ]  메서드) 천의 자리마다 콤마 찍기 - 테스트하기
-   */
+      expect(rangeNumbers.includes(randomNumber)).toBeTruthy();
+    }
+  );
 
   test.each([
     [1, '1'],
