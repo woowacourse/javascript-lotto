8f139ca50f09cae2fc30af55a3d703d194278c0b
turtle601 <turtle601@naver.com>
2023-02-19 13:27:04 +0900

    refactor(LottoStatisticsTest): 등수에 따른 테스트 코드 분리

--- __tests__/LottoStatistics.test.js
+++ __tests__/LottoStatistics.test.js
@@ -3,28 +3,66 @@ const LottoStatistics = require('../src/domain/LottoStatistics');
 const WinningNumbers = require('../src/domain/WinningNumbers');
 const BonusNumber = require('../src/domain/BonusNumber');
 
-const LottoMachine = require('../src/domain/LottoMachine');
-
 describe('LottoStatistics 클래스 테스트', () => {
-  test.each([
-    ['1,2,3,4,5,6', '7', 1],
-    ['1,2,3,4,5,45', '6', 2],
-    ['1,2,3,4,5,45', '44', 3],
-    ['1,2,3,4,44,45', '33', 4],
-    ['1,2,3,43,44,45', '33', 5],
-    ['1,2,42,43,44,45', '33', 6],
-  ])(
-    '각 로또의 등수를 결정한다.',
-    (winningNumbersInput, bonusNumberInput, expected) => {
-      const winningNumbers = new WinningNumbers(winningNumbersInput);
-      const bonusNumber = new BonusNumber(bonusNumberInput, winningNumbers);
-      const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+  test('로또 당첨결과가 모두 일치할 때 1등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
+    const bonusNumber = new BonusNumber('7', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
 
-      const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
 
-      expect(rank).toBe(expected);
-    }
-  );
+    expect(rank).toBe(1);
+  });
+
+  test('로또 당첨결과가 5개 일치하고 보너스 번호가 일치할 때 2등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,5,45');
+    const bonusNumber = new BonusNumber('6', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+
+    expect(rank).toBe(2);
+  });
+
+  test('로또 당첨결과가 5개 일치하고 보너스 번호가 일치하지 않을 때 3등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,5,45');
+    const bonusNumber = new BonusNumber('44', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+
+    expect(rank).toBe(3);
+  });
+
+  test('로또 당첨결과가 4개 일치하고 4등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,4,44,45');
+    const bonusNumber = new BonusNumber('33', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+
+    expect(rank).toBe(4);
+  });
+
+  test('로또 당첨결과가 3개 일치하고 5등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,3,43,44,45');
+    const bonusNumber = new BonusNumber('33', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+
+    expect(rank).toBe(5);
+  });
+
+  test('로또 당첨 결과가 아무것도 없을 때 6등인지 확인한다.', () => {
+    const winningNumbers = new WinningNumbers('1,2,42,43,44,45');
+    const bonusNumber = new BonusNumber('33', winningNumbers);
+    const statistics = new LottoStatistics(winningNumbers, bonusNumber);
+
+    const rank = statistics.determineLottoRank(new Lotto([1, 2, 3, 4, 5, 6]));
+
+    expect(rank).toBe(6);
+  });
 
   test('모든 로또의 당첨 결과를 배열로 반환한다.', () => {
     const winningNumbers = new WinningNumbers('1,2,3,4,5,6');
