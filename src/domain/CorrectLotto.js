560140f2f21920c9447e1379115aba408e7d6dc0
turtle601 <turtle601@naver.com>
2023-02-19 20:38:53 +0900

    refactor(CorrectLotto): 입력한 당첨번호와 보너스 번호의 데이터를 가지고 있는 클래스 구현

---
+++ src/domain/CorrectLotto.js
@@ -0,0 +1,35 @@
+class CorrectLotto {
+  #winningNumbers;
+
+  #bonusNumber;
+
+  setWinningNumbers(winningNumbersInstance) {
+    this.#winningNumbers = winningNumbersInstance.numbers;
+  }
+
+  setBonusNumber(bonusNumberInstance) {
+    this.#bonusNumber = bonusNumberInstance.number;
+  }
+
+  validateLottos() {
+    if (this.isDuplicateFor()) {
+      throw new Error(
+        '[ERROR] 당첨 번호와 보너스 번호에 중복이 존재할 수 없습니다.'
+      );
+    }
+  }
+
+  isDuplicateFor() {
+    return this.#winningNumbers.includes(this.#bonusNumber);
+  }
+
+  get winningNumbers() {
+    return this.#winningNumbers;
+  }
+
+  get bonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
+module.exports = CorrectLotto;
