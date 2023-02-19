b72d5ebbc7f929fd658fd0b9b395dc6c5e19a08d
turtle601 <turtle601@naver.com>
2023-02-19 13:06:02 +0900

    refactor(LottoGameTest): 재시작/종료 테스트 세분화

--- __tests__/LottoGame.test.js
+++ __tests__/LottoGame.test.js
@@ -3,14 +3,17 @@ const WinningNumbers = require('../src/domain/WinningNumbers');
 const LottoGame = require('../src/LottoGame');
 
 describe('LottoGame 클래스 테스트', () => {
-  test.each([['n'], ['y'], ['N'], ['Y']])(
-    '재시작/종료 명령어 입력 시 정상 동작.',
-    (input) => {
-      expect(() => {
-        new LottoGame().validateCommand(input);
-      }).not.toThrow();
-    }
-  );
+  test.each([['n'], ['N']])('종료 명령어 입력 시 정상 동작.', (input) => {
+    expect(() => {
+      new LottoGame().validateCommand(input);
+    }).not.toThrow();
+  });
+
+  test.each([['y'], ['Y']])('재시작 명령어 입력 시 정상 동작.', (input) => {
+    expect(() => {
+      new LottoGame().validateCommand(input);
+    }).not.toThrow();
+  });
 
   test.each([['t'], ['1'], [''], ['t    ']])(
     '재시작 또는 종료 명령어가 아닌 경우 예외 처리',
