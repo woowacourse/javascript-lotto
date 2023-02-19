81bb3761003f8ee303d051fd1aafbea43b66a3fe
guridaek <xylitol79@gmail.com>
2023-02-19 22:23:18 +0900

    test: 필요없는 부분 삭제

--- __tests__/lottoUtils.test.js
+++ __tests__/lottoUtils.test.js
@@ -13,12 +13,11 @@ describe('기능 테스트', () => {
     const lottos = new Array(10).fill().map(() => LottoUtils.createLottoNumbers());
     lottos.forEach((numbers) => {
       expect(isValidLotto(numbers)).toBe(true);
-    })
+    });
   });
 
   test('수익률 계산', () => {
     const winningResult = { first: 0, second: 0, third: 1, fourth: 1, fifth: 0 };
-    LottoUtils.calculateYieldRate([1, 2, 3, 4, 5, 6], 7);
 
     expect(LottoUtils.calculateYieldRate(winningResult, 20)).toBe('7750.0');
   });
