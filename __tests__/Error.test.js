508ca05f94e1c6bdfbe1c02f6a3eb53be7f4b0a7
gyeongza <gyeongza@naver.com>
2023-02-19 15:35:31 +0900

    refactor : 에러처리 테스트 케이스 통합

--- __tests__/Error.test.js
+++ __tests__/Error.test.js
@@ -1,26 +1,9 @@
 import App from "../src/App.js";
 
-
 describe("구입금액 Error처리 테스트", () => {
-  test("구입금액이 숫자가 아닐 경우 Error처리 테스트", () => {
-    const buyMoney = "aiden";
+  test.each(["aiden", 200, -2000, 1.2])("구입 금액", (buyMoney) => {
     const app = new App();
-    expect(() => {
-      app.validateBuyMoney(buyMoney);
-    }).toThrow();
-  });
 
-  test("구입금액이 1000원 단위가 아닐 경우 Error처리 테스트", () => {
-    const buyMoney = 200;
-    const app = new App();
-    expect(() => {
-      app.validateBuyMoney(buyMoney);
-    }).toThrow();
-  });
-
-  test("구매금액이 양의 정수가 아닐 경우 Error처리 테스트", () => {
-    const buyMoney = -2000;
-    const app = new App();
     expect(() => {
       app.validateBuyMoney(buyMoney);
     }).toThrow();
@@ -28,27 +11,11 @@ describe("구입금액 Error처리 테스트", () => {
 });
 
 describe("당첨로또 Error처리 테스트", () => {
-  test("당첨번호와 보너스번호가 숫자가 아닐 경우 Error처리 테스트", () => {
-    const eachNumber = "a";
+  test.each(["a", 47, -124, 44.5])("당첨로또 Error처리", (eachNumber) => {
     const app = new App();
-    expect(() => {
-      app.checkEachNumber(eachNumber);
-    }).toThrow();
-  });
 
-  test("당첨번호와 보너스번호가 해당 범위를 벗어날 경우 Error처리 테스트", () => {
-    const eachNumber = 47;
-    const app = new App();
     expect(() => {
-      app.checkEachNumber(eachNumber);
-    }).toThrow();
-  });
-
-  test("당첨번호와 보너스번호가 양의 정수가 아닐 경우 Error처리 테스트", () => {
-    const eachNumber = 44.5;
-    const app = new App();
-    expect(() => {
-      app.checkEachNumber(eachNumber);
+      app.validateBuyMoney(eachNumber);
     }).toThrow();
   });
 });
@@ -57,6 +24,7 @@ describe("재시작 여부 입력 Error처리 테스트", () => {
   test("재시작 여부 입력이 정해진 형식이 아닐 경우 Error처리 테스트", () => {
     const retryInput = "a";
     const app = new App();
+
     expect(() => {
       app.validateRetryInput(retryInput);
     }).toThrow();
