03e390f1c472d20c5f09cfb4b6a21f1bd6f9a8ab
Leejin-Yang <ejin1996@gmail.com>
2023-02-19 20:41:02 +0900

    refactor: 수익률 계산 클래스 -> 함수로 수정

---
+++ src/domain/calculateProfitRate.js
@@ -0,0 +1,10 @@
+const { RANK, LOTTO } = require('../constant/setting');
+
+const calculateTotalRewards = (ranking) =>
+  Object.entries(ranking).reduce((acc, [rank, count]) => acc + RANK[rank].REWARDS * count, 0);
+
+const calculateProfitRate = (ranking, lottoQuantity) => {
+  return (calculateTotalRewards(ranking) * 100) / (lottoQuantity * LOTTO.UNIT);
+};
+
+module.exports = calculateProfitRate;
