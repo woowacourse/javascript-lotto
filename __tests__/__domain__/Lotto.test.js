5525f6d1347524ec5c7db6d345a47b7cb88c2941
semnil5202 <semnil5202@gmail.com>
2023-02-20 19:04:46 +0900

    test: 로또 테스트 케이스 추가 및 수정
    generateLotto 함수 재활용을 위해 파라미터를 변경한다.

--- __tests__/__domain__/Lotto.test.js
+++ __tests__/__domain__/Lotto.test.js
@@ -3,45 +3,84 @@ import LottoMachine from '../../src/domain/LottoMachine.js';
 
 const AMOUNT = 8000;
 const PRICE = 1000;
-const TOTAL = AMOUNT / PRICE;
+const LOTTO_COUNT = AMOUNT / PRICE;
 const LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6];
 const DRAWING_NUMBERS = {
   winningNumbers: [1, 2, 3, 4, 5, 6],
   bonusNumber: 7,
 };
 
-const generateLotto = (lottoCount) =>
+const generateLotto = ({ lottoCount, lottoNumbers, drawingNumbers }) =>
   Array(lottoCount)
     .fill()
-    .map(() => new Lotto(LOTTO_NUMBERS).setDrawingNumbers(DRAWING_NUMBERS));
+    .map(() => new Lotto(lottoNumbers).setDrawingNumbers(drawingNumbers));
 
-describe('로또', () => {
+describe('LottoMachine 클래스', () => {
   test('구입 금액만큼 로또를 구매한다', () => {
-    const lottoList = LottoMachine.purchase(TOTAL);
+    const lottoList = LottoMachine.purchase(LOTTO_COUNT);
 
-    expect(lottoList.length).toBe(TOTAL);
+    expect(lottoList.length).toBe(LOTTO_COUNT);
   });
 
-  test('로또 번호와 당첨 번호를 비교하여 당첨 결과를 확인한다', () => {
-    const [lotto] = generateLotto(1);
+  test.each([
+    {
+      lottoCount: 1,
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      drawingNumbers: {
+        winningNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 7,
+      },
+      winningPlace: 6,
+    },
+    {
+      lottoCount: 1,
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      drawingNumbers: {
+        winningNumbers: [1, 2, 3, 4, 5, 42],
+        bonusNumber: 6,
+      },
+      winningPlace: 'BONUS',
+    },
+    {
+      lottoCount: 1,
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      drawingNumbers: {
+        winningNumbers: [1, 2, 3, 4, 5, 32],
+        bonusNumber: 44,
+      },
+      winningPlace: 5,
+    },
+  ])(
+    '로또 번호와 당첨 번호를 비교하여 당첨 결과를 확인한다',
+    ({ lottoCount, lottoNumbers, drawingNumbers, winningPlace }) => {
+      const [lotto] = generateLotto({ lottoCount, lottoNumbers, drawingNumbers });
 
-    const result = LottoMachine.draw(lotto);
+      const result = LottoMachine.draw(lotto);
 
-    expect(result).toBe(6);
-  });
+      expect(result).toBe(winningPlace);
+    }
+  );
 
   test('당첨 통계를 계산한다', () => {
-    const lottoList = generateLotto(5);
+    const lottoList = generateLotto({
+      lottoCount: LOTTO_COUNT,
+      lottoNumbers: LOTTO_NUMBERS,
+      drawingNumbers: DRAWING_NUMBERS,
+    });
 
     const statistics = LottoMachine.calculateStatistics(lottoList);
 
     expect(statistics).toEqual({
-      6: 5,
+      6: 8,
     });
   });
 
   test('총 수익률을 계산한다', () => {
-    const lottoList = generateLotto(5);
+    const lottoList = generateLotto({
+      lottoCount: LOTTO_COUNT,
+      lottoNumbers: LOTTO_NUMBERS,
+      drawingNumbers: DRAWING_NUMBERS,
+    });
 
     const earningRate = LottoMachine.calculateEarningRate(lottoList);
 
