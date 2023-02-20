d8cf51860dafc9add7b868650aa02ba5d62157f8
TaeeunKim <xodms0309@naver.com>
2023-02-20 20:34:25 +0900

    chore: step1의 App 코드 제거

--- src/App.js
+++ src/App.js
@@ -4,25 +4,6 @@ class App {
   constructor() {
     this.lottoController = new LottoController();
   }
-
-  async play() {
-    this.repeatGame();
-  }
-
-  async repeatGame() {
-    await this.playGame();
-    const restart = await this.lottoController.proceedRestartCommand();
-    if (!restart) {
-      return;
-    }
-    this.repeatGame();
-  }
-
-  async playGame() {
-    await this.lottoController.proceedBuyLottos();
-    await this.lottoController.proceedWinningLotto();
-    this.lottoController.proceedLottoResult();
-  }
 }
 
 export default App;
