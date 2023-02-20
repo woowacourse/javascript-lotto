f4c09463efc2d88e30a91cf9d8e963281523f62b
minjaeKim <smallkdb@gmail.com>
2023-02-20 13:00:03 +0900

    test: 일부 변수 메이밍 변경 및 테스트 케이스 정의 구체화

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,32 +8,32 @@ test('숫자 6개를 받아 로또를 발행한다.', () => {
   expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-test('로또 갯수는 6개다.', () => {
+test('로또 번호는 6개를 입력해야하며, 그렇지 않으면 에러가 발생한다.', () => {
   const lotto = [1, 2, 3, 4, 5];
   expect(() => InputValidator.checkArrayLength(lotto, LOTTO.length)).toThrow();
 });
 
-test('로또 구입 금액은 자연수이다.', () => {
-  const a = '1.5';
-  expect(() => InputValidator.checkNaturalNumber(a)).toThrow();
+test('로또 구입 금액은 String 타입 자연수이며, 그렇지 않으면 에러가 발생한다.', () => {
+  const stringNumber = '1.5';
+  expect(() => InputValidator.checkNaturalNumber(stringNumber)).toThrow();
 });
 
-test('1000원 단위로 나뉘는지 검사', () => {
+test('1000원 단위로 나뉘는지 검사하며, 그렇지 않으면 에러가 발생한다.', () => {
   const money = 14100;
   expect(() => InputValidator.checkFallApart(money, LOTTO.price)).toThrow();
 });
 
-test('로또 번호는 1~45의 자연수', () => {
-  const number = '0';
+test('로또 번호는 1~45의 String 타입 자연수이며, 그렇지 않으면 에러가 발생한다.', () => {
+  const stringNumber = '0';
   expect(() => InputValidator.checkLottoNumber(number)).toThrow();
 });
 
-test('각 번호는 중복되지 않는 수', () => {
+test('각 번호는 중복되지 않는 수이며, 그렇지 않으면 에러가 발생한다.', () => {
   const numbers = [1, 2, 3, 4, 5, 5];
   expect(() => InputValidator.checkDuplicatedNumbers(numbers)).toThrow();
 });
 
-test('입력값이 y 혹은 n 만 가능하다.', () => {
+test('입력값은 y 혹은 n 만 가능하며, 그렇지 않으면 에러가 발생한다.', () => {
   const command = 's';
   expect(() => InputValidator.checkRetryCommand(command)).toThrow();
 });
