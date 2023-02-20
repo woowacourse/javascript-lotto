ea8f0ee073378a0c72129958e1fe5c408e8b19d2
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-02-20 17:33:10 +0900

    refactor: lotto unit 상수화

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,4 +1,4 @@
-import { LOTTO_LENGTH } from '../src/data/Constants';
+import { LOTTO_LENGTH, MINIMUM_LOTTO_UNIT } from '../src/data/Constants';
 import Lotto from '../src/domain/Lotto';
 
 test('중복 숫자가 없는 로또가 잘 생성되는지 테스트', () => {
@@ -6,7 +6,7 @@ test('중복 숫자가 없는 로또가 잘 생성되는지 테스트', () => {
   const price = 8000;
 
   // when
-  const lottoCount = Math.floor(price / 1000);
+  const lottoCount = Math.floor(price / MINIMUM_LOTTO_UNIT);
   const lottos = Array.from({ length: lottoCount }, () => new Lotto());
 
   // then
