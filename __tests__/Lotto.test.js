2636e6b0104820cc7cc7735f4ec2b2576a81565c
kyw0716 <kyw0716@naver.com>
2023-02-20 12:37:35 +0900

    style(Lotto.test): 테스트 코드 예상되는 결과 값 네이밍 변경 expected => caculated

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,16 +1,16 @@
 /*eslint-disable */
-const { profitByRank } = require('../src/constants/constants');
-const Lotto = require('../src/domain/model/Lotto');
-const { calculateRank, calculateProfit } = require('../src/utils');
+const { profitByRank } = require("../src/constants/constants");
+const Lotto = require("../src/domain/model/Lotto");
+const { calculateProfit } = require("../src/utils");
 
-describe('로또 클래스 테스트', () => {
+describe("로또 클래스 테스트", () => {
   test.each([
     [8, 21, 23, 41, 42, 43],
     [3, 5, 11, 16, 32, 38],
     [7, 11, 16, 35, 36, 44],
     [1, 8, 11, 31, 41, 42],
   ])(
-    '당첨 번호를 넘겨주었을 때, 해당 값을 필드로 갖는 인스턴스를 생성 기능',
+    "당첨 번호를 넘겨주었을 때, 해당 값을 필드로 갖는 인스턴스를 생성 기능",
     (lottoNumbers) => {
       const lotto = new Lotto(lottoNumbers);
 
@@ -19,33 +19,33 @@ describe('로또 클래스 테스트', () => {
   );
 
   test.each([
-    { input: [1, 2, 3, 4, 5, 6], expectedRank: 1 },
-    { input: [1, 2, 3, 4, 5, 7], expectedRank: 2 },
-    { input: [1, 2, 3, 4, 5, 8], expectedRank: 3 },
-    { input: [1, 2, 3, 4, 8, 9], expectedRank: 4 },
-    { input: [1, 2, 3, 8, 9, 10], expectedRank: 5 },
-    { input: [8, 9, 10, 11, 12, 13], expectedRank: undefined },
+    { input: [1, 2, 3, 4, 5, 6], caculatedRank: 1 },
+    { input: [1, 2, 3, 4, 5, 7], caculatedRank: 2 },
+    { input: [1, 2, 3, 4, 5, 8], caculatedRank: 3 },
+    { input: [1, 2, 3, 4, 8, 9], caculatedRank: 4 },
+    { input: [1, 2, 3, 8, 9, 10], caculatedRank: 5 },
+    { input: [8, 9, 10, 11, 12, 13], caculatedRank: undefined },
   ])(
-    '당첨 번호와 보너스 번호를 넘겨주었을 때, 로또 번호와 비교하여 등수를 계산 기능',
-    ({ input, expectedRank }) => {
+    "당첨 번호와 보너스 번호를 넘겨주었을 때, 로또 번호와 비교하여 등수를 계산 기능",
+    ({ input, caculatedRank }) => {
       const lotto = new Lotto(input);
       const winningNumbers = [1, 2, 3, 4, 5, 6];
       const bonusNumber = 7;
 
       lotto.calculateRank(winningNumbers, bonusNumber);
 
-      expect(lotto.getRank()).toEqual(expectedRank);
+      expect(lotto.getRank()).toEqual(caculatedRank);
     }
   );
 
   test.each([
-    { lottoNumber: [1, 2, 3, 4, 5, 6], expectedProfit: profitByRank[0] },
-    { lottoNumber: [1, 2, 3, 4, 5, 7], expectedProfit: profitByRank[1] },
-    { lottoNumber: [1, 2, 3, 4, 5, 8], expectedProfit: profitByRank[2] },
-    { lottoNumber: [1, 2, 3, 4, 8, 9], expectedProfit: profitByRank[3] },
-    { lottoNumber: [1, 2, 3, 8, 9, 10], expectedProfit: profitByRank[4] },
-    { lottoNumber: [8, 9, 10, 11, 12, 13], expectedProfit: 0 },
-  ])('등수 입력시 수익을 반환하는 기능', ({ lottoNumber, expectedProfit }) => {
+    { lottoNumber: [1, 2, 3, 4, 5, 6], caculatedProfit: profitByRank[0] },
+    { lottoNumber: [1, 2, 3, 4, 5, 7], caculatedProfit: profitByRank[1] },
+    { lottoNumber: [1, 2, 3, 4, 5, 8], caculatedProfit: profitByRank[2] },
+    { lottoNumber: [1, 2, 3, 4, 8, 9], caculatedProfit: profitByRank[3] },
+    { lottoNumber: [1, 2, 3, 8, 9, 10], caculatedProfit: profitByRank[4] },
+    { lottoNumber: [8, 9, 10, 11, 12, 13], caculatedProfit: 0 },
+  ])("등수 입력시 수익을 반환하는 기능", ({ lottoNumber, caculatedProfit }) => {
     const lotto = new Lotto(lottoNumber);
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
@@ -54,17 +54,17 @@ describe('로또 클래스 테스트', () => {
 
     const profit = calculateProfit(lotto.getRank());
 
-    expect(profit).toBe(expectedProfit);
+    expect(profit).toBe(caculatedProfit);
   });
 
   test.each([
-    { lottoNumber: [1, 2, 3, 4, 5, 6], expectedProfit: profitByRank[0] },
-    { lottoNumber: [1, 2, 3, 4, 5, 7], expectedProfit: profitByRank[1] },
-    { lottoNumber: [1, 2, 3, 4, 5, 8], expectedProfit: profitByRank[2] },
-    { lottoNumber: [1, 2, 3, 4, 8, 9], expectedProfit: profitByRank[3] },
-    { lottoNumber: [1, 2, 3, 8, 9, 10], expectedProfit: profitByRank[4] },
-    { lottoNumber: [8, 9, 10, 11, 12, 13], expectedProfit: 0 },
-  ])('등수 입력시 수익을 반환하는 기능', ({ lottoNumber, expectedProfit }) => {
+    { lottoNumber: [1, 2, 3, 4, 5, 6], caculatedProfit: profitByRank[0] },
+    { lottoNumber: [1, 2, 3, 4, 5, 7], caculatedProfit: profitByRank[1] },
+    { lottoNumber: [1, 2, 3, 4, 5, 8], caculatedProfit: profitByRank[2] },
+    { lottoNumber: [1, 2, 3, 4, 8, 9], caculatedProfit: profitByRank[3] },
+    { lottoNumber: [1, 2, 3, 8, 9, 10], caculatedProfit: profitByRank[4] },
+    { lottoNumber: [8, 9, 10, 11, 12, 13], caculatedProfit: 0 },
+  ])("등수 입력시 수익을 반환하는 기능", ({ lottoNumber, caculatedProfit }) => {
     const lotto = new Lotto(lottoNumber);
     const winningNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
@@ -73,6 +73,6 @@ describe('로또 클래스 테스트', () => {
 
     const profit = calculateProfit(lotto.getRank());
 
-    expect(profit).toBe(expectedProfit);
+    expect(profit).toBe(caculatedProfit);
   });
 });
