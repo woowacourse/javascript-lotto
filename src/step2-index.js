0501e9af402b09c7e6ed7357fad2323f1b886140
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-21 02:33:06 +0900

    feat: 로또 모달 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,7 +1,34 @@
+/* eslint-disable no-undef */
 import './css/style.css';
+import './css/modal.css';
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 console.log('step2-index.js');
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
