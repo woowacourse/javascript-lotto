8518520de31919e226e49db7b5c853d32cc3bd83
cruelladevil <dev.timetravel@gmail.com>
2023-02-20 14:35:24 +0900

    feat: LottoController를 ConsoleController로 변경

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import LottoController from './LottoController';
-import InputView from './view/console/InputView';
-
 class App {
-  // 고민해볼 것 : 해당 커맨드를 웹 에서는 어떻게 사용할지?, 위치가 컨트롤러로 가야할 지?
-  static RESTART_COMMAND = 'y';
-  static EXIT_COMMAND = 'n';
+  #controller;
 
-  #lottoController;
-
-  constructor() {
-    this.#lottoController = new LottoController();
+  constructor(controller) {
+    this.#controller = controller;
   }
 
-  async play() {
-    await this.#lottoController.readMoney();
-    this.#lottoController.printBuyLottos();
-    await this.#lottoController.readWinNumbers();
-    await this.#lottoController.readBonusNumber();
-    this.#lottoController.printWinStatistics();
-    await this.readRestartCommand();
-  }
-
-  async readRestartCommand() {
-    const command = await InputView.readRestartCommand();
-    this.actionByCommand(command);
-  }
-
-  actionByCommand(command) {
-    if (command === App.RESTART_COMMAND) {
-      this.play();
-    }
-    if (command === App.EXIT_COMMAND) {
-      InputView.close();
-    }
+  play() {
+    this.#controller.init();
   }
 }
 
---
+++ src/ConsoleController.js
@@ -0,0 +1,45 @@
+import COMMAND from './constants/command';
+import LOTTO from './constants/lotto';
+import LottoService from './domain/LottoService';
+import InputView from './view/console/InputView';
+import OutputView from './view/console/OutputView';
+
+class ConsoleController {
+  #lottoService;
+
+  constructor() {
+    this.#lottoService = new LottoService();
+  }
+
+  async init() {
+    await this.purchaseLottos();
+    await this.setWinningLotto();
+    this.printStatstics();
+    await this.restartCommand();
+  }
+
+  async purchaseLottos() {
+    const money = await InputView.readPurchaseAmount();
+    this.#lottoService.purchaseLottos(money / LOTTO.COST);
+    this.#lottoService.getLottosNumbers().forEach(OutputView.printLottoNumbers);
+  }
+
+  async setWinningLotto() {
+    const winNumbers = await InputView.readWinNumbers();
+    const bonusNumber = await InputView.readBonusNumber();
+
+    this.#lottoService.setWinningLotto(winNumbers, bonusNumber);
+  }
+
+  printStatstics() {
+    this.#lottoService.getStatstics().forEach(([, stat]) => OutputView.printStatstics(stat));
+  }
+
+  async restartCommand() {
+    const command = await InputView.readRestartCommand();
+    if (command === COMMAND.RESTART) this.init();
+    if (command === COMMAND.QUIT) InputView.close();
+  }
+}
+
+export default ConsoleController;
