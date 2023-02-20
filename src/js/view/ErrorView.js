abd9f0c6bc219984de7e99b00222299dfdc3dbc1
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-20 22:15:21 +0900

    feat(view): 에러 메시지를 표시하는 기능

--- src/js/view/ErrorView.js
+++ src/js/view/ErrorView.js
@@ -2,6 +2,16 @@ class ErrorView {
   #errorAreaElement;
   #focusElement;
 
+  constructor(errorAreaElement, focusElement) {
+    this.#errorAreaElement = errorAreaElement;
+    this.#focusElement = focusElement;
+  }
+
+  renderErrorMessage(message) {
+    this.#errorAreaElement.innerHTML = this.#getErrorElement(message);
+    this.#focusElement.select();
+  }
+
   #getErrorElement(message) {
     return `
       <img class="error-icon" class="error-icon" src="./src/images/error_icon.png" />
