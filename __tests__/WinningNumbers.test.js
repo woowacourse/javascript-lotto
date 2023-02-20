675600624833530937ff41cdf626c82dcb99b20a
Suyoung <jsy8492@gmail.com>
2023-02-20 15:04:27 +0900

    refactor(WinningNumbers): 보너스 번호에 대한 테스트 케이스 추가

--- __tests__/WinningNumbers.test.js
+++ __tests__/WinningNumbers.test.js
@@ -26,4 +26,34 @@ describe('WinningNumbers 클래스 테스트', () => {
       }).not.toThrow();
     }
   );
+
+  test.each([['1'], ['23'], ['45']])(
+    '보너스 번호 입력 값이 1 ~ 45 이내의 숫자인 경우 정상 동작.',
+    (input) => {
+      const winningNumbers = new WinningNumbers('2,3,4,5,6,7');
+
+      expect(() => {
+        winningNumbers.initBonusNumber(input);
+      }).not.toThrow();
+    }
+  );
+
+  test.each([['0'], ['46']])(
+    '보너스 번호 입력 값이 1 ~ 45 이내의 숫자가 아닌 경우 예외처리.',
+    (input) => {
+      const winningNumbers = new WinningNumbers('2,3,4,5,6,7');
+
+      expect(() => {
+        winningNumbers.initBonusNumber(input);
+      }).toThrow();
+    }
+  );
+
+  test('보너스 번호와 당첨번호의 중복이 존재하는 경우 예외처리.', () => {
+    const winningNumbers = new WinningNumbers('2,3,4,5,6,7');
+
+    expect(() => {
+      winningNumbers.initBonusNumber(input);
+    }).toThrow();
+  });
 });
