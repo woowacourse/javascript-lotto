0aee2d375ab48e4e252c0958b2150efdd0c76b59
Suyoung <jsy8492@gmail.com>
2023-02-20 14:20:49 +0900

    refactor(LottoStatistics): 불필요한 유효성 검사 메서드 제거

--- src/domain/LottoStatistics.js
+++ src/domain/LottoStatistics.js
@@ -13,21 +13,8 @@ class LottoStatistics {
   #bonusNumber;
 
   constructor(winningNumbers, bonusNumber) {
-    this.validate(winningNumbers, bonusNumber);
     this.#winningNumbers = winningNumbers;
     this.#bonusNumber = bonusNumber;
-  }
-
-  validate(winningNumbers, bonusNumber) {
-    if (this.isDuplicateFor(winningNumbers, bonusNumber)) {
-      throw new Error(
-        '[ERROR] 당첨 번호와 보너스 번호에 중복이 존재할 수 없습니다.'
-      );
-    }
-  }
-
-  isDuplicateFor(winningNumbers, bonusNumber) {
-    return winningNumbers.includes(bonusNumber);
   }
 
   determineAllLottosRank(lottos) {
