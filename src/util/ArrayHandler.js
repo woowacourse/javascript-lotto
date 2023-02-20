fcc59d49d1d715e5b0fbb6aa5cf5745ac28a637c
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-02-20 18:01:33 +0900

    refactor: ArrayHandler.js 및 관련 테스트 삭제

--- src/util/ArrayHandler.js
+++
@@ -1,9 +0,0 @@
-const ArrayHandler = (function () {
-  return {
-    sortAscendingOrder(numbers) {
-      return [...numbers.sort((a, b) => a - b)];
-    },
-  };
-})();
-
-export default ArrayHandler;
