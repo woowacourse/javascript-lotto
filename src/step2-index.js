2a36af98c1dfa1afdc26d16c0296c6924a3a1559
2yunseong <dbsdltjd3701@naver.com>
2023-02-20 16:26:47 +0900

    feat: 모달창 삭제 버튼 이벤트 핸들링

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,11 @@
+/* eslint-disable no-undef */
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+const modal = document.querySelector('.modal');
+const modalCloseBtn = modal.querySelector('.modal-close-btn');
+
+modalCloseBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
