9a348a19528e3550fa4c3de796d52ce7887e5213
minjaeKim <smallkdb@gmail.com>
2023-02-20 14:23:36 +0900

    refeactor: numbers separator 적용

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,8 @@ export const LOTTO = {
   min: 1,
   max: 45,
   length: 6,
-  price: 1000,
-  prize: [2000000000, 30000000, 1500000, 50000, 5000],
+  price: 1_000,
+  prize: [2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000],
 };
 
 export const RANKING = {
@@ -19,4 +19,3 @@ export const COMMAND = {
   restart: 'y',
   quit: 'n',
 };
-
