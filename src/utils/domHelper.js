28945c1a71bbd8580e6276545f17bc6da8f4130d
n0eyes <doll4586633@naver.com>
2023-02-21 17:08:00 +0900

    feat: createEl 메서드 추가

--- src/utils/domHelper.js
+++ src/utils/domHelper.js
@@ -1,5 +1,20 @@
 const qs = (selector, target = document) => target.querySelector(selector);
 const qsAll = (selector, target = document) => target.querySelectorAll(selector);
 const component = (name) => qs(`[data-component="${name}"]`);
+const createEl = (tag, options = {}) => {
+  const $el = document.createElement(tag);
 
-export { qs, qsAll, component };
+  if (options.id) $el.id = options.id;
+  if (options.class) $el.className = options.class;
+  if (options.style)
+    Object.entries(options.style).forEach(([key, value]) => {
+      $el.style[key] = value;
+    });
+
+  if (options.attributes)
+    Object.entries(options.attributes).forEach(([key, value]) => $el.setAttribute(key, value));
+
+  return $el;
+};
+
+export { qs, qsAll, component, createEl };
