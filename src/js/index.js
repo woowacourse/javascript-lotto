a2de9f8a217d45cd629c071a5f872031dfcb7a04
woo-jk <201702042@o.cnu.ac.kr>
2023-02-21 16:16:16 +0900

    feat: index.js 컨트롤러 init 함수 생성

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
+import { $, $$ } from './dom';
+class App {
+  init() {
+    this.bindEventListener();
+  }
 
+  test() {
+    const money = $('#money-input').value;
+    alert(money);
+  }
+
+  bindEventListener() {
+    $('#purchase-button').addEventListener('click', this.test);
+  }
+}
+
+const app = new App();
+app.init();
