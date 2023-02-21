eec541980a043622b6cfb0cf6ca7c4d5b82e8b62
inyeong-kang <tomatorkd@naver.com>
2023-02-21 13:17:23 +0900

    chore: 불필요한 테스트 삭제

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,6 +1,4 @@
-const { profitByRank } = require('../src/constants/constants');
 const Lotto = require('../src/domain/model/Lotto');
-const { calculateProfit } = require('../src/utils');
 
 describe('Lotto 클래스 테스트', () => {
   const INPUT_LOTTO_NUMBERS = [
@@ -19,15 +17,6 @@ describe('Lotto 클래스 테스트', () => {
       { lottoNumbers: [1, 2, 3, 8, 9, 10], expectedRank: 5 },
       { lottoNumbers: [8, 9, 10, 11, 12, 13], expectedRank: undefined },
     ],
-  ];
-
-  const INPUT_LOTTO_NUMBERS_PER_PROFIT = [
-    { lottoNumbers: [1, 2, 3, 4, 5, 6], expectedProfit: profitByRank[0] },
-    { lottoNumbers: [1, 2, 3, 4, 5, 7], expectedProfit: profitByRank[1] },
-    { lottoNumbers: [1, 2, 3, 4, 5, 8], expectedProfit: profitByRank[2] },
-    { lottoNumbers: [1, 2, 3, 4, 8, 9], expectedProfit: profitByRank[3] },
-    { lottoNumbers: [1, 2, 3, 8, 9, 10], expectedProfit: profitByRank[4] },
-    { lottoNumbers: [8, 9, 10, 11, 12, 13], expectedProfit: 0 },
   ];
 
   const winningNumbers = [1, 2, 3, 4, 5, 6];
@@ -50,19 +39,6 @@ describe('Lotto 클래스 테스트', () => {
       lotto.calculateRank(winningNumbers, bonusNumber);
 
       expect(lotto.getRank()).toEqual(expectedRank);
-    }
-  );
-
-  test.each(INPUT_LOTTO_NUMBERS_PER_PROFIT)(
-    '주어진 당첨 번호와 보너스 번호로 로또 번호와 비교하여 수익을 계산하여 반환해야 한다.',
-    ({ lottoNumbers, expectedProfit }) => {
-      const lotto = new Lotto(lottoNumbers);
-
-      lotto.calculateRank(winningNumbers, bonusNumber);
-
-      const profit = calculateProfit(lotto.getRank());
-
-      expect(profit).toBe(expectedProfit);
     }
   );
 });
