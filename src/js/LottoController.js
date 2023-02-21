152a92b10bf01dbae9deba0b87d8cacc54d5625d
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-21 21:33:16 +0900

    feat(controller): WinningLottoInputView로부터 받아온 당첨 번호 및 보너스 넘버의 데이터를 가공하는 기능

--- src/js/LottoController.js
+++ src/js/LottoController.js
@@ -1,5 +1,8 @@
 import { $ } from './util/querySelector.js';
 import PurchaseView from './view/PurchaseView.js';
+import WinningLottoInputView from './view/WinningLottoInputView.js';
+import ResultModalView from './view/ResultModalView.js';
+import InputCleaner from './view/InputCleaner.js';
 import ErrorView from './view/ErrorView.js';
 import LottoGame from './domain/LottoGame.js';
 import validator from './domain/validator.js';
@@ -7,6 +10,10 @@ import validator from './domain/validator.js';
 class LottoController {
   #lottoGame;
   #purchaseView = new PurchaseView((budget) => this.handleBudget(budget));
+  #winningLottoInputView = new WinningLottoInputView((winningLotto) =>
+    this.handleWinningLotto(winningLotto)
+  );
+  #resultModalView = new ResultModalView(() => this.handleRestart());
 
   handleBudget(budget) {
     const budgetErrorView = new ErrorView($('#buyErrorArea'), $('#budgetInput'));
@@ -16,9 +23,24 @@ class LottoController {
       budgetErrorView.hideErrorMessage();
       this.#lottoGame = new LottoGame(budget);
       this.#purchaseView.render(this.#lottoGame.getBoughtLottos());
+      this.#winningLottoInputView.show();
     } catch ({ message }) {
       budgetErrorView.renderErrorMessage(message);
     }
+  }
+
+  #parseWinningLotto(lottoFormData) {
+    const winningLottos = [];
+
+    Array.from({ length: 6 }).forEach((_, index) => {
+      winningLottos.push(Number(lottoFormData[`number-${index + 1}`]));
+    });
+
+    return winningLottos;
+  }
+
+  #parseBonusNumber(lottoFormData) {
+    return Number(lottoFormData['bonus-number']);
   }
 }
 
