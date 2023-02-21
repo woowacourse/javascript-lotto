392cb6a8656c7053320c2095490ba6a5dba31872
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-21 14:22:23 +0900

    feat: 불러와야 하는 파일 경로 포함

--- src/step2-index.js
+++ src/step2-index.js
@@ -3,14 +3,8 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-import express from 'express';
-import path from 'path';
+import './css/style.css';
+import './js/index.js';
 
-const __dirname = path.resolve();
-const app = express();
-
-app.use(express.static(__dirname + '/../'));
-
-app.listen(3000, () => {
-  console.log('로또 게임 서버를 시작했습니다!');
-});
+import './images/error_icon.png';
+import './images/lotto_icon.png';
