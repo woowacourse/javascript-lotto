75ce5a907cd12938dd454c06d86fcc174256b489
hozzijeong <ghwls0905@naver.com>
2023-02-21 15:49:59 +0900

    refactor: 구매한 로또 개수 paint 메소드 변경

--- src/components/enterGameBoard.js
+++ src/components/enterGameBoard.js
@@ -1,10 +1,13 @@
 import { LOTTO_LENGTH } from '../data/Constants';
+import { clearConatiner } from '../utils/Utils';
+import { showModal } from '../view/modal';
 import {
   enterWinNumberMessage,
   winNumberMessage,
   bonusNumberMessage,
   numberInput,
 } from '../view/templates/lottoGame';
+import lottoResultBoard from './lottoResult';
 
 function numberTitleContainer() {
   const $container = document.createElement('div');
@@ -49,7 +52,7 @@ function checkResultButton(callback) {
   return $button;
 }
 
-export default function enterGameBoard(callback) {
+function enterGameBoard(callback) {
   const enterBoard = document.createElement('div');
 
   enterBoard.innerHTML = enterWinNumberMessage;
@@ -58,4 +61,27 @@ export default function enterGameBoard(callback) {
   enterBoard.appendChild(checkResultButton(callback));
 
   return enterBoard;
+}
+
+const showResultButton = () => {
+  showModal();
+  const $modalContent = document.querySelector('.modal-content');
+  clearConatiner($modalContent);
+  $modalContent.appendChild(
+    lottoResultBoard(
+      {
+        FIFTH: 0,
+        FOURTH: 0,
+        THIRD: 0,
+        SECOND: 0,
+        FIRST: 0,
+      },
+      0.0
+    )
+  );
+};
+
+export default function paintEnterWinningNumber() {
+  const $lottoSection = document.querySelector('.lotto-section');
+  $lottoSection.appendChild(enterGameBoard(showResultButton));
 }
