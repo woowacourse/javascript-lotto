b39d62d712d8eafe989e5d07051207edfd848236
sh981013s <sh981013s@gmail.com>
2023-02-21 17:39:17 +0900

    feat(generalStyleValues): 스타일 관련 상수 정리

---
+++ src/js/constants/generalStyleValues.js
@@ -0,0 +1,44 @@
+const generalStyleValues = {
+  colors: {
+    lottoPrimary: '#4E5BA6',
+    lottoGreyScaleOne: '#FFFFFF',
+    lottoGreyScaleTwo: '#FCFCFD',
+    lottoGreyScaleThree: '#B4B4B4',
+    lottoGreyScaleFour: '#8B8B8B',
+    lottoGreyScaleFive: '#000000',
+  },
+
+  typo: {
+    title: {
+      weight: 800,
+      size: 24,
+      height: 36,
+    },
+
+    subtitle: {
+      weight: 600,
+      size: 20,
+      height: 24,
+    },
+
+    body: {
+      weight: 400,
+      size: 15,
+      height: 24,
+    },
+
+    placeholder: {
+      weight: 400,
+      size: 15,
+      height: 24,
+    },
+
+    caption: {
+      weight: 700,
+      size: 14,
+      height: 24,
+    },
+  },
+};
+
+export default generalStyleValues;
