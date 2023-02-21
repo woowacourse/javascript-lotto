890c7adee1f781ed06f35189092d9bfc6bb980d3
2yunseong <dbsdltjd3701@naver.com>
2023-02-21 13:41:17 +0900

    feat: 구매한 로또 출력하는 기능 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,11 +1,44 @@
+import LottoMachine from './domain/LottoMachine';
+
 /* eslint-disable no-undef */
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+let lottoMachine;
 const modal = document.querySelector('.modal');
 const modalCloseBtn = modal.querySelector('.modal-close-btn');
+const moneyInput = document.querySelector('.money-input');
+const buyBtn = document.querySelector('.buy-btn');
+const lottosContainer = document.querySelector('.lottos-container');
+const moneyContainer = document.querySelector('.money-container');
+
+const renderLottoListTitle = (amount) => {
+  const title = document.createElement('p');
+  title.innerText = `총 ${amount}개를 구매했습니다.`;
+  moneyContainer.appendChild(title);
+};
+
+const renderLottoList = (lottoNumbers) => {
+  renderLottoListTitle(lottoNumbers.length);
+  lottoNumbers.forEach((lottoNumber) => {
+    const lottoElement = document.createElement('div');
+    const img = document.createElement('img');
+    const lottoNumberElement = document.createElement('p');
+    img.src = './lotto.png';
+    img.className = 'lotto-picture';
+    const lottoText = `${lottoNumber.join(', ')}`;
+    lottoNumberElement.innerText = lottoText;
+    lottoElement.appendChild(img);
+    lottoElement.appendChild(lottoNumberElement);
+    lottosContainer.appendChild(lottoElement);
+  });
+};
 
 modalCloseBtn.addEventListener('click', () => {
   modal.style.display = 'none';
+});
+
+buyBtn.addEventListener('click', () => {
+  const buyMoney = moneyInput.value;
+  // TODO: 유효성 검사
+  lottoMachine = new LottoMachine(buyMoney);
+  const lottoNumbers = lottoMachine.getLottoNumbers();
+  renderLottoList(lottoNumbers);
 });
