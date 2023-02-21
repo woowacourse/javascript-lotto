bbc262c2c78123c64d13fda592f1652281c7b354
hafnium1923 <hafnium1923@gmail.com>
2023-02-21 18:04:12 +0900

    feat: 구매한 로또 화면 출력

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,15 +1,24 @@
 const LottoView = require("../view/LottoView");
 const LottoGame = require("../domain/LottoGame");
+const Validation = require("../domain/Validation");
+
 const moneyForm = document.getElementById("money_form");
 
 const Controller = {
   /** @type {LottoGame} */
   lottoGame: undefined,
   play() {
+    moneyForm.addEventListener("submit", this.purchaseLotto);
+  },
+
+  purchaseLotto(event) {
     this.lottoGame = new LottoGame();
-    moneyForm.addEventListener("submit", LottoView.readMoney);
+    event.preventDefault();
+    const money = document.getElementById("input_money").value;
+    this.lottoGame.lottos = money;
+    LottoView.printLottoConunt(this.lottoGame.lottoCount);
+    LottoView.printLottos(this.lottoGame.lottos);
   },
 };
 
 module.exports = Controller;
-
--- src/view/LottoView.js
+++ src/view/LottoView.js
@@ -1,26 +1,26 @@
-const moneyForm = document.getElementById("money_form");
-
-function readMoney(event) {
-  event.preventDefault();
-  const text = document.getElementById("buy_count_text");
-  const money = document.getElementById("input_money").value;
-  text.innerText = `총${money}개를 구매했습니다.`;
-  // return money;
-}
-
-moneyForm.addEventListener("submit", readMoney);
-
 const LottoView = {
-  readMoney(event) {
-    event.preventDefault();
+  // readMoney(event) {
+  //   event.preventDefault();
+  //   const text = document.getElementById("buy_count_text");
+  //   const money = document.getElementById("input_money").value;
+  //   text.innerText = `총${money}개를 구매했습니다.`;
+  //   // return money;
+  // },
+
+  printLottoConunt(lottoCount) {
+    //event.preventDefault();
     const text = document.getElementById("buy_count_text");
-    const money = document.getElementById("input_money").value;
-    text.innerText = `총${money}개를 구매했습니다.`;
+    text.innerText = `총${lottoCount}개를 구매했습니다.`;
     // return money;
   },
-  
-  
 
+  printLottos(lottos) {
+    const result = lottos
+      .map((lotto) => `🎟️ ${lotto.numbers.join(", ")}\n\n`)
+      .join("");
+    const text = document.getElementById("buy_lottos");
+    text.innerText = result;
+  },
 };
 
 module.exports = LottoView;
