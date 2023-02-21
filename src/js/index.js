461c0010b9f72d57bf509691938168455f19fbe3
woo-jk <201702042@o.cnu.ac.kr>
2023-02-21 17:44:24 +0900

    feat: 구입 버튼 클릭시 당첨 번호 입력 칸 보이는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,14 @@ class App {
   }
 
   handleClickPurchaseButton = () => {
-    const money = $('#money-input').value;
+    const $moneyInput = $('#money-input');
+    const money = $moneyInput.value;
     try {
       this.#lottoGame.purchaseLottos(money);
       this.showLottoList();
+      this.showWinningLottoInput();
+      this.showResultButton();
+      $moneyInput.value = '';
     } catch (error) {
       alert(error.message);
     }
@@ -40,60 +44,39 @@ class App {
   bindEventListener() {
     $('#purchase-button').addEventListener('click', this.handleClickPurchaseButton);
   }
+
+  showWinningLottoInput = () => {
+    const $winningLottoContainer = $('#winning-lotto-container');
+    const winningLottoInputTemplate = `<div class="game-message">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
+    <div id="number-input-container">
+      <div id="lotto-number-container" class="number-container">
+        <div class="game-message">당첨 번호</div>
+        <div id="lotto-number-input-list">
+          <input id="first-number" class="lotto-number-input" type="text" />
+          <input id="second-number" class="lotto-number-input" type="text" />
+          <input id="third-number" class="lotto-number-input" type="text" />
+          <input id="fourth-number" class="lotto-number-input" type="text" />
+          <input id="fifth-number" class="lotto-number-input" type="text" />
+          <input id="sixth-number" class="lotto-number-input" type="text" />
+        </div>
+      </div>
+      <div id="bonus-number-container" class="number-container">
+        <div class="game-message">보너스 번호</div>
+        <input id="bonus-number" class="lotto-number-input" type="text" />
+      </div>
+    </div>`;
+    $winningLottoContainer.innerHTML = winningLottoInputTemplate;
+  };
+
+  showResultButton = () => {
+    const $gameContainer = $('#game-container');
+    const $resultButton = $('#resultButton');
+    const resultButtonTemplate = `<button id='resultButton'>결과 확인하기</button>`;
+
+    if ($resultButton) $resultButton.remove();
+    $gameContainer.insertAdjacentHTML('beforeend', resultButtonTemplate);
+  };
 }
 
 const app = new App();
 app.init();
-
-{
-  /* <div class="game-message">총 7개를 구매하셨습니다.</div>
-            <ul id="lotto-list">
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-              <li>
-                <span class="ticket-emoji">🎟️</span
-                ><span class="lotto-number">12, 28, 22, 37, 19, 23</span>
-              </li>
-            </ul> */
-}
-
-// <div class="game-message">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
-// <div id="number-input-container">
-//   <div id="lotto-number-container" class="number-container">
-//     <div class="game-message">당첨 번호</div>
-//     <div id="lotto-number-input-list">
-//       <input id="first-number" class="lotto-number-input" type="text" />
-//       <input id="second-number" class="lotto-number-input" type="text" />
-//       <input id="third-number" class="lotto-number-input" type="text" />
-//       <input id="fourth-number" class="lotto-number-input" type="text" />
-//       <input id="fifth-number" class="lotto-number-input" type="text" />
-//       <input id="sixth-number" class="lotto-number-input" type="text" />
-//     </div>
-//   </div>
-//   <div id="bonus-number-container" class="number-container">
-//     <div class="game-message">보너스 번호</div>
-//     <input id="bonus-number" class="lotto-number-input" type="text" />
-//   </div>
-// </div>
