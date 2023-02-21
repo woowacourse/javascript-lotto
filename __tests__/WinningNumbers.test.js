5a0ce4e0c47cfc2892771ac6dea7c40bd3e1b875
inyeong-kang <tomatorkd@naver.com>
2023-02-21 13:16:46 +0900

    refactor: WinningNumbers 클래스 테스트

--- __tests__/WinningNumbers.test.js
+++ __tests__/WinningNumbers.test.js
@@ -1,11 +1,12 @@
 const WinningNumbers = require('../src/domain/model/WinningNumbers');
 
 describe('WinningNumbers 클래스 테스트', () => {
-  test('입력된 당첨 번호 값을 필드로 가지는 WinningNumbers 인스턴스를 생성해야 한다.', () => {
-    const input = '1,2,3,4,5,6';
+  test('주어진 당첨 번호와 보너스 번호를 필드로 가지는 WinningNumbers 인스턴스를 생성해야 한다.', () => {
+    const winningNumbersArr = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    const winningNumbers = new WinningNumbers(winningNumbersArr, bonusNumber);
 
-    const winningNumbers = new WinningNumbers(input);
-
-    expect(winningNumbers.getNumbers()).toEqual(input.split(',').map(Number));
+    expect(winningNumbers.getWinningNumbers()).toBe(winningNumbersArr);
+    expect(winningNumbers.getBonusNumber()).toBe(bonusNumber);
   });
 });
