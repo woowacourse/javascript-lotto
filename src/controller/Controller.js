aeb8a000bc4587dc973e755704bf98406f0b5381
hafnium1923 <hafnium1923@gmail.com>
2023-02-21 18:16:13 +0900

    feat: 금액 입력 시 당첨 번호 입력칸 출력

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,8 +1,8 @@
 const LottoView = require("../view/LottoView");
 const LottoGame = require("../domain/LottoGame");
-const Validation = require("../domain/Validation");
 
 const moneyForm = document.getElementById("money_form");
+const win_contents = document.getElementById("win_contents");
 
 const Controller = {
   /** @type {LottoGame} */
@@ -18,6 +18,7 @@ const Controller = {
     this.lottoGame.lottos = money;
     LottoView.printLottoConunt(this.lottoGame.lottoCount);
     LottoView.printLottos(this.lottoGame.lottos);
+    win_contents.classList.remove("hidden");
   },
 };
 
--- src/view/LottoView.js
+++ src/view/LottoView.js
@@ -1,17 +1,7 @@
 const LottoView = {
-  // readMoney(event) {
-  //   event.preventDefault();
-  //   const text = document.getElementById("buy_count_text");
-  //   const money = document.getElementById("input_money").value;
-  //   text.innerText = `총${money}개를 구매했습니다.`;
-  //   // return money;
-  // },
-
   printLottoConunt(lottoCount) {
-    //event.preventDefault();
     const text = document.getElementById("buy_count_text");
     text.innerText = `총${lottoCount}개를 구매했습니다.`;
-    // return money;
   },
 
   printLottos(lottos) {
