7a34223cbba564bba7735b27aa26c7fdd1d57755
2yunseong <dbsdltjd3701@naver.com>
2023-02-21 16:46:04 +0900

    test: 로직 이전에 따른 테스트 이전

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -14,24 +14,6 @@ describe('Lotto 클래스 입니다.', () => {
     const lotto = new Lotto(numbers);
     expect(lotto.getNumbers()).toEqual(expected);
   });
-  test.each([1, 2, 3, 4, 34, 44, 45])('유효한 로또 숫자이면 true를 반환한다.', (number) => {
-    expect(Lotto.isValidLottoNumber(number)).toBeTruthy();
-  });
-
-  test.each([-1, 0, 46, 47])('유효한 로또 숫자가 아니면 false를 반환한다.', (number) => {
-    expect(Lotto.isValidLottoNumber(number)).toBeFalsy();
-  });
-
-  test.each([
-    [[2, 1, 2, 3, 4, 5]],
-    [[1, 1, 2, 3, 4, 5, 6]],
-    [[5, 6, 7, 8, 44, 44]],
-    [[1, 44, 45, 42, 43, 44]],
-    [[1, 13, 8, 16, 4, 12, 15, 16]],
-    [[10, 11, 12, 13, 14]],
-  ])('로또 번호가 중복되었는지와 길이(갯수)가 유효한지 검사한다.', (numbers) => {
-    expect(Lotto.isDuplicateNumbers(numbers)).toBeTruthy();
-  });
 
   test('보너스 번호를 가지고 있으면 true를 반환한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,4 +1,9 @@
-import { isPositiveInteger, isValidRestartCommand } from '../src/validation';
+import {
+  isPositiveInteger,
+  isValidRestartCommand,
+  isValidLottoNumber,
+  isDuplicateNumbers,
+} from '../src/validation';
 
 describe('유효성 검증 테스트입니다.', () => {
   test.each([1.11, -1, 2.3, 3.1])('양의 정수가 아니면 false를 반환한다.', (value) => {
@@ -15,4 +20,23 @@ describe('유효성 검증 테스트입니다.', () => {
       expect(isValidRestartCommand(input)).toBeFalsy();
     },
   );
+
+  test.each([1, 2, 3, 4, 34, 44, 45])('유효한 로또 숫자이면 true를 반환한다.', (number) => {
+    expect(isValidLottoNumber(number)).toBeTruthy();
+  });
+
+  test.each([-1, 0, 46, 47])('유효한 로또 숫자가 아니면 false를 반환한다.', (number) => {
+    expect(isValidLottoNumber(number)).toBeFalsy();
+  });
+
+  test.each([
+    [[2, 1, 2, 3, 4, 5]],
+    [[1, 1, 2, 3, 4, 5, 6]],
+    [[5, 6, 7, 8, 44, 44]],
+    [[1, 44, 45, 42, 43, 44]],
+    [[1, 13, 8, 16, 4, 12, 15, 16]],
+    [[10, 11, 12, 13, 14]],
+  ])('로또 번호가 중복되었는지와 길이(갯수)가 유효한지 검사한다.', (numbers) => {
+    expect(isDuplicateNumbers(numbers)).toBeTruthy();
+  });
 });
