e3c0fbf6bd255102bb7edbcf0dc657a95a91ada9
solo5star <solo_5@naver.com>
2023-02-22 18:21:14 +0900

    refactor(console): 엔트리포인트 리팩토링

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,4 +1,12 @@
-import App from './App';
+import LottoController from './LottoController';
+import App from './view/console/App';
+import InputView from './view/web/InputView';
+import OutputView from './view/web/OutputView';
 
-const app = new App();
+const lottoController = new LottoController({
+  inputView: InputView,
+  outputView: OutputView,
+});
+
+const app = new App(lottoController);
 app.play();
