2046b99cccf817a6300f0aebd67b3b091fb2006a
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-22 12:53:10 +0900

    feat: 단계별 안내 메시지 세분화

--- src/js/constants/index.js
+++ src/js/constants/index.js
@@ -9,37 +9,62 @@ const GAME_VALUE = Object.freeze({
 });
 
 const FORMATTING_TYPE = Object.freeze({
-  BOUGHT_LOTTOS: 0,
-  WINNING_STATUS: 1,
-  PROFIT_RATE: 2,
+  BOUGHT_LOTTOS: 'boughtLottos',
+  WINNING_STATUS: 'winningStatus',
+  PROFIT_RATE: 'profitRate',
+});
+
+const QUESTION = Object.freeze({
+  BUDGET: '> 구입금액을 입력해 주세요.',
+  WINNING_LOTTO: '\n> 당첨 번호를 입력해 주세요. ',
+  BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요. ',
+  RESTART: '\n> 다시 시작하시겠습니까? (y/n) ',
 });
 
 const MESSAGE = Object.freeze({
-  ASK_BUDGET: '> 구입금액을 입력해 주세요.',
-  ASK_WINNING_LOTTO: '\n> 당첨 번호를 입력해 주세요. ',
-  ASK_BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요. ',
-  ASK_RESTART: '\n> 다시 시작하시겠습니까? (y/n) ',
-
-  BUY_RESULT: (buyCount) => `${buyCount}개를 구매했습니다.`,
+  BUY_COUNT: (buyCount) => `${buyCount}개를 구매했습니다.`,
   PRIZE_RESULT: (gameData) =>
     Trimmer.trimTemplate(
       `
-        당첨 통계
-        ${'-'.repeat(20)}
-        3개 일치 (${GAME_VALUE.PRIZE[4].toLocaleString()}원) - ${gameData.fifth}개
-        4개 일치 (${GAME_VALUE.PRIZE[3].toLocaleString()}원) - ${gameData.fourth}개
-        5개 일치 (${GAME_VALUE.PRIZE[2].toLocaleString()}원) - ${gameData.third}개
-        5개 일치, 보너스 볼 일치 (${GAME_VALUE.PRIZE[1].toLocaleString()}원) - ${gameData.second}개
-        6개 일치 (${GAME_VALUE.PRIZE[0].toLocaleString()}원) - ${gameData.first}개`
+      당첨 통계
+      ${'-'.repeat(20)}
+      3개 일치 (${GAME_VALUE.PRIZE[4].toLocaleString()}원) - ${gameData.fifth}개
+      4개 일치 (${GAME_VALUE.PRIZE[3].toLocaleString()}원) - ${gameData.fourth}개
+      5개 일치 (${GAME_VALUE.PRIZE[2].toLocaleString()}원) - ${gameData.third}개
+      5개 일치, 보너스 볼 일치 (${GAME_VALUE.PRIZE[1].toLocaleString()}원) - ${gameData.second}개
+      6개 일치 (${GAME_VALUE.PRIZE[0].toLocaleString()}원) - ${gameData.first}개`
     ),
   PROFIT_RATE_RESULT: (formattedRate) => `총 수익률은 ${formattedRate}%입니다.`,
+});
 
-  ERROR_DECIMAL: '구입 금액은 정수여야 합니다.',
-  ERROR_INVALID_LOTTO_FORMAT: '로또는 콤마(,)로 구분되는 6개의 1 이상 45 이하의 정수여야 합니다.',
-  ERROR_LOTTO_DUPLICATES: '로또 번호는 중복되는 수가 없어야 합니다',
-  ERROR_INVALID_BONUS_NUMBER: '보너스 번호는 1이상 45이하의 정수여야 합니다',
-  ERROR_INVALID_RETRY_COMMAND: '재시작 여부는 y 또는 n이어야 합니다.',
-  ERROR_BUDGET_NOT_DIVISIBLE: (divisor) => `구입 금액은 ${divisor}원 단위여야 합니다`,
+const ERROR = Object.freeze({
+  ERROR_DECIMAL: {
+    [PROJECT_MODE.CONSOLE]: '[ERROR] 구입 금액은 정수여야 합니다.',
+    [PROJECT_MODE.WEB]: '구입 금액은 정수로 입력해 주세요!',
+  },
+  ERROR_INVALID_LOTTO_FORMAT: {
+    [PROJECT_MODE.CONSOLE]:
+      '[ERROR] 로또는 콤마(,)로 구분되는 6개의 1 이상 45 이하의 정수여야 합니다.',
+    [PROJECT_MODE.WEB]: '당첨 로또 번호를 모두 입력해 주세요!',
+  },
+  ERROR_LOTTO_DUPLICATES: {
+    [PROJECT_MODE.CONSOLE]: '[ERROR] 로또 번호는 중복되는 수가 없어야 합니다',
+    [PROJECT_MODE.WEB]: '중복되는 로또 번호가 없도록 입력해 주세요!',
+  },
+  ERROR_INVALID_BONUS_NUMBER: {
+    [PROJECT_MODE.CONSOLE]: '[ERROR] 보너스 번호는 1이상 45이하의 정수여야 합니다',
+    [PROJECT_MODE.WEB]: '보너스 번호는 1 이상 45 이하의 정수로 입력해 주세요!',
+  },
+  ERROR_INVALID_RETRY_COMMAND: '[ERROR] 재시작 여부는 y 또는 n이어야 합니다.',
+  ERROR_BUDGET_NOT_DIVISIBLE: {
+    [PROJECT_MODE.CONSOLE]: (divisor) => `[ERROR] 구입 금액은 ${divisor}원 단위여야 합니다`,
+    [PROJECT_MODE.WEB]: (divisor) => `구입 금액은 ${divisor}원 단위로 입력해 주세요!`,
+  },
+});
+
+const PROJECT_MODE = Object.freeze({
+  CONSOLE: 'console',
+  WEB: 'web',
 });
 
 const REGEX = Object.freeze({
@@ -61,4 +86,14 @@ const RANK_FORMAT = Object.freeze({
   5: 'fifth',
 });
 
-export { FORMATTING_TYPE, MESSAGE, REGEX, COMMAND, GAME_VALUE, RANK_FORMAT };
+export {
+  GAME_VALUE,
+  FORMATTING_TYPE,
+  QUESTION,
+  MESSAGE,
+  ERROR,
+  PROJECT_MODE,
+  REGEX,
+  COMMAND,
+  RANK_FORMAT,
+};
