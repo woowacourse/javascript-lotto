49c3a0aef27464f01afe72bc9c69607b6bf9b0c8
Suyoung <jsy8492@gmail.com>
2023-02-22 13:29:25 +0900

    feat(LottoPurchaseForm): 로또 구입 후 구입 금액 입력 폼 비활성화 기능 구현

--- src/view/CheckWinningSection/LottoPurchaseForm.js
+++ src/view/CheckWinningSection/LottoPurchaseForm.js
@@ -39,6 +39,8 @@ class LottoPurchaseForm {
       eventType: 'purchaseLotto',
       data: inputEl.valueAsNumber,
     });
+    $('#lotto-purchase-input').disabled = true;
+    $('.lotto-purchase-button').disabled = true;
   }
 }
 
