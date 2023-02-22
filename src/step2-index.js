b26d3236af9ba273129d0de577b5ad8504b54241
yogjin <youngjin988@gmail.com>
2023-02-23 02:27:17 +0900

    feat: 다시 시작하는 기능 추가, 로또를 구입한 후에만 (구매한 로또, 당첨번호 입력 칸, 결과보기 버튼)이 보이도록 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -9,23 +9,36 @@ import { view } from "./view-web/view";
 const lottoGame = new LottoGame(view);
 
 const $purchaseAmountForm = document.getElementById("div-purchaseAmount");
-const $resultButton = document.getElementById("button-result");
-
 const $purchaseAmountInput = document.getElementById("input-purchaseAmount");
+const $lottos = document.querySelector(".lottos");
+const $winningNumberInputs = document.querySelectorAll(".input-winningLotto");
+const $bonusNumberInput = document.querySelector(".input-bonusNumber");
+const $resultButton = document.getElementById("button-result");
+const $statisticsModal = document.getElementById("modal-statistics");
+const $restartButton = document.getElementById("restart-button");
+
+const $afterPurchaseDiv = document.getElementById("after-purchase");
 
 $purchaseAmountForm.addEventListener("submit", (e) => {
   e.preventDefault();
   lottoGame.lottoPurchase($purchaseAmountInput.value);
   lottoGame.showLottos();
+  $afterPurchaseDiv.style.visibility = "visible";
 });
 
 $resultButton.addEventListener("click", (e) => {
-  const $statisticsModal = document.getElementById("modal-statistics");
-  const winningLottoNumbers = [...document.querySelectorAll(".input-winningLotto")].map((input) =>
-    Number(input.value)
-  );
-  const bonusNumber = Number(document.querySelector(".input-bonusNumber").value);
+  const winningLottoNumbers = [...$winningNumberInputs].map((input) => Number(input.value));
+  const bonusNumber = Number($bonusNumberInput.value);
   lottoGame.setWinningLotto(winningLottoNumbers, bonusNumber);
   lottoGame.showStatistics($purchaseAmountInput.value);
   $statisticsModal.showModal();
+});
+
+$restartButton.addEventListener("click", (e) => {
+  $purchaseAmountInput.value = "";
+  $lottos.innerHTML = "";
+  $winningNumberInputs.forEach((input) => (input.value = ""));
+  $bonusNumberInput.value = "";
+  $afterPurchaseDiv.style.visibility = "hidden";
+  $statisticsModal.close();
 });
