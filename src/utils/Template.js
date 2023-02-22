64420904d01b8d8e08dbb259c8c3087c807317e9
ashleysyheo <ashleysyheo@gmail.com>
2023-02-22 14:03:05 +0900

    refactor: display user lotto list with list tags

--- src/utils/Template.js
+++ src/utils/Template.js
@@ -1,10 +1,10 @@
 import { StaticValue } from '../constants/Constants.js';
 
 const generateUserLottoNumbers = (numbers) => `
-  <div class="user-lotto">
+  <li class="user-lotto">
     <p class="lotto-icon">🎟️</p>
     <p class="lotto-numbers">${numbers.join(StaticValue.PRINT_SEPARATOR)}</p>
-  </div>
+  </li>
 `
 
 export { generateUserLottoNumbers };
