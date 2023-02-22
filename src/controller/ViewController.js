7a15acbddf0c259db3d6158d31c4315b7999141d
hafnium1923 <hafnium1923@gmail.com>
2023-02-22 14:41:11 +0900

    feat: 당첨 번호 입력 시 당첨 데이터 생성 기능

--- src/controller/ViewController.js
+++ src/controller/ViewController.js
@@ -2,23 +2,54 @@ const LottoView = require("../view/LottoView");
 const LottoGame = require("../domain/LottoGame");
 
 const moneyForm = document.getElementById("money_form");
-const win_contents = document.getElementById("win_contents");
+const winContents = document.getElementById("win_contents");
+const winNumForm = document.getElementById("winNum_form");
 
 const ViewController = {
   /** @type {LottoGame} */
   lottoGame: undefined,
+
   play() {
-    moneyForm.addEventListener("submit", this.purchaseLotto);
+    this.lottoGame = new LottoGame();
+    moneyForm.addEventListener("submit", (event) => {
+      this.purchaseLotto(event, this.lottoGame);
+    });
+    winNumForm.addEventListener("submit", (event) => {
+      this.lotteryTicket(event, this.lottoGame);
+    });
   },
 
-  purchaseLotto(event) {
-    this.lottoGame = new LottoGame();
+  lotteryTicket(event, lottoGame) {
     event.preventDefault();
+
+    const winNum = document.getElementsByName("winNum[]");
+    const bonusNum = document.getElementsByName("bonusNum")[0].value;
+    const winNumbers = new Array();
+
+    winNum.forEach((winNum) => {
+      const num = parseInt(winNum.value, 10);
+      winNumbers.push(num);
+    });
+    const bonusNumber = parseInt(bonusNum, 10);
+
+    lottoGame.makeWinLotto(winNumbers, bonusNumber);
+    const rankResult = lottoGame.calculateRankResult();
+    const revenue = lottoGame.calculateRevenueRate(rankResult);
+
+    console.log(rankResult);
+    console.log(revenue);
+  },
+
+  purchaseLotto(event, lottoGame) {
+    event.preventDefault();
+
     const money = document.getElementById("input_money").value;
-    this.lottoGame.lottos = money;
-    LottoView.printLottoConunt(this.lottoGame.lottoCount);
-    LottoView.printLottos(this.lottoGame.lottos);
-    win_contents.classList.remove("hidden");
+    lottoGame.lottos = money;
+
+    LottoView.printLottoConunt(lottoGame.lottoCount);
+    LottoView.printLottos(lottoGame.lottos);
+
+    winContents.classList.remove("hidden");
   },
 };
 
