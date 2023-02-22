3f7a86a79e9bcbb158b2383e1f3576444cf34f26
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-22 23:55:04 +0900

    feat: 투입된 금액을 검사하는 기능 구현

--- src/view/containers/GameBox/gameBoxEvent.js
+++ src/view/containers/GameBox/gameBoxEvent.js
@@ -1,6 +1,7 @@
 // import LottoController from "../../../domain/LottoController";
 
 import { generateLottos } from "../../../domain/generateLottos";
+import LottoValidator from "../../../domain/LottoValidator";
 
 /* eslint-disable no-undef */
 const gameBoxEvent = () => {
@@ -8,9 +9,13 @@ const gameBoxEvent = () => {
   const btn = document.getElementById("purchase-button");
 
   btn.onclick = function () {
-    console.log(input.value);
-    console.log(generateLottos((input.value)));
-    input.value = '';
+    try {
+      LottoValidator.checkMoney(input.value);
+      console.log(generateLottos((input.value)));
+      input.value = '';
+    } catch (error) {
+      alert(error.message);
+    }
   };
 };
 
