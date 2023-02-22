d806ff66a5090d722b57c78fa288a00b21266aff
solo5star <solo_5@naver.com>
2023-02-23 02:18:03 +0900

    fix(web/component): `Custom Elements Registry` 등록 시 의존성이 없는 순부터 등록

--- src/view/web/component/index.js
+++ src/view/web/component/index.js
@@ -9,12 +9,13 @@ function toKebabCase(text) {
   return text.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
+// NOTE: 의존성의 순서에 맞춰 정렬할 것 (의존성이 없는 것부터 가장 위에)
 const components = [
+  LtTextInput,
   LtLottoList,
   LtLottoResult,
-  LtMoneyInput,
-  LtTextInput,
   LtTypography,
+  LtMoneyInput,
   LtWinningLottoInput,
 ];
 
