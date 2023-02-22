26cbc6e9936f871d9ee6b495004a4413be41fd3e
woo-jk <201702042@o.cnu.ac.kr>
2023-02-22 15:38:39 +0900

    feat: 로또 번호 검증, 당첨 통계 기능, 다시 시작하기 기능 구현(모든 기능 구현 완료)

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import LottoGame from '../domain/lottoGame/LottoGame';
 import { $, $$ } from './dom';
 class App {
   #lottoGame = new LottoGame();
-  #modalToggle = false;
 
   init() {
     this.bindEventListener();
@@ -46,17 +45,17 @@ class App {
       <div id="lotto-number-container" class="number-container">
         <div class="game-message">당첨 번호</div>
         <div id="lotto-number-input-list">
-          <input id="first-number" class="lotto-number-input" type="text" />
-          <input id="second-number" class="lotto-number-input" type="text" />
-          <input id="third-number" class="lotto-number-input" type="text" />
-          <input id="fourth-number" class="lotto-number-input" type="text" />
-          <input id="fifth-number" class="lotto-number-input" type="text" />
-          <input id="sixth-number" class="lotto-number-input" type="text" />
+          <input id="first-number" class="lotto-number-input winning-number" type="text" />
+          <input id="second-number" class="lotto-number-input winning-number" type="text" />
+          <input id="third-number" class="lotto-number-input winning-number" type="text" />
+          <input id="fourth-number" class="lotto-number-input winning-number" type="text" />
+          <input id="fifth-number" class="lotto-number-input winning-number" type="text" />
+          <input id="sixth-number" class="lotto-number-input winning-number" type="text" />
         </div>
       </div>
       <div id="bonus-number-container" class="number-container">
         <div class="game-message">보너스 번호</div>
-        <input id="bonus-number" class="lotto-number-input" type="text" />
+        <input id="bonus-number" class="lotto-number-input bonus-number" type="text" />
       </div>
     </div>`;
     $winningLottoContainer.innerHTML = winningLottoInputTemplate;
@@ -64,12 +63,49 @@ class App {
 
   showResultButton = () => {
     const $gameContainer = $('#game-container');
-    const $resultButton = $('#resultButton');
+    const $resultButton = $('#result-button');
     const resultButtonTemplate = `<button id='result-button' class='my-button'>결과 확인하기</button>`;
 
     if ($resultButton) $resultButton.remove();
     $gameContainer.insertAdjacentHTML('beforeend', resultButtonTemplate);
-    $('#result-button').addEventListener('click', this.showModal);
+    $('#result-button').addEventListener('click', this.handleClickResultButton);
+  };
+
+  handleClickResultButton = () => {
+    const lottoNumber = [...$$('.winning-number')]
+      .filter((input) => input.value !== '')
+      .map((input) => Number(input.value));
+    const bonusNumber = Number($('.bonus-number').value);
+    try {
+      console.log(lottoNumber, bonusNumber);
+      this.#lottoGame.generateWinningLotto(lottoNumber, bonusNumber);
+      this.renderRankResult();
+      this.showModal();
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  handleClickResetButton = () => {
+    const $lottoListContainer = $('#lotto-list-container');
+    const $winningLottoContainer = $('#winning-lotto-container');
+    const $resultButton = $('#result-button');
+
+    $lottoListContainer.textContent = '';
+    $winningLottoContainer.textContent = '';
+    $resultButton.remove();
+    this.closeModal();
+  };
+
+  renderRankResult = () => {
+    const winningRankResult = this.#lottoGame.getWinningRankResult();
+    const profitRate = Number(this.#lottoGame.getProfitRateOfPrize().toFixed(1)).toLocaleString();
+    $('#first-rank-count').innerHTML = `${winningRankResult[1]}개`;
+    $('#second-rank-count').innerHTML = `${winningRankResult[2]}개`;
+    $('#third-rank-count').innerHTML = `${winningRankResult[3]}개`;
+    $('#fourth-rank-count').innerHTML = `${winningRankResult[4]}개`;
+    $('#fifth-rank-count').innerHTML = `${winningRankResult[5]}개`;
+    $('#profit-message').innerHTML = `당신의 총 수익률은 ${profitRate}% 입니다.`;
   };
 
   showModal = () => {
@@ -84,6 +120,8 @@ class App {
     $('#purchase-button').addEventListener('click', this.handleClickPurchaseButton);
 
     $('#modal-close-button').addEventListener('click', this.closeModal);
+
+    $('#reset-button').addEventListener('click', this.handleClickResetButton);
   }
 }
 
