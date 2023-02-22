798f982400edcb983c77877873ab2ca231791d53
Q Kim <qoli0703@gmail.com>
2023-02-22 18:25:03 +0900

    refactor: 에러 메시지 변경

--- src/constant/messages.js
+++ src/constant/messages.js
@@ -4,11 +4,11 @@ const MESSAGE = {
   inputBonusNumber: '> 보너스 번호를 입력해 주세요.',
   inputRetry: '> 다시 시작하시겠습니까? (y/n)',
   errorMoneyRange: '[ERROR] 로또 구매 금액은 1000원 이상만 가능해요. 그런데 너무 큰 숫자는 제게 버겁습니다.',
-  errorPositiveInteger: '[ERROR] 양(+)의 십진수가 아니에요!',
+  errorPositiveInteger: '[ERROR] 자연수가 아니에요!',
   errorLottoCount: '[ERROR] 어머! 로또 숫자가 6개가 아니에요! ㅠㅠ',
   errorLottoDuplicated: '[ERROR] 숫자가 중복되었습니다.',
   errorLottoInRange: '[ERROR] 로또숫자는 1부터 45까지입니다.',
-  errorContent: '[ERROR] 쉼표(,) 뒤에 내용을 입력해주세요.',
+  errorContent: '[ERROR] 빈 칸 대신 숫자를 입력해주세요.',
   errorRetry: '[ERROR] y 또는 n만 입력 가능합니다.',
 };
 
