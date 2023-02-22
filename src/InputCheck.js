f5c080bbf4f32864a5b244f81c2e491c8d6512a2
NaveOWO <pianowell96@gmail.com>
2023-02-22 20:57:46 +0900

    feat : 에러던지는 로직 web/console 분기처리

--- src/InputCheck.js
+++ src/InputCheck.js
@@ -2,52 +2,85 @@ import ERROR_MESSAGE from "./constants/ErrorMessage";
 import Validators from "./Validators";
 
 const InputCheck = {
-  validateBuyMoney(buyMoney) {
+  validateBuyMoney(buyMoney, isWeb) {
     if (!Validators.isNumber(buyMoney)) {
-      throw new Error(ERROR_MESSAGE.INPUT_NUMBER);
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_NUMBER)
+        : this.throwError(ERROR_MESSAGE.INPUT_NUMBER);
     }
-    if (!Validators.isDevidedByThousand(buyMoney)) {
-      throw new Error(ERROR_MESSAGE.INPUT_NUMBER_DEVIDED_BY_THOUSAND);
+    if (!Validators.isDevidedByThousand(buyMoney, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_NUMBER_DEVIDED_BY_THOUSAND)
+        : this.throwError(ERROR_MESSAGE.INPUT_NUMBER_DEVIDED_BY_THOUSAND);
     }
-    if (!Validators.isPositiveInteger(buyMoney)) {
-      throw new Error(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_MONEY);
+    if (!Validators.isPositiveInteger(buyMoney, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_MONEY)
+        : this.throwError(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_MONEY);
     }
   },
 
-  validateWinningNumbers(winningLotto) {
-    if (!Validators.isCorrectLength(winningLotto)) {
-      throw new Error(ERROR_MESSAGE.INPUT_SIX_NUMBERS);
+  validateWinningNumbers(winningLotto, isWeb) {
+    if (Validators.isDuplicatedNumbers(winningLotto, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_NOT_DUPLICATED_EACH_NUMBER)
+        : this.throwError(ERROR_MESSAGE.INPUT_NOT_DUPLICATED_EACH_NUMBER);
+    }
+    if (!Validators.isCorrectLength(winningLotto, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_SIX_NUMBERS)
+        : this.throwError(ERROR_MESSAGE.INPUT_SIX_NUMBERS);
     }
     for (let i = 0; i < winningLotto.length; i++) {
-      this.checkNumber(winningLotto[i]);
+      this.checkNumber(winningLotto[i], isWeb);
     }
   },
 
-  checkNumber(eachNumber) {
-    if (!Validators.isNumber(eachNumber)) {
-      throw new Error(ERROR_MESSAGE.INPUT_NUMBER);
+  checkNumber(eachNumber, isWeb) {
+    if (!Validators.isNumber(eachNumber, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_NUMBER)
+        : this.throwError(ERROR_MESSAGE.INPUT_NUMBER);
     }
-    if (!Validators.isCorrectRange(eachNumber)) {
-      throw new Error(ERROR_MESSAGE.INPUT_CORRECT_RANGE_NUMBER);
+    if (!Validators.isCorrectRange(eachNumber, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_CORRECT_RANGE_NUMBER)
+        : this.throwError(ERROR_MESSAGE.INPUT_CORRECT_RANGE_NUMBER);
     }
-    if (!Validators.isPositiveInteger(eachNumber)) {
-      throw new Error(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_LOTTO);
+    if (!Validators.isPositiveInteger(eachNumber, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_LOTTO)
+        : this.throwError(ERROR_MESSAGE.INPUT_POSITIVE_INTEGER_LOTTO);
     }
   },
 
-  validateBonusNumber(bonusNumber, winningLotto) {
-    if (Validators.hasBonusNumber(bonusNumber, winningLotto)) {
-      throw new Error(ERROR_MESSAGE.INPUT_NOT_DUPLICATED_NUMBER);
+  validateBonusNumber(bonusNumber, winningLotto, isWeb) {
+    if (Validators.hasBonusNumber(bonusNumber, winningLotto, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_NOT_DUPLICATED_NUMBER)
+        : this.throwError(ERROR_MESSAGE.INPUT_NOT_DUPLICATED_NUMBER);
     }
   },
 
-  validateRetryInput(retryInput) {
-    if (!Validators.isString(retryInput)) {
-      throw new Error(ERROR_MESSAGE.INPUT_STRING);
+  validateRetryInput(retryInput, isWeb) {
+    if (!Validators.isString(retryInput, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_STRING)
+        : this.throwError(ERROR_MESSAGE.INPUT_STRING);
     }
-    if (!Validators.isCorrectRetryInput(retryInput)) {
-      throw new Error(ERROR_MESSAGE.INPUT_CORRECT_RETRY);
+    if (!Validators.isCorrectRetryInput(retryInput, isWeb)) {
+      isWeb
+        ? this.alertError(ERROR_MESSAGE.INPUT_CORRECT_RETRY)
+        : this.throwError(ERROR_MESSAGE.INPUT_CORRECT_RETRY);
     }
+  },
+
+  throwError(errorMessage) {
+    throw new Error(errorMessage);
+  },
+
+  alertError(errorMessage) {
+    alert(errorMessage);
   },
 };
 
