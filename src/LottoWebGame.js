b6f33302748f20716af9a4fc2d48d01fd95bdeb7
nlom0218 <nlom0218@naver.com>
2023-02-22 13:41:34 +0900

    feat: 구입 금액 유효성 검사 성공 후 로또 생성

--- src/LottoWebGame.js
+++ src/LottoWebGame.js
@@ -1,5 +1,8 @@
+import { LOTTO } from './constants';
 import { $ } from './dom/dom';
 import lottoGameValidator from './domain/lottoGameValidator';
+import Lotto from './domain/models/Lotto';
+import generateRandomNumber from './utils/generateRandomNumber';
 
 const $purchaseInput = $('#purchase-amount-form input[type=text]');
 
@@ -9,12 +12,33 @@ const LottoWebGame = function () {
     initAddEventListener();
   };
 
-  const submitPurchaseAmount = async (event) => {
+  const buyLottos = (purchaseAmount) => {
+    new Array(purchaseAmount / LOTTO.price).fill().forEach(() => {
+      this.lottos.push(publishLotto());
+    });
+  };
+
+  const publishLotto = () => {
+    return new Lotto(generateLottoNumbers());
+  };
+
+  const generateLottoNumbers = () => {
+    const lottoNumbers = [];
+    while (lottoNumbers.length < 6) {
+      const number = generateRandomNumber(LOTTO.minNumber, LOTTO.maxNumber);
+      if (!lottoNumbers.includes(number)) lottoNumbers.push(number);
+    }
+
+    return lottoNumbers.sort((a, b) => a - b);
+  };
+
+  const submitPurchaseAmount = (event) => {
     event.preventDefault();
 
     const purchaseAmount = $purchaseInput.value;
     try {
       lottoGameValidator.checkPruchaseAmount(purchaseAmount);
+      buyLottos(purchaseAmount);
     } catch (error) {
       window.alert(error);
       $purchaseInput.value = '';
