37a7f5ae7934146a8f2cd709662920dc44261bd5
yeopto <gunyeop530@gmail.com>
2023-02-22 17:13:24 +0900

    feat: 구입한 로또 목록을 보여준다

--- src/js/controller/step2/LottoController.js
+++ src/js/controller/step2/LottoController.js
@@ -2,12 +2,15 @@ import LottoValidator from '../../model/LottoValidator';
 import PurchaseMoneyInputView from '../../view/step2/PurchaseMoneyInputView';
 import { convertToNum } from '../../utils/common';
 import LottoMachine from '../../model/LottoMachine';
+import PurchaseLottoView from '../../view/step2/PurchaseLottoView';
+
 class LottoController {
   #lottoMachine;
 
   constructor() {
     this.view = {
       purchaseMoneyInputView: new PurchaseMoneyInputView(),
+      purchaseLottoView: new PurchaseLottoView(),
     };
     this.start();
   }
@@ -20,11 +23,16 @@ class LottoController {
         LottoValidator.validateMoneyInput(convertToNum(money));
         this.#lottoMachine = new LottoMachine(money);
         this.view.purchaseMoneyInputView.reset();
+        this.showPurchaseLotto(this.#lottoMachine.lottos);
       } catch (error) {
         this.view.purchaseMoneyInputView.reset();
         alert(error);
       }
     });
+  }
+
+  showPurchaseLotto(lottos) {
+    this.view.purchaseLottoView.rendering(lottos);
   }
 }
 
---
+++ src/js/view/step2/PurchaseLottoView.js
@@ -0,0 +1,35 @@
+const LOTTO_IMAGE = `<span class="purchase-lotto-image">🎟️</span>`;
+
+const getTemplate = (count, lottoListTemplate) => `
+  <div id="purchase-lotto-box">
+    <div id="purchase-lotto-item-message">총 ${count}개를 구매하였습니다.</div>
+    ${lottoListTemplate}
+  </div>
+`;
+
+const getLottoItemTemplate = lotto => `
+  <div class="purchase-lotto-item">
+    ${LOTTO_IMAGE}
+    <div class="purchase-lotto-number">${lotto.join(', ')}</div>
+  </div>
+`;
+
+const getLottoListTemplate = lottos =>
+  lottos.reduce((result, lotto) => result + getLottoItemTemplate(lotto.lottoNum), '');
+
+class PurchaseLottoView {
+  constructor() {
+    this.container = document.getElementById('purchase-lotto-container');
+  }
+
+  rendering(lottos) {
+    this.reset();
+    this.container.insertAdjacentHTML('beforeend', getTemplate(lottos.length, getLottoListTemplate(lottos)));
+  }
+
+  reset() {
+    this.container.replaceChildren();
+  }
+}
+
+export default PurchaseLottoView;
