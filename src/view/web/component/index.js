ab27d8bdda9fb18d67ecc556f50dd70256d560bf
solo5star <solo_5@naver.com>
2023-02-22 18:01:47 +0900

    feat(web/component): Custom Element를 Registry에 등록 및 export

---
+++ src/view/web/component/index.js
@@ -0,0 +1,30 @@
+import LtLottoList from './LtLottoList';
+import LtLottoResult from './LtLottoResult';
+import LtMoneyInput from './LtMoneyInput';
+import LtTextInput from './LtTextInput';
+import LtTypography from './LtTypography';
+import LtWinningLottoInput from './LtWinningLottoInput';
+
+import '@fontsource/roboto';
+import './dialog.css';
+import './form.css';
+import './index.css';
+
+function toKebabCase(text) {
+  return text.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+const components = [
+  LtLottoList,
+  LtLottoResult,
+  LtMoneyInput,
+  LtTextInput,
+  LtTypography,
+  LtWinningLottoInput,
+];
+
+components.forEach((component) => {
+  customElements.define(toKebabCase(component.name), component);
+});
+
+export { LtLottoList, LtLottoResult, LtMoneyInput, LtTextInput, LtTypography, LtWinningLottoInput };
