79c22389d4f0f8447d523b57ed6df4d02770e4cc
Leejin-Yang <ejin1996@gmail.com>
2023-02-22 19:34:27 +0900

    feat: 구입 로또 목록 view 구현

---
+++ src/view/web/LottoListView.js
@@ -0,0 +1,25 @@
+const LottoListView = {
+  render(container, lottos) {
+    container.innerHTML = LottoListView.template(lottos);
+  },
+
+  template(lottos) {
+    return LottoListView.toQuantityTemplate(lottos) + LottoListView.toLottoListTemplate(lottos);
+  },
+
+  toQuantityTemplate(lottos) {
+    return `<p>총 ${lottos.length}개를 구매하였습니다.</p>`;
+  },
+
+  toLottoListTemplate(lottos) {
+    return `<ul class="lotto-list">${lottos
+      .map((lotto) => LottoListView.toLottoItemTemplate(lotto))
+      .join('')}</ul>`;
+  },
+
+  toLottoItemTemplate(lotto) {
+    return `<li class="lotto-item">${lotto.join(', ')}</li>`;
+  },
+};
+
+export default LottoListView;
