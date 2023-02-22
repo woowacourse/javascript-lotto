833fda5f8f88451339580dd386ad7bfb250fa8fc
hafnium1923 <hafnium1923@gmail.com>
2023-02-22 10:50:04 +0900

    refactor: 컨트롤러 이름 변경

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,7 +1,7 @@
 require("./css/index.css");
-const Controller = require("./controller/Controller");
+const ViewController = require("./controller/ViewController");
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
-Controller.play();
+ViewController.play();
