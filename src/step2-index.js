3925d1fcadbedb44eb795e0d14df835b159aadd7
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-22 14:07:55 +0900

    feat(index): App 불러오고 실행하는 기능 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,7 @@
 import './css/style.css';
-import './js/index.js';
 import './images/error_icon.png';
 import './images/lotto_icon.png';
+import LottoApp from './LottoApp.js';
+
+const lottoApp = new LottoApp('web');
+lottoApp.play();
