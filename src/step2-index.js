f0d0e87b5173a6743c3e846004fa934a9e80bf57
Suyoung <jsy8492@gmail.com>
2023-02-22 14:29:00 +0900

    refactor: 파일 제거에 따른 진입점이 되는 파일 내용 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,9 +2,10 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
-import LottoGame from './LottoGame';
+import App from './App';
 
-import './css/index.css';
 import { $ } from './utils/dom';
 
-new LottoGame($('#app'));
+import './css/index.css';
+
+new App($('#app'));
