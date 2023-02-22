1238162a2818713d3d860391a0e64978adf96046
wzrabbit <curious.wzrabbit@gmail.com>
2023-02-22 12:53:25 +0900

    style: 주석 제거

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,10 +1,4 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
-
 import './css/style.css';
 import './js/index.js';
-
 import './images/error_icon.png';
 import './images/lotto_icon.png';
