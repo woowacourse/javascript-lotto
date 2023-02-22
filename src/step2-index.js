a7aa87d8a4c9be7f250f5651ddceb501b2565fe4
hae-on <solbi2004@naver.com>
2023-02-22 16:08:36 +0900

    refactor: 실행 방식 변경

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,6 @@
 import LottoDomSimulator from './controller/LottoDomSimulator.js';
+import './css/reset.css';
+import './css/style.css';
 
 const lottoDomSimulator = new LottoDomSimulator();
 lottoDomSimulator.play();
