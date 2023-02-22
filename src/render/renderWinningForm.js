3e9e5e92512147ab047e068623527e6411cde661
nlom0218 <nlom0218@naver.com>
2023-02-22 18:16:56 +0900

    feat: 당첨 번호 보너스 번호 입력 폼 렌더링 화면

--- src/render/renderWinningForm.js
+++ src/render/renderWinningForm.js
@@ -3,20 +3,20 @@ import { $ } from '../dom/dom';
 
 const renderWinningForm = () => {
   $('#winning-lotto-from').innerHTML = `
-    <div>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
-    <div>
-        <div>
-            <div>당첨 번호</div>
-            <div>${new Array(LOTTO.numbersLength)
+    <div id="winning-lotto-from-title">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
+    <div id="winning-lotto-from-layout">
+        <div id="winnging-numbers-input">
+            <div class="winning-form-input-title">당첨 번호</div>
+            <div id="winning-numbers-inputs">${new Array(LOTTO.numbersLength)
               .fill()
               .map(() => {
                 return `<input type="text" name="winning-number" />`;
               })
               .join('')}
-            <div>
+            </div>
         </div>
-        <div>
-            <div>보너스 번호</div>
+        <div id="bonusNumber-input">
+            <div class="winning-form-input-title">보너스 번호</div>
             <input type="text" name="bonus-number" />
         </div>
     </div>
