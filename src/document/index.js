8796b6cf5d7e208365525a79768de9eb74a7a4a9
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-23 17:48:59 +0900

    ✨ feat(document): document 관리 폴더 생성

---
+++ src/document/index.js
@@ -0,0 +1,31 @@
+const moneyInput = document.getElementById('moneyInput');
+const bonusNumberInput = document.getElementById('bonusNumberInput');
+
+const purchaseButton = document.getElementById('purchaseButton');
+const confirmButton = document.getElementById('confirmButton');
+const retryButton = document.getElementById('retryButton');
+
+const winningNumbersGroup = document.getElementById('winningNumbersGroup');
+const lottoContent = document.getElementById('lottoContent');
+const result = document.getElementById('result');
+const closeButton = document.getElementById('closeButton');
+const count = document.getElementById('count');
+const rate = document.getElementById('rate');
+const resultListContainer = document.getElementById('resultListContainer');
+const lottoList = document.getElementById('lottoList');
+
+module.exports = {
+  moneyInput,
+  bonusNumberInput,
+  purchaseButton,
+  confirmButton,
+  retryButton,
+  winningNumbersGroup,
+  lottoContent,
+  result,
+  closeButton,
+  count,
+  rate,
+  resultListContainer,
+  lottoList,
+};
