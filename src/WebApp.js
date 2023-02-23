513c2bfacf968dc037d48be27e65d53ed6760046
ukkodeveloper <ukkodeveloper@gmail.com>
2023-02-23 11:27:20 +0900

    feat: 당첨번호 입력 ui 출력

--- src/WebApp.js
+++ src/WebApp.js
@@ -59,9 +59,56 @@ const renderLottoList = (lottoList) => {
   appendBySequence($ticketSection, $('.app-box'));
 };
 
-// const renderWinningForm = () => {
+const renderWinningForm = () => {
+  // section
+  const $winningSection = createDomWith('section')({ id: 'winning-lotto' });
 
-// };
+  // p
+  const $message = createDomWith('p')({
+    class: 'message',
+    innerText: '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.',
+  });
+
+  // form
+  const $form = document.createElement('form');
+  // div
+  const $spanContainer = createDomWith('div')({ id: 'number-message', class: 'message' });
+
+  // span
+  const $winningSpan = createDomWith('span')({ innerText: '당첨 번호' });
+  const $bonusSpan = createDomWith('span')({ innerText: '보너스 번호' });
+  $spanContainer.append($winningSpan, $bonusSpan);
+
+  // div
+  const $lottoContainer = createDomWith('div')({ id: 'number-box' });
+
+  // div
+  const $lottoWinningContainer = createDomWith('div')({ id: 'winning-numbers' });
+
+  // inputS
+  appendDomByList($lottoWinningContainer)(Array.from({ length: 6 }))((elem) => {
+    return createDomWith('input')({ type: 'text', minlength: '1', maxlength: '2' });
+  });
+
+  // div
+  const $lottoBonusContainer = createDomWith('div')({ id: 'bonus-number' });
+
+  // input
+  const $bonusInput = createDomWith('input')({ type: 'text' });
+  $lottoBonusContainer.appendChild($bonusInput);
+
+  // submitButton
+  const $submitButton = createDomWith('button')({
+    id: 'result-button',
+    type: 'submit',
+    innerText: '결과 확인하기',
+  });
+
+  $lottoContainer.append($lottoWinningContainer, $lottoBonusContainer);
+  $form.append($spanContainer, $lottoContainer, $submitButton);
+  $winningSection.append($message, $form);
+  $('.app-box').append($winningSection);
+};
 
 // purchasingButton
 const purchasingButton = $('#purchasing-button');
@@ -71,6 +118,7 @@ const purchasingHandler = (event) => {
     const money = Number($('#money').value);
     const lottoList = lottoGame.setLottos(money).getLottos();
     renderLottoList(lottoList);
+    renderWinningForm();
     purchasingButton.disabled = 'disabled';
   } catch (error) {
     console.dir(error);
