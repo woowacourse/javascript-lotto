fa179cbc9c0bd84c58b1181c6a9d67fbb4c0fe7e
Dain Lee <feb.dain@gmail.com>
2023-02-23 23:48:27 +0900

    feat: 사용자에게 로또 게임 결과를 보여주는 기능 추가

---
+++ src/ui/lottoResult.js
@@ -0,0 +1,28 @@
+import { getRateOfReturn, getTotalPrize } from "../domain/calculator";
+
+const fifthPlace = document.querySelector(".fifth");
+const fourthPlace = document.querySelector(".fourth");
+const thirdPlace = document.querySelector(".third");
+const secondPlace = document.querySelector(".second");
+const firstPlace = document.querySelector(".first");
+
+const rateOfReturn = document.querySelector(".rate-of-return > span");
+
+export const printLottoResult = (placesOfLottoTickets, purchaseAmount) => {
+  printWinningTicketCount(placesOfLottoTickets);
+  printRateOfReturn(placesOfLottoTickets, purchaseAmount);
+};
+
+export const printWinningTicketCount = (placesOfLottoTickets) => {
+  fifthPlace.innerHTML = `${placesOfLottoTickets.filter((num) => num === 5).length}`;
+  fourthPlace.innerHTML = `${placesOfLottoTickets.filter((num) => num === 4).length}`;
+  thirdPlace.innerHTML = `${placesOfLottoTickets.filter((num) => num === 3).length}`;
+  secondPlace.innerHTML = `${placesOfLottoTickets.filter((num) => num === 2).length}`;
+  firstPlace.innerHTML = `${placesOfLottoTickets.filter((num) => num === 1).length}`;
+};
+
+export const printRateOfReturn = (placesOfLottoTickets, purchaseAmount) => {
+  const percent = getRateOfReturn(getTotalPrize(placesOfLottoTickets), purchaseAmount).toFixed(1);
+
+  rateOfReturn.innerHTML = `${String(percent).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+};
