aa9596c5b179a28cd6d18e63048d380dcb68a1d4
NaveOWO <pianowell96@gmail.com>
2023-02-23 14:08:35 +0900

    feat : 메서드 추가

--- src/view/Element.js
+++ src/view/Element.js
@@ -1,33 +1,43 @@
 import Utils from "../util/Utils";
 import LOTTO_SCORE from "../constants/LottoBoard";
+import VIEW from "../constants/View";
 
 const Element = {
-  createBuyLottos(lotto) {
+  createBuyLottos(lotto, buyAmount, lottoAmount) {
     const lottoList = Utils.$(".lotto__numbers-list");
     const buyLottoTemplate = Utils.$("#lotto-tem");
     const buyLottoClone = document.importNode(buyLottoTemplate.content, true);
 
-    buyLottoClone.querySelector(".lotto__lotto-icon").innerText = "🎟";
-    buyLottoClone.querySelector(".lotto__numbers").innerText =
-      lotto.lottoNumbers;
+    this.createInnerText(
+      buyLottoClone.querySelector(".lotto__lotto-icon"),
+      "🎟"
+    );
+    this.createInnerText(
+      buyLottoClone.querySelector(".lotto__numbers"),
+      lotto.lottoNumbers
+    );
+    this.createInnerText(
+      buyAmount,
+      `총 ${lottoAmount}${VIEW.PRINT_LOTTO_AMOUNT}`
+    );
     lottoList.appendChild(buyLottoClone);
   },
 
-  createResults(lottoRanking) {
+  createResults(lottoRanking, benefitRate) {
     this.createResultContainer();
     this.createResultTitle();
     for (const matchingCount in lottoRanking) {
       this.createResultContents(lottoRanking, matchingCount);
     }
-    this.createRetryButton();
+    this.createRetryButton(benefitRate);
   },
 
   createResultContainer() {
     const resultBg = document.createElement("section");
     const resultContainer = document.createElement("div");
 
-    resultBg.classList.add("result-background");
-    resultContainer.classList.add("result-container");
+    this.addClassList(resultBg, "result-background");
+    this.addClassList(resultContainer, "result-container");
     Utils.$("#app").appendChild(resultBg);
     resultBg.appendChild(resultContainer);
   },
@@ -43,24 +53,42 @@ const Element = {
     const contentsTemplate = Utils.$("#result");
     const contentsClone = document.importNode(contentsTemplate.content, true);
 
-    contentsClone.querySelector(".result__matching-count").innerText =
-      matchingCount;
-    contentsClone.querySelector(".result__price").innerText =
-      LOTTO_SCORE.moneyBoard[matchingCount];
-    contentsClone.querySelector(".result__matching-lotto-count").innerText =
-      lottoRanking[matchingCount];
+    this.createInnerText(
+      contentsClone.querySelector(".result__matching-count"),
+      LOTTO_SCORE.uiBoard[matchingCount]
+    );
+    this.createInnerText(
+      contentsClone.querySelector(".result__price"),
+      LOTTO_SCORE.moneyBoard[matchingCount]
+    );
+    this.createInnerText(
+      contentsClone.querySelector(".result__matching-lotto-count"),
+      `${lottoRanking[matchingCount]}개`
+    );
     document.querySelector(".result-container").appendChild(contentsClone);
   },
 
-  createRetryButton() {
+  createRetryButton(benefitRate) {
     const retryTemplate = document.querySelector("#result-footer");
     const retryClone = document.importNode(retryTemplate.content, true);
 
     document.querySelector(".result-container").appendChild(retryClone);
+    this.createInnerText(
+      Utils.$(".result__comment"),
+      `${VIEW.PRINT_BENEFIT_RATE_START} ${benefitRate}${VIEW.PRINT_BENEFIT_RATE_END}`
+    );
   },
 
   createInnerText(element, text) {
     element.innerText = text;
+  },
+
+  removeClassList(element, className) {
+    element.classList.remove(className);
+  },
+
+  addClassList(element, className) {
+    element.classList.add(className);
   },
 };
 
