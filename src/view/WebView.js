a8b09354b4d91072636d21462a1a055fb1a46e34
nangkyeonglim <nangkyeonglim@gmail.com>
2023-02-23 23:16:45 +0900

    feat: 구매한 로또 티켓을 출력하는 기능 구현

--- src/view/WebView.js
+++ src/view/WebView.js
@@ -1,8 +1,29 @@
 import { $ } from '../util/querySelector';
 
+const purchasedLottoTemplate = (lotto) => {
+  return `
+  <li>
+    <span class="lotto-emoji">🎟️</span> <span class="lotto-numbers">${lotto.join(', ')}</span>
+  </li>
+  `;
+};
+
 const WebView = {
   printLottoTicketCount(ticketCount) {
     $('#purchased-lotto-count').innerHTML = ticketCount;
+  },
+
+  printLottoTickets(lottoTickets) {
+    while ($('#purchased-lotto-container').firstChild) {
+      $('#purchased-lotto-container').removeChild($('#purchased-lotto-container').firstChild);
+    }
+
+    lottoTickets.forEach((lottoTicket) =>
+      $('#purchased-lotto-container').insertAdjacentHTML(
+        'beforeend',
+        purchasedLottoTemplate(lottoTicket)
+      )
+    );
   },
 };
 
