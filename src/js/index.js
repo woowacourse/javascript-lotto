1a107a42c382a5f380386d8c273178d1011542b5
woo-jk <201702042@o.cnu.ac.kr>
2023-02-23 11:35:35 +0900

    refactor: html 및 dom 조작 로직 변경

--- src/js/index.js
+++ src/js/index.js
@@ -12,64 +12,56 @@ class App {
     const money = $moneyInput.value;
     try {
       this.#lottoGame.purchaseLottos(money);
+      this.showLottoTemplate();
+      this.setLottoCount();
       this.showLottoList();
-      this.showWinningLottoInput();
-      this.showResultButton();
+      $('#result-button').addEventListener('click', this.handleClickResultButton);
     } catch (error) {
+      console.error(error);
       alert(error.message);
     }
     $moneyInput.value = '';
   };
 
   showLottoList = () => {
-    const $lottoListContainer = $('#lotto-list-container');
+    const $lottoList = $('#lotto-list');
     const lottos = this.#lottoGame.getLottos();
-    const lottoListTemplate = `<div class="game-message">총 ${
-      lottos.length
-    }개를 구매하셨습니다.</div>
-    <ul id="lotto-list">${lottos
+    const lottoListTemplate = lottos
       .map(
         (lotto) => `<li>
     <span class="ticket-emoji">🎟️</span
     ><span class="lotto-number">${lotto.getLottoNumber().join(', ')}</span>
   </li>`
       )
-      .join('')}</ul>`;
-    $lottoListContainer.innerHTML = lottoListTemplate;
+      .join('');
+    $lottoList.innerHTML = lottoListTemplate;
   };
 
-  showWinningLottoInput = () => {
-    const $winningLottoContainer = $('#winning-lotto-container');
-    const winningLottoInputTemplate = `<div class="game-message">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
-    <div id="number-input-container">
-      <div id="lotto-number-container" class="number-container">
-        <div class="game-message">당첨 번호</div>
-        <div id="lotto-number-input-list">
-          <input id="first-number" class="lotto-number-input winning-number" type="text" />
-          <input id="second-number" class="lotto-number-input winning-number" type="text" />
-          <input id="third-number" class="lotto-number-input winning-number" type="text" />
-          <input id="fourth-number" class="lotto-number-input winning-number" type="text" />
-          <input id="fifth-number" class="lotto-number-input winning-number" type="text" />
-          <input id="sixth-number" class="lotto-number-input winning-number" type="text" />
-        </div>
-      </div>
-      <div id="bonus-number-container" class="number-container">
-        <div class="game-message">보너스 번호</div>
-        <input id="bonus-number" class="lotto-number-input bonus-number" type="text" />
-      </div>
-    </div>`;
-    $winningLottoContainer.innerHTML = winningLottoInputTemplate;
+  setLottoCount() {
+    const lottoCount = this.#lottoGame.getLottos().length;
+    $('#lotto-count').innerText = lottoCount;
+  }
+
+  showLottoTemplate = () => {
+    if ($('#lotto-list')) return;
+
+    const $target = $('#game-container');
+    const $template = $('#lotto-template');
+
+    const copiedTemplate = document.importNode($template.content, true);
+
+    $target.appendChild(copiedTemplate);
   };
 
-  showResultButton = () => {
-    const $gameContainer = $('#game-container');
-    const $resultButton = $('#result-button');
-    const resultButtonTemplate = `<button id='result-button' class='my-button'>결과 확인하기</button>`;
+  // showResultButton = () => {
+  //   const $gameContainer = $('#game-container');
+  //   const $resultButton = $('#result-button');
+  //   const resultButtonTemplate = `<button id='result-button' class='my-button'>결과 확인하기</button>`;
 
-    if ($resultButton) $resultButton.remove();
-    $gameContainer.insertAdjacentHTML('beforeend', resultButtonTemplate);
-    $('#result-button').addEventListener('click', this.handleClickResultButton);
-  };
+  //   if ($resultButton) $resultButton.remove();
+  //   $gameContainer.insertAdjacentHTML('beforeend', resultButtonTemplate);
+  //   $('#result-button').addEventListener('click', this.handleClickResultButton);
+  // };
 
   handleClickResultButton = () => {
     const lottoNumber = [...$$('.winning-number')]
@@ -91,8 +83,8 @@ class App {
     const $winningLottoContainer = $('#winning-lotto-container');
     const $resultButton = $('#result-button');
 
-    $lottoListContainer.textContent = '';
-    $winningLottoContainer.textContent = '';
+    $lottoListContainer.remove();
+    $winningLottoContainer.remove();
     $resultButton.remove();
     this.closeModal();
   };
@@ -100,12 +92,12 @@ class App {
   renderRankResult = () => {
     const winningRankResult = this.#lottoGame.getWinningRankResult();
     const profitRate = Number(this.#lottoGame.getProfitRateOfPrize().toFixed(1)).toLocaleString();
-    $('#first-rank-count').innerHTML = `${winningRankResult[1]}개`;
-    $('#second-rank-count').innerHTML = `${winningRankResult[2]}개`;
-    $('#third-rank-count').innerHTML = `${winningRankResult[3]}개`;
-    $('#fourth-rank-count').innerHTML = `${winningRankResult[4]}개`;
-    $('#fifth-rank-count').innerHTML = `${winningRankResult[5]}개`;
-    $('#profit-message').innerHTML = `당신의 총 수익률은 ${profitRate}% 입니다.`;
+    $('#first-rank-count').innerText = winningRankResult[1];
+    $('#second-rank-count').innerText = winningRankResult[2];
+    $('#third-rank-count').innerText = winningRankResult[3];
+    $('#fourth-rank-count').innerText = winningRankResult[4];
+    $('#fifth-rank-count').innerText = winningRankResult[5];
+    $('#profit-rate').innerText = profitRate;
   };
 
   showModal = () => {
