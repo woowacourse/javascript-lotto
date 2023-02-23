80bcdbb746be813017c18d21465e211c5f44c08d
sh981013s <sh981013s@gmail.com>
2023-02-23 14:49:20 +0900

    feat(domList): DOM 리스트 상수화

---
+++ src/js/view/stepTwo/domList.js
@@ -0,0 +1,10 @@
+import { $ } from '@lotto/utils/selector';
+
+const domList = {
+  buyBtn: $('.buy-btn'),
+  moneyInput: $('.money-input'),
+  moneyInputErrorText: $('.money-input-error-text'),
+  mainContainer: $('.main-container'),
+};
+
+export default domList;
