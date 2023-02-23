5e942f7f6af8ccdf6f01198621f452c591897768
Jungwoo <evencoding@gmail.com>
2023-02-23 12:31:08 +0900

    refactor: Dom style의 visibility를 변환하는 로직을 함수로 분리

--- src/step2-index.js
+++ src/step2-index.js
@@ -5,47 +5,54 @@ import '../modal.css';
 import validator from './domain/validator';
 import LottoGame from './domain/LottoGame';
 
-const budgetForm = document.querySelector('.budget_form');
-const budgetInput = document.querySelector('.budget_input');
-const budgetError = document.querySelector('.budget_error');
+const $budgetForm = document.querySelector('.budget_form');
+const $budgetInput = document.querySelector('.budget_input');
+const $budgetError = document.querySelector('.budget_error');
 
-const step2 = document.querySelector('#step2');
+const $step2 = document.querySelector('#step2');
 
-const lottoCount = document.querySelector('.lotto_count');
-const lottoListBox = document.querySelector('.lotto_list_box');
+const $lottoCount = document.querySelector('.lotto_count');
+const $lottoList = document.querySelector('.lotto_list_box');
 
-const lottoNumberForm = document.querySelector('.lotto_number_form');
-const winningNumberInput = document.querySelectorAll('.winning_number');
-const bonusNumberInput = document.querySelector('.bonus_number');
-const numberError = document.querySelector('.number_error');
+const $lottoNumberForm = document.querySelector('.lotto_number_form');
+const $winningNumberInput = document.querySelectorAll('.winning_number');
+const $bonusNumberInput = document.querySelector('.bonus_number');
+const $numberError = document.querySelector('.number_error');
 
-const modal = document.querySelector('#modal');
-const winningCounts = document.querySelectorAll('.winning_count');
-const profitRateDiv = document.querySelector('.profit_rate');
-const modalCloseBtn = document.querySelector('.modal_close');
-const modalBackground = document.querySelector('.modal_background');
+const $modal = document.querySelector('#modal');
+const $winningCounts = document.querySelectorAll('.winning_count');
+const $profitRate = document.querySelector('.profit_rate');
+const $modalCloseButton = document.querySelector('.modal_close');
+const $modalBackground = document.querySelector('.modal_background');
 
-const retryBtn = document.querySelector('.retry_btn');
+const $retryButton = document.querySelector('.retry_btn');
 
 let lottoGame;
+
+const convertVisibilityToHidden = (...doms) => {
+  return [...doms].forEach((dom) => (dom.style.visibility = 'hidden'));
+};
+const convertVisibilityToVisible = (...doms) => {
+  return [...doms].forEach((dom) => (dom.style.visibility = 'visible'));
+};
 
 const displayModal = (winningNumbers, bonusNumber) => {
   const winningStatus = [...lottoGame.getWinningStatus(winningNumbers, bonusNumber)].reverse();
   const profitRate = lottoGame.getProfitRate().toFixed(2);
-  [...winningCounts].forEach((winningCount, index) => {
+  [...$winningCounts].forEach((winningCount, index) => {
     winningCount.innerText = winningStatus[index];
   });
-  profitRateDiv.innerText = `당신의 총 수익률은 ${profitRate}%입니다.`;
+  $profitRate.innerText = `당신의 총 수익률은 ${profitRate}%입니다.`;
 };
 
 const displayBudgetError = (message) => {
-  budgetError.innerText = message;
-  budgetError.style.visibility = 'visible';
+  $budgetError.innerText = message;
+  convertVisibilityToVisible($budgetError);
 };
 
-const displayNumberError = (message) => {
-  numberError.innerText = message;
-  numberError.style.visibility = 'visible';
+const displayLottoNumberError = (message) => {
+  $numberError.innerText = message;
+  convertVisibilityToVisible($numberError);
 };
 
 const displayBoughtLottos = (lottos) => {
@@ -56,7 +63,7 @@ const displayBoughtLottos = (lottos) => {
       <span>${lotto.join(', ')}</span>
     </div>`);
   }, '');
-  lottoListBox.innerHTML = lottoDivs;
+  $lottoList.innerHTML = lottoDivs;
 };
 
 const onSubmitBudgetForm = (event) => {
@@ -64,55 +71,46 @@ const onSubmitBudgetForm = (event) => {
   const budget = event.target[0].value;
   try {
     validator.validateBudget(budget);
-    budgetError.innerText = '';
+    $budgetError.innerText = '';
   } catch ({ message }) {
     return displayBudgetError(message);
   }
 
-  lottoCount.innerText = `총 ${budget / 1000}개를 구매하였습니다.`;
-  step2.style.visibility = 'visible';
+  $lottoCount.innerText = `총 ${budget / 1000}개를 구매하였습니다.`;
+  convertVisibilityToVisible($step2);
   lottoGame = new LottoGame(budget);
   displayBoughtLottos(lottoGame.getBoughtLottos());
 };
 
 const onSubmitLottoNumberForm = (event) => {
   event.preventDefault();
-  const winningNumbers = [...winningNumberInput].map((input) => Number(input.value));
-  const bonusNumber = bonusNumberInput.value;
+  const winningNumbers = [...$winningNumberInput].map((input) => Number(input.value));
+  const bonusNumber = $bonusNumberInput.value;
   try {
     validator.validateWinningNumber(winningNumbers.join(','));
     validator.validateBonusNumber(bonusNumber);
-    numberError.style.visibility = 'hidden';
+    convertVisibilityToHidden($numberError);
   } catch ({ message }) {
-    return displayNumberError(message);
+    return displayLottoNumberError(message);
   }
 
   displayModal(winningNumbers, bonusNumber);
-  modal.style.visibility = 'visible';
+  convertVisibilityToVisible($modal);
 };
 
-const onClickModalCloseBtn = () => {
-  modal.style.visibility = 'hidden';
-};
-
-const onClickModalBackGround = () => {
-  modal.style.visibility = 'hidden';
-};
-
-const onClickRetryBtn = () => {
-  budgetInput.value = '';
-  modal.style.visibility = 'hidden';
-  step2.style.visibility = 'hidden';
-  [...winningNumberInput].forEach((input) => {
+const onClickRetryButton = () => {
+  $budgetInput.value = '';
+  convertVisibilityToHidden($modal, $step2);
+  [...$winningNumberInput].forEach((input) => {
     input.value = '';
   });
-  bonusNumberInput.value = '';
+  $bonusNumberInput.value = '';
 };
 
-budgetForm.addEventListener('submit', onSubmitBudgetForm);
-lottoNumberForm.addEventListener('submit', onSubmitLottoNumberForm);
+$budgetForm.addEventListener('submit', onSubmitBudgetForm);
+$lottoNumberForm.addEventListener('submit', onSubmitLottoNumberForm);
 
-modalCloseBtn.addEventListener('click', onClickModalCloseBtn);
-modalBackground.addEventListener('click', onClickModalBackGround);
+$modalCloseButton.addEventListener('click', () => convertVisibilityToHidden($modal));
+$modalBackground.addEventListener('click', () => convertVisibilityToHidden($modal));
 
-retryBtn.addEventListener('click', onClickRetryBtn);
+$retryButton.addEventListener('click', onClickRetryButton);
