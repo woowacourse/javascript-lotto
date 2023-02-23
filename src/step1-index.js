e85c18dade14308ffc0d78307a7d757ee4f3c81b
minjaeKim <smallkdb@gmail.com>
2023-02-23 10:34:00 +0900

    chore: step1을 동작하기 위한 파일 백업

--- src/step1-index.js
+++ src/step1-index.js
@@ -2,6 +2,6 @@
  * step 1의 시작점이 되는 파일입니다.
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
-import LottoController from "./domain/LottoController.js";
+import LottoConsoleController from './domain/LottoConsoleController.js';
 
-new LottoController().play();
+new LottoConsoleController().play();
---
+++ src/view/OutputView.js
@@ -0,0 +1,33 @@
+import Console from '../utils/Console.js';
+
+const OutputView = {
+  printPurchaseResult(number) {
+    Console.print(`${number}개를 구매했습니다.`);
+  },
+  printLotto(numbers) {
+    Console.print(`[${numbers.join(', ')}]`);
+  },
+  // eslint-disable-next-line max-lines-per-function
+  printResult(rankingResult) {
+    Console.print(`
+당첨 통계
+--------------------
+3개 일치 (5,000원) - ${rankingResult[4]}개
+4개 일치 (50,000원) - ${rankingResult[3]}개
+5개 일치 (1,500,000원) - ${rankingResult[2]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankingResult[1]}개
+6개 일치 (2,000,000,000원) - ${rankingResult[0]}개`);
+  },
+  printBenefit(benefit) {
+    Console.print(`총 수익률은 ${benefit}%입니다.`);
+    this.printNewLine();
+  },
+  printNewLine() {
+    Console.print('');
+  },
+  printError(error) {
+    Console.print(error.message);
+    this.printNewLine();
+  },
+};
+export default OutputView;
