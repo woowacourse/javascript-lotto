03b725c49e17e91b09fbd3c4c215cea111a52ccb
sh981013s <sh981013s@gmail.com>
2023-02-23 18:13:45 +0900

    feat(domList): DOM element 상수 추가

--- src/js/view/stepTwo/domList.js
+++ src/js/view/stepTwo/domList.js
@@ -1,10 +1,21 @@
-import { $ } from '@lotto/utils/selector';
+import { $, $$ } from '@lotto/utils/selector';
 
 const domList = {
   buyBtn: $('.buy-btn'),
   moneyInput: $('.money-input'),
   moneyInputErrorText: $('.money-input-error-text'),
   mainContainer: $('.main-container'),
+  lottoBox: $('.lotto-box'),
+  lottoLengthText: $('.lotto-length-text'),
+  targetNumberInputs: $$('.square-input'),
+  resultBtn: $('.result-btn'),
+  targetNumberInputErrorText: $('.target-number-input-error-text'),
+  resultModal: $('#myModal'),
+  resultTable: $('table'),
+  resultTableBody: $('#myTableBody'),
+  ropText: $('.rop-text'),
+  retryBtn: $('.retry-btn'),
+  closeModalBtn: $('.close'),
 };
 
 export default domList;
