88e2ee8ac30a5f36b71ec2d7a9ee416cee2d79ef
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-23 17:50:05 +0900

    ♻️ refactor(controller): step1, step2에서 공통으로 사용되는 컨트롤러 로직만 존재하게 구현

--- src/domain/controller/LottoMachine.js
+++ src/domain/controller/LottoMachine.js
@@ -1,16 +1,5 @@
-const Benefit = require('../model/Benefit');
-const Money = require('../model/Money');
-const Winning = require('../model/Winning');
-const inputHandler = require('../../view/inputView');
-const outputView = require('../../view/outputView');
-const { pickRandomNumberInRange, errorHandler } = require('../../utils');
-const Console = require('../../utils/Console');
-const {
-  MAGIC_NUMBER,
-  RANK_INFORMATIONS,
-  MAGIC_LITERAL,
-  ERROR_MESSAGE,
-} = require('../../constant');
+const { pickRandomNumberInRange } = require('../../utils');
+const { MAGIC_NUMBER, RANK_INFORMATIONS } = require('../../constant');
 
 class LottoMachine {
   #lottos;
@@ -24,95 +13,46 @@ class LottoMachine {
     };
   }
 
-  play() {
-    this.readMoney();
+  getLottos() {
+    return this.#lottos;
   }
 
-  readMoney() {
-    inputHandler('> 구입금액을 입력해 주세요.', this.#afterReadMoney);
+  getMoney() {
+    return this.#machineInput.money;
   }
 
-  readWinningNumbers() {
-    inputHandler(
-      '\n> 당첨 번호를 입력해 주세요.',
-      this.#afterReadWinningNumbers
-    );
+  getWinning() {
+    return this.#machineInput.winning;
   }
 
-  readBonusNumber() {
-    inputHandler(
-      '\n> 보너스 번호를 입력해 주세요.',
-      this.#afterReadBonusNumber
-    );
+  setMoney(money) {
+    this.#machineInput.money = money;
   }
 
-  readRetryOption() {
-    inputHandler(
-      `\n> 다시 시작하시겠습니까? (${MAGIC_LITERAL.retry}/${MAGIC_LITERAL.quit})`,
-      this.#afterReadRetryOption
-    );
+  setWinning(winning) {
+    this.#machineInput.winning = winning;
   }
 
-  #afterReadMoney = (input) => {
-    try {
-      this.#machineInput.money = new Money(input);
-      outputView.printLottoCount(
-        this.#machineInput.money.getAmount() / MAGIC_NUMBER.moneyUnit
-      );
-      this.#generateLottos(this.#machineInput.money.getAmount());
-      this.#showLottos();
-      this.readWinningNumbers();
-    } catch (error) {
-      errorHandler(error, () => this.readMoney());
-    }
-  };
-
-  #afterReadWinningNumbers = (input) => {
-    try {
-      const winningNumbers = input
-        .split(MAGIC_LITERAL.comma)
-        .map((winningNumber) => Number(winningNumber));
-      this.#machineInput.winning = new Winning();
-      this.#machineInput.winning.setWinningNumbers(winningNumbers);
-      this.readBonusNumber();
-    } catch (error) {
-      errorHandler(error, () => this.readWinningNumbers());
-    }
-  };
-
-  #afterReadBonusNumber = (input) => {
-    try {
-      this.#machineInput.winning.setBonusNumber(Number(input));
-      const ranks = this.#getCollectedRanks();
-      const benefit = new Benefit();
-      benefit.calculateRate(this.#machineInput.money.getAmount(), ranks);
-      this.#showResult(benefit, ranks);
-      this.readRetryOption();
-    } catch (error) {
-      errorHandler(error, () => this.readBonusNumber());
-    }
-  };
-
-  #afterReadRetryOption = (input) => {
-    try {
-      this.#checkRetryOrQuit(input);
-    } catch (error) {
-      errorHandler(error, () => this.readRetryOption());
-    }
-  };
-
-  #checkRetryOrQuit(input) {
-    if (input === MAGIC_LITERAL.retry) return this.#retry();
-    if (input === MAGIC_LITERAL.quit) return this.#quit();
-    throw new Error(ERROR_MESSAGE.retryOption);
-  }
-
-  #generateLottos(amount) {
-    const lottoCount = amount / MAGIC_NUMBER.moneyUnit;
+  generateLottos() {
+    const lottoCount = this.getLottoCount();
 
     this.#lottos = Array.from({ length: lottoCount }).map(() =>
       this.#getComposedLottoNumbers().sort((first, second) => first - second)
     );
+  }
+
+  getLottoCount() {
+    return this.#machineInput.money.getAmount() / MAGIC_NUMBER.moneyUnit;
+  }
+
+  getCollectedRanks() {
+    const RANK_TEMPLATE = [0, 0, 0, 0, 0];
+
+    const ranks = this.#lottos.reduce((accumulator, lotto) => {
+      return this.#getIncreasedRanks(lotto, accumulator);
+    }, RANK_TEMPLATE);
+
+    return ranks;
   }
 
   #getComposedLottoNumbers() {
@@ -129,26 +69,18 @@ class LottoMachine {
     return [...lottoNumbers];
   }
 
-  #getCollectedRanks() {
-    const RANK_TEMPLATE = [0, 0, 0, 0, 0];
-
-    const ranks = this.#lottos.reduce((accumulator, lotto) => {
-      return this.#getIncreasedRanks(lotto, accumulator);
-    }, RANK_TEMPLATE);
-
-    return ranks;
-  }
-
   #getIncreasedRanks(lotto, ranks) {
+    const updatedRanks = ranks;
     const matchedCount = this.#getMatchedCount(lotto);
     const rankIndex = this.#getRankIndex(matchedCount, this.#isBonus(lotto));
-
-    return rankIndex !== MAGIC_NUMBER.losing ? (ranks[rankIndex] += 1) : ranks;
+    if (rankIndex !== MAGIC_NUMBER.losing) {
+      updatedRanks[rankIndex] += 1;
+    }
+    return updatedRanks;
   }
 
   #getMatchedCount(lotto) {
     const winningNumbers = this.#machineInput.winning.getWinningNumbers();
-
     return lotto.filter((number) => winningNumbers.includes(number)).length;
   }
 
@@ -166,27 +98,5 @@ class LottoMachine {
   #isBonus(lotto) {
     return lotto.includes(this.#machineInput.winning.getBonusNumber());
   }
-
-  #showLottos() {
-    this.#lottos.forEach((lotto) => {
-      outputView.printLotto(lotto);
-    });
-  }
-
-  #showResult(benefit, ranks) {
-    outputView.printResultTitle();
-    outputView.printResult(ranks);
-    outputView.printBenefit(benefit.getRate());
-  }
-
-  #retry() {
-    const lottoMachine = new LottoMachine();
-    lottoMachine.play();
-  }
-
-  #quit() {
-    Console.quit();
-  }
 }
-
 module.exports = LottoMachine;
