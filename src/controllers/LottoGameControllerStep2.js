d605c245ed2db8b3eea46e1a3119d6d29c448ea1
chex <chex417@naver.com>
2023-02-23 19:33:50 +0900

    refactor: 컨트롤러의 유효성 검사 로직 메서드로 분리

--- src/controllers/LottoGameControllerStep2.js
+++ src/controllers/LottoGameControllerStep2.js
@@ -39,18 +39,22 @@ class LottoGameControllerStep2 {
 
     const buyMoney = Number(this.view.buyMoneyInput.value);
 
+    if (!this.validateBuyMoney(buyMoney)) return false;
+
+    this.lottoGame = new LottoGame(buyMoney);
+
+    const lottoNumbersList = this.lottoGame.getLottoNumbersList();
+    this.view.printPurchasedLottos(lottoNumbersList);
+  };
+
+  validateBuyMoney(buyMoney) {
     if (!lottoGameValidatorStep2.isValidBuyMoney(buyMoney)) {
       alert(ERROR.BUY_MONEY);
       this.view.buyMoneyInput.focus();
       return false;
     }
-
-    this.lottoGame = new LottoGame(buyMoney);
-
-    const lottoNumbersList = this.lottoGame.getLottoNumbersList();
-
-    this.view.printPurchasedLottos(lottoNumbersList);
-  };
+    return true;
+  }
 
   onClickShowResultButton = event => {
     event.preventDefault();
@@ -58,6 +62,17 @@ class LottoGameControllerStep2 {
     const luckyNumbers = [...this.view.luckyNumbersInput].map(number => Number(number.value));
     const bonusNumber = Number(this.view.bonusNumberInput.value);
 
+    if (!this.validateWinningNumbers(luckyNumbers, bonusNumber)) return false;
+
+    this.lottoGame.initWinningNumbers(luckyNumbers, bonusNumber);
+
+    const amountOfRanks = this.lottoGame.getAmountOfRanks();
+    const profit = this.lottoGame.calculateProfit();
+
+    this.view.printResult(amountOfRanks, profit);
+  };
+
+  validateWinningNumbers(luckyNumbers, bonusNumber) {
     if (!lottoGameValidatorStep2.isValidLuckyNumbers(luckyNumbers)) {
       alert(ERROR.LUCKY_NUMBERS);
       this.view.luckyNumbersInput[0].focus();
@@ -70,13 +85,8 @@ class LottoGameControllerStep2 {
       return false;
     }
 
-    this.lottoGame.initWinningNumbers(luckyNumbers, bonusNumber);
-
-    const amountOfRanks = this.lottoGame.getAmountOfRanks();
-    const profit = this.lottoGame.calculateProfit();
-
-    this.view.printResult(amountOfRanks, profit);
-  };
+    return true;
+  }
 
   onClickModalCloseButton = () => {
     this.view.hideModal();
