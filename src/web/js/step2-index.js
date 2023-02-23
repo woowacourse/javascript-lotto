2080b78867ddf834322232c4ccea182771594a16
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-02-23 18:25:52 +0900

    fix: 결과값 잘못 나오는 버그 수정

--- src/web/js/step2-index.js
+++ src/web/js/step2-index.js
@@ -5,10 +5,76 @@ import LINK from './constants/link.js';
 import { $ } from './utils/dom.js';
 
 const App = {
-  state: {},
+  state: {
+    winningNumbers: {},
+    result: {},
+  },
 
   init: function () {
     this.initEventListeners();
+  },
+
+  renderResult: function (dialog) {
+    dialog.innerHTML = `<div id="result">
+		<input id="exit-button" type="button" value="❎">
+		<span id="result-title">🏆 당첨 통계 🏆</span>
+		<table id="result-table">
+			<tr>
+				<th scope="col">일치 갯수</th>
+				<th scope="col">당첨금</th>
+				<th scope="col">당첨 갯수</th>
+			</tr>
+			<tr>
+				<td>3개</td>
+				<td>5,000</td>
+				<td>${
+          this.state.result.amountOfRanks[
+            this.state.result.amountOfRanks.length - 1
+          ]
+        }개</td>
+			</tr>
+			<tr>
+				<td>4개</td>
+				<td>50,000</td>
+				<td>${
+          this.state.result.amountOfRanks[
+            this.state.result.amountOfRanks.length - 2
+          ]
+        }개</td>
+			</tr>
+			<tr>
+				<td>5개</td>
+				<td>1,500,000</td>
+				<td>${
+          this.state.result.amountOfRanks[
+            this.state.result.amountOfRanks.length - 3
+          ]
+        }개</td>
+			</tr>
+			<tr>
+				<td>5개 + 보너스 볼</td>
+				<td>30,000,000</td>
+				<td>${
+          this.state.result.amountOfRanks[
+            this.state.result.amountOfRanks.length - 4
+          ]
+        }개</td>
+			</tr>
+			<tr>
+				<td>6개</td>
+				<td>2,000,000,000</td>
+				<td>${
+          this.state.result.amountOfRanks[
+            this.state.result.amountOfRanks.length - 5
+          ]
+        }개</td>
+			</tr>
+		</table>
+		<span id="profit-text">당신의 총 수익률은 ${
+      this.state.result.profit
+    }%입니다.</span>
+		<input id="retry-button" type="button" value="다시 시작하기">
+	</div>`;
   },
 
   render: {
@@ -19,47 +85,6 @@ const App = {
     winningNumbers: () => {
       $('#winning-numbers').innerHTML =
         tagGenerator.generateWinningNumberTags();
-    },
-
-    result: (dialog, resultData) => {
-      dialog.innerHTML = `<div id="result">
-			<input id="exit-button" type="button" value="❎">
-			<span id="result-title">🏆 당첨 통계 🏆</span>
-			<table id="result-table">
-				<tr>
-					<th scope="col">일치 갯수</th>
-					<th scope="col">당첨금</th>
-					<th scope="col">당첨 갯수</th>
-				</tr>
-				<tr>
-					<td>3개</td>
-					<td>5,000</td>
-					<td>${resultData.amountOfRanks[resultData.amountOfRanks.length - 1]}개</td>
-				</tr>
-				<tr>
-					<td>4개</td>
-					<td>50,000</td>
-					<td>${resultData.amountOfRanks[resultData.amountOfRanks.length - 2]}개</td>
-				</tr>
-				<tr>
-					<td>5개</td>
-					<td>1,500,000</td>
-					<td>${resultData.amountOfRanks[resultData.amountOfRanks.length - 3]}개</td>
-				</tr>
-				<tr>
-					<td>5개 + 보너스 볼</td>
-					<td>30,000,000</td>
-					<td>${resultData.amountOfRanks[resultData.amountOfRanks.length - 4]}개</td>
-				</tr>
-				<tr>
-					<td>6개</td>
-					<td>2,000,000,000</td>
-					<td>${resultData.amountOfRanks[resultData.amountOfRanks.length - 5]}개</td>
-				</tr>
-			</table>
-			<span id="profit-text">당신의 총 수익률은 ${resultData.profit}%입니다.</span>
-			<input id="retry-button" type="button" value="다시 시작하기">
-		</div>`;
     },
   },
 
@@ -73,33 +98,40 @@ const App = {
       () => (window.location = LINK.HOME)
     );
 
-    $('#price-form').addEventListener('submit', event =>
-      this.purchaseLottos(event)
-    );
+    $('#price-form').addEventListener('submit', event => {
+      this.purchaseLottos(event);
+    });
+  },
+
+  getFormData: function (target) {
+    const formData = new FormData(target);
+
+    return Object.fromEntries(formData);
   },
 
   purchaseLottos: function (event) {
-    const formData = new FormData(event.target);
-    const fields = Object.fromEntries(formData);
+    const fields = this.getFormData(event.target);
     const price = InputChecker.checkLottoPrice(fields.price);
     if (!price) {
       return;
     }
+
     LottoGame.init(price);
 
     this.render.lottos(LottoGame.getLottos());
     this.render.winningNumbers();
-    $('#winning-numbers-form').addEventListener('submit', event =>
-      event.preventDefault()
-    );
-    $('#winning-numbers-form').addEventListener('submit', event =>
-      this.calculateResult(event)
-    );
+
+    $('#winning-numbers-form').addEventListener('submit', event => {
+      event.preventDefault();
+    });
+    $('#winning-numbers-form').addEventListener('submit', event => {
+      this.calculateResult(event);
+      this.loadDialog();
+    });
   },
 
   calculateResult: function (event) {
-    const formData = new FormData(event.target);
-    const fields = Object.fromEntries(formData);
+    const fields = this.getFormData(event.target);
     const luckyNumbersInput = Array.from(
       { length: 6 },
       (_, index) => fields[`lucky-number-${index + 1}`]
@@ -117,21 +149,28 @@ const App = {
       return;
     }
 
+    this.state.winningNumbers = { luckyNumbers, bonusNumber };
+  },
+
+  loadDialog: function () {
     const dialog = $('dialog');
     dialog.showModal();
+
     dialog.addEventListener('click', event => {
       if (event.target.nodeName === 'DIALOG') {
         dialog.close();
       }
     });
 
-    LottoGame.initWinningNumbers({ luckyNumbers, bonusNumber });
-    const resultData = {
-      amountOfRanks: LottoGame.drawLotto(),
-      profit: LottoGame.calculateProfit(),
-    };
+    LottoGame.initWinningNumbers(this.state.winningNumbers);
+    if (Object.keys(this.state.result).length === 0) {
+      this.state.result = {
+        amountOfRanks: LottoGame.drawLotto(),
+        profit: LottoGame.calculateProfit(),
+      };
+    }
 
-    this.render.result(dialog, resultData);
+    this.renderResult(dialog);
 
     $('#exit-button').addEventListener('click', () => dialog.close());
 
