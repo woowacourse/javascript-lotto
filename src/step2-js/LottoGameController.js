a0e530856402171dc9da7ec284525fb6bd47ee14
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-23 22:23:41 +0900

    feat: 구입 금액 입력 에러시 입력 초기화 기능 추가

--- src/step2-js/LottoGameController.js
+++ src/step2-js/LottoGameController.js
@@ -25,6 +25,7 @@ export default class LottoGameController {
       Validation.validatePurchaseAmount(purchaseAmount);
     } catch ({ message }) {
       this.#view.showAlert(message);
+      this.#view.initPurchaseAmountInput();
 
       return;
     }
