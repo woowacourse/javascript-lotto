51910d9ce8d3a5872c4d39354ed0bcc24f458ebe
afds4567 <afds4567@gmail.com>
2023-02-24 06:29:29 +0900

    refactor: 우승 금액 출력 형식 고정

--- src/view/modalTemplate.js
+++ src/view/modalTemplate.js
@@ -4,13 +4,13 @@ const modalTemplate = (prize, count) => {
   if (prize === 'secondPrize') {
     return `<tr>
   <td>${PRIZE_MATCH_COUNT[prize]}개 + 보너스볼</td>
-  <td>${LOTTO_PRIZE_MONEY[prize].toLocaleString()}원</td>
+  <td>${LOTTO_PRIZE_MONEY[prize].toLocaleString('ko-KR')}원</td>
   <td>${count}개</td>
 </tr>`;
   }
   return `<tr>
   <td>${PRIZE_MATCH_COUNT[prize]}개</td>
-  <td>${LOTTO_PRIZE_MONEY[prize].toLocaleString()}원</td>
+  <td>${LOTTO_PRIZE_MONEY[prize].toLocaleString('ko-KR')}원</td>
   <td>${count}개</td>
 </tr>`;
 };
