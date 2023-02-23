70532979776d35d069f19cb865cf7845ac451842
chex <chex417@naver.com>
2023-02-23 13:57:24 +0900

    feat: 웹 기반 로또게임 진입점 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,17 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+import './css/index.css';
+import './css/reset.css';
+import LottoGameControllerStep2 from './controllers/LottoGameControllerStep2.js';
+
+class App {
+  #lottoController = new LottoGameControllerStep2();
+
+  play() {
+    this.#lottoController.startGame();
+  }
+}
+
+const app = new App();
+app.play();
