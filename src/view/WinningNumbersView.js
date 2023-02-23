1e17055a966d876bdfdd86ddb55df9652b2a7016
regularPark <srk99279@naver.com>
2023-02-23 13:20:34 +0900

    feat: winning container 숨기는 기능 구현

--- src/view/WinningNumbersView.js
+++ src/view/WinningNumbersView.js
@@ -2,7 +2,8 @@ import { LOTTO_SIZE } from "../constants";
 
 class WinningNumbersView {
   constructor(submitWinningNumbers) {
-    document.querySelector(".winning-container").style.display = "flex";
+    this.winningContainer = document.querySelector(".winning-container");
+    this.winningContainer.style.display = "flex";
     this.lottoContainer = document.getElementsByClassName("lotto-container");
     this.resultButton = document.getElementsByClassName("result-btn");
     this.bonusNumber = document.getElementsByClassName("bonus-input");
@@ -16,6 +17,11 @@ class WinningNumbersView {
     Array.from({ length: LOTTO_SIZE }, () => {
       this.lotteryContainer.innerHTML += `<input type="number" name="lotto-number" class="lotto-input" min="1" max="45" required>`;
     });
+  }
+
+  hideWinningContainer() {
+    this.winningContainer.style.display = "none";
+    this.lotteryContainer.innerHTML = "";
   }
 }
 
