18a939310a9416a2cfb7bd00b87ba78ba0185bff
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-23 20:01:38 +0900

    feat: 입력된 로또 번호와 보너스 번호 검사

--- src/view/containers/InputWinningNumberBox/inputWinningNumberEvent.js
+++ src/view/containers/InputWinningNumberBox/inputWinningNumberEvent.js
@@ -1,31 +1,31 @@
 /* eslint-disable no-undef */
 
+import LottoValidator from "../../../domain/LottoValidator";
+
 const inputWinningNumberEvent = () => {
   const form = document.getElementById("winning-number-submit");
 
   form.onsubmit = function (event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    console.log({
-      main: [
-        formData.get('main0'),
-        formData.get('main1'),
-        formData.get('main2'),
-        formData.get('main3'),
-        formData.get('main4'),
-        formData.get('main5'),
-      ],
-      bonus: formData.get('bonus'),
-    });
-    // try {
-    //   LottoValidator.checkMoney(money);
-    //   store['lottos'] = generateLottos((money));
-    //   event.target.money.value = '';
-    //   Render.container("purchase-result", () => PurchaseResults(), () => { });
-    //   Render.container("input-winning-number", () => InputWinningNumberBox(), () => { });
-    // } catch (error) {
-    //   alert(error.message);
-    // }
+    try {
+      const formData = new FormData(event.target);
+      const winningNumber = {
+        main: [
+          formData.get('main0'),
+          formData.get('main1'),
+          formData.get('main2'),
+          formData.get('main3'),
+          formData.get('main4'),
+          formData.get('main5'),
+        ],
+        bonus: formData.get('bonus'),
+      };
+      LottoValidator.checkWinningNumber(winningNumber.main.join(','));
+      LottoValidator.checkBonusNumber(winningNumber.bonus);
+      LottoValidator.checkLottoDuplicate(winningNumber);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 };
 
