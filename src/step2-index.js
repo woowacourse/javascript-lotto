8ee5739a56480763f6b6420a43cc966deff22635
kyw0716 <kyw0716@naver.com>
2023-02-23 15:42:50 +0900

    feat: html, css 뼈대 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,14 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+require('./style.css');
+
+const form = document.getElementById('priceInputContainer');
+const winningInputContainer = document.getElementById('winningInputContainer');
+const result = document.getElementById('resultContainer');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  winningInputContainer.classList = '';
+  result.classList = '';
+});
