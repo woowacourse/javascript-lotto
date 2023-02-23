b73510ca0942072f6471fc44ef4b47648d6c37d0
Patrick <kuyt3122@naver.com>
2023-02-23 11:56:45 +0900

    🐛 fix: 총 수익률 값 다르게 나오는 오류 수정

--- src/domain/LottoResult.js
+++ src/domain/LottoResult.js
@@ -25,9 +25,10 @@ class LottoResult {
 
   earningsRate(lottoMoney, result) {
     const ConstantMoney = [
-      NUMBER.FIFTH_PRIZE_MONEY, NUMBER.FOUR_PRIZE_MONEY, NUMBER.THIRD_PRIZE_MONEY,
-      NUMBER.SECOND_PRIZE_MONEY, NUMBER.FIRST_PRIZE_MONEY,
+       NUMBER.FIRST_PRIZE_MONEY,NUMBER.SECOND_PRIZE_MONEY, NUMBER.THIRD_PRIZE_MONEY,
+       NUMBER.FOUR_PRIZE_MONEY, NUMBER.FIFTH_PRIZE_MONEY
     ];
+    console.log(result)
     Array.from({ length: result.length }, (v, index) => {
       this.profit += result[index] * ConstantMoney[index];
     });
