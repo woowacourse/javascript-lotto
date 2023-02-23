86ced34d4221cdc14c9daaf4e55c02cfa6dccb7c
gyeongza <gyeongza@naver.com>
2023-02-23 19:04:32 +0900

    [step2] feat : 당첨 번호와 보너스 번호를 입력받는 기능 구현

--- src/controller/WebController.js
+++ src/controller/WebController.js
@@ -3,17 +3,23 @@ import { SETTINGS, ERROR_MESSAGE } from "../constants/Config.js";
 import { $, $$ } from "../utils/Dom.js";
 import Lotto from "../domain/Lotto.js";
 import Random from "../utils/Random.js";
+import Lottos from "../domain/Lottos.js";
 
 class WebController {
   #lottoArray;
+  #winningLotto;
+  #bonusNumber;
 
   constructor() {
     this.#lottoArray = [];
+    this.#winningLotto = [];
+    this.#bonusNumber = 0;
     this.play();
   }
 
   play() {
     $(".input-money-btn").addEventListener("click", this.getBuyMoney);
+    $(".check-winning-lotto-btn").addEventListener("click", this.compareLottos);
   }
 
   getBuyMoney = (e) => {
@@ -25,7 +31,7 @@ class WebController {
       const lottoAmount = parseInt(buyMoney / SETTINGS.DIVIDE_MONEY_VALUE);
       this.createLotto(lottoAmount);
       this.printLotto(lottoAmount);
-      $('.hidden-area').classList.add('show');
+      $(".hidden-area").classList.add("show");
     } catch (e) {
       alert(e.message);
     } finally {
@@ -57,6 +63,58 @@ class WebController {
       .join("");
 
     $(".print-lottos-list").innerHTML = lottoList;
+  };
+
+  getWinningNumbers = () => {
+    for (let i = 0; i < SETTINGS.MAX_WINNING_NUMBER_LENGTH; i++) {
+      this.#winningLotto.push(parseInt($$(".winning-number")[i].value, 10));
+    }
+
+    try {
+      this.validateWinningNumbers(this.#winningLotto);
+      return this.#winningLotto;
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  validateWinningNumbers = (winningLottoNumbers) => {
+    Validator.isDuplicatedNumber(winningLottoNumbers);
+    Validator.isCorrectLength(winningLottoNumbers);
+    for (let i = 0; i < winningLottoNumbers.length; i++) {
+      this.checkEachNumber(winningLottoNumbers[i]);
+    }
+  };
+
+  checkEachNumber = (eachNumber) => {
+    Validator.isNumber(eachNumber);
+    Validator.isCorrectRange(eachNumber);
+    Validator.isPositiveInteger(eachNumber);
+  };
+
+  getBonusNumber = () => {
+    try {
+      this.#bonusNumber = parseInt($(".bonus-number").value, 10);
+      this.validateBonusNumber();
+      this.checkEachNumber(this.#bonusNumber);
+      return this.#bonusNumber;
+    } catch (e) {
+      alert(e.message);
+    } finally {
+      this.#winningLotto = [];
+      this.#bonusNumber = 0;
+    }
+  };
+
+  validateBonusNumber = () => {
+    console.log(this.#bonusNumber, this.#winningLotto);
+    Validator.hasBonusNumber(this.#bonusNumber, this.#winningLotto);
+  };
+
+  compareLottos = (e) => {
+    e.preventDefault();
+    const winning = this.getWinningNumbers();
+    const bonus = this.getBonusNumber();
   };
 }
 
