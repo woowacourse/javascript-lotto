e96edb12674f67c4f8def0cbcfe208f28c94ad4e
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-25 00:41:09 +0900

    refactor: 로또 구매 페이지 디자인 구현

--- src/view/containers/PurchaseResults/index.js
+++ src/view/containers/PurchaseResults/index.js
@@ -2,9 +2,14 @@
 const PurchaseResults = () => {
   return `
 <div>
-  <div>총 ${store.lottos.length}개를 구매하였습니다.</div>
+  <div id="purchase-results-title">총 ${store.lottos.length}개를 구매하였습니다.</div>
   <div>
-    ${store.lottos.map((lotto) => `<div>🎟️${lotto.getNumbers().join(', ')}</div>`).join('')}
+    ${store.lottos.map((lotto) => `
+    <div class="d-flex align-items-center">
+      <div class="lotto-ticket-icon">🎟️</div>
+      <div class="lotto-ticket ">${lotto.getNumbers().join(', ')}</div>
+    </div>
+    `).join('')}
   </div>
 </div>
 `;
