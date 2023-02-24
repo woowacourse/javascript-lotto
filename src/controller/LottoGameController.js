07e8794a4d44edbdc13df7d9884abd40060d9a40
jeongwusi <jeongwusi@gmail.com>
2023-02-24 17:27:30 +0900

    ✨ feat: 금액을 입력 받는 기능 추가

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,3 +1,22 @@
-class LottoGameController {}
+const { $, $$ } = require('../utils/DOM.js');
+
+class LottoGameController {
+  constructor() {
+    this.money = 0;
+    this.buttonEvents();
+  }
+
+  buttonEvents() {
+    $('.purchase-amount-form').addEventListener('submit', event => {
+      event.preventDefault();
+      this.purchaseButton();
+    });
+  }
+
+  purchaseButton() {
+    this.money = $('.purchase-amount-input').value;
+    console.log(this.money);
+  }
+}
 
 module.exports = LottoGameController;
