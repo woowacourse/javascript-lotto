5fa7efaaa9edbc02d6bb0137fed77ffc9c847183
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-24 20:04:47 +0900

    fix: 모달 생성 시 result를 참조하지 못하는 문제 수정

--- src/view/containers/GameModal/index.js
+++ src/view/containers/GameModal/index.js
@@ -1,61 +1,54 @@
 /* eslint-disable no-undef */
 const GameModal = () => {
-  return `
-  <!-- The Modal -->
-  <div id="game-modal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-      <div>
-        <span class="game-modal-open-button">&times;</span>
-      </div>
-      <div>
-        <div>🏆 당첨 통계 🏆</div>
-        <table>
-          <thead>
-            <tr>
-              <th>일치 갯수</th>
-              <th>당첨금</th>
-              <th>당첨 갯수</th>
-            </tr>
-          </thead>
-          <tbody id="tbody">
-            <tr>
-              <td>3개</td>
-              <td>5,000</td>
-              <td>${store.result[4]}개</td>
-            </tr>
-            <tr>
-              <td>4개</td>
-              <td>50,000</td>
-              <td>${store.result[3]}개</td>
-            </tr>
-            <tr>
-              <td>5개</td>
-              <td>1,500,000</td>
-              <td>${store.result[2]}개</td>
-            </tr>
-            <tr>
-              <td>5개+보너스볼</td>
-              <td>30,000,000</td>
-              <td>${store.result[1]}개</td>
-            </tr>
-            <tr>
-              <td>6개</td>
-              <td>2,000,000,000</td>
-              <td>${store.result[0]}개</td>
-            </tr>
-          </tbody>
-        </table>
-        <div>
-          당신의 총 수익률은 ${store.benefit}입니다.
-        </div>
-        <button>다시 시작하기</button>
-      </div>
+  if (store.result) {
+    return `
+<div>
+  <div>🏆 당첨 통계 🏆</div>
+    <table>
+      <thead>
+        <tr>
+          <th>일치 갯수</th>
+          <th>당첨금</th>
+          <th>당첨 갯수</th>
+        </tr>
+      </thead>
+      <tbody id="tbody">
+        <tr>
+          <td>3개</td>
+          <td>5,000</td>
+          <td>${store.result[4]}개</td>
+        </tr>
+        <tr>
+          <td>4개</td>
+          <td>50,000</td>
+          <td>${store.result[3]}개</td>
+        </tr>
+        <tr>
+          <td>5개</td>
+          <td>1,500,000</td>
+          <td>${store.result[2]}개</td>
+        </tr>
+        <tr>
+          <td>5개+보너스볼</td>
+          <td>30,000,000</td>
+          <td>${store.result[1]}개</td>
+        </tr>
+        <tr>
+          <td>6개</td>
+          <td>2,000,000,000</td>
+          <td>${store.result[0]}개</td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      당신의 총 수익률은 ${store.benefit}%입니다.
     </div>
-
+    <button>다시 시작하기</button>
   </div>
-
+</div>
 `;
+  }
+  return '';
+
 };
 export default GameModal;
