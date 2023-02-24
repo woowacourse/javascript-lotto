a1b6522512b73edcbb665383416f8513ac1bcd2c
jeongwusi <jeongwusi@gmail.com>
2023-02-24 16:28:58 +0900

    ✨ feat: 시작하는 코드 추가

---
+++ src/controller/LottoGameController.js
@@ -0,0 +1,3 @@
+class LottoGameController {}
+
+module.exports = LottoGameController;
--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,8 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+const CSS = require('./css/index.css');
+const LottoGameController = require('./controller/LottoGameController.js');
+
+new LottoGameController();
