90a7141e0eb5afc2059549f6dd2ca53bfcb49605
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-25 15:06:13 +0900

    fix: 불필요한 콘솔 찍히는 버그 수정

--- src/domain/judgeResult.js
+++ src/domain/judgeResult.js
@@ -1,7 +1,6 @@
 import { LOTTO } from "../constants";
 
 export const judgeResult = (lottos, winningNumber) => {
-  console.log(lottos, winningNumber);
   const rankingCount = Array(LOTTO.prize.length).fill(0);
 
   return lottos.reduce((acc, lotto) => {
