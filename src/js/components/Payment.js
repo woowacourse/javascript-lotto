871d8b0dca696b678e83f7a83bb24063010bbf2d
cruelladevil <dev.timetravel@gmail.com>
2023-02-25 18:28:19 +0900

    feat: 구입 금액을 입력받을 Payment

---
+++ src/js/components/Payment.js
@@ -0,0 +1,13 @@
+const Payment = () => {
+  return `
+    <div class="payment">
+      <span>구입할 금액을 입력해주세요.</span>
+      <div class="lotto-money">
+        <input id="money-input" type="text" placeholder="금액" maxLength="8"/>
+        <button class="lotto-button purchase-button caption">구입</button>
+      </div>
+    </div>
+  `;
+};
+
+export default Payment;
