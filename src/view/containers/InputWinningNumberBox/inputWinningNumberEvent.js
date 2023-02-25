380e313bd157e371b1bb86e2a7bc61d0498e1b87
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-25 15:06:34 +0900

    refactor: 입력 처리 관련 함수 분리

--- src/view/containers/InputWinningNumberBox/inputWinningNumberEvent.js
+++ src/view/containers/InputWinningNumberBox/inputWinningNumberEvent.js
@@ -6,6 +6,47 @@ import Container from "../../../utils/Container";
 import GameModal from "../GameModal";
 import modalEvent from "../GameModal/modalEvent";
 
+const winningNumberObject = (event) => {
+  const formData = new FormData(event.target);
+  return {
+    main: [
+      formData.get('main0'),
+      formData.get('main1'),
+      formData.get('main2'),
+      formData.get('main3'),
+      formData.get('main4'),
+      formData.get('main5'),
+    ],
+    bonus: formData.get('bonus'),
+  };
+};
+
+const winningNumberValidator = (winningNumber) => {
+  LottoValidator.checkWinningNumber(winningNumber.main.join(','));
+  LottoValidator.checkBonusNumber(winningNumber.bonus);
+  LottoValidator.checkLottoDuplicate(winningNumber);
+};
+
+const calculateLottos = (global) => {
+  const result = judgeResult(global.getStore('lottos'), global.getStore('winningNumber'));
+  const benefit = calculateBenefit(global.getStore('lottos').length * 1000, result);
+  global.setStore('result', result);
+  global.setStore('benefit', benefit);
+};
+
+const renderGameResult = (global) => {
+  Container.render(
+    "game-result",
+    () => GameModal(global),
+    () => modalEvent()
+  );
+};
+
+const openGameModal = () => {
+  const modal = document.getElementById("game-modal");
+  modal.style.display = "block";
+};
+
 const inputWinningNumberEvent = (global) => {
 
   const form = document.getElementById("winning-number-submit");
@@ -13,35 +54,12 @@ const inputWinningNumberEvent = (global) => {
   form.onsubmit = function (event) {
     event.preventDefault();
     try {
-      const formData = new FormData(event.target);
-      const winningNumber = {
-        main: [
-          formData.get('main0'),
-          formData.get('main1'),
-          formData.get('main2'),
-          formData.get('main3'),
-          formData.get('main4'),
-          formData.get('main5'),
-        ],
-        bonus: formData.get('bonus'),
-      };
-      LottoValidator.checkWinningNumber(winningNumber.main.join(','));
-      LottoValidator.checkBonusNumber(winningNumber.bonus);
-      LottoValidator.checkLottoDuplicate(winningNumber);
+      const winningNumber = winningNumberObject(event);
+      winningNumberValidator(winningNumber);
       global.setStore('winningNumber', winningNumber);
-      const result = judgeResult(global.getStore('lottos'), global.getStore('winningNumber'));
-      const benefit = calculateBenefit(global.getStore('lottos').length * 1000, result);
-      console.log(result, benefit);
-      global.setStore('result', result);
-      global.setStore('benefit', benefit);
-      Container.render(
-        "game-result",
-        () => GameModal(global),
-        () => modalEvent()
-      );
-      const modal = document.getElementById("game-modal");
-      console.log(modal);
-      modal.style.display = "block";
+      calculateLottos(global);
+      renderGameResult(global);
+      openGameModal();
     } catch (error) {
       alert(error.message);
     }
