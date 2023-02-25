5e647e73641be6cdebdb195b8b5ebe8a394e08d5
bassyu <yrkdgus@gmail.com>
2023-02-25 14:35:50 +0900

    style(randomShuffle): i -- 에서 i -= 1 변경

--- src/utils/randomShuffle.js
+++ src/utils/randomShuffle.js
@@ -1,6 +1,6 @@
 const randomShuffle = (array) => {
   const shuffledArray = [...array];
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = array.length - 1; i > 0; i -= 1) {
     const randomIndex = Math.floor(Math.random() * (i + 1));
     [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
   }
