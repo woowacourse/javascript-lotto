bce07d2aa8ebe76b912fc0f93dd9f19128ba73b1
inyeong-kang <tomatorkd@naver.com>
2023-02-26 11:53:50 +0900

    refactor: 길어지는 연산식에서 변수들 분리

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,16 +6,22 @@ const {
 } = require('../constants/constants');
 
 const showTableRow = (rank, correctCount) => {
+  const correctCountPerRank =
+    CORRECT_COUNT_PER_RANK[INDEX_TO_KEY_CONVERTER[rank - 1]];
+  const bonusOrNotString = rank === 2 ? '+보너스 볼' : '';
+  const profitPerRank = PROFIT[INDEX_TO_KEY_CONVERTER[rank - 1]];
+  const profitByStringFormat = profitPerRank
+    .toString()
+    .replace(REGEX.PRICE_FORMAT, ',');
+
   return `
   <tr>
     <td>
-      ${CORRECT_COUNT_PER_RANK[INDEX_TO_KEY_CONVERTER[rank - 1]]}개 일치
-      ${rank === 2 ? '+보너스 볼' : ''}
+      ${correctCountPerRank}개 일치
+      ${bonusOrNotString}
     </td>
     <td>
-      ${PROFIT[INDEX_TO_KEY_CONVERTER[rank - 1]]
-        .toString()
-        .replace(REGEX.PRICE_FORMAT, ',')}원
+      ${profitByStringFormat}원
     </td>
     <td>${correctCount}개</td>
   </tr>
