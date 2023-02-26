e94fb1f498fedd7a4bcdddb769f88b49df0303cc
전증훈 <jeonjeunghoon@gmail.com>
2023-02-26 23:09:38 +0900

    fix: numberHandler 파일명 수정되지 않는 버그 해결

--- src/web/utils/NumberHandler.js
+++
@@ -1,9 +0,0 @@
-const getQuotient = (target, divider) => {
-  return parseInt(target / divider, 10);
-};
-
-const roundOff = value => {
-  return Math.round(value * 10) / 10;
-};
-
-export { getQuotient, roundOff };
