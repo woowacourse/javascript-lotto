70b1ddd86b8adcdbf3edf203ecce928f0806f78f
bassyu <yrkdgus@gmail.com>
2023-02-26 20:29:05 +0900

    feat(render): default 메세지 설정 추가

--- src/view/render.js
+++ src/view/render.js
@@ -14,13 +14,16 @@ export const renderResultModal = (rankings, rewardRate) => {
   $('#reward-rate').innerHTML = `당신의 총 수익률은 ${rewardRate}입니다.`;
 };
 
-export const renderBuyMessage = (message, color) => {
+export const renderBuyMessage = (message = '구입할 금액을 입력해 주세요.', color = 'black') => {
   const buyMessage = $('#buy-message');
   buyMessage.innerHTML = message;
   buyMessage.style.color = color;
 };
 
-export const renderLottoInputErrorMessage = (message, color) => {
+export const renderLottoInputErrorMessage = (
+  message = '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.',
+  color = 'black'
+) => {
   const lottoInputMessage = $('#lotto-input-message');
   lottoInputMessage.innerHTML = message;
   lottoInputMessage.style.color = color;
