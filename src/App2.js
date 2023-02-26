e4531bc75fb49a78af99b499565e47a9829dadcc
NaveOWO <pianowell96@gmail.com>
2023-02-27 01:41:24 +0900

    fix : 오타수정

--- src/App2.js
+++ src/App2.js
@@ -105,7 +105,7 @@ class App2 {
       const lottoScore = this.getLottoGameResult();
       this.retryLottoGame(lottoScore);
     } catch (e) {
-      this, this.resetBonusLottoInput();
+      this.resetBonusLottoInput();
     }
   }
 
