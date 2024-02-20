ef8b8c65126dcd15a5670c0e14578d3e9e2eb204
손진영 <sm099@naver.com>
2024-02-20 18:48:26 +0900

    chore(ESLint & Prettier): 프로젝트 내 ESLint 및 Prettier 초기 세팅
    1. .eslintrc.js 설정
    
    2. .prettierrc 초기 설정
    
    3. 변경 사항 다른 모듈들에 적용
    
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier', "plugin:jsdoc/recommended", 'plugin:jest/recommended'],
+  overrides: [
+    {
+      files: ['*.test.js', 'ApplicationTest.js'],
+      rules: {
+        'max-lines-per-function': ['off'],
+      },
+    },
+    {
+      files: ['*.config.js'],
+      rules: {
+        'import/no-extraneous-dependencies': ['off'],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-use-before-define': ['off'],
+    'import/prefer-default-export': ['off'],
+    'import/extensions': ['off'],
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 1],
+    // 함수 당 라인 수 제한
+    'max-lines-per-function': ['error', { max: 10 }],
+    // 함수 내 매개변수 갯수 제한
+    'max-params': ['error', 2],
+  },
+};
