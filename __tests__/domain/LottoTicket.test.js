58b5412a6857cc4343fc0826323a9741167c6bb1
soosoo22 <lllllh00@naver.com>
2024-02-20 21:16:53 +0900

    fix: 로또 티켓의 중복 확인 테스트 코드 오류 해결
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- __tests__/domain/LottoTicket.test.js
+++ __tests__/domain/LottoTicket.test.js
@@ -1,27 +1,16 @@
-describe('로또 티켓', () => {
-  describe('랜덤 값을 뽑는 기능 테스트', () => {
-    test('랜덤 값이 중복 없이 6개 뽑히는지 확인한다.', () => {
-      const lottoTicket = new LottoTicket();
-      const uniqueNumbers = new Set(lottoTicket.getLottoNumbers);
-      expect(uniqueNumbers.size).toHaveLength(6);
-    });
+import LottoTicket from '../../src/domain/LottoTicket';
 
-    test('1 ~ 45 범위의 숫자가 뽑히는 확인한다.', () => {
-      const lottoTicket = new LottoTicket().getLottoNumbers;
+describe('로또 티켓을 구성하는 랜덤 값을 뽑는 기능 테스트', () => {
+  test('랜덤 값이 중복 없이 6개 뽑히는지 확인한다.', () => {
+    const lottoTicket = new LottoTicket();
+    const uniqueNumbers = new Set(lottoTicket.ticket);
 
-      expect(
-        lottoTicket.every((lottoNumber) => lottoNumber >= 1 && lottoNumber <= 45),
-      ).toBeTruthy();
-    });
+    expect(uniqueNumbers.size).toBe(6);
   });
-  describe('로또 번호 오름차순 정리 기능 테스트', () => {
-    test('로또 번호가 오름차순으로 정리되는지 확인한다.', () => {
-      const lottoTicket = new LottoTicket().getLottoNumbers;
-      const sorttedLottoTicket = lottoTicket.sort(
-        (prevLottoNumber, nextLottoNumber) => prevLottoNumber - nextLottoNumber,
-      );
 
-      expect(lottoTicket).toEqual(sorttedLottoTicket);
-    });
+  test('1 ~ 45 범위의 숫자가 뽑히는 확인한다.', () => {
+    const lottoTicket = new LottoTicket().ticket;
+
+    expect(lottoTicket.every((lottoNumber) => lottoNumber >= 1 && lottoNumber <= 45)).toBeTruthy();
   });
 });
