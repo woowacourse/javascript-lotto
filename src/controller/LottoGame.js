f3c4e0b2d9ca0f18500a9c325abad1ce89ab5d18
anttiey <hanuuny@gmail.com>
2024-02-20 17:03:45 +0900

    feat: 보너스 번호 생성 기능 추가
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/controller/LottoGame.js
+++ src/controller/LottoGame.js
@@ -2,6 +2,10 @@ import Lotto from '../domain/Lotto';
 import LottoValidator from '../domain/LottoValidator';
 
 class LottoGame {
+  #winningNumbers;
+
+  #bonusNumber;
+
   createLotto(money) {
     return Array.from({ length: money / 1000 }).map(() => new Lotto([1, 2, 3, 4, 5, 6]));
   }
@@ -12,7 +16,15 @@ class LottoGame {
     LottoValidator.validateNumbersType(numbers);
     LottoValidator.validateNumbersRange(numbers);
 
-    return numbers;
+    this.#winningNumbers = numbers;
+  }
+
+  createBonusNumber(number) {
+    LottoValidator.validateNumbersType([number]);
+    LottoValidator.validateNumbersRange([number]);
+    LottoValidator.validateNumbersDuplicate([...this.#winningNumbers, number]);
+
+    this.#bonusNumber = number;
   }
 }
 
