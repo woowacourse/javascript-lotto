a4d69e908541abcfe4fe1c964239ff90b0dd0f7a
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-20 14:57:18 +0900

    test: 당첨 번호 에러 테스트
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

---
+++ __tests__/domain/WinningLotto.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-lines-per-function */
+import WinningLotto from "../../src/domain/WinningLotto.js";
+
+describe("WinningLotto 객체 테스트", () => {
+  test("당첨 로또 번호는 중복되면 안된다.", () => {
+    const DUPLICATE_WINNING_LOTTO_NUMBERS = [1, 1, 2, 3, 4, 5];
+
+    expect(() => new WinningLotto(DUPLICATE_WINNING_LOTTO_NUMBERS)).toThrow(
+      "[ERROR]",
+    );
+  });
+
+  test("당첨 로또 번호는 1 ~ 45 사이의 숫자여야 한다.", () => {
+    const INVALID_WINNING_LOTTO_NUMBERS = [0, 1, 2, 3, 4, 46];
+
+    expect(() => new WinningLotto(INVALID_WINNING_LOTTO_NUMBERS)).toThrow(
+      "[ERROR]",
+    );
+  });
+
+  test("당첨 로또 번호는 6개여야한다.", () => {
+    const INVALID_WINNING_LOTTO_LENGTH = [1, 2, 3, 4, 5];
+
+    expect(() => new WinningLotto(INVALID_WINNING_LOTTO_LENGTH)).toThrow(
+      "[ERROR]",
+    );
+  });
+});
