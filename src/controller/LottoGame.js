f4b6d838f71ff5a2831cb58c06f3d2c256c41c82
anttiey <hanuuny@gmail.com>
2024-02-20 16:38:19 +0900

    feat: 당첨 번호 생성 기능 추가
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/controller/LottoGame.js
+++ src/controller/LottoGame.js
@@ -1,8 +1,18 @@
 import Lotto from '../domain/Lotto';
+import LottoValidator from '../domain/LottoValidator';
 
 class LottoGame {
   createLotto(money) {
     return Array.from({ length: money / 1000 }).map(() => new Lotto([1, 2, 3, 4, 5, 6]));
+  }
+
+  createWinningNumbers(numbers) {
+    LottoValidator.validateNumbersLength(numbers);
+    LottoValidator.validateNumbersDuplicate(numbers);
+    LottoValidator.validateNumbersType(numbers);
+    LottoValidator.validateNumbersRange(numbers);
+
+    return numbers;
   }
 }
 
