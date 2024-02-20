b4b7ffed4431471ba537327b2c1ca98de758a0ce
anttiey <hanuuny@gmail.com>
2024-02-20 15:56:26 +0900

    test: 구입 금액에 해당하는 로또 발행 테스트 작성
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

---
+++ __tests__/LottoGame.test.js
@@ -0,0 +1,8 @@
+describe('로또 게임 기능 테스트', () => {
+  test('구입 금액에 해당하는 만큼 1장에 1,000원인 로또를 발행한다.', () => {
+    const money = 5000;
+    const game = new LottoGame();
+    const lottoTickets = game.createLotto(money);
+    expect(lottoTickets.length).toBe(5);
+  });
+});
