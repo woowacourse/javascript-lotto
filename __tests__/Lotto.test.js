725017c73cee11712097a3e920915dfddc15f8f4
lurgi <wahoo9040@naver.com>
2024-02-20 17:49:51 +0900

    refactor: Lotto 테스트 winNumbers -> winObj으로 수정
    Co-authored-by: brgndyy <brgndyy@gmail.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,20 +1,20 @@
-import Lotto from "./Lotto";
+import Lotto from "../src/Domain/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test.each([
     [
       {
         numbers: [1, 2, 3, 4, 5, 6],
-        winNumbers: [1, 2, 3, 4, 5, 6],
+        winObj: { winNumbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7 },
         rankExpected: 1,
       },
     ],
   ])(
     "로또 당첨번호를 입력받아서 등수를 계산한다",
-    ({ numbers, winNumbers, rankExpected }) => {
+    ({ numbers, winObj, rankExpected }) => {
       const lotto = new Lotto(numbers);
 
-      lotto.calculateRank(winNumbers);
+      lotto.calculateRank(winObj);
       expect(lotto.getRank()).toBe(rankExpected);
     }
   );
