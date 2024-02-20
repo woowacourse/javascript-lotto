0c9807946606a968810ca78b68ef193b75026139
ss0526100 <ss0526100@gmail.com>
2024-02-20 17:19:13 +0900

    feat(WinningLotto): 로또 번호 정수 타입 유효성 검증 / 보너스 번호 1 ~ 45 사이 정수 유효성 검증 추가

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -13,6 +13,49 @@ describe("WinningLotto에 대한 유닛 테스트", () => {
     }
   );
 
+  test.each([
+    ["1", "string"],
+    [0.1, "decimal"],
+    [-1, "negative int"],
+    [true, "boolean"],
+    [null, "null"],
+    [undefined, "undefined"],
+    [Symbol(1), "symbol"],
+    [BigInt(1), "BigInt"],
+    [{}, "object"],
+  ])("당첨 번호에 %s(%i)이 포함될 경우 예외 처리한다.", (value) => {
+    const numbers = [1, 2, 3, 4, 5, value];
+    const bonusNumber = 7;
+    const createWrongWinningLotto = () =>
+      new WinningLotto(numbers, bonusNumber);
+
+    //Assert
+    expect(createWrongWinningLotto).toThrow();
+  });
+
+  test.each([
+    ["1", "string"],
+    [0.1, "decimal"],
+    [-1, "negative int"],
+    [true, "boolean"],
+    [null, "null"],
+    [undefined, "undefined"],
+    [Symbol(1), "symbol"],
+    [BigInt(1), "BigInt"],
+    [{}, "object"],
+  ])(
+    "보너스 번호에 %s(%i)이 포함될 경우 예외 처리한다.",
+    (wrongBonusNumber) => {
+      const numbers = [1, 2, 3, 4, 5, 6];
+
+      const createWrongWinningLotto = () =>
+        new WinningLotto(numbers, wrongBonusNumber);
+
+      //Assert
+      expect(createWrongWinningLotto).toThrow();
+    }
+  );
+
   test.each([0, 46])(
     "당첨 번호는 1 ~ 45 사이의 정수가 아니면 예외 처리를 한다.",
     (number) => {
--- src/domain/WinningLotto.js
+++ src/domain/WinningLotto.js
@@ -3,9 +3,11 @@ import Lotto from "./Lotto";
 class WinningLotto {
   constructor(numbers, bonusNumber) {
     this.#validateNumbersInRange(numbers);
+    this.#validateInteger(bonusNumber);
     this.#validateNumberInRange(bonusNumber);
 
     this.#validateNumbersLength(numbers);
+    this.#validateIntegers(numbers);
   }
 
   #validateNumbersInRange(numbers) {
@@ -21,6 +23,16 @@ class WinningLotto {
   #validateNumbersLength(numbers) {
     if (numbers.length !== Lotto.NUMBERS_LENGTH) {
       throw new Error("[ERROR] 유효한 개수의 로또 숫자가 아닙니다");
+    }
+  }
+
+  #validateIntegers(numbers) {
+    numbers.forEach((number) => this.#validateInteger(number));
+  }
+
+  #validateInteger(number) {
+    if (!Number.isInteger(number)) {
+      throw new Error("[ERROR] 정수가 아닌 값입니다.");
     }
   }
 }
