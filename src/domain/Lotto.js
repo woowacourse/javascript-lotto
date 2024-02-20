6081b72fdad7e5b4ca9ea6cf81ebd80a9b10e881
jinsil <jinsil378900@gmail.com>
2024-02-20 17:43:10 +0900

    feat: Lotto 당첨, 보너스 번호 매칭 구현

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -5,6 +5,19 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  countMatchedNumbers(winningNumbers) {
+    return winningNumbers.reduce((result, winningNumber) => {
+      if (this.hasNumber(winningNumber)) {
+        result += 1;
+      }
+      return result;
+    }, 0);
+  }
+
+  hasNumber(number) {
+    return this.#numbers.includes(number);
+  }
+
   getNumbers() {
     return this.#numbers;
   }
