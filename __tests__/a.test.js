89d6b6cead7527709612ea6fbf2ebf5ce6ff0f97
ss0526100 <ss0526100@gmail.com>
2024-02-20 15:34:24 +0900

    feat: 로또 번호가 1 ~ 45 사이의 숫자인지 유효성 검증한다

--- __tests__/a.test.js
+++ __tests__/a.test.js
@@ -10,5 +10,18 @@ describe("로또에 대한 유닛 테스트", () => {
       //Assert
       expect(createWrongLotto).toThrow();
     });
+
+    test.each([0, 46])(
+      "로또 번호는 1 ~ 45 사이의 정수가 아니면 예외 처리를 한다.",
+      (number) => {
+        //Arrange
+        const numbers = [1, 2, 3, 4, 5, number];
+
+        const createWrongLotto = () => new Lotto(numbers);
+
+        //Assert
+        expect(createWrongLotto).toThrow();
+      }
+    );
   });
 });
--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,20 +1,31 @@
 class Lotto {
   static NUMBERS_LENGTH = 6;
+  static MIN_LOTTO_NUMBER = 1;
+  static MAX_LOTTO_NUMBER = 45;
 
   #numbers;
 
   constructor(numbers) {
     this.#validateNumbersLength(numbers);
+    this.#validateNumbersInRange(numbers);
+
     this.#numbers = [...numbers];
   }
 
   #validateNumbersLength(numbers) {
-    if (!this.#isValidNumbersLength(numbers))
-      throw new Error("[ERROR]유효한 개수의 로또 숫자가 아닙니다");
+    if (numbers.length !== Lotto.NUMBERS_LENGTH) {
+      throw new Error("[ERROR] 유효한 개수의 로또 숫자가 아닙니다");
+    }
   }
 
-  #isValidNumbersLength(numbers) {
-    return numbers.length === 6;
+  #validateNumbersInRange(numbers) {
+    numbers.forEach((number) => this.#validateNumberInRange(number));
+  }
+
+  #validateNumberInRange(number) {
+    if (number < Lotto.MIN_LOTTO_NUMBER || number > Lotto.MAX_LOTTO_NUMBER) {
+      throw new Error("[ERROR] 유효한 범위 로또 숫자가 아닙니다.");
+    }
   }
 }
 
