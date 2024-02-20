754e515b3a70dd98515f06b82d7ce401fc76e63e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-20 14:36:17 +0900

    test: 로또 번호 범위 에러 테스트
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- __tests__/domain/Lotto.test.js
+++ __tests__/domain/Lotto.test.js
@@ -1,7 +1,16 @@
+/* eslint-disable max-lines-per-function */
 import Lotto from "../../src/domain/Lotto.js";
 
 describe("Lotto 객체 테스트", () => {
   test("로또 번호는 중복되면 안된다.", () => {
-    expect(() => new Lotto([1, 1, 2, 3, 4, 5])).toThrow("[ERROR]");
+    const DUPLICATE_LOTTO_NUMBERS = [1, 1, 2, 3, 4, 5];
+
+    expect(() => new Lotto(DUPLICATE_LOTTO_NUMBERS)).toThrow("[ERROR]");
+  });
+
+  test("로또 번호는 1 ~ 45 사이의 숫자여야 한다.", () => {
+    const INVALID_LOTTO_NUMBERS = [0, 1, 2, 3, 4, 46];
+
+    expect(() => new Lotto(INVALID_LOTTO_NUMBERS)).toThrow("[ERROR]");
   });
 });
