a66ff7b9046f22e809e8c9370f67a7bc4d355da4
anttiey <hanuuny@gmail.com>
2024-02-20 17:04:47 +0900

    test: 변수 저장 방법 변경에 따른 테스트 삭제
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- __tests__/LottoGame.test.js
+++ __tests__/LottoGame.test.js
@@ -7,18 +7,4 @@ describe('로또 게임 기능 테스트', () => {
     const lottoTickets = game.createLotto(money);
     expect(lottoTickets.length).toBe(5);
   });
-
-  test('당첨 번호가 정상적으로 생성된다.', () => {
-    const numbers = [1, 2, 3, 4, 5, 6];
-    const game = new LottoGame();
-
-    expect(game.createWinningNumbers(numbers)).toEqual(numbers);
-  });
-
-  test('보너스 번호가 정상적으로 생성된다.', () => {
-    const number = 7;
-    const game = new LottoGame();
-
-    expect(game.createBonusNumber(number)).toEqual(number);
-  });
 });
