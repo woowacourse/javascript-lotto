685685f1347b6c4736936620bdeb6e41d977e72f
pakxe <pigkill40@naver.com>
2024-02-20 18:14:53 +0900

    feat: 로또 번호 문자열이 들어오면 쉼표로 나눠서 저장하는 로또 클래스 생성자 구현
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -3,13 +3,25 @@ import { generateRandomNumbers } from "../utils/generateRandomNumbers.js";
 const MIN_LOTTO_NUMBER = 1;
 const MAX_LOTTO_NUMBER = 45;
 const LOTTO_NUMBER_COUNT = 6;
+const SEPARATOR = ",";
 
 class Lotto {
   #numbers;
 
-  constructor() {
+  constructor(numbers) {
     // NOTE: 생성자의 타입이 다양할 때 이렇게 조건문을 걸어서 판별하는게 옳을까요?
-    // if(typeof numbers === 'string') numbers.split()
+    if (typeof numbers === "string") this.#constructorWithNumStr(numbers);
+    if (numbers === undefined) this.#constructorWithoutArg();
+  }
+
+  // 오버로딩
+  #constructorWithNumStr(numbers) {
+    const parsedNumbers = numbers.split(SEPARATOR).map((num) => Number(num));
+
+    this.#numbers = parsedNumbers;
+  }
+
+  #constructorWithoutArg() {
     this.#numbers = generateRandomNumbers(
       MIN_LOTTO_NUMBER,
       MAX_LOTTO_NUMBER,
@@ -20,6 +32,8 @@ class Lotto {
   getNumbers() {
     return this.#numbers;
   }
+
+  #validate(numbers) {}
 }
 
 export default Lotto;
