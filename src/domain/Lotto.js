6fdf0697d7ff15bca4dabcc7b643fb7e112fa79e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-20 18:01:22 +0900

    feat: 당첨 번호 비교
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,3 +1,4 @@
+import { PRIZE } from "../constants/system.js";
 import lottoNumberValidator from "../validator/LottoNumberValidator.js";
 
 class Lotto {
@@ -11,6 +12,35 @@ class Lotto {
 
   getNumbers() {
     return this.#numbers;
+  }
+
+  getMatchCount(winningLotto) {
+    const numberMatchCount = this.#numbers.filter((number) =>
+      winningLotto.getNumbers().includes(number),
+    ).length;
+
+    const isBonus = this.#numbers.includes(winningLotto.getBonusNumber());
+
+    console.log(
+      numberMatchCount,
+      isBonus,
+      this.#numbers,
+      winningLotto.getNumbers(),
+      winningLotto.getBonusNumber(),
+    );
+
+    return { numberMatchCount, isBonus };
+  }
+
+  getRank(winningLotto) {
+    const { numberMatchCount, isBonus } = this.getMatchCount(winningLotto);
+
+    if (numberMatchCount === 6) return PRIZE.FIRST;
+    if (numberMatchCount === 5 && isBonus) return PRIZE.SECOND;
+    if (numberMatchCount === 5) return PRIZE.THIRD;
+    if (numberMatchCount === 4) return PRIZE.FOURTH;
+    if (numberMatchCount === 3) return PRIZE.FIFTH;
+    return PRIZE.NONE;
   }
 }
 export default Lotto;
