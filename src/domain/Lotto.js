243ea820495426da0c8c84101ec6842dff0b87df
anttiey <hanuuny@gmail.com>
2024-02-20 14:58:50 +0900

    feat: 로또 번호 범위 유효성 검사 기능 추가
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -18,6 +18,10 @@ class Lotto {
     if (!numbers.every((number) => Number.isInteger(Number(number)))) {
       throw new Error('[ERROR] 로또 번호는 전부 정수여야 합니다.');
     }
+
+    if (!numbers.every((number) => number >= 1 && number <= 45)) {
+      throw new Error('[ERROR] 로또 번호는 전부 1에서 45 사이의 숫자여야 합니다.');
+    }
   }
 
   getNumbers() {
