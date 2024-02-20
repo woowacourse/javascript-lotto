3e0219f69bfd778830b48a83419aa4b95a427131
novice0840 <magnolia0840@gmail.com>
2024-02-20 19:55:33 +0900

    feat(lotto): 로또 번호 오름차순 반환

--- src/domain/lotto.js
+++ src/domain/lotto.js
@@ -1,7 +1,18 @@
 import { validateLotto } from '../utils/validation.js';
 
 export default class Lotto {
+  #numbers;
+
   constructor(numbers) {
     validateLotto(numbers);
+    this.#numbers = numbers;
+  }
+
+  get ascendingNumbers() {
+    const ascendingNumbers = this.#numbers
+      .map((number) => number)
+      .sort((prevNumber, nextNumber) => prevNumber - nextNumber);
+
+    return ascendingNumbers;
   }
 }
