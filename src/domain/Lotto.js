078c92f66b2ab92a982dc4e355bb0047a374ecad
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-20 14:36:00 +0900

    feat: 로또 번호 범위 체크
    Co-Authored-By: river <130737187+0jenn0@users.noreply.github.com>

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,10 +1,17 @@
 class Lotto {
   constructor(numbers) {
     this.#validateDuplicate(numbers);
+    this.#validateRange(numbers);
   }
 
   #validateDuplicate(numbers) {
     if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR]");
+    }
+  }
+
+  #validateRange(numbers) {
+    if (numbers.filter((number) => number < 1 || number > 45)) {
       throw new Error("[ERROR]");
     }
   }
