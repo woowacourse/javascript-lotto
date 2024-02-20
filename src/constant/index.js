7dd5dd0e41e54970e7908d031080a96bffefc6f9
novice0840 <magnolia0840@gmail.com>
2024-02-20 17:47:15 +0900

    refactor(constant, lotto): 로또 길이 상수처리

---
+++ src/constant/index.js
@@ -0,0 +1,3 @@
+export const LOTTO_RULES = {
+  length: 6,
+};
--- src/domain/lotto.js
+++ src/domain/lotto.js
@@ -1,6 +1,8 @@
+import { LOTTO_RULES } from '../constant/index.js';
+
 export default class Lotto {
   constructor(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== LOTTO_RULES.length) {
       throw new Error();
     }
   }
