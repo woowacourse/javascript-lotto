f4561a244fd18e3e3a8007be2f016c9c7f957936
anttiey <hanuuny@gmail.com>
2024-02-20 14:27:23 +0900

    test: 로또 번호 개수 유효성 테스트 작성
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

---
+++ __tests__/Lotto.test.js
@@ -0,0 +1,14 @@
+describe('로또 기능 테스트', () => {
+  test('로또 번호는 6개여야 한다.', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
+    expect(lotto.getNumbers().length).toBe(6);
+  });
+
+  test('로또 번호가 6개가 아니면 에러를 발생시킨다.', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7];
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow();
+  });
+});
