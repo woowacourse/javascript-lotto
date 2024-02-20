28720ae533bab71e9a0c18ee6f83588114ab7d6c
pakxe <pigkill40@naver.com>
2024-02-20 18:12:50 +0900

    test: 쉼표로 구분된 로또 번호 문자열이 들어오면 숫자 배열로 바꿔서 저장하는 기능 테스트 작성
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,8 +1,16 @@
 import Lotto from "../src/domain/Lotto.js";
 
 describe("로또 기능 테스트", () => {
-  test("숫자 6개를 가진 로또를 발행한다.", () => {
+  test("숫자 6개를 가진 로또를 생성한다.", () => {
     const lotto = new Lotto();
     expect(lotto.getNumbers()).toHaveLength(6);
+  });
+
+  test("쉼표로 구분된 로또 번호 문자열이 들어오면 숫자 배열로 바꿔서 저장한다.", () => {
+    const numbersStr = "1,9,12,15,28,45";
+    const lotto = new Lotto(numbersStr);
+    const expectNumbers = [1, 9, 12, 15, 28, 45];
+
+    expect(lotto.getNumbers()).toEqual(expectNumbers);
   });
 });
