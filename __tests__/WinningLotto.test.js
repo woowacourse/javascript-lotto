49a1263eb243248b04b7bcfebdc9bfcc45b37cff
ss0526100 <ss0526100@gmail.com>
2024-02-20 17:08:29 +0900

    feat(WinningLotto): 로또 번호 6개가 아닐 경우에 대한 유효성 검증 추가

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -1,6 +1,18 @@
 import WinningLotto from "../src/domain/WinningLotto";
 
 describe("WinningLotto에 대한 유닛 테스트", () => {
+  test.each([[[1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5, 6, 7]]])(
+    "당첨 번호의 개수가 6개가 아니면 예외 처리를 한다.",
+    (numbers) => {
+      const bonusNumber = 8;
+      const createWrongWinningLotto = () =>
+        new WinningLotto(numbers, bonusNumber);
+
+      //Assert
+      expect(createWrongWinningLotto).toThrow();
+    }
+  );
+
   test.each([0, 46])(
     "당첨 번호는 1 ~ 45 사이의 정수가 아니면 예외 처리를 한다.",
     (number) => {
--- src/domain/WinningLotto.js
+++ src/domain/WinningLotto.js
@@ -4,6 +4,8 @@ class WinningLotto {
   constructor(numbers, bonusNumber) {
     this.#validateNumbersInRange(numbers);
     this.#validateNumberInRange(bonusNumber);
+
+    this.#validateNumbersLength(numbers);
   }
 
   #validateNumbersInRange(numbers) {
@@ -13,6 +15,12 @@ class WinningLotto {
   #validateNumberInRange(number) {
     if (number < Lotto.MIN_LOTTO_NUMBER || number > Lotto.MAX_LOTTO_NUMBER) {
       throw new Error("[ERROR] 유효한 범위 로또 숫자가 아닙니다.");
+    }
+  }
+
+  #validateNumbersLength(numbers) {
+    if (numbers.length !== Lotto.NUMBERS_LENGTH) {
+      throw new Error("[ERROR] 유효한 개수의 로또 숫자가 아닙니다");
     }
   }
 }
