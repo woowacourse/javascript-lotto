95befe2a6fa3b93942aa83fc09226696172ce2fe
lurgi <wahoo9040@naver.com>
2024-02-20 16:23:23 +0900

    test: 로또 번호 중복 숫자 혹은 6개 넘을 시 에러
    Co-authored-by: brgndyy <brgndyy@gmail.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -10,4 +10,16 @@ describe("단일 로또에 대한 테스트", () => {
       expect(() => new Lotto(invalidLottoNumbers)).toThrow("❌");
     }
   );
+
+  test("로또 번호 내에서 중복 되는 숫자가 있을시에 예외 처리 된다.", () => {
+    const numbers = [1, 1, 2, 3, 4, 5];
+
+    expect(() => new Lotto(numbers)).toThrow("❌");
+  });
+
+  test("로또 번호가 6개를 넘어갈시에 예외 처리 된다.", () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+    expect(() => new Lotto(numbers)).toThrow("❌");
+  });
 });
