a4186efff16f1f2fc32976320cc7dccf28169f90
jaeml06 <jaeml0630@gmail.com>
2024-02-20 19:26:02 +0900

    test: 로또 구입 금액 유효성 검사 작성

---
+++ __tests__/validation.test.js
@@ -0,0 +1,12 @@
+import validation from '../src/validation/validation';
+
+describe('로또 구입 금액 유효성 검사', () => {
+  test.each([[0], [999], [-1000], [1000.5]])(
+    '유효하지 않은 구입 금액이면 에러 반환',
+    (purchaseAmount) => {
+      expect(() => validation.purchaseAmount(purchaseAmount)).toThrow(
+        '[ERROR]'
+      );
+    }
+  );
+});
