80c9e517ec1839949f8dbbed21bc41f4335004c9
chysis <chysiss@naver.com>
2024-02-20 19:40:27 +0900

    chore(lottoMaker.test): 테스트케이스명 수정

--- __tests__/lottoMaker.test.js
+++ __tests__/lottoMaker.test.js
@@ -1,6 +1,6 @@
 import LottoMaker from '../src/LottoMaker';
 
-describe('', () => {
+describe('로또 발행 테스트', () => {
   test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
     const PURCHASE_AMOUNT = 2000;
     const LOTTERY_AMOUNT = 2;
