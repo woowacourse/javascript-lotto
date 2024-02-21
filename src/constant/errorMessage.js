55d6c32c2af4292628f31c66a31e3dc934fc573a
jinsil <jinsil378900@gmail.com>
2024-02-21 21:19:51 +0900

    feat: errorMessage 작성

--- src/constant/errorMessage.js
+++ src/constant/errorMessage.js
@@ -1,5 +1,3 @@
-import { SETTING } from './setting';
-
 const PREFIX = '[ERROR]';
 const SUFFIX = '다시 입력해주세요.';
 
@@ -8,24 +6,11 @@ const generateErrorMessage = (message) => `${PREFIX} ${message} ${SUFFIX}`;
 const ERROR_MESSAGE = {
   INPUT_IS_EMPTY: generateErrorMessage('필수값입니다.'),
   INPUT_IS_NOT_NUMBER: generateErrorMessage('숫자로 입력해주셔야 합니다.'),
-  PURCHASE_AMOUNT_NOT_DIVIDED: generateErrorMessage(`${SETTING.LOTTO_PRICE} 단위의 숫자여야 합니다.`),
-  EXCEEDED_MAX_PURCHASE_AMOUNT: generateErrorMessage(
-    `${SETTING.MAX_LOTTO_PURCHASE_AMOUNT.toLocaleString()}원을 초과하여 입력하실 수 없습니다.`,
-  ),
-  LOTTO_NUMBER_LENGTH: generateErrorMessage(`쉼표로 구분되는 ${SETTING.LOTTO_LENGTH}개의 숫자여야 합니다.`),
-  LOTTO_NUMBER_RANGE: generateErrorMessage(
-    `각각의 숫자는 ${SETTING.MIN_LOTTO_NUMBER}~${SETTING.MAX_LOTTO_NUMBER} 사이의 숫자여야 합니다.`,
-  ),
+  PURCHASE_AMOUNT_NOT_DIVIDED: generateErrorMessage('1000 단위의 숫자여야 합니다.'),
+  LOTTO_NUMBER_LENGTH: generateErrorMessage('쉼표로 구분되는 6개의 숫자여야 합니다.'),
+  LOTTO_NUMBER_RANGE: generateErrorMessage('각각의 숫자는 1~45 사이의 숫자여야 합니다.'),
   LOTTO_NUMBER_FORMAT: generateErrorMessage('각각의 당첨 번호는 숫자여야 합니다.'),
   LOTTO_NUMBER_DUPLICATED: generateErrorMessage('서로 중복되지 않는 숫자여야 합니다.'),
-  BONUS_NUMBER_RANGE: generateErrorMessage(
-    `보너스 번호는 ${SETTING.MIN_LOTTO_NUMBER}~${SETTING.MAX_LOTTO_NUMBER} 사이의 숫자여야 합니다.`,
-  ),
-  BONUS_NUMBER_FORMAT: generateErrorMessage('보너스 번호는 숫자여야 합니다.'),
-  BONUS_NUMBER_DUPLICATED: generateErrorMessage('보너스 번호는 당첨 번호와 중복되지 않아야 합니다.'),
-  RESTART_COMMAND_FORMAT: generateErrorMessage(
-    `재시작 여부는 ${SETTING.RESTART_COMMAND} 또는 ${SETTING.EXIT_COMMAND}으로 입력하셔야 합니다.`,
-  ),
 };
 
 export default ERROR_MESSAGE;
