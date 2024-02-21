ea56b5acfd0a12c94811c09f2b1c3bd639fcc22c
novice0840 <magnolia0840@gmail.com>
2024-02-21 16:21:46 +0900

    feat(random): 숫자 범위 내에서 지정된 개수만큼 겹치지 않는 숫자를 반환 함수 추가

---
+++ src/utils/random.js
@@ -0,0 +1,14 @@
+const Random = {
+  pickUniqueNumbersInRange({ minNumber, maxNumber, count }) {
+    if (count > maxNumber - minNumber + 1) throw new Error('Cannot generate unique numbers. Count exceeds range.');
+    const uniqueNumbers = new Set();
+
+    while (uniqueNumbers.size < count) {
+      uniqueNumbers.add(Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber);
+    }
+
+    return Array.from(uniqueNumbers);
+  },
+};
+
+export default Random;
