801db72e8902474d5daacf7e4a760452280af08f
anttiey <hanuuny@gmail.com>
2024-02-21 15:48:59 +0900

    feat: 구입 금액 단위 유효성 검사 기능 추가
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/domain/MoneyValidator.js
+++ src/domain/MoneyValidator.js
@@ -10,6 +10,12 @@ const MoneyValidator = {
       throw new Error('[ERROR] 구입 금액은 0보다 커야합니다.');
     }
   },
+
+  validateMoneyUnit(money) {
+    if (money % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1000원 단위여야 합니다.');
+    }
+  },
 };
 
 export default MoneyValidator;
