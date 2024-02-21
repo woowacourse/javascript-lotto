1fbfd522fb084b8b5ffbda1ec4dc80d69dfce803
novice0840 <magnolia0840@gmail.com>
2024-02-21 17:55:07 +0900

    feat(lottoMachine): 구입 금액에 따라 알맞은 개수의 로또를 발행한다.

--- src/domain/lottoMachine.js
+++ src/domain/lottoMachine.js
@@ -1,3 +1,5 @@
+import Random from '../utils/random.js';
+import Lotto from './lotto.js';
 import { LOTTO_RULES } from '../constant/index.js';
 
 export default class LottoMachine {
@@ -9,5 +11,20 @@ export default class LottoMachine {
 
   get getLottoCount() {
     return this.#cost / LOTTO_RULES.cost;
+  }
+
+  #makeOneLotto() {
+    const lotto = new Lotto(
+      Random.pickUniqueNumbersInRange({
+        minNumber: LOTTO_RULES.min_number,
+        maxNumber: LOTTO_RULES.max_number,
+        count: LOTTO_RULES.length,
+      }),
+    );
+    return lotto;
+  }
+
+  makeLottos(count) {
+    return Array.from({ length: count }, () => this.#makeOneLotto());
   }
 }
