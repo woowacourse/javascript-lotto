c79c05244a5d8790da1fdbd292809a4d4664edf1
jaeml06 <jaeml0630@gmail.com>
2024-02-21 18:49:37 +0900

    feat: 당첨 번호를 입력받는 함수 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,10 +3,17 @@ import Console from '../util/Console';
 
 const InputView = {
   async askPurchaseAmount() {
-    const purchaseAmount = await Console.readLineAsync(
-      MESSAGE.QUERY.PURCHASE_AMOUNT
-    );
+    const purchaseAmount = await Console.readLineAsync(MESSAGE.QUERY.PURCHASE_AMOUNT);
     return purchaseAmount;
+  },
+
+  async askWinNumbers() {
+    const winNumbers = await Console.readLineAsync(MESSAGE.QUERY.WIN_NUMBERS);
+    return winNumbers.split(',').map((value) => Number(value));
+  },
+  async askBonusNumber() {
+    const bonusNumber = await Console.readLineAsync(MESSAGE.QUERY.BONUS_NUMBER);
+    return bonusNumber;
   },
 };
 
