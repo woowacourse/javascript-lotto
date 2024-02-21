148395dcc594583ec5ab314e28a48df4abefe1fd
GimGayeon <kky1373@naver.com>
2024-02-21 17:31:28 +0900

    feat:당첨 통계 객체의 로또 일치 개수 증가 기능 구현
    co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/domains/LottoCalculator.js
+++ src/domains/LottoCalculator.js
@@ -1,14 +1,63 @@
+import { LOTTO_STATICS } from '../constant/lotto-statics.js';
 class LottoCalculator {
-  constructor(lottoNumbers, generatedLottos) {}
+  #lottoStatics;
 
-  compare(winningNumbers, generatedLottos) {
+  constructor(lottoNumbers, generatedLottos) {
+    this.#lottoStatics = {
+      three: 0,
+      four: 0,
+      five: 0,
+      fiveBonus: 0,
+      six: 0,
+    };
+    this.#calculateAllLottoStatics(lottoNumbers, generatedLottos);
+  }
+
+  compare(winningNumbers, generatedLotto) {
     return winningNumbers.filter((winningNumber) =>
-      generatedLottos.includes(winningNumber),
+      generatedLotto.includes(winningNumber),
     ).length;
   }
 
-  isEqualBonusNumber(bonusNumber, generatedLottos) {
-    return generatedLottos.includes(bonusNumber);
+  isEqualBonusNumber(bonusNumber, generatedLotto) {
+    return generatedLotto.includes(bonusNumber);
+  }
+
+  #increaseLottoCount(number) {
+    Object.keys(LOTTO_STATICS).forEach((key) => {
+      if (LOTTO_STATICS[key].number === number) {
+        this.#lottoStatics[key]++;
+      }
+    });
+  }
+
+  #calculateLottoStatics(lottoNumbers, generatedLotto) {
+    const { winningNumbers, bonusNumber } = lottoNumbers;
+    const count = this.compare(winningNumbers, generatedLotto);
+
+    if (count === 5) {
+      this.#increaseFiveOrBonus(bonusNumber, generatedLotto);
+      return;
+    }
+    this.#increaseLottoCount(count);
+  }
+
+  #calculateAllLottoStatics(lottoNumbers, generatedLottos) {
+    for (let i = 0; i < generatedLottos[0].length; i++) {
+      this.#calculateLottoStatics(lottoNumbers, generatedLottos[i]);
+    }
+  }
+
+  #increaseFiveOrBonus(bonusNumber, generatedLottos) {
+    if (this.isEqualBonusNumber(bonusNumber, generatedLottos)) {
+      this.#lottoStatics.fiveBonus++;
+      return;
+    }
+    this.#lottoStatics.five++;
+  }
+
+  get lottoStatics() {
+    return this.#lottoStatics;
   }
 }
 
