6a9f71095e0255046cbda631919f9fb8205a574d
jinsil <jinsil378900@gmail.com>
2024-02-21 21:18:59 +0900

    feat: 당첨 번호 검증 유효성 검사

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -7,7 +7,17 @@ const Validator = {
     this.purchaseAmountNotDivided(purchaseAmount);
   },
 
-  validateWinningNumbers(winningNumbers) {},
+  validateWinningNumbers(winningNumbers) {
+    this.checkEmpty(winningNumbers);
+    this.checkNumbersLength(winningNumbers);
+
+    const winningNumberList = winningNumbers.split(',').map((number) => number.trim());
+    this.checkNumbersDuplicated(winningNumberList);
+    winningNumberList.forEach((number) => {
+      this.checkLottoNotNumber(number);
+      this.checkNumbersRange(number);
+    });
+  },
 
   validateBonusNumber(bonusNumber, winningNumbers) {},
 
@@ -28,6 +38,31 @@ const Validator = {
   purchaseAmountNotDivided(input) {
     if (input === 0 || input % 1000 !== 0) {
       throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_NOT_DIVIDED);
+    }
+  },
+
+  checkNumbersLength(input) {
+    const numbers = input.split(',');
+    if (numbers.length !== 6) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_LENGTH);
+    }
+  },
+
+  checkNumbersDuplicated(input) {
+    if (new Set(input).size !== 6) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_DUPLICATED);
+    }
+  },
+
+  checkLottoNotNumber(input) {
+    if (!Number.isInteger(Number(input))) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_FORMAT);
+    }
+  },
+
+  checkNumbersRange(input) {
+    if (Number(input) < 1 || Number(input) > 45) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBER_RANGE);
     }
   },
 };
