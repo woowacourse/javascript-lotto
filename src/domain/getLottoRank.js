6800ac45aec5ea57aa964a2235e8cf9ded1b4179
pakxe <pigkill40@naver.com>
2024-02-21 19:47:00 +0900

    feat: 로또 등수를 반환하는 함수 구현
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

---
+++ src/domain/getLottoRank.js
@@ -0,0 +1,31 @@
+import { getMatchCount } from "../utils/getMatchCount.js";
+import { isIncludingValue } from "../utils/isIncludingValue.js";
+import Lotto from "./Lotto.js";
+
+const prize = [1, 2, 3, 4, 5];
+const START_MATCH_COUNT = 3;
+
+function getRankIndex(matchCount, isBonus) {
+  if (matchCount === Lotto.NUMBER_COUNT) return prize.length - 1;
+  if (matchCount === Lotto.NUMBER_COUNT - 1 && isBonus) return prize.length - 2;
+  return matchCount - 3;
+}
+
+function getLottoRank({ winningLotto, bonusLottoNumber, randomLottos }) {
+  const ranks = Array.from({ length: 5 }, () => 0);
+
+  randomLottos.forEach((randomLotto) => {
+    const matchCount = getMatchCount(randomLotto.get(), winningLotto.get());
+    const isBonus = isIncludingValue(randomLotto.get(), bonusLottoNumber.get());
+
+    if (matchCount < START_MATCH_COUNT) return;
+
+    const rankIndex = getRankIndex(matchCount, isBonus);
+
+    ranks[rankIndex] += 1;
+  });
+
+  return ranks;
+}
+
+export default getLottoRank;
