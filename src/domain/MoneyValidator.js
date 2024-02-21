afabdc9710d7a131360b56ae8ba3271b884db5d9
anttiey <hanuuny@gmail.com>
2024-02-21 15:46:54 +0900

    feat: 구입 금액 최솟값 검사 기능 추가
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- src/domain/MoneyValidator.js
+++ src/domain/MoneyValidator.js
@@ -4,6 +4,12 @@ const MoneyValidator = {
       throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
     }
   },
+
+  validateMoneyMinimum(money) {
+    if (money <= 0) {
+      throw new Error('[ERROR] 구입 금액은 0보다 커야합니다.');
+    }
+  },
 };
 
 export default MoneyValidator;
