ae5f4b335c6354eacea2f7af39cc5475f32965f8
pakxe <pigkill40@naver.com>
2024-02-21 21:45:40 +0900

    feat: 등수 배열을 받아 총 상금을 반환하는 함수 구현
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

---
+++ src/domain/calculateTotalPrize.js
@@ -0,0 +1,8 @@
+const PRIZE = [5000, 50000, 1500000, 30000000, 2000000000];
+// const로 빼기
+
+function calculateTotalPrize(ranks) {
+  return ranks.reduce((acc, curr, index) => acc + curr * PRIZE[index], 0);
+}
+
+export { PRIZE, calculateTotalPrize };
