4b9dcd4736682dc530fc94fa439572ea38d436fe
anttiey <hanuuny@gmail.com>
2024-02-21 20:26:15 +0900

    test: 재시작 또는 종료 여부 문자 유효성 테스트 작성
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

---
+++ __tests__/SystemValidator.test.js
@@ -0,0 +1,7 @@
+describe('로또 게임 재시작/종료 여부 입력 유효성 테스트', () => {
+  test('재시작/종료 여부 입력 문자가 y/n이 아니라면 에러를 발생시킨다.', () => {
+    const restartOption = '파슬리';
+
+    expect(() => SystemValidator.validateOptionCharacter(restartOption)).toThrow('[ERROR]');
+  });
+});
