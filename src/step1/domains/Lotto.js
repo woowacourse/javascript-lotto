8de782f933cf04d681194980fa5c46c13c17ae64
hwinkr <dnddl8280@naver.com>
2024-02-21 22:07:36 +0900

    feat(Lotto): 로또 도메인 구현
    - 로또 번호 유효성 검증
    - 정답 로또 번호와 몇개 일치하는지 판단 로직 구현
    
    Co-authored-by: hain-tain <hain7ce@gmail.com>

--- src/step1/domains/Lotto.js
+++ src/step1/domains/Lotto.js
@@ -10,22 +10,33 @@ class Lotto {
     const regex = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
 
     if (!numbers.every((number) => regex.test(number))) {
-      throw new Error("에러다");
+      throw new Error("[ERROR]");
     }
     if (numbers.length !== 6) {
-      throw new Error("에러다");
+      throw new Error("[ERROR]");
     }
     if (new Set(numbers).size !== numbers.length) {
-      throw new Error("에러다");
+      throw new Error("[ERROR]");
     }
   }
 
-  countMatchedNumber(answer) {
+  #countMatchedNumber(answer) {
     return this.#numbers.filter((number) => answer.includes(number)).length;
   }
 
-  hasBonusNumber(bonusNumber) {
+  hasNumber(bonusNumber) {
     return this.#numbers.includes(bonusNumber);
+  }
+
+  getMatchedInfo(answer, bonusNumber) {
+    const matchedCount = this.#countMatchedNumber(answer);
+    const hasBonusNumber = this.hasNumber(bonusNumber);
+
+    return { matchedCount, hasBonusNumber };
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
   }
 }
 
