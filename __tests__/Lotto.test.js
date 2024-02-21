afa9e77501645374740f2c3fcc2a1c06e2ebac67
lurgi <wahoo9040@naver.com>
2024-02-21 13:57:47 +0900

    refactor: Lotto 테스트 명세 수정
    Co-authored-by: JEON TAEHEON <brgndyy@users.noreply.github.com정

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -2,15 +2,33 @@ import Lotto from "../src/Domain/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test.each([
-    [
-      {
-        numbers: [1, 2, 3, 4, 5, 6],
-        winObj: { winNumbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7 },
-        rankExpected: 1,
-      },
-    ],
+    {
+      numbers: [1, 2, 3, 4, 5, 6],
+      winObj: { winNumbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7 },
+      rankExpected: 1,
+    },
+    {
+      numbers: [1, 2, 3, 4, 5, 6],
+      winObj: { winNumbers: [1, 2, 3, 4, 5, 7], bonusNumber: 6 },
+      rankExpected: 2,
+    },
+    {
+      numbers: [1, 2, 3, 4, 5, 45],
+      winObj: { winNumbers: [1, 2, 3, 4, 5, 7], bonusNumber: 6 },
+      rankExpected: 3,
+    },
+    {
+      numbers: [11, 23, 33, 41, 44, 45],
+      winObj: { winNumbers: [11, 23, 41, 42, 43, 44], bonusNumber: 6 },
+      rankExpected: 4,
+    },
+    {
+      numbers: [1, 2, 3, 7, 8, 9],
+      winObj: { winNumbers: [1, 2, 3, 10, 15, 22], bonusNumber: 45 },
+      rankExpected: 5,
+    },
   ])(
-    "로또 당첨번호를 입력받아서 등수를 계산한다",
+    "로또 당첨번호를 입력받아서 $rankExpected 등수를 계산한다",
     ({ numbers, winObj, rankExpected }) => {
       const lotto = new Lotto(numbers);
 
