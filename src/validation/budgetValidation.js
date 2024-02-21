8d6ed49696098e07b6f884c32a05fd53d14df680
ImxYJL <allensain14@gmail.com>
2024-02-21 18:04:40 +0900

    feat: 로또 구입 금액 입력 구현
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

--- src/validation/budgetValidation.js
+++ src/validation/budgetValidation.js
@@ -11,19 +11,35 @@ const budgetValidation = {
     },
 
     outOfRange: {
+<<<<<<< HEAD
       errorMessage: ERROR_MESSAGE.BUDGET_RANGE,
       isValid(input) {
         return input >= LOTTO_SETTING.PRICE && input <= LOTTO_SETTING.MAX_PRICE;
+=======
+      errorMessage: ERROR_MESSAGE.outOfRange,
+      isValid(input) {
+        return input >= 1000 && input <= 100000;
+>>>>>>> e57b0b7 (feat: 로또 구입 금액 입력 구현)
       },
     },
 
     divisibleByMinimumPrice: {
+<<<<<<< HEAD
       errorMessage: ERROR_MESSAGE.DIVISIBLE_BY_MIN_PRICE,
       isValid(input) {
         return input % LOTTO_SETTING.PRICE === 0;
+=======
+      errorMessage: ERROR_MESSAGE.divisibleByMinimumPrice,
+      isValid(input) {
+        return input % 1000 === 0;
+>>>>>>> e57b0b7 (feat: 로또 구입 금액 입력 구현)
       },
     },
   },
 };
 
+<<<<<<< HEAD
 export default budgetValidation;
+=======
+export default BudgetValidation;
+>>>>>>> e57b0b7 (feat: 로또 구입 금액 입력 구현)
