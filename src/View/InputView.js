b231ab278b8caa11a26735cc0f0baab216ebab59
lurgi <wahoo9040@naver.com>
2024-02-21 17:45:42 +0900

    fix: 함수 표현식 수정
    Co-authored-by: brgndyy <brgndyy@gmail.com>

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,40 +1,41 @@
-import readline from "readline";
+import readLine from "readline";
 
 const MESSAGE = Object.freeze({
   MONEY: "구입금액을 입력해 주세요.",
   NO_INPUT: "입력 값을 적어주세요.",
-  WIN_LOTTO: "당첨 번호를 입력해주세요."
+  WIN_LOTTO: "당첨 번호를 입력해주세요.",
 });
 
 const InputView = {
-  readMoney: async () => {
+  async readMoney() {
     const moneyInput = await this.readLineAsync(MESSAGE.MONEY);
     this.validInput(moneyInput);
     return Number(moneyInput);
   },
 
-  readWinLottoNumbers: async () => {
+  async readWinLottoNumbers() {
     const winLottoInput = await this.readLineAsync(MESSAGE.WIN_LOTTO);
-    this.validInput(winLottoInput)
-    return winLottoInput.split(",")
-  }
+    this.validInput(winLottoInput);
+    return winLottoInput.split(",");
+  },
 
-  validInput(input) {
+  async validInput(input) {
     if (!input) throw new Error(MESSAGE.NO_INPUT);
   },
 
-  readLineAsync: async (message) =>
-    new Promise((resolve) => {
-      const rl = readline.createInterface({
+  async readLineAsync(message) {
+    return new Promise((resolve) => {
+      const rl = readLine.createInterface({
         input: process.stdin,
         output: process.stdout,
       });
 
       rl.question(message, (input) => {
-        rl.close();
         resolve(input);
+        rl.close();
       });
-    }),
+    });
+  },
 };
 
 export default InputView;
