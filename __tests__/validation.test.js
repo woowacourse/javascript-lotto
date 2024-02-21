dea8106342bb41d4eb337d76ddcb882920120c53
jaeml06 <jaeml0630@gmail.com>
2024-02-21 20:04:40 +0900

    test: 재시작 응답 유효성 테스트 추가

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,7 +1,11 @@
 import PurchaseAmountValidation from '../src/validation/purchaseAmount';
+import RestartResponseValidation from '../src/validation/responseValidation';
 
-describe('로또 구입 금액 유효성 검사', () => {
+describe('유효성 관련 함수 테스트', () => {
   test.each([['0'], ['999'], ['-1000'], ['1000.5']])('유효하지 않은 구입 금액이면 에러 반환', (money) => {
     expect(() => PurchaseAmountValidation.validate(money)).toThrow('[ERROR]');
+  });
+  test.each(['yes', '1', 1, '!'])('유효하지 않은 재시작 응답시 에러 반환', (restartResponse) => {
+    expect(() => RestartResponseValidation.validate(restartResponse)).toThrow('[ERROR]');
   });
 });
