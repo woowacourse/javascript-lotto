219557f8c2c26198d9bb8d191c2824e7cd173fd6
pakxe <pigkill40@naver.com>
2024-02-21 15:29:08 +0900

    test: 로또 번호 유효성 검사 테스트 작성
    입력된 각 로또 번호는 1이상 45이하의 정수가 아니면 오류를 발생시킨다.
    
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/domain/Lotto.js";
+import LottoNumber from "../src/domain/LottoNumber.js";
 
 describe("로또 기능 테스트", () => {
   test("숫자 6개를 가진 로또를 생성한다.", () => {
@@ -13,4 +14,17 @@ describe("로또 기능 테스트", () => {
 
     expect(lotto.getNumbers()).toEqual(expectNumbers);
   });
+});
+
+describe("로또 번호 유효성 테스트", () => {
+  const strValue = [" ", "a"];
+  const floatValue = 0.4;
+  const boundaryValue = [0, 46];
+
+  test.each([strValue, floatValue, ...boundaryValue])(
+    "입력된 각 로또 번호는 1이상 45이하의 정수가 아니면 오류를 발생시킨다.",
+    (wrongValue) => {
+      expect(() => new LottoNumber(wrongValue)).toThrow("[ERROR]");
+    }
+  );
 });
