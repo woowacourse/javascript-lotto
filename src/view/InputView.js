49412d27545e113ec90a2b3cab67cbd2e9f4d0eb
soosoo22 <lllllh00@naver.com>
2024-02-21 19:58:59 +0900

    docs: 구입 금액 유효성 검사 항목 수정
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -4,8 +4,10 @@ class InputView {
   static async readPurchaseAmount() {
     return await Console.readLineAsync('구입금액을 입력해 주세요.');
   }
+
+  static async readWinningNumber() {
+    return await Console.readLineAsync('당첨 번호를 입력해 주세요. ');
+  }
 }
 
 export default InputView;
-
-const input = await InputView.readPurchaseAmount();
