8711673a155a4308a5a4e90e8602f9c0ae55d147
jaeml06 <jaeml0630@gmail.com>
2024-02-21 19:10:17 +0900

    feat: Controller 기능 구현
    - 당첨 번호 유효할때까지 재입력 받는 함수 구현
    -보너스 번호 유효할때까지 재입력 받는 함수 구현
    -수익률을 계산하는 함수 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,24 +1,37 @@
-import { NUMBER } from '../constants/number';
+import NUMBER from '../constants/number';
 import Lotto from '../domain/Lotto';
+import LottoProcess from '../domain/LottoProcess';
 import Random from '../util/Random';
-import validation from '../validation/validation';
 import InputView from '../view/InputView';
 import OutputView from '../view/OutputView';
+import LottoValidation from '../validation/lottoValidation';
+import PurchaseAmountValidation from '../validation/purchaseAmount';
 
 class LottoController {
   async play() {
     const lottoCount = await this.getValidateLottoAmount();
     const lottos = this.getLottos(lottoCount);
+    const lottoProcess = new LottoProcess(lottos);
+    const lottosNumbers = lottoProcess.getAllLottosNumbers();
+
     OutputView.printLottoCount(lottoCount);
+    OutputView.printRandomLottos(lottosNumbers);
+
+    const winLotto = new Lotto(await this.getValidateWinNumbers());
+    const winNumbers = winLotto.getNumbers();
+    const bonusNumber = await this.getValidateBonusNumber(winNumbers);
+    const result = lottoProcess.getResult(winLotto, bonusNumber);
+    OutputView.printResultTitle();
+    OutputView.printWinningStatistics(result);
   }
 
   async getValidateLottoAmount() {
     const purchaseAmount = await InputView.askPurchaseAmount();
     try {
-      validation.purchaseAmount(purchaseAmount);
+      PurchaseAmountValidation.validate(purchaseAmount);
     } catch (error) {
       OutputView.printError(error.message);
-      this.getValidateLottoAmount();
+      await this.getValidateLottoAmount();
     }
     return Number.parseInt(purchaseAmount / NUMBER.LOTTO_PRICE, 10);
   }
@@ -32,6 +45,36 @@ class LottoController {
       return this.getRandomNumbers();
     }
     return [...lottoNumbers];
+  }
+
+  async getValidateWinNumbers() {
+    const winNumbers = await InputView.askWinNumbers();
+    try {
+      LottoValidation.validateNumbers(winNumbers);
+    } catch (error) {
+      OutputView.printError(error.message);
+      await this.getValidateWinNumbers();
+    }
+    return winNumbers;
+  }
+
+  async getValidateBonusNumber(winNumbers) {
+    const bonusNumber = await InputView.askBonusNumber();
+    try {
+      LottoValidation.validateBonusNumber(winNumbers, bonusNumber);
+    } catch (error) {
+      OutputView.printError(error.message);
+      await this.getValidateBonusNumber();
+    }
+  }
+
+  getRateOfRevenue(result = 0, lottoCount = 0) {
+    const revenue = result.reduce((acc, cur) => {
+      const [, , price, winCount] = cur;
+      return acc + price * winCount;
+    }, 0);
+
+    return ((revenue / (lottoCount * NUMBER.LOTTO_PRICE)) * 100).toFixed(1);
   }
 
   getLottos(count) {
