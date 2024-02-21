a6d098b3bf7372467074d7cd6bf2829bbf4f6b69
jaeml06 <jaeml0630@gmail.com>
2024-02-21 18:42:11 +0900

    feat: 구입 금액 유효성 검사

---
+++ src/validation/purchaseAmount.js
@@ -0,0 +1,12 @@
+import MESSAGE from '../constants/message';
+import NUMBER from '../constants/number';
+
+const PurchaseAmountValidation = {
+  validate(moneyString) {
+    const money = Number(moneyString);
+    if (money % NUMBER.LOTTO_PRICE !== 0 || !Number.isInteger(money) || money < NUMBER.LOTTO_PRICE)
+      throw new Error(MESSAGE.ERROR.PURCHASE_AMOUNT_ERROR);
+  },
+};
+
+export default PurchaseAmountValidation;
