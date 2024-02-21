9f1c210b59ef9f0053b9699dc40bcd7539df5012
pakxe <pigkill40@naver.com>
2024-02-21 18:57:39 +0900

    feat: 두 배열의 일치하는 요소 개수를 반환하는 함수 구현
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

---
+++ src/utils/getMatchCount.js
@@ -0,0 +1,9 @@
+function getMatchCount(arr1, arr2) {
+  const uniqueArr = new Set([...arr1, ...arr2]);
+
+  const totalLength = arr1.length + arr2.length;
+
+  return totalLength - uniqueArr.size;
+}
+
+export { getMatchCount };
