6dc75b0eb2cbfe79d8dff686f6411df142c97975
pakxe <pigkill40@naver.com>
2024-02-21 16:33:23 +0900

    refactor: default export 대신 export로 수정
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/utils/generateRandomNumbers.js
+++ src/utils/generateRandomNumbers.js
@@ -15,4 +15,6 @@ function generateRandomNumbers(min, max, count) {
   return [...uniqueNumbers];
 }
 
+export { generateRandomNumbers };
+
 // NOTE: const만 사용하며 내장 함수만 활용하기 위해 for문을 O(2n)의 시간복잡도로 구현하는 것이 맞는지, 혹은 let을 사용하더라도 O(n)의 시간복잡도로 끝내는 것이 맞는지 궁금합니다.
