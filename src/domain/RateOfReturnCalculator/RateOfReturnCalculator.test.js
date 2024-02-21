5611128b126f94450be8bbbbb93ff439eed6ca8b
손진영 <sm099@naver.com>
2024-02-21 21:46:15 +0900

    test(RateOfReturnCalculator): 수익률 계산 테스트 수정
    1. numeric operator 추가
    
    2. winningRankDetail 데이터 형식 수정
    
    3. expectedRateOfReturn이 .0으로 포맷되지 않는 문제로 인해 문자열로 변경
    
    4. 테스트 케이스 이름 변경
    
    5. matcher를 toMatch로 변경
    
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/domain/RateOfReturnCalculator/RateOfReturnCalculator.test.js
+++ src/domain/RateOfReturnCalculator/RateOfReturnCalculator.test.js
@@ -1,35 +1,37 @@
+import RateOfReturnCalculator from './RateOfReturnCalculator';
+
 describe('수익률 계산 테스트', () => {
   // given
   const TEST_CASE = [
     {
-      buyLottoPrice: 3000,
-      winningRankDetail: { 1: 1, 2: 0, 3: 0, 4: 0, 5: 0 },
-      expectedRateOfReturn: 66666666.7,
+      buyLottoPrice: 3_000,
+      winningRankDetail: { '1st': 1, '2nd': 0, '3rd': 0, '4th': 0, '5th': 0 },
+      expectedRateOfReturn: '66666666.7',
     },
     {
-      buyLottoPrice: 9000,
-      winningRankDetail: { 1: 0, 2: 1, 3: 0, 4: 0, 5: 0 },
-      expectedRateOfReturn: 333333.3,
+      buyLottoPrice: 9_000,
+      winningRankDetail: { '1st': 0, '2nd': 1, '3rd': 0, '4th': 0, '5th': 0 },
+      expectedRateOfReturn: '333333.3',
     },
     {
-      buyLottoPrice: 8000,
-      winningRankDetail: { 1: 0, 2: 0, 3: 1, 4: 0, 5: 0 },
-      expectedRateOfReturn: 18750.0,
+      buyLottoPrice: 8_000,
+      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 1, '4th': 0, '5th': 0 },
+      expectedRateOfReturn: '18750.0',
     },
     {
-      buyLottoPrice: 3000,
-      winningRankDetail: { 1: 0, 2: 0, 3: 0, 4: 1, 5: 0 },
-      expectedRateOfReturn: 16666.7,
+      buyLottoPrice: 3_000,
+      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 1, '5th': 0 },
+      expectedRateOfReturn: '1666.7',
     },
     {
-      buyLottoPrice: 1000,
-      winningRankDetail: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 },
-      expectedRateOfReturn: 500.0,
+      buyLottoPrice: 1_000,
+      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 0, '5th': 1 },
+      expectedRateOfReturn: '500.0',
     },
   ];
 
   test.each(TEST_CASE)(
-    '로또 구매 금액이 $buyLottoPrice이고 등수 정보가 $winningRankDetail일 때 수익률은 $expectedRateOfReturn% 이다.',
+    '로또 구매 금액이 $buyLottoPrice원 이고 등수 정보가 $winningRankDetail일 때 수익률은 $expectedRateOfReturn% 이다.',
     ({ buyLottoPrice, winningRankDetail, expectedRateOfReturn }) => {
       const rateOfReturnCalculator = new RateOfReturnCalculator({
         buyLottoPrice,
@@ -40,7 +42,7 @@ describe('수익률 계산 테스트', () => {
       const rateOfReturn = rateOfReturnCalculator.execute();
 
       // then
-      expect(rateOfReturn).toBeCloseTo(expectedRateOfReturn, 1);
+      expect(rateOfReturn).toMatch(expectedRateOfReturn);
     },
   );
 });
