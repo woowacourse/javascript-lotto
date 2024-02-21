0557c666235b443a036b8413f1a9318d68a5b461
chysis <chysiss@naver.com>
2024-02-21 18:31:30 +0900

    chore(LotteryMachine): 파일 이름 수정

--- __tests__/lotteryMachine.test.js
+++ __tests__/lotteryMachine.test.js
@@ -1,12 +1,12 @@
-import LottoMaker from '../src/LottoMaker';
+import LotteryMachine from '../src/LotteryMachine';
 
 describe('로또 발행 테스트', () => {
   test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
     const PURCHASE_AMOUNT = 2000;
     const LOTTERY_AMOUNT = 2;
 
-    const lottoMaker = new LottoMaker(PURCHASE_AMOUNT);
-    const lottoNumberList = lottoMaker.run();
+    const lotteryMachine = new LotteryMachine(PURCHASE_AMOUNT);
+    const lottoNumberList = lotteryMachine.makeLottery();
 
     expect(lottoNumberList.length).toEqual(LOTTERY_AMOUNT);
   });
--- src/LotteryMachine.js
+++ src/LotteryMachine.js
@@ -1,23 +1,25 @@
-class LottoMaker {
+import Lotto from './Lotto';
+
+class LotteryMachine {
   #amount;
 
   constructor(purchaseAmount) {
     this.#amount = parseInt(purchaseAmount, 10) / 1000;
   }
 
-  run() {
-    return Array.from({ length: this.#amount }).map(() => this.make());
+  makeLottery() {
+    return Array.from({ length: this.#amount }).map(() => this.makeLotto());
   }
 
-  make() {
+  makeLotto() {
     const lotto = [];
     while (lotto.length < 6) {
       const randomNumber = Math.ceil(Math.random() * 45);
       if (lotto.includes(randomNumber)) continue;
       lotto.push(randomNumber);
     }
-    return lotto;
+    return new Lotto(lotto);
   }
 }
 
-export default LottoMaker;
+export default LotteryMachine;
