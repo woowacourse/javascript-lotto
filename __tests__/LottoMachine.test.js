fd87bfcfd2f6fc184a20ab6d15b16a4e3573d67f
lurgi <wahoo9040@naver.com>
2024-02-21 14:53:20 +0900

    test: 1000원 미만 입력인 경우 에러를 던진다

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -6,12 +6,19 @@ describe("로또 머신에 대한 테스트", () => {
     [8500, 8],
   ])("구입금액에 해당하는 만큼 로또를 발행한다.", (money, count) => {
     // arrange
-    const lottoMachine = new LottoMachine();
+    const lottoMachine = new LottoMachine(money);
     // action
-    const result = lottoMachine.makeLottoByMoney(money);
+    const result = lottoMachine.getLottos();
     // assert
     expect(result.map((lotto) => lotto.constructor.name)).toStrictEqual(
       Array.from({ length: count }, () => "Lotto")
     );
   });
+
+  test.each([0, -1000, 999])(
+    "1000원 미만인 경우, (%i 인 경우) 에러를 던진다.",
+    (money) => {
+      expect(() => new LottoMachine(money)).toThrow();
+    }
+  );
 });
