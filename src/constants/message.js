aaa22f2c825fd30a8b0952064f192c6a428e87c6
chysis <chysiss@naver.com>
2024-02-21 18:32:38 +0900

    refactor: 상수 분리

---
+++ src/constants/message.js
@@ -0,0 +1,12 @@
+const MESSAGE = {
+  PURCHASE_AMOUNT_INPUT: '> 구입금액을 입력해 주세요.',
+  LOTTO_COUNT: '개를 구매했습니다.',
+  WINNING_NUMBERS_INPUT: '> 당첨 번호를 입력해 주세요. ',
+  BONUS_NUMBER_INPUT: '> 보너스 번호를 입력해 주세요. ',
+  LOTTO_RESULT: '당첨 통계\n--------------------',
+  RESTART: '> 다시 시작하시겠습니까? (y/n)',
+};
+
+const ERROR_MESSAGE = {};
+
+export { MESSAGE, ERROR_MESSAGE };
