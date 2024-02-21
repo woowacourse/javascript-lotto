65ae21d3bf458d2d35bf6b292a053ffc1c9fb923
chysis <chysiss@naver.com>
2024-02-21 16:49:35 +0900

    refactor(lottoService.test): 파라미터화를 통해 등수 계산 테스트 코드 리팩토링

--- __tests__/lottoService.test.js
+++ __tests__/lottoService.test.js
@@ -1,13 +1,18 @@
 import lottoService from '../src/lottoService';
 
 describe('등수 계산 테스트', () => {
-  test('당첨 번호를 비교한 결과로 1등부터 5등까지 뽑는다.', () => {
-    const RANK_RESULT = 2;
-    const MATCHED_RESULT = { matchedCount: 5, isBonusMatched: true };
+  describe('당첨 번호를 비교한 결과로 1등부터 5등까지 뽑는다.', () => {
+    const testcases = [
+      { rank: 2, matchedResult: { matchedCount: 5, isBonusMatched: true } },
+      { rank: 1, matchedResult: { matchedCount: 6, isBonusMatched: false } },
+      { rank: 4, matchedResult: { matchedCount: 4, isBonusMatched: true } },
+    ];
 
-    const result = lottoService.pickLottoRank(MATCHED_RESULT);
+    test.each(testcases)('$rank 등이 뽑히는 경우', ({ rank, matchedResult }) => {
+      const result = lottoService.pickLottoRank(matchedResult);
 
-    expect(result).toEqual(RANK_RESULT);
+      expect(result).toEqual(rank);
+    });
   });
 
   test('로또 당첨 금액의 수익률을 계산한다.', () => {
