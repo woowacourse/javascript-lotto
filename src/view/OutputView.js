8ffeeb30858116693891ab6f3d8e8746614f722b
jaeml06 <jaeml0630@gmail.com>
2024-02-21 18:50:25 +0900

    feat: 당첨 결과를 출력하는 함수 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,11 +1,24 @@
 const OutputView = {
   printLottoCount(count = 0) {
-    console.log(`${count}개를 구매했습니다.`);
+    console.log(`\n${count}개를 구매했습니다.`);
   },
 
-  printRnadomLottos(numbers) {
-    console.log(numbers);
+  printRandomLottos(numbersArray = []) {
+    numbersArray.forEach((numbers) => {
+      console.log(`[${numbers.join(', ')}]`);
+    });
   },
+  printResultTitle() {
+    console.log('당첨 통계');
+    console.log('--------------------');
+  },
+  printWinningStatistics(result = []) {
+    result.forEach((value) => {
+      const [matchCount, isBonus, price, winCount] = value;
+      console.log(`${matchCount}개 일치${isBonus ? ', 보너스 볼 일치' : ''} (${price.toLocaleString()}원) - ${winCount}개`);
+    });
+  },
+
   printError(message) {
     console.log(message);
   },
