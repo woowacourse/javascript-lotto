0ebf165839e74227e65ed375a52228599c543ea8
Kim Da Eun <midekuna@gmail.com>
2024-02-21 17:05:02 +0900

    feat: 중복되지 않는 숫자 배열을 반환하는 Random 모듈 추가
    Co-Authored-By: badahertz52 <badahertz52@gmail.com>

--- src/utils/RandomNumber.js
+++ src/utils/RandomNumber.js
@@ -4,6 +4,18 @@ class RandomNumber {
 
     return Math.floor(Math.random() * (range[1] - range[0] + 1)) + range[0];
   }
+
+  static pickUniqueNumbersInRange(range, length) {
+    const numbers = new Set();
+
+    while (numbers.size < length) {
+      const randomNumber = this.pickNumberInRange(range.start, range.end);
+
+      numbers.add(randomNumber);
+    }
+
+    return [...numbers];
+  }
 }
 
 export default RandomNumber;
