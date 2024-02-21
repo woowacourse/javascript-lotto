5f686ddd9bee7128f7e673beecbf3d3d18cd948a
pakxe <pigkill40@naver.com>
2024-02-21 16:32:10 +0900

    test: getter 이름 변경에 따른 코드 수정
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,10 +1,9 @@
 import Lotto from "../src/domain/Lotto.js";
-import LottoNumber from "../src/domain/LottoNumber.js";
 
 describe("로또 기능 테스트", () => {
   test("숫자 6개를 가진 로또를 생성한다.", () => {
     const lotto = new Lotto();
-    expect(lotto.getNumbers()).toHaveLength(6);
+    expect(lotto.get()).toHaveLength(6);
   });
 
   test("쉼표로 구분된 로또 번호 문자열이 들어오면 숫자 배열로 바꿔서 저장한다.", () => {
@@ -12,19 +11,6 @@ describe("로또 기능 테스트", () => {
     const lotto = new Lotto(numbersStr);
     const expectNumbers = [1, 9, 12, 15, 28, 45];
 
-    expect(lotto.getNumbers()).toEqual(expectNumbers);
+    expect(lotto.get()).toEqual(expectNumbers);
   });
-});
-
-describe("로또 번호 유효성 테스트", () => {
-  const strValue = [" ", "a"];
-  const floatValue = 0.4;
-  const boundaryValue = [0, 46];
-
-  test.each([strValue, floatValue, ...boundaryValue])(
-    "입력된 각 로또 번호는 1이상 45이하의 정수가 아니면 오류를 발생시킨다.",
-    (wrongValue) => {
-      expect(() => new LottoNumber(wrongValue)).toThrow("[ERROR]");
-    }
-  );
 });
