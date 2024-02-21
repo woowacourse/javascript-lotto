2b340b8d43c94d2d73ebe294534f48ba74571053
jaeml06 <jaeml0630@gmail.com>
2024-02-21 19:07:50 +0900

    refactor: Airbnb 스타일 준수

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,10 +1,7 @@
-import validation from '../src/validation/validation';
+import PurchaseAmountValidation from '../src/validation/purchaseAmount';
 
 describe('로또 구입 금액 유효성 검사', () => {
-  test.each([['0'], ['999'], ['-1000'], ['1000.5']])(
-    '유효하지 않은 구입 금액이면 에러 반환',
-    (money) => {
-      expect(() => validation.purchaseAmount(money)).toThrow('[ERROR]');
-    }
-  );
+  test.each([['0'], ['999'], ['-1000'], ['1000.5']])('유효하지 않은 구입 금액이면 에러 반환', (money) => {
+    expect(() => PurchaseAmountValidation.validate(money)).toThrow('[ERROR]');
+  });
 });
