bd4d3a1e9e87663832413df9ffcbce93f7d79696
jaeml06 <jaeml0630@gmail.com>
2024-02-21 20:08:36 +0900

    refactor: 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -6,9 +6,10 @@ const MESSAGE = Object.freeze({
     PURCHASE_AMOUNT_ERROR: '[ERROR] 유효한 구입 금액이 아닙니다.',
   }),
   QUERY: Object.freeze({
-    PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
-    WIN_NUMBERS: '당첨번호를 입력해 주세요.',
-    BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+    PURCHASE_AMOUNT: '> 구입금액을 입력해 주세요.',
+    WIN_NUMBERS: '\n> 당첨번호를 입력해 주세요.',
+    BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요.',
+    RESTART: '\n> 다시 시작하시겠습니까? (y/n)',
   }),
 });
 
