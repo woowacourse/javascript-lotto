2e8730df9d936447282f8f7cb3f6684411b76e36
pakxe <pigkill40@naver.com>
2024-02-21 16:29:21 +0900

    feat: 로또에 getter 추가
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/domain/LottoNumber.js
+++ src/domain/LottoNumber.js
@@ -1,9 +1,11 @@
 class LottoNumber {
+  #number;
   static MIN = 1;
   static MAX = 45;
 
   constructor(number) {
     this.#validate(number);
+    this.#number = number;
   }
 
   #validate(number) {
@@ -12,6 +14,10 @@ class LottoNumber {
     const isOutOfRange = number < LottoNumber.MIN || number > LottoNumber.MAX;
 
     if (isOutOfRange) throw new Error("[ERROR]");
+  }
+
+  get() {
+    return this.#number;
   }
 }
 
