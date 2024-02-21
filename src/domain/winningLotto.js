6730a8c588765d126bf3875245e23ef4b03b5413
novice0840 <magnolia0840@gmail.com>
2024-02-21 15:26:15 +0900

    feat(winninglotto): 당첨번호 범위 필터링

--- src/domain/winningLotto.js
+++ src/domain/winningLotto.js
@@ -1,4 +1,4 @@
-import { validateLotto } from '../utils/validation.js';
+import { LOTTO_RULES } from '../constant/index.js';
 
 export default class WinningLotto {
   #lotto;
@@ -6,6 +6,10 @@ export default class WinningLotto {
 
   constructor(lotto, bonusNumber) {
     this.#lotto = lotto;
+
+    if (bonusNumber < LOTTO_RULES.min_number || bonusNumber > LOTTO_RULES.max_number) {
+      throw new Error('보너스 번호는 1부터 45 사이여야합니다.');
+    }
 
     this.#bonusNumber = bonusNumber;
   }
