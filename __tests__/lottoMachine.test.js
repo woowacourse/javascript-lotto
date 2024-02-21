c4583bc3e6d8dc92b7512860221fec70f265e963
novice0840 <magnolia0840@gmail.com>
2024-02-21 15:49:45 +0900

    test(lottoMachine): 로또 개수 발행 테스트

---
+++ __tests__/lottoMachine.test.js
@@ -0,0 +1,9 @@
+describe('로또 기계 객체 테스트', () => {
+  test('로또 구입 금액에 맞는 로또 개수를 발행한다.', () => {
+    const cost = 5000;
+    const lottoMachine = new LottoMachine(cost);
+    const lottoCount = 5;
+
+    expect(lottoMachine.getLottoCount).toBe(lottoCount);
+  });
+});
