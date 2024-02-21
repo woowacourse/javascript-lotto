f3a2da4a644cf300a4b7fcbb56db6749cae95ec3
ImxYJL <allensain14@gmail.com>
2024-02-21 18:07:20 +0900

    test: 로또 구입 금액 입력 테스트 구현
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

--- __tests__/budgetValidation.test.js
+++ __tests__/budgetValidation.test.js
@@ -3,7 +3,8 @@ import startValidation from "../src/validation/startValidation.js";
 import ERROR_MESSAGE from "../src/constants/errorMessage.js";
 
 describe("로또 구입 금액 유효성 검사 테스트", () => {
-  const executeValidation = (input) => () => startValidation(budgetValidation.categories, input);
+  const executeValidation = (input) => () =>
+    startValidation(budgetValidation.categories, input);
 
   test.each([
     [
@@ -30,7 +31,10 @@ describe("로또 구입 금액 유효성 검사 테스트", () => {
       },
       ERROR_MESSAGE.DIVISIBLE_BY_MIN_PRICE,
     ],
-  ])('입력값이 %o 일 때 "%s" 메시지와 함께 에러가 발생해야 한다.', ({ input }, expectedErrorMessage) => {
-    expect(executeValidation(input)).toThrow(expectedErrorMessage);
-  });
+  ])(
+    '입력값이 %o 일 때 "%s" 메시지와 함께 에러가 발생해야 한다.',
+    ({ input }, expectedErrorMessage) => {
+      expect(executeValidation(input)).toThrow(expectedErrorMessage);
+    }
+  );
 });
