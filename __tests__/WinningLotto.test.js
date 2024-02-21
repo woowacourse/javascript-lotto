e45eacfb65c507fea3cd9173b8fe2e82a9468d8d
ss0526100 <ss0526100@gmail.com>
2024-02-21 14:42:02 +0900

    feat(WinningLotto):등수가 담긴 배열을 반환

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -1,6 +1,21 @@
+import Lotto from "../src/domain/Lotto";
 import WinningLotto from "../src/domain/WinningLotto";
 
 describe("WinningLotto에 대한 유닛 테스트", () => {
+  test("Lotto 배열을 입력받아 각 로또의 등수가 배열을 반환한다", () => {
+    const lottoNumbers1 = [1, 2, 3, 4, 5, 6];
+    const lottoNumbers2 = [1, 2, 3, 4, 40, 42];
+
+    const winningNumbers = [1, 2, 3, 4, 40, 41];
+    const bonusNumber = 42;
+
+    const lotto1 = new Lotto(lottoNumbers1);
+    const lotto2 = new Lotto(lottoNumbers2);
+    const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+    expect(winningLotto.getLottosRanks([lotto1, lotto2])).toEqual([4, 2]);
+  });
+
   test.each([[[1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5, 6, 7]]])(
     "당첨 번호의 개수가 6개가 아니면 예외 처리를 한다.",
     (numbers) => {
--- src/domain/WinningLotto.js
+++ src/domain/WinningLotto.js
@@ -1,13 +1,57 @@
 import Lotto from "./Lotto";
 
 class WinningLotto {
+  #lottoBoardTmp;
+  #bonusNumber;
+
   constructor(numbers, bonusNumber) {
     this.#validateNumbers(numbers);
     this.#validateBonusNumber(bonusNumber);
     this.#validateUniqueNumbers([...numbers, bonusNumber]);
+
+    this.#setLottoBoardTmp(numbers);
+    this.#bonusNumber = bonusNumber;
   }
 
-  #validateNumbers() {
+  getLottosRanks(lottos) {
+    return lottos.map((lotto) => this.#getLottoRank(lotto));
+  }
+
+  #getLottoRank(lotto) {
+    const matchCount = this.#getMatchCount(lotto);
+
+    const hasBonusNumber = lotto.getNumbers().includes(this.#bonusNumber);
+
+    return this.#getLottoRankByMatchCountAndHasBonusNumber(
+      matchCount,
+      hasBonusNumber
+    );
+  }
+
+  #getLottoRankByMatchCountAndHasBonusNumber(matchCount, hasBonusNumber) {
+    if (matchCount < 3) return -1;
+    if (matchCount === 3) return 5;
+    if (matchCount === 4) return 4;
+    if (matchCount === 5 && !hasBonusNumber) return 3;
+    if (matchCount === 5 && hasBonusNumber) return 2;
+    if (matchCount === 6) return 1;
+  }
+  #getMatchCount(lotto) {
+    return lotto
+      .getNumbers()
+      .reduce(
+        (count, number) => count + (this.#lottoBoardTmp[number] ? 1 : 0),
+        0
+      );
+  }
+
+  #setLottoBoardTmp(numbers) {
+    this.#lottoBoardTmp = new Array(Lotto.MAX_LOTTO_NUMBER + 1).fill(false);
+
+    numbers.forEach((number) => (this.#lottoBoardTmp[number] = true));
+  }
+
+  #validateNumbers(numbers) {
     this.#validateNumbersLength(numbers);
     this.#validateIntegers(numbers);
     this.#validateNumbersInRange(numbers);
