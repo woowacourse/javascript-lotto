76735ffa6772418a117689be8a411b324ec27eb0
pakxe <pigkill40@naver.com>
2024-02-21 21:44:45 +0900

    test: 등수 배열을 받아 총 수익률을 계산해주는 함수 테스트 작성
    등수 배열을 받아 총 상금을 계산하는 테스트
    
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- __tests__/calculateTotalPrize.test.js
+++ __tests__/calculateTotalPrize.test.js
@@ -2,7 +2,6 @@ import {
   calculateTotalPrize,
   PRIZE,
 } from "../src/domain/calculateTotalPrize.js";
-import { calculateProfitRate } from "../src/utils/calculateProfitRate.js";
 
 describe("등수 배열을 받아 총 수익률을 계산해주는 함수 테스트", () => {
   test("등수 배열을 받아 총 상금을 계산한다.", () => {
@@ -11,15 +10,5 @@ describe("등수 배열을 받아 총 수익률을 계산해주는 함수 테스
       return acc + cur;
     });
     expect(calculateTotalPrize(ranks)).toBe(expectedTotal);
-  });
-});
-
-describe("구입 금액 대비 수익률을 계산해주는 함수 테스트", () => {
-  test("구입 금액과 총 상금으로 수익률을 계산해 반환한다.", () => {
-    const totalProfit = 5000;
-    const money = 8000;
-    const expectedProfitRate = 62.5;
-
-    expect(calculateProfitRate(totalProfit, money)).toBe(expectedProfitRate);
   });
 });
