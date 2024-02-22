c6b14a6431e4d160069d5f069744a3b03ac0ac36
jinsil <jinsil378900@gmail.com>
2024-02-22 11:17:59 +0900

    feat: 입출력 관리 함수 구현

--- src/util/handleIO.js
+++ src/util/handleIO.js
@@ -5,26 +5,21 @@ const handleIO = {
     console.log(message);
   },
 
-  validateArguments(query) {
-    if (arguments.length !== 1) {
-      throw new Error('인자는 1개만 허용됩니다.');
-    }
-    if (typeof query !== 'string') {
-      throw new Error('질의에 포함시킬 내용은 오직 문자열 타입만 허용됩니다.');
-    }
-  },
-
-  createReadlineInterface() {
-    return readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-  },
-
   read(query) {
-    this.validateArguments(query);
-    const rl = this.createReadlineInterface();
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+      if (arguments.length !== 1) {
+        reject(new Error('arguments must be 1'));
+      }
+
+      if (typeof query !== 'string') {
+        reject(new Error('query must be string'));
+      }
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
       rl.question(query, (input) => {
         rl.close();
         resolve(input);
