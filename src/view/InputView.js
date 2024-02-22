c86cfa27c3cfcf372cd9bedef46657b8541503df
pakxe <pigkill40@naver.com>
2024-02-22 15:11:25 +0900

    feat: 입력 view 객체 작성
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

---
+++ src/view/InputView.js
@@ -0,0 +1,21 @@
+import { readLineAsync } from "../utils/console.js";
+
+const InputView = {
+  async readMoney() {
+    return await readLineAsync("구입금액을 입력해 주세요.");
+  },
+
+  async readWinningLottoNumbers() {
+    return await readLineAsync("당첨 번호를 입력해 주세요.");
+  },
+
+  async readBonusLottoNumber() {
+    return await readLineAsync("보너스 번호를 입력해 주세요.");
+  },
+
+  async readRestartGame() {
+    return await readLineAsync("다시 시작하시겠습니까? (y/n)");
+  },
+};
+
+export default InputView;
