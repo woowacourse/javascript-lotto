9b1bc955751603b203671bc7474605ccfca317ff
novice0840 <magnolia0840@gmail.com>
2024-02-22 15:26:54 +0900

    refactor(statistics): calculateResult 함수를 분리

--- src/domain/statistics.js
+++ src/domain/statistics.js
@@ -1,35 +1,39 @@
 export default class Statistics {
   #result = { three: 0, four: 0, five: 0, five_bonus: 0, six: 0 };
+
   #profit = 0;
-  #total = 0;
 
   constructor({ lottos, winningLotto, bonusNumber, cost }) {
-    this.calculateResult({ lottos, winningLotto, bonusNumber, cost });
-    this.#profit = ((this.#total / cost) * 100).toFixed(1);
+    this.#calculateResult({ lottos, winningLotto, bonusNumber, cost });
+
+    this.#profit = ((this.#calculateTotal() / cost) * 100).toFixed(1);
   }
 
-  calculateResult({ lottos, winningLotto, bonusNumber }) {
+  #calculateTotal() {
+    return (
+      this.#result.three * 5_000 +
+      this.#result.four * 50_000 +
+      this.#result.five * 1_500_000 +
+      this.#result.five_bonus * 30_000_000 +
+      this.#result.six * 2_000_000_000
+    );
+  }
+
+  #calculateResult({ lottos, winningLotto, bonusNumber }) {
     lottos.forEach((lotto) => {
       const correctNumber = this.#correctCount(lotto, winningLotto);
       const hasBonusNumber = this.#hasBonusNumber(lotto, bonusNumber);
 
-      if (correctNumber === 3) {
-        this.#total += 5_000;
-        this.#result.three += 1;
-      } else if (correctNumber === 4) {
-        this.#total += 50_000;
-        this.#result.four += 1;
-      } else if (correctNumber === 5 && hasBonusNumber) {
-        this.#total += 30_000_000;
-        this.#result.five_bonus += 1;
-      } else if (correctNumber === 5) {
-        this.#total += 1_500_000;
-        this.#result.five += 1;
-      } else if (correctNumber === 6) {
-        this.#total = 2_000_000_000;
-        this.#result.six += 1;
-      }
+      this.#addResult(correctNumber, hasBonusNumber);
     });
+  }
+
+  #addResult(correctNumber, hasBonusNumber) {
+    if (correctNumber === 3) this.#result.three += 1;
+    else if (correctNumber === 4) this.#result.four += 1;
+    else if (correctNumber === 5 && hasBonusNumber) this.#result.five_bonus += 1;
+    else if (correctNumber === 5) this.#result.five += 1;
+    else if (correctNumber === 6) this.#result.six += 1;
   }
 
   #correctCount(lotto, winningLotto) {
