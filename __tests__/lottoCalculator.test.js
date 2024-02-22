686f8fd7ffee62240db29d717b684c91cec23523
GimGayeon <kky1373@naver.com>
2024-02-22 11:58:15 +0900

    test:당첨 번호와 발행 번호 비교 테스트 수정
    co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- __tests__/lottoCalculator.test.js
+++ __tests__/lottoCalculator.test.js
@@ -4,17 +4,16 @@ describe('당첨 번호와 발행 번호 비교하는 테스트', () => {
   test('당첨 번호와 발행 번호를 비교시 3개가 일치한다.', () => {
     const lottoNumbers = {
       winningNumbers: [1, 2, 3, 4, 5, 6],
-
       bonusNumber: 7,
     };
 
-    const generatedLottos = [1, 2, 3, 7, 8, 9];
+    const generatedLottos = [[1, 2, 3, 7, 8, 9]];
 
     const lottoCalculator = new LottoCalculator(lottoNumbers, generatedLottos);
 
     const result = lottoCalculator.compare(
       lottoNumbers.winningNumbers,
-      generatedLottos,
+      generatedLottos[0],
     );
 
     expect(result).toEqual(3);
@@ -27,14 +26,13 @@ describe('당첨 번호와 발행 번호 비교하는 테스트', () => {
       bonusNumber: 7,
     };
 
-    const generatedLottos = [1, 2, 3, 4, 5, 7];
+    const generatedLottos = [[1, 2, 3, 4, 5, 7]];
 
     const lottoCalculator = new LottoCalculator(lottoNumbers, generatedLottos);
 
     const isEqualBonusNumber = lottoCalculator.isEqualBonusNumber(
       lottoNumbers.bonusNumber,
-
-      generatedLottos,
+      generatedLottos[0],
     );
 
     expect(isEqualBonusNumber).toEqual(true);
