70d0a4a36c51e68aa55a92276033219f07524537
soosoo22 <lllllh00@naver.com>
2024-02-23 02:36:01 +0900

    feat: 로또 등수 및 상금을 나타내는 상수 추가

---
+++ src/constant/rank.js
@@ -0,0 +1,31 @@
+export const RANK = {
+  FIRST: {
+    PRIZE: 2000000000,
+    MATCHING_COUNT: 6,
+    BONUS_COUNT: 0,
+  },
+
+  SECOND: {
+    PRIZE: 30000000,
+    MATCHING_COUNT: 5,
+    BONUS_COUNT: 1,
+  },
+
+  THIRD: {
+    PRIZE: 1500000,
+    MATCHING_COUNT: 5,
+    BONUS_COUNT: 0,
+  },
+
+  FOURTH: {
+    PRIZE: 50000,
+    MATCHING_COUNT: 4,
+    BONUS_COUNT: 0,
+  },
+
+  FIFTH: {
+    PRIZE: 5000,
+    MATCHING_COUNT: 3,
+    BONUS_COUNT: 0,
+  },
+};
