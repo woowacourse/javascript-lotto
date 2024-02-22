f0fd1a41e8c94f776a77ac6979769f800a7263aa
badahertz52 <badahertz52@gmail.com>
2024-02-22 16:47:58 +0900

    fix: 테스트 오류 수정

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -1,7 +1,6 @@
-import LottoMachine from '../src/domains';
+import { LottoMachine } from '../src/domains';
 
 describe('LottoMachine 기능 테스트', () => {
-  // TODO 통합 테스트(로또 게임 전반에 대한) 할때 만들자
   test.skip('유효한 로또를 발행한다.', () => {});
 
   test('발행한 로또 개수는 구매 금액에 의해 정해진다.', () => {
