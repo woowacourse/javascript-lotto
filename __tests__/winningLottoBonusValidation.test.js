23ced56ce7533ffbf88aaabaea4f57bced07aa61
ImxYJL <allensain14@gmail.com>
2024-02-22 12:08:44 +0900

    test: 로또 보너스 번호 공통 유효성 검사 테스트 및 범위 검사 테스트 분리 구현
    Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

--- __tests__/winningLottoBonusValidation.test.js
+++ __tests__/winningLottoBonusValidation.test.js
@@ -1,19 +1,18 @@
-import winningLottoNumbersValidation from "../src/validation/winningLottoNumbersValidation.js";
+import winningLottoBonusValidation from "../src/validation/winningLottoBonusValidation.js";
 import startValidation from "../src/validation/startValidation.js";
 import ERROR_MESSAGE from "../src/constants/errorMessage.js";
 
-describe("로또 당첨 번호 유효성 검사 테스트", () => {
-  const executeValidation = (input) => () => startValidation(winningLottoNumbersValidation.winningNumbers, input);
+describe("로또 보너스 번호 유효성 검사 테스트", () => {
+  const executeValidation = (input) => () => startValidation(winningLottoBonusValidation.winningBonus, input);
 
   describe("예외 테스트", () => {
     test.each([
       {
-        input: [1, 2, 3, 4, 5],
-        expectedErrorMessage: ERROR_MESSAGE.sixNumbers,
-      },
-      {
-        input: [1, 2, 3, 4, 4, 6],
-        expectedErrorMessage: ERROR_MESSAGE.notDuplicated,
+        input: {
+          normalNumbers: [1, 2, 3, 4, 5, 6],
+          bonusNumber: 1,
+        },
+        expectedErrorMessage: ERROR_MESSAGE.notInWinningNumbers,
       },
     ])(
       '입력값이 "$input"일 때 "$expectedErrorMessage" 메시지와 함께 에러가 발생해야 한다.',
@@ -26,10 +25,10 @@ describe("로또 당첨 번호 유효성 검사 테스트", () => {
   describe("정상 동작 테스트", () => {
     test.each([
       {
-        input: [1, 2, 3, 4, 5, 6],
-      },
-      {
-        input: [40, 41, 42, 43, 44, 45],
+        input: {
+          normalNumbers: [1, 2, 3, 4, 5, 6],
+          bonusNumber: 7,
+        },
       },
     ])('입력값이 "$input"일 때 에러가 발생하지 않는다.', ({ input }) => {
       expect(executeValidation(input)).not.toThrow();
--- __tests__/winningLottoValidation.test.js
+++ __tests__/winningLottoValidation.test.js
@@ -1,19 +1,19 @@
-import winningLottoNumbersValidation from "../src/validation/winningLottoNumbersValidation.js";
+import winningLottoValidation from "../src/validation/winningLottoValidation.js";
 import startValidation from "../src/validation/startValidation.js";
 import ERROR_MESSAGE from "../src/constants/errorMessage.js";
 
-describe("로또 당첨 번호 유효성 검사 테스트", () => {
-  const executeValidation = (input) => () => startValidation(winningLottoNumbersValidation.winningNumbers, input);
+describe("로또 당첨 번호와 보너스 번호 유효성 검사 테스트", () => {
+  const executeValidation = (input) => () => startValidation(winningLottoValidation.winningCombination, input);
 
   describe("예외 테스트", () => {
     test.each([
       {
-        input: [1, 2, 3, 4, 5],
-        expectedErrorMessage: ERROR_MESSAGE.sixNumbers,
+        input: 0,
+        expectedErrorMessage: ERROR_MESSAGE.outOfRange,
       },
       {
-        input: [1, 2, 3, 4, 4, 6],
-        expectedErrorMessage: ERROR_MESSAGE.notDuplicated,
+        input: 46,
+        expectedErrorMessage: ERROR_MESSAGE.outOfRange,
       },
     ])(
       '입력값이 "$input"일 때 "$expectedErrorMessage" 메시지와 함께 에러가 발생해야 한다.',
@@ -26,10 +26,10 @@ describe("로또 당첨 번호 유효성 검사 테스트", () => {
   describe("정상 동작 테스트", () => {
     test.each([
       {
-        input: [1, 2, 3, 4, 5, 6],
+        input: 1,
       },
       {
-        input: [40, 41, 42, 43, 44, 45],
+        input: 45,
       },
     ])('입력값이 "$input"일 때 에러가 발생하지 않는다.', ({ input }) => {
       expect(executeValidation(input)).not.toThrow();
