6447b417cf315bf74e84d9fb8ea9bc281b71d2c4
pakxe <pigkill40@naver.com>
2024-02-22 15:16:15 +0900

    feat: 자동 로또 번호 생성기 구현
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/domain/lottoGenerator.js
+++ src/domain/lottoGenerator.js
@@ -1,11 +1,14 @@
 import Lotto from "./Lotto.js";
 // 함수를 클래스와 같은 폴더에 배치하는 것이 괜찮을까요? 저희는 도메인의 역할이므로 괜찮다고 생각했습니다. [리뷰어분]의 생각은 어떠신가요?
 
-function lottoGenerator(winningStr, count) {
-  const winningLotto = new Lotto(winningStr);
-  const randomLottos = Array.from({ length: count }).map(() => new Lotto());
+const lottoGenerator = {
+  generateWinningLotto(numbers) {
+    return new Lotto(numbers);
+  },
 
-  return { winningLotto, randomLottos };
-}
+  generateRandomLotto(count) {
+    return Array.from({ length: count }).map(() => new Lotto());
+  },
+};
 
 export default lottoGenerator;
