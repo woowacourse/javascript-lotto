1336fcd39d1befc65695944f6d1499f89ebba99f
jaeml06 <jaeml0630@gmail.com>
2024-02-22 12:14:00 +0900

    test: 모든 로또의 숫자를 배열로 잘 반환하는지 확인

--- __tests__/lottoProcess.test.js
+++ __tests__/lottoProcess.test.js
@@ -16,11 +16,22 @@ describe('LottoProcess 클래스 검사', () => {
     const lottoProcess = new LottoProcess(lottos);
 
     expect(lottoProcess.getResult(winLotto, bonusNumber)).toEqual([
-      [3, false, 5000, 0],
-      [4, false, 50000, 0],
-      [5, false, 1500000, 0],
-      [5, true, 30000000, 0],
-      [6, false, 2000000000, 1],
+      [3, false, 5_000, 0],
+      [4, false, 50_000, 0],
+      [5, false, 1_500_000, 0],
+      [5, true, 30_000_000, 0],
+      [6, false, 2_000_000_000, 1],
+    ]);
+  });
+
+  test('모든 로또의 숫자를 배열로 잘 반환하는지 확인', () => {
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = new Lotto([5, 10, 15, 20, 25, 30]);
+    const lottoProcess = new LottoProcess([lotto1, lotto2]);
+
+    expect(lottoProcess.getAllLottosNumbers()).toEqual([
+      [1, 2, 3, 4, 5, 6],
+      [5, 10, 15, 20, 25, 30],
     ]);
   });
 });
