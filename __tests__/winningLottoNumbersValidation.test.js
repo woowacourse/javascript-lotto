c2010c55b2f2c7ff2f6608d732eba392dfd689b3
ImxYJL <allensain14@gmail.com>
2024-02-22 11:16:02 +0900

    test: winningLottoNumbersValidation 테스트 구현

--- __tests__/winningLottoNumbersValidation.test.js
+++ __tests__/winningLottoNumbersValidation.test.js
@@ -19,7 +19,10 @@ describe("로또 당첨 번호 유효성 검사 테스트", () => {
       },
       ERROR_MESSAGE.UNIQUE_NUMBER,
     ],
-  ])('입력값이 %o 일 때 "%s" 메시지와 함께 에러가 발생해야 한다.', ({ input }, expectedErrorMessage) => {
-    expect(executeValidation(input)).toThrow(expectedErrorMessage);
-  });
+  ])(
+    '입력값이 %o 일 때 "%s" 메시지와 함께 에러가 발생해야 한다.',
+    ({ input }, expectedErrorMessage) => {
+      expect(executeValidation(input)).toThrow(expectedErrorMessage);
+    }
+  );
 });
