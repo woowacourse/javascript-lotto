2b1a7927847b33fba28d553e877c852a99504c96
jinsil <jinsil378900@gmail.com>
2024-02-22 17:07:50 +0900

    refactor: 출력 결과 이름 및 위치 수정

--- src/constant/errorMessage.js
+++ src/constant/errorMessage.js
@@ -9,7 +9,7 @@ const ERROR_MESSAGE = {
   INPUT_IS_EMPTY: generateErrorMessage('필수값입니다.'),
   INPUT_IS_NOT_NUMBER: generateErrorMessage('숫자로 입력해주셔야 합니다.'),
   PURCHASE_AMOUNT_NOT_DIVIDED: generateErrorMessage(`${SETTING.LOTTO_PRICE} 단위의 숫자여야 합니다.`),
-  LOTTO_NUMBER_LENGTH: generateErrorMessage(`쉼표로 구분되는 ${SETTING.LOTTO_LENGTH} 숫자여야 합니다.`),
+  LOTTO_NUMBER_LENGTH: generateErrorMessage(`쉼표로 구분되는 ${SETTING.LOTTO_LENGTH}개의 숫자여야 합니다.`),
   LOTTO_NUMBER_RANGE: generateErrorMessage(
     `각각의 숫자는 ${SETTING.MIN_LOTTO_NUMBER}~${SETTING.MAX_LOTTO_NUMBER} 사이의 숫자여야 합니다.`,
   ),
--- src/constant/gameMessage.js
+++ src/constant/gameMessage.js
@@ -10,13 +10,11 @@ const GAME_MESSAGE = {
   PROMPT_RESTART_COMMAND: generatePromptMessage('다시 시작하시겠습니까? (y/n) '),
   PURCHASE_RESULT_TEXT: (lottoCount) => `${lottoCount}개를 구매했습니다.`,
   LOTTO_ITEM: (lotto) => `[${lotto.join(', ')}]`,
-  LOTTO_WINNING_RESULT_TITLE: '\n당첨 통계\n--------------------',
-  LOTTO_WINNING_RESULTS: (match, reward, count) => `${match} (${reward.toLocaleString()}원) - ${count}개`,
-  LOTTO_PROFIT_RATE: (profitRate) => `총 수익률은 ${profitRate}%입니다.`,
   EXIT: '로또 게임을 종료합니다.',
 };
 
 const LOTTO_WINNING_RESULTS = {
+  LOTTO_WINNING_RESULT_TITLE: '\n당첨 통계\n--------------------',
   WINNING_RESULT: (rank, winningCount) => {
     const matchingMessage = `${RANKING[rank].MATCHING_COUNT}개 일치${
       rank === RANKING.SECOND.NAME ? ', 보너스 볼 일치' : ''
