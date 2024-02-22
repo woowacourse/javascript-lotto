ed5a2ce91013b0ab44e4a9a154c70612a37ddd0e
chysis <chysiss@naver.com>
2024-02-22 17:26:11 +0900

    feat(InputView): 입력 로직 처리

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,19 +1,75 @@
+import CONFIG from '../constants/config';
 import { MESSAGE } from '../constants/message';
 import readLineAsync from '../utils/readLineAsync';
+import bonusNumberValidator from '../validators/bonusNumberValidator';
+import purchaseAmountValidator from '../validators/purchaseAmountValidator';
 import OutputView from './OutputView';
+import restartValidator from '../validators/restartValidator';
+import winningNumbersValidator from '../validators/winningNumbersValidator';
+
+const Private = {
+  readPurchaseAmount() {
+    return readLineAsync(MESSAGE.PURCHASE_AMOUNT_INPUT);
+  },
+
+  readWinningNumbers() {
+    return readLineAsync(MESSAGE.WINNING_NUMBERS_INPUT);
+  },
+
+  readBonusNumber() {
+    return readLineAsync(MESSAGE.BONUS_NUMBER_INPUT);
+  },
+
+  readRestart() {
+    return readLineAsync(MESSAGE.RESTART);
+  },
+};
 
 const InputView = {
   async readPurchaseAmount() {
-    return this.inputLoop(await readLineAsync(MESSAGE.PURCHASE_AMOUNT_INPUT));
+    try {
+      const purchaseAmountInput = await Private.readPurchaseAmount();
+      const purchaseAmount = purchaseAmountInput.trim();
+      purchaseAmountValidator.validate(purchaseAmount);
+      return purchaseAmount;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readPurchaseAmount();
+    }
   },
 
-  inputLoop(func) {
-    while (true) {
-      try {
-        return func();
-      } catch (error) {
-        OutputView.print(error.message);
-      }
+  async readWinningNumbers() {
+    try {
+      const winningNumbersInput = await Private.readWinningNumbers();
+      const winningNumbers = winningNumbersInput.split(CONFIG.SEPARATOR).map(number => parseInt(number.trim(), 10));
+      winningNumbersValidator.validate(winningNumbers);
+      return winningNumbers;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readWinningNumbers();
+    }
+  },
+
+  async readBonusNumber(winningNumbers) {
+    try {
+      const result = await Private.readBonusNumber();
+      bonusNumberValidator.validate(result, winningNumbers);
+      return parseInt(result, 10);
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readBonusNumber(winningNumbers);
+    }
+  },
+
+  async readRestart() {
+    try {
+      const restartInput = await Private.readRestart();
+      const restart = restartInput.toLowerCase();
+      restartValidator.validate(restart);
+      return restart;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readRestart();
     }
   },
 };
