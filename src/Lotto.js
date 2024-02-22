f6a2e5a7b4c86b17b74fdc15fbeae1430eb8577f
chysis <chysiss@naver.com>
2024-02-22 11:10:59 +0900

    feat(OutputView): 구입한 로또 개수와 번호 출력 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,19 @@
 class Lotto {
-  numberList;
+  #numberList;
 
   constructor(numberList) {
     numberList.sort((a, b) => a - b);
-    this.numberList = numberList;
+    this.#numberList = numberList;
   }
 
   getMatchedAmount(winningNumbers, bonusNumber) {
-    const matchedCount = this.numberList.filter(number => winningNumbers.includes(number)).length;
-    const isBonusMatched = this.numberList.some(number => number === bonusNumber);
+    const matchedCount = this.#numberList.filter(number => winningNumbers.includes(number)).length;
+    const isBonusMatched = this.#numberList.some(number => number === bonusNumber);
     return { matchedCount, isBonusMatched };
+  }
+
+  get numberList() {
+    return this.#numberList;
   }
 }
 
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,3 +1,23 @@
-const OutputView = {};
+import { MESSAGE } from '../constants/message';
+
+const OutputView = {
+  print(message = '') {
+    console.log(message);
+  },
+
+  printLotto(lotto) {
+    this.print(`[${lotto.join(', ')}]`);
+  },
+
+  printLottery(lottery) {
+    lottery.forEach(lotto => {
+      this.printLotto(lotto.numberList);
+    });
+  },
+
+  printLottoCount(lottoCount) {
+    this.print(`${lottoCount}${MESSAGE.LOTTO_COUNT}`);
+  },
+};
 
 export default OutputView;
