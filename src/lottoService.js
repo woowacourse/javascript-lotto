09082f610b78473cebf3059f61704361130ae0f9
chysis <chysiss@naver.com>
2024-02-22 14:04:11 +0900

    refactor(lottoService): 등수별 당첨 개수를 구하는 로직 수정

--- src/lottoService.js
+++ src/lottoService.js
@@ -1,16 +1,31 @@
+import CONFIG from './constants/config';
+
 const lottoService = {
-  pickLottoRank(matchedResult) {
-    const MATCHED_RANK_COUNT = { 0: 6, 1: 6, 2: 6, 3: 5, 4: 4, 6: 1 };
-    const { matchedCount, isBonusMatched } = matchedResult;
-    if (matchedCount === 5) return isBonusMatched ? 2 : 3;
-    return MATCHED_RANK_COUNT[matchedCount];
+  calculateProfit(matchedResultList, purchaseAmount) {
+    const rankCounts = this.calculateRankCounts(matchedResultList);
+    const totalPrice = Array.from({ length: 6 }, (_, i) => i + 1).reduce(
+      (acc, rank) => acc + rankCounts[rank] * CONFIG.PRIZE[rank],
+      0,
+    );
+
+    return ((totalPrice / purchaseAmount) * 100).toFixed(1);
   },
 
-  calculateProfit(matchedResultList, purchaseAmount) {
-    const rankList = matchedResultList.map(matchedResult => this.pickLottoRank(matchedResult));
-    const PRIZE = { 1: 2000000000, 2: 30000000, 3: 1500000, 4: 50000, 5: 5000, 6: 0 };
-    const totalPrice = rankList.reduce((acc, rank) => acc + PRIZE[rank], 0);
-    return ((totalPrice / purchaseAmount) * 100).toFixed(1);
+  calculateRankCounts(matchedResultList) {
+    const rankCounts = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
+    matchedResultList.forEach(matchedResult => {
+      const rank = this.pickLottoRank(matchedResult);
+      rankCounts[rank] += 1;
+    });
+    return rankCounts;
+  },
+
+  pickLottoRank(matchedResult) {
+    const { matchedCount, isBonusMatched } = matchedResult;
+    if (matchedCount === CONFIG.SECOND_PRIZE_CONDITION) {
+      return isBonusMatched ? CONFIG.SECOND_PRIZE : CONFIG.THIRD_PRIZE;
+    }
+    return CONFIG.MATCHED_RANK_COUNT[matchedCount];
   },
 };
 
