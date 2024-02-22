5461c2b745e5ed3affe07ae1cbb03a9a9aee1920
jaeml06 <jaeml0630@gmail.com>
2024-02-22 12:13:24 +0900

    refacotr: each사용하여 테스트 단순화

--- __tests__/lottoController.test.js
+++ __tests__/lottoController.test.js
@@ -1,16 +1,20 @@
 import LottoController from '../src/controller/LottoController';
 
 describe('lottoController 클래스 검사 ', () => {
-  test('로또 결과에 따른 총 수익률을 계산하여 반환', () => {
+  test.each([
+    [[1, 0, 0, 0, 0], 8, '62.5'],
+    [[0, 0, 0, 1, 0], 30, '100000.0'],
+  ])('로또 결과에 따른 총 수익률을 계산하여 반환', (winResult, lottoCount, revenue) => {
     const lottoController = new LottoController();
+
     const result = [
-      [3, false, 5000, 1],
-      [4, false, 50000, 0],
-      [5, false, 1500000, 0],
-      [5, true, 30000000, 0],
-      [6, false, 2000000000, 0],
+      [3, false, 5_000, winResult[0]],
+      [4, false, 50_000, winResult[1]],
+      [5, false, 150_0000, winResult[2]],
+      [5, true, 30_000_000, winResult[3]],
+      [6, false, 2_000_000_000, winResult[4]],
     ];
-    const lottoCount = 8;
-    expect(lottoController.getRateOfRevenue(result, lottoCount)).toBe('62.5');
+
+    expect(lottoController.getRateOfRevenue(result, lottoCount)).toBe(revenue);
   });
 });
