33f14faf2692bbd72e3e6f16f4cc0043b25f0c0f
손진영 <sm099@naver.com>
2024-02-22 11:40:04 +0900

    refactor(BonusNumberValidator): 하드 코딩 된 값 상수화
    1. TYPE_OF_NUMBER_REGEXP 가져오는 방식으로 변경
    
    2. winningNumbers를 winningNumber로 변경
    
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/validator/bonusNumber/BonusNumberValidator.js
+++ src/validator/bonusNumber/BonusNumberValidator.js
@@ -1,6 +1,12 @@
-import { startValidation } from '../startValidation.js';
-import { deepFreeze } from '../../utils/object/object.js';
 import Lotto from '../../domain/Lotto/Lotto.js';
+
+import { startValidation } from '../startValidation.js';
+
+import { TYPE_OF_NUMBER_REGEXP } from '../../constants/regexp.js';
+
+import { deepFreeze } from '../../utils/object/object.js';
+
+const { LOTTO_RULE } = Lotto;
 
 /**
  * @module BonusNumberValidator
@@ -15,24 +21,25 @@ const BonusNumberValidator = deepFreeze({
     isTypeOfNumber: {
       errorMessage: '보너스 번호의 형식이 유효하지 않습니다. 다시 입력해 주세요.',
       isValid({ inputValue }) {
-        return /^\d+$/.test(inputValue);
+        return TYPE_OF_NUMBER_REGEXP.test(inputValue);
       },
     },
 
     isValidRange: {
-      errorMessage: `보너스 번호는 ${Lotto.LOTTO_DETAILS.min} ~ ${Lotto.LOTTO_DETAILS.max} 사이여야 합니다. 다시 입력해 주세요.`,
+      errorMessage: `보너스 번호는 ${LOTTO_RULE.min} ~ ${LOTTO_RULE.max} 사이여야 합니다. 다시 입력해 주세요.`,
       isValid({ inputValue }) {
         const bonusNumber = Number(inputValue);
 
-        return bonusNumber >= Lotto.LOTTO_DETAILS.min && bonusNumber <= Lotto.LOTTO_DETAILS.max;
+        return bonusNumber >= LOTTO_RULE.min && bonusNumber <= LOTTO_RULE.max;
       },
     },
 
     isUniqueBonusNumber: {
       errorMessage: '보너스 번호는 당첨 번호에 포함되지 않은 번호여야 합니다. 다시 입력해 주세요.',
-      isValid({ inputValue, winningNumbers }) {
+      isValid({ inputValue, winningNumber }) {
         const bonusNumber = Number(inputValue);
-        return !winningNumbers.includes(bonusNumber);
+
+        return !winningNumber.includes(bonusNumber);
       },
     },
   },
@@ -40,12 +47,12 @@ const BonusNumberValidator = deepFreeze({
   /**
    * 사용자의 입력 보너스 번호에 대한 유효성 검사 실행
    * @param {string} inputValue - 사용자의 입력 값
-   * @param {number[]} winningNumbers - 당첨 번호 목록
+   * @param {number[]} winningNumber - 당첨 번호 목록
    * @throws {AppError} 유효성을 만족하지 않을 경우 에러 발생
    * @returns {void}
    */
-  check(inputValue, winningNumbers) {
-    startValidation(this.validationTypes, { inputValue, winningNumbers });
+  check(inputValue, winningNumber) {
+    startValidation(this.validationTypes, { inputValue, winningNumber });
   },
 });
 
