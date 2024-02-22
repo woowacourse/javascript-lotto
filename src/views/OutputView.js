1cf28ad64a40dddf6713b25772b0ba282833eebe
chysis <chysiss@naver.com>
2024-02-22 17:00:36 +0900

    feat(OutputView): 당첨 내역 및 수익률 출력 기능 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,4 +1,5 @@
 import { MESSAGE } from '../constants/message';
+import CONFIG from '../constants/config';
 
 const OutputView = {
   print(message = '') {
@@ -6,7 +7,7 @@ const OutputView = {
   },
 
   printLotto(lotto) {
-    this.print(`[${lotto.join(', ')}]`);
+    this.print(`[${lotto.join(CONFIG.JOIN_SEPARATOR)}]`);
   },
 
   printLottery(lottery) {
@@ -17,6 +18,19 @@ const OutputView = {
 
   printLottoCount(lottoCount) {
     this.print(`${lottoCount}${MESSAGE.LOTTO_COUNT}`);
+  },
+
+  printLottoResult(rankList) {
+    this.print(MESSAGE.LOTTO_RESULT);
+    this.print(`3개 일치 (${CONFIG.PRIZE[5].toLocaleString('ko-KR')}원) - ${rankList[5]}개`);
+    this.print(`4개 일치 (${CONFIG.PRIZE[4].toLocaleString('ko-KR')}원) - ${rankList[4]}개`);
+    this.print(`5개 일치 (${CONFIG.PRIZE[3].toLocaleString('ko-KR')}원) - ${rankList[3]}개`);
+    this.print(`5개 일치, 보너스 볼 일치 (${CONFIG.PRIZE[2].toLocaleString('ko-KR')}원) - ${rankList[2]}개`);
+    this.print(`6개 일치 (${CONFIG.PRIZE[1].toLocaleString('ko-KR')}원) - ${rankList[1]}개`);
+  },
+
+  printProfit(profit) {
+    this.print(`총 수익률은 ${profit}%입니다.`);
   },
 };
 
