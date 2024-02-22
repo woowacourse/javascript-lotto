30e9b51f19d2741e3fda524e72246daa1d725ac5
jinsil <jinsil378900@gmail.com>
2024-02-22 16:30:17 +0900

    refactor: setting에 따라 상수화하기

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -1,4 +1,5 @@
 import LottoMachine from '../src/domain/LottoMachine';
+import { SETTING } from '../src/constant/setting';
 
 describe('[LottoMachine] 로또 번호 발행 기능 테스트', () => {
   test('인자로 넘어온 금액에 해당하는 숫자 만큼의 로또 번호들을 발행해야 한다.', () => {
@@ -10,6 +11,6 @@ describe('[LottoMachine] 로또 번호 발행 기능 테스트', () => {
     const lottoNumberList = lottoMachine.getLottoNumberList();
 
     // then
-    expect(lottoNumberList.length).toBe(purchaseAmount / 1000);
+    expect(lottoNumberList.length).toBe(purchaseAmount / SETTING.LOTTO_PRICE);
   });
 });
