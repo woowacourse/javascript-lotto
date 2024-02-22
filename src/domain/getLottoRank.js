48965ee27a05e555bd7ed9d52ab53b2f9c886b45
pakxe <pigkill40@naver.com>
2024-02-22 15:56:33 +0900

    refactor: 함수 내부 로직을 분리
    Co-authored-by: greetings1012 <ahnqwerty@gmail.com>

--- src/domain/getLottoRank.js
+++ src/domain/getLottoRank.js
@@ -11,18 +11,21 @@ function getRankIndex(matchCount, isBonus) {
   return matchCount - 3;
 }
 
-// TODO: 리팩토링 해야한다.
+function calcMatchCountAndBonus({ winningLotto, bonusLottoNumber, randomLotto }) {
+  const matchCount = getMatchCount(randomLotto.get(), winningLotto.get());
+  const isBonus = isIncludingValue(randomLotto.get(), bonusLottoNumber.get());
+
+  return { matchCount, isBonus };
+}
+
 function getLottoRank({ winningLotto, bonusLottoNumber, randomLottos }) {
   const ranks = Array.from({ length: 5 }, () => 0);
 
   randomLottos.forEach((randomLotto) => {
-    const matchCount = getMatchCount(randomLotto.get(), winningLotto.get());
-    const isBonus = isIncludingValue(randomLotto.get(), bonusLottoNumber.get());
-
+    const { matchCount, isBonus } = calcMatchCountAndBonus({ winningLotto, bonusLottoNumber, randomLotto });
     if (matchCount < START_MATCH_COUNT) return;
 
     const rankIndex = getRankIndex(matchCount, isBonus);
-
     ranks[rankIndex] += 1;
   });
 
