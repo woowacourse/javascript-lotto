8f6b38bbfd626acf27e0f2ca393a94823e9d7fc3
jinsil <jinsil378900@gmail.com>
2024-02-22 15:57:16 +0900

    feat: outputView 당첨 결과 수정, 종료 메세지 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,27 +1,27 @@
-import GAME_MESSAGE from '../constant/gameMessage.js';
+import { GAME_MESSAGE, LOTTO_WINNING_RESULTS } from '../constant/gameMessage.js';
 import handleIO from '../util/handleIO.js';
 
 const OutputView = {
-  printPurchaseResult(purchaseAmount) {
-    handleIO.print(GAME_MESSAGE.PURCHASE_RESULT_TEXT(purchaseAmount));
-  },
-
-  printLottoItems(lottos) {
-    lottos.forEach((lotto) => {
+  printPurchaseResult(lottoList) {
+    handleIO.print(GAME_MESSAGE.PURCHASE_RESULT_TEXT(lottoList.length));
+    lottoList.forEach((lotto) => {
       handleIO.print(GAME_MESSAGE.LOTTO_ITEM(lotto));
     });
   },
 
-  printWinningResultTitle() {
+  printWinningResults(winningResults) {
     handleIO.print(GAME_MESSAGE.LOTTO_WINNING_RESULT_TITLE);
-  },
-
-  printWinningResults(match, reward, count) {
-    handleIO.print(GAME_MESSAGE.LOTTO_WINNING_RESULTS(match, reward, count));
+    Object.entries(winningResults).forEach(([rank, winningCount]) => {
+      handleIO.print(LOTTO_WINNING_RESULTS.WINNING_RESULT(rank, winningCount));
+    });
   },
 
   printProfitRate(profitRate) {
     handleIO.print(GAME_MESSAGE.LOTTO_PROFIT_RATE(profitRate));
+  },
+
+  printExitMessage() {
+    handleIO.print(GAME_MESSAGE.EXIT);
   },
 };
 
