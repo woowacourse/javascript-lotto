9063d3c5603ef52402214243a21f16d854eb787f
chysis <chysiss@naver.com>
2024-02-22 15:43:20 +0900

    feat(bonusNumberValidator): 보너스 번호가 당첨 번호와 중복되는 경우 예외처리

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,3 +1,4 @@
+import bonusNumberValidator from '../src/validators/bonusNumberValidator';
 import numberValidator from '../src/validators/numberValidator';
 import purchaseAmountValidator from '../src/validators/purchaseAmountValidator';
 
@@ -28,6 +29,19 @@ describe('유효성 검사 테스트', () => {
 
       const mockFn = () => {
         numberValidator.validateNumber(input);
+      };
+
+      expect(mockFn).toThrow('[ERROR]');
+    });
+  });
+
+  describe('보너스 번호 테스트', () => {
+    test('보너스 번호가 당첨 번호와 중복되는 경우 예외를 발생시킨다.', () => {
+      const winningNumbers = [1, 2, 3, 4, 5, 6];
+      const bonusNumber = 6;
+
+      const mockFn = () => {
+        bonusNumberValidator.validateDuplicate(bonusNumber, winningNumbers);
       };
 
       expect(mockFn).toThrow('[ERROR]');
