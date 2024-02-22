dca2dda35531fed40f65caf8d29aacca05156036
손진영 <sm099@naver.com>
2024-02-22 10:53:15 +0900

    test(RateOfReturnCalculator): WinningRank 내 멤버 변수 명 변경에 따른 테스트 케이스 수정
    Co-authored-by: Todari <rhymint@gmail.com>

--- src/domain/RateOfReturnCalculator/RateOfReturnCalculator.test.js
+++ src/domain/RateOfReturnCalculator/RateOfReturnCalculator.test.js
@@ -5,37 +5,37 @@ describe('수익률 계산 테스트', () => {
   const TEST_CASE = [
     {
       buyLottoPrice: 3_000,
-      winningRankDetail: { '1st': 1, '2nd': 0, '3rd': 0, '4th': 0, '5th': 0 },
+      winningRankResult: { '1st': 1, '2nd': 0, '3rd': 0, '4th': 0, '5th': 0 },
       expectedRateOfReturn: '66666666.7',
     },
     {
       buyLottoPrice: 9_000,
-      winningRankDetail: { '1st': 0, '2nd': 1, '3rd': 0, '4th': 0, '5th': 0 },
+      winningRankResult: { '1st': 0, '2nd': 1, '3rd': 0, '4th': 0, '5th': 0 },
       expectedRateOfReturn: '333333.3',
     },
     {
       buyLottoPrice: 8_000,
-      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 1, '4th': 0, '5th': 0 },
+      winningRankResult: { '1st': 0, '2nd': 0, '3rd': 1, '4th': 0, '5th': 0 },
       expectedRateOfReturn: '18750.0',
     },
     {
       buyLottoPrice: 3_000,
-      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 1, '5th': 0 },
+      winningRankResult: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 1, '5th': 0 },
       expectedRateOfReturn: '1666.7',
     },
     {
       buyLottoPrice: 1_000,
-      winningRankDetail: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 0, '5th': 1 },
+      winningRankResult: { '1st': 0, '2nd': 0, '3rd': 0, '4th': 0, '5th': 1 },
       expectedRateOfReturn: '500.0',
     },
   ];
 
   test.each(TEST_CASE)(
-    '로또 구매 금액이 $buyLottoPrice원 이고 등수 정보가 $winningRankDetail일 때 수익률은 $expectedRateOfReturn% 이다.',
-    ({ buyLottoPrice, winningRankDetail, expectedRateOfReturn }) => {
+    '로또 구매 금액이 $buyLottoPrice원 이고 등수 정보가 $winningRankResult일 때 수익률은 $expectedRateOfReturn% 이다.',
+    ({ buyLottoPrice, winningRankResult, expectedRateOfReturn }) => {
       const rateOfReturnCalculator = new RateOfReturnCalculator({
         buyLottoPrice,
-        winningRankDetail,
+        winningRankResult,
       });
 
       // when
