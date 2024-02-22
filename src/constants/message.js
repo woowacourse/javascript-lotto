f033662cd197b2b6ece7a9e295e32da1104ac94e
chysis <chysiss@naver.com>
2024-02-22 11:11:46 +0900

    refactor(message): 개행 문자 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,10 +1,12 @@
+const NEW_LINE = '\r\n';
+
 const MESSAGE = {
   PURCHASE_AMOUNT_INPUT: '> 구입금액을 입력해 주세요.',
   LOTTO_COUNT: '개를 구매했습니다.',
-  WINNING_NUMBERS_INPUT: '> 당첨 번호를 입력해 주세요. ',
-  BONUS_NUMBER_INPUT: '> 보너스 번호를 입력해 주세요. ',
-  LOTTO_RESULT: '당첨 통계\n--------------------',
-  RESTART: '> 다시 시작하시겠습니까? (y/n)',
+  WINNING_NUMBERS_INPUT: `${NEW_LINE}> 당첨 번호를 입력해 주세요. `,
+  BONUS_NUMBER_INPUT: `${NEW_LINE}> 보너스 번호를 입력해 주세요. `,
+  LOTTO_RESULT: `당첨 통계${NEW_LINE}--------------------`,
+  RESTART: `${NEW_LINE}> 다시 시작하시겠습니까? (y/n)`,
 };
 
 const ERROR_MESSAGE = {};
