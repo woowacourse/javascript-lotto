31de19dcda7459a8d5d9a5cb9170fc15ae4dcd57
jinsil <jinsil378900@gmail.com>
2024-02-23 15:27:43 +0900

    refator: 상수화 중 동적인 것 OutputView에서 바로 출력하기

--- src/constant/gameMessage.js
+++ src/constant/gameMessage.js
@@ -8,8 +8,6 @@ const GAME_MESSAGE = {
   PROMPT_WINNING_NUMBERS: generatePromptMessage('당첨 번호를 입력해 주세요. '),
   PROMPT_BONUS_NUMBER: generatePromptMessage('보너스 번호를 입력해 주세요. '),
   PROMPT_RESTART_COMMAND: generatePromptMessage('다시 시작하시겠습니까? (y/n) '),
-  PURCHASE_RESULT_TEXT: (lottoCount) => `${lottoCount}개를 구매했습니다.`,
-  LOTTO_ITEM: (lotto) => `[${lotto.join(', ')}]`,
   EXIT: '로또 게임을 종료합니다.',
 };
 
@@ -23,7 +21,6 @@ const LOTTO_WINNING_RESULTS = {
     const winningCountMessage = `${winningCount}개`;
     return `${matchingMessage} ${rewardMessage} - ${winningCountMessage}`;
   },
-  PROFIT_RATE: (profitRate) => `총 수익률은 ${profitRate}%입니다.`,
 };
 
 export { GAME_MESSAGE, LOTTO_WINNING_RESULTS };
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,9 +3,9 @@ import handleIO from '../util/handleIO.js';
 
 const OutputView = {
   printPurchaseResult(lottoList) {
-    handleIO.print(GAME_MESSAGE.PURCHASE_RESULT_TEXT(lottoList.length));
+    handleIO.print(`${lottoList.length}개를 구매했습니다.`);
     lottoList.forEach((lotto) => {
-      handleIO.print(GAME_MESSAGE.LOTTO_ITEM(lotto));
+      handleIO.print(`[${lotto.join(', ')}]`);
     });
   },
 
@@ -17,7 +17,7 @@ const OutputView = {
   },
 
   printProfitRate(profitRate) {
-    handleIO.print(LOTTO_WINNING_RESULTS.PROFIT_RATE(profitRate));
+    handleIO.print(`총 수익률은 ${profitRate}%입니다.`);
   },
 
   printExitMessage() {
