b68344a517086e51357871d448d9b87889cd687d
pakxe <pigkill40@naver.com>
2024-02-23 17:40:02 +0900

    rename: 명확한 의미를 가진 변수명으로 수정

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -5,22 +5,27 @@ import { sortNumbersAscend } from "../utils/sortNumbersAscend.js";
 import LottoNumber from "./LottoNumber.js";
 import { ERROR_MESSAGE } from "../error/ErrorMessage.js";
 import CustomError from "../error/CustomError.js";
+
+const LOTTO_CONSTANTS = {
+  NUMBER_COUNT: 6,
+  SEPARATOR: ",",
+};
+
 class Lotto {
   static NUMBER_COUNT = 6;
+
   static SEPARATOR = ",";
+
   #numbers;
 
   constructor(numbers) {
-    // NOTE: 생성자의 타입이 다양할 때 이렇게 조건문을 걸어서 판별하는게 옳을까요?
     if (typeof numbers === "string") this.#constructorWithNumStr(numbers);
     if (numbers === undefined) this.#constructorWithoutArg();
-
     this.#numbers = sortNumbersAscend(this.#numbers).map(
       (num) => new LottoNumber(num)
     );
   }
 
-  // 오버로딩
   #constructorWithNumStr(numbers) {
     const parsedNumbers = numbers.split(Lotto.SEPARATOR).map(Number);
 
@@ -33,7 +38,7 @@ class Lotto {
     this.#numbers = generateRandomNumbers(
       LottoNumber.MIN,
       LottoNumber.MAX,
-      Lotto.NUMBER_COUNT
+      LOTTO_CONSTANTS.NUMBER_COUNT
     );
   }
 
@@ -43,7 +48,7 @@ class Lotto {
 
   #validate(numbers) {
     // 중복없이 6개가 아닌 로또 넘버가 들어온 경우와 중복있는 로또 넘버가 들어온 경우의 오류를 다르게 주기 위해
-    if (!isExpectedArrayLength(numbers, Lotto.NUMBER_COUNT))
+    if (!isExpectedArrayLength(numbers, LOTTO_CONSTANTS.NUMBER_COUNT))
       throw new CustomError(ERROR_MESSAGE.lottoNumbersTooManyOrLess);
 
     if (isOverlapped(numbers))
@@ -51,13 +56,13 @@ class Lotto {
   }
 
   checkHaveBonus(bonusLottoNumber) {
-    const isAlreadyHave = this.#numbers.some(
+    const hasBonusNumber = this.#numbers.some(
       (num) => num.get() === bonusLottoNumber
     );
 
-    if (isAlreadyHave)
+    if (hasBonusNumber)
       throw new CustomError(ERROR_MESSAGE.bonusNumberOverlapped);
   }
 }
 
-export default Lotto;
+export { Lotto, LOTTO_CONSTANTS };
