51cab63a7de23a09627a025797e77817e74822ce
ImxYJL <allensain14@gmail.com>
2024-02-23 11:26:45 +0900

    fix: 금액 입력 에러에서 로또 관련 에러가 출력되는 버그 수정

--- src/validation/budgetValidation.js
+++ src/validation/budgetValidation.js
@@ -18,26 +18,12 @@ const budgetValidation = {
     },
 
     divisibleByMinimumPrice: {
-<<<<<<< HEAD
       errorMessage: ERROR_MESSAGE.DIVISIBLE_BY_MIN_PRICE,
       isValid(input) {
         return input % LOTTO_SETTING.PRICE === 0;
-=======
-      errorMessage: ERROR_MESSAGE.divisibleByMinimumPrice,
-      isValid(input) {
-        return input % 1000 === 0;
->>>>>>> e57b0b7 (feat: 로또 구입 금액 입력 구현)
       },
     },
   },
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default budgetValidation;
-=======
-export default BudgetValidation;
->>>>>>> e57b0b7 (feat: 로또 구입 금액 입력 구현)
-=======
-export default budgetValidation;
->>>>>>> bd91b0d (fix: budgetValidation camelCase로 수정 및 readBudget 메서드 수정)
