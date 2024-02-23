7a03d028274770bc58bc488eaed8184e689bc6cb
손진영 <sm099@naver.com>
2024-02-23 14:38:56 +0900

    remove(array): swap 함수 제거
    swap 함수를 더 이상 하지 않음에 따른 함수 제거

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,12 +1,4 @@
 /**
- * @template T
- * @param {{target : T[], indexA : number, indexB : number}} swapParams - array 내 요소 들을 swap을 위한 targetArray, indexA, indexB
- */
-export const swap = ({ targetArray, indexA, indexB }) => {
-  [targetArray[indexA], targetArray[indexB]] = [targetArray[indexB], targetArray[indexA]];
-};
-
-/**
  * @param {number[]} numbers - 숫자 들이 있는 배열
  * @returns {number[]} 오름차순으로 정렬된 숫자 배열
  */
