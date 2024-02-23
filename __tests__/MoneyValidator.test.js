95aab7e2b8357303b2b984ddafcd2a914a7db131
anttiey <hanuuny@gmail.com>
2024-02-23 15:47:51 +0900

    test: 폴더 및 함수 이름 변경에 따른 테스트 리팩토링

--- __tests__/MoneyValidator.test.js
+++ __tests__/MoneyValidator.test.js
@@ -4,18 +4,18 @@ describe('구입 금액 기능 테스트', () => {
   test('구입 금액이 숫자가 아니라면 에러를 발생시킨다.', () => {
     const money = '쿠키';
 
-    expect(() => MoneyValidator.validateMoneyType(money)).toThrow('[ERROR]');
+    expect(() => MoneyValidator.validateType(money)).toThrow('[ERROR]');
   });
 
   test('구입금액이 0 이하이면 에러를 발생시킨다', () => {
     const money = 0;
 
-    expect(() => MoneyValidator.validateMoneyMinimum(money)).toThrow('[ERROR]');
+    expect(() => MoneyValidator.validateRange(money)).toThrow('[ERROR]');
   });
 
   test('구입금액이 1000원 단위가 아니라면 에러를 발생시킨다.', () => {
     const money = 1001;
 
-    expect(() => MoneyValidator.validateMoneyUnit(money)).toThrow('[ERROR]');
+    expect(() => MoneyValidator.validateUnit(money)).toThrow('[ERROR]');
   });
 });
--- __tests__/RestartOptionValidator.test.js
+++ __tests__/RestartOptionValidator.test.js
@@ -1,9 +1,9 @@
-import RestartValidator from '../src/domain/RestartValidator';
+import RestartOptionValidator from '../src/domain/RestartOptionValidator';
 
 describe('로또 게임 재시작/종료 여부 입력 유효성 테스트', () => {
   test('재시작/종료 여부 입력 문자가 y/n이 아니라면 에러를 발생시킨다.', () => {
     const restartOption = '파슬리';
 
-    expect(() => RestartValidator.validateOptionCharacter(restartOption)).toThrow('[ERROR]');
+    expect(() => RestartOptionValidator.validateCharacter(restartOption)).toThrow('[ERROR]');
   });
 });
