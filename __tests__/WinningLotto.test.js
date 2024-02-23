675a78a42c76117d90bde10af9cc4c7bc84a01ee
Kim Da Eun <midekuna@gmail.com>
2024-02-23 19:44:49 +0900

    chore: WinningLotto 테스트 코드에 주석 추가

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -14,14 +14,17 @@ describe('WinningLotto 기능 테스트', () => {
   ])(
     'compareLotto - 추첨 로또와 당첨 로또의 비교 결과를 반환한다.',
     ({ comparedLottoNumbers, result }) => {
+      // given
       const WINNING_LOTTO_NUMBERS = new Lotto([1, 2, 3, 4, 5, 6]);
       const BONUS_NUMBER_INPUT = '7';
 
+      // when
       const winningLotto = new WinningLotto(
         WINNING_LOTTO_NUMBERS,
         BONUS_NUMBER_INPUT,
       );
 
+      // then
       expect(winningLotto.compareLotto(comparedLottoNumbers)).toEqual(result);
     },
   );
