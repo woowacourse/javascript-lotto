fa3ad52e2d08a179cf623ea017063fb86cf5232d
Hyelim Choi <HyelimChoi01@gmail.com>
2024-02-23 14:57:50 +0900

    refactor: LOTTO_PRICE으로 변수간 관계성 부여

--- __tests__/domain/LottoMachine.test.js
+++ __tests__/domain/LottoMachine.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable max-lines-per-function */
+import { LOTTO_PRICE } from "../../src/constants/system.js";
 import LottoMachine from "../../src/domain/LottoMachine.js";
 
 describe("LottoMachine 객체 테스트", () => {
   test("구입 금액만큼 로또를 생성한다.", () => {
     const PURCHASE_AMOUNT = 4_000;
-    const EXPECTED_LOTTO_COUNT = 4;
+    const EXPECTED_LOTTO_COUNT = PURCHASE_AMOUNT / LOTTO_PRICE;
     const lottoMachine = new LottoMachine(PURCHASE_AMOUNT);
 
     expect(lottoMachine.makeLottos(PURCHASE_AMOUNT).length).toBe(
