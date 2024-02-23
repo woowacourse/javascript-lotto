77cce753121f98acf892861893cdd390408c1cd1
ss0526100 <ss0526100@gmail.com>
2024-02-23 19:41:12 +0900

    refactor: 변수 분리 및 유효성 로직 수정

--- src/utils/RandomNumbersPicker.js
+++ src/utils/RandomNumbersPicker.js
@@ -3,6 +3,7 @@ class RandomUniquePositiveIntegersPicker {
   #MIN_UPPER = 1;
 
   #upper;
+  #maxCount;
   #itUsedBooleans;
 
   constructor(upper) {
@@ -11,8 +12,9 @@ class RandomUniquePositiveIntegersPicker {
     if (this.#MAX_UPPER < upper)
       throw new Error(`[ERROR] ${this.#MAX_UPPER}보다 작은 값이 들어와야 함`);
     if (upper < this.#MIN_UPPER)
-      throw new Error(`[ERROR] ${this.#MAX_UPPER}보다 작은 값이 들어와야 함`);
+      throw new Error(`[ERROR] ${this.#MIN_UPPER}보다 큰 값이 들어와야 함`);
     this.#upper = upper;
+    this.#maxCount = Math.floor(this.#upper / 2);
 
     this.#itUsedBooleans = new Array(1 + upper).fill(false);
   }
@@ -20,10 +22,14 @@ class RandomUniquePositiveIntegersPicker {
   getRandomUniquePositiveIntegers(count) {
     // count가 너무 많아지면 중복의 경우가 너무 많아져,
     // 중복된 무작위 값이 너무 많이 나오게 되므로, 시간이 너무 오래 걸림
-    const maxCount = Math.floor(this.#upper / 2);
-    if (maxCount / 2 < count)
+    // count가 Number나 BigInt, 숫자로 변형가능한 타입의 값이 아니라면
+    // false가 나오므로 예외 처리됨
+
+    // 이 클래스는 빠른 연산을 목적으로 만들어진 클래스 이므로
+    // 유효성 검사를 최대한 적은 시간안에 수행하도록 만들고자 해당 방식을 사용함
+    if (!(this.#maxCount > count))
       throw new Error(
-        `[ERROR] count는 최대 값의 절반 이하(${maxCount})여야 함`
+        `[ERROR] count는 최대 값의 절반 이하(${maxCount})인 Number형이여야 함`
       );
 
     const randomNumbers = this.#getRandomNumbers(count);
