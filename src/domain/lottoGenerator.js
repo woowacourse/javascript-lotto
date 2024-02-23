42a65a5917994a1fae94daad42328c702c237609
pakxe <pigkill40@naver.com>
2024-02-24 02:07:54 +0900

    chore: 정답 로또 생성기가 전용 클래스를 반환하도록 수정

--- src/domain/lottoGenerator.js
+++ src/domain/lottoGenerator.js
@@ -1,11 +1,13 @@
 import { LOTTO, LOTTO_NUMBER } from "../constants/lotto.js";
 import Lotto from "./Lotto.js";
 import generateRandomNumbers from "../utils/generateRandomNumbers.js";
+import WinningLotto from "./WinningLotto.js";
 
 function generateWinningLotto(numbers) {
   const parsedNumbers = numbers.split(",").map(Number);
+  const winningLotto = new Lotto(parsedNumbers);
 
-  return new Lotto(parsedNumbers);
+  return new WinningLotto(winningLotto);
 }
 
 function generateRandomLottos(count) {
