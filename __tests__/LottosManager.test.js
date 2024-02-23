023960e0fd1d2619a0395588a69cf8c9686ed81b
jinsil <jinsil378900@gmail.com>
2024-02-23 16:55:10 +0900

    Fix: Lottos 테스트 이름 LottosManager 로 변경

--- __tests__/LottosManager.test.js
+++ __tests__/LottosManager.test.js
@@ -1,4 +1,4 @@
-import Lottos from '../src/domain/Lottos';
+import LottosManager from '../src/domain/LottosManager';
 
 describe('[Lottos] 당첨 확인 로직 테스트', () => {
   test('각 등수별 로또 당첨 개수를 산출할 수 있다.', () => {
@@ -8,20 +8,20 @@ describe('[Lottos] 당첨 확인 로직 테스트', () => {
       [7, 8, 9, 10, 11, 12],
       [13, 14, 15, 16, 17, 18],
     ];
-    const lottos = new Lottos(lottoList);
+    const lottosManager = new LottosManager(lottoList);
     const winningNumbers = [2, 3, 4, 5, 6, 7];
     const bonusNumber = 1;
 
     // when
-    const winningResults = lottos.getWinningResults(winningNumbers, bonusNumber);
+    const winningResults = lottosManager.getWinningResults(winningNumbers, bonusNumber);
 
     // then
     const expectedResult = {
-      FIRST: 0,
-      SECOND: 1,
-      THIRD: 0,
-      FOURTH: 0,
-      FIFTH: 0,
+      6: { title: 'FIRST', count: 0 },
+      B5: { title: 'SECOND', count: 1 },
+      5: { title: 'THIRD', count: 0 },
+      4: { title: 'FOURTH', count: 0 },
+      3: { title: 'FIFTH', count: 0 },
     };
 
     expect(winningResults).toEqual(expectedResult);
