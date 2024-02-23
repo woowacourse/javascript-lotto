395701391db47521edeb7df5c8f9ce9dd9476632
anttiey <hanuuny@gmail.com>
2024-02-23 16:37:48 +0900

    test: 구입 금액 범위 유효성 테스트 추가

--- __tests__/MoneyValidator.test.js
+++ __tests__/MoneyValidator.test.js
@@ -7,15 +7,11 @@ describe('구입 금액 기능 테스트', () => {
     expect(() => MoneyValidator.validateType(money)).toThrow('[ERROR]');
   });
 
-  test('구입금액이 0 이하이면 에러를 발생시킨다', () => {
-    const money = 0;
-
+  test.each([0, 1000000001])('구입금액이 0 이하 1000000000 초과라면 에러를 발생시킨다', (money) => {
     expect(() => MoneyValidator.validateRange(money)).toThrow('[ERROR]');
   });
 
-  test('구입금액이 1000원 단위가 아니라면 에러를 발생시킨다.', () => {
-    const money = 1001;
-
+  test.each([1001, 1010, 1100])('구입금액이 1000원 단위가 아니라면 에러를 발생시킨다.', (money) => {
     expect(() => MoneyValidator.validateUnit(money)).toThrow('[ERROR]');
   });
 });
