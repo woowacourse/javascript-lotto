872157515d373d904e10d3afd6dc80374d4d6051
useon <yoosk4986@naver.com>
2024-02-23 17:55:14 +0900

    refactor: 인스턴스를 생성하지 않는 클래스를 객체로 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,20 +1,21 @@
 import Console from '../utils/Console';
 
-class InputView {
-  static async readPurchaseAmount() {
+const InputView = {
+  async readPurchaseAmount() {
     return await Console.readLineAsync('> 구입금액을 입력해 주세요.');
-  }
+  },
 
-  static async readWinningNumbers() {
-    return await Console.readLineAsync('\n> 당첨 번호를 입력해 주세요. ');
-  }
+  async readWinningNumbers() {
+    await Console.readLineAsync('\n> 당첨 번호를 입력해 주세요. ');
+  },
 
-  static async readBonusNumber() {
+  async readBonusNumber() {
     return await Console.readLineAsync('\n> 보너스 번호를 입력해 주세요. ');
-  }
-  static async readRestartOrExit() {
+  },
+
+  async readRestartOrExit() {
     return await Console.readLineAsync('\n> 다시 시작하시겠습니까? (y/n) ');
-  }
-}
+  },
+};
 
 export default InputView;
