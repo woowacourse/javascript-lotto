12a67a9850864d92d80e7d606591ef483365eb62
jaeml06 <jaeml0630@gmail.com>
2024-02-23 17:49:00 +0900

    fix: 재시작 에러 문구 수정

--- src/validation/responseValidation.js
+++ src/validation/responseValidation.js
@@ -3,7 +3,7 @@ import { ERROR_MESSAGE, RESPONSE_MESSAGE } from '../constants/message';
 const RestartResponseValidation = {
   validate(response = '') {
     if (response !== RESPONSE_MESSAGE.RESTART.YES && response !== RESPONSE_MESSAGE.RESTART.NO) {
-      throw new Error(`${ERROR_MESSAGE.PREFIX} ${RESPONSE_MESSAGE.RESTART_RESPONSE}`);
+      throw new Error(`${ERROR_MESSAGE.PREFIX} ${ERROR_MESSAGE.RESTART_RESPONSE}`);
     }
   },
 };
