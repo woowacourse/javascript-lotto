ff048ae1c641913dca4a874e6cb9a66bcec7cdbc
jaeml06 <jaeml0630@gmail.com>
2024-02-23 10:39:27 +0900

    refactor: 무엇을 테스트 하는지 설명 구체화

--- __tests__/lottoController.test.js
+++ __tests__/lottoController.test.js
@@ -2,9 +2,11 @@ import LottoController from '../src/controller/LottoController';
 
 describe('lottoController 클래스 내 수익률을 계산하는 함수를 검사하는 테스트입니다.', () => {
   test.each([
-    [[1, 0, 0, 0, 0], 8, '62.5'],
-    [[0, 0, 0, 1, 0], 30, '100000.0'],
-  ])('로또 결과에 따른 총 수익률을 계산하여 반환', (winResult, lottoCount, revenue) => {
+    // 각 배열의 첫 번째 항목을 테스트 설명으로 사용
+    ['5등 1회 당첨, 나머지 등수 미당첨 시 수익률 62.5%입니다', [1, 0, 0, 0, 0], 8, '62.5'],
+    ['1등(보너스 번호 포함) 1회 당첨 시 수익률 100000.0%입니다', [0, 0, 0, 1, 0], 30, '100000.0'],
+  ])('%s', (description, winResult, lottoCount, revenue) => {
+    // description을 테스트 이름으로 포함
     const lottoController = new LottoController();
 
     const result = [
--- __tests__/lottoProcess.test.js
+++ __tests__/lottoProcess.test.js
@@ -1,15 +1,15 @@
 import Lotto from '../src/domain/Lotto';
 import LottoProcess from '../src/domain/LottoProcess';
 
-describe('LottoProcess 클래스 검사', () => {
-  test('당첨 번호와 로또 번호가 몇개 일치하는지 확인', () => {
+describe('LottoProcess 클래스의 당첨 결과값을 담은 배열이 정상 반환되는지 확인하는 테스트입니다.', () => {
+  test('당첨 번호와 로또 번호가 몇개 일치하는지 확인합니다.', () => {
     const lottos = [new Lotto([1, 2, 3, 4, 5, 6])];
     const winLotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const lottoProcess = new LottoProcess(lottos);
     expect(lottoProcess.matchLottoNumbers(lottos[0], winLotto)).toBe(6);
   });
 
-  test('당첨결과를 배열로 반환', () => {
+  test('1등일때 당첨결과를 배열로 반환합니다.', () => {
     const lottos = [new Lotto([1, 2, 3, 4, 5, 6])];
     const winLotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const bonusNumber = 7;
