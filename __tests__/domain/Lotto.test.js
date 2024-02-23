a144d61ad00c01fca7ccbea5c99662db8b02d7e9
0jenn0 <jenn0.6n@gmail.com>
2024-02-23 14:01:29 +0900

    Test: Lotto.test에서 lotto 객체 생성 함수로 분리

--- __tests__/domain/Lotto.test.js
+++ __tests__/domain/Lotto.test.js
@@ -4,10 +4,12 @@ import { LOTTO_LENGTH, LOTTO_RANGE } from "../../src/constants/option.js";
 import Lotto from "../../src/domain/Lotto.js";
 
 describe("Lotto 객체 테스트", () => {
+  const createLottoWithNumbers = (numbers) => () => new Lotto(numbers);
+
   test(`로또 번호는 ${LOTTO_LENGTH}개여야한다.`, () => {
     const INVALID_LOTTO_LENGTH = [1, 2, 3, 4, 5];
 
-    expect(() => new Lotto(INVALID_LOTTO_LENGTH)).toThrow(
+    expect(createLottoWithNumbers(INVALID_LOTTO_LENGTH)).toThrow(
       ERROR_MESSAGE.INVALID_LOTTO_NUMBER_LENGTH,
     );
   });
@@ -15,7 +17,7 @@ describe("Lotto 객체 테스트", () => {
   test("로또 번호는 중복되면 안된다.", () => {
     const DUPLICATE_LOTTO_NUMBERS = [1, 1, 2, 3, 4, 5];
 
-    expect(() => new Lotto(DUPLICATE_LOTTO_NUMBERS)).toThrow(
+    expect(createLottoWithNumbers(DUPLICATE_LOTTO_NUMBERS)).toThrow(
       ERROR_MESSAGE.DUPLICATE_LOTTO_NUMBER,
     );
   });
@@ -23,7 +25,7 @@ describe("Lotto 객체 테스트", () => {
   test(`로또 번호는 ${LOTTO_RANGE.MIN} ~ ${LOTTO_RANGE.MAX} 사이의 숫자가 아니면 에러가 나야된다.`, () => {
     const INVALID_LOTTO_NUMBERS = [0, 1, 2, 3, 4, 46];
 
-    expect(() => new Lotto(INVALID_LOTTO_NUMBERS)).toThrow(
+    expect(createLottoWithNumbers(INVALID_LOTTO_NUMBERS)).toThrow(
       ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE,
     );
   });
@@ -31,7 +33,7 @@ describe("Lotto 객체 테스트", () => {
   test("로또 번호는 숫자만 입력할 수 있다.", () => {
     const INVALID_LOTTO_NUMBERS = ["하나", 2, 3, 4, 5, 6];
 
-    expect(() => new Lotto(INVALID_LOTTO_NUMBERS)).toThrow(
+    expect(createLottoWithNumbers(INVALID_LOTTO_NUMBERS)).toThrow(
       ERROR_MESSAGE.INVALID_LOTTO_NUMBER_TYPE,
     );
   });
@@ -39,6 +41,6 @@ describe("Lotto 객체 테스트", () => {
   test(`로또 번호는 ${LOTTO_RANGE.MIN} ~ ${LOTTO_RANGE.MAX} 사이의 숫자면 에러가 나지 않는다.`, () => {
     const VALID_LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6];
 
-    expect(() => new Lotto(VALID_LOTTO_NUMBERS)).not.toThrow();
+    expect(createLottoWithNumbers(VALID_LOTTO_NUMBERS)).not.toThrow();
   });
 });
