0c9cc50d734c2de14d1ffb9af381ccd9352bc4ab
jaeml06 <jaeml0630@gmail.com>
2024-02-23 10:53:01 +0900

    refactor: 테스트 문구에 어떤 문자가 입력됐는지도 같이 출력

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -2,10 +2,10 @@ import PurchaseAmountValidation from '../src/validation/purchaseAmount';
 import RestartResponseValidation from '../src/validation/responseValidation';
 
 describe('유효성 관련 함수 테스트', () => {
-  test.each([['0'], ['999'], ['-1000'], ['1000.5']])('유효하지 않은 구입 금액이면 에러 반환', (money) => {
+  test.each([['0'], ['999'], ['-1000'], ['1000.5']])('유효하지 않은 구입 금액 (%s) 이면 에러 반환합니다', (money) => {
     expect(() => PurchaseAmountValidation.validate(money)).toThrow('[ERROR]');
   });
-  test.each(['yes', '1', 1, '!'])('유효하지 않은 재시작 응답시 에러 반환', (restartResponse) => {
+  test.each(['yes', '1', 1, '!'])('유효하지 않은 재시작 응답 (%s)시 에러 반환합니다', (restartResponse) => {
     expect(() => RestartResponseValidation.validate(restartResponse)).toThrow('[ERROR]');
   });
 });
