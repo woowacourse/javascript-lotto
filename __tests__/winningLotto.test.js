d8e8a769134c1c3ddcd7ededa45771318b20f9d8
novice0840 <magnolia0840@gmail.com>
2024-02-23 14:33:32 +0900

    refactor: 테스트 이름 명시적으로 변경

--- __tests__/winningLotto.test.js
+++ __tests__/winningLotto.test.js
@@ -1,9 +1,9 @@
-/* eslint-disable */
 import Lotto from '../src/domain/lotto.js';
 import WinningLotto from '../src/domain/winningLotto.js';
+import { LOTTO_RULES } from '../src/constant/index.js';
 
 describe('로또 당첨 번호 객체 테스트', () => {
-  test('보너스 번호가 1이상 45이하가 아니면 에러 발생', () => {
+  test(`보너스 번호가 ${LOTTO_RULES.min_number}이상 ${LOTTO_RULES.max_number}이하가 아니면 에러가 발생한다`, () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const bonusNumber = 46;
 
@@ -12,7 +12,7 @@ describe('로또 당첨 번호 객체 테스트', () => {
     }).toThrow();
   });
 
-  test('당첨 번호와 보너스가 중복된다면 에러 발생', () => {
+  test('당첨 번호와 보너스가 중복된다면 에러가 발생한다', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const bonusNumber = 6;
 
