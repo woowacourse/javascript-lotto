7e83f355b6ade8670290b80ea0d601fc31c1206b
0jenn0 <jenn0.6n@gmail.com>
2024-02-23 16:56:05 +0900

    Fix: bonusNumberValidator에서 보너스 번호의 범위와 숫자인지 확인하는 검증 추가

--- src/validator/BonusNumberValidator.js
+++ src/validator/BonusNumberValidator.js
@@ -7,5 +7,25 @@ const bonusNumberValidator = {
       throw new AppError(ERROR.INVALID_BONUS_NUMBER_DUPLICATE);
     }
   },
+
+  validateRange(number) {
+    if (number < LOTTO_NUMBER_RANGE.MIN || number > LOTTO_NUMBER_RANGE.MAX) {
+      throw new AppError(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE);
+    }
+  },
+
+  validateIsNumber(number) {
+    if (!Number.isInteger(number)) {
+      throw new AppError(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_TYPE);
+    }
+  },
+
+  validate(number) {
+    const formatedNumbnumber = Number(number);
+    this.validateIsNumber(formatedNumbnumber);
+    this.validateRange(formatedNumbnumber);
+    this.validateDivided(formatedNumbnumber);
+  },
 };
+
 export default bonusNumberValidator;
