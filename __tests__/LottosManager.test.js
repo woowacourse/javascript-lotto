4d25c802dea2569253533d4bfbea1edd3ac0419e
jinsil <jinsil378900@gmail.com>
2024-02-23 17:26:06 +0900

    refactor: winningResults 값을 객체에서 숫자로 바꾼 테스트

--- __tests__/LottosManager.test.js
+++ __tests__/LottosManager.test.js
@@ -17,11 +17,11 @@ describe('[Lottos] 당첨 확인 로직 테스트', () => {
 
     // then
     const expectedResult = {
-      6: { title: 'FIRST', count: 0 },
-      B5: { title: 'SECOND', count: 1 },
-      5: { title: 'THIRD', count: 0 },
-      4: { title: 'FOURTH', count: 0 },
-      3: { title: 'FIFTH', count: 0 },
+      6: 0,
+      B5: 1,
+      5: 0,
+      4: 0,
+      3: 0,
     };
 
     expect(winningResults).toEqual(expectedResult);
