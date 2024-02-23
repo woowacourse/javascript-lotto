d55fca1932af789bdc61586562bdcb18bc211423
0jenn0 <jenn0.6n@gmail.com>
2024-02-23 16:38:54 +0900

    Refactor: PurchaseAmountValidator에서 Number 포맷팅 누락 수정 및 중복제거

--- src/validator/PurchaseAmountValidator.js
+++ src/validator/PurchaseAmountValidator.js
@@ -1,31 +1,31 @@
 import ERROR from "../constants/error-messages.js";
 import { LOTTO_PRICE } from "../constants/lotto-constants.js";
-
 import AppError from "../utils/Error.js";
 
 const purchaseAmountValidator = {
-  validateType(price) {
-    if (Number.isNaN(Number(price))) {
+  validateType(formatedPrice) {
+    if (Number.isNaN(formatedPrice)) {
       throw new AppError(ERROR.INVALID_PURCHASE_AMOUNT_TYPE);
     }
   },
 
-  validateRange(price) {
-    if (Number(price) < LOTTO_PRICE) {
+  validateRange(formatedPrice) {
+    if (formatedPrice < LOTTO_PRICE) {
       throw new AppError(ERROR.INVALID_PURCHASE_AMOUNT_RANGE);
     }
   },
 
-  validateDivided(price) {
-    if (price % LOTTO_PRICE !== 0) {
+  validateDivided(formatedPrice) {
+    if (formatedPrice % LOTTO_PRICE !== 0) {
       throw new AppError(ERROR.INVALID_PURCHASE_AMOUNT_DIVIDED);
     }
   },
 
   validate(price) {
-    this.validateType(price);
-    this.validateRange(price);
-    this.validateDivided(price);
+    const formatedPrice = Number(price);
+    this.validateType(formatedPrice);
+    this.validateRange(formatedPrice);
+    this.validateDivided(formatedPrice);
   },
 };
 
