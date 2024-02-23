71efb39274c918356d8440f1274e559dbd12f43c
손진영 <sm099@naver.com>
2024-02-23 14:43:49 +0900

    test(RetryHandler): 테스트 케이스 네이밍 수정
    execute 함수에 대한 테스트 임을 명시하기 위한 테스트 케이스 네이밍 수정

--- src/errors/RetryHandler/RetryHandler.test.js
+++ src/errors/RetryHandler/RetryHandler.test.js
@@ -11,10 +11,13 @@ describe('입력 관련 예외 처리 테스트', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     // given
-    executeTest = jest.fn().mockRejectedValueOnce(new Error('Test Error')).mockResolvedValueOnce('Success');
+    executeTest = jest
+      .fn()
+      .mockRejectedValueOnce(new Error('Test Error'))
+      .mockResolvedValueOnce('Success');
   });
 
-  test('함수가 두 번 호출된다.', async () => {
+  test('실행 되어야 할 execute 함수는 총 두 번 호출된다.', async () => {
     // when
     await RetryHandler.errorWithLogging(executeTest);
     // then
