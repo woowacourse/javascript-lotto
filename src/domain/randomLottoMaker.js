3d41b4239d1fb80a08c643ffde947b81f8fb73f7
bmj0053 <bmj05378@gmail.com>
2024-02-24 02:40:59 +0900

    fix: randomLottoMaker map 제거 및 기존과 동일한 결과 나오도록 수정

--- src/domain/randomLottoMaker.js
+++ src/domain/randomLottoMaker.js
@@ -1,9 +1,7 @@
 import random from "../utils/random.js";
 
 const randomLottoArray = (counts) => {
-  return Array.from({ length: counts }, () =>
-    random.pickUniqueNumbersInRange(1, 45, 6)
-  ).map((array) => array.sort((a, b) => a - b));
+  return Array.from({ length: counts }, () => random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b));
 };
 
 export default randomLottoArray;
