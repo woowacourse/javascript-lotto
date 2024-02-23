b6764bb4bc13ade8e6ab1a6873399ef684e4e234
anttiey <hanuuny@gmail.com>
2024-02-23 14:23:53 +0900

    test: LottoGame 클래스 역할 분리에 따른 테스트 리팩토링

--- __tests__/LottoGame.test.js
+++ __tests__/LottoGame.test.js
@@ -1,13 +1,11 @@
-import LottoGame from '../src/controller/LottoGame';
+import LottoGenerator from '../src/controller/LottoGenerator';
+import StatisticsGenerator from '../src/controller/StatisticsGenerator';
 import Lotto from '../src/domain/Lotto';
 
 describe('로또 게임 기능 테스트', () => {
-  const game = new LottoGame();
-
   test('구입 금액에 해당하는 만큼 1장에 1,000원인 로또를 발행한다.', () => {
     const money = 5000;
-    const lottoTickets = game.createLotto(money);
-    expect(lottoTickets.length).toBe(5);
+    expect(LottoGenerator.createLotto(money).length).toBe(5);
   });
 
   test('사용자가 구매한 전체 로또의 당첨 내역을 계산한다.', () => {
@@ -24,12 +22,18 @@ describe('로또 게임 기능 테스트', () => {
       new Lotto([4, 5, 6, 7, 8, 9]),
     ];
 
-    expect(game.calculateAllPrize(lottoTickets, winningLotto)).toEqual(['1', '2', '3', '4', '5']);
+    expect(StatisticsGenerator.calculateAllPrize(lottoTickets, winningLotto)).toEqual([
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+    ]);
   });
 
   test('당첨 내역을 통해 수익률을 계산한다.', () => {
     const prizes = ['5', '0', '0', '0', '0', '0', '0', '0'];
 
-    expect(game.calculateReturnOnInvestment(prizes)).toEqual(62.5);
+    expect(StatisticsGenerator.calculateReturnOnInvestment(prizes)).toEqual(62.5);
   });
 });
