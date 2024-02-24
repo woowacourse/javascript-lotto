855ced597d51a0cf9f04d74bf9d352c2b48ffe68
ooherin <555ohr@naver.com>
2024-02-24 12:32:56 +0900

    test: 수익률 계산기 테스트 추가

--- __tests__/lottoCalculator.test.js
+++ __tests__/lottoCalculator.test.js
@@ -3,9 +3,9 @@ import LottoCalculator from '../src/domain/LottoCalculator';
 describe('로또 수익 계산기 클래스 테스트', () => {
   test('로또 수익을 잘 계산해서 반환하는지 판단한다1.', () => {
     const lottoCalculator = new LottoCalculator();
-    const lottoWinResult = [0, 0, 0, 0, 1];
-    const lottosCount = 5;
-    expect(lottoCalculator.getRateOfRevenue(lottoWinResult, lottosCount)).toBe('100.0');
+    const lottoWinResult = [0, 1, 0, 0, 0];
+    const lottosCount = 2;
+    expect(lottoCalculator.getRateOfRevenue(lottoWinResult, lottosCount)).toBe('1500000.0');
   });
 
   test('로또 수익을 잘 계산해서 반환하는지 판단한다2.', () => {
