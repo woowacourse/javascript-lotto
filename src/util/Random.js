3aaa3d8872dfcec57469a1be96142f30e9132206
jaeml06 <jaeml0630@gmail.com>
2024-02-25 00:45:26 +0900

    refactor: controller의 getRandomNumbers와 병합

--- src/util/Random.js
+++ src/util/Random.js
@@ -1,6 +1,11 @@
 const Random = {
-  pickNumberInRange(from = 0, to = 0) {
-    return Math.floor(Math.random() * (to - from + 1)) + from;
+  pickUniqueNumbersInRange({from = 0, to = 0, size= 0}) {
+    const numbers = new Set();
+    while (numbers.size < size) {
+      const randomNum = Math.floor(Math.random() * (to - from + 1)) + from;
+      numbers.add(randomNum);
+    }
+    return Array.from(numbers);
   },
 };
 export default Random;
