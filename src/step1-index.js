7b088432e3e41d7f422973191cdf3f92046f897a
brgndy <brgndy96@gmail.com>
2024-02-24 12:44:13 +0900

    refactor: 필요없는 파일 삭제 및 진입점 파일 컨트롤러 이름 수정

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,11 +1,11 @@
-import Controller from "./Controller";
+import LottoController from './LottoController';
 
 /**
  * step 1의 시작점이 되는 파일입니다.
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 async function start() {
-  const app = new Controller();
+  const app = new LottoController();
   await app.run();
 }
 
