62893f360bcc1696fb0e0947106fabd1f39d91df
pakxe <pigkill40@naver.com>
2024-02-24 15:15:05 +0900

    test: 테스트 이름 내부의 '자료형' 표현 수정

--- __tests__/LottoMoney.test.js
+++ __tests__/LottoMoney.test.js
@@ -8,20 +8,13 @@ describe("로또머니 클래스 기능 테스트", () => {
   test.each([MINIMUM_MONEY_THRESHOLD, MAXIMUM_MONEY_THRESHOLD])(
     "구입 금액이 1000 미만이거나 1,000,000,000을 초과하면 오류를 발생시킨다.",
     (wrongMoney) => {
-      expect(() => new LottoMoney(wrongMoney)).toThrow(
-        ERROR_MESSAGE.lottoMoneyNotInRange
-      );
+      expect(() => new LottoMoney(wrongMoney)).toThrow(ERROR_MESSAGE.lottoMoneyNotInRange);
     }
   );
 
-  test.each(["a", 123.45])(
-    "구입 금액의 자료형이 정수가 아니면 오류를 발생시킨다.",
-    (wrongMoney) => {
-      expect(() => new LottoMoney(wrongMoney)).toThrow(
-        ERROR_MESSAGE.lottoMoneyNotInteger
-      );
-    }
-  );
+  test.each(["a", 123.45])("구입 금액이 정수가 아니면 오류를 발생시킨다.", (wrongMoney) => {
+    expect(() => new LottoMoney(wrongMoney)).toThrow(ERROR_MESSAGE.lottoMoneyNotInteger);
+  });
 
   test("구입 금액에 따라 로또 발행 개수를 반환한다.", () => {
     const money = 3000;
