314d157676d1e0314623ec3f26d35ebb55937984
chysis <chysiss@naver.com>
2024-02-24 22:56:54 +0900

    refactor(InputView): 불필요한 객체 제거, 입력마다 재사용 가능한 메서드 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,4 +1,4 @@
-import CONFIG from '../constants/config';
+import { CONFIG_FORMAT } from '../constants/config';
 import { MESSAGE } from '../constants/message';
 import readLineAsync from '../utils/readLineAsync';
 import bonusNumberValidator from '../validators/bonusNumberValidator';
@@ -7,31 +7,17 @@ import OutputView from './OutputView';
 import restartValidator from '../validators/restartValidator';
 import winningNumbersValidator from '../validators/winningNumbersValidator';
 
-const Private = {
-  readPurchaseAmount() {
-    return readLineAsync(MESSAGE.PURCHASE_AMOUNT_INPUT);
-  },
-
-  readWinningNumbers() {
-    return readLineAsync(MESSAGE.WINNING_NUMBERS_INPUT);
-  },
-
-  readBonusNumber() {
-    return readLineAsync(MESSAGE.BONUS_NUMBER_INPUT);
-  },
-
-  readRestart() {
-    return readLineAsync(MESSAGE.RESTART);
-  },
-};
-
 const InputView = {
+  readWithMessage(message) {
+    return readLineAsync(message);
+  },
+
   async readPurchaseAmount() {
     try {
-      const purchaseAmountInput = await Private.readPurchaseAmount();
+      const purchaseAmountInput = await this.readWithMessage(MESSAGE.PURCHASE_AMOUNT_INPUT);
       const purchaseAmount = purchaseAmountInput.trim();
       purchaseAmountValidator.validate(purchaseAmount);
-      return purchaseAmount;
+      return parseInt(purchaseAmount, 10);
     } catch (error) {
       OutputView.print(error.message);
       return this.readPurchaseAmount();
@@ -40,8 +26,9 @@ const InputView = {
 
   async readWinningNumbers() {
     try {
-      const winningNumbersInput = await Private.readWinningNumbers();
-      const winningNumbers = winningNumbersInput.split(CONFIG.SEPARATOR).map(number => parseInt(number.trim(), 10));
+      const winningNumbersInput = await this.readWithMessage(MESSAGE.WINNING_NUMBERS_INPUT);
+      let winningNumbers = winningNumbersInput.split(CONFIG_FORMAT.SEPARATOR);
+      winningNumbers = winningNumbers.map(number => parseInt(number.trim(), 10));
       winningNumbersValidator.validate(winningNumbers);
       return winningNumbers;
     } catch (error) {
@@ -52,7 +39,7 @@ const InputView = {
 
   async readBonusNumber(winningNumbers) {
     try {
-      const bonusNumberInput = await Private.readBonusNumber();
+      const bonusNumberInput = await this.readWithMessage(MESSAGE.BONUS_NUMBER_INPUT);
       const bonusNumber = parseInt(bonusNumberInput, 10);
       bonusNumberValidator.validate(bonusNumber, winningNumbers);
       return bonusNumber;
@@ -64,7 +51,7 @@ const InputView = {
 
   async readRestart() {
     try {
-      const restartInput = await Private.readRestart();
+      const restartInput = await this.readWithMessage(MESSAGE.RESTART);
       const restart = restartInput.toLowerCase();
       restartValidator.validate(restart);
       return restart;
