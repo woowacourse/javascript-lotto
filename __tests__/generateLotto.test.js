b1591d27353387ef7efc57c51fc801a32853c633
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-02-24 13:21:48 +0900

    test: 테스트 중복 최소화 (beforeEach를 이용한 LottoGenerator 초기화 로직 분리)

--- __tests__/generateLotto.test.js
+++ __tests__/generateLotto.test.js
@@ -1,27 +1,28 @@
 import LottoGenerator from '../src/domains/LottoGenerator';
 
 describe('당첨 번호 6개를 랜덤으로 생성하는지 확인하는 테스트.', () => {
-  test('생성된 배열 안의 숫자가 1 ~ 45 사이의 숫자인지 확인한다.', () => {
-    const lottoGenerator = new LottoGenerator();
-    const lottoList = lottoGenerator.generateRandomLotto();
+  let lottoGenerator;
+  let generatedLottos;
+  const ticketCount = 1;
 
-    lottoList.forEach((lotto) => {
+  beforeEach(() => {
+    lottoGenerator = new LottoGenerator(ticketCount);
+    generatedLottos = lottoGenerator.generateLotto();
+  });
+
+  test('생성된 배열 안의 랜덤 값이 1 ~ 45 사이의 숫자인지 확인한다.', () => {
+    generatedLottos.forEach((lotto) => {
       expect(lotto).toBeGreaterThanOrEqual(1);
       expect(lotto).toBeLessThanOrEqual(45);
     });
   });
 
-  test('생성된 배열 안의 숫자가 6글자인지 확인한다.', () => {
-    const lottoGenerator = new LottoGenerator();
-    const lottoList = lottoGenerator.generateRandomLotto();
-
-    expect(lottoList.length).toBe(6);
+  test('생성된 배열 안의 랜덤 값의 개수가 6개인지 확인한다.', () => {
+    expect(generatedLottos.length).toBe(6);
   });
 
-  test('생성된 배열 안의 숫자가 중복되지 않는지 확인한다.', () => {
-    const lottoGenerator = new LottoGenerator();
-    const lottoList = lottoGenerator.generateRandomLotto();
-    const lottoSet = new Set(lottoList);
+  test('생성된 배열 안의 랜덤 값들이 중복되지 않는지 확인한다.', () => {
+    const lottoSet = new Set(generatedLottos);
 
     expect(lottoSet.size).toBe(6);
   });
