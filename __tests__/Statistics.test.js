377cf441895c594b11027c3bd2177b52dce079d1
badahertz52 <badahertz52@gmail.com>
2024-02-24 18:22:39 +0900

    test: Statistics 리팩토링에 따른 Statistics.test 수정

--- __tests__/Statistics.test.js
+++ __tests__/Statistics.test.js
@@ -2,7 +2,7 @@ import Statistics from '../src/domains/Statistics';
 
 describe('Statistics 기능 테스트', () => {
   test('여러 장의 티켓의 비교해서 일치 여부에 따른 등수를 반환한다.', () => {
-    const RESULTS = [
+    const MATCHING_RESULTS = [
       { isBonus: false, matchedCount: 4 },
       { isBonus: true, matchedCount: 4 },
       { isBonus: false, matchedCount: 5 },
@@ -11,23 +11,20 @@ describe('Statistics 기능 테스트', () => {
       { isBonus: false, matchedCount: 1 },
     ];
     const RANKS = { 1: 0, 2: 1, 3: 1, 4: 2, 5: 0 };
-    const PAYMENT_AMOUNT = RESULTS.length * 1_000;
+    const PAYMENT_AMOUNT = MATCHING_RESULTS.length * 1_000;
 
-    const statistics = new Statistics(PAYMENT_AMOUNT);
-    statistics.matchResultsToRank(RESULTS);
+    const statistics = new Statistics(MATCHING_RESULTS, PAYMENT_AMOUNT);
 
-    expect(statistics.statisticsResult).toEqual(RANKS);
+    expect(statistics.lottoAnalytics.statisticsResult).toEqual(RANKS);
   });
 
   test('구매금액과 당첨 금액을 이용해 총 수익률을 반환한다.', () => {
-    const RESULTS = [{ isBonus: false, matchedCount: 4 }];
+    const MATCHING_RESULTS = [{ isBonus: false, matchedCount: 4 }];
     const PAYMENT_AMOUNT = 1000;
     const PROFIT_RATE = 5000;
 
-    const statistics = new Statistics();
-    statistics.matchResultsToRank(RESULTS);
-    statistics.calculateProfitRate(PAYMENT_AMOUNT);
+    const statistics = new Statistics(MATCHING_RESULTS, PAYMENT_AMOUNT);
 
-    expect(statistics.profitRate).toBe(PROFIT_RATE);
+    expect(statistics.lottoAnalytics.profitRate).toBe(PROFIT_RATE);
   });
 });
