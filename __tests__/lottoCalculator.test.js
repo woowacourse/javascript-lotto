6707152ae5b34c684f55c96806eeac3723bfdd2c
GimGayeon <kky1373@naver.com>
2024-02-24 19:04:26 +0900

    fix:보너스 번호, 당첨 번호 비교 테스트 명세 수정

--- __tests__/lottoCalculator.test.js
+++ __tests__/lottoCalculator.test.js
@@ -19,7 +19,7 @@ describe('당첨 번호와 발행 번호 비교하는 테스트', () => {
     expect(result).toEqual(3);
   });
 
-  test('보너스 번호가 일치하면 true를 반환한다.', () => {
+  test('보너스 번호가 당첨 번호 리스트에 있으면 true를 반환한다.', () => {
     const lottoNumbers = {
       winningNumbers: [1, 2, 3, 4, 5, 6],
       bonusNumber: 7,
