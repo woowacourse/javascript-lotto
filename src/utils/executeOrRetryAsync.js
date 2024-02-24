95c6443a92c02b471f9a1a88fdb90df73dc87b9c
river <jenn0.6n@gmail.com>
2024-02-25 01:49:08 +0900

    Chore: executeOrRetryAsync에 eslint 무시 주석 추가

--- src/utils/executeOrRetryAsync.js
+++ src/utils/executeOrRetryAsync.js
@@ -1,8 +1,8 @@
+import AppError from './Error.js';
 import ERROR_MESSAGE from '../constants/error-messages.js';
 
-import AppError from './Error.js';
-
 /* eslint-disable max-lines-per-function */
+/* eslint-disable max-depth */
 export default async function executeOrRetryAsync({ asyncFn, handleError, retryLimit = 10, attempts = 0 }) {
   try {
     return await asyncFn();
