c19cc49f47037ae40b6a836d93f73ab99c934bf6
0jenn0 <jenn0.6n@gmail.com>
2024-02-24 14:21:08 +0900

    Refactor: LottoMachine 테스트 test each로 더 자세하게 테스트 케이스 구성

--- __tests__/domain/LottoMachine.test.js
+++ __tests__/domain/LottoMachine.test.js
@@ -3,13 +3,21 @@ import { LOTTO_PRICE } from "../../src/constants/lotto-constants.js";
 import LottoMachine from "../../src/domain/LottoMachine.js";
 
 describe("LottoMachine 객체 테스트", () => {
-  test("구입 금액만큼 로또 인스턴스를 생성한다.", () => {
-    const PURCHASE_AMOUNT = 4_000;
-    const EXPECTED_LOTTO_COUNT = 4;
-    const lottoMachine = new LottoMachine(PURCHASE_AMOUNT);
+  describe("구입금액만큼 로또 인스턴스를 만들어낸다.", () => {
+    test.each([
+      { purcaseAmount: 1_000, expectedLottoCount: 1 },
+      { purcaseAmount: 2_000, expectedLottoCount: 2 },
+      { purcaseAmount: 3_000, expectedLottoCount: 3 },
+      { purcaseAmount: 4_000, expectedLottoCount: 4 },
+    ])(
+      "$purcaseAmount원치 구매시 로또 인스턴스는 $expectedLottoCount개 생성한다.",
+      ({ purcaseAmount, expectedLottoCount }) => {
+        const lottoMachine = new LottoMachine(purcaseAmount);
 
-    expect(lottoMachine.makeLottos(LOTTO_PRICE).length).toBe(
-      EXPECTED_LOTTO_COUNT,
+        expect(lottoMachine.makeLottos(LOTTO_PRICE).length).toBe(
+          expectedLottoCount,
+        );
+      },
     );
   });
 });
