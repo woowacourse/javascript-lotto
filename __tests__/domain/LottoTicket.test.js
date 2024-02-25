39a29724af6a07e4f24edaf81ce4bd787255eea3
soosoo22 <lllllh00@naver.com>
2024-02-25 18:19:56 +0900

    test: 로또 번호 생성 테스트 코드 수정
    - 6개의 수로 이루어진 로또 번호를 생성한다.
    - 1 ~ 45 범위의 숫자로 이루어진 로또 번호를 생성한다.
    - 중복 없이 6개의 로또 번호를 생성한다.

--- __tests__/domain/LottoTicket.test.js
+++ __tests__/domain/LottoTicket.test.js
@@ -1,16 +1,21 @@
 import LottoTicket from '../../src/domain/LottoTicket';
 
-describe('로또 티켓을 구성하는 랜덤 값을 뽑는 기능 테스트', () => {
-  test('랜덤 값이 중복 없이 6개 뽑히는지 확인한다.', () => {
-    const lottoTicket = new LottoTicket();
-    const uniqueNumbers = new Set(lottoTicket.ticket);
+describe('로또 번호 생성 테스트', () => {
+  test('6개의 수로 이루어진 로또 번호를 생성한다.', () => {
+    const lottoTickets = new LottoTicket().tickets;
 
-    expect(uniqueNumbers.size).toBe(6);
+    expect(lottoTickets.length).toBe(6);
   });
 
-  test('1 ~ 45 범위의 숫자가 뽑히는 확인한다.', () => {
-    const lottoTicket = new LottoTicket().ticket;
+  test('1 ~ 45 범위의 숫자로 이루어진 로또 번호를 생성한다.', () => {
+    const lottoTickets = new LottoTicket().tickets;
 
-    expect(lottoTicket.every((lottoNumber) => lottoNumber >= 1 && lottoNumber <= 45)).toBeTruthy();
+    expect(lottoTickets.every((lottoNumber) => lottoNumber >= 1 && lottoNumber <= 45)).toBeTruthy();
+  });
+
+  test('중복 없이 6개의 로또 번호를 생성한다.', () => {
+    const lottoTickets = new LottoTicket().tickets;
+
+    expect(new Set(lottoTickets).size).toBe(6);
   });
 });
