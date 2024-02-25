e45f8d9226b78ce4b30157b37cb61709322444ff
soosoo22 <lllllh00@naver.com>
2024-02-25 18:35:34 +0900

    rafactor: 메시지 출력을 위한 MESSAGE, RESULT 상수 적용

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,26 +1,27 @@
+import { MESSAGE, RESULT } from '../constants/message';
+
 const OutputView = {
   printPurchaseCount(purchaseCount) {
-    console.log(`${purchaseCount}개를 구매했습니다.`);
+    console.log(`${purchaseCount}${MESSAGE.NUMBER_OF_TICKETS_PURCHASED}`);
   },
 
-  printLottoTickets(tickets) {
-    tickets.forEach((ticket) => {
-      console.log(`[${ticket.join(', ')}]`);
+  printLottoTickets(lottoTickets) {
+    lottoTickets.forEach((lottoTicket) => {
+      console.log(`[${lottoTicket.tickets.join(', ')}]`);
     });
   },
 
   printWinningStats(matchingResult) {
-    console.log('\n당첨 통계');
-    console.log('--------------------');
-    console.log(`3개 일치 (5,000원) - ${matchingResult[5]}개`);
-    console.log(`4개 일치 (50,000원) - ${matchingResult[4]}개`);
-    console.log(`5개 일치 (1,500,000원) - ${matchingResult[3]}개`);
-    console.log(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchingResult[2]}개`);
-    console.log(`6개 일치 (2,000,000,000원) - ${matchingResult[1]}개`);
+    console.log(`${MESSAGE.WINNING_STATS}\n${MESSAGE.SEPARATOR}`);
+    Object.keys(matchingResult)
+      .reverse()
+      .map((rank) => {
+        console.log(`${RESULT[rank]}${matchingResult[rank]}개`);
+      });
   },
 
   printRateOfReturn(rateOfReturn) {
-    console.log(`총 수익률은 ${rateOfReturn}%입니다.`);
+    console.log(`${MESSAGE.PROFIT.replace('_', rateOfReturn)}`);
   },
 };
 
