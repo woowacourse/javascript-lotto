9cb3cafed9029828f29fec4f3cceef54979e7d17
soosoo22 <lllllh00@naver.com>
2024-02-25 18:38:30 +0900

    chore: constant, validator 폴더명 변경

--- src/validator/RestartOrExitValidator.js
+++
@@ -1,7 +0,0 @@
-const RestartOrExitValidator = {
-  isNotRestartOrExitKeyword(inputValue) {
-    return inputValue !== 'y' && inputValue !== 'n' ? true : false;
-  },
-};
-
-export default RestartOrExitValidator;
