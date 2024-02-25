09835f375e9f8f2fc9f934364a0b76c16c3420e8
soosoo22 <lllllh00@naver.com>
2024-02-25 19:41:23 +0900

    refactor: LottoController의 재시작/종료 기능을 App 클래스로 이동

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import LottoController from './controller/LottoController';
+import { InputView } from './view';
+import { RestartOrExitValidator } from './validators';
+import { RESTART } from './constants/message';
+import retryOrEnd from './utils/RetryOrEnd';
 
 class App {
   async init() {
-    await new LottoController().start();
+    const lottoController = new LottoController();
+    await lottoController.start();
+
+    if ((await retryOrEnd([this.processRestartOrExit, this])) === RESTART.YES) this.init();
+  }
+
+  async processRestartOrExit() {
+    const inputValue = await InputView.readRestartOrExit();
+    RestartOrExitValidator.validateKeyword(inputValue);
+
+    return inputValue;
   }
 }
 
