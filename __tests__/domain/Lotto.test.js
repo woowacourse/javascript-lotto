6c055d6cc56185524e87a8f67c8976c8ae081b3e
river <jenn0.6n@gmail.com>
2024-02-25 10:03:17 +0900

    Fix: Lotto 객체 테스트에서 테스트할 값 수정
    - isBonus 상관 없는게 아니라 false. 테스트 케이스명도 너무 과한것 같아 수정

--- __tests__/domain/Lotto.test.js
+++ __tests__/domain/Lotto.test.js
@@ -2,9 +2,6 @@
 import Lotto from '../../src/domain/lotto.js';
 
 describe('Lotto 객체 테스트', () => {
-  const HAVE_BONUS_NUMBER_TEXT = '가지고있으면';
-  const WHATEVER_TEXT = '상관없이';
-
   describe('로또 객체는 자신이 몇등인지 알아야한다.', () => {
     const winningLottoNumbers = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 20;
@@ -13,41 +10,41 @@ describe('Lotto 객체 테스트', () => {
       {
         lottoNumbers: [1, 2, 3, 4, 5, 6],
         matchCount: 6,
-        isBonus: WHATEVER_TEXT,
+        isBonus: false,
         expectedRank: 'FIRST_PLACE',
       },
       {
         lottoNumbers: [1, 2, 3, 4, 5, 20],
         matchCount: 5,
-        isBonus: HAVE_BONUS_NUMBER_TEXT,
+        isBonus: true,
         expectedRank: 'SECOND_PLACE',
       },
       {
         lottoNumbers: [1, 2, 3, 4, 5, 7],
         matchCount: 5,
-        isBonus: WHATEVER_TEXT,
+        isBonus: false,
         expectedRank: 'THIRD_PLACE',
       },
       {
         lottoNumbers: [1, 2, 3, 4, 7, 8],
         matchCount: 5,
-        isBonus: WHATEVER_TEXT,
+        isBonus: false,
         expectedRank: 'FOURTH_PLACE',
       },
       {
         lottoNumbers: [1, 2, 3, 7, 8, 9],
         matchCount: 5,
-        isBonus: WHATEVER_TEXT,
+        isBonus: false,
         expectedRank: 'FIFTH_PLACE',
       },
       {
         lottoNumbers: [7, 8, 9, 10, 11, 12],
         matchCount: 5,
-        isBonus: WHATEVER_TEXT,
+        isBonus: false,
         expectedRank: 'NONE_PLACE',
       },
     ])(
-      '당첨번호와 $matchCount개 일치하고 보너스 번호 $isBonus $expectedRank를 리턴한다.',
+      '당첨번호와 $matchCount개 일치하고 isBonus가 $isBonus이면 $expectedRank를 리턴한다.',
       ({ lottoNumbers, expectedRank }) => {
         const lotto = new Lotto(lottoNumbers);
         const rank = lotto.getRank({ winningLottoNumbers, bonusNumber });
