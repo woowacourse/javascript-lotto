38b0def0b80724adb5c99a1a0b36e19b83e31b21
Seongjin Hong <hongseongjin.to@gmail.com>
2024-02-25 15:40:11 +0900

    refactor: 로또 구매 금액의 최대값 설정 및 검증 로직 추가

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -3,10 +3,11 @@ import Validator from '../src/validator/Validator';
 
 describe('[Validator] 로또 구입 금액 검증', () => {
   test.each`
-    title                               | input       | errorMessage
-    ${'입력값은 공백이 아니여야 한다.'} | ${''}       | ${ERROR_MESSAGE.INPUT_IS_EMPTY}
-    ${'입력값은 숫자여야 한다.'}        | ${'string'} | ${ERROR_MESSAGE.INPUT_IS_NOT_NUMBER}
-    ${'입력 값은 1000 단위여야 한다.'}  | ${'500'}    | ${ERROR_MESSAGE.PURCHASE_AMOUNT_NOT_DIVIDED}
+    title                                   | input       | errorMessage
+    ${'입력값은 공백이 아니여야 한다.'}     | ${''}       | ${ERROR_MESSAGE.INPUT_IS_EMPTY}
+    ${'입력값은 숫자여야 한다.'}            | ${'string'} | ${ERROR_MESSAGE.INPUT_IS_NOT_NUMBER}
+    ${'입력값은 1000 단위여야 한다.'}       | ${'500'}    | ${ERROR_MESSAGE.PURCHASE_AMOUNT_NOT_DIVIDED}
+    ${'입력값은 100,000을 넘겨선 안 된다.'} | ${'101000'} | ${ERROR_MESSAGE.EXCEEDED_MAX_PURCHASE_AMOUNT}
   `('$title', ({ input, errorMessage }) => {
     const validation = () => Validator.validatePurchaseAmount(input);
     expect(validation).toThrow(errorMessage);
