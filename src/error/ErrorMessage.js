e08db3ca949b5dde5b7d3e391753d027df6f29b0
greetings1012 <ahnqwerty@gmail.com>
2024-02-26 16:54:50 +0900

    refactor: 에러 메시지 각 에러에 해당하는 내용으로 분리해서 재작성

--- src/error/ErrorMessage.js
+++ src/error/ErrorMessage.js
@@ -11,12 +11,12 @@ const ERROR_MESSAGE = {
   lottoNumbersOverlapped: "당첨 번호는 중복될 수 없습니다.",
   bonusNumberOverlapped: "보너스 번호는 당첨 번호와 중복될 수 없습니다.",
 
-  lottoMoneyNotNumber: `구입 금액은 ${LottoMoney.MIN}원 이상 ${LottoMoney.MAX}원 이하의 정수로 입력해야 합니다.`,
-  lottoMoneyNotInRange: `구입 금액은 ${LottoMoney.MIN}원 이상 ${LottoMoney.MAX}원 이하의 정수로 입력해야 합니다.`,
-  lottoMoneyNotInteger: `구입 금액은 ${LottoMoney.MIN}원 이상 ${LottoMoney.MAX}원 이하의 정수로 입력해야 합니다.`,
+  lottoMoneyNotNumber: `구입 금액은 숫자로 입력해야 합니다.`,
+  lottoMoneyNotInRange: `구입 금액은 ${LottoMoney.MIN}원 이상 ${LottoMoney.MAX}원 이하의 숫자로 입력해야 합니다.`,
+  lottoMoneyNotInteger: `구입 금액은 정수로 입력해야 합니다.`,
 
-  lottoNumberNotNumber: `각 로또 번호는 ${LottoNumber.MIN} 이상 ${LottoNumber.MAX} 이하의 정수로 입력해야 합니다.`,
-  lottoNumberOutOfRange: `각 로또 번호는 ${LottoNumber.MIN} 이상 ${LottoNumber.MAX} 이하의 정수로 입력해야 합니다.`,
+  lottoNumberNotNumber: `각 로또 번호는 숫자로 입력해야 합니다.`,
+  lottoNumberOutOfRange: `각 로또 번호는 ${LottoNumber.MIN} 이상 ${LottoNumber.MAX} 이하의 숫자로 입력해야 합니다.`,
 
   commandNotInList: "잘못된 커맨드 입력입니다.",
 
