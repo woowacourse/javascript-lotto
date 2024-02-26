afec577f774155139de3b01f850613939b143c13
Largopie <106071687+Largopie@users.noreply.github.com>
2024-02-26 20:27:39 +0900

    refactor(constants): 상수명 수정 및 반영

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,9 +1,5 @@
 export const SYMBOL = {
-  one: 1,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
+  decimalPlaces: 1,
   hundred: 100,
   delimiter: ',',
   input_prefix: '>',
@@ -14,12 +10,21 @@ export const REGEXP = {
   numericPattern: /^\d+$/,
 };
 
+export const RANK = {
+  first: '1st',
+  second: '2nd',
+  third: '3rd',
+  fourth: '4th',
+  fifth: '5th',
+  sixth: '6th',
+};
+
 export const PRIZE = {
-  first: 2_000_000_000,
-  second: 30_000_000,
-  third: 1_500_000,
-  fourth: 50_000,
-  fifth: 5_000,
+  [RANK.first]: 2_000_000_000,
+  [RANK.second]: 30_000_000,
+  [RANK.third]: 1_500_000,
+  [RANK.fourth]: 50_000,
+  [RANK.fifth]: 5_000,
 };
 
 export const LOTTO_RULES = {
