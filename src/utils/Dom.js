a153449712dcb7b91d2b9356e4b86d3ad3493540
김진호 <rlawlsgh1227@gmail.com>
2024-02-26 15:34:39 +0900

    refactor: 노드 생성하는 함수 리팩토링

--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -1,6 +1,35 @@
+function setAttributes(element, attribute) {
+  Object.entries(attribute).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+}
+
+function setText(element, text) {
+  const elementText = document.createTextNode(text);
+  element.appendChild(elementText);
+}
+
 const Dom = {
   $: (selector) => document.querySelector(selector),
   $$: (selector) => document.querySelectorAll(selector),
+  /**
+   * content
+   *
+   * target: wrapper
+   * tag: 태그
+   * attribute: 속성들
+   * text: 안에 들어갈 내용
+   */
+  createAppendTagNode: (content) => {
+    const {
+      target, tag, attribute, text,
+    } = content;
+    const article = Dom.$(target);
+    const element = document.createElement(tag);
+    if (attribute !== undefined) setAttributes(element, attribute);
+    if (text !== undefined) setText(element, text);
+    article.appendChild(element);
+  },
 };
 
 export default Dom;
--- src/view/web/Input.js
+++ src/view/web/Input.js
@@ -1,14 +1,20 @@
+import Dom from '../../utils/Dom';
 import Web from '../../utils/Web';
 
 const Input = {
   async readMoney() {
-    const selector = {
-      button: '#read-money-form-submit',
-      input: '#read-money-input',
-    };
+    Dom.createAppendTagNode({ target: '.read-money-form', tag: 'input', attribute: { id: 'read-money-input', type: 'text', placeholder: '금액' } });
+    Dom.createAppendTagNode({
+      target: '.read-money-form', tag: 'button', attribute: { id: 'read-money-form-submit', type: 'button' }, text: '구입',
+    });
 
-    const money = await Web.readTagValue(selector);
+    const money = await Web.readTagValue({ button: '#read-money-form-submit', input: '#read-money-input' });
     return money;
+  },
+
+  async readWinningNumbers() {
+    const input = await Web.readTagValues({ button: '#read-winning-numbers-submit', inputs: '.read-winning-numbers-input' });
+    return input;
   },
 };
 
