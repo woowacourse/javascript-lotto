3600ef9784a4e6969416bd21caf05510f453fdc7
김진호 <rlawlsgh1227@gmail.com>
2024-02-26 14:34:44 +0900

    feat: 생성된 결과를 보여주는 output 연결

--- src/view/web/Output.js
+++ src/view/web/Output.js
@@ -1,5 +1,42 @@
-const Output = {
+import Dom from '../../utils/Dom';
 
+/**
+ * content
+ *
+ * target: wrapper
+ * tag: 태그
+ * text: 안에 들어갈 내용
+ */
+function createAppendNode(content) {
+  const { target, tag, text } = content;
+  const article = Dom.$(target);
+  const element = document.createElement(tag);
+  const elementText = document.createTextNode(text);
+  element.appendChild(elementText);
+  article.appendChild(element);
+}
+
+const Output = {
+  printLottoTicketsCount(lottoTicketsCount) {
+    const content = {
+      target: '.print-purchase-result-detail',
+      tag: 'h2',
+      text: lottoTicketsCount,
+    };
+
+    createAppendNode(content);
+  },
+
+  printPurchaseResultDetail(purchaseResultDetails) {
+    purchaseResultDetails.forEach((detail) => {
+      const content = {
+        target: '.print-purchase-result-detail',
+        tag: 'li',
+        text: detail,
+      };
+      createAppendNode(content);
+    });
+  },
 };
 
 export default Output;
