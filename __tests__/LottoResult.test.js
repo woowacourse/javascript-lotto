e048efd3529b5ba70a63031bdb38a202d749c0cf
hain-tain <hain7ce@gmail.com>
2024-02-27 14:08:28 +0900

    test: LottoResult 테스트 명세 상세 작성

--- __tests__/LottoResult.test.js
+++ __tests__/LottoResult.test.js
@@ -1,54 +1,59 @@
 import Lotto from "../src/step1/domains/Lotto";
 import LottoResult from "../src/step1/domains/LottoResult";
 import WinningLotto from "../src/step1/domains/WinningLotto";
+import LottoRules from "../src/step1/domains/LottoRules";
+import {
+  LOTTO_RULES,
+  LOTTO_REWARDS,
+  exchangeRank,
+} from "../src/step1/constants/rules";
+
+const lottoRules = new LottoRules({
+  price: LOTTO_RULES.price,
+  maxQuantity: LOTTO_RULES.maxQuantity,
+  lottoLength: LOTTO_RULES.length,
+  minNumber: LOTTO_RULES.minNumber,
+  maxNumber: LOTTO_RULES.maxNumber,
+  rewardInfo: LOTTO_REWARDS,
+  exchangeRank: exchangeRank,
+});
 
 describe("로또 결과 도메인 테스트", () => {
-  //given
-  const ANSWER = [1, 2, 3, 4, 5, 6];
-  const BONUS_NUMBER = 7;
   const lottos = [
-    new Lotto([1, 2, 3, 18, 28, 40]),
-    new Lotto([1, 3, 17, 18, 23, 33]),
-    new Lotto([1, 2, 3, 4, 5, 7]),
+    new Lotto([1, 2, 3, 18, 28, 40], lottoRules),
+    new Lotto([1, 3, 17, 18, 23, 33], lottoRules),
+    new Lotto([1, 2, 3, 4, 5, 7], lottoRules),
   ];
-  const EXPECTED_BOARDS = {
-    first: 0,
-    second: 1,
-    third: 0,
-    fourth: 0,
-    fifth: 1,
-  };
-  const lottoResult = new LottoResult();
 
-  test("로또 결과판을 만든다.", () => {
-    //when
-    lottoResult.generateResult(
-      lottos,
-      new WinningLotto(new Lotto(ANSWER), BONUS_NUMBER)
-    );
+  const winningLotto = new WinningLotto(
+    new Lotto([1, 2, 3, 4, 5, 6], lottoRules),
+    7,
+    lottoRules
+  );
 
-    //then
-    const resultBoard = lottoResult.getRankBoard();
-    const lottoRanks = Object.keys(resultBoard);
+  const lottoResult = new LottoResult(lottoRules);
 
-    const matchedCountBoard = lottoRanks.reduce((accBoard, currRank) => {
-      return {
-        ...accBoard,
-        [currRank]: resultBoard[currRank].matchedCount,
-      };
-    }, {});
+  test("구입한 로또들과 당첨 로또를 입력받아 등수를 확인하여 로또 결과판을 만든다.", () => {
+    const resultBoard = lottoResult.generateResultBoard(lottos, winningLotto);
 
-    expect(matchedCountBoard).toEqual(EXPECTED_BOARDS);
+    const EXPECTED_BOARDS = {
+      first: 0,
+      second: 1,
+      third: 0,
+      fourth: 0,
+      fifth: 1,
+      nothing: 1,
+    };
+
+    expect(resultBoard).toEqual(EXPECTED_BOARDS);
   });
 
-  test("수익률을 계산한다.", () => {
-    // given
-    const EXPECTED_RETURN_RATE = 1000166667.0;
+  test("구입 금액을 입력받아 총 수익률을 계산한다", () => {
+    const amount = lottos.length * lottoRules.getLottoPrice();
+    const returnRate = lottoResult.calculateReturnRate(amount);
 
-    // when
-    const returnRate = lottoResult.calculateReturnRate(3);
+    const EXPECTED_RETURN_RATE = 1000166.67;
 
-    // then
     expect(returnRate).toBe(EXPECTED_RETURN_RATE);
   });
 });
