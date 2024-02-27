a1835d487729d7865e99ddc9a5075c50a698811d
hain-tain <hain7ce@gmail.com>
2024-02-27 14:37:31 +0900

    fix: LottoGame 에 전달할 LottoRules 생성 및 추가

--- src/step1/App.js
+++ src/step1/App.js
@@ -1,14 +1,22 @@
-import LottoController from "./controllers/LottoController";
+// import LottoController from "./controllers/LottoController";
+
+import { LOTTO_REWARDS, LOTTO_RULES, exchangeRank } from "./constants/rules";
+import LottoGame from "./controllers/LottoGame";
+import LottoRules from "./domains/LottoRules";
 
 class App {
-  #lottoController;
-
-  constructor() {
-    this.#lottoController = new LottoController();
-  }
-
   async run() {
-    await this.#lottoController.run();
+    const lottoRules = new LottoRules({
+      price: LOTTO_RULES.price,
+      maxQuantity: LOTTO_RULES.maxQuantity,
+      lottoLength: LOTTO_RULES.length,
+      minNumber: LOTTO_RULES.minNumber,
+      maxNumber: LOTTO_RULES.maxNumber,
+      rewardInfo: LOTTO_REWARDS,
+      exchangeRank: exchangeRank,
+    });
+
+    await new LottoGame(lottoRules).start();
   }
 }
 
