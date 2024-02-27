a43db49f647a082054253673d31deecc4d1f511a
brgndy <brgndy96@gmail.com>
2024-02-27 21:35:28 +0900

    feat: winningLotto 인스턴스 추가

--- src/step2/instances.js
+++ src/step2/instances.js
@@ -1,5 +1,7 @@
 import Money from '../Domain/Money.js';
 import LottoMachine from '../Domain/LottoMachine.js';
+import WinningLotto from '../Domain/WinningLotto.js';
 
 export const money = new Money();
 export const lottoMachine = new LottoMachine();
+export const winningLotto = new WinningLotto();
