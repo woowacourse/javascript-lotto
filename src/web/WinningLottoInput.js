d4c6adb5a79774229be77aac4c5a1a6d8bfbbf51
anttiey <hanuuny@gmail.com>
2024-02-27 09:49:38 +0900

    design: 당첨 로또 번호 입력 컴포넌트 스타일 추가

--- src/web/WinningLottoInput.js
+++ src/web/WinningLottoInput.js
@@ -2,25 +2,27 @@ import Component from './Component';
 
 class WinningLottoInput extends Component {
   template() {
-    return `    
+    return ` 
+        <p>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>   
         <form class="winning-lotto-form">
-            <label>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</label>
-            <section>
-                <section>
+            <fieldset>
+                <section class="winning-number-input">
                     <label>당첨 번호</label>
+                    <section>
                     <input class="winning-number" type="text"></input>
                     <input class="winning-number" type="text"></input>
                     <input class="winning-number" type="text"></input>
                     <input class="winning-number" type="text"></input>
                     <input class="winning-number" type="text"></input>
                     <input class="winning-number" type="text"></input>
+                    </section>
                 </section>
-                <section>
+                <section class="bonus-number-input">
                     <label>보너스 번호</label>
                     <input class="bonus-number" type="text"></input>
                 </section>
-                <input id="winning-lotto-btn" type="submit" value="결과 확인하기"></input>
-            </section>
+            </fieldset>
+            <input id="winning-lotto-btn" type="submit" value="결과 확인하기"></input>
         </form>
     `;
   }
