10fd51432c48c0d106f79548efcdef89fdf5de5a
hain-tain <hain7ce@gmail.com>
2024-02-27 14:11:48 +0900

    fix: LottoStore 내부 validate 삭제 및 constructor에 lottoRules 추가

--- src/step1/domains/LottoStore.js
+++ src/step1/domains/LottoStore.js
@@ -1,58 +1,40 @@
-import { ERROR_MESSAGES } from '../constants/message';
-import LOTTO_RULES from '../constants/rules';
-import InvalidInputException from '../exceptions/InvalidInputException';
-import Lotto from './Lotto';
+import { pickRandomNumberInRange } from "../utils/pickRandomNumberInRange";
+import Lotto from "./Lotto";
 
 class LottoStore {
-  static #validateAmountType(amount) {
-    const regex = /^[1-9]\d*$/;
-    if (!regex.test(amount)) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidPurchaseAmount);
-    }
+  #lottoRules;
+
+  constructor(lottoRules) {
+    this.#lottoRules = lottoRules;
   }
 
-  static #validateAmountDivision(amount) {
-    if (amount % LOTTO_RULES.price !== 0) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidPurchaseAmount);
-    }
-  }
-
-  static #validatePurchaseAmount(amount) {
-    LottoStore.#validateAmountType(amount);
-    LottoStore.#validateAmountDivision(amount);
-  }
-
-  static #pickRandomNumberInRange(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1);
-  }
-
-  static #processLottoNumber(numbers, randomNumber) {
-    if (numbers.includes(randomNumber)) return;
-
-    numbers.push(randomNumber);
-  }
-
-  static #makeLottoNumbers() {
-    const numbers = [];
-    while (numbers.length < LOTTO_RULES.length) {
-      const randomNumber = this.#pickRandomNumberInRange(LOTTO_RULES.minRandomNumber, LOTTO_RULES.maxRandomNumber);
-      LottoStore.#processLottoNumber(numbers, randomNumber);
-    }
-
-    return numbers;
-  }
-
-  static purchaseLottos(amount) {
+  publishLottos(amount) {
     const numericAmount = Number(amount);
-    LottoStore.#validatePurchaseAmount(numericAmount);
+    this.#lottoRules.validateForAmount(numericAmount);
 
-    const lottoCount = numericAmount / LOTTO_RULES.price;
+    const lottoCount = numericAmount / this.#lottoRules.getLottoPrice();
+
     const lottos = Array.from({ length: lottoCount }).map(() => {
       const lottoNumbers = this.#makeLottoNumbers();
-      return new Lotto(lottoNumbers);
+      return new Lotto(lottoNumbers, this.#lottoRules);
     });
 
     return lottos;
+  }
+
+  #makeLottoNumbers() {
+    const numbers = [];
+
+    while (numbers.length < this.#lottoRules.getLottoLength()) {
+      const randomNumber = pickRandomNumberInRange(
+        this.#lottoRules.getMinNumber(),
+        this.#lottoRules.getMaxNumber()
+      );
+      if (numbers.includes(randomNumber)) continue;
+      numbers.push(randomNumber);
+    }
+
+    return numbers;
   }
 }
 
