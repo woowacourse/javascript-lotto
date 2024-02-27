0e92e09f511d3663b204a22c761582f1ab557bd9
hain-tain <hain7ce@gmail.com>
2024-02-27 14:17:06 +0900

    chore: 따옴표 변경 (''에서 ""로)

--- src/step1/constants/input-query.js
+++ src/step1/constants/input-query.js
@@ -8,15 +8,15 @@ const applyMessagePrefix = (messages, callback) => {
       ...accMessages,
       [currType]: callback(messages[currType]),
     }),
-    {},
+    {}
   );
 };
 
 const RAW_INPUT_QUERY = {
-  purchaseAmount: '구입금액을 입력해 주세요.',
-  winningLotto: '당첨 번호를 입력해 주세요.',
-  bonusNumber: '보너스 번호를 입력해 주세요.',
-  retrySign: '다시 시작하시겠습니까?(y/n)',
+  purchaseAmount: "구입금액을 입력해 주세요.",
+  winningLotto: "당첨 번호를 입력해 주세요.",
+  bonusNumber: "보너스 번호를 입력해 주세요.",
+  retrySign: "다시 시작하시겠습니까?(y/n)",
 };
 
 const INPUT_QUERY = applyMessagePrefix(RAW_INPUT_QUERY, addQueryPrefix);
