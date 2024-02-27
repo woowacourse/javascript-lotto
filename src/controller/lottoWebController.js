5bcf0f00629797fa29dbfad09991d2f3c91bad32
bmj0053 <bmj05378@gmail.com>
2024-02-27 14:33:57 +0900

    feat: budget 관리 기능 분리 및 당첨 & 보너스 번호 유효성 검사 추가

--- src/controller/lottoWebController.js
+++ src/controller/lottoWebController.js
@@ -1,9 +1,12 @@
 import "../../index.css";
-import Lotto from "../domain/Lotto";
-import randomLottoArray from "../domain/randomLottoMaker";
-import { $ } from "../utils/querySelector";
-import budgetValidation from "../validation/budgetValidation";
-import startValidation from "../validation/startValidation";
+import Lotto from "../domain/Lotto.js";
+import randomLottoArray from "../domain/randomLottoMaker.js";
+import { $, $$ } from "../utils/querySelector.js";
+import budgetValidation from "../validation/budgetValidation.js";
+import startValidation from "../validation/startValidation.js";
+import winningLottoBonusValidation from "../validation/winningLottoBonusValidation.js";
+import winningLottoNumbersValidation from "../validation/winningLottoNumbersValidation.js";
+import winningLottoValidation from "../validation/winningLottoValidation.js";
 
 const lottoWebController = {
   async start() {
@@ -13,16 +16,25 @@ const lottoWebController = {
       "submit",
       this.handleWebBudget.bind(this)
     );
+    $("#result-btn").addEventListener(
+      "click",
+      this.handleWebLottoCombinationInput.bind(this)
+    );
+  },
+
+  getWebBudget() {
+    const webBudgetInput = $("#budget").value;
+    const webBudget = Number(webBudgetInput);
+    return webBudget;
   },
 
   /**
    * 예산 입력 관리 및 유효성 검사
    * @param {event} e
    */
-  handleWebBudget(e) {
-    e.preventDefault();
-    const webBudgetInput = $("#budget").value;
-    const webBudget = Number(webBudgetInput);
+  handleWebBudget(event) {
+    event.preventDefault();
+    const webBudget = this.getWebBudget();
     try {
       // 유효성 검사 해줘
       startValidation(budgetValidation.categories, webBudget);
@@ -31,17 +43,13 @@ const lottoWebController = {
       const webIssuedLottoCount = this.calculateWebIssuedLottoCount(webBudget);
 
       // 발행된 로또 번호 가져와서 보여줘
-      const webIssuedLottoArray = randomLottoArray(webIssuedLottoCount);
-      webIssuedLottoArray.forEach((array) => {
-        const issuedLottoDiv = document.createElement("div");
-        const curr = $("#content-box-lottos");
-        issuedLottoDiv.innerHTML = array;
-        curr.append(issuedLottoDiv);
-      });
+      // TODO : 구입 누를 때마다 div 추가되는 것 수정하기
+      this.printWebIssuedLottoCount(webIssuedLottoCount);
 
-      return true;
+      // 입력한 당첨 번호 관리
+      // this.handleWebLottoCombinationInput();
     } catch (error) {
-      alert(error.message); // TODO : alert 말고 다른 종류로 바꾸기
+      return alert(error.message); // TODO : alert 말고 다른 종류로 바꾸기
     }
   },
 
@@ -50,6 +58,49 @@ const lottoWebController = {
     const webIssuedLottoCount = webLotto.calculateIssuedLottoCount();
     $("#issued-lotto-count").innerHTML = webIssuedLottoCount;
     return webIssuedLottoCount;
+  },
+
+  printWebIssuedLottoCount(webIssuedLottoCount) {
+    const webIssuedLottoArray = randomLottoArray(webIssuedLottoCount);
+    webIssuedLottoArray.forEach((array) => {
+      const issuedLottoDiv = document.createElement("div");
+      const curr = $("#content-box-lottos");
+      issuedLottoDiv.innerHTML = array;
+      curr.append(issuedLottoDiv);
+    });
+  },
+
+  /**
+   * 당첨 번호 및 보너스 번호 입력값 관리
+   */
+  handleWebLottoCombinationInput() {
+    const webLottoNumbersInput = Array.from({ length: 6 }, (_, index) => {
+      return Number($$("#lotto-numbers-input")[index].value);
+    });
+
+    const webLottoBonusInput = Number($("#lotto-bonus-input").value);
+    const webLottoCombination = {
+      normalNumbers: webLottoNumbersInput,
+      bonusNumber: webLottoBonusInput,
+    };
+
+    try {
+      startValidation(
+        winningLottoNumbersValidation.winningNumbers,
+        webLottoNumbersInput
+      );
+      startValidation(
+        winningLottoValidation.commonCategories,
+        webLottoBonusInput
+      );
+      startValidation(
+        winningLottoBonusValidation.winningBonus,
+        webLottoCombination
+      );
+      console.log("validation success");
+    } catch (error) {
+      alert(error.message);
+    }
   },
 };
 
