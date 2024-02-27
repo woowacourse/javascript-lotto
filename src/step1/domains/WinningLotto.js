f94494928ca21df41fc521d90de6b22833485ade
hain-tain <hain7ce@gmail.com>
2024-02-27 14:24:03 +0900

    fix: WinnigLotto 내부 validate 삭제 및 constructor에 lottoRules 추가

--- src/step1/domains/WinningLotto.js
+++ src/step1/domains/WinningLotto.js
@@ -1,36 +1,16 @@
-import { ERROR_MESSAGES } from '../constants/message';
-import InvalidInputException from '../exceptions/InvalidInputException';
-
 class WinningLotto {
   #winningLotto;
-
   #bonusNumber;
 
-  constructor(winningLotto, bonusNumber) {
-    WinningLotto.#validateDuplication(winningLotto, bonusNumber);
+  constructor(winningLotto, bonusNumber, lottoRules) {
+    lottoRules.validateForBonusNumber(winningLotto, bonusNumber);
     this.#winningLotto = winningLotto;
-    WinningLotto.#validateBonusNumber(bonusNumber);
     this.#bonusNumber = bonusNumber;
-  }
-
-  static #validateBonusNumber(bonusNumber) {
-    const regex = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
-
-    if (!regex.test(bonusNumber)) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidBonusNumberType);
-    }
-  }
-
-  static #validateDuplication(winningLotto, bonusNumber) {
-    const numericBonusNumber = Number(bonusNumber);
-    if (winningLotto.hasNumber(numericBonusNumber)) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidBonusNumberUniqueness);
-    }
   }
 
   getWinningInfo() {
     return {
-      answer: this.#winningLotto.getNumbers(),
+      winningNumber: this.#winningLotto.getNumbers(),
       bonusNumber: this.#bonusNumber,
     };
   }
