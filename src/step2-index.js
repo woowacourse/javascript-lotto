d42a7c7077e8ce6aaf331f07edb47e3b0c667639
00kang <kdb070970@gmail.com>
2024-02-27 20:16:04 +0900

    feat: 구입할 금액 입력 동작 구현 및 구입 버튼에 이벤트 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,17 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+import './css/theme.css';
+import './css/style.css';
+
+window.onload = function () {
+  const purchaseInputField = document.getElementById('purchaseInputField');
+  const purchaseSubmitButton = document.getElementById('purchaseSubmitButton');
+
+  purchaseSubmitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputValue = purchaseInputField.value;
+    console.log(`${inputValue}`);
+  });
+};
