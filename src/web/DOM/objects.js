c15add24c4b17c971136e65c1e4a0d640465dea0
chysis <chysiss@naver.com>
2024-02-28 01:38:05 +0900

    Revert "feat(WebInputView): UI를 통해 로또 구입 금액을 입력받는 기능 구현"
    This reverts commit b14894d4bf6978a38054c58c26a2a9a72e958392.

--- src/web/DOM/objects.js
+++
@@ -1,6 +0,0 @@
-import { $ } from '../../utils/querySelector.js';
-
-const purchaseAmountForm = $('#purchase-amount-form');
-const purchaseResult = $('#purchase-result');
-
-export { purchaseResult, purchaseAmountForm };
