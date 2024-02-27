de31467030f566dc49a7a141e1b6b6eb0539216b
brgndy <brgndy96@gmail.com>
2024-02-27 16:58:43 +0900

    refactor: 상수 메세지 인자로 받도록 하기

--- src/Constants/Messages/progressMessage.js
+++ src/Constants/Messages/progressMessage.js
@@ -3,7 +3,7 @@ const PROGRESS_MESSAGE = Object.freeze({
   INPUT_WINNING_LOTTO: '당첨 번호를 입력해주세요.',
   INPUT_BONUS_NUMBER: '보너스 번호를 입력해주세요.',
   SELECT_RE_RUN: '다시 시작하시겠습니까? (y/n)',
-  BUY_LOTTO: '개를 구매했습니다.',
+  BUY_LOTTO: (lottoLength) => `총 ${lottoLength}개를 구매했습니다.`,
   RESULT_HEADER: '당첨 통계\n--------------------',
   RATE_OF_RETURN_MESSAGE: (rateOfReturn) => `총 수익률은 ${rateOfReturn.toLocaleString()}% 입니다.`,
 });
