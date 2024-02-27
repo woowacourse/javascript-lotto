4a9a2dbe3ed495162b86d323dc020ae0343eeda5
bmj0053 <bmj05378@gmail.com>
2024-02-27 17:18:39 +0900

    fix: lottoWebController를 class로 변경 및 클래스 멤버 설정, webLottoBonusInput, webLottoCombination 변수명 변경

--- src/controller/lottoWebController.js
+++ src/controller/lottoWebController.js
@@ -1,5 +1,6 @@
 import "../../index.css";
 import Lotto from "../domain/Lotto.js";
+import lottoResultMaker from "../domain/lottoResultMaker.js";
 import randomLottoArray from "../domain/randomLottoMaker.js";
 import { $, $$ } from "../utils/querySelector.js";
 import budgetValidation from "../validation/budgetValidation.js";
@@ -8,7 +9,8 @@ import winningLottoBonusValidation from "../validation/winningLottoBonusValidati
 import winningLottoNumbersValidation from "../validation/winningLottoNumbersValidation.js";
 import winningLottoValidation from "../validation/winningLottoValidation.js";
 
-const lottoWebController = {
+class LottoWebController {
+  #webIssuedLottoArray;
   async start() {
     // 입력받은 budget 처리 (submit) 후
     // budget 유효성 검사 통과하면 로또 발행
@@ -18,16 +20,16 @@ const lottoWebController = {
     );
     $("#result-btn").addEventListener(
       "click",
-      this.handleWebLottoCombinationInput.bind(this)
+      this.handleWebWinningCombinationInput.bind(this)
     );
     $("#modal-close-btn").addEventListener("click", this.closeModal.bind(this));
-  },
+  }
 
   getWebBudget() {
     const webBudgetInput = $("#budget").value;
     const webBudget = Number(webBudgetInput);
     return webBudget;
-  },
+  }
 
   /**
    * 예산 입력 관리 및 유효성 검사
@@ -44,77 +46,89 @@ const lottoWebController = {
       const webIssuedLottoCount = this.calculateWebIssuedLottoCount(webBudget);
 
       // 발행된 로또 번호 가져와서 보여줘
-      // TODO : 구입 누를 때마다 div 추가되는 것 수정하기
-      this.printWebIssuedLottoCount(webIssuedLottoCount);
-
-      // 입력한 당첨 번호 관리
-      // this.handleWebLottoCombinationInput();
+      $("#after-budget").style.display = "block";
+      this.handleWebIssuedLottoArray(webIssuedLottoCount);
     } catch (error) {
       return alert(error.message); // TODO : alert 말고 다른 종류로 바꾸기
     }
-  },
+  }
 
   calculateWebIssuedLottoCount(webBudget) {
     const webLotto = new Lotto(webBudget);
     const webIssuedLottoCount = webLotto.calculateIssuedLottoCount();
     $("#issued-lotto-count").innerHTML = webIssuedLottoCount;
     return webIssuedLottoCount;
-  },
+  }
 
-  printWebIssuedLottoCount(webIssuedLottoCount) {
-    const webIssuedLottoArray = randomLottoArray(webIssuedLottoCount);
-    webIssuedLottoArray.forEach((array) => {
+  handleWebIssuedLottoArray(webIssuedLottoCount) {
+    this.#webIssuedLottoArray = randomLottoArray(webIssuedLottoCount);
+    this.printWebIssuedLottoArray();
+  }
+
+  // TODO : 구입 누를 때마다 div 추가되는 것 수정하기
+  printWebIssuedLottoArray() {
+    this.#webIssuedLottoArray.forEach((array) => {
       const issuedLottoDiv = document.createElement("div");
       const curr = $("#content-box-lottos");
       issuedLottoDiv.innerHTML = array;
       curr.append(issuedLottoDiv);
     });
-  },
+  }
 
   /**
    * 당첨 번호 및 보너스 번호 입력값 관리
    */
-  handleWebLottoCombinationInput() {
-    const webLottoNumbersInput = Array.from({ length: 6 }, (_, index) => {
+  handleWebWinningCombinationInput() {
+    const webWinningNumbersInput = Array.from({ length: 6 }, (_, index) => {
       return Number($$("#lotto-numbers-input")[index].value);
     });
 
-    const webLottoBonusInput = Number($("#lotto-bonus-input").value);
-    const webLottoCombination = {
-      normalNumbers: webLottoNumbersInput,
-      bonusNumber: webLottoBonusInput,
+    const webWinningBonusInput = Number($("#lotto-bonus-input").value);
+    const webWinningCombination = {
+      normalNumbers: webWinningNumbersInput,
+      bonusNumber: webWinningBonusInput,
     };
 
     try {
       startValidation(
         winningLottoNumbersValidation.winningNumbers,
-        webLottoNumbersInput
+        webWinningNumbersInput
       );
       startValidation(
         winningLottoValidation.commonCategories,
-        webLottoBonusInput
+        webWinningBonusInput
       );
       startValidation(
         winningLottoBonusValidation.winningBonus,
-        webLottoCombination
+        webWinningCombination
       );
       console.log("validation success");
-
+      this.calculateWebLottoResult(webWinningCombination);
       this.openModal();
     } catch (error) {
-      alert(error.message);
+      alert(error.message); // TODO : alert 말고 다른 종류로 바꾸기
     }
-  },
+  }
 
   openModal() {
     console.log("openModal");
     $("#modal").style.display = "flex";
-  },
+  }
 
   closeModal() {
     console.log("closeModal");
     $("#modal").style.display = "none";
-  },
-};
+  }
 
-export default lottoWebController;
+  calculateWebLottoResult(webWinningCombination) {
+    console.log("calculateWebLottoResult");
+    console.log(this.#webIssuedLottoArray);
+    // const webLottoResult = lottoResultMaker.calculateLottoResult(
+    //   this.#webIssuedLottoArray,
+    //   webWinningCombination
+    // );
+    // console.log(webLottoResult);
+  }
+}
+
+export default LottoWebController;
