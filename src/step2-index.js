da976d35ed6561235fb8248106b2b938f11edb48
skiende74 <skiende74@gmail.com>
2024-02-27 21:19:25 +0900

    feat(웹뷰): 기능완성

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,22 +2,17 @@ import PurchaseLottoService from './domain/service/PurchaseLottoService';
 import '../index.css';
 import WinningResultService from './domain/service/WinningResultService';
 import CONDITION from './constant/Condition';
+import ResultModal from './webView/ResultModal';
 
-document.querySelector('.purchase-form__button').addEventListener('click', purchaseListener);
-document.querySelector('.result__button').addEventListener('click', resultButtonListener);
-document
-  .querySelector('.result-modal-header__close-button')
-  .addEventListener('click', closeModalListener);
+const addClickListener = (selector, listener) => {
+  document.querySelector(selector).addEventListener('click', listener);
+};
+
+addClickListener('.purchase-form__button', purchaseListener);
+addClickListener('.result__button', ResultModal.resultButtonListener);
+addClickListener('.result-modal-header__close-button', ResultModal.closeModalListener);
 
 const CLASSNAME_HIDDEN = 'hidden';
-
-function nextElementSiblings(element) {
-  const result = [];
-  while (element.nextElementSibling) {
-    result.push(element.nextElementSibling);
-  }
-  return result;
-}
 
 function purchaseListener(event) {
   event.preventDefault();
@@ -30,7 +25,7 @@ function purchaseListener(event) {
   const purchaseMoneyString = purchase.value;
   const lottos = new PurchaseLottoService(purchaseMoneyString).getLottos();
 
-  const lottoStrings = lottos.map((lotto) => '🎟️ ' + lotto.join(', '));
+  const lottoStrings = lottos.map((lotto) => `🎟️ ${lotto.join(', ')}`);
   const lottosFragment = makeDivsFragmentFromStrings(lottoStrings);
 
   [...purchased.children].map((node) => node.remove());
@@ -58,47 +53,10 @@ function showWinningPart() {
   document.querySelector(SELECTOR_RESULT).classList.remove(CLASSNAME_HIDDEN);
 }
 
-function resultButtonListener(event) {
-  event.preventDefault();
-
-  // TODO : 서비스 연결로직 마무리
-  // const lottoNumbers = getLottoNumbers();
-  // const winningResults = new WinningResultService(
-  //   lottoNumbers,
-  //   getWinningNumbers(),
-  // ).getWinningResults();
-
-  openResultModal();
-}
-
-function openResultModal() {
-  document.querySelector('.result-modal').classList.remove(CLASSNAME_HIDDEN);
-  document.querySelector('.result-modal-backdrop').classList.remove(CLASSNAME_HIDDEN);
-}
-function closeResultModal() {
-  document.querySelector('.result-modal').classList.add(CLASSNAME_HIDDEN);
-  document.querySelector('.result-modal-backdrop').classList.add(CLASSNAME_HIDDEN);
-}
-
-function getLottoNumbers() {
-  return undefined;
-}
-
-function closeModalListener(event) {
-  event.preventDefault();
-  closeResultModal();
-}
-
-function getWinningNumbers() {
-  const SELECTOR_WINNING_NUMBERS_INPUTS = '.winning-numbers-inputs';
-  const SELECTOR_BONUS_NUMBER_INPUT = '.bonus-number-inputs__input';
-
-  const winningNumberInputs = document.querySelector(SELECTOR_WINNING_NUMBERS_INPUTS);
-  const bonusNumberInput = document.querySelector(SELECTOR_BONUS_NUMBER_INPUT);
-
-  const winningNumbers = [...winningNumberInputs.childNodes].map((node) =>
-    Number(node.textContent),
-  );
-  const bonusNumber = Number(bonusNumberInput.textContent);
-  return { numbers: winningNumbers, bonusNumber };
+function nextElementSiblings(element) {
+  const result = [];
+  while (element.nextElementSibling) {
+    result.push(element.nextElementSibling);
+  }
+  return result;
 }
