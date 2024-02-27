696983913991361e54cadb784490012674491c19
hain-tain <hain7ce@gmail.com>
2024-02-27 14:10:25 +0900

    fix: Lotto 내부 validate 제거 및 constructor에 lottoRules 추가

--- src/step1/domains/Lotto.js
+++ src/step1/domains/Lotto.js
@@ -1,51 +1,17 @@
-import { ERROR_MESSAGES } from '../constants/message';
-import LOTTO_RULES from '../constants/rules';
-import InvalidInputException from '../exceptions/InvalidInputException';
-
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    Lotto.#validate(numbers);
+  constructor(numbers, lottoRules) {
+    lottoRules.validateForLotto(numbers);
     this.#numbers = numbers;
   }
 
-  static #validate(numbers) {
-    Lotto.#validateNumbersType(numbers);
-    Lotto.#validateLength(numbers);
-    Lotto.#validateUniqueness(numbers);
+  hasNumber(number) {
+    return this.#numbers.includes(number);
   }
 
-  static #validateNumbersType(numbers) {
-    const regex = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
-
-    if (!numbers.every((number) => regex.test(number))) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidNumbersType);
-    }
-  }
-
-  static #validateLength(numbers) {
-    if (numbers.length !== LOTTO_RULES.length) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidLottoLength);
-    }
-  }
-
-  static #validateUniqueness(numbers) {
-    if (new Set(numbers).size !== numbers.length) {
-      throw new InvalidInputException(ERROR_MESSAGES.invalidLottoUniqueness);
-    }
-  }
-
-  #countMatchedNumber(answer) {
-    return this.#numbers.filter((number) => answer.includes(number)).length;
-  }
-
-  hasNumber(bonusNumber) {
-    return this.#numbers.includes(bonusNumber);
-  }
-
-  getMatchedInfo(answer, bonusNumber) {
-    const matchedCount = this.#countMatchedNumber(answer);
+  getMatchedInfo(winningNumber, bonusNumber) {
+    const matchedCount = this.#countMatchedNumber(winningNumber);
     const hasBonusNumber = this.hasNumber(bonusNumber);
 
     return { matchedCount, hasBonusNumber };
@@ -53,6 +19,11 @@ class Lotto {
 
   getNumbers() {
     return [...this.#numbers];
+  }
+
+  #countMatchedNumber(winningNumber) {
+    return this.#numbers.filter((number) => winningNumber.includes(number))
+      .length;
   }
 }
 
