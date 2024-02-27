873e113ce8bd769e94782fd0f962b8147a6e156f
Kim Da Eun <midekuna@gmail.com>
2024-02-27 17:30:45 +0900

    feat: 로또 번호 리스트 컴포넌트

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,12 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+// styles
+import './styles/body.css';
+import './styles/global.css';
+
+// components
+import LottoItems from './components/LottoItems';
+
+window.customElements.define('lotto-items', LottoItems);
