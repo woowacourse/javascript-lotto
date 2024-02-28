d0f6515ff0b82092d6cd1a257e476fc9ae9f6ede
jinsil <jinsil378900@gmail.com>
2024-02-28 21:25:26 +0900

    fix: 로또 머신 인자 변경

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -4,13 +4,13 @@ import { SETTING } from '../src/constant/setting';
 describe('[LottoMachine] 로또 번호 발행 기능 테스트', () => {
   test('인자로 넘어온 금액에 해당하는 숫자 만큼의 로또 번호들을 발행해야 한다.', () => {
     // given
-    const purchaseAmount = 5000;
+    const lottoCount = 5;
 
     // when
-    const lottoMachine = new LottoMachine(purchaseAmount);
+    const lottoMachine = new LottoMachine(lottoCount);
     const lottoNumberList = lottoMachine.getLottoNumberList();
 
     // then
-    expect(lottoNumberList.length).toBe(purchaseAmount / SETTING.LOTTO_PRICE);
+    expect(lottoNumberList.length).toBe(lottoCount);
   });
 });
--- src/controller/LottoGameController2.js
+++ src/controller/LottoGameController2.js
@@ -3,16 +3,16 @@ import LottoMachine from '../domain/LottoMachine.js';
 import LottosManager from '../domain/LottosManager.js';
 import elementHandler from '../handler/elementHandler.js';
 import Validator from '../validator/Validator.js';
+import View from '../view/view.js';
 
 const $purchaseAmount = elementHandler.$('.purchase-input-box');
 const $purchaseForm = elementHandler.$('.purchase-form');
 
-const $lottosText = elementHandler.$('.lottos-text');
 class LottoGameController2 {
   #purchaseAmount;
   #lottos;
 
-  async play() {
+  play() {
     this.inputPurchaseAmount();
   }
 
@@ -26,7 +26,6 @@ class LottoGameController2 {
 
   checkPurchaseAmount(purchaseAmount) {
     try {
-      console.log(purchaseAmount);
       Validator.validatePurchaseAmount(purchaseAmount);
       this.#purchaseAmount = purchaseAmount;
       this.#createRandomLottos(this.#purchaseAmount / SETTING.LOTTO_PRICE);
@@ -38,10 +37,32 @@ class LottoGameController2 {
 
   #createRandomLottos(lottoCount) {
     console.log(lottoCount);
-    $lottosText.innerHTML = `총 ${lottoCount}개를 구매하였습니다.`;
+    View.outputLottoCount(lottoCount);
     const lottoList = new LottoMachine(lottoCount).getLottoNumberList();
     this.#lottos = new LottosManager(lottoList);
   }
+
+  // #calculateWinningResult(winningNumbers, bonusNumber) {
+  //   const winningResults = this.#lottos.getWinningResults(winningNumbers, bonusNumber);
+  //   OutputView.printWinningResults(winningResults);
+  //   OutputView.printProfitRate(this.#calculateProfitRate(winningResults));
+  // }
+
+  // #calculateProfitRate(winningResults) {
+  //   const totalProfit = Object.entries(winningResults).reduce((profit, [matchedKey, count]) => {
+  //     return profit + RANKING[matchedKey].REWARD * count;
+  //   }, 0);
+  //   return ((totalProfit * 100) / this.#purchaseAmount).toLocaleString('ko-KR', { minimumFractionDigits: 1 });
+  // }
+
+  // #restartGame(restartCommand) {
+  //   if (restartCommand === SETTING.RESTART_COMMAND) {
+  //     this.play();
+  //   }
+  //   if (restartCommand === SETTING.EXIT_COMMAND) {
+  //     OutputView.printExitMessage();
+  //   }
+  // }
 }
 
 export default LottoGameController2;
