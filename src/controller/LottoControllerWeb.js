742811533e9ddcc971e558abda7fb5dc275d9ae7
Soyeon Choe <soy2302ten@gmail.com>
2024-02-28 23:53:22 +0900

    refactor: 구입 금액 입력 기능 로직 수정 (money 출력 불가 이유)

--- src/controller/LottoControllerWeb.js
+++ src/controller/LottoControllerWeb.js
@@ -7,30 +7,26 @@ import purchaseAfter from '../../static/html/purchaseAfter';
 import purchaseBefore from '../../static/html/purchaseBefore';
 import onError from '../util/onError';
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('purchase-before').insertAdjacentHTML('afterbegin', purchaseBefore());
+document.getElementById('purchase-before').insertAdjacentHTML('afterbegin', purchaseBefore());
 
-  purchaseLotto();
-});
+const purchaseAmount = document.querySelector('.lp-pa-input-group');
+const purchaseAmountButton = document.querySelector('.lp-pa-input-btn');
 
-function purchaseLotto() {
-  const purchaseAmount = document.querySelector('.lp-pa-input-group');
-  const purchaseLottoButton = document.querySelector('.lp-pa-input-btn');
-
-  // TODO: 포커싱이 사라지면 에러 메세지를 출력하게 하는 건?
-  // TODO: 값이 입력될 때 마다 에러 메세지 출력은? -> 이상할까?
-  purchaseAmount.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const money = event.target.amount.value;
-    try {
-      new Money(money);
-      handleClickAddComponent('purchase-after', purchaseAfter);
-    } catch (error) {
-      onError(purchaseAmount, error.message);
+purchaseAmountButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const inputMoney = document.querySelector('.lp-pa-input-amount').value;
+  const findPurchaseAfter = document.getElementById('purchase-after');
+  try {
+    const money = new Money(Number(inputMoney));
+    const lottoMachine = new LottoMachine(money.count);
+    if (!findPurchaseAfter) {
+      findPurchaseAfter.insertAdjacentHTML('afterbegin', purchaseAfter(money.count, lottoMachine.lottos));
     }
-  });
-}
-
-const handleClickAddComponent = (getId, addFunc) => {
-  document.getElementById(getId).insertAdjacentHTML('afterbegin', addFunc());
-};
+    if (findPurchaseAfter) {
+      findPurchaseAfter.innerHTML = purchaseAfter(money.count, lottoMachine.lottos);
+    }
+  } catch (error) {
+    onError(purchaseAmount, error.message);
+    reject(error);
+  }
+});
