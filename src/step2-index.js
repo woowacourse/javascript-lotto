e59c874be5655a6445081bb26dd2093cfd541fa9
jinsil <jinsil378900@gmail.com>
2024-02-28 17:49:00 +0900

    feat: 로또 게임 컨트롤러 실행

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,1 +1,5 @@
 import './css/style.css';
+import LottoGameController2 from './controller/LottoGameController2.js';
+
+const lottoGameController2 = new LottoGameController2();
+lottoGameController2.play();
