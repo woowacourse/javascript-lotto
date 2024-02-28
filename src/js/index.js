91f248e96c4b3e6990b307396f7b8c10076188e4
Lee sang Yeop <mma7710@naver.com>
2024-02-28 21:19:14 +0900

    feat(js): 입력받은 구매금액에 맞게 로또 리스트를 출력

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,80 @@
 import WebController from "../controller/WebController.js";
-import CreateElement from "./CreateElement.js";
+import ElementTree from "./ElementTree.js";
 
 const webController = new WebController();
 
 const purchaseButton = document.getElementById("purchase-lotto");
 
-const purchaseLotto = () => {
-  const newParentElement = new CreateElement("div");
-  const showPurchaseLottosTag = document.getElementById(
-    "purchased-lotto-lists",
+const showPurchaseLottoCount = (lottos) => {
+  const lottoListsElement = new ElementTree("div");
+
+  const message = `총 ${lottos.length}개를 구매하였습니다.`;
+  lottoListsElement.createNewTag("div", message);
+
+  return lottoListsElement.tags;
+};
+
+const showLottoLists = (lottos) => {
+  const lottoListsElement = new ElementTree("ul");
+  lottos.forEach((lotto) => {
+    lottoListsElement.generateTmpStack("li", "");
+    lottoListsElement.pushTmpTag("div", "🎟️ ", { class: "ticket-icon" });
+    lottoListsElement.pushTmpTag("div", lotto.numbers.join(", "));
+    lottoListsElement.flushTmpTag();
+  });
+
+  return lottoListsElement.tags;
+};
+
+const showInputBonusNumber = () => {
+  const bonusNumberElement = new ElementTree("div");
+  bonusNumberElement.createNewTag(
+    "h2",
+    "지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.",
   );
 
-  const purchaseAmount = document.getElementById("amount").value;
+  bonusNumberElement.generateTmpStack("div", "", {
+    class: "input-bonus-title",
+  });
+  bonusNumberElement.pushTmpTag("h2", "당첨 번호");
+  bonusNumberElement.pushTmpTag("h2", "보너스 번호");
+  bonusNumberElement.flushTmpTag();
 
-  const lottos = webController.purchaseLottos(Number(purchaseAmount));
-  const message = `총 ${lottos.length}개를 구매하였습니다.`;
-
-  const newUlElement = new CreateElement("ul");
-  lottos.forEach((lotto) =>
-    newUlElement.createNewTag("li", {
-      contents: "🎟️ " + lotto.numbers.join(", "),
+  bonusNumberElement.generateTmpStack("div", "", {
+    class: "input-bonusball-wrapper",
+  });
+  Array.from({ length: 6 }).forEach(() =>
+    bonusNumberElement.pushTmpTag("input", "", {
+      class: "input-winningnumber",
+      type: "number",
+      min: 1,
+      max: 45,
     }),
   );
 
-  newParentElement.createNewTag("div", { contents: message });
+  bonusNumberElement.pushTmpTag("input", "", {
+    class: "input-bonusnumber",
+    type: "number",
+  });
+  bonusNumberElement.flushTmpTag();
 
-  showPurchaseLottosTag.innerHTML = newParentElement.tags;
-  showPurchaseLottosTag.innerHTML += newUlElement.tags;
+  bonusNumberElement.createNewTag("button", "결과 확인하기", {
+    class: "check-lotto-result",
+  });
+
+  return bonusNumberElement.tags;
+};
+
+const purchaseLotto = () => {
+  const showPurchaseLottosTag = document.getElementById(
+    "purchased-lotto-lists",
+  );
+  const purchaseAmount = document.getElementById("amount").value;
+  const lottos = webController.purchaseLottos(Number(purchaseAmount));
+
+  showPurchaseLottosTag.innerHTML = showPurchaseLottoCount(lottos);
+  showPurchaseLottosTag.innerHTML += showLottoLists(lottos);
+  showPurchaseLottosTag.innerHTML += showInputBonusNumber();
 };
 
 purchaseButton.addEventListener("click", purchaseLotto);
