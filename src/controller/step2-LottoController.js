981d8fe4c7b5929147992e2adaf4951792f9377a
00kang <kdb070970@gmail.com>
2024-02-28 19:51:30 +0900

    feat: step2 전용 로또 컨트롤러 생성
    - 기존의 로또 컨트롤러는 readline 을 불러오기 때문에 에러가 발생해 ui에서 입력받은 값을 매개변수로 넘겨주는 수정
    - step2-index.js에 step2-LottoController.js 연결

---
+++ src/controller/step2-LottoController.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-constant-condition */
+// import OPTIONS from '../constant/Options.js';
+import LottoMachine from '../domain/LottoMachine.js';
+// import BonusNumberValidator from '../util/validation/BonusNumberValidator.js';
+// import LottoNumbersValidator from '../util/validation/LottoNumbersValidator.js';
+import PurchaseAmountValidator from '../util/validation/PurchaseAmountValidator.js';
+// import RestartValidator from '../util/validation/RestartValidator.js';
+// import OutputView from '../view/OutputView.js';
+
+class LottoController {
+  #lottoMachine;
+
+  constructor() {
+    this.#lottoMachine = new LottoMachine();
+  }
+
+  async getInputAndValidate(input, validateFunction) {
+    try {
+      return validateFunction(input) ?? input;
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  }
+
+  async inputPurchaseAmount(purchaseAmount) {
+    const validatedPurchaseAmount = await this.getInputAndValidate(purchaseAmount, (input) =>
+      PurchaseAmountValidator.validate(parseInt(input.trim(), 10))
+    );
+
+    return validatedPurchaseAmount ? parseInt(validatedPurchaseAmount.trim(), 10) : null;
+  }
+}
+
+export default LottoController;
--- src/step2-index.js
+++ src/step2-index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
@@ -6,13 +7,17 @@
 import './css/theme.css';
 import './css/style.css';
 
+import LottoController from './controller/step2-LottoController';
+
+const lottoController = new LottoController();
+
 window.onload = function () {
   const purchaseInputField = document.getElementById('purchaseInputField');
   const purchaseSubmitButton = document.getElementById('purchaseSubmitButton');
 
-  purchaseSubmitButton.addEventListener('click', function (event) {
+  purchaseSubmitButton.addEventListener('click', async function (event) {
     event.preventDefault();
-    const inputValue = purchaseInputField.value;
-    console.log(`${inputValue}`);
+    const purchaseAmount = purchaseInputField.value;
+    await lottoController.inputPurchaseAmount(purchaseAmount);
   });
 };
