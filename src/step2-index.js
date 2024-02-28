8cba38fee42b3a6fb93a5013022f662270563f90
novice0840 <magnolia0840@gmail.com>
2024-02-28 18:33:59 +0900

    feat: 로또 구입 Skeleton UI 완성

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,28 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+import './styles/reset.css';
+import './styles/style.css';
+import LottoMachine from './domain/lottoMachine';
+
+const $buyForm = document.querySelector('.buy-form');
+const $lottoResult = document.querySelector('.lotto-result');
+const $answerForm = document.querySelector('.answer-form');
+
+$buyForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const money = formData.get('buy-input');
+
+  if (money % 1000 !== 0) {
+    alert('1000원 단위의 숫자를 입력해주세요');
+    return;
+  }
+
+  $lottoResult.classList.remove('hidden');
+  $answerForm.classList.remove('hidden');
+
+  const lottoMachine = new LottoMachine(money);
+  console.log(lottoMachine.getLottoCount);
+  console.log(lottoMachine.getLottoNumbers);
+});
