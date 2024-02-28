f08e7dfbcfbfe262ad1373b6451b50969226bda3
rbgksqkr <rbgks1937@naver.com>
2024-02-28 15:08:24 +0900

    chore: PurchaseForm 파일명 대소문자 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,6 +1,5 @@
 import './components/App.js';
 import './components/titleSection/TitleSection.js';
 import './components/purchaseSection/PurchaseSection.js';
-import './components/purchaseSection/PurchaseForm.js';
 import './components/winningNumberSection/WinningNumberSection.js';
 import './components/LottoResultModal/LottoResultModal.js';
