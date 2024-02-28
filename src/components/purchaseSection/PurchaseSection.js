e1686745d137c500ec621553c213fbef5d9e23f2
rbgksqkr <rbgks1937@naver.com>
2024-02-28 21:47:20 +0900

    feat(PurchaseSection): 로또 구매 입력 컴포넌트 분리

--- src/components/purchaseSection/PurchaseSection.js
+++ src/components/purchaseSection/PurchaseSection.js
@@ -1,12 +1,34 @@
-import Component from '../Component';
+import './PurchaseSection.css';
+import Component from '../core/Component';
+import PurchaseLottoForm from './PurchaseLottoForm';
 
 class PurchaseSection extends Component {
-  render() {
-    this.innerHTML = `
+  mounted() {
+    const { handlePurchaseBtn } = this;
+    const $purchaseLottoForm = document.querySelector('#purchaseLottoForm');
+    new PurchaseLottoForm($purchaseLottoForm, {
+      handlePurchaseBtn: handlePurchaseBtn.bind(this),
+    });
+  }
+
+  template() {
+    return `
         <div>구입할 금액을 입력해주세요.</div>
-        <purchase-form></purchase-form>    
-        <div id="purchaseError" class="hidden"></div>`;
+          <form id="purchaseLottoForm"></form>
+          <div id="purchaseError" class="hidden">
+        </div>`;
+  }
+
+  setEvent() {
+    const $purchaseLottoForm = this.$target.querySelector('#purchaseLottoForm');
+    $purchaseLottoForm.addEventListener('click', e => {
+      e.preventDefault();
+    });
+  }
+
+  handlePurchaseBtn() {
+    this.props.play();
   }
 }
 
-customElements.define('purchase-section', PurchaseSection);
+export default PurchaseSection;
