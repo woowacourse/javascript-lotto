f448015ebc84ecdad67aa049335f2eda2e0d677a
useon <yoosk4986@naver.com>
2024-02-28 17:23:28 +0900

    style: 발행된 로또 목록 표시 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -37,6 +37,35 @@ class LottoController {
       return PURCHASE_AMOUT_INPUT_ERROR.RANGE;
     return true;
   }
+
+  processLottoTicket() {
+    const lottoTicketCount = this.purchaseAmount / PURCHASE_SYMBOL.UNIT;
+    const lottoTicketsSection = document.querySelector('.section-lotto-ticket');
+    lottoTicketsSection.classList.remove('invisible');
+    document.querySelector(
+      '.text-lotto-count',
+    ).textContent = `총 ${lottoTicketCount}개를 구매하셨습니다.`;
+    const tickets = [];
+    Array.from({ length: lottoTicketCount }).forEach(() => {
+      const ticket = new LottoTicket().publishTicket();
+      tickets.push(ticket);
+      const lottoTicketsView = document.querySelector('.ul-lotto-tickets');
+      lottoTicketsView.innerHTML += `
+    <li class='li-lotto-ticket'>
+      <p class='text-ticket-emoji'>🎟️</p>
+      <p class='text-ticket-numbers'>${ticket.join(', ')}</p>
+    <li>
+    `;
+    });
+    this.lottoTickets = tickets;
+    this.click();
+  }
+
+  click() {
+    document
+      .querySelector('.btn-submit-lotto')
+      .addEventListener('click', this.processLottoNumbers.bind(this));
+  }
 }
 
 export default LottoController;
