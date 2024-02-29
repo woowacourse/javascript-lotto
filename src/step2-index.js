186dfbe9701b724d379176616c11c849dd6b3000
novice0840 <magnolia0840@gmail.com>
2024-02-29 09:51:36 +0900

    chrore: eslint 설정 변경

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,6 +2,7 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+/* eslint-disable*/
 import './styles/reset.css';
 import './styles/style.css';
 import LottoMachine from './domain/lottoMachine';
@@ -26,7 +27,6 @@ $buyForm.addEventListener('submit', (e) => {
   $answerForm.classList.remove('hidden');
 
   const lottoMachine = new LottoMachine(money);
-  console.log(lottoMachine.getLottoCount);
   console.log(lottoMachine.getLottoNumbers);
-  $lottoResultLabel.value = `총 ${lottoMachine.getLottoCount}개를 구매하였습니다.`;
+  $lottoResultLabel.innerText = `총 ${lottoMachine.getLottoCount}개를 구매하였습니다.`;
 });
