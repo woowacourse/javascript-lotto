c5bbb0116d4ddaabdd8b09a22664d0ff5a3570e1
Largopie <106071687+Largopie@users.noreply.github.com>
2024-02-29 15:52:24 +0900

    refactor(modal): modal innerHTML 로직 수정

---
+++ src/view/web/ModalContent.js
@@ -0,0 +1,72 @@
+export default function ModalContent() {
+  const main = document.createElement('main');
+  // 닫기 버튼
+  const closeButtonSection = document.createElement('section');
+  closeButtonSection.setAttribute('id', 'close-btn-container');
+
+  const closeButton = document.createElement('button');
+  closeButton.setAttribute('id', 'close-btn');
+
+  const closeBtnText = document.createElement('span');
+  closeBtnText.setAttribute('id', 'close-btn-text');
+  closeBtnText.innerText = 'X';
+
+  closeButton.appendChild(closeBtnText);
+  closeButtonSection.appendChild(closeButton);
+
+  // 당첨 통계 제목
+  const statisticsTitleContainer = document.createElement('section');
+  statisticsTitleContainer.setAttribute('id', 'statistics-title-container');
+  const h1 = document.createElement('h1');
+  h1.innerText = '🏆 당첨 통계 🏆';
+
+  statisticsTitleContainer.appendChild(h1);
+
+  // 통계
+  const statisticsContainer = document.createElement('section');
+  statisticsContainer.setAttribute('id', 'statistics-container');
+
+  const table = document.createElement('table');
+  table.setAttribute('id', 'statistics');
+
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th>일치 갯수</th>
+        <th>당첨금</th>
+        <th>당첨 갯수</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  `;
+
+  statisticsContainer.appendChild(table);
+
+  // 이익 출력
+  const profitContainer = document.createElement('section');
+  profitContainer.setAttribute('id', 'profit-container');
+
+  const profitText = document.createElement('span');
+  profitText.setAttribute('id', 'profit');
+
+  profitContainer.appendChild(profitText);
+
+  // 재시작 버튼
+  const retryButtonContainer = document.createElement('section');
+  retryButtonContainer.setAttribute('id', 'retry-btn-container');
+
+  const retryButton = document.createElement('button');
+  retryButton.setAttribute('id', 'retry-btn');
+  retryButton.innerText = '다시 시작하기';
+
+  retryButtonContainer.appendChild(retryButton);
+
+  main.appendChild(closeButtonSection);
+  main.appendChild(statisticsTitleContainer);
+  main.appendChild(statisticsContainer);
+  main.appendChild(profitContainer);
+  main.appendChild(retryButtonContainer);
+
+  return main;
+}
--- src/view/web/modal.js
+++ src/view/web/modal.js
@@ -1,52 +1,10 @@
-import { $ } from './utils/dom';
+import ModalContent from './ModalContent.js';
 
-export default function modal(element) {
-  const handleCloseBtn = (event) => {
-    event.preventDefault();
-    $('#modal-container').style.visibility = 'hidden';
-  };
+export default function Modal() {
+  const container = document.createElement('div');
+  container.setAttribute('id', 'modal-container');
 
-  const handleRetryBtn = (event) => {
-    event.preventDefault();
-    $('#buy-lotto-form').reset();
-    $('#winning-lotto-form').reset();
-    $('#step2').style.visibility = 'hidden';
-    $('#modal-container').style.visibility = 'hidden';
-  };
+  container.appendChild(ModalContent());
 
-  const render = (element) => {
-    element.innerHTML = `
-      <main>
-        <section id="close-btn-container"><button id="close-btn"><span id="close-btn-text">X</span></button></section>
-        
-        <section id="statistics-title-container"><h1>🏆 당첨 통계 🏆</h1></section>
-        
-        <section id="statistics-container">
-          <table id="statistics">
-            <thead>
-              <tr>
-                <th>일치 갯수</th>
-                <th>당첨금</th>
-                <th>당첨 갯수</th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
-        </section>
-        
-        <section id="profit-container">
-          <span id="profit"></span>
-        </section>
-
-        <section id="retry-btn-container">
-          <button id="retry-btn">다시 시작하기</button>
-        </section>
-      </main>
-    `;
-  };
-
-  render(element);
-  $('#close-btn').addEventListener('click', handleCloseBtn);
-  $('#retry-btn').addEventListener('click', handleRetryBtn);
+  return container;
 }
