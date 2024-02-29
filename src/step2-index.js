f173cb09352987e1d921863908d6afca7cebc30e
ss0526100 <ss0526100@gmail.com>
2024-02-29 21:38:42 +0900

    feat: 로또 결과창 출력

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,12 +2,12 @@ import "../style.css";
 
 const initBody = () => {
   preventDefaults(document.getElementsByTagName("button"), "click");
-  // grantHiddenVisibilityClass(
-  //   document.getElementById("main-contents__purchased-lotto-span")
-  // );
-  // grantHiddenVisibilityClass(
-  //   document.getElementById("main-contents__purchased-lotto-box")
-  // );
+  grantHiddenVisibilityClass(
+    document.getElementById("main-contents__purchased-lotto-span")
+  );
+  grantHiddenVisibilityClass(
+    document.getElementById("main-contents__purchased-lotto-box")
+  );
   grantHiddenVisibilityClass(
     document.getElementById("main-contents__winning-lotto-form")
   );
@@ -17,6 +17,11 @@ const initBody = () => {
 const grantHiddenVisibilityClass = (element) => {
   element.classList.add("visibility-hidden");
 };
+
+const removeHiddenVisibilityClass = (element) => {
+  element.classList.remove("visibility-hidden");
+};
+
 const deactivateButtonClass = (element) => {
   element.classList.add("unavailable_button");
   element.classList.remove("available_button");
@@ -41,9 +46,9 @@ const priceButtonClickEvent = (event) => {
   preventDefault(event);
   deactivateButtonClass(event.target);
 
+  console.log(document.getElementById("main-contents__price-input").value);
   document.getElementById("main-contents__price-input").value = "";
   deactivatePriceButton(event.target);
-  console.log(document.getElementById("main-contents__price-input").value);
 };
 
 const activatePriceButton = () => {
@@ -61,23 +66,27 @@ const deactivatePriceButton = (element) => {
 };
 // print lottos
 
-const printBoughtLottos = (lottos, numberOfLotto) => {
-  printPurchasedLottoSpan(numberOfLotto);
+const printBoughtLottos = (lottos) => {
   const lottoNumberElements = lottos.map((lotto) =>
     getPurchasedLottoElement(lotto)
   );
 
-  document.getElementById("main-contents__purchased-lotto-box").innerHTML = "";
+  const purchasedLottobox = document.getElementById(
+    "main-contents__purchased-lotto-box"
+  );
 
-  document
-    .getElementById("main-contents__purchased-lotto-box")
-    .append(...lottoNumberElements);
+  removeHiddenVisibilityClass(purchasedLottobox);
+  purchasedLottobox.innerHTML = "";
+
+  purchasedLottobox.append(...lottoNumberElements);
 };
 
-const printPurchasedLottoSpan = (numberOfLotto) => {
+const printNumberOfLotto = (numberOfLotto) => {
   const purchasedLottoSpan = document.getElementById(
     "main-contents__purchased-lotto-span"
   );
+
+  removeHiddenVisibilityClass(purchasedLottoSpan);
   purchasedLottoSpan.innerHTML = "";
   purchasedLottoSpan.innerHTML = `총 ${numberOfLotto}개를 구매하였습니다.`;
 };
@@ -105,7 +114,7 @@ const getLottoNumberSpan = (lotto) => {
 };
 
 const printBoughtSlicedLottos = (lottos, originalNumberOfLotto) => {
-  printBoughtLottos(lottos, originalNumberOfLotto);
+  printBoughtLottos(lottos);
   document
     .getElementById("main-contents__purchased-lotto-box")
     .append(
@@ -115,6 +124,68 @@ const printBoughtSlicedLottos = (lottos, originalNumberOfLotto) => {
     );
 };
 
+// activateWinningLottoForm
+
+const activateWinningLottoForm = () => {
+  const winningLottoForm = document.getElementById(
+    "main-contents__winning-lotto-form"
+  );
+  const winningLottoButton = document.getElementById(
+    "main-contents__winning-lotto-button"
+  );
+  removeHiddenVisibilityClass(winningLottoForm);
+  activateButtonClass(winningLottoButton);
+
+  winningLottoButton.removeEventListener("click", preventDefault);
+
+  winningLottoButton.addEventListener("click", winningLottoButtonClickEvent);
+};
+
+const winningLottoButtonClickEvent = (event) => {
+  preventDefault(event);
+  deactivateButtonClass(event.target);
+
+  const winningNumbersAndBonus = Array.from(
+    document.getElementsByClassName("winning-number-input")
+  );
+
+  winningNumbersAndBonus.forEach((element) => {
+    console.log(element.value);
+    element.value = "";
+  });
+
+  deactivateWinningLottoButton(event.target);
+};
+const deactivateWinningLottoButton = (element) => {
+  deactivateButtonClass(element);
+  element.removeEventListener("click", winningLottoButtonClickEvent);
+  element.addEventListener("click", preventDefault);
+};
+
+const printLottoResultIntro = () => {
+  removeHiddenVisibilityClass(document.getElementById("modal"));
+};
+const printLottoResult = (rankCounts, profitRate) => {
+  const transformIndexToRank = (index) => 5 - index;
+  const rankCountElements = Array.from(
+    document.getElementsByClassName("rank-count")
+  );
+  rankCountElements.forEach(
+    (element, index) =>
+      (element.innerText = `${rankCounts[transformIndexToRank(index)]}개`)
+  );
+  printLottoProfitRate(profitRate);
+};
+
+const printLottoProfitRate = (profitRate) => {
+  const resultSpan = document.getElementById("modal__result-span");
+  resultSpan.innerText = `당신의 총 수익률은 ${profitRate.toFixed(1)}%입니다.`;
+};
+
 initBody();
-// activatePriceButton();
+activatePriceButton();
+// printNumberOfLotto(10000);
 // printBoughtSlicedLottos(new Array(100).fill([1, 2, 3, 4, 5, 6]), 10000);
+// activateWinningLottoForm();
+// printLottoResultIntro();
+// printLottoResult([0, 0, 0, 0, 0, 0], 10);
