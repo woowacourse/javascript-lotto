aeafca8d1bdada5329ec6583a95af48156a8c578
이태훈 <rhymint@gmail.com>
2024-02-29 19:43:33 +0900

    feat: 결과 확인 버튼 클릭 시 당첨번호와 보너스 번호의 유효성 검사 및 에러 메세지 출력 기능 추가

--- src/components/WinningAndBonusForm.js
+++ src/components/WinningAndBonusForm.js
@@ -1,19 +1,20 @@
 import './WinningAndBonusForm.css';
 import './WinningNumbersInputField.js';
 import './BonusNumberInputField.js';
+import { BonusNumberValidator, WinningNumbersValidator } from '../validator/index.js';
 
 const WINNING_NUMBERS_FORM = `
 <div class="winning-and-bonus-container">
     <div class="winning-and-bonus-title-container">
       <p class="lotto-body">지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</p>
     </div>
-
+    
     <div class="winning-and-bonus-input-container">
       <winning-numbers-input-field></winning-numbers-input-field>
       <bonus-number-input-field></bonus-number-input-field>
-      
     </div>
-</div>
+    <div class="error-message"></div>
+  </div>
 <lotto-button id="result-button"></lotto-button>
 `;
 
@@ -34,7 +35,7 @@ class WinningNumbersForm extends HTMLElement {
       const winningNumbers = document.querySelector('winning-numbers-input-field').getValue();
       const bonusNumber = document.querySelector('bonus-number-input-field').getValue();
 
-      this.#lottoResultEvent({ winningNumbers, bonusNumber });
+      this.#errorHandler({ winningNumbers, bonusNumber });
     });
   }
 
@@ -46,6 +47,21 @@ class WinningNumbersForm extends HTMLElement {
       bubbles: true,
     });
     this.dispatchEvent(lottoResultEvent);
+  }
+
+  setErrorMessage(error) {
+    this.querySelector('.error-message').textContent = error.message;
+  }
+
+  #errorHandler({ winningNumbers, bonusNumber }) {
+    try {
+      WinningNumbersValidator.check(winningNumbers.join(','));
+      BonusNumberValidator.check(`${bonusNumber}`, winningNumbers);
+
+      this.#lottoResultEvent({ winningNumbers, bonusNumber });
+    } catch (error) {
+      this.setErrorMessage(error);
+    }
   }
 }
 
