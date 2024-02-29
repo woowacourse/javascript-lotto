9bd6fbc1ef5a0ac23b23ab59cb3b409c4825288c
jinsil <jinsil378900@gmail.com>
2024-02-29 17:02:05 +0900

    feat: 당첨, 보너스 번호 입력 추가

--- src/view/View.js
+++ src/view/View.js
@@ -1,8 +1,13 @@
+import { SETTING } from '../constant/setting.js';
 import elementHandler from '../handler/elementHandler.js';
 
 const $lottosContainer = elementHandler.$('.lottos-container');
 const $lottosText = elementHandler.$('.lottos-text');
 const $lottosList = elementHandler.$('.lottos-list');
+
+const $winningLottoForm = elementHandler.$('.winning-lotto-form');
+const $winningLottoInputContainer = elementHandler.$('.winning-lotto-input-container');
+const $bonusLottoInputContainer = elementHandler.$('.bonus-lotto-input-container');
 
 const View = {
   renderPurchasedLottos(lottos) {
@@ -14,7 +19,15 @@ const View = {
       .join('')}`;
   },
 
-  renderWinningNumbersInput() {},
+  renderWinningNumbersInput() {
+    $winningLottoForm.hidden = false;
+
+    $winningLottoInputContainer.innerHTML =
+      `<input class="lotto-input-box winning" type="number" min="1" max="45" required />`.repeat(SETTING.LOTTO_LENGTH);
+    $bonusLottoInputContainer.innerHTML = `<input class="lotto-input-box bonus" type="number" min="1" max="45" required />`;
+
+    elementHandler.$$('.lotto-input-box')[0].focus();
+  },
 };
 
 export default View;
