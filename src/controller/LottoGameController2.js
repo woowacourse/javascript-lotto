19ef5b741ec65a66f174266fa4c3a6ee8373b408
jinsil <jinsil378900@gmail.com>
2024-02-29 22:17:37 +0900

    feat: 당첨, 보너스 번호 입력, 유효성 검사

--- src/controller/LottoGameController2.js
+++ src/controller/LottoGameController2.js
@@ -8,9 +8,12 @@ import View from '../view/View.js';
 const $purchaseAmount = elementHandler.$('.purchase-input-box');
 const $purchaseForm = elementHandler.$('.purchase-form');
 
+const $winningLottoForm = elementHandler.$('.winning-lotto-form');
 class LottoGameController2 {
   #purchaseAmount;
   #lottosManager;
+  #winningNumbers = [];
+  #bonusNumber = 0;
 
   play() {
     this.inputPurchaseAmount();
@@ -36,13 +39,59 @@ class LottoGameController2 {
 
   #createRandomLottos() {
     const lottos = new LottoMachine(this.#purchaseAmount).getLottoNumberList();
-    this.#lottosManager = new LottosManager(lottos);
     this.#showPurchasedLottos(lottos);
+    this.#lottosManager = new LottosManager(lottos);
   }
 
   #showPurchasedLottos(lottos) {
     View.renderPurchasedLottos(lottos);
     View.renderWinningNumbersInput();
+    this.#inputWinningLotto();
+  }
+
+  #inputWinningLotto() {
+    const $winningInputs = elementHandler.$$('.lotto-input-box.winning');
+    const $bonusInput = elementHandler.$('.lotto-input-box.bonus');
+
+    $winningLottoForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const winningNumberList = [...$winningInputs].map((winningNumber) => Number(winningNumber.value));
+      const bonusNumber = $bonusInput.value;
+      this.#checkWinningNumbers(winningNumberList);
+      this.#checkBonusNumber(bonusNumber, winningNumberList);
+    });
+  }
+
+  #checkWinningNumbers(winningNumberList) {
+    const $winningInputs = elementHandler.$$('.lotto-input-box.winning');
+    const $bonusInput = elementHandler.$('.lotto-input-box.bonus');
+
+    try {
+      Validator.validateWinningNumberList(winningNumberList);
+      this.#winningNumbers = winningNumberList;
+    } catch (error) {
+      alert(error.message);
+      // 메서드 분리 예정
+      $winningInputs.forEach((winningNumber) => {
+        winningNumber.value = '';
+      });
+      $bonusInput.value = '';
+      this.#winningNumbers = [];
+      $winningInputs[0].focus();
+    }
+  }
+
+  #checkBonusNumber(bonusNumber, winningNumberList) {
+    const $bonusInput = elementHandler.$('.lotto-input-box.bonus');
+
+    try {
+      Validator.validateBonusNumber(bonusNumber, winningNumberList);
+      this.#winningNumbers = winningNumberList;
+    } catch (error) {
+      alert(error.message);
+      // 메서드 분리 예정
+      $bonusInput.value = '';
+    }
   }
 }
 
