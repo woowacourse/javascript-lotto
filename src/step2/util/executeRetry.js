b12b4773042f268b20e8972414ecac47c6b4baea
GimGayeon <kky1373@naver.com>
2024-03-01 16:27:48 +0900

    fix:async 함수로 수정,alert return 해주도록 수정

--- src/step2/util/executeRetry.js
+++ src/step2/util/executeRetry.js
@@ -1,10 +1,10 @@
-function executeRetry(asyncFunc) {
+async function executeRetry(asyncFunc) {
   try {
-    return asyncFunc();
+    return await asyncFunc();
   } catch (error) {
-    alert(error.message);
-    return executeRetry(asyncFunc);
+    return alert(error.message);
   }
 }
-
+// return 해줘야 모든 6개의 인풋 돌지 않고 에러나면 하나의 alert 창 띄움
+// async 로 감싸는 이유: asyncFunc 가 Promise 를 리턴하기 때문에
 export default executeRetry;
