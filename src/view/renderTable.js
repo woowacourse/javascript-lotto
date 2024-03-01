9aaed1fa0ffb67a9f7a25fef5ebefd5ff5ee9dda
pakxe <pigkill40@naver.com>
2024-03-01 16:06:15 +0900

    feat: 테이블을 동적으로 생성하기 위한 렌더 구현

---
+++ src/view/renderTable.js
@@ -0,0 +1,17 @@
+import { $ } from '../utils/selector.js';
+
+function renderTable (target, content) {
+  const $target = $(target);
+  const tableHeaderHtml = `<thead>${content.shift().map(title => `<th>${title}</th>`).join('')}</thead>`;
+  const tableBodyHtml = `
+  <tbody>
+    ${content.map((row) => `<tr>
+      ${row.map(el => `<td>${el}</td>`).join('')}
+      </tr>`
+    ).join('')}
+  </tbody>`
+ 
+  $target.innerHTML = tableHeaderHtml + tableBodyHtml;
+}
+
+export default renderTable;
