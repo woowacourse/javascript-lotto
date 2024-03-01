b59570a824de3c351e39df38e0957c4b4ae7e6da
brgndy <brgndy96@gmail.com>
2024-03-02 00:52:19 +0900

    test: 테스트코드 수정

--- __tests__/Money.test.js
+++ __tests__/Money.test.js
@@ -4,7 +4,7 @@ describe('돈을 담당하는 클래스에 관한 테스트 코드 작성', () =
   test.each([[1000], [8300]])('1000원 이상이 되는 금액을 투입 했을때, 1000으로 나눈 몫을 저장한다', (validInput) => {
     const money = new Money();
 
-    money.fromInputValue(validInput);
+    money.receiveInjectionValue(validInput);
 
     expect(money.getMoney()).toBe(validInput);
   });
@@ -14,6 +14,6 @@ describe('돈을 담당하는 클래스에 관한 테스트 코드 작성', () =
 
     const money = new Money();
 
-    expect(() => money.fromInputValue(invalidInput)).toThrow();
+    expect(() => money.receiveInjectionValue(invalidInput)).toThrow();
   });
 });
