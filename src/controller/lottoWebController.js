427f42d7bbdd766730f592928f66267e7d9be342
bmj0053 <bmj05378@gmail.com>
2024-03-02 00:09:33 +0900

    fix: controller async 추가

--- src/controller/lottoWebController.js
+++ src/controller/lottoWebController.js
@@ -15,7 +15,7 @@ import winningLottoValidation from "../validation/winningLottoValidation.js";
 class LottoWebController {
   #webIssuedLottoArray;
   #webBudget;
-  start() {
+  async start() {
     $("#budget").addEventListener("keydown", this.handleWebBudget.bind(this));
     $("#budget-btn").addEventListener("click", this.handleWebBudget.bind(this));
     $("#result-btn").addEventListener(
