9c1c1faf12de2541d3db11b321416e9409045499
GimGayeon <kky1373@naver.com>
2024-03-01 19:54:35 +0900

    refactor:button css 통일

--- src/step2/util/dom.js
+++ src/step2/util/dom.js
@@ -1,19 +1,19 @@
 const domSelector = {
-  lottoPrice: document.querySelector('.thousandUnitInput'),
-  lottoPriceButton: document.querySelector('.lottoPriceButton'),
-  generatedLottos: document.getElementById('generatedLottos'),
-  afterBuyLottos: document.querySelector('.afterBuyLottos'),
+  lottoPrice: document.querySelector('.thousand-unit-input'),
+  lottoPriceButton: document.querySelector('.lotto-price-button'),
+  generatedLottos: document.querySelector('.generated-lottos'),
+  afterBuyLottos: document.querySelector('.after-buy-lottos'),
 
-  inputWinningLottos: document.querySelectorAll('.inputWinningLottos'),
-  checkResultButton: document.querySelector('.checkResultButton'),
-  inputBonusLotto: document.querySelector('.inputBonusLotto'),
-  totalProfit: document.querySelector('.totalProfit'),
+  inputWinningLottos: document.querySelectorAll('.input-winning-lottos'),
+  checkResultButton: document.querySelector('.check-result-button'),
+  inputBonusLotto: document.querySelector('.input-bonus-lotto'),
+  totalProfit: document.querySelector('.total-profit'),
 
   modal: document.querySelector('.modal'),
-  restartButton: document.querySelector('.restartButton'),
-  close: document.getElementById('close'),
+  restartButton: document.querySelector('.restart-button'),
+  close: document.querySelector('.close'),
 
-  winningStatistics: document.getElementById('winningStatistics'),
+  winningStatistics: document.querySelector('.winning-statistics'),
 };
 
 export default domSelector;
