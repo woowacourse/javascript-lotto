4b158cd808e96f3e476f291f63a7922ced29366e
pakxe <pigkill40@naver.com>
2024-03-01 16:04:16 +0900

    feat: 배경을 눌러도 모달이 꺼지도록 구현

--- src/view/modal.js
+++ src/view/modal.js
@@ -8,11 +8,16 @@ const modal = {
 
     const $closeModalButton = document.getElementById("closeModalButton");
     $closeModalButton.addEventListener("click", modal.closeModal);
+    $modal.addEventListener("click", modal.closeModalByBackground);
   },
 
   closeModal() {
     const $modalBackground = document.getElementsByClassName("modalBackground")[0];
     $modalBackground.style.display = "none";
+  },
+
+  closeModalByBackground(event) {
+    if (event.target === event.currentTarget) modal.closeModal();
   },
 };
 
