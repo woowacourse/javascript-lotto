d7f88bcd9d55444c069b243f5f94c9e848ab2d61
jaeml06 <jaeml0630@gmail.com>
2024-03-01 16:11:31 +0900

    feat: 이벤트 리스너 click에서 submit으로 변경

--- src/web/WinNumberEvent.js
+++ src/web/WinNumberEvent.js
@@ -1,31 +1,33 @@
 import NUMBER from "../constants/number";
-import Modal from "./Modal";
+import Modal from "./ModalEvent";
 
 const winNumberEvent = {
-    createInputUIWinNumber(){
+    createInputUIWinNumber(randomLottos = []){
         const winNumbersBox = document.querySelector('.winnumber-input-box');
         winNumbersBox.innerHTML = `${this.createWinNumberInputBox()}`;
-        const resultButton = document.querySelector('.result-button');
-        resultButton.addEventListener('click',  Modal.createModal.bind(Modal))
+        const winNumberForm = document.querySelector('.winnumber-form');
+        winNumberForm.addEventListener('submit',  (event) => Modal.addResultButton(event, randomLottos))
     },
 
     createWinNumberInputBox(){
         return `
         <div class='winnumber-title'>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
+        <form class= 'winnumber-form'>
         <div class='input-box'>
         ${this.createWinNumberInput()}
         ${this.createBonusNumber()}
         </div>
-        <button class='result-button'>결과를 확인하기</button>
+        <div class= 'input-error error-message'></div>
+        <input class='result-button button' type = 'submit' value = '결과를 확인하기'>
+        </form>
         `
     },
 
     createWinNumberInput(){
         return`
-        
         <div>
             <div>당첨 번호</div>
-            ${Array.from({length:NUMBER.LOTTO_LENGTH}, () => `<input class='winnumber-input' type = 'text'/>`).join('')}
+            ${Array.from({length:NUMBER.LOTTO_LENGTH}, () => `<input class='winnumber-input one-number-input' type = 'text'/>`).join('')}
         </div>
         `
     },
@@ -34,7 +36,7 @@ const winNumberEvent = {
         return`
         <div>
             <div>보너스 번호</div>
-            <input class='bonusnumber-input' type = 'text'/>
+            <input class='bonusnumber-input one-number-input' type = 'text'/>
         </div>
         `
     }
