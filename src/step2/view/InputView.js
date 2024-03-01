ba51d84b4a85f19ad3c25359447f6b869356de8d
GimGayeon <kky1373@naver.com>
2024-03-01 15:28:18 +0900

    feat:당첨번호, 보너스 번호 입력 구현하기

--- src/step2/view/InputView.js
+++ src/step2/view/InputView.js
@@ -1,5 +1,5 @@
 import domSelector from '../util/dom';
-const { lottoPrice } = domSelector;
+const { lottoPrice, inputWinningLottos, inputBonusLotto } = domSelector;
 
 // input 에는 input 받는 함수 2개랑,
 // 화면에 그리는 함수들!
@@ -7,12 +7,21 @@ const { lottoPrice } = domSelector;
 //2. 생성된 로또번호
 //3. 결과 확인 시 테이블
 
-class InputView {
-  static inputLottoPrice() {
+const inputView = {
+  inputLottoPrice() {
     return parseInt(lottoPrice.value);
-  }
+  },
 
-  // innerHtml
-}
+  inputWinningNumbers() {
+    const winningLottos = [];
+    inputWinningLottos.forEach((inputWinningLotto) => {
+      winningLottos.push(parseInt(inputWinningLotto.value));
+    });
+    return winningLottos;
+  },
+  inputBonusNumber() {
+    return parseInt(inputBonusLotto.value);
+  },
+};
 
-export default InputView;
+export default inputView;
