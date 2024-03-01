bba910f7670c44bf98edf49204f9d3d41f2651c0
brgndy <brgndy96@gmail.com>
2024-03-02 00:53:01 +0900

    refactor: step2-index.js 변경된 요구사항에 맞게 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -7,6 +7,7 @@ import { inputMoneyHandler } from './step2/handlers/inputMoneyHandler.js';
 import { inputWinningLottoNumbersHandler } from './step2/handlers/inputWinningLottoNumbersHandler.js';
 import { statisticsResultHandler, modalCloseHandler } from './step2/handlers/statisticsResultHandler.js';
 import { reloadPage } from './step2/handlers/uiUtils.js';
+import './step2/styles/globals.css';
 import './step2/styles/main.css';
 import './step2/styles/header.css';
 import './step2/styles/content.css';
@@ -14,13 +15,15 @@ import './step2/styles/moneyInput.css';
 import './step2/styles/winningLottoInput.css';
 import './step2/styles/modal.css';
 import './step2/styles/footer.css';
+import './step2/styles/variables.css';
+import './step2/styles/table.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const moneyInputForm = document.getElementById('moneyInputForm');
-  const winningLottoInputs = document.querySelectorAll('.winningLottoInput');
+  const winningLottoInputs = document.querySelectorAll('.winning-lotto-input');
   const winningLottoForm = document.getElementById('winningLottoForm');
   const modalCloseButton = document.getElementById('modalCloseButton');
-  const winningCounts = document.querySelectorAll('.winningCount');
+  const winningCounts = document.querySelectorAll('.match-count-text');
   const retrySelectButton = document.getElementById('retrySelectButton');
 
   moneyInputForm.addEventListener('submit', inputMoneyHandler);
