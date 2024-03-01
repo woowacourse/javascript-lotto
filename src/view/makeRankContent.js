16a5656a54c210f9040b0195fc26af695ee27b86
pakxe <pigkill40@naver.com>
2024-03-01 15:53:43 +0900

    feat: 매치 개수로 table 내용을 반환하는 함수 구현

---
+++ src/view/makeRankContent.js
@@ -0,0 +1,10 @@
+import { PRIZE } from "../domain/calculateTotalPrize.js";
+
+function makeRankContent(rank, i) {
+  if (i < PRIZE.length - 2) return [`${i + 3}개`, PRIZE[i], `${rank}개`];
+  if (i === PRIZE.length - 2) return [`${i + 2}개 + 보너스볼`, PRIZE[i], `${rank}개`];
+
+  return [`${i + 2}개`, PRIZE[i], `${rank}개`];
+}
+
+export default makeRankContent;
