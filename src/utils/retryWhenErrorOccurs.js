c4c6430c6a76aaf2e8a99383a9d621d9087fc897
ss0526100 <ss0526100@gmail.com>
2024-03-02 19:52:48 +0900

    feat(retryWhenErrorOccurs): 브라우저 동작 추가

--- src/utils/retryWhenErrorOccurs.js
+++ src/utils/retryWhenErrorOccurs.js
@@ -1,8 +1,15 @@
+const errorPrint = (error) => {
+  if (typeof window === "undefined") {
+    console.log(error.message);
+  } else window.alert(error.message);
+};
+
 async function retryWhenErrorOccurs(callback, ...args) {
   try {
     return await callback(...args);
   } catch (error) {
-    console.log(error.message);
+    errorPrint(error);
+
     return retryWhenErrorOccurs(callback, ...args);
   }
 }
