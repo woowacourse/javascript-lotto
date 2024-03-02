8cd0296ebbff6ce6bafbf16a53fb4889bae135fa
김진호 <rlawlsgh1227@gmail.com>
2024-03-02 15:22:09 +0900

    refactor: prizeDetail 함수를 더 보기 좋게 변경

--- src/controller/generator/WebMessageGenerator.js
+++ src/controller/generator/WebMessageGenerator.js
@@ -10,9 +10,12 @@ const WebMessageGenerator = {
     return `총 ${count}개를 구매했습니다.`;
   },
   prizeDetail(prizeInfo) {
+    const matchText = prizeInfo.detail.MATCH ? `${prizeInfo.detail.MATCH}개` : '0개';
+    const bonusBallText = prizeInfo.rank === RANK.SECOND_PLACE ? '+보너스볼' : '';
+    const rewardText = prizeInfo.detail.REWARD ? prizeInfo.detail.REWARD.toLocaleString() : '0';
     return {
-      count: `${prizeInfo.detail.MATCH}개${prizeInfo.rank === RANK.SECOND_PLACE ? '+보너스볼' : ''}`,
-      reward: prizeInfo.detail.REWARD.toLocaleString(),
+      count: `${matchText}${bonusBallText}`,
+      reward: rewardText,
       prizeCount: `${prizeInfo.count}개`,
     };
   },
