5845e5df073850eb93979d26c4588e66a7511025
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-03-02 13:03:40 +0900

    refactor: 'fiveBonus' 문자열을 상수로 선언

--- src/utils/createWinningResult.js
+++ src/utils/createWinningResult.js
@@ -1,15 +1,17 @@
 import LOTTO_STATISTICS from '../constants/lotto-statistics';
 
+const COMPARE_LOTTO_COUNT = 'fiveBonus';
+
 export function createWinningResult(key, count) {
   const { number, price } = LOTTO_STATISTICS[key];
-  const bonusText = key === 'fiveBonus' ? ', 보너스 일치' : '';
+  const bonusText = key === COMPARE_LOTTO_COUNT ? ', 보너스 일치' : '';
 
   return `${number}개 일치${bonusText} (${price.toLocaleString()}원) - ${count}개`;
 }
 
 export function createWebWinningResult(key, count) {
   const { number, price } = LOTTO_STATISTICS[key];
-  const bonusText = key === 'fiveBonus' ? ' + 보너스볼' : '';
+  const bonusText = key === COMPARE_LOTTO_COUNT ? ' + 보너스볼' : '';
 
   return `<tr><td>${number}개${bonusText}</td><td>${price.toLocaleString()}원</td><td>${count}개</td></tr>`;
 }
