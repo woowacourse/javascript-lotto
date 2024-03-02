7777e5af6b65de4ec03b5bfc42ad537efe956e15
pakxe <pigkill40@naver.com>
2024-03-02 18:59:03 +0900

    feat: 주입식으로 뷰를 받도록 수정

--- src/controller/WebLottoController.js
+++ src/controller/WebLottoController.js
@@ -1,94 +1,111 @@
 import LottoMoney from "../domain/LottoMoney.js";
 import LottoNumber from "../domain/LottoNumber.js";
-import { calculateTotalPrize } from '../domain/calculateTotalPrize.js';
+import { calculateTotalPrize } from "../domain/calculateTotalPrize.js";
 import getLottoRank from "../domain/getLottoRank.js";
 import { generateRandomLottos, generateWinningLotto } from "../domain/lottoGenerator.js";
-import { calculateProfitRate } from '../utils/calculateProfitRate.js';
-import modal from "../view/modal.js";
-import view from "../view/view.js";
+import BonusNumberError from "../error/BonusNumberError.js";
+import LottoError from "../error/LottoError.js";
+import { calculateProfitRate } from "../utils/calculateProfitRate.js";
+import catchReturnInWeb from "../utils/web/catchReturnInWeb.js";
+import modal from "../view/web/modal.js";
+import toast from "../view/web/toast.js";
 
 class WebLottoController {
-  #lottoMoney;
+  #inputView;
 
-  #randomLottos;
+  #outputView;
 
-  #winningLotto;
-
-  getMoney(moneyInput) {
-    try {
-
-      this.#lottoMoney = new LottoMoney(moneyInput);
-      this.#randomLottos = this.#generateRandomLottos(this.#lottoMoney.getLottoCount());
-      this.#printRandomLottos(this.#randomLottos);
-      view.printWinningLottoAndBonusInputForm();
-      // 랜덤 번호와 정답 번호 입력 창 렌더링
-    } catch (e) {
-      alert(e);
-    }
+  constructor(inputView, outputView) {
+    this.#inputView = inputView;
+    this.#outputView = outputView;
   }
 
-  getWinningLottoAndBonus() {}
+  async game() {
+    const money = await catchReturnInWeb(this.getMoney.bind(this));
+    const randomLottos = this.generateRandomLottos(money.getLottoCount());
+    const winningLotto = await this.initWinningLotto();
+    this.#outputView.blockWinningLottoAndBonusInput();
+    const rank = this.calcRank({
+      winningLotto: winningLotto.get(),
+      bonusNumber: winningLotto.getBonusNumber(),
+      randomLottos,
+    });
 
-  #generateRandomLottos(money) {
+    this.printProfitRate(money.get(), rank);
+    await this.restart();
+  }
+
+  async getMoney() {
+    const moneyInput = await this.#inputView.readMoney();
+
+    const money = new LottoMoney(moneyInput);
+
+    this.#outputView.blockMoneyInput();
+
+    return money;
+  }
+
+  generateRandomLottos(money) {
     const randomLottos = generateRandomLottos(money);
 
-    this.#printRandomLottos(randomLottos);
+    this.printRandomLottos(randomLottos);
+    this.#outputView.printWinningLottoAndBonusInputForm();
 
     return randomLottos;
   }
 
-  #printRandomLottos(randomLottos) {
-    const randomLottosUnpack = randomLottos.map((lotto) => lotto.get());
-    view.printRandomLottos(randomLottosUnpack);
-  }
-
-  initWinningLotto(winningLottoInput, bonusNumberInput) {
+  async initWinningLotto() {
     try {
-      const winningLotto = generateWinningLotto(winningLottoInput);
-      const bonusNumber = new LottoNumber(bonusNumberInput);
+      return await this.getWinningLottoAndBonusNumber();
+    } catch (error) {
+      toast(error.message);
+      if (error instanceof LottoError) this.#outputView.resetTargetForm("#winningLottoInputForm");
+      if (error instanceof BonusNumberError) this.#outputView.resetTargetForm("#bonusInputForm");
 
-      winningLotto.setBonusNumber(bonusNumber);
-
-      this.#winningLotto = winningLotto;
-
-      this.printRankAndProfitRate();
-    } catch (e) {
-      alert(e);
+      return await this.initWinningLotto();
     }
   }
 
-  printRankAndProfitRate() {
+  async getWinningLottoAndBonusNumber() {
+    const [winningLottoInput, bonusNumberInput] = await this.#inputView.readWinningLottoNumbersAndBonus();
+    const winningLotto = generateWinningLotto(winningLottoInput);
+    const bonusNumber = new LottoNumber(bonusNumberInput);
+
+    winningLotto.setBonusNumber(bonusNumber);
+
+    return winningLotto;
+  }
+
+  printRandomLottos(randomLottos) {
+    const randomLottosUnpack = randomLottos.map((lotto) => lotto.get());
+
+    this.#outputView.printRandomLottos(randomLottosUnpack);
+  }
+
+  printProfitRate(money, ranks) {
+    const totalPrize = calculateTotalPrize(ranks);
+    const profitRate = calculateProfitRate(totalPrize, money);
+
+    this.#outputView.printProfitRate(profitRate);
+  }
+
+  calcRank({ winningLotto, bonusLottoNumber, randomLottos }) {
     modal.openModal();
 
-    const ranks = getLottoRank({
-      winningLotto: this.#winningLotto.get(),
-      bonusLottoNumber: this.#winningLotto.getBonusNumber(),
-      randomLottos: this.#randomLottos,
+    const ranks = getLottoRank({ winningLotto, bonusLottoNumber, randomLottos });
+
+    this.#outputView.printRanks(ranks);
+
+    return ranks;
+  }
+
+  async restart() {
+    this.#inputView.restart(() => {
+      modal.closeModal();
+      this.#outputView.resetToStart();
+      this.game();
     });
-
-    view.printRank(ranks);
-    this.printProfitRate(ranks)
-  }
-
-  printProfitRate(ranks) {
-    const totalPrize = calculateTotalPrize(ranks);
-    const profitRate = calculateProfitRate(totalPrize, this.#lottoMoney.get());
-
-    view.printProfitRate(profitRate);
-  }
-
-  restart() {
-    modal.closeModal();
-    view.removeRandomLottosAndWinningForm();
-    view.clearForm();
   }
 }
 
 export default WebLottoController;
-
-/**
- * 핸들러를 장착한다. 그 안의 콟백은 랜덤 로또 출력하기와 정답 입력창 만들기
- * 그리고 정답 인풋의 핸들러를 장착한다.
- *
- * 정답 인풋의 핸들러를 클릭하면 모달이 뜬다.
- */
