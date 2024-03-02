44aebcead261878098cdde740518c7a5bf8cf00f
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-02 15:01:09 +0900

    refactor(header): 중복 css 속성 제거 및 헤더 클래스 추가

--- src/view/web/header/Header.js
+++ src/view/web/header/Header.js
@@ -2,7 +2,9 @@ export default function Header() {
   const header = document.createElement('header');
   const lottoGameTitle = document.createElement('h1');
 
+  lottoGameTitle.classList.add('header-title');
   lottoGameTitle.innerText = '🎱 행운의 로또';
+
   header.appendChild(lottoGameTitle);
 
   return header;
