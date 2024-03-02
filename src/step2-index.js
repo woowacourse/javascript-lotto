b59c89f43b45fac0c92441e2c60f13a6b51036c1
hwinkr <dnddl8280@naver.com>
2024-03-02 19:56:44 +0900

    feat(step2-index.js): 웹 기반 로또 게임 엔트리 구현
    - 로또 게임 컨트롤러 호출 후 이벤트 바인딩

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,21 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import "./step2/styles/reset.css";
+import "./step2/styles/layout.css";
+import "./step2/styles/common.css";
+import LottoController from "./step2/controllers/LottoController";
+import {
+  registerCloseModalEvent,
+  registerPurchaseEvent,
+  registerRenderResultEvent,
+  registerRestartEvent,
+} from "./step2/dom/event";
+
+const lottoController = new LottoController();
+
+registerPurchaseEvent(lottoController.purchaseLottos.bind(lottoController));
+registerRenderResultEvent(
+  lottoController.renderWinningResult.bind(lottoController),
+);
+registerCloseModalEvent(
+  lottoController.closeWinningResultModal.bind(lottoController),
+);
+registerRestartEvent(lottoController.restartLottoGame.bind(lottoController));
