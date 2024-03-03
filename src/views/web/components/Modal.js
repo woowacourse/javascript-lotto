15025a2ab78d12b5df221a55f0fab8a0732361c9
Kim Da Eun <midekuna@gmail.com>
2024-03-03 20:47:31 +0900

    refactor: Modal style 정의를 template-modal의 style 태그로 이동

--- src/views/web/components/Modal.js
+++ src/views/web/components/Modal.js
@@ -5,54 +5,6 @@ export const MODAL_EVENTS = {
 
 export const MODAL_SELECTOR = 'app-modal';
 
-const styleText = `
-  .modal {
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 100vh;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    background-color: rgba(var(--color-grey-scale-5), 0.5);
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-
-    background-color: rgba(var(--color-grey-scale-1));
-    padding: 1.6rem;
-  }
-  
-  .close {
-    /* cancel default style */
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-
-    /* appearance */
-    background-color: transparent;
-    border: none;
-
-    /* text */
-    line-height: 1.6rem;
-    letter-spacing: 0.125rem;
-    font-size: 2rem;
-    font-weight: var(--weight-bold);
-  }
-
-  .close:hover {
-    cursor: pointer;
-  }
-`;
-
 export default class Modal extends HTMLElement {
   #shadowRoot;
 
@@ -63,7 +15,6 @@ export default class Modal extends HTMLElement {
 
     this.#shadowRoot = this.attachShadow({ mode: 'open' });
     this.#initDOM();
-    this.#initStyles();
 
     this.#closeBtn = this.shadowRoot.querySelector('.close');
   }
@@ -89,11 +40,5 @@ export default class Modal extends HTMLElement {
     const template = document.getElementById('template-modal');
     const content = template.content.cloneNode(true);
     this.#shadowRoot.appendChild(content);
-  }
-
-  #initStyles() {
-    const styles = document.createElement('style');
-    styles.textContent = styleText;
-    this.#shadowRoot.appendChild(styles);
   }
 }
