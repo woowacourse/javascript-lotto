385a129f503183cafa89d419101754db990da02d
Soyeon Choe <soy2302ten@gmail.com>
2024-03-03 11:44:07 +0900

    refactor: form, label 태그로 변경

--- static/html/purchaseBefore.js
+++ static/html/purchaseBefore.js
@@ -1,10 +1,10 @@
 export default function purchaseBefore() {
   return `<div class="lp-title">🎱 내 번호 당첨 확인 🎱</div>
     <div class="lp-purchase-amount">
-      <div class="lp-pa-text">구입할 금액을 입력해주세요.</div>
-      <div class="lp-pa-input-group">
+      <label class="lp-pa-text" for="purchase-amount">구입할 금액을 입력해주세요.</label>
+      <form class="lp-pa-input-group">
         <input type="number" class="lp-pa-input-amount" id="purchase-amount" placeholder="금액" autofocus required />
         <button class="lp-pa-input-btn">구입</button>
-      </div>
+      </form>
     </div>`;
 }
