9d4a6304743b34c47903973ca56f22982508fa39
Soyeon Choe <soy2302ten@gmail.com>
2024-03-03 12:41:02 +0900

    refactor: 반복되는 당첨금 출력, 반복문 사용

--- static/html/statisticsModal.js
+++ static/html/statisticsModal.js
@@ -1,5 +1,6 @@
 export default function statisticsModal(lottoRanks, totalProfitRate) {
   const ranks = lottoRanks.map((rank) => rank[1]);
+  console.log([...ranks].reverse());
   return `<div class="modal-body">
       <div class="lotto-modal">
         <button class="lm-close-btn">X</button>
@@ -10,34 +11,31 @@ export default function statisticsModal(lottoRanks, totalProfitRate) {
             <div class="lm-wr-text">당첨금</div>
             <div class="lm-wr-text">당첨 갯수</div>
           </div>
-          <div class="lm-wr-row-box">
-            <div class="lm-wr-text">3개</div>
-            <div class="lm-wr-text">5,000</div>
-            <div class="lm-wr-text">${ranks[4]}개</div>
-          </div>
-          <div class="lm-wr-row-box">
-            <div class="lm-wr-text">4개</div>
-            <div class="lm-wr-text">50,000</div>
-            <div class="lm-wr-text">${ranks[3]}개</div>
-          </div>
-          <div class="lm-wr-row-box">
-            <div class="lm-wr-text">5개</div>
-            <div class="lm-wr-text">1,500,000</div>
-            <div class="lm-wr-text">${ranks[2]}개</div>
-          </div>
-          <div class="lm-wr-row-box">
-            <div class="lm-wr-text">5개+보너스볼</div>
-            <div class="lm-wr-text">30,000,000</div>
-            <div class="lm-wr-text">${ranks[1]}개</div>
-          </div>
-          <div class="lm-wr-row-box">
-            <div class="lm-wr-text">6개</div>
-            <div class="lm-wr-text">2,000,000,000</div>
-            <div class="lm-wr-text">${ranks[0]}개</div>
-          </div>
+          ${displayWinningResultList(ranks)}
         </div>
         <div class="lm-winning-statistics">당신의 총 수익률은 ${totalProfitRate}%입니다.</div>
         <button class="lm-retry-btn default-button">다시 시작하기</button>
       </div>
     </div>`;
+}
+
+function displayWinningResultList(ranks) {
+  const reward = [
+    ['3개', '5,000'],
+    ['4개', '50,000'],
+    ['5개', '1,500,000'],
+    ['5개+보너스볼', '30,000,000'],
+    ['6개', '2,000,000,000'],
+  ];
+  let winningResult = '';
+  [...ranks].reverse().forEach((rank, idx) => {
+    winningResult += `
+    <div class="lm-wr-row-box">
+      <div class="lm-wr-text">${reward[idx][0]}</div>
+      <div class="lm-wr-text">${reward[idx][1]}</div>
+      <div class="lm-wr-text">${rank}</div>
+    </div>
+    `;
+  });
+  return winningResult;
 }
