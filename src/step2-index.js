f68023533a759a604d48147998a3fac6fff51f5a
hain-tain <hain7ce@gmail.com>
2024-03-03 12:27:42 +0900

    feat: step2-index.js 에 LottoStoreBox, WinningLottoForm 적용

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,10 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+import LottoStoreBox from "./step2/web/LottoStoreBox.js";
+import WinningLottoForm from "./step2/web/WinningLottoForm.js";
+import "./css/style.css";
+
+LottoStoreBox.addSubmitEvent();
+WinningLottoForm.addSubmitEvent();
