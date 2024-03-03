b551cdd70cf90c8400d3f2c43473d24dd7a0bfc8
hain-tain <hain7ce@gmail.com>
2024-03-03 12:25:12 +0900

    feat: Outputview 생성 - lottos 출력하는 printLottos 와 지우는 removeLottos - 에러 메세지 출력하는 printErrorMessage 와 지우는 removeErrorMessage - resultModal에 당첨 통계와 수익률 출력하는 printResultTable, printReturnRate

---
+++ src/step2/view/Outputview.js
@@ -0,0 +1,47 @@
+import WinningLottoForm from "../web/WinningLottoForm.js";
+
+export const printLottos = (lottos) => {
+  const $lottosCount = document.getElementById("lottosCount");
+  const $lottosUl = document.getElementById("lottosUl");
+  if ($lottosUl.firstChild) {
+    $lottosCount.innerText = ``;
+    $lottosUl.replaceChildren();
+  }
+  const $lottoElements = lottos.map((lotto) => {
+    const $lottoLi = document.createElement("li");
+    $lottoLi.innerText = `🎟️ ${lotto
+      .getNumbers()
+      .sort((a, b) => a - b)
+      .join(", ")}`;
+    return $lottoLi;
+  });
+  $lottosUl.append(...$lottoElements);
+  $lottosCount.innerText = `총 ${lottos.length}개를 구매하였습니다.`;
+
+  WinningLottoForm.show();
+};
+
+export const removeLottos = () => {
+  const $lottosCount = document.getElementById("lottosCount");
+  const $lottosUl = document.getElementById("lottosUl");
+  $lottosCount.innerText = ``;
+  $lottosUl.replaceChildren();
+};
+
+export const printResultTable = (results) => {
+  const resultTable = document.getElementById("resultTable");
+  resultTable.append(...results.reverse());
+};
+
+export const printReturnRate = (returnRate) => {
+  const $returnRate = document.getElementById("returnRate");
+  $returnRate.innerText = `당신의 총 수익률은 ${returnRate}% 입니다.`;
+};
+
+export const printErrorMessage = ({ location, errorMessage }) => {
+  location.innerText = errorMessage;
+};
+
+export const removeErrorMessage = ({ location }) => {
+  location.innerText = "";
+};
