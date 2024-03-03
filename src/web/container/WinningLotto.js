962a0023b7207ef4822e457dc051a0d5ae087868
greetings1012 <ahnqwerty@gmail.com>
2024-03-03 22:18:50 +0900

    feat: 결과 버튼을 누르면 당첨 번호와 보너스 번호 입력받는 기능 구현

--- src/web/container/WinningLotto.js
+++ src/web/container/WinningLotto.js
@@ -12,24 +12,34 @@ const WinningLotto = {
 
   createWinningLottoInteraction() {
     const winningLottoInputInformation = document.querySelector('#game-winninglotto-input-numbers-information');
-    winningLottoInputInformation.innerHTML = '당첨 번호'
+    winningLottoInputInformation.innerHTML = '당첨 번호';
     const winningLottoInteraction = document.querySelector('#game-winninglotto-input-numbers-interaction');
     winningLottoInteraction.innerHTML = `
     ${Array.from({length:Lotto.NUMBER_COUNT}, () => 
-    `<input class='number-input-interaction' name='winning-number' type = 'number'/>`).join('  ')}`
+    `<input class='number-input-interaction' name='winning-number' type='number'/>`).join('  ')}`
   },
 
   createBonusLottoInteraction() {
     const bonusLottoInputInformation = document.querySelector('#game-winninglotto-input-bonus-information');
-    bonusLottoInputInformation.innerHTML = '보너스 번호'
+    bonusLottoInputInformation.innerHTML = '보너스 번호';
     const bonusLottoInputInteraction = document.querySelector('#game-winninglotto-input-bonus-interaction');
-    bonusLottoInputInteraction.innerHTML =  `<input class='number-input-interaction' name='bonus-number' type = 'number'/>`;
+    bonusLottoInputInteraction.innerHTML =  `<input class='number-input-interaction' name='bonus-number' type='number'/>`;
   },
   
   createResultButton() {
     const resultButton = document.querySelector('.result-button-container');
     resultButton.innerHTML = '<button class="result-button"> 결과 확인</button>';
-  }
+    resultButton.addEventListener('click', () => {
+      try {
+        const winningNumbers = Array.from(document.querySelectorAll('[name="winning-number"]')).map(input => String(input.value));
+        const winningLotto = new Lotto(winningNumbers.join());
+        const bonusNumber = new LottoNumber(document.querySelector('[name="bonus-number"]').value);
+        winningLotto.checkHaveBonus(bonusNumber.getLottoNumber());
+      } catch (error) {
+          alert(error);
+      }
+    });
+}
 }
 
 export default WinningLotto;
