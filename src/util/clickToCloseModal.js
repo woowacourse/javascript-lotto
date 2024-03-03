2b92e97b343b1ada8b391604b8e02f3ab171f3a9
Soyeon Choe <soy2302ten@gmail.com>
2024-03-03 13:28:16 +0900

    refactor: html 의존성 제거하여 모달 닫기

--- src/util/clickToCloseModal.js
+++ src/util/clickToCloseModal.js
@@ -2,7 +2,7 @@ export default function clickToCloseModal(clickBox) {
   document.querySelector(`.${clickBox}`).addEventListener('click', (event) => {
     if (event.target.classList.contains(clickBox)) {
       const modal = document.querySelector('.modal-body');
-      modal.parentNode.removeChild(modal);
+      modal.remove();
     }
   });
 }
