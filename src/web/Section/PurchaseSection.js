e535188bbf3dc239a779737b356b44df077ee255
jaeml06 <jaeml0630@gmail.com>
2024-03-04 17:36:55 +0900

    refactor: Ident 2로 수정

--- src/web/Section/PurchaseSection.js
+++ src/web/Section/PurchaseSection.js
@@ -1,29 +1,29 @@
-import NUMBER from "../../constants/number";
-import LottoService from "../../domain/LottoService";
-import PurchaseAmountValidation from "../../validation/purchaseAmount";
-import RandomSection from "./RandomSection";
-import WinNumberSection from "./WinNumberSection";
-import Error from "../../util/Error";
+import NUMBER from '../../constants/number';
+import LottoService from '../../domain/LottoService';
+import PurchaseAmountValidation from '../../validation/purchaseAmount';
+import RandomSection from './RandomSection';
+import WinNumberSection from './WinNumberSection';
+import Error from '../../util/Error';
 
 const PurchaseSection = {
-    initPurchaseEvent(){
-        const puchaseForm = document.querySelector('.puchase-form');
-        puchaseForm.addEventListener('submit', this.purchaseSubmitEvent.bind(this));
-    },
+  initPurchaseEvent() {
+    const puchaseForm = document.querySelector('.puchase-form');
+    puchaseForm.addEventListener('submit', this.purchaseSubmitEvent.bind(this));
+  },
 
-    purchaseSubmitEvent(event = {}){
-        event.preventDefault();
-        const purchaseInput = document.querySelector('.purchase-input-text').value;
-        try{
-            PurchaseAmountValidation.validate(purchaseInput);
-            const randomLottos = LottoService.getLottos(Number.parseInt(Number(purchaseInput) / NUMBER.LOTTO_PRICE, 10));
-            Error.closeMessage('.purchase-error');
-            RandomSection.showRandomLottos('.random-lottos', randomLottos)
-            WinNumberSection.createInputUIWinNumber(randomLottos);
-        } catch (error){
-            Error.showMessage('.purchase-error', error);
-        }
-    },
-}
+  purchaseSubmitEvent(event = {}) {
+    event.preventDefault();
+    const purchaseInput = document.querySelector('.purchase-input-text').value;
+    try {
+      PurchaseAmountValidation.validate(purchaseInput);
+      const randomLottos = LottoService.getLottos(Number.parseInt(Number(purchaseInput) / NUMBER.LOTTO_PRICE, 10));
+      Error.closeMessage('.purchase-error');
+      RandomSection.showRandomLottos('.random-lottos', randomLottos);
+      WinNumberSection.createInputUIWinNumber(randomLottos);
+    } catch (error) {
+      Error.showMessage('.purchase-error', error);
+    }
+  },
+};
 
 export default PurchaseSection;
