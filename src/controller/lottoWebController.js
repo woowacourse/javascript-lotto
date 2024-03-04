783cc079980a3c6eec63b1ad8b890013bfab67fa
bmj0053 <bmj05378@gmail.com>
2024-03-04 23:13:49 +0900

    refactor: budget을 form 태그로 변경

--- src/controller/lottoWebController.js
+++ src/controller/lottoWebController.js
@@ -15,9 +15,14 @@ import winningLottoValidation from "../validation/winningLottoValidation.js";
 class LottoWebController {
   #webIssuedLottoArray;
   #webBudget;
+
   async start() {
-    $("#budget").addEventListener("keydown", this.handleWebBudget.bind(this));
-    $("#budget-btn").addEventListener("click", this.handleWebBudget.bind(this));
+    $("#content-box-input-budget").addEventListener(
+      "submit",
+      this.handleWebBudget.bind(this)
+    );
+    // $("#budget").addEventListener("keydown", this.handleWebBudget.bind(this));
+    // $("#budget-btn").addEventListener("click", this.handleWebBudget.bind(this));
     $("#result-btn").addEventListener(
       "click",
       this.handleWebWinningCombinationInput.bind()
@@ -43,22 +48,21 @@ class LottoWebController {
    * @param {event} e
    */
   handleWebBudget(event) {
-    if (event.type === "click" || event.keyCode === 13) {
-      this.getWebBudget();
-      try {
-        startValidation(budgetValidation.categories, this.#webBudget);
+    event.preventDefault();
+    this.getWebBudget();
+    try {
+      startValidation(budgetValidation.categories, this.#webBudget);
 
-        const webIssuedLottoCount = this.calculateWebIssuedLottoCount();
+      const webIssuedLottoCount = this.calculateWebIssuedLottoCount();
 
-        $("#after-budget").style.display = "flex";
-        const budgetInputNodes = $("#content-box-input-budget");
-        budgetInputNodes.querySelector("input").disabled = true;
-        budgetInputNodes.querySelector("button").disabled = true;
+      $("#after-budget").style.display = "flex";
+      const budgetInputNodes = $("#content-box-input-budget");
+      budgetInputNodes.querySelector("input").disabled = true;
+      budgetInputNodes.querySelector("button").disabled = true;
 
-        this.handleWebIssuedLottoArray(webIssuedLottoCount);
-      } catch (error) {
-        return alert(error.message);
-      }
+      this.handleWebIssuedLottoArray(webIssuedLottoCount);
+    } catch (error) {
+      return alert(error.message);
     }
   }
 
