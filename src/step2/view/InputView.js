999ddd188dc265b65db57fe789f0a15ed06b4f93
GimGayeon <kky1373@naver.com>
2024-03-04 14:23:39 +0900

    fix:input-label 연결

--- src/step2/view/InputView.js
+++ src/step2/view/InputView.js
@@ -1,12 +1,6 @@
 import domSelector from '../util/dom';
 const { lottoPrice, inputWinningLottos, inputBonusLotto } = domSelector;
 
-// input 에는 input 받는 함수 2개랑,
-// 화면에 그리는 함수들!
-//1. 총 몇개
-//2. 생성된 로또번호
-//3. 결과 확인 시 테이블
-
 const inputView = {
   inputLottoPrice() {
     return parseInt(lottoPrice.value);
