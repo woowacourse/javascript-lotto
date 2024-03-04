7233a5ca1be42aaebe584e4322b5ba7deb3d4eb0
pakxe <pigkill40@naver.com>
2024-03-04 17:29:22 +0900

    feat: 입력되지 않은 번호가 있을 때 오류메세지 추가 작성

--- src/error/errorMessage.js
+++ src/error/errorMessage.js
@@ -4,6 +4,7 @@ const ERROR_MESSAGE = {
   lottoNumbersTooManyOrLess: `로또 번호는 ${LOTTO.count}개입니다.`,
   lottoNumbersOverlapped: "당첨 번호는 중복될 수 없습니다.",
   bonusNumberOverlapped: "보너스 번호는 당첨 번호와 중복될 수 없습니다.",
+  lottoNumberEmpty: "입력되지 않은 번호가 있습니다.",
 
   lottoMoneyNotNumber: `구입 금액은 ${LOTTO_MONEY.min}원 이상 ${LOTTO_MONEY.max}원 이하의 정수로 입력해야 합니다.`,
   lottoMoneyNotInRange: `구입 금액은 ${LOTTO_MONEY.min}원 이상 ${LOTTO_MONEY.max}원 이하의 정수로 입력해야 합니다.`,
