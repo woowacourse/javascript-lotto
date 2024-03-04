27d055d0bb41b4637d56f4602ff80e872bf14a9c
jaeml06 <jaeml0630@gmail.com>
2024-03-04 17:35:37 +0900

    refactor: innerHTML을 createElement로 수정

--- src/web/Section/RandomSection.js
+++ src/web/Section/RandomSection.js
@@ -1,34 +1,38 @@
 const RandomSection = {
-    showRandomLottos(selector = '', lottos = []) {
-        const randomLottoContainer = document.querySelector(selector);
-        randomLottoContainer.appendChild(this.createRandomLottos(lottos));
-    },
-
-    createRandomLottos(lottos = []) {
-        const containerDiv = document.createElement('div');
-        const titleDiv = document.createElement('div');
-        titleDiv.className = "random-lottos-result-title";
-        titleDiv.textContent = `총 ${lottos.length}개를 구매하였습니다.`;
-        containerDiv.appendChild(titleDiv);
-        containerDiv.appendChild(this.createRandomLottoList(lottos));
-        return containerDiv;
-    },
-
-    createRandomLottoList(lottos = []) {
-        const ul = document.createElement('ul');
-        lottos.forEach(lotto => {
-            const li = document.createElement('li');
-            li.className = "random-lotto-list";
-            const span = document.createElement('span');
-            span.className = 'lotto-list-p';
-            span.textContent = lotto.getNumbers().join(', ');
-
-            li.textContent = '🎟️ ';
-            li.appendChild(span);
-            ul.appendChild(li);
-        });
-        return ul;
+  showRandomLottos(selector = '', lottos = []) {
+    const randomLottoContainer = document.querySelector(selector);
+    while (randomLottoContainer.firstChild) {
+      randomLottoContainer.removeChild(randomLottoContainer.firstChild);
     }
-}
+    randomLottoContainer.appendChild(this.createRandomLottos(lottos));
+  },
+
+  createRandomLottos(lottos = []) {
+    const containerDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'random-lottos-result-title';
+    titleDiv.textContent = `총 ${lottos.length}개를 구매하였습니다.`;
+    containerDiv.appendChild(titleDiv);
+    containerDiv.appendChild(this.createRandomLottoList(lottos));
+    return containerDiv;
+  },
+
+  createRandomLottoList(lottos = []) {
+    const ul = document.createElement('ul');
+    const fragment = document.createDocumentFragment();
+    lottos.forEach((lotto) => {
+      const li = document.createElement('li');
+      li.className = 'random-lotto-list';
+      const span = document.createElement('span');
+      span.className = 'lotto-list-p';
+      span.textContent = lotto.getNumbers().join(', ');
+      li.textContent = '🎟️ ';
+      li.appendChild(span);
+      fragment.appendChild(li);
+    });
+    ul.appendChild(fragment);
+    return ul;
+  },
+};
 
 export default RandomSection;
--- src/web/Section/WinNumberSection.js
+++ src/web/Section/WinNumberSection.js
@@ -1,45 +1,69 @@
-import NUMBER from "../../constants/number";
-import ModalSection from "./ModalSection";
+import NUMBER from '../../constants/number';
+import ModalSection from './ModalSection';
 
 const WinNumberSection = {
-    createInputUIWinNumber(randomLottos = []){
-        const winNumbersBox = document.querySelector('.winnumber-input-box');
-        winNumbersBox.innerHTML = `${this.createWinNumberInputBox()}`;
-        const winNumberForm = document.querySelector('.winnumber-form');
-        winNumberForm.addEventListener('submit',  (event) => ModalSection.addResultButton(event, randomLottos))
-    },
-
-    createWinNumberInputBox(){
-        return `
-        <div class='winnumber-title'>지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</div>
-        <form class= 'winnumber-form'>
-        <div class='input-box'>
-        ${this.createWinNumberInput()}
-        ${this.createBonusNumber()}
-        </div>
-        <div class= 'input-error error-message'></div>
-        <input class='result-button button' type = 'submit' value = '결과를 확인하기'>
-        </form>
-        `
-    },
-
-    createWinNumberInput(){
-        return`
-        <div>
-            <div>당첨 번호</div>
-            ${Array.from({length:NUMBER.LOTTO_LENGTH}, () => `<input class='winnumber-input one-number-input' type = 'text'/>`).join('')}
-        </div>
-        `
-    },
-
-    createBonusNumber(){
-        return`
-        <div>
-            <div>보너스 번호</div>
-            <input class='bonusnumber-input one-number-input' type = 'text'/>
-        </div>
-        `
+  createInputUIWinNumber(randomLottos = []) {
+    const winNumbersBox = document.querySelector('.winnumber-input-box');
+    while (winNumbersBox.firstChild) {
+      winNumbersBox.removeChild(winNumbersBox.firstChild);
     }
-}
+    const form = this.createWinNumberInputBox();
+    winNumbersBox.appendChild(form);
+    form.addEventListener('submit', (event) => ModalSection.addResultButton(event, randomLottos));
+  },
+
+  createWinNumberInputBox() {
+    const form = document.createElement('form');
+    form.className = 'winnumber-form';
+
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'winnumber-title';
+    titleDiv.textContent = '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.';
+
+    const inputBoxDiv = document.createElement('div');
+    inputBoxDiv.className = 'input-box';
+    inputBoxDiv.append(this.createWinNumberInput(), this.createBonusNumber());
+
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'input-error error-message';
+
+    const submitInput = document.createElement('input');
+    submitInput.className = 'result-button button';
+    submitInput.type = 'submit';
+    submitInput.value = '결과를 확인하기';
+    form.append(titleDiv, inputBoxDiv, errorDiv, submitInput);
+    return form;
+  },
+
+  createWinNumberInput() {
+    const div = document.createElement('div');
+    const labelDiv = document.createElement('div');
+    labelDiv.textContent = '당첨 번호';
+    div.appendChild(labelDiv);
+
+    for (let i = 0; i < NUMBER.LOTTO_LENGTH; i += 1) {
+      const input = document.createElement('input');
+      input.className = 'winnumber-input one-number-input';
+      input.type = 'text';
+      div.appendChild(input);
+    }
+
+    return div;
+  },
+
+  createBonusNumber() {
+    const div = document.createElement('div');
+    const labelDiv = document.createElement('div');
+    labelDiv.textContent = '보너스 번호';
+    div.appendChild(labelDiv);
+
+    const input = document.createElement('input');
+    input.className = 'bonusnumber-input one-number-input';
+    input.type = 'text';
+    div.appendChild(input);
+
+    return div;
+  },
+};
 
 export default WinNumberSection;
