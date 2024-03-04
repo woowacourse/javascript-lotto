7a13b5675daa9ba23527bae922bb5fd95ac810f9
이태훈 <rhymint@gmail.com>
2024-03-04 16:40:15 +0900

    refactor: LottoResultTable의 table-col을 묶어 재사용이 가능하도록 refactoring

--- src/components/LottoResultTable.js
+++ src/components/LottoResultTable.js
@@ -1,6 +1,10 @@
+import { RateOfReturnCalculator, WinningRankCalculator } from '../domain';
 import './LottoResultTable.css';
 
-const LOTTO_RESULT_TABLE = (winningRankResult) => `
+const winningPrize = RateOfReturnCalculator.WINNING_PRIZE_DETAIL;
+const rankRule = WinningRankCalculator.RANK_RULE;
+
+const LOTTO_RESULT_TABLE = `
   <div class="divider"></div>
   <div class="table-col">
     <div class="count-row">
@@ -14,66 +18,21 @@ const LOTTO_RESULT_TABLE = (winningRankResult) => `
     </div>
   </div>
   <div class="divider"></div>
-  <div class="table-col">
-    <div class="count-row">
-      <p class="lotto-body">3개</p>
-    </div>
-    <div class="prize-row">
-      <p class="lotto-body">5,000</p>
-    </div>
-    <div class="match-row">
-      <p class="lotto-body">${winningRankResult['5th']}개</p>
-    </div>
+`;
+
+const LOTTO_RESULT_TABLE_COL = ({ rank, match }) => `
+<div class = "table-col">
+  <div class="count-row">
+    <p class="lotto-body">${rankRule[rank].match}개</p>
   </div>
-  <div class="divider"></div>
-  <div class="table-col">
-    <div class="count-row">
-      <p class="lotto-body">4개</p>
-    </div>
-    <div class="prize-row">
-      <p class="lotto-body">50,000</p>
-    </div>
-    <div class="match-row">
-      <p class="lotto-body">${winningRankResult['4th']}개</p>
-    </div>
+  <div class="prize-row">
+    <p class="lotto-body">${winningPrize[rank].toLocaleString('ko-KR')}원</p>
   </div>
-  <div class="divider"></div>
-  <div class="table-col">
-    <div class="count-row">
-      <p class="lotto-body">5개</p>
-    </div>
-    <div class="prize-row">
-      <p class="lotto-body">1,500,000</p>
-    </div>
-    <div class="match-row">
-      <p class="lotto-body">${winningRankResult['3rd']}개</p>
-    </div>
+  <div class="match-row">
+    <p class="lotto-body">${match}개</p>
   </div>
-  <div class="divider"></div>
-  <div class="table-col">
-    <div class="count-row">
-      <p class="lotto-body">5개+보너스볼</p>
-    </div>
-    <div class="prize-row">
-      <p class="lotto-body">30,000,000</p>
-    </div>
-    <div class="match-row">
-      <p class="lotto-body">${winningRankResult['2nd']}개</p>
-    </div>
-  </div>
-  <div class="divider"></div>
-  <div class="table-col">
-    <div class="count-row">
-      <p class="lotto-body">6개</p>
-    </div>
-    <div class="prize-row">
-      <p class="lotto-body">2,000,000,000</p>
-    </div>
-    <div class="match-row">
-      <p class="lotto-body">${winningRankResult['1st']}개</p>
-    </div>
-  </div>
-  <div class="divider"></div>
+</div>
+<div class="divider"></div>
 `;
 
 class LottoResultTable extends HTMLElement {
@@ -84,7 +43,11 @@ class LottoResultTable extends HTMLElement {
   render() {
     const app = document.querySelector('lotto-app');
     const { winningRankResult } = app.controller().getLottoGameInfo();
-    this.innerHTML = LOTTO_RESULT_TABLE(winningRankResult);
+    let contents = LOTTO_RESULT_TABLE;
+    for (const [rank, match] of Object.entries(winningRankResult).reverse()) {
+      contents += LOTTO_RESULT_TABLE_COL({ rank, match });
+    }
+    this.innerHTML = contents;
   }
 }
 
