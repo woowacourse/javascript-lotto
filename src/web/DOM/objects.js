812d9117ed8893370de44bace20d4f6e420f664d
chysis <chysiss@naver.com>
2024-03-04 17:36:05 +0900

    feat(objects): 프로그램에서 사용될 HTML 요소들을 파일로 분리

--- src/web/DOM/objects.js
+++ src/web/DOM/objects.js
@@ -1,6 +1,27 @@
-import { $ } from '../../utils/querySelector.js';
+import { $ } from '../../utils/querySelector';
 
 const purchaseAmountForm = $('#purchase-amount-form');
+const purchaseAmountInput = $('#purchase-amount-input');
+const purchaseButton = $('#purchase-amount-submit-button');
 const purchaseResult = $('#purchase-result');
+const purchaseLottoList = $('#purchased-lotto-list');
 
-export { purchaseResult, purchaseAmountForm };
+const winningNumbersForm = $('#winning-numbers-form');
+const winningNumbersButton = $('#winning-numbers-submit-button');
+const winningNumbersResult = $('#winning-numbers-result');
+
+const lottoResultModalSection = $('#lotto-result-modal-section');
+const lottoResultModal = $('#lotto-result-modal-overlay');
+
+export {
+  purchaseResult,
+  purchaseAmountForm,
+  purchaseAmountInput,
+  purchaseButton,
+  purchaseLottoList,
+  winningNumbersForm,
+  winningNumbersButton,
+  winningNumbersResult,
+  lottoResultModalSection,
+  lottoResultModal,
+};
