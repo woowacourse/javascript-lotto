5638873df5a452067715d31f0d9e1bd430823498
chysis <chysiss@naver.com>
2024-03-11 02:07:59 +0900

    refactor(objects): 중간에 재선언되는 요소들 제거

--- src/web/DOM/objects.js
+++ src/web/DOM/objects.js
@@ -1,27 +1,6 @@
 import { $ } from '../../utils/querySelector';
 
-const purchaseAmountForm = $('#purchase-amount-form');
-const purchaseAmountInput = $('#purchase-amount-input');
-const purchaseButton = $('#purchase-amount-submit-button');
 const purchaseResult = $('#purchase-result');
-const purchaseLottoList = $('#purchased-lotto-list');
+const purchasedLottoList = $('#purchased-lotto-list');
 
-const winningNumbersForm = $('#winning-numbers-form');
-const winningNumbersButton = $('#winning-numbers-submit-button');
-const winningNumbersResult = $('#winning-numbers-result');
-
-const lottoResultModalSection = $('#lotto-result-modal-section');
-const lottoResultModal = $('#lotto-result-modal-overlay');
-
-export {
-  purchaseResult,
-  purchaseAmountForm,
-  purchaseAmountInput,
-  purchaseButton,
-  purchaseLottoList,
-  winningNumbersForm,
-  winningNumbersButton,
-  winningNumbersResult,
-  lottoResultModalSection,
-  lottoResultModal,
-};
+export { purchaseResult, purchasedLottoList };
