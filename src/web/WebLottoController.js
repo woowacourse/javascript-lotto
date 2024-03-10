d8dd2e52973556ca1d79b1a5494249437ca1d056
chysis <chysiss@naver.com>
2024-03-11 02:12:55 +0900

    feat(WebLottoController): 당첨 결과를 모달창에 보여주는 기능 및 게임 재시작 로직 구현

--- src/web/WebLottoController.js
+++ src/web/WebLottoController.js
@@ -1,58 +1,67 @@
-import LotteryMachine from '../domain/services/LotteryMachine';
-import { purchaseResult, purchaseLottoList, winningNumbersForm, lottoResultModalSection } from './DOM/objects';
+import { purchaseResult, purchasedLottoList } from './DOM/objects';
 import { CONFIG_FORMAT, CONFIG_LOTTO } from '../constants/config';
+import LotteryMachine from '../domain/services/LotteryMachine';
 import WebInputView from './views/WebInputView';
 import WebOutputView from './views/WebOutputView';
 import lottoService from '../domain/services/lottoService';
+import { $ } from '../utils/querySelector.js';
 
 class WebLottoController {
   #purchaseAmount;
   #lottery;
 
-  constructor(purchaseAmount) {
-    this.#purchaseAmount = purchaseAmount;
+  constructor() {
+    this.showLottoResult = this.showLottoResult.bind(this);
   }
 
   run() {
-    this.#lottery = new LotteryMachine(this.#purchaseAmount).makeLottery();
-    this.showPurchasedResult(this.#purchaseAmount / CONFIG_LOTTO.PURCHASE_UNIT);
-    this.renderWinningNumbersForm();
-    winningNumbersForm.addEventListener('submit', this.winningNumbersHandler);
+    this.initApp();
+    this.getPurchaseAmount();
   }
 
-  showPurchasedResult(amount) {
-    const message = `총 ${amount}개를 구매하였습니다.`;
+  initApp() {
+    const purchaseAmountInput = $('#purchase-amount-input');
+    const winningNumbersForm = $('#winning-numbers-form');
+    const lottoResultModalSection = $('#lotto-result-modal-section');
+
+    purchaseAmountInput.value = '';
+    purchaseAmountInput.focus();
+    WebOutputView.reset(purchaseResult);
+    WebOutputView.reset(purchasedLottoList);
+    WebOutputView.reset(winningNumbersForm);
+    lottoResultModalSection.classList.add('hide');
+  }
+
+  getPurchaseAmount() {
+    const purchaseAmountForm = $('#purchase-amount-form');
+    purchaseAmountForm.addEventListener('submit', e => {
+      const purchaseAmount = WebInputView.readPurchaseAmount(e);
+      if (!purchaseAmount) return;
+      this.#purchaseAmount = purchaseAmount;
+      this.#lottery = new LotteryMachine(this.#purchaseAmount).makeLottery();
+      this.#showPurchasedResult(this.#purchaseAmount);
+    });
+  }
+
+  #showPurchasedResult(amount) {
+    const lottoCount = amount / CONFIG_LOTTO.PURCHASE_UNIT;
+    const message = `총 ${lottoCount}개를 구매하였습니다.`;
     WebOutputView.printMessage(purchaseResult, message);
+
     const lottoList = this.#lottery.map(lotto => {
       return `<li><span>🎟️</span>${lotto.numberList.join(CONFIG_FORMAT.JOIN_SEPARATOR)}</li>`;
     });
-    WebOutputView.printMessage(purchaseLottoList, lottoList.join(''));
+    WebOutputView.printMessage(purchasedLottoList, lottoList.join(''));
+    WebOutputView.renderWinningNumbersForm();
+    this.manageFormEvents();
   }
 
-  renderWinningNumbersForm() {
-    const winningNumbersFormHTML = `<label>지난주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.</label>
-    <div id="winning-numbers-input-field">
-      <div id="winning-numbers-container">
-        <span>당첨 번호</span>
-        <div id="winning-numbers-inputs">
-          <input type="number" class="winning-number-input" required />
-          <input type="number" class="winning-number-input" required />
-          <input type="number" class="winning-number-input" required />
-          <input type="number" class="winning-number-input" required />
-          <input type="number" class="winning-number-input" required />
-          <input type="number" class="winning-number-input" required />
-        </div>
-      </div>
-      <div id="bonus-number-container">
-        <label>보너스 번호</label>
-        <input type="number" id="bonus-number-input" required />
-      </div>
-    </div>
-    <button type="submit" id="winning-numbers-submit-button">결과 확인하기</button>`;
-    WebOutputView.printMessage(winningNumbersForm, winningNumbersFormHTML);
+  manageFormEvents() {
+    const winningNumbersForm = $('#winning-numbers-form');
+    winningNumbersForm.addEventListener('submit', this.showLottoResult);
   }
 
-  winningNumbersHandler(event) {
+  showLottoResult(event) {
     const winningNumbersAndBonus = WebInputView.readWinningAndBonusNumbers(event);
     if (winningNumbersAndBonus) {
       const { winningNumbers, bonusNumber } = winningNumbersAndBonus;
@@ -64,9 +73,24 @@ class WebLottoController {
   }
 
   openResultModal(rankList, profit) {
-    console.log(lottoResultModalSection);
-    // TODOLIST - 당첨 결과를 출력하는 모달창 띄우기
-    // lottoResultModalSection.classList.remove('hide');
+    WebOutputView.renderLottoResult(rankList, profit);
+    WebOutputView.openModal();
+    this.manageModalEvents();
+  }
+
+  manageModalEvents() {
+    const modalBackdrop = $('#lotto-result-modal-overlay');
+    const modalContainer = $('#lotto-result-modal-container');
+    const modalCloseButton = $('#modal-close-button');
+    const restartButton = $('#lotto-result-restart-button');
+
+    modalContainer.addEventListener('click', e => e.stopPropagation());
+    modalBackdrop.addEventListener('click', () => WebOutputView.closeModal());
+    modalCloseButton.addEventListener('click', () => WebOutputView.closeModal());
+    restartButton.addEventListener('click', () => {
+      WebOutputView.closeModal();
+      this.run();
+    });
   }
 }
 
