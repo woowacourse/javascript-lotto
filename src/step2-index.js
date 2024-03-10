e4cab58b7ed62775ee394e260a86338b8a72c1ec
chysis <chysiss@naver.com>
2024-03-11 02:14:21 +0900

    refactor: 구입 금액을 입력받는 로직을 controller로 이전

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,40 +1,10 @@
 import './web/styles/reset.css';
 import './web/styles/index.css';
 import WebLottoController from './web/WebLottoController';
-import {
-  lottoResultModalSection,
-  purchaseAmountForm,
-  purchaseButton,
-  purchaseLottoList,
-  purchaseResult,
-  winningNumbersButton,
-  winningNumbersForm,
-} from './web/DOM/objects';
-import WebInputView from './web/views/WebInputView';
-import WebOutputView from './web/views/WebOutputView';
 
 const app = {
   play() {
-    this.initApp();
-    this.initEventListener();
-  },
-
-  purchaseAmountHandler(event) {
-    const purchaseAmount = WebInputView.readPurchaseAmount(event);
-    if (purchaseAmount) {
-      new WebLottoController(purchaseAmount).run();
-    }
-  },
-
-  initApp() {
-    WebOutputView.reset(purchaseResult);
-    WebOutputView.reset(purchaseLottoList);
-    WebOutputView.reset(winningNumbersForm);
-    lottoResultModalSection.classList.add('hide');
-  },
-
-  initEventListener() {
-    purchaseAmountForm.addEventListener('submit', this.purchaseAmountHandler);
+    new WebLottoController().run();
   },
 };
 
