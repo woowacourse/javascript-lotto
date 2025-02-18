2a696397456a580192f3009f7eb80db466a95b29
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 17:22:12 +0900

    refactor(view): handleUserInput 매개변수 추가에 따른 로직 리팩토링
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/view/handleUserInput.js
+++ src/view/handleUserInput.js
@@ -1,9 +1,14 @@
-import { retryInput } from "../utils/retryInput.js";
-import { readLineAsync } from "./input.js";
+import { retryInput } from '../utils/retryInput.js';
+import { readLineAsync } from './input.js';
 
-export default async function handleUserInput(message, validateFn) {
+export default async function handleUserInput(message, validateFn, extraValue = null) {
   return await retryInput(async () => {
     const input = await readLineAsync(message);
+    if (extraValue) {
+      validateFn(input, extraValue);
+      return input;
+    }
+
     validateFn(input);
     return input;
   });
