d488d11c41b19c6095d6749db87d885df6e4838d
sooyeoniya <tndus1306403@naver.com>
2025-02-18 16:57:06 +0900

    chore: 당첨 번호 유효성 테스트 명세 수정
    - 각 테스트 명세에 맞게 문장 수정함
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- __tests__/validateWinningNumbers.test.js
+++ __tests__/validateWinningNumbers.test.js
@@ -4,43 +4,40 @@ import validateWinningNumbers from "../src/domain/validation/validateWinningNumb
 describe("당첨 번호 유효성 테스트", () => {
   test.each([
     {
-      description: "숫자가 6개가 아닌 경우",
+      description: "당첨 번호의 숫자가 6개가 아닌 경우",
       input: "1,2,3,4,5,6,7",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.INVALID_COUNT,
     },
     {
-      description: "숫자가 아닌 경우",
+      description: "당첨 번호 중 숫자가 아닌 값이 있는 경우",
       input: "a,b,c,d,e,f",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.NOT_A_NUMBER,
     },
     {
-      description: "정수가 아닌 경우",
+      description: "당첨 번호 중 정수가 아닌 숫자가 있는 경우",
       input: "1.1,2,3,4,5,6",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.NOT_AN_INTEGER,
     },
     {
-      description: "숫자 범위가 1 미만인 경우",
+      description: "당첨 번호에서 1 미만인 숫자가 있는 경우",
       input: "1,2,3,0,4,5",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.OUT_OF_RANGE,
     },
     {
-      description: "숫자 범위가 45 초과인 경우",
+      description: "당첨 번호에서 45 초과인 숫자가 있는 경우",
       input: "1,2,3,4,5,66",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.OUT_OF_RANGE,
     },
     {
-      description: "숫자가 중복되는 경우",
+      description: "당첨 번호의 숫자가 중복되는 경우",
       input: "1,2,3,4,4,5",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.DUPLICATE_NUMBER,
     },
-  ])(
-    "당첨 번호가 $description 에러가 발생한다.",
-    ({ input, expectedErrorMessage }) => {
-      // given
-      // when & then
-      expect(() => {
-        validateWinningNumbers(input);
-      }).toThrow(expectedErrorMessage);
-    },
-  );
+  ])("$description 에러가 발생한다.", ({ input, expectedErrorMessage }) => {
+    // given
+    // when & then
+    expect(() => {
+      validateWinningNumbers(input);
+    }).toThrow(expectedErrorMessage);
+  });
 });
