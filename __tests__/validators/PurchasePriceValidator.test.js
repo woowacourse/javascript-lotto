58d42a7909e6ec89da4a42e3bef8510e72da425f
suhwa <tnghk9611@naver.com>
2025-02-18 18:02:42 +0900

    test: 구입 금액 예외 처리 테스트 코드 작성
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- __tests__/validators/PurchasePriceValidator.test.js
+++ __tests__/validators/PurchasePriceValidator.test.js
@@ -1,19 +1,36 @@
-import { validateRange, validateType } from "../../src/validators/PurchasePriceValidator";
+import {
+  validateRange,
+  validateType,
+  validateUnit,
+} from "../../src/validators/PurchasePriceValidator";
 
 describe("구매 금액 검증 테스트", () => {
   describe("예외 케이스", () => {
-    test("구매 금액이 숫자가 아니면 에러가 발생한다.", () => {
-      const purchasePrice = "1000";
-      expect(() => validateType(purchasePrice)).toThrow(
-        "구매 금액은 숫자여야 합니다."
-      );
-    });
+    test.each(["^", NaN, undefined, {}])(
+      "구매 금액이 숫자가 아니면 에러가 발생한다. (입력 값 : %p)",
+      (purchasePrice) => {
+        expect(() => validateType(purchasePrice)).toThrow(
+          "구매 금액은 숫자여야 합니다."
+        );
+      }
+    );
 
-    test("구매 금액이 1000원 이상이 아니면 에러가 발생한다.", () => {
-      const purchasePrice = 999;
-      expect(() => validateRange(purchasePrice)).toThrow(
-        "구매 금액은 1,000원 이상이어야 합니다."
-      );
-    });
+    test.each([999, 1000001, -Infinity, Infinity])(
+      "구매 금액이 1,000원 이상 1,000,000 이하가 아니면 에러가 발생한다. (입력 값 : %p)",
+      (purchasePrice) => {
+        expect(() => validateRange(purchasePrice)).toThrow(
+          "구매 금액은 1,000원 이상 1,000,000원 이하여야 합니다."
+        );
+      }
+    );
+
+    test.each([1001, 1999, 3023, 999999])(
+      "구매 금액이 1,000원 단위로 나누어 떨어지지 않으면 에러가 발생한다. (입력 값 : %p)",
+      (purchasePrice) => {
+        expect(() => validateUnit(purchasePrice)).toThrow(
+          "구매 금액은 1,000원 단위로 입력해야 합니다."
+        );
+      }
+    );
   });
 });
