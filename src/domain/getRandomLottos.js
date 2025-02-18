2b2b3b566e248e912b97ccba4e67a91b0109cfad
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 18:11:08 +0900

    feat(lotto): quantity에 따른 random 로또 발행 함수 구현

---
+++ src/domain/getRandomLottos.js
@@ -0,0 +1,26 @@
+import { getRandomNumber } from '../utils/getRandomNumber.js';
+
+export const getRandomLottos = (quantity) => {
+  const lottos = [];
+  Array.from({ length: quantity }, () => {
+    const lotto = retryIfDuplicatedRandomNumbers();
+
+    lottos.push(lotto);
+  });
+
+  return lottos;
+};
+
+export const retryIfDuplicatedRandomNumbers = () => {
+  const setRandomNumbers = new Set();
+
+  while (1) {
+    if (setRandomNumbers.size === 6) {
+      break;
+    }
+    setRandomNumbers.clear();
+    Array.from({ length: 6 }, () => setRandomNumbers.add(getRandomNumber()));
+  }
+
+  return [...setRandomNumbers];
+};
