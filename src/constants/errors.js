d0b3499cda7890a7b4550ddeba3d4533cecaccf3
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 15:40:37 +0900

    feat(constants): 구입 금액 양의 정수가 아닐 떄 에러 정의
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,4 +1,5 @@
 export const ERROR = Object.freeze({
   IS_EMPTY: '구입금액은 빈 값이 아니여야 해요.',
-  IS_LOWER_THAN_MINIMUM_OF_PUCHASE_PRICE: '구입 금액은 1000원 이상인 양의 정수이어야 해요.',
+  IS_NOT_POSITIVE_INTEGER: '구입 금액은 양의 정수여야 해요',
+  IS_LOWER_THAN_MINIMUM_OF_PUCHASE_PRICE: '구입 금액은 1000원 이상이어야 해요.',
 });
