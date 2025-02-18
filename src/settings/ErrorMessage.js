6a631c84b23e2d6c555f93a021377ed66eb392f9
spoyodevelop <kjy9505@gmail.com>
2025-02-18 15:28:55 +0900

    refactor(ErrorMessage,isNumber): 에러 메시지 분리
    Co-authored-by: Db0111 <deokbin@naver.com>

---
+++ src/settings/ErrorMessage.js
@@ -0,0 +1,5 @@
+const ERROR_MESSAGE = Object.freeze({
+  notANumber: '[ERROR] 문자는 입력할 수 없습니다. 다시 입력해주세요.',
+});
+
+export default ERROR_MESSAGE;
--- src/util/isNumber.js
+++ src/util/isNumber.js
@@ -1,6 +1,8 @@
+import ERROR_MESSAGE from '../settings/ErrorMessage.js';
+
 export default function isNumber(input) {
   if (Number.isNaN(Number(input))) {
-    throw new Error('Invalid input: must be a valid number');
+    throw new Error(ERROR_MESSAGE.notANumber);
   }
   return Number(input);
 }
