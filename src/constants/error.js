06b26bb79811e053c6efebf713ee03988dd1cc0f
hwanheejung <hwanheejung117@gmail.com>
2025-02-18 14:48:16 +0900

    refactor: 에러메시지 상수로 분리

---
+++ src/constants/error.js
@@ -0,0 +1,3 @@
+export const ERROR_MESSAGE = {
+  INVALID_NUMBER: "숫자를 입력해주세요.",
+};
--- src/domain/validatePrice.js
+++ src/domain/validatePrice.js
@@ -1,7 +1,9 @@
+import { ERROR_MESSAGE } from "../constants/error.js";
+
 export const isNumber = (input) => {
   const regex = /^[0-9]*$/;
 
   if (!regex.test(input)) {
-    throw new Error("숫자를 입력해주세요.");
+    throw new Error(ERROR_MESSAGE.INVALID_NUMBER);
   }
 };
