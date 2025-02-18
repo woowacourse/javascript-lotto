8cbcc6b6386ecd3d5e180e009b4a520b9bce7222
hanheel <hanhee630630@gmail.com>
2025-02-18 15:05:32 +0900

    feat : 로또 객체 길이가 유효한지 검사

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,12 +1,23 @@
+import { ERROR_MESSAGE } from "../src/constants/errorMessage.js";
 import Lotto from "../src/domain/Lotto.js";
 
-test("로또 번호는 숫자 배열이 입력되면, 로또 숫자를 필드로 갖는다", () => {
-  // given
-  const numbers = [1, 2, 3, 4, 5, 6];
+describe("로또 객체 생성 테스트", () => {
+  test("로또 번호는 숫자 배열이 입력되면, 로또 숫자를 필드로 갖는다", () => {
+    // given
+    const numbers = [1, 2, 3, 4, 5, 6];
 
-  // when
-  const lotto = new Lotto(numbers);
+    // when
+    const lotto = new Lotto(numbers);
 
-  // then
-  expect(lotto.numbers).toEqual(numbers);
+    // then
+    expect(lotto.numbers).toEqual(numbers);
+  });
+
+  test("로또 번호가 6개가 아닐 경우, 에러를 출력한다", () => {
+    //given
+    const numbers = [1, 2, 3, 4, 5];
+
+    //then
+    expect(() => new Lotto(numbers)).toThrow(ERROR_MESSAGE.LOTTO_NUMBERS_LENGTH);
+  });
 });
--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,12 +1,22 @@
+import { ERROR_MESSAGE } from "../constants/errorMessage";
+import { SYSTEM_CONSTANTS } from "../constants/systemConstants";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    if (!this.isLengthValid(numbers)) {
+      throw new Error(ERROR_MESSAGE.LOTTO_NUMBERS_LENGTH);
+    }
     this.#numbers = numbers;
   }
 
   get numbers() {
     return this.#numbers;
+  }
+
+  isLengthValid(numbers) {
+    return numbers.length === SYSTEM_CONSTANTS.LOTTO_NUMBERS_LENGTH;
   }
 }
 
