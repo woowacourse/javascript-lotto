2b1352c7f56fd3161564d95618784578c442bc2a
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 16:18:56 +0900

    feat(constants): 당첨 번호 에러 메시지 정의
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,5 +1,9 @@
 export const ERROR = Object.freeze({
-  IS_EMPTY: '구입금액은 빈 값이 아니여야 해요.',
-  IS_NOT_POSITIVE_INTEGER: '구입 금액은 양의 정수여야 해요',
+  IS_VALUE_EMPTY: '입력 값은 빈 값이 아니여야 해요.',
+  IS_NOT_POSITIVE_INTEGER: '입력 값은 양의 정수여야 해요',
   IS_LOWER_THAN_MINIMUM_OF_PUCHASE_PRICE: '구입 금액은 1000원 이상이어야 해요.',
+  IS_NOT_RANGE_OF_WINNING_NUMBER: '당첨 번호는 1~45 사이여야 해요.',
+  IS_NOT_SAME_LENGTH_OF_WINNING_NUMBER: '당첨 번호는 6개여야 해요.',
+  IS_DUPLICATED_WINNING_NUMBER: '당첨 번호는 중복될 수 없어요.',
+  IS_BONUS_NUMBER_DUPLICATED: '보너스 번호는 당첨 번호와 중복될 수 없어요.',
 });
