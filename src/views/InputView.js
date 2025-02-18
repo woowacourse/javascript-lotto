420635ed0eb26d91a9eb7f00edefc3733d22ce1d
suhwa <tnghk9611@naver.com>
2025-02-18 17:22:04 +0900

    feat: 입력 메서드 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

---
+++ src/views/InputView.js
@@ -0,0 +1,17 @@
+import readLineAsync from "../utils/readLineAsync.js";
+
+const InputView = {
+  enterPurchasePrice: async function () {
+    return await readLineAsync("> 구입금액을 입력해 주세요. ");
+  },
+
+  enterWinningNumbers: async function () {
+    return await readLineAsync("> 당첨 번호를 입력해 주세요. ");
+  },
+
+  enterBonusNumber: async function () {
+    return await readLineAsync("> 보너스 번호를 입력해 주세요.");
+  },
+};
+
+export default InputView;
