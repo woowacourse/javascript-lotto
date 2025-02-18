12fb3cf4816cb72178b316337ee7ae4568ff81b7
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 17:24:47 +0900

    feat(index): 구매금액 및 당첨 번호 입력 로직 적용

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,9 +1,15 @@
 import { INPUT } from './constants/messages.js';
-import { validatePurchasePrice } from './utils/validaition.js';
+import { parseWinningNumbers } from './utils/parseString.js';
+import { validateBonusNumber, validatePurchasePrice, validateWinningNumbers } from './utils/validaition.js';
 import handleUserInput from './view/handleUserInput.js';
 
 async function run() {
   const purchasePrice = await handleUserInput(INPUT.PURCHASE_PRICE, validatePurchasePrice);
+
+  const stringOfWinningNumbers = await handleUserInput(INPUT.WINNING_NUMBERS, validateWinningNumbers);
+  const winningNumbers = parseWinningNumbers(stringOfWinningNumbers);
+
+  const bonusNumber = await handleUserInput(INPUT.BONUS_NUMBER, validateBonusNumber, winningNumbers);
 }
 
 run();
