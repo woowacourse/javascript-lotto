8ef873083654917d15018698e0af5bf88c1c5653
hwanheejung <hwanheejung117@gmail.com>
2025-02-18 15:01:42 +0900

    feat: 입력받은 구입 금액이 1000원 이상 10만원 이하인지 확인

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,4 +1,5 @@
 export const ERROR_MESSAGE = {
   INVALID_NUMBER: "숫자를 입력해주세요.",
   INVALID_THOUSAND_UNIT: "1000원 단위로 입력해주세요.",
+  INVALID_PRICE_RANGE: "1000원 이상 10만원 이하로 입력해주세요.",
 };
--- src/domain/validatePrice.js
+++ src/domain/validatePrice.js
@@ -1,5 +1,8 @@
 import { ERROR_MESSAGE } from "../constants/error.js";
 
+const MIN_PRICE = 1_000;
+const MAX_PRICE = 1_000_000;
+
 export const isNumber = (input) => {
   const regex = /^[0-9]*$/;
 
@@ -11,5 +14,11 @@ export const isNumber = (input) => {
 export const isThousandUnit = (input) => {
   if (input % 1000 !== 0) {
     throw new Error(ERROR_MESSAGE.INVALID_THOUSAND_UNIT);
+  }
+};
+
+export const isValidPriceRange = (input) => {
+  if (input < MIN_PRICE || input > MAX_PRICE) {
+    throw new Error(ERROR_MESSAGE.INVALID_PRICE_RANGE);
   }
 };
