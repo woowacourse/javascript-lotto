c5af175092c491457f14fde0489c22b362a18395
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 16:19:55 +0900

    test: 당첨 번호 예외 케이스 및 통과 테스트코드 구현
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,9 +1,9 @@
 import { ERROR } from '../src/constants/errors';
 import { hasEmptyString, isValueInteger } from '../src/utils/validaition';
 
-describe('유효성 검사 테스트', () => {
+describe('구입 금액 유효성 검사 테스트', () => {
   test('구입 금액은 빈 값일 수 없다.', () => {
-    expect(() => hasEmptyString('').toThrow(ERROR.IS_PURCHASE_PRICE_EMPTY));
+    expect(() => hasEmptyString('').toThrow(ERROR.IS_VALUE_EMPTY));
   });
   test.each(['a', 2.1])('구입 금액은 문자와 실수가 아니여야 한다.', (value) => {
     expect(() => isValueInteger(value).toThrow(ERROR.IS_NOT_POSITIVE_INTEGER));
@@ -19,5 +19,33 @@ describe('유효성 검사 테스트', () => {
 
   test('구입 금액은 1000원 이상이여야 한다.', () => {
     expect(() => validatePurchasePrice(1000).not.toThrow());
+  });
+});
+
+describe('당첨 번호 유효성 검사 테스트', () => {
+  test('당첨 번호는 빈 값일 수 없다.', () => {
+    expect(() => hasEmptyString('').toThrow(ERROR.IS_VALUE_EMPTY));
+  });
+  test.each(['a', 2.1, -1])('당첨 번호는 문자와 실수가 아니여야 한다.', (value) => {
+    expect(() => isValueInteger(value).toThrow(ERROR.IS_NOT_POSITIVE_INTEGER));
+  });
+
+  test.each([1, 4, 3])('당첨 번호는 양의 정수여야 한다.', (value) => {
+    expect(() => isValueInteger(value).not.toThrow(ERROR.IS_NOT_POSITIVE_INTEGER));
+  });
+
+  test('당첨 번호는 중복될 수 없다.', () => {
+    const testWinningNumbers = [1, 2, 3, 4, 5, 5];
+    expect(() => validatePurchasePrice(testWinningNumbers)).toThrow();
+  });
+
+  test('당첨 번호는 6개여야 한다.', () => {
+    const testWinningNumbers = [1, 2, 3, 4, 5];
+    expect(() => validatePurchasePrice(testWinningNumbers)).toThrow();
+  });
+
+  test('당첨 번호는 1이상 45이하의 정수이다.', () => {
+    const testWinningNumbers = [1, 2, 3, 4, 5, 99];
+    expect(() => validateArrayOfWinningNumbers(testWinningNumbers)).toThrow();
   });
 });
