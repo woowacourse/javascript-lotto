b44085664ccb5f8bc4c739a1990da3c22564c5cf
wo-o29 <woogur29@gmail.com>
2025-02-18 14:48:20 +0900

    ​feat: 도메인에 필요한 상수 설정
    Co-authored-by: JeLee-river <106021313+JeLee-river@users.noreply.github.com>

---
+++ src/constants/lotto.js
@@ -0,0 +1,29 @@
+export const LOTTO_RULE = Object.freeze({
+  MULTIPLE_PRICE: 1000,
+  MAX_PRICE: 100_000,
+  MIN_LOTTO_NUMBER: 1,
+  MAX_LOTTO_NUMBER: 45,
+  LOTTO_LENGTH: 6,
+});
+
+export const LOTTO_PRIZE_MONEY = new FrozenMap([
+  [3, 5000],
+  [4, 50_000],
+  [5, 1_500_000],
+  ["5B", 30_000_000],
+  [6, 2_000_000_000],
+]);
+
+export const LOTTO_RESULT_MESSAGES_MAP = Array.from(LOTTO_PRIZE_MONEY).reduce(
+  (message, [key, value]) => {
+    const prizeMoney = value.toLocaleString();
+    if (key === "5B") {
+      message.set(key, `5개 일치, 보너스 볼 일치 (${prizeMoney}원) - `);
+      return message;
+    }
+
+    message.set(key, `${key}개 일치 (${prizeMoney}원) - `);
+    return message;
+  },
+  new FrozenMap()
+);
