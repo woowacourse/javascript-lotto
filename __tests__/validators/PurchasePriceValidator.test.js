a2aac0322e956d1bc0fa44bf536db0721e2ec994
suhwa <tnghk9611@naver.com>
2025-02-18 17:48:37 +0900

    test: 구입 금액 범위 테스트 코드 작성
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- __tests__/validators/PurchasePriceValidator.test.js
+++ __tests__/validators/PurchasePriceValidator.test.js
@@ -1,4 +1,4 @@
-import { validateType } from "../../src/validators/PurchasePriceValidator";
+import { validateRange, validateType } from "../../src/validators/PurchasePriceValidator";
 
 describe("구매 금액 검증 테스트", () => {
   describe("예외 케이스", () => {
@@ -6,6 +6,13 @@ describe("구매 금액 검증 테스트", () => {
       const purchasePrice = "1000";
       expect(() => validateType(purchasePrice)).toThrow(
         "구매 금액은 숫자여야 합니다."
+      );
+    });
+
+    test("구매 금액이 1000원 이상이 아니면 에러가 발생한다.", () => {
+      const purchasePrice = 999;
+      expect(() => validateRange(purchasePrice)).toThrow(
+        "구매 금액은 1,000원 이상이어야 합니다."
       );
     });
   });
