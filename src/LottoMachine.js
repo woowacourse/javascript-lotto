499cbf0d40a1eaae9a6e5b8aaf8505c048fea8dd
suhwa <tnghk9611@naver.com>
2025-02-18 16:19:10 +0900

    feat: 로또 번호 생성 기능 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

---
+++ src/LottoMachine.js
@@ -0,0 +1,24 @@
+import generateRandomNumber from "./utils/generateRandomNumber.js";
+import Lotto from "./Lotto.js";
+
+class LottoMachine {
+  #lottos = [];
+
+  constructor(lottoCount) {
+    this.#lottos = this.#generateLottos(lottoCount);
+  }
+
+  #generateLottos(lottoCount) {
+    const lotto = Array.from({ length: 6 }, () =>
+      generateRandomNumber(1, 45)
+    ).sort((a, b) => a - b);
+
+    return Array.from({ length: lottoCount }, () => new Lotto(lotto));
+  }
+
+  get lottos() {
+    return this.#lottos;
+  }
+}
+
+export default LottoMachine;
