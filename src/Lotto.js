2e398a072ceb620a7539bb05dd813421d8f35657
suhwa <tnghk9611@naver.com>
2025-02-18 15:55:01 +0900

    feat: 로또 번호 타입 예외 처리 기능 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,11 +2,18 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.#validate(numbers);
     this.#numbers = numbers;
   }
 
   get numbers() {
     return this.#numbers;
+  }
+
+  #validate(numbers) {
+    if (!numbers.every((number) => typeof number === "number")) {
+      throw new Error("로또 번호는 숫자여야 합니다.");
+    }
   }
 }
 
