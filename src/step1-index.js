157c309235191b2d81c4cc9edf8cd878ba32783e
bunju20 <ghkd2271@naver.com>
2025-02-18 18:45:08 +0900

    feat: OutputView 출력 로직 구현

--- src/step1-index.js
+++ src/step1-index.js
@@ -4,12 +4,17 @@
  */
 
 import InputView from "./view/InputView.js";
+import OutputView from "./view/OutputView.js";
 
 export async function run() {
   const buyMoney = await InputView.getInput("> 구입금액을 입력해 주세요.");
   const targetNumber = await InputView.getInput("> 당첨 번호를 입력해 주세요.");
   const bonusNumber =
     await InputView.getInput("> 보너스 번호를 입력해 주세요.");
+
+  OutputView.printMessage("당첨 통계");
+  OutputView.printMessage("--------------------");
+  OutputView.printWinning({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 }, 62.5);
   const retryAnswer = await InputView.getInput(
     "> 다시 시작하시겠습니까? (y/n) ",
   );
