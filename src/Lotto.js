4f4e01a2684298e00fc382a45cc4f26a4d6daef5
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-18 16:06:08 +0900

    feat: 로또의 번호의 숫자 범위는 1 미만 혹은 45 초과하면 에러가 발생
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,11 +2,23 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.#validate(numbers);
     this.#numbers = numbers;
   }
 
   getNumbers() {
     return this.#numbers;
+  }
+
+  #validate(numbers) {
+    const MIN_LOTTO_NUMBER = 1;
+    const MAX_LOTTO_NUMBER = 45;
+    const isValidLottoNumber = (
+      number,
+    ) => number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER;
+    if (!numbers.every(isValidLottoNumber)) {
+      throw new Error('[ERROR] 로또 번호의 숫자 범위 1 ~ 45이다.');
+    }
   }
 }
 
