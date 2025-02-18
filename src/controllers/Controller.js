a4e9df4ee7ac6b036df3dc6eb39f88755d7ca9c7
sooyeoniya <tndus1306403@naver.com>
2025-02-18 17:41:52 +0900

    chore: 안쓰는 import 삭제

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -2,7 +2,6 @@ import Input from "../views/Input.js";
 import validatePurchaseAmount from "../validations/validatePurchaseAmount.js";
 import validateWinningNumbers from "../validations/validateWinningNumbers.js";
 import throwIfInvalid from "../utils/throwIfInvalid.js";
-import Output from "../views/Output.js";
 import validateBonusNumber from "../validations/validateBonusNumber.js";
 
 class Controller {
