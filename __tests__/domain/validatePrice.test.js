fa8ea4d2732b2baa2504ae5abee6eb38b3a2a247
hwanheejung <hwanheejung117@gmail.com>
2025-02-18 14:49:01 +0900

    test: 구입 금액 비정상 입력 시 테스트케이스 추가

--- __tests__/domain/validatePrice.test.js
+++ __tests__/domain/validatePrice.test.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from "../../src/constants/error.js";
 import { isNumber } from "../../src/domain/validatePrice.js";
 
 describe("domain/validatePrice", () => {
@@ -5,6 +6,13 @@ describe("domain/validatePrice", () => {
     "입력된 값이 양의 정수여야 한다",
     (input) => {
       expect(() => isNumber(input)).not.toThrow();
+    }
+  );
+
+  test.each([["-1"], ["0.5"], ["1000j"], ["asdf"]])(
+    "입력된 값이 양의 정수가 아니면 에러를 출력한다",
+    (input) => {
+      expect(() => isNumber(input)).toThrow(ERROR_MESSAGE.INVALID_NUMBER);
     }
   );
 });
