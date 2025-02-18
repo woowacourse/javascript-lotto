4272cb9cae0e7157eab3bbb4343949ebd098cdd8
spoyodevelop <kjy9505@gmail.com>
2025-02-18 15:57:12 +0900

    feat(isPositive): 양수를 판단하는 validator 작성
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/settings/ErrorMessage.js
+++ src/settings/ErrorMessage.js
@@ -1,5 +1,8 @@
 const ERROR_MESSAGE = Object.freeze({
   notANumber: '[ERROR] 문자는 입력할 수 없습니다. 다시 입력해주세요.',
+  notInteger: '[ERROR] 정수가 아닌 수는 입력할 수 없습니다. 다시 입력해주세요.',
+  notPositive:
+    '[ERROR] 양수가 아닌 수는 입력할 수 없습니다. 다시 입력해주세요.',
 });
 
 export default ERROR_MESSAGE;
