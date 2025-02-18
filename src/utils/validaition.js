cf57a2c2a10a33047b55a437f6673c9a7eac88be
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 15:41:23 +0900

    feat(validation): 구입 금액 유효성 검사 로직 분리
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/utils/validaition.js
+++ src/utils/validaition.js
@@ -1,12 +1,24 @@
 import { ERROR } from '../constants/errors.js';
 
-export const validatePurchasePrice = (input) => {
-  const value = Number(input);
-  if (value === '') {
+export const hasEmptyString = (input) => {
+  if (input === '') {
     throw new Error(ERROR.IS_PURCHASE_PRICE_EMPTY);
   }
+};
 
-  if (!Number.isInteger(value) || value < 1000) {
+export const isValueInteger = (input) => {
+  if (!Number.isInteger(input)) {
+    throw new Error(ERROR.IS_NOT_POSITIVE_INTEGER);
+  }
+};
+
+export const validatePurchasePrice = (input) => {
+  const value = Number(input);
+
+  hasEmptyString(input);
+  isValueInteger(value);
+
+  if (value < 1000) {
     throw new Error(ERROR.IS_LOWER_THAN_MINIMUM_OF_PUCHASE_PRICE);
   }
 };
