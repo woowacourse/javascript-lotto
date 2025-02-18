21963c6eb92f8bffad28d55f7d1bed117636f98e
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-18 16:19:19 +0900

    feat(validation): 당첨 번호 유효성 검사 구현
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/utils/validaition.js
+++ src/utils/validaition.js
@@ -22,3 +22,30 @@ export const validatePurchasePrice = (input) => {
     throw new Error(ERROR.IS_LOWER_THAN_MINIMUM_OF_PUCHASE_PRICE);
   }
 };
+
+const validateArrayOfWinningNumbers = (winningNumbers) => {
+  winningNumbers.forEach((value) => {
+    const winningNumber = Number(value);
+    hasEmptyString(value);
+    isValueInteger(winningNumber);
+
+    if (winningNumber < 1 || winningNumber > 45) {
+      throw new Error(ERROR.IS_NOT_RANGE_OF_WINNING_NUMBER);
+    }
+  });
+};
+
+export const validateWinningNumbers = (input) => {
+  const winningNumbers = input.split(',');
+  const winningNumberSet = new Set(winningNumbers);
+
+  if (winningNumberSet.size !== 6) {
+    throw new Error(ERROR.IS_DUPLICATED_WINNING_NUMBER);
+  }
+
+  if (winningNumbers.length !== 6) {
+    throw new Error(ERROR.IS_NOT_SAME_LENGTH_OF_WINNING_NUMBER);
+  }
+
+  validateArrayOfWinningNumbers(winningNumbers);
+};
