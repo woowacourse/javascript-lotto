c4f07df7596ca03511db703a4ffeb8b0cd3c6ff5
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-19 18:15:54 +0900

    test: 사용자가 구매한 로또 번호와 당첨된 번호가 5개 일치하고 보너스 번호도 일치할 경우, 해당 복권의 숫자를 +1 증가
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- __tests__/LottoStatistics.test.js
+++ __tests__/LottoStatistics.test.js
@@ -1,34 +1,43 @@
 import LottoStatistics from '../src/LottoStatistics';
 
+let lottoStatistics;
+beforeEach(() => {
+  lottoStatistics = new LottoStatistics();
+});
+
 test('사용자가 구매한 로또 번호와 당첨 번호의 일치 개수를 반환한다', () => {
   const machineLotto = [1, 2, 3, 4, 5, 6];
   const winningLotto = [1, 2, 3, 33, 34, 45];
 
-  const lottoStatistics = new LottoStatistics();
   expect(lottoStatistics.matchSameCount(machineLotto, winningLotto)).toBe(3);
+});
+
+test('사용자가 구매한 로또 번호와 당첨된 번호가 5개 일치하고 보너스 번호도 일치할 경우, 해당 복권의 숫자를 +1 증가시킨다.', () => {
+  const sameCount = 5;
+  const isBonusNumber = true;  
+  lottoStatistics.determineRank(sameCount, isBonusNumber);
+
+  expect(lottoStatistics.getRankResult()[`${sameCount}개 일치, 보너스 볼 일치`].count).toBe(1);
 });
 
 test.each([
   [3], [4], [6], [5],
 ])('사용자가 구매한 로또 번호와 당첨된 번호가 %s개 일치하면 해당 복권의 숫자를 +1 증가시킨다.', (sameCount) => {
-  const lottoStatistics = new LottoStatistics();
-  lottoStatistics.increaseCount(sameCount);
+  lottoStatistics.determineRank(sameCount,false);
 
   expect(lottoStatistics.getRankResult()[`${sameCount}개 일치`].count).toBe(1);
 });
 
 describe('사용자가 구매한 로또 번호와 보너스 번호를 비교한 후 참/거짓 값을 반환한다.', () => {
   const machineLotto = [1, 2, 3, 4, 5, 6];
-  let bonus;
-  const lottoStatistics = new LottoStatistics();
 
   test('참을 값을 반환한다.', () => {
-    bonus = 6;
+    const bonus = 6;
     expect(lottoStatistics.hasBonusNumber(machineLotto, bonus)).toBeTruthy();
   });
 
   test('거짓을 값을 반환한다.', () => {
-    bonus = 7;
+    const bonus = 7;
     expect(lottoStatistics.hasBonusNumber(machineLotto, bonus)).toBe(false);
   });
 });
