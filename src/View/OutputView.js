3d9ab48461e650b26c7dd8eca482fee209ba70be
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:09:42 +0900

    feat(OutputView): 생성된 로또 번호 출력하는 함수 추가
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -9,3 +9,7 @@ export function printError(errorMessage) {
 export function printPurchasedAmount(amount) {
   console.log(`${amount}개를 구매했습니다.`);
 }
+
+export function printLotto(lotto) {
+  console.log(`[${lotto.numbers.join(', ')}]`);
+}
