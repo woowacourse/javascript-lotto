31038f345d4d2274786096f6525f164787a560e1
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-19 19:30:19 +0900

    feat(constants): 로또 결과 출력 상수들 정의
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,10 +1,22 @@
 export const INPUT = Object.freeze({
-  PURCHASE_PRICE: '구입금액을 입력해 주세요.',
-  WINNING_NUMBERS: '당첨 번호를 입력해 주세요. ',
-  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요. ',
+  PURCHASE_PRICE: '> 구입금액을 입력해 주세요.',
+  WINNING_NUMBERS: '> 당첨 번호를 입력해 주세요. ',
+  BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요. ',
+});
+
+export const OUTPUT = Object.freeze({
+  PURCHASED_QUANTITY: (quantity) => `${quantity}개를 구매했습니다.`,
+  STATISTICS_HEADER: '\n당첨 통계\n--------------------',
+  TRHEE_MATCH: (count) => `3개 일치 (5,000원) - ${count}개`,
+  FOUR_MATCH: (count) => `4개 일치 (50,000원) - ${count}개`,
+  FIVE_MATCH: (count) => `5개 일치 (5,000원) - ${count}개`,
+  FIVE_WITH_BONUS_MATCH: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  SIX_MATCH: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+  TOTAL_REVENUE: (revenue) => `총 수익률은 ${revenue}%입니다`,
 });
 
 export const LOTTO = Object.freeze({
+  FIVE_WITH_BONUS_MATCH_IDX: 7,
   SIX_MATCH: 6,
   FIVE_WITH_BONUS_MATCH: 5.5,
   FIVE_MATCH: 5,
