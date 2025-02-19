8e08c694146bffb5f43fc977a1719e7bc0d654c1
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:57:09 +0900

    feat(Lotto): lotto validation 보강
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -1,5 +1,8 @@
 import ERROR_MESSAGE from '../settings/ErrorMessage.js';
 import checkNumberInRange from '../util/checkNumberInRange.js';
+import isInteger from '../util/isInteger.js';
+import isNumber from '../util/isNumber.js';
+import isPositive from '../util/isPositive.js';
 class Lotto {
   #numbers;
 
@@ -8,6 +11,9 @@ class Lotto {
     this.#numbers = numbers;
   }
   #lottoValidation(numbers) {
+    numbers.forEach((number) => isNumber(number));
+    numbers.forEach((number) => isInteger(number));
+    numbers.forEach((number) => isPositive(number));
     if (numbers.length !== 6) throw new Error(ERROR_MESSAGE.notSixNumbers);
     if (numbers.length !== new Set(numbers).size)
       throw new Error(ERROR_MESSAGE.duplicatedNumbers);
