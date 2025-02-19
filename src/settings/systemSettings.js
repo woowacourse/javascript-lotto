abf1b506b77e2d593f3c50191d11640e87e7c894
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:36:42 +0900

    chores(settings) 세팅 메세지 추가
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/settings/systemSettings.js
+++ src/settings/systemSettings.js
@@ -1,5 +1,7 @@
 const systemSettings = {
   getPurchasePrice: '> 구입금액을 입력해 주세요.',
+  getWinningNumber: '> 당첨 번호를 입력해 주세요.',
+  getBonusNumber: '> 보너스 번호를 입력해 주세요.',
 };
 
 export default systemSettings;
