205130a6868f6a221f565858d8f4a0bc0c836c03
suhwa <tnghk9611@naver.com>
2025-02-19 16:18:24 +0900

    refactor: App.run 메서드 분리
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- src/App.js
+++ src/App.js
@@ -8,34 +8,44 @@ import OutputView from "./views/OutputView.js";
 
 class App {
   async run() {
-    const lottoPurchasePrice = await InputView.enterPurchasePrice();
-    PurchasePriceValidator.validate(Number(lottoPurchasePrice));
-    const lottoCount = lottoPurchasePrice / 1000;
-
+    const [lottoPurchasePrice, lottoCount] = await this.#getPurchasePrice();
     const lottoMachine = new LottoMachine(lottoCount);
     OutputView.printPurchaseLottos(lottoMachine.lottos);
 
+    const winningNumbers = await this.#getWinningNumbers();
+    const bonusNumber = await this.#getBonusNumber();
+    const winningResult = new WinningResult(winningNumbers, bonusNumber);
+
+    const winningCounts = winningResult.calculate(lottoMachine.lottos);
+    const profitRate = winningResult.calculateProfitRate(
+      lottoPurchasePrice,
+      winningCounts
+    );
+    OutputView.printResult(winningCounts, profitRate);
+  }
+
+  async #getPurchasePrice() {
+    const lottoPurchasePrice = await InputView.enterPurchasePrice();
+    PurchasePriceValidator.validate(Number(lottoPurchasePrice));
+    const lottoCount = lottoPurchasePrice / 1000;
+    return [lottoPurchasePrice, lottoCount];
+  }
+
+  async #getWinningNumbers() {
     const winningNumbers = await InputView.enterWinningNumbers();
     const splittedWinningNumbers = winningNumbers.split(",").map(Number);
     WinningNumbersValidator.validate(splittedWinningNumbers);
+    return splittedWinningNumbers;
+  }
 
+  async #getBonusNumber() {
     const bonusNumber = await InputView.enterBonusNumber();
     const transformedBonusNumber = Number(bonusNumber);
     BonusNumberValidator.validate(
       transformedBonusNumber,
       splittedWinningNumbers
     );
-
-    const winningResult = new WinningResult(
-      splittedWinningNumbers,
-      transformedBonusNumber
-    );
-    const counts = winningResult.calculate(lottoMachine.lottos);
-    const profitRate = winningResult.calculateProfitRate(
-      lottoPurchasePrice,
-      counts
-    );
-    OutputView.printResult(counts, profitRate);
+    return transformedBonusNumber;
   }
 }
 
