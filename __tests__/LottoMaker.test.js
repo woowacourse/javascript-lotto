6585e91a71cf4b16ca92b882e61c549256382a45
sanghee01 <sang6299@naver.com>
2025-02-19 15:53:56 +0900

    feat: 생성할 로또 개수 만큼 랜덤 숫자 6개 추출해서 발행
    Co-authored-by: shuyeon <moa0823@naver.com>

--- __tests__/LottoMaker.test.js
+++ __tests__/LottoMaker.test.js
@@ -1,10 +1,18 @@
 import LottoMaker from '../src/domain/LottoMaker.js';
 import { LOTTO_CONDITION } from '../src/constants/constants.js';
+import Lotto from '../src/domain/Lotto.js';
 
 describe('Lotto Maker 클래스 테스트', () => {
   test('구입 금액에 따른 로또 발행 테스트', () => {
     const lottoMaker = new LottoMaker(LOTTO_CONDITION.PRICE * 5);
 
     expect(lottoMaker.getLottoCount()).toBe(5);
+  });
+
+  test('로또 생성 테스트', () => {
+    const lottoMaker = new LottoMaker(LOTTO_CONDITION.PRICE);
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lottoMaker.create([1, 2, 3, 4, 5, 6]).numbers).toEqual(lotto.numbers);
   });
 });
--- src/domain/LottoMaker.js
+++ src/domain/LottoMaker.js
@@ -1,18 +1,34 @@
 import { LOTTO_CONDITION } from '../constants/constants.js';
-import { generateLottoNumbers } from '../utils/getRandomNumber.js';
+import { getRandomNumber } from '../utils/getRandomNumber.js';
+import Lotto from './Lotto.js';
 
 class LottoMaker {
   #money;
+  #lottoList;
 
   constructor(money) {
     this.#money = money;
+    this.#lottoList = [];
   }
 
   getLottoCount() {
     return this.#money / LOTTO_CONDITION.PRICE;
   }
 
-  make() {}
+  make() {
+    const count = this.getLottoCount();
+    for (let i = 0; i < count; i++) {
+      this.#lottoList.push(getRandomNumber());
+    }
+  }
+
+  create(randomNumber) {
+    return new Lotto(randomNumber);
+  }
+
+  get lottoList() {
+    return this.#lottoList;
+  }
 }
 
 export default LottoMaker;
