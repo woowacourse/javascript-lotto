50c8e3dbaf1e380f8fb232aac601613e2306c996
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-19 14:19:51 +0900

    feat: 사용자 구입금액 입력  추가
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+import readLineAsync from "./util/readLineAsync.js";
+
 class App {
-  run() {}
+  async getPurchaseMoney() {
+    const input = await readLineAsync("구입금액을 입력해 주세요.");
+    return input;
+  }
+
+  async run() {
+    const money = await this.getPurchaseMoney();
+  }
 }
 
 export default App;
