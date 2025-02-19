3139b677475bc6418ebb066a356b3976799f431a
thgml05 <aisohii@naver.com>
2025-02-19 14:55:59 +0900

    [feat] : 용자의 로또와 당첨 번호가 몇 개 동일한지 비교한다.

--- __tests__/lottoManager.test.js
+++ __tests__/lottoManager.test.js
@@ -1,9 +1,12 @@
 import LottoManager from "../src/LottoManager";
 
-test("구입 금액에 해당하는 만큼 로또를 발행해야 한다", () => {
+let lottoManager;
+beforeEach(() => {
   const price = 5000;
-  const lottoManager = new LottoManager(price);
+  lottoManager = new LottoManager(price);
+});
 
+test("구입 금액에 해당하는 만큼 로또를 발행해야 한다", () => {
   lottoManager.generateLottos();
   expect(lottoManager.lottos.length).toBe(5);
 });
@@ -11,9 +14,29 @@ test("구입 금액에 해당하는 만큼 로또를 발행해야 한다", () =>
 test("당첨 번호와 보너스 번호가 중복되는 경우 예외를 발생시킨다.", () => {
   const winningNumbers = [1, 2, 3, 4, 5, 6];
   const bonusNumber = 1;
-  const lottoManager = new LottoManager(5000);
 
   expect(() =>
     lottoManager.validateBonusNumberUnique(winningNumbers, bonusNumber)
   ).toThrow("보너스 번호는 당첨 번호와 중복되면 안됩니다.");
+});
+
+test("사용자의 로또와 당첨 번호가 몇 개 동일한지 비교한다.", () => {
+  const userLottos = [
+    [1, 2, 3, 4, 5, 6],
+    [1, 3, 4, 5, 6, 7],
+    [1, 4, 5, 6, 7, 8],
+    [1, 5, 6, 7, 8, 9],
+    [1, 6, 7, 8, 9, 10],
+    [1, 7, 8, 9, 10, 11],
+  ];
+  const winningNumber = [1, 2, 3, 4, 5, 6];
+  const countResults = lottoManager.countMatchingNumbers(
+    userLottos,
+    winningNumber
+  );
+
+  expect(countResults.threeMatch).toBe(1);
+  expect(countResults.fourMatch).toBe(1);
+  expect(countResults.fiveMatch).toBe(1);
+  expect(countResults.sixMatch).toBe(1);
 });
--- src/LottoManager.js
+++ src/LottoManager.js
@@ -25,6 +25,53 @@ class LottoManager {
     if (winningNumbers.includes(bonusNumber))
       throw new Error("보너스 번호는 당첨 번호와 중복되면 안됩니다.");
   }
+
+  #compareMatchingNumbers(userLotto, winningNumber) {
+    return userLotto.reduce((acc, lottoNumber) => {
+      if (winningNumber.includes(lottoNumber)) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
+  }
+
+  #switchMatchCount = (count) => {
+    switch (count) {
+      case 3:
+        return "threeMatch";
+      case 4:
+        return "fourMatch";
+      case 5:
+        return "fiveMatch";
+      case 6:
+        return "sixMatch";
+      default:
+        return;
+    }
+  };
+
+  countMatchingNumbers(userLottos, winningNumber) {
+    let countResults = {
+      threeMatch: 0,
+      fourMatch: 0,
+      fiveMatch: 0,
+      sixMatch: 0,
+    };
+
+    userLottos.forEach((userLotto) => {
+      const matchingCount = this.#compareMatchingNumbers(
+        userLotto,
+        winningNumber
+      );
+
+      if (matchingCount >= 3) {
+        const count = this.#switchMatchCount(matchingCount);
+        countResults[count] += 1;
+      }
+    });
+
+    return countResults;
+  }
 }
 
 export default LottoManager;
