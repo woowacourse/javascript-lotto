4d10174cb4c07dee303f1226503678addef897b4
spoyodevelop <kjy9505@gmail.com>
2025-02-19 17:38:48 +0900

    chores(eslint): eslint 컨피그 수정
    Co-authored-by: Db0111 <deokbin@naver.com>

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,24 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginJest from 'eslint-plugin-jest';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   pluginJs.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node, // Node.js 환경도 고려 (선택 사항)
-        ...globals.jest, // Jest 글로벌 변수 추가
+        ...globals.node,
+        ...globals.jest,
       },
     },
-    plugins: ['jest'],
-    extends: ['plugin:jest/recommended'],
+    plugins: {
+      jest: pluginJest,
+    },
     rules: {
-      'max-depth': ['error', 1], // 함수의 중첩 깊이 1단계까지만 허용
-      'max-params': ['error', 2], // 함수의 매개변수 2개 이하만 허용
+      ...pluginJest.configs.recommended.rules,
+      'max-depth': ['error', 1],
+      'max-params': ['error', 2],
     },
   },
 ];
