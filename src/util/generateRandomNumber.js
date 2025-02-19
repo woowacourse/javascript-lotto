f3f1ced2eb135a747f91f9f59feb9419fca3dc2e
spoyodevelop <kjy9505@gmail.com>
2025-02-19 14:27:03 +0900

    feat: generateRandomNumber에 fisher-Yate 알고리즘 적용
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/util/generateRandomNumber.js
+++ src/util/generateRandomNumber.js
@@ -1,3 +1,20 @@
-export default function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function createNumberArray(min, max) {
+  return Array.from({ length: max - min + 1 }, (_, i) => i + min);
 }
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export function getUniqueRandomNumbers(numberRange, count) {
+  const { min, max } = numberRange;
+  const numbers = createNumberArray(min, max);
+  const shuffled = shuffleArray(numbers);
+  return shuffled.slice(0, count);
+}
+
+// const sets = Array.from({ length: 6 }, () => getUniqueRandomNumbers(1, 45, 6));
