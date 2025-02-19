24599401d265c74061c784e642f77ac078ee45ef
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-19 14:51:23 +0900

    feat: 구입 금액이 공백인 경우 예외 처리
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -2,18 +2,24 @@ import Validator from "../src/Validator/validator.js";
 
 test("구입 금액이 문자인 경우에 예외 처리한다.", () => {
   const input = "천원";
-  const purchaseMoney = Number(input);
 
-  expect(() => Validator.validatePurchaseMoney(purchaseMoney)).toThrow(
+  expect(() => Validator.validatePurchaseMoney(input)).toThrow(
     "[ERROR] 구입 금액은 숫자로 입력해야 합니다.",
   );
 });
 
 test("구입 금액이 소수인 경우에 예외 처리한다.", () => {
   const input = "1000.5";
-  const purchaseMoney = Number(input);
 
-  expect(() => Validator.validatePurchaseMoney(purchaseMoney)).toThrow(
+  expect(() => Validator.validatePurchaseMoney(input)).toThrow(
     "[ERROR] 구입 금액은 정수로 입력해야 합니다.",
+  );
+});
+
+test("구입 금액이 소수인 경우에 예외 처리한다.", () => {
+  const input = "";
+
+  expect(() => Validator.validatePurchaseMoney(input)).toThrow(
+    "[ERROR] 구입 금액을 입력해주세요.",
   );
 });
--- src/Validator/validator.js
+++ src/Validator/validator.js
@@ -1,5 +1,10 @@
 const Validator = {
-  validatePurchaseMoney(purchaseMoney) {
+  validatePurchaseMoney(purchaseMoneyInput) {
+    if (purchaseMoneyInput === "")
+      throw new Error("[ERROR] 구입 금액을 입력해주세요.");
+
+    const purchaseMoney = Number(purchaseMoneyInput);
+
     if (Number.isNaN(purchaseMoney))
       throw new Error("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
 
