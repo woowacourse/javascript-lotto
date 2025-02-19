eff2dc5087214359a4a431c3dffbb09fc7a05b8c
hoyyChoi <hoyeon8165@gmail.com>
2025-02-19 17:03:41 +0900

    feat: profitCalculator 기능 구현 및 테스트 (당첨금 및 수익률 계산)
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>

---
+++ src/domain/profitCalculator.js
@@ -0,0 +1,12 @@
+import WINNING_PRICE from "../constants/WINNING_PRICE";
+
+const profitCalculator = (purchaseAmount, winningResult) => {
+  const totalWinningAmount = Object.entries(winningResult).reduce((count, [matchCount, winningCount]) => {
+    return count + WINNING_PRICE[matchCount] * winningCount;
+  }, 0);
+  const rate = (totalWinningAmount / purchaseAmount) * 100;
+
+  return rate;
+};
+
+export default profitCalculator;
