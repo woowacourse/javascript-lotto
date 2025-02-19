2a082959e052be0d581309e12dce8f255d31f4f7
hoyyChoi <hoyeon8165@gmail.com>
2025-02-19 18:12:43 +0900

    fix: purchaseLottoCount 검증 로직 분리
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>

--- src/domain/purchaseLottoCount.js
+++ src/domain/purchaseLottoCount.js
@@ -1,11 +1,3 @@
-import ERROR_MESSAGE from "../constants/ERROR_MESSAGE.js";
-
-const purchaseLottoCount = (money) => {
-  if (money % 1000 !== 0) {
-    throw new Error(ERROR_MESSAGE.INVALID_INPUT_PRICE);
-  }
-
-  return money / 1000;
-};
+const purchaseLottoCount = (money) => money / 1000;
 
 export default purchaseLottoCount;
