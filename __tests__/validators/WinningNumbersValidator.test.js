dc26a0268900d07e614181ef0eec093c0646ea1d
suhwa <tnghk9611@naver.com>
2025-02-19 14:51:34 +0900

    test: 당첨 번호 개수 예외 테스트 코드 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- __tests__/validators/WinningNumbersValidator.test.js
+++ __tests__/validators/WinningNumbersValidator.test.js
@@ -1,3 +1,4 @@
+import { validateCount } from "../../src/validators/validate";
 import { validateTypeAll } from "../../src/validators/WinningNumbersValidator";
 
 describe("당첨 번호 검증", () => {
@@ -7,6 +8,14 @@ describe("당첨 번호 검증", () => {
 
       expect(() => validateTypeAll(winningNumbers)).toThrow(
         "당첨 번호은(는) 숫자여야 합니다."
+      );
+    });
+
+    test("당첨 번호가 6개가 아니면 에러가 발생한다.", () => {
+      const winningNumbers = [1, 2, 3, 4, 5];
+
+      expect(() => validateCount("당첨 번호", winningNumbers)).toThrow(
+        "당첨 번호은(는) 6개여야 합니다."
       );
     });
   });
