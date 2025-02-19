bc182f1cb32c43a506488d70fb981fc88e562966
hoyyChoi <hoyeon8165@gmail.com>
2025-02-19 18:12:07 +0900

    feat: ErrorMessage 추가
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>

--- src/constants/ERROR_MESSAGE.js
+++ src/constants/ERROR_MESSAGE.js
@@ -1,5 +1,6 @@
 const ERROR_MESSAGE = Object.freeze({
-  INVALID_INPUT_PRICE: "구매 가격은 1000원 단위로 입력해주세요. ",
+  NOT_DIVISIBLE_BY_UNIT: "구매 가격은 1000원 단위로 입력해주세요. ",
+  INVALID_INPUT_PRICE: "숫자 값만 입력해주세요.",
 });
 
 export default ERROR_MESSAGE;
