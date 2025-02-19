da557cd7fa84cc38022f3f6dce4c8b5acbc4fb67
sooyeoniya <tndus1306403@naver.com>
2025-02-19 16:30:21 +0900

    feat(Output): 당첨 통계 내역 출력
    Co-authored-by: AHHYUNJU <AHHYUNJU@users.noreply.github.com>

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -34,6 +34,7 @@ class Controller {
       winningNumbers,
       bonusNumber,
     );
+    Output.printStatistics(winningStatistics.statistics);
   }
 }
 
--- src/views/Output.js
+++ src/views/Output.js
@@ -2,10 +2,22 @@ const Output = {
   printErrorMessage(errorMessage) {
     console.log(`[ERROR] ${errorMessage}\n`);
   },
+
   printIssuedLottos(purchaseAmount, lottos) {
     const lottoCount = purchaseAmount / 1_000;
     console.log(`${lottoCount}개를 구매했습니다.`);
     lottos.forEach((lotto) => console.log(`[${lotto.join(", ")}]`));
+  },
+
+  printStatistics(statistics) {
+    console.log("\n당첨 통계\n--------------------");
+    console.log(`3개 일치 (5,000원) - ${statistics.get(3)}개`);
+    console.log(`4개 일치 (50,000원) - ${statistics.get(4)}개`);
+    console.log(`5개 일치 (1,500,000원) - ${statistics.get(5)}개`);
+    console.log(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${statistics.get(5.5)}개`,
+    );
+    console.log(`6개 일치 (2,000,000,000원) - ${statistics.get(6)}개`);
   },
 };
 
