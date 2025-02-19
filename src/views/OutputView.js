27c8e1f1bec523b1fcdbf316880a5e4f9d5c3989
suhwa <tnghk9611@naver.com>
2025-02-19 17:18:19 +0900

    style: 출력 형식 수정
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -10,11 +10,11 @@ const OutputView = {
   printResult: function (counts, profitRate) {
     console.log("\n당첨 통계");
     console.log("--------------------");
-    console.log(`3개 일치 (5,000원)- ${counts[0]}개`);
-    console.log(`4개 일치 (50,000원)- ${counts[1]}개`);
-    console.log(`5개 일치 (1,500,000원)- ${counts[2]}개`);
-    console.log(`5개 일치, 보너스 볼 일치 (30,000,000원)- ${counts[3]}개`);
-    console.log(`6개 일치 (2,000,000,000원)- ${counts[4]}개`);
+    console.log(`3개 일치 (5,000원) - ${counts[0]}개`);
+    console.log(`4개 일치 (50,000원) - ${counts[1]}개`);
+    console.log(`5개 일치 (1,500,000원) - ${counts[2]}개`);
+    console.log(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${counts[3]}개`);
+    console.log(`6개 일치 (2,000,000,000원) - ${counts[4]}개`);
     console.log(
       `총 수익률은 ${profitRate.toFixed(1).toLocaleString()}%입니다.`
     );
