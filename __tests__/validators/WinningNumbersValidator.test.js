89e964f08643d9f2edb7843da40798dbad9a3938
suhwa <tnghk9611@naver.com>
2025-02-19 15:05:43 +0900

    test: 당첨 번호 범위 예외 테스트 코드 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- __tests__/validators/WinningNumbersValidator.test.js
+++ __tests__/validators/WinningNumbersValidator.test.js
@@ -1,5 +1,8 @@
-import { validateCount } from "../../src/validators/validate";
-import { validateTypeAll } from "../../src/validators/WinningNumbersValidator";
+import { validateCount, validateRange } from "../../src/validators/validate";
+import {
+  validateTypeAll,
+  validateRangeAll,
+} from "../../src/validators/WinningNumbersValidator";
 
 describe("당첨 번호 검증", () => {
   describe("예외 케이스", () => {
@@ -16,6 +19,14 @@ describe("당첨 번호 검증", () => {
 
       expect(() => validateCount("당첨 번호", winningNumbers)).toThrow(
         "당첨 번호은(는) 6개여야 합니다."
+      );
+    });
+
+    test("당첨 번호의 범위가 1~45 사이가 아니면 에러가 발생한다.", () => {
+      const winningNumbers = [0, 2, 3, 4, 5, 46];
+
+      expect(() => validateRangeAll(winningNumbers)).toThrow(
+        "당첨 번호은(는) 1 이상 45 이하여야 합니다."
       );
     });
   });
