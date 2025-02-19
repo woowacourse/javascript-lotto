fe221ff0e8d14d0b615699310828049b1f9e9f0f
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:49:38 +0900

    refactor(checkBonusNumber): 로또 및 보너스 번호 반환값 추가
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/util/checkBonusNumber.js
+++ src/util/checkBonusNumber.js
@@ -2,5 +2,6 @@ import ERROR_MESSAGE from '../settings/ErrorMessage.js';
 
 export default function checkBonusNumber(lotto, bonusNumber) {
   if (lotto.numbers.includes(bonusNumber))
-    throw new Error(ERROR_MESSAGE.duplicatedNumbers);
+    throw new Error(ERROR_MESSAGE.duplicatedBonusNumbers);
+  return { checkedLotto: lotto, checkedBonusNumber: bonusNumber };
 }
