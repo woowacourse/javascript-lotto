5365af689eaca7779f013d3fbcc0bd4ba70c866b
spoyodevelop <kjy9505@gmail.com>
2025-02-19 16:56:21 +0900

    feat(checkLottoPurchase): checkLottoPurchase validation 보강
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/util/checkLottoPurchase.js
+++ src/util/checkLottoPurchase.js
@@ -1,6 +1,12 @@
 import ERROR_MESSAGE from '../settings/ErrorMessage.js';
+import isInteger from './isInteger.js';
+import isNumber from './isNumber.js';
+import isPositive from './isPositive.js';
 
 export default function checkLottoPurchase(input) {
+  isNumber(input);
+  isInteger(input);
+  isPositive(input);
   if (Number(input) < 1000) throw new Error(ERROR_MESSAGE.notEnoughMoney);
   if (Number(input) % 1000 !== 0) throw new Error(ERROR_MESSAGE.notANote);
   return Number(input) / 1000;
