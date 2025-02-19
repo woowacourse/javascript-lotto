9ea0a8231fa4d5104cd60532c5df6d0472baea61
aydenote <dslgpgh@gmail.com>
2025-02-19 18:57:42 +0900

    feat: 로또 컨트롤러 생성
    - 로또 머신 생성
    - 구입 금액 입력
    - 로또 생성
    - 당첨 번호 입력
    - 보너스 번호 입력
    - 입력 예외 처리
    
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

---
+++ src/Controller/LottoController.js
@@ -0,0 +1,89 @@
+import InputView from '../View/InputView.js';
+import OutputView from '../View/OutputView.js';
+import Validate from '../Model/Validate.js';
+import LottoMachine from '../Model/LottoMachine.js';
+import Winning from '../Model/Winning.js';
+
+class LottoController {
+  async start() {
+    const lottoMachine = new LottoMachine();
+    const price = await this.#readPrice();
+
+    const lottos = lottoMachine.generateLotto(price);
+    OutputView.printLottos(lottos);
+
+    const winningNumbers = await this.#readWinningNumbers();
+    const bonusNumber = await this.#readBonusNumber(winningNumbers);
+  }
+
+  async #readPrice() {
+    while (true) {
+      try {
+        const price = await InputView.readPrice();
+        this.#validatePrice(price);
+        return price;
+      } catch (error) {
+        OutputView.printErrorMessage(error.message);
+      }
+    }
+  }
+
+  #validatePrice(price) {
+    try {
+      Validate.checkIsEmpty(price);
+      Validate.checkIsNumber(price);
+      Validate.checkThousandUnit(price);
+      Validate.checkPriceRange(price);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async #readWinningNumbers() {
+    try {
+      const winningNumbers = await InputView.readWinningNumbers();
+      this.#validateWinningNumbers(winningNumbers);
+      return winningNumbers.split(',').map(Number);
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+    }
+  }
+
+  #validateWinningNumbers(numbers) {
+    try {
+      Validate.checkIsEmpty(numbers);
+      const winningNumbers = numbers.split(',');
+      winningNumbers.forEach((winningNumber) => {
+        Validate.checkIsEmpty(winningNumber);
+        Validate.checkIsNumber(winningNumber);
+        Validate.checkLottoNumberRange(winningNumber);
+      });
+      Validate.checkWinningNumberDuplicate(winningNumbers);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async #readBonusNumber(winningNumbers) {
+    try {
+      const bonusNumber = await InputView.readBonusNumbers();
+      this.#validateBonusNumber(bonusNumber, winningNumbers);
+      return Number(bonusNumber);
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+    }
+  }
+
+  #validateBonusNumber(bonusNumber, winningNumbers) {
+    try {
+      Validate.checkIsEmpty(bonusNumber);
+      Validate.checkIsNumber(bonusNumber);
+      Validate.checkLottoNumberRange(bonusNumber);
+      Validate.checkBonusNumberDuplicate(winningNumbers, bonusNumber);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default LottoController;
