f3d0feb7bdfe0bb787201ee04273fac141f86739
keemsebeen <sebin0580@gmail.com>
2025-02-19 17:29:47 +0900

    feat: 에러메세지 constants 정의
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>
    Co-authored-by: Daeun-100 <Daeun-100@users.noreply.github.com>

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,5 +1,5 @@
 export const INPUT = Object.freeze({
-  PRICE: "구입금액을 입력해주세요.",
+  PRICE: "> 구입금액을 입력해주세요.",
   WINNER_NUMBERS: "> 당첨 번호를 입력해 주세요.",
   BONUS_NUMBER: "> 보너스 번호를 입력해주세요.",
   RETRY: "> 다시 시작하시겠습니까? (y/n)",
@@ -9,4 +9,12 @@ export const OUTPUT = Object.freeze({
   BUY_COUNT: "개를 구매했습니다.",
   WINNING_HISTORY: "당첨통계",
   LINE: "--------------------",
+});
+
+export const ERROR_PREFIX = "[ERROR]";
+
+export const ERROR = Object.freeze({
+  EMPTY: "빈 값은 입력할 수 없습니다.",
+  NOT_NUMBER: "숫자가 아닌 값은 입력할 수 없습니다.",
+  INVALID_RANGE: "범위를 벗어난 입력은 할 수 없습니다.",
 });
