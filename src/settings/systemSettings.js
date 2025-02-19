598b676ae170124a65aefaa7b91b2611f080474d
spoyodevelop <kjy9505@gmail.com>
2025-02-19 17:48:44 +0900

    refactor: winCount 상수 작성
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/settings/systemSettings.js
+++ src/settings/systemSettings.js
@@ -2,6 +2,14 @@ const systemSettings = {
   getPurchasePrice: '> 구입금액을 입력해 주세요.',
   getWinningNumber: '> 당첨 번호를 입력해 주세요.',
   getBonusNumber: '> 보너스 번호를 입력해 주세요.',
+  winCount: {
+    NO_MATCH: 0,
+    THREE_MATCH: 0,
+    FOUR_MATCH: 0,
+    FIVE_MATCH: 0,
+    FIVE_MATCH_WITH_BONUS: 0,
+    SIX_MATCH: 0,
+  },
 };
 
 export default systemSettings;
