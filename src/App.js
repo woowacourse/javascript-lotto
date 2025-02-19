3ab0f8e65956e23776eee3ed771c39bb395bf6bb
guesung <gueit214@naver.com>
2025-02-19 15:02:35 +0900

    feat: 미션 요구사항에 맞게 OutputView 분리

--- src/App.js
+++ src/App.js
@@ -9,37 +9,37 @@ import {
 } from "./lib/constants.js";
 import { generateRandomNumber, getIntersectCount } from "./lib/utils.js";
 import InputView from "./views/InputView.js";
+import OutputView from "./views/OutputView.js";
 
 class App {
   constructor() {}
 
   async run() {
-    // const purchaseAmount = await InputView.readPurchaseAmount();
-    const purchaseAmount = 1000;
+    const purchaseAmount = await InputView.readPurchaseAmount();
     const purchaseCount = purchaseAmount / LOTTO_PRICE;
 
-    const purchasedLottos = this.createLotto();
+    const purchasedLottos = this.createLotto(purchaseCount);
 
-    // const winNumbers = await InputView.readWinNumbers();
-    // const bonusNumber = await InputView.readBonusNumber();
+    OutputView.printPurchasedLottos(purchasedLottos);
 
-    const winNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 7;
-    // const purchasedLottos =
+    const winNumbers = await InputView.readWinNumbers();
+    const bonusNumber = await InputView.readBonusNumber();
 
     const lottoRanks = purchasedLottos.map((lotto) => {
       const winningLottoCount = getIntersectCount(lotto, winNumbers);
-      console.log({ lotto, winNumbers, winningLottoCount });
       const isBonusNumber = lotto.includes(bonusNumber);
 
       const rank = this.getRank(winningLottoCount, isBonusNumber);
       return rank;
     });
-    // console.log("lottoRanks", lottoRanks);
 
     /**@todo 당첨번호들이 중복되지 않아야 한다. */
 
     const totalPrize = this.calculateTotalProfit(lottoRanks);
+    const profitRate = this.calculateProfitRate(totalPrize, purchaseAmount);
+
+    OutputView.printStatistics(lottoRanks);
+    OutputView.printProfitRate(profitRate);
   }
 
   calculateProfitRate(profit, price) {
@@ -79,11 +79,10 @@ class App {
       .map((_) => generateRandomNumber(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
   }
 
-  createLotto() {
-    return [[1, 2, 3, 4, 5, 6]];
-    // return Array.from({ length: purchaseCount }, () => {
-    //   return this.createLottoNumber();
-    // });
+  createLotto(purchaseCount) {
+    return Array.from({ length: purchaseCount }, () => {
+      return this.createLottoNumber();
+    });
   }
 }
 
---
+++ src/views/OutputView.js
@@ -0,0 +1,42 @@
+//@ts-check
+
+import { LOTTO_RANK } from "../lib/constants.js";
+
+class OutputView {
+  static #print(message) {
+    console.log(message);
+  }
+
+  static printPurchasedLottos(purchasedLottos) {
+    return purchasedLottos.forEach((lotto) => this.#print(lotto));
+  }
+
+  static printStatistics(lottoRanks) {
+    this.#print("당첨 통계");
+    this.#print("------------");
+
+    [...Object.keys(LOTTO_RANK)].reverse().forEach((rank) => {
+      const bonusOutput = this.#getBonusOutput(LOTTO_RANK[rank].isBonusNumber);
+      const rankCount = lottoRanks.filter(
+        (lottoRank) => lottoRank === rank
+      ).length;
+      this.#print(
+        `${LOTTO_RANK[rank].winNumber}개 일치${bonusOutput} (${LOTTO_RANK[
+          rank
+        ].prize.toLocaleString()}) - ${rankCount}개`
+      );
+    });
+  }
+  static #getBonusOutput(isBonusNumber) {
+    if (isBonusNumber) {
+      return ", 보너스 볼 일치";
+    }
+    return "";
+  }
+
+  static printProfitRate(profitRate) {
+    console.log(`총 수익률은 ${profitRate}%입니다.`);
+  }
+}
+
+export default OutputView;
