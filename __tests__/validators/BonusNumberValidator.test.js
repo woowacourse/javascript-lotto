8d895c954bdc611b401bb68f1e7f72770527fac0
suhwa <tnghk9611@naver.com>
2025-02-19 15:13:17 +0900

    test: 보너스 번호 범위 예외 테스트 코드 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- __tests__/validators/BonusNumberValidator.test.js
+++ __tests__/validators/BonusNumberValidator.test.js
@@ -1,4 +1,4 @@
-import { validateType } from "../../src/validators/validate";
+import { validateRange, validateType } from "../../src/validators/validate";
 
 describe("보너스 번호 검증 테스트", () => {
   describe("예외 케이스", () => {
@@ -8,6 +8,20 @@ describe("보너스 번호 검증 테스트", () => {
         expect(() => validateType("보너스 번호", bonusNumber)).toThrow(
           "보너스 번호은(는) 숫자여야 합니다."
         );
+      }
+    );
+
+    test.each([-Infinity, 0, 46, Infinity])(
+      "보너스 번호의 범위가 1~45 사이가 아니면 에러가 발생한다. (입력 값 : %p)",
+      (bonusNumber) => {
+        expect(() =>
+          validateRange({
+            key: "보너스 번호",
+            value: bonusNumber,
+            min: 1,
+            max: 45,
+          })
+        ).toThrow("보너스 번호은(는) 1 이상 45 이하여야 합니다.");
       }
     );
   });
