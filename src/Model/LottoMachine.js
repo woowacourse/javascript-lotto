81a6afcb4f15e76133102d8df1621ff7284be9c3
aydenote <dslgpgh@gmail.com>
2025-02-19 15:17:59 +0900

    refactor: 구입 금액 만큼 로또 발행 함수 기능 분리
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/Model/LottoMachine.js
+++ src/Model/LottoMachine.js
@@ -8,16 +8,26 @@ class LottoMachine {
     this.#price = price;
   }
 
+  #generateUniqueRandomNumber(lottoNumbers) {
+    const randomNumber = getRandomNumberInRange();
+    if (!lottoNumbers.includes(randomNumber)) {
+      return randomNumber;
+    }
+  }
+
+  #generateLottoNumbers() {
+    const lottoNumbers = [];
+    while (lottoNumbers.length !== 6) {
+      const uniqueNumber = this.#generateUniqueRandomNumber(lottoNumbers);
+      lottoNumbers.push(uniqueNumber);
+    }
+    return lottoNumbers;
+  }
+
   generateLotto() {
     const lottos = [];
     for (let i = 0; i < this.#price / 1000; i++) {
-      const lottoNumbers = [];
-      while (lottoNumbers.length !== 6) {
-        const randomNumber = getRandomNumberInRange();
-        if (!lottoNumbers.includes(randomNumber)) {
-          lottoNumbers.push(randomNumber);
-        }
-      }
+      const lottoNumbers = this.#generateLottoNumbers();
       lottos.push(new Lotto(lottoNumbers));
     }
     return lottos;
