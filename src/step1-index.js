99af677e9aa9660995da7bd7b6528c0f15a7399f
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:39:04 +0900

    feat: 유저로 부터 로또를 받은 수를 validation
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/step1-index.js
+++ src/step1-index.js
@@ -9,17 +9,24 @@ import checkLottoPurchase from './util/checkLottoPurchase.js';
 import Lotto from './model/Lotto.js';
 import { getUniqueRandomNumbers } from './util/getUniqueRandomNumbers.js';
 
-const purchasePrice = await readLineAsync(systemSettings.getPurchasePrice);
-const purchaseAmount = checkLottoPurchase(purchasePrice);
+// const purchasePrice = await readLineAsync(systemSettings.getPurchasePrice);
+// const purchaseAmount = checkLottoPurchase(purchasePrice);
 
-printPurchasedAmount(purchaseAmount);
+// printPurchasedAmount(purchaseAmount);
 
-const lottos = [];
+// const lottos = [];
 
-for (let i = 0; i < purchaseAmount; i++) {
-  const numberRange = { min: 1, max: 45 };
-  const numbers = getUniqueRandomNumbers(numberRange, 6);
-  const lotto = new Lotto(numbers);
-  printLotto(lotto);
-  lottos.push(lotto);
-}
+// for (let i = 0; i < purchaseAmount; i++) {
+//   const numberRange = { min: 1, max: 45 };
+//   const numbers = getUniqueRandomNumbers(numberRange, 6);
+//   const lotto = new Lotto(numbers);
+//   printLotto(lotto);
+//   lottos.push(lotto);
+// }
+
+const winningNumber = await readLineAsync(systemSettings.getWinningNumber);
+
+// const bonusNumber = await readLineAsync(systemSettings.getBonusNumber);
+const userLotto = new Lotto(
+  winningNumber.split(',').map((number) => Number(number)),
+);
