6d084d38514938ac42050b01478aaed22af01562
thgml05 <aisohii@naver.com>
2025-02-19 17:26:50 +0900

    [feat] : 구입 금액, 당첨 번호, 보너스 번호 입력 받기

--- src/step1-index.js
+++ src/step1-index.js
@@ -2,3 +2,12 @@
  * step 1의 시작점이 되는 파일입니다.
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+import readLineAsync from "./readLineAsync.js";
+
+async function run() {
+  const price = await readLineAsync("구입금액을 입력해 주세요.");
+  const winningNumbers = await readLineAsync("당첨 번호를 입력해 주세요.");
+  const bonusNumber = await readLineAsync("보너스 번호를 입력해 주세요.");
+}
+run();
