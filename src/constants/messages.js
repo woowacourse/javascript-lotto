02d7ae43c654d0639ab1171f4e6a39dd897dcd2d
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-19 14:58:34 +0900

    feat(constants): 로또 갯수 매칭을 위한 상수 정의
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,3 +3,11 @@ export const INPUT = Object.freeze({
   WINNING_NUMBERS: '당첨 번호를 입력해 주세요. ',
   BONUS_NUMBER: '\n보너스 번호를 입력해 주세요. ',
 });
+
+export const LOTTO = Object.freeze({
+  FIVE_MATCH: 6,
+  FIVE_WITH_BONUS_MATCH: 5.5,
+  FIVE_MATCH: 5,
+  FOUR_MATCH: 4,
+  THREE_MATCH: 3,
+});
