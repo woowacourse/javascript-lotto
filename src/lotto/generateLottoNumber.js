12cde412ead39d04d30a1bdeb7f2a7938550129e
wo-o29 <woogur29@gmail.com>
2025-02-19 14:03:15 +0900

    ​refactor: 셔플 알고리즘 수정
    Co-authored-by: JeLee-river <106021313+JeLee-river@users.noreply.github.com>

--- src/lotto/generateLottoNumber.js
+++ src/lotto/generateLottoNumber.js
@@ -1,7 +1,21 @@
+import { LOTTO_RULE } from "../constants/lotto.js";
+
+/**
+ * 피셔 예이츠 셔플 사용
+ * {@link https://ko.wikipedia.org/wiki/%ED%94%BC%EC%85%94-%EC%98%88%EC%9D%B4%EC%B8%A0_%EC%85%94%ED%94%8C}
+ */
 const generateLottoNumbers = () => {
-  const numbers = Array.from({ length: 45 }, (_, i) => i + 1);
-  numbers.sort(() => Math.random() - 0.5);
-  const lottoNumbers = numbers.slice(0, 6);
+  const numbers = Array.from(
+    { length: LOTTO_RULE.MAX_LOTTO_NUMBER },
+    (_, i) => i + 1
+  );
+
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+
+  const lottoNumbers = numbers.slice(0, LOTTO_RULE.LOTTO_LENGTH);
   return lottoNumbers.sort((a, b) => a - b);
 };
 
