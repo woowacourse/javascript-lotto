3a175f292d773dc5b33fc68ab3837968b83b6c6e
aydenote <dslgpgh@gmail.com>
2025-02-19 18:34:45 +0900

    fix: 중복 숫자 없이 로또 발행하도록 오류 수정
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/Model/LottoMachine.js
+++ src/Model/LottoMachine.js
@@ -1,19 +1,24 @@
-import { getRandomNumberInRange } from '../util/randomNumber';
+import { getRandomNumberInRange } from '../util/randomNumber.js';
 import Lotto from './Lotto.js';
 
 class LottoMachine {
-  #generateUniqueRandomNumber(lottoNumbers) {
-    const randomNumber = getRandomNumberInRange();
-    if (!lottoNumbers.includes(randomNumber)) {
-      return randomNumber;
-    }
+  constructor(price) {
+    this.rankHistory = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
   }
 
   #generateLottoNumbers() {
     const lottoNumbers = [];
     while (lottoNumbers.length !== 6) {
-      const uniqueNumber = this.#generateUniqueRandomNumber(lottoNumbers);
-      lottoNumbers.push(uniqueNumber);
+      const randomNumber = getRandomNumberInRange();
+      if (!lottoNumbers.includes(randomNumber)) {
+        lottoNumbers.push(randomNumber);
+      }
     }
     return lottoNumbers;
   }
@@ -25,6 +30,29 @@ class LottoMachine {
       lottos.push(new Lotto(lottoNumbers));
     }
     return lottos;
+  }
+
+  calculateRank(rank) {
+    if (rank === 1) {
+      this.rankHistory.first += 1;
+      return;
+    }
+    if (rank === 2) {
+      this.rankHistory.second += 1;
+      return;
+    }
+    if (rank === 3) {
+      this.rankHistory.third += 1;
+      return;
+    }
+    if (rank === 4) {
+      this.rankHistory.fourth += 1;
+      return;
+    }
+    if (rank === 5) {
+      this.rankHistory.fifth += 1;
+      return;
+    }
   }
 }
 
