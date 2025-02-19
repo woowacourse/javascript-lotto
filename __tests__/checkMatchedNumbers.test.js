16e416ea84d3401b051a1783a942fa265f97f90c
spoyodevelop <kjy9505@gmail.com>
2025-02-19 16:31:01 +0900

    feat(MatchService): 두 배열을 넣었을 때의 중복되는 숫자의 갯수를 반환하는 함수 추가
    Co-authored-by: Db0111 <deokbin@naver.com>

---
+++ __tests__/checkMatchedNumbers.test.js
@@ -0,0 +1,9 @@
+import countMatchedNumbers from '../src/service/MatchService.js';
+
+describe('생성된 로또 번호와 사용자의 당첨 번호 비교 테스트', () => {
+  it('checkMatchedCount는 두 배열을 넣었을 때 중복이 되는 숫자의 개수를 반환해야 한다.', () => {
+    const a = [1, 2, 3, 4, 5, 6];
+    const b = [2, 4, 6, 8, 10, 12];
+    expect(countMatchedNumbers(a, b)).toBe(3);
+  });
+});
