e5bb31adb04087c34010257478690e8c01a323d8
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-19 19:29:58 +0900

    test: 리팩토링에 따른 테스트코드 수정
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- __tests__/getWinningMatchCount.test.js
+++ __tests__/getWinningMatchCount.test.js
@@ -11,20 +11,20 @@ describe('로또 당첨 개수 테스트', () => {
     const randomLottos = [[8, 12, 14, 23, 41, 1]];
     const matchedCount = getWinningMatchCount(randomLottos, { winningNumbers, bonusNumber });
 
-    expect(matchedCount).toEqual([5]);
+    expect(matchedCount).toEqual([0, 0, 0, 0, 0, 1, 0, 0]);
   });
 
   test('로또 당첨 개수가 5개이고 보너스 넘버가 있어야 한다.', () => {
     const randomLottos = [[8, 12, 14, 23, 41, 45]];
     const matchedCount = getWinningMatchCount(randomLottos, { winningNumbers, bonusNumber });
 
-    expect(matchedCount).toEqual([5.5]);
+    expect(matchedCount).toEqual([0, 0, 0, 0, 0, 0, 0, 1]);
   });
 
   test('로또 당첨 개수가 6개여야 한다.', () => {
     const randomLottos = [[8, 12, 14, 23, 41, 38]];
     const matchedCount = getWinningMatchCount(randomLottos, { winningNumbers, bonusNumber });
 
-    expect(matchedCount).toEqual([6]);
+    expect(matchedCount).toEqual([0, 0, 0, 0, 0, 0, 1, 0]);
   });
 });
