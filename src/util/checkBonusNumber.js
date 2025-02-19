53d6d8f1e15134e06691393818121079374dca9a
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:57:50 +0900

    feat(bonusNumber): bonus number validation 보강
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/util/checkBonusNumber.js
+++ src/util/checkBonusNumber.js
@@ -1,6 +1,12 @@
 import ERROR_MESSAGE from '../settings/ErrorMessage.js';
+import isInteger from './isInteger.js';
+import isNumber from './isNumber.js';
+import isPositive from './isPositive.js';
 
 export default function checkBonusNumber(lotto, bonusNumber) {
+  isInteger(bonusNumber);
+  isNumber(bonusNumber);
+  isPositive(bonusNumber);
   if (lotto.numbers.includes(bonusNumber))
     throw new Error(ERROR_MESSAGE.duplicatedBonusNumbers);
   return { checkedLotto: lotto, checkedBonusNumber: bonusNumber };
