28a0949f6ed8c3acd47e62be9083421abb3f7455
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-19 14:27:39 +0900

    feat: 사용자 입력 당첨 번호 입력 추가
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import readLineAsync from "./util/readLineAsync.js";
 
 class App {
   async getPurchaseMoney() {
-    const input = await readLineAsync("구입금액을 입력해 주세요.");
+    const input = await readLineAsync("> 구입금액을 입력해 주세요. ");
+    return input;
+  }
+
+  async getWinningNumbers() {
+    const input = await readLineAsync("> 당첨 번호를 입력해 주세요. ");
     return input;
   }
 
   async run() {
     const money = await this.getPurchaseMoney();
+    const winningNumbers = await this.getWinningNumbers();
   }
 }
 
