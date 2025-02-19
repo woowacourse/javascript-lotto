2bd8d02efcb3d90b9f400b54be8aff3bc79b1a2b
spoyodevelop <kjy9505@gmail.com>
2025-02-19 18:24:54 +0900

    refactor(prizeMoney): prizeMoney systemSettings로 분리
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/settings/systemSettings.js
+++ src/settings/systemSettings.js
@@ -10,6 +10,14 @@ const systemSettings = {
     FIVE_MATCH_WITH_BONUS: 0,
     SIX_MATCH: 0,
   },
+  prizeMoney: {
+    NO_MATCH: 0,
+    THREE_MATCH: 5_000,
+    FOUR_MATCH: 50_000,
+    FIVE_MATCH: 1_500_000,
+    FIVE_MATCH_WITH_BONUS: 30_000_000,
+    SIX_MATCH: 2_000_000_000,
+  },
 };
 
 export default systemSettings;
