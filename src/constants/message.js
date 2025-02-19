8fe12bc15fb787bf7ab41c34ec1fdbac29fe4dcb
keemsebeen <sebin0580@gmail.com>
2025-02-19 15:28:22 +0900

    feat: INPUT, OUTPUT 상수화
    Co-authored-by: Daeun-100 <Daeun-100@users.noreply.github.com>
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/constants/message.js
@@ -0,0 +1,12 @@
+export const INPUT = {
+  PRICE: "구입금액을 입력해주세요.",
+  WINNER_NUMBERS: "> 당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER: "> 보너스 번호를 입력해주세요.",
+  RETRY: "> 다시 시작하시겠습니까? (y/n)",
+};
+
+export const OUTPUT = {
+  BUY_COUNT: "개를 구매했습니다.",
+  WINNING_HISTORY: "당첨통계",
+  LINE: "--------------------",
+};
