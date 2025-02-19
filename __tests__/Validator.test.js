4eb8a582c61d4ecde017a2d5eecfb48404868bc4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-19 14:45:49 +0900

    feat: 구입 금액이 소수인 경우 예외 처리
    Co-authored-by: ha-kuku <hakukudh@gmail.com>

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -8,3 +8,12 @@ test("구입 금액이 문자인 경우에 예외 처리한다.", () => {
     "[ERROR] 구입 금액은 숫자로 입력해야 합니다.",
   );
 });
+
+test("구입 금액이 소수인 경우에 예외 처리한다.", () => {
+  const input = "1000.5";
+  const purchaseMoney = Number(input);
+
+  expect(() => Validator.validatePurchaseMoney(purchaseMoney)).toThrow(
+    "[ERROR] 구입 금액은 정수로 입력해야 합니다.",
+  );
+});
--- src/Validator/validator.js
+++ src/Validator/validator.js
@@ -2,6 +2,9 @@ const Validator = {
   validatePurchaseMoney(purchaseMoney) {
     if (Number.isNaN(purchaseMoney))
       throw new Error("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+
+    if (!Number.isInteger(purchaseMoney))
+      throw new Error("[ERROR] 구입 금액은 정수로 입력해야 합니다.");
   },
 };
 
