d619e73aae8756d05d1e7fae20c5224fc1c3a559
suhwa <tnghk9611@naver.com>
2025-02-19 15:06:00 +0900

    feat: 당첨 번호 범위 예외 처리 구현
    Co-authored-by: eunoia-jaxson <eunoia@handong.ac.kr>

--- src/validators/WinningNumbersValidator.js
+++ src/validators/WinningNumbersValidator.js
@@ -1,8 +1,9 @@
-import { validateType } from "./validate";
+import { validateType, validateRange } from "./validate";
 
 const WinningNumbersValidator = {
   validate: (winningNumbers) => {
     validateTypeAll(winningNumbers);
+    validateCount(winningNumbers);
   },
 };
 
@@ -13,9 +14,20 @@ const validateTypeAll = (winningNumbers) => {
 };
 
 const validateCount = () => {
-    if (value.length !== 6) {
-      throw new Error(`은(는) 6개여야 합니다.`);
-    }
-  };
+  if (value.length !== 6) {
+    throw new Error(`은(는) 6개여야 합니다.`);
+  }
+};
 
-export { WinningNumbersValidator, validateTypeAll };
+const validateRangeAll = (winningNumbers) => {
+  winningNumbers.forEach((number) => {
+    validateRange({
+      key: "당첨 번호",
+      value: number,
+      min: 1,
+      max: 45,
+    });
+  });
+};
+
+export { WinningNumbersValidator, validateTypeAll, validateRangeAll };
