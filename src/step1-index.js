aa0dca75b85c80edd11d5f54b56a89b6ba6fb4ee
spoyodevelop <kjy9505@gmail.com>
2025-02-19 15:11:37 +0900

    feat(Index): 로또 구매 갯수에 따른 로또 번호 생성 및 출력
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,9 +1,25 @@
 import readLineAsync from './View/InputView.js';
 import systemSettings from './settings/systemSettings.js';
-import { printMessage, printPurchasedAmount } from './View/OutputView.js';
+import {
+  printMessage,
+  printPurchasedAmount,
+  printLotto,
+} from './View/OutputView.js';
 import checkLottoPurchase from './util/checkLottoPurchase.js';
+import Lotto from './model/Lotto.js';
+import { getUniqueRandomNumbers } from './util/getUniqueRandomNumbers.js';
 
 const purchasePrice = await readLineAsync(systemSettings.getPurchasePrice);
 const purchaseAmount = checkLottoPurchase(purchasePrice);
 
 printPurchasedAmount(purchaseAmount);
+
+const lottos = [];
+
+for (let i = 0; i < purchaseAmount; i++) {
+  const numberRange = { min: 1, max: 45 };
+  const numbers = getUniqueRandomNumbers(numberRange, 6);
+  const lotto = new Lotto(numbers);
+  printLotto(lotto);
+  lottos.push(lotto);
+}
