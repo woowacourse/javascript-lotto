458a28b6e6b46f9c5cb23c7b0c552089c3792d84
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 14:19:23 +0900

    refactor: 중복해서 사용하는 유효성 검사 함수는 공용 파일로 이동
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -30,11 +30,3 @@ test('로또 번호는 오름차순이여야 한다.', () => {
   // then
   expect(lotto.getNumbers()).toEqual([1, 2, 5, 9, 24, 35]);
 });
-
-test.each([
-  [[0, 1, 2, 3, 4, 5]], [[41, 42, 43, 44, 45, 46]],
-])('로또의 번호의 숫자 범위는 1 미만 혹은 45 초과하면 에러가 발생한다.', (lottoNumbers) => {
-  // then
-  expect(() => new Lotto(lottoNumbers))
-    .toThrow('[ERROR] 로또 번호의 숫자 범위 1 ~ 45이다.');
-});
--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -23,3 +23,13 @@ describe('로또 구입 금액',() => {
     }).toThrow();
   });
 })
+
+describe('로또 숫자',() => {
+  test.each([
+    [[0, 1, 2, 3, 4, 5]], [[41, 42, 43, 44, 45, 46]],
+  ])('로또의 번호의 숫자 범위는 1 미만 혹은 45 초과하면 에러가 발생한다.', (lottoNumbers) => {
+    // then
+    expect(() => new Lotto(lottoNumbers))
+      .toThrow('[ERROR] 로또 번호의 숫자 범위 1 ~ 45이다.');
+  });
+});
