d69954207be352b366b8d8a19d1b302c3299117d
Oliy <eunsoa0310@gmail.com>
2025-02-20 16:30:35 +0900

    refactor: initialize를 각각 분리
    - initializePurchaseAmount
    - initializeWinningNumbers
    - initializeBonusNumbers
    
    Co-Authored-By: 신종욱 <66900221+dlsxjzld@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ import {
   validateWinningNumbers,
   validateBonusNumber,
 } from './View/Validation/inputView.js';
+import LottoManager from './Domain/Model/LottoManager.js';
+
 class App {
-  async #initialize() {
+  async #initializePurchaseAmount() {
     const purchaseAmountInput = await readUserInputUntilSuccess({
       readUserInput: getPurchaseAmountInput,
       formatter: (input) => {
@@ -23,7 +25,10 @@ class App {
         return convertedInput;
       },
     });
-
+    return purchaseAmountInput
+  }
+  
+  async #initializeWinningNumbers (){
     const winningNumbersInput = await readUserInputUntilSuccess({
       readUserInput: getWinningNumbersInput,
       formatter: (input) => {
@@ -36,27 +41,36 @@ class App {
         return numbers;
       },
     });
+    return winningNumbersInput,
+  }
 
-    const bonusNumberInput = await readUserInputUntilSuccess({
-      readUserInput: getBonusNumberInput,
-      formatter: (input) => {
-        validateEmptySpace(input);
-        const convertedInput = convertFormat.toNumber(input);
-        validateBonusNumber(convertedInput, winningNumbersInput);
-        return convertedInput;
-      },
-    });
-
-    return {
-      purchaseAmountInput,
-      winningNumbersInput,
-      bonusNumberInput,
-    };
+  async #initializeBonusNumbers(){
+      const bonusNumberInput = await readUserInputUntilSuccess({
+        readUserInput: getBonusNumberInput,
+        formatter: (input) => {
+          validateEmptySpace(input);
+          const convertedInput = convertFormat.toNumber(input);
+          validateBonusNumber(convertedInput, winningNumbersInput);
+          return convertedInput;
+        },
+      });
+    return bonusNumberInput;
   }
 
   async run() {
-    const { purchaseAmountInput, winningNumbersInput, bonusNumberInput } =
-      await this.#initialize();
+    const purchaseAmountInput = await this.#initializePurchaseAmount(); 
+
+    const lottoManager = new LottoManager();
+    const lottoCounts = lottoManager.purchaseLotto(purchaseAmountInput)
+    lottoManager.makeLottoList(lottoCounts);
+    // TODO: ${lottoCounts}개를 구매했습니다. view
+    // TODO: lotto 구매한만큼(lottoList) 보여주기
+    
+    const winningNumbersInput = await this.#initializeBonusNumbers();
+    
+    const bonusNumberInput = await this.#initializeBonusNumbers();
+
+
   }
 
   async retryRun() {
