221fc26280c1ea7ef61fd55af4bccd882c9574eb
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-20 11:28:23 +0900

    feat(validation): 재입력 받는 로직 유효성 검사 구현
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/validation/validateInput.js
+++ src/validation/validateInput.js
@@ -2,12 +2,18 @@ import { ERROR } from '../constants/errors.js';
 
 export const hasEmptyString = (input) => {
   if (input === '') {
-    throw new Error(ERROR.IS_VALUE_EMPTY);
+    throw new Error(ERROR.VALUE_EMPTY);
   }
 };
 
 export const isValueInteger = (input) => {
   if (!Number.isInteger(input)) {
-    throw new Error(ERROR.IS_NOT_POSITIVE_INTEGER);
+    throw new Error(ERROR.NOT_POSITIVE_INTEGER);
+  }
+};
+
+export const isYesOrNo = (input) => {
+  if (input !== 'y' && input !== 'n') {
+    throw new Error(ERROR.CHECK_REPLAY_GAME);
   }
 };
