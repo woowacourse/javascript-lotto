76ca4e4cdccc0d1cc587ab3c5333a0661f7a01de
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-21 00:22:37 +0900

    refactor(test): 테스트코드 네이밍 명료화

--- __tests__/caculateRevenue.test.js
+++ __tests__/caculateRevenue.test.js
@@ -1,12 +1,12 @@
 import { calculateRevenue } from '../src/domain/calculateRevenue.js';
 
-describe('로또 수익률 계산 테스트', () => {
+describe('로또 구입금액이 1000원일 때, 로또 수익률 계산 테스트', () => {
   let purchasePrice;
 
   beforeEach(() => {
     purchasePrice = 1000;
   });
-  test('로또 당첨 개수가 6개인 경우 수익률 ', () => {
+  test('로또 당첨 개수가 6개인 경우, 상금은 2,000,000,000이므로 수익률은 2,000,000,000/1000*100 이다.', () => {
     const matchCounts = [0, 0, 0, 0, 0, 0, 1, 0];
 
     const expectedResult = Number(((2_000_000_000 / purchasePrice) * 100).toFixed(1));
@@ -15,7 +15,7 @@ describe('로또 수익률 계산 테스트', () => {
     expect(revenue).toBe(expectedResult);
   });
 
-  test('로또 당첨 개수가 5.5개인 경우 수익률', () => {
+  test('로또 당첨 개수가 5개 + 보너스 번호인 경우, 상금은 30,000,000이므로 수익률은 30,000,000/1000*100 이다.', () => {
     const matchCounts = [0, 0, 0, 0, 0, 0, 0, 1];
 
     const expectedResult = Number(((30_000_000 / purchasePrice) * 100).toFixed(1));
@@ -24,7 +24,7 @@ describe('로또 수익률 계산 테스트', () => {
     expect(revenue).toBe(expectedResult);
   });
 
-  test('로또 당첨 개수가 5개인 경우 수익률', () => {
+  test('로또 당첨 개수가 5개인 경우, 상금은 1,500,000이므로 수익률은 1,500,000/1000*100 이다.', () => {
     const matchCounts = [0, 0, 0, 0, 0, 1, 0, 0];
 
     const expectedResult = Number(((1_500_000 / purchasePrice) * 100).toFixed(1));
