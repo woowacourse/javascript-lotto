e2469cb314a485e8ab66349b0bc2883ffb3fe011
dev-dino22 <wlsskdud02@naver.com>
2025-02-20 11:57:23 +0900

    feat(Error): 에러 메세지 객체 생성

---
+++ src/constant/Error.js
@@ -0,0 +1,25 @@
+const Error = {
+  USER_INPUT: {
+    IS_EMPTY: "[ERROR] 빈 값은 입력할 수 없습니다.",
+  },
+  WINNING_NUMBERS: {
+    IS_WRONG_ARRAY_LENGTH: "[ERROR] 로또는 6개의 숫자로 이루어져야합니다.",
+    IS_DUPLICATED_NUMBER: "[ERROR] 중복된 숫자는 입력하실 수 없습니다.",
+    IS_ARRAY_NUMBER_RANGE_OVER: "[ERROR] 1~45 사이의 숫자를 입력해야합니다.",
+    IS_NOT_NATURAL_NUMBER_IN_ARRAY: "[ERROR] 숫자는 자연수여야 합니다.",
+  },
+  BONUS_NUMBER: {
+    IS_NUMBER_RANGE_OVER: "[ERROR] 1~45 사이의 숫자를 입력해야합니다.",
+    IS_NOT_NATURAL_NUMBER: "[ERROR] 숫자는 자연수여야 합니다.",
+  },
+  WINNINGS_AND_BONUS: {
+    IS_DUPLICATED: "[ERROR] 당첨번호와 중복된 숫자는 입력하실 수 없습니다.",
+  },
+  PURCHASE_PRICE: {
+    IS_NUMBER_RANGE_OVER:
+      "[ERROR] 1000 이상 100억 이하의 숫자를 입력해야합니다.",
+    IS_NOT_MULTIPLE: "[ERROR] 1000 단위로 나누어 떨어지는 숫자여야합니다.",
+  },
+};
+
+export default Error;
