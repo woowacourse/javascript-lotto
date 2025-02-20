04f5dffba7ff876a748297e313a304fca4eed916
keemsebeen <sebin0580@gmail.com>
2025-02-20 16:32:06 +0900

    refactor: index.js Input 로직 분리
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>
    Co-authored-by: Daeun-100 <Daeun-100@users.noreply.github.com>

--- src/step1-index.js
+++ src/step1-index.js
@@ -15,19 +15,14 @@ import Ticket from "./domain/Ticket.js";
  * step 1의 시작점이 되는 파일입니다.
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
-const run = async () => {
-  const price = await Input.retry(async () => {
-    const input = await Input.readLineAsync(INPUT.PRICE);
-    new PriceValidator().validatePrice(Number(input));
-    return Number(input);
-  });
 
-  const countNumber = divideByUnit(PRICE.UNIT, price);
-  Output.print(`${countNumber}${OUTPUT.BUY_COUNT}`);
+const priceInput = await Input.retry(async () => {
+  const input = await Input.readLineAsync(INPUT.PRICE);
+  new PriceValidator().validatePrice(Number(input));
+  return Number(input);
+});
 
-  const lottos = Ticket.createLottos(countNumber);
-  Output.printLottoNumber(lottos);
-
+const getNeededLottoNumbers = async () => {
   const winningLotto = await Input.retry(async () => {
     const input = await Input.readLineAsync(INPUT.WINNER_NUMBERS);
     const winningNumbersArray = input.split(",").map(Number);
@@ -43,12 +38,23 @@ const run = async () => {
     );
     return Number(input);
   });
+  return { winningLotto, bonusLottoNumber };
+};
 
+const game = async () => {
+  const price = await priceInput();
+
+  const countNumber = divideByUnit(PRICE.UNIT, price);
+  Output.print(`${countNumber}${OUTPUT.BUY_COUNT}`);
+
+  const lottos = Ticket.createLottos(countNumber);
+  Output.printLottoNumber(lottos);
+
+  const { winningLotto, bonusLottoNumber } = await getNeededLottoNumbers();
   Output.print(OUTPUT.WINNING_HISTORY);
   Output.print(OUTPUT.LINE);
 
   const lottoMachine = new LottoMachine(lottos);
-
   const lottoStatus = lottoMachine.getMatchedLottoStatus(
     winningLotto.getLottoNumbers(),
     bonusLottoNumber
@@ -62,7 +68,7 @@ const run = async () => {
 };
 
 const start = async () => {
-  await run();
+  await game();
 
   const retry = await Input.retry(async () => {
     const input = await Input.readLineAsync(INPUT.RETRY);
@@ -72,7 +78,7 @@ const start = async () => {
     return input.toLowerCase();
   });
 
-  if (retry === "y") await start();
+  if (retry === "y") await game();
 };
 
 start();
