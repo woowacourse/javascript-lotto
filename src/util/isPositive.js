354bc7da101c07b4cf7d23daf77d4bac258da564
spoyodevelop <kjy9505@gmail.com>
2025-02-20 13:48:25 +0900

    fix: isPositive 0을 검증하지 못하는 로직 수정
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/util/isPositive.js
+++ src/util/isPositive.js
@@ -1,6 +1,6 @@
 import ERROR_MESSAGE from '../settings/ErrorMessage.js';
 
 export default function isPositive(input) {
-  if (Number(input) < 0) throw new Error(ERROR_MESSAGE.notPositive);
+  if (Number(input) <= 0) throw new Error(ERROR_MESSAGE.notPositive);
   return Number(input);
 }
