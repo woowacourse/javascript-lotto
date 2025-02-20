a4c2ec83348b4a1ff8b119dd83383956eb055abc
keemsebeen <sebin0580@gmail.com>
2025-02-20 16:17:47 +0900

    refactor: Ticket 객체에 로또 발행 함수 추가
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>
    Co-authored-by: Daeun-100 <Daeun-100@users.noreply.github.com>

--- src/domain/Ticket.js
+++ src/domain/Ticket.js
@@ -1,14 +1,25 @@
 import { LOTTO_NUMBER } from "../constants/lotto.js";
-import { getUniqueRandomNumbers } from "../utils/random.js";
+import { getRandomNumber } from "../utils/random.js";
 import Lotto from "./Lotto.js";
 
 const Ticket = {
+  checkAndAddLottoNumbers: (store, number) => {
+    if (store.has(number)) return;
+    store.add(number);
+  },
+
+  issueLottoNumbers: () => {
+    const randomNumberStore = new Set();
+    while (randomNumberStore.size < LOTTO_NUMBER.LENGTH) {
+      const number = getRandomNumber(LOTTO_NUMBER.MIN, LOTTO_NUMBER.MAX);
+      Ticket.checkAndAddLottoNumbers(randomNumberStore, number);
+    }
+    return [...randomNumberStore];
+  },
+
   createLottos: (count) => {
     return Array.from({ length: count }, () => {
-      const lottoNumbers = getUniqueRandomNumbers(
-        { min: LOTTO_NUMBER.MIN, max: LOTTO_NUMBER.MAX },
-        LOTTO_NUMBER.LENGTH
-      );
+      const lottoNumbers = Ticket.issueLottoNumbers();
       return new Lotto(lottoNumbers);
     });
   },
