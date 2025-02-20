433a6925bbcdcb596e700c24927cf4c3a35f5fc2
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-20 14:30:38 +0900

    fix: import 파일 경로 수정
    Co-authored-by: Hongju Lee <hongju4385@gmail.com>

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,4 +1,4 @@
-import { LOTTO } from '../constants/messages';
+import { LOTTO } from '../constants/messages.js';
 
 export const getRandomNumber = () => {
   return Math.floor(Math.random() * LOTTO.MAX_RANDOM_NUMBER) + LOTTO.MIN_RANDOM_NUMBER;
