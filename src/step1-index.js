446d87df7cf6bf32dbcc5e336b89ce72805c3b7b
keemsebeen <sebin0580@gmail.com>
2025-02-20 17:17:12 +0900

    fix: printInput 함수 화살표 함수 누락 추가
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>
    Co-authored-by: Daeun-100 <Daeun-100@users.noreply.github.com>

--- src/step1-index.js
+++ src/step1-index.js
@@ -16,11 +16,12 @@ import Ticket from "./domain/Ticket.js";
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-const priceInput = await Input.retry(async () => {
-  const input = await Input.readLineAsync(INPUT.PRICE);
-  new PriceValidator().validatePrice(Number(input));
-  return Number(input);
-});
+const priceInput = async () =>
+  Input.retry(async () => {
+    const input = await Input.readLineAsync(INPUT.PRICE);
+    new PriceValidator().validatePrice(Number(input));
+    return Number(input);
+  });
 
 const getNeededLottoNumbers = async () => {
   const winningLotto = await Input.retry(async () => {
