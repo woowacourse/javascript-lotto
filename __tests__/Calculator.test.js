eccf772ce56db36df832e8a8c447451b331a06fc
yeji0214 <1225yj50@naver.com>
2025-02-20 15:08:51 +0900

    refactor: totalPrize 로직 수정 및 테스트 추가
    Co-authored-by: ohgus <anakin3740@gmail.com>

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,14 +1,32 @@
 import Calculator from "../src/Calculator.js";
+import { KEYS } from "../src/constant/lotto.js";
 
-test.each([
-  [[[1,2,3,4,5,6]], 1, '1등'],
-  [[[1,2,3,4,5,9]], 1, '2등'],
-  [[[1,2,3,4,5,10]], 1, '3등'],
-  [[[1,2,3,4,7,10]], 1, '4등'],
-  [[[1,2,3,8,7,10]], 1, '5등'],
+describe('', () => {
+  test.each([
+    [[[1,2,3,4,5,6]], 1, '1등'],
+    [[[1,2,3,4,5,9]], 1, '2등'],
+    [[[1,2,3,4,5,10]], 1, '3등'],
+    [[[1,2,3,4,7,10]], 1, '4등'],
+    [[[1,2,3,8,7,10]], 1, '5등'],
+  
+  ])('일치하는 개수에 맞는 등수의 값이 증가한다.', (lottos, expected, key) => {
+    const winningCount = Calculator.winningCount(lottos, {winning:[1,2,3,4,5,6], bonus: 9});
+  
+    expect(winningCount[key]).toBe(expected);
+  })
 
-])('일치하는 개수에 맞는 등수의 값이 증가한다.', (lottos, expected, key) => {
-  const winningCount = Calculator.winningCount(lottos, {winning:[1,2,3,4,5,6], bonus: 9});
+  test('당첨금을 계산한다.', () => {
+    const winningCount = { [KEYS.FIRST]: 0, [KEYS.SECOND]: 0, [KEYS.THIRD]: 2, [KEYS.FOURTH]: 0, [KEYS.FIFTH]: 0 };
+    const totalPrize = Calculator.totalPrize(winningCount);
 
-  expect(winningCount[key]).toBe(expected);
+    expect(totalPrize).toBe(3_000_000);
+  })
+
+  test('수익률을 계산한다.', () => {
+    const amount = '5000';
+    const totalPrize = 50_000;
+    const yieldRate = Calculator.yieldRate(amount, totalPrize);
+
+    expect(yieldRate).toBe('1000.0');
+  })
 })
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,41 +1,33 @@
 import { WINNING, KEYS } from "./constant/lotto.js";
 
 const Calculator = {
-    winningCount(lottos, {winning, bonus}) {
-      const winningCount = {
-          [KEYS.FIRST]: 0,
-          [KEYS.SECOND]: 0,
-          [KEYS.THIRD]: 0,
-          [KEYS.FOURTH]: 0,
-          [KEYS.FIFTH]: 0,
-      }
+  winningCount(lottos, { winning, bonus }) {
+    const winningCount = { [KEYS.FIRST]: 0, [KEYS.SECOND]: 0, [KEYS.THIRD]: 0, [KEYS.FOURTH]: 0, [KEYS.FIFTH]: 0 };
 
-      lottos.forEach((lotto) => {
-        const matchCount = lotto.filter((num) => winning.includes(num)).length;
+    lottos.forEach((lotto) => {
+      const matchCount = lotto.filter((num) => winning.includes(num)).length;
 
-        if (matchCount === WINNING[KEYS.FIRST].MATCH) winningCount[KEYS.FIRST] += 1;
-        if (matchCount === WINNING[KEYS.SECOND].MATCH && lotto.includes(bonus)) winningCount[KEYS.SECOND] += 1;
-        if (matchCount === WINNING[KEYS.THIRD].MATCH) winningCount[KEYS.THIRD] += 1;
-        if (matchCount === WINNING[KEYS.FOURTH].MATCH) winningCount[KEYS.FOURTH] += 1;
-        if (matchCount === WINNING[KEYS.FIFTH].MATCH) winningCount[KEYS.FIFTH] += 1;
-      })
+      if (matchCount === WINNING[KEYS.FIRST].MATCH) winningCount[KEYS.FIRST] += 1;
+      if (matchCount === WINNING[KEYS.SECOND].MATCH && lotto.includes(bonus)) winningCount[KEYS.SECOND] += 1;
+      if (matchCount === WINNING[KEYS.THIRD].MATCH) winningCount[KEYS.THIRD] += 1;
+      if (matchCount === WINNING[KEYS.FOURTH].MATCH) winningCount[KEYS.FOURTH] += 1;
+      if (matchCount === WINNING[KEYS.FIFTH].MATCH) winningCount[KEYS.FIFTH] += 1;
+    });
 
-      return winningCount;
-    },
+    return winningCount;
+  },
 
   totalPrize(winningCount) {
-      let total = 0;
+    const total = Object.values(KEYS).reduce((total, key) => {
+      return total + WINNING[key].PRIZES * winningCount[key];
+    }, 0);
 
-      Object.values(KEYS).forEach((key) => {
-        total += WINNING[key].PRIZES * winningCount[key];
-      });
+    return total;
+  },
 
-      return total;
-    },
-
-    yieldRate(amount, totalPrize) {
-      return ((totalPrize / amount ) * 100).toFixed(1);
-    }
-}
+  yieldRate(amount, totalPrize) {
+    return ((totalPrize / amount) * 100).toFixed(1);
+  },
+};
 
 export default Calculator;
