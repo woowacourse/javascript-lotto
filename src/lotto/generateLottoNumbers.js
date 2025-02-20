ae9e9c231af5e6d9273d08a9cbac381990483fb9
wo-o29 <woogur29@gmail.com>
2025-02-21 01:02:27 +0900

    ​refactor: 셔플 로직 유틸 함수로 분리
    Co-authored-by: JeLee-river <106021313+JeLee-river@users.noreply.github.com>

--- src/lotto/generateLottoNumbers.js
+++ src/lotto/generateLottoNumbers.js
@@ -1,21 +1,14 @@
 import { LOTTO_RULE } from "../constants/lotto.js";
+import { shuffle } from "../utils/array.js";
 
-/**
- * 피셔 예이츠 셔플 사용
- * {@link https://ko.wikipedia.org/wiki/%ED%94%BC%EC%85%94-%EC%98%88%EC%9D%B4%EC%B8%A0_%EC%85%94%ED%94%8C}
- */
 const generateLottoNumbers = () => {
   const numbers = Array.from(
     { length: LOTTO_RULE.MAX_LOTTO_NUMBER },
     (_, i) => i + 1
   );
 
-  for (let i = numbers.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-  }
-
-  const lottoNumbers = numbers.slice(0, LOTTO_RULE.LOTTO_LENGTH);
+  const shuffleNumbers = shuffle(numbers);
+  const lottoNumbers = shuffleNumbers.slice(0, LOTTO_RULE.LOTTO_LENGTH);
   return lottoNumbers.sort((a, b) => a - b);
 };
 
--- src/utils/array.test.js
+++ src/utils/array.test.js
@@ -1,4 +1,4 @@
-import { getIntersection } from "./array.js";
+import { getIntersection, shuffle } from "./array.js";
 
 describe("getIntersection 함수 테스트", () => {
   test("두 배열의 공통 원소는 5개이다.", () => {
@@ -12,5 +12,18 @@ describe("getIntersection 함수 테스트", () => {
     expect(() => getIntersection("123", [1, 2, 3, 4, 5, 6])).toThrow(
       "인자가 배열이 아닙니다."
     );
+  });
+});
+
+describe("shuffle 함수 테스트", () => {
+  test("배열의 셔플 전과 후는 다르다", () => {
+    const beforeShuffledNumbers = [1, 2, 3, 4, 5, 6];
+    const afterShuffledNumbers = shuffle(beforeShuffledNumbers);
+
+    expect(beforeShuffledNumbers).not.toEqual(afterShuffledNumbers);
+  });
+
+  test("배열이 아닌 값을 입력하면 에러가 발생한다.", () => {
+    expect(() => shuffle("123")).toThrow("인자가 배열이 아닙니다.");
   });
 });
