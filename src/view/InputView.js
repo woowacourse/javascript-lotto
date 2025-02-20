4b62d480014cba56561dc26018106c3c3fd4202b
hoyyChoi <hoyeon8165@gmail.com>
2025-02-20 14:21:05 +0900

    refactor: inputMessage 상수화
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,19 +1,20 @@
 import Console from "../utils/Console.js";
+import INPUT_MESSAGE from "../constants/INPUT_MESSAGE.js";
 const InputView = {
   async purchaseAmount() {
-    return await Console.readLineAsync(`> 구입금액을 입력해 주세요.`);
+    return await Console.readLineAsync(INPUT_MESSAGE.PURCHASE_AMOUNT);
   },
 
   async winningNumbers() {
-    return await Console.readLineAsync(`> 당첨 번호를 입력해 주세요.`);
+    return await Console.readLineAsync(INPUT_MESSAGE.WINNING_NUMBERS);
   },
 
   async bonusNumber() {
-    return await Console.readLineAsync(`> 보너스 번호를 입력해 주세요.`);
+    return await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
   },
 
   async restart() {
-    return await Console.readLineAsync(`> 다시 시작하시겠습니까? (y/n)`);
+    return await Console.readLineAsync(INPUT_MESSAGE.RESTART);
   },
 };
 
