4e9a163829812418dd43617b3acd128027cb3b0a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 09:40:16 +0900

    feat: 당첨 번호 입력
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -5,7 +5,8 @@ class Controller {
   }
 
   async start() {
-    const money = await readLineAsync('구입금액을 입력해주세요');
+    const money = await readLineAsync('> 구입금액을 입력해 주세요.');
+    const winningNumber = await readLineAsync('> 당첨 번호를 입력해 주세요.');
   }
 }
 
