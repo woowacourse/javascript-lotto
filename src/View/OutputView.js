9b7a7bd1e7db058b7052edde344fceb3c5fc750b
spoyodevelop <kjy9505@gmail.com>
2025-02-20 11:11:25 +0900

    fix: 출력 형식 수정
    Co-authored-by: Db0111 <deokbin@naver.com>

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -15,14 +15,17 @@ export function printLotto(lotto) {
 }
 
 export function printPrizeHeader() {
-  console.log('당첨 통계\n --------------------');
+  console.log('당첨 통계\n--------------------');
 }
 
 export function printPrize(systemSettings) {
   const { prizeMoney, winCount } = systemSettings;
-  console.log(`3개 일치 (${prizeMoney.THREE_MATCH}원) - ${winCount.THREE_MATCH}개\n
-  4개 일치 (${prizeMoney.FOUR_MATCH}원) - ${winCount.FOUR_MATCH}개\n
-  5개 일치 (${prizeMoney.FIVE_MATCH}원) - ${winCount.FIVE_MATCH}개\n
-  5개 일치, 보너스 볼 일치 (${prizeMoney.FIVE_MATCH_WITH_BONUS}원) - ${winCount.FIVE_MATCH_WITH_BONUS}개\n
-  6개 일치 (${prizeMoney.SIX_MATCH}원) - ${winCount.SIX_MATCH}개`);
+
+  console.log(`
+3개 일치 (${prizeMoney.THREE_MATCH}원) - ${winCount.THREE_MATCH}개
+4개 일치 (${prizeMoney.FOUR_MATCH}원) - ${winCount.FOUR_MATCH}개
+5개 일치 (${prizeMoney.FIVE_MATCH}원) - ${winCount.FIVE_MATCH}개
+5개 일치, 보너스 볼 일치 (${prizeMoney.FIVE_MATCH_WITH_BONUS}원) - ${winCount.FIVE_MATCH_WITH_BONUS}개
+6개 일치 (${prizeMoney.SIX_MATCH}원) - ${winCount.SIX_MATCH}개
+  `);
 }
