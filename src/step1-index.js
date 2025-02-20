0e76f2ac7bd89a7ed6b2b9b17ed2aa4d6aa8a4ba
wo-o29 <woogur29@gmail.com>
2025-02-21 01:35:25 +0900

    ​refactor: 메인 로직 추상화
    Co-authored-by: JeLee-river <106021313+JeLee-river@users.noreply.github.com>

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,62 +1,11 @@
-import {
-  calculatePrizeResult,
-  generateLottoNumberSets,
-  getTotalPrizeMoney,
-} from "./lotto/index.js";
-import { retryUntilValidInput } from "./utils/input.js";
-import { getRevenueRate } from "./utils/math.js";
-import {
-  validationBonusNumber,
-  validationLottoPrice,
-  validationRestartInput,
-  validationWinningNumbers,
-} from "./validation/index.js";
-import {
-  readLottoPriceInput,
-  readWinningNumbersInput,
-  readBonusNumberInput,
-  readRestartInput,
-} from "./view/input/index.js";
-import { printLottoNumbers, printLottoResult } from "./view/output/index.js";
 import { LOTTO_RESTART_COMMAND } from "./constants/lotto.js";
+import playLotto from "./lotto/playLotto.js";
 
 const app = async () => {
   while (true) {
-    const lottoPrice = await retryUntilValidInput({
-      readUserInput: readLottoPriceInput,
-      validator: validationLottoPrice,
-    });
+    const restartCommand = await playLotto();
 
-    const lottoNumbers = generateLottoNumberSets(lottoPrice);
-    printLottoNumbers(lottoNumbers);
-
-    const winningNumbers = await retryUntilValidInput({
-      readUserInput: readWinningNumbersInput,
-      validator: validationWinningNumbers,
-    });
-
-    const bonusNumber = await retryUntilValidInput({
-      readUserInput: readBonusNumberInput,
-      validator: (bonusNumber) =>
-        validationBonusNumber(bonusNumber, winningNumbers),
-    });
-
-    const result = calculatePrizeResult(
-      lottoNumbers,
-      winningNumbers,
-      bonusNumber
-    );
-
-    const totalPrizeMoney = getTotalPrizeMoney(result);
-    const revenueRate = getRevenueRate(totalPrizeMoney, lottoPrice);
-    printLottoResult(result, revenueRate);
-
-    const restartInput = await retryUntilValidInput({
-      readUserInput: readRestartInput,
-      validator: validationRestartInput,
-    });
-
-    if (restartInput === LOTTO_RESTART_COMMAND.end) {
+    if (restartCommand === LOTTO_RESTART_COMMAND.end) {
       break;
     }
   }
