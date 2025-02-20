afe2831639425998403998d31c7524e940ad1238
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 09:40:58 +0900

    feat: 보너스 번호를 입력
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -7,6 +7,7 @@ class Controller {
   async start() {
     const money = await readLineAsync('> 구입금액을 입력해 주세요.');
     const winningNumber = await readLineAsync('> 당첨 번호를 입력해 주세요.');
+    const bonus = await readLineAsync('> 보너스 번호를 입력해 주세요.');
   }
 }
 
