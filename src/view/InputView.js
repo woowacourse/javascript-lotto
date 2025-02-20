e36be2b3b304a37a7745264bcb596c756fbe634d
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 20:53:24 +0900

    feat: 예외 발생한 부분부터 입력을 다시 받음
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,32 +1,55 @@
 import readLineAsync from './readLineAsync.js';
 import { INPUT_MESSAGE } from '../constants/message.js';
-import { validateMoney, validateLottoNumber, validateBonus } from '../domain/validation.js';
+import {
+  validateMoney, validateLottoNumber, validateBonus, validateRestart,
+} from '../domain/validation.js';
 import CONFIG from '../constants/config.js';
+import OutputView from './OutputView.js';
 
 const InputView = {
   async readMoney() {
-    const input = await readLineAsync(INPUT_MESSAGE.READ_MONEY);
-    const money = parseInt(input, CONFIG.DECIMAL);
-    validateMoney(money);
-    return money;
+    try {
+      const input = await readLineAsync(INPUT_MESSAGE.READ_MONEY);
+      const money = parseInt(input, CONFIG.DECIMAL);
+      validateMoney(money);
+      return money;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.readMoney();
+    }
   },
   async readWinningLotto() {
-    const input = await readLineAsync(INPUT_MESSAGE.READ_WINNING_LOTTO);
-    const winningLotto = input?.split(',').map((item) => parseInt(item, CONFIG.DECIMAL));
-    validateLottoNumber(winningLotto);
-    return winningLotto;
+    try {
+      const input = await readLineAsync(INPUT_MESSAGE.READ_WINNING_LOTTO);
+      const winningLotto = input?.split(',').map((item) => parseInt(item, CONFIG.DECIMAL));
+      validateLottoNumber(winningLotto);
+      return winningLotto;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.readWinningLotto();
+    }
   },
   async readBonus(winningLotto) {
-    const input = await readLineAsync(INPUT_MESSAGE.READ_BONUS);
-    const bonus = parseInt(input, CONFIG.DECIMAL);
-    validateBonus(bonus, winningLotto);
-    return bonus;
+    try {
+      const input = await readLineAsync(INPUT_MESSAGE.READ_BONUS);
+      const bonus = parseInt(input, CONFIG.DECIMAL);
+      validateBonus(bonus, winningLotto);
+      return bonus;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.readBonus(winningLotto);
+    }
   },
   async readReStart() {
-    const input = await readLineAsync(INPUT_MESSAGE.READ_RESTART);
-    const lowerCaseInput = input.toLowerCase();
-
-    return !(lowerCaseInput === CONFIG.ANSWER_NO) && lowerCaseInput === CONFIG.ANSWER_YES;
+    try {
+      const input = await readLineAsync(INPUT_MESSAGE.READ_RESTART);
+      const lowerCaseInput = input.toLowerCase();
+      validateRestart(lowerCaseInput);
+      return !(lowerCaseInput === CONFIG.ANSWER_NO) && lowerCaseInput === CONFIG.ANSWER_YES;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.readReStart();
+    }
   },
 };
 
