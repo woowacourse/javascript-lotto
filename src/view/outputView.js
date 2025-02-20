346ec376c9a151d548470acaba5cedf4db2104fa
ha-kuku <hakukudh@gmail.com>
2025-02-20 11:23:24 +0900

    feat: 로또 출력 기능 추가 Co-authored-by: jaeyoung-kwon <wodud2443@gmail.com>

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -2,6 +2,12 @@ class OutputView {
   printLottoCount(lottoCount) {
     console.log(`${lottoCount}개를 구매했습니다.`);
   }
+
+  printLotto(lottos) {
+    lottos.forEach((lotto) => {
+      console.log(lotto);
+    });
+  }
 }
 
 export default OutputView;
