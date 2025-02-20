8e194e4d0c8e1ec382e7f6a1fce8bbce6f5819eb
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 09:23:55 +0900

    test: feat: 수익률은 소숫점 둘째짜리에서 반올림
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- __tests__/LottoStatistics.test.js
+++ __tests__/LottoStatistics.test.js
@@ -43,7 +43,15 @@ describe('사용자가 구매한 로또 번호와 보너스 번호를 비교한 
 });
 
 test('사용자가 구매한 로또 번호화 당첨 번호를 비교하여 수익률을 구한다.', () => {
-  const  profit = 5000;
+  const profit = 5000;
   const investmentCost = 8000;
   expect(lottoStatistics.calculateRevenueRate(profit, investmentCost)).toBe(62.5);
 });
+
+test.each([
+  [5000,13000, 38.5],
+  [5000,14000, 35.7],
+  [5000,16000, 31.3],
+])('소숫점 둘째짜리에서 반올림한다.', (profit, investmentCost, result) => {
+  expect(lottoStatistics.calculateRevenueRate(profit, investmentCost)).toBe(result);
+})
