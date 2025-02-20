e4979040b891d592b97ff11950409c2a626fd6bc
hoyyChoi <hoyeon8165@gmail.com>
2025-02-20 11:08:13 +0900

    feat: 당첨번호 및 보너스 번호 입력 메서드 구현
    Co-authored-by: Beomtae <Beomtae@users.noreply.github.com>

--- src/Controller.js
+++ src/Controller.js
@@ -2,6 +2,8 @@ import InputView from "./view/InputView.js";
 class Controller {
   async start() {
     await InputView.purchaseAmount();
+    await InputView.winningNumbers();
+    await InputView.bonusNumber();
   }
 }
 export default Controller;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,6 +3,14 @@ const InputView = {
   async purchaseAmount() {
     await Console.readLineAsync(`> 구입금액을 입력해 주세요.`);
   },
+
+  async winningNumbers() {
+    await Console.readLineAsync(`> 당첨 번호를 입력해 주세요.`);
+  },
+
+  async bonusNumber() {
+    await Console.readLineAsync(`> 보너스 번호를 입력해 주세요.`);
+  },
 };
 
 export default InputView;
