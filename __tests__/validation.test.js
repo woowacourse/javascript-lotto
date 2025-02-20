871deba32c0b05d6e080b44cfac32eb8216bf891
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 14:50:09 +0900

    test: 보너스 번호와 당첨번호는 중복된 숫자가 있으면 안되는 유효성 검사
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -51,11 +51,20 @@ describe('로또 숫자',() => {
 });
 
 describe('보너스 숫자', () => {
+    const winningLotto = [1,2,3,4,5,6];
+  
   test.each([
     [0], [46],
   ])('보너스 번호의 숫자 범위는 1 미만 혹은 45 초과하면 에러가 발생한다.', (bonus) => {
     // then
-    expect(() => validateBonus(bonus))
+    expect(() => validateBonus(bonus, winningLotto))
       .toThrow('[ERROR] 보너스 번호의 숫자 범위 1 ~ 45이다.');
   });
+
+  test('보너스 번호는 당첨 로또의 있는 숫자와 중복되면 안된다.', () => {
+    const bonus = 6;
+
+    expect(() => validateBonus(bonus, winningLotto))
+      .toThrow('[ERROR] 보너스 번호는 당첨 로또의 있는 숫자와 중복되면 안된다.');
+  })
 })
