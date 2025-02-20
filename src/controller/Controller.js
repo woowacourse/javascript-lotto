72fdc0af6ce0380ebe3a109cfc3ad6b333c77afe
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-20 09:48:14 +0900

    feat: 재시작/종료 여부를 입력
    Co-Authored-By: seo_dev <72060681+jin123457@users.noreply.github.com>

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -5,9 +5,16 @@ class Controller {
   }
 
   async start() {
-    const money = await readLineAsync('> 구입금액을 입력해 주세요.');
-    const winningNumber = await readLineAsync('> 당첨 번호를 입력해 주세요.');
-    const bonus = await readLineAsync('> 보너스 번호를 입력해 주세요.');
+    let condition = true;
+    while (condition) {
+      const money = await readLineAsync('> 구입금액을 입력해 주세요.');
+      const winningNumber = await readLineAsync('> 당첨 번호를 입력해 주세요.');
+      const bonus = await readLineAsync('> 보너스 번호를 입력해 주세요.');
+      const reStart = await readLineAsync('> 다시 시작하시겠습니까? (y/n)');
+      if (reStart.toLowerCase() === 'n') {
+        condition = false;
+      }
+    }
   }
 }
 
