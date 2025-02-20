83194d3b08e96bc0d11549f17a6b2dccf0136a8c
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-20 14:06:32 +0900

    feat(index): 재시작 로직 적용

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,15 +1,35 @@
 import { INPUT } from './constants/messages.js';
+import { calculateRevenue } from './domain/calculateRevenue.js';
+import { getRandomLottos } from './domain/getRandomLottos.js';
+import { getWinningMatchCount } from './domain/getWinningMatchCount.js';
+import { arrayToString } from './utils/arrayToString.js';
+import { checkReplay } from './utils/checkReplay.js';
 import { parseWinningNumbers } from './utils/parseString.js';
-import { validateBonusNumber, validatePurchasePrice, validateWinningNumbers } from './utils/validaition.js';
+import { isYesOrNo } from './validation/validateInput.js';
+import { validateBonusNumber, validateWinningNumbers } from './validation/validateLottoNumbers.js';
+import { validatePurchasePrice } from './validation/validatePurchasePrice.js';
 import handleUserInput from './view/handleUserInput.js';
+import { printPurchasedQuantity, printRandomLottos, printStatistics } from './view/output.js';
 
 async function run() {
-  const purchasePrice = await handleUserInput(INPUT.PURCHASE_PRICE, validatePurchasePrice);
+  const purchasePrice = Number(await handleUserInput(INPUT.PURCHASE_PRICE, validatePurchasePrice));
+  const randomlottos = getRandomLottos(purchasePrice / 1000);
+  printPurchasedQuantity(purchasePrice / 1000);
+  printRandomLottos(arrayToString(randomlottos));
 
   const stringOfWinningNumbers = await handleUserInput(INPUT.WINNING_NUMBERS, validateWinningNumbers);
   const winningNumbers = parseWinningNumbers(stringOfWinningNumbers);
 
   const bonusNumber = await handleUserInput(INPUT.BONUS_NUMBER, validateBonusNumber, winningNumbers);
+  const lottoNumbers = { winningNumbers, bonusNumber };
+
+  const matchCounts = getWinningMatchCount(randomlottos, lottoNumbers);
+  const revenue = calculateRevenue(matchCounts, purchasePrice);
+
+  printStatistics(matchCounts, revenue);
+
+  const yesOrNo = await handleUserInput(INPUT.REPLAY_GAME, isYesOrNo);
+  await checkReplay(yesOrNo, run);
 }
 
 run();
