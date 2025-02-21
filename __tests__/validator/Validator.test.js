4037a20afbbbc3b11cc099f6ef042383fb414cb0
Ko Daeun <drechsu@gmail.com>
2025-02-22 02:02:38 +0900

    [TEST] : 입력값 범위 테스트를 경계값을 기준으로 테스트 하도록 변경

--- __tests__/validator/Validator.test.js
+++ __tests__/validator/Validator.test.js
@@ -20,14 +20,26 @@ describe("입력 값 유효성 테스트를 진행한다.", () => {
     expect(() => validator.number(input)).toThrow(ERROR.NOT_NUMBER);
   });
 
-  test("범위를 벗어난 입력을 할 경우 오류를 발생시킨다.", () => {
-    // given
-    const input = 0;
-    // when
-    const validator = new Validator();
-    // then
-    expect(() => validator.range({ min: 1, max: 45 }, input)).toThrow(
-      ERROR.INVALID_RANGE
-    );
+  describe("범위를 벗어난 입력을 할 경우 오류를 발생시킨다.", () => {
+    test("1보다 작은 값을 입력하면 오류를 발생시킨다.", () => {
+      // given
+      const input = 0;
+      // when
+      const validator = new Validator();
+      // then
+      expect(() => validator.range({ min: 1, max: 45 }, input)).toThrow(
+        ERROR.INVALID_RANGE
+      );
+    });
+    test("45보다 큰 값을 입력하면 오류를 발생시킨다.", () => {
+      // given
+      const input = 46;
+      // when
+      const validator = new Validator();
+      // then
+      expect(() => validator.range({ min: 1, max: 45 }, input)).toThrow(
+        ERROR.INVALID_RANGE
+      );
+    });
   });
 });
