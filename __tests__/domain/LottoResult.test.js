349a9b6b4b45fd66b7a1340c775f8ed79a2476ff
Ko Daeun <drechsu@gmail.com>
2025-02-22 01:42:00 +0900

    [REFACTOR] : LottoResult test에 describe 추가

--- __tests__/domain/LottoResult.test.js
+++ __tests__/domain/LottoResult.test.js
@@ -1,46 +1,48 @@
 import LottoResult from "../../src/domain/LottoResult.js";
 
-test("발행된 로또의 당첨 내역을 계산한다.", () => {
-  //given
-  const lottoStatus = [
-    { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
-    { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
-  ];
-  const price = 1000;
-  const lottoResult = new LottoResult(lottoStatus, price);
+describe("LottoResult 클래스 테스트", () => {
+  test("발행된 로또의 당첨 내역을 계산한다.", () => {
+    //given
+    const lottoStatus = [
+      { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
+      { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
+    ];
+    const price = 1000;
+    const lottoResult = new LottoResult(lottoStatus, price);
 
-  //when
-  const result = lottoResult.getWinningHistory();
+    //when
+    const result = lottoResult.getWinningHistory();
 
-  //then
-  expect(result).toEqual({ 1: 0, 2: 1, 3: 0, 4: 1, 5: 0 });
-});
+    //then
+    expect(result).toEqual({ 1: 0, 2: 1, 3: 0, 4: 1, 5: 0 });
+  });
 
-test("당첨 내역을 기반으로 총 수입을 구한다.", () => {
-  //given
-  const lottoStatus = [
-    { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
-    { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
-  ];
-  const price = 1000;
-  const lottoResult = new LottoResult(lottoStatus, price);
+  test("당첨 내역을 기반으로 총 수입을 구한다.", () => {
+    //given
+    const lottoStatus = [
+      { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
+      { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
+    ];
+    const price = 1000;
+    const lottoResult = new LottoResult(lottoStatus, price);
 
-  //when
-  const profit = lottoResult.getTotalProfit();
+    //when
+    const profit = lottoResult.getTotalProfit();
 
-  //then
-  expect(profit).toEqual(30_050_000);
-});
+    //then
+    expect(profit).toEqual(30_050_000);
+  });
 
-test("당첨된 내역을 기반으로 수익률을 구한다.", () => {
-  //given
-  const lottoStatus = [{ RANK: 5, COUNT: 3, REWORD: 5000, IS_BONUS: false }];
-  const price = 8000;
-  const lottoResult = new LottoResult(lottoStatus, price);
+  test("당첨된 내역을 기반으로 수익률을 구한다.", () => {
+    //given
+    const lottoStatus = [{ RANK: 5, COUNT: 3, REWORD: 5000, IS_BONUS: false }];
+    const price = 8000;
+    const lottoResult = new LottoResult(lottoStatus, price);
 
-  //when
-  const rate = lottoResult.getRate();
+    //when
+    const rate = lottoResult.getRate();
 
-  //then
-  expect(rate).toEqual("62.5");
+    //then
+    expect(rate).toEqual("62.5");
+  });
 });
