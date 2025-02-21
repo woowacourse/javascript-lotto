977783d8dd6c4adb413942891aee175bae25aa24
Jeongeun Lee <leeju1009@gmail.com>
2025-02-22 01:46:34 +0900

    refactor: 상수를 활용하고, 다른 예외 처리 테스트와 형식이 통일되도록 테스트 설명 수정

--- src/validation/validationLottoPrice.test.js
+++ src/validation/validationLottoPrice.test.js
@@ -1,37 +1,38 @@
 import validationLottoPrice from "./validationLottoPrice.js";
 import CustomError from "../CustomError.js";
 import { ERROR_MESSAGE } from "../constants/message.js";
+import { LOTTO_RULE } from "../constants/lotto.js";
 
 describe("validationLottoPrice 유효성 검사", () => {
-  test("1000.1원은 정수가 아니다.", () => {
+  test("로또 구입 금액이 정수가 아니면 에러가 발생한다.", () => {
     const price = 1000.1;
     expect(() => validationLottoPrice(price)).toThrow(
       new CustomError(ERROR_MESSAGE.INVALID_INTEGER)
     );
   });
 
-  test("5001원은 1000원 단위로 나눠지지 않는다.", () => {
+  test(`로또 구입 금액이 ${LOTTO_RULE.MIN_PRICE}원 단위로 나눠지지 않으면 에러가 발생한다.`, () => {
     const price = 5001;
     expect(() => validationLottoPrice(price)).toThrow(
       new CustomError(ERROR_MESSAGE.INVALID_MULTIPLE_OF_THOUSAND)
     );
   });
 
-  test("999원은 최소 구입 금액(1000원)보다 작다.", () => {
+  test(`로또 구입 금액이 최소 단위인 ${LOTTO_RULE.MIN_PRICE}원 보다 작으면 에러가 발생한다.`, () => {
     const price = 999;
     expect(() => validationLottoPrice(price)).toThrow(
       new CustomError(ERROR_MESSAGE.INVALID_MIN_PRICE)
     );
   });
 
-  test("101,000원은 최대 구입 금액(10만원)을 초과한다.", () => {
+  test(`로또 구입 금액이 최대 구입 가능 금액인 ${LOTTO_RULE.MAX_PRICE}원을 초과하면 에러가 발생한다.`, () => {
     const price = 101_000;
     expect(() => validationLottoPrice(price)).toThrow(
       new CustomError(ERROR_MESSAGE.INVALID_OVER_MAX_PRICE)
     );
   });
 
-  test("1000원은 로또 구입 금액 유효성 검사에 통과한다.", () => {
+  test("로또 구입 금액이 1,000원이면 유효성 검사에서 에러가 발생하지 않는다.", () => {
     const price = 1_000;
     expect(() => validationLottoPrice(price)).not.toThrow();
   });
