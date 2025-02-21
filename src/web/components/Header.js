e3723149acb1288c85ee9eb51f9847ad28b806cf
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-21 16:39:13 +0900

    feat(header): 헤더 컴포넌트 UI 구현

---
+++ src/web/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  const navBar = document.createElement('header');
+  const title = document.createElement('h1');
+
+  title.innerText = '🎱행운의 로또';
+
+  navBar.appendChild(title);
+  return navBar;
+}
