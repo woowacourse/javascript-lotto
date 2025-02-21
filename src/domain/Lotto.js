e1975db704fed759198916ade97cac6d9008fbdc
Ko Daeun <drechsu@gmail.com>
2025-02-22 02:05:41 +0900

    [REFACOR] : Lotto 클래스의 number 필드를 private으로 변경

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,9 +1,10 @@
 import LottoValidator from "../validation/LottoValidator.js";
 
 class Lotto {
+  #numbers;
   constructor(numbers) {
-    this.numbers = this.sortLottoNumber(numbers);
-    this.validate(this.numbers);
+    this.#numbers = this.sortLottoNumber(numbers);
+    this.validate(this.#numbers);
   }
 
   validate(numbers) {
@@ -16,16 +17,16 @@ class Lotto {
   }
 
   getLottoNumbers() {
-    return this.numbers;
+    return this.#numbers;
   }
 
   getSameNumbers(givenLottoNumber) {
-    return this.numbers.filter((number) => givenLottoNumber.includes(number))
+    return this.#numbers.filter((number) => givenLottoNumber.includes(number))
       .length;
   }
 
   hasBonusNumber(bonusNumber) {
-    return this.numbers.includes(bonusNumber);
+    return this.#numbers.includes(bonusNumber);
   }
 }
 
