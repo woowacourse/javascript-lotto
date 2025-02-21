ef47ce403e3c06eb4bba24431e69d9c2490d489f
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-21 20:10:49 +0900

    feat(app): header,main,footer를 app 태그에 삽입

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,10 +1,10 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- */
-
-import Header from './web/components/Header.js';
-import './web/css/index.css';
+import './web/index.css';
+import Header from './web/header/Header';
+import Main from './web/main/Main';
+import Footer from './web/footer/Footer';
 
 const app = document.getElementById('app');
 
 app.appendChild(Header());
+app.appendChild(Main());
+app.appendChild(Footer());
