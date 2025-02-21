6932dab131e39d9797582353e43b03f4b4650632
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-21 17:35:12 +0900

    refactor: validate test 수정
    - 상수 적용 가능한 부분 적용
    - 변수 이름을 테스트 케이스에 맞게 수정

--- __tests__/validate.test.js
+++ __tests__/validate.test.js
@@ -1,4 +1,5 @@
 import ERROR_MESSAGE from "../src/constant/error.js";
+import RESTART_ANSWER from "../src/constant/answer.js";
 import { LOTTO } from "../src/constant/lotto.js";
 import {
   validateBonusNumber,
@@ -10,7 +11,7 @@ describe("validate", () => {
   describe("restart", () => {
     describe("예외 케이스", () => {
       test.each([["우디"], ["k"], ["에리얼"], ["오거스"], [1], [""]])(
-        "y or n이 아니면 에러가 발생한다.",
+        `"${RESTART_ANSWER.YES} or ${RESTART_ANSWER.NO}이 아니면 에러가 발생한다."`,
         (input) => {
           expect(() => validateRestart(input)).toThrow();
         }
@@ -22,10 +23,10 @@ describe("validate", () => {
     describe("예외 케이스", () => {
       test(`숫자가 아닌 값인 경우`, () => {
         const numbers = [1, 2, 3, 4, 5, 6];
-        const number = "에리얼";
+        const notNumber = "에리얼";
 
         expect(() =>
-          validateBonusNumber(number, numbers).toThrow(
+          validateBonusNumber(notNumber, numbers).toThrow(
             ERROR_MESSAGE.NOT_A_NUMBER
           )
         );
@@ -33,18 +34,18 @@ describe("validate", () => {
 
       test(`${LOTTO.MIN_RANDOM_VALUE}~${LOTTO.MAX_RANDOM_VALUE} 사이의 숫자가 아닌 경우`, () => {
         const numbers = [1, 2, 3, 4, 5, 6];
-        const number = "46";
+        const outOfRangeNumber = `${LOTTO.MAX_RANDOM_VALUE + 1}`;
 
-        expect(() => validateBonusNumber(number, numbers)).toThrow(
+        expect(() => validateBonusNumber(outOfRangeNumber, numbers)).toThrow(
           ERROR_MESSAGE.NUMBER_OUT_OF_RANGE
         );
       });
 
       test(`중복된 번호가 있는 경우`, () => {
         const numbers = [1, 2, 3, 4, 5, 6];
-        const number = "6";
+        const duplicateNumber = "6";
 
-        expect(() => validateBonusNumber(number, numbers)).toThrow(
+        expect(() => validateBonusNumber(duplicateNumber, numbers)).toThrow(
           ERROR_MESSAGE.DUPLICATE_BONUS_NUMBER
         );
       });
@@ -54,33 +55,33 @@ describe("validate", () => {
   describe("Lotto ", () => {
     describe("예외 케이스", () => {
       test(`당첨 번호의 길이는 ${LOTTO.LENGTH}이다`, () => {
-        const numbers = "1,2,3,4,5";
+        const shortNumbers = "1,2,3,4,5";
 
-        expect(() => validateWinningNumbers(numbers)).toThrow(
+        expect(() => validateWinningNumbers(shortNumbers)).toThrow(
           ERROR_MESSAGE.LOTTO_LENGTH
         );
       });
 
       test(`중복되는 번호가 있는 경우`, () => {
-        const numbers = "1,2,3,4,5,5";
+        const duplicateNumbers = "1,2,3,4,5,5";
 
-        expect(() => validateWinningNumbers(numbers)).toThrow(
+        expect(() => validateWinningNumbers(duplicateNumbers)).toThrow(
           ERROR_MESSAGE.DUPLICATE_WINNING_NUMBER
         );
       });
 
-      test(`1~45 사이의 숫자가 아닌 값이 포함되어 있는 경우`, () => {
-        const numbers = "1,2,3,4,5,66";
+      test(`${LOTTO.MIN_RANDOM_VALUE}~${LOTTO.MAX_RANDOM_VALUE}사이의 숫자가 아닌 값이 포함되어 있는 경우`, () => {
+        const outOfRangeNumbers = "1,2,3,4,5,66";
 
-        expect(() => validateWinningNumbers(numbers)).toThrow(
+        expect(() => validateWinningNumbers(outOfRangeNumbers)).toThrow(
           ERROR_MESSAGE.NUMBER_OUT_OF_RANGE
         );
       });
 
       test(`숫자가 아닌 값이 포함되어 있는 경우`, () => {
-        const numbers = "1,a,3,4,5,6";
+        const numbersWithNonNumber = "1,a,3,4,5,6";
 
-        expect(() => validateWinningNumbers(numbers)).toThrow(
+        expect(() => validateWinningNumbers(numbersWithNonNumber)).toThrow(
           ERROR_MESSAGE.NOT_A_NUMBER
         );
       });
