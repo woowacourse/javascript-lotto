956dd022d3d382d82c240419a9e29bc14c1e9c91
H0ngJu <hongju4385@gmail.com>
2025-02-21 16:23:02 +0900

    style(lotto): 로또 최대, 최소 숫자 상수명 변경

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,5 +1,5 @@
 import { LOTTO } from '../constants/messages.js';
 
 export const getRandomNumber = () => {
-  return Math.floor(Math.random() * LOTTO.MAX_RANDOM_NUMBER) + LOTTO.MIN_RANDOM_NUMBER;
+  return Math.floor(Math.random() * LOTTO.MAX_LOTTO_NUMBER) + LOTTO.MIN_LOTTO_NUMBER;
 };
--- src/validation/validateLottoNumbers.js
+++ src/validation/validateLottoNumbers.js
@@ -40,7 +40,7 @@ export const validateBonusNumber = (input, winningNumbers) => {
 };
 
 export const checkRangeOfLottoNumber = (input) => {
-  if (input < LOTTO.MIN_RANDOM_NUMBER || input > LOTTO.MAX_RANDOM_NUMBER) {
+  if (input < LOTTO.MIN_LOTTO_NUMBER || input > LOTTO.MAX_LOTTO_NUMBER) {
     throw new Error(ERROR.NOT_RANGE_OF_WINNING_NUMBER);
   }
 };
