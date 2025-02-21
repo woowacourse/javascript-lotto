12e31e86e3b98bc8856855fc13526c6eb0a49e96
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-21 20:10:05 +0900

    refactor(header): 헤더 변수 네이밍 변경

--- src/web/header/Header.js
+++ src/web/header/Header.js
@@ -1,11 +1,11 @@
 import './header.css';
 
 export default function Header() {
-  const navBar = document.createElement('header');
+  const header = document.createElement('header');
   const title = document.createElement('h1');
 
   title.innerText = '🎱행운의 로또';
 
-  navBar.appendChild(title);
-  return navBar;
+  header.appendChild(title);
+  return header;
 }
