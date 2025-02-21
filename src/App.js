f23da81a93016c9dfe6388fed2025405c2c12d84
yeji0214 <1225yj50@naver.com>
2025-02-21 15:35:00 +0900

    refactor: App 별도의 파일로 분리

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import purchase from "./LottoStore.js";
+import InputHandler from "./util/InputHandler.js";
+
+class App {
+  #running;
+
+  constructor() {
+    this.#running = true;
+  }
+
+  async start() {
+    while (this.#running) {
+      await purchase();
+
+      const answer = await this.getRestartAnswer();
+      this.endGame(answer);
+    }
+  }
+
+  async getRestartAnswer() {
+    const answer = await InputHandler.getRestartAnswer();
+
+    return answer;
+  }
+
+  endGame(answer) {
+    if (answer.toLowerCase() === "n") this.#running = false;
+  }
+}
+
+export default App;
--- src/step1-index.js
+++ src/step1-index.js
@@ -1,34 +1,4 @@
-import purchase from "./LottoStore.js";
-import InputHandler from "./util/InputHandler.js";
-
-class App {
-  #running;
-
-  constructor() {
-    this.#running = true;
-  }
-
-  async start() {
-    while (this.#running) {
-      await purchase();
-
-      const answer = await this.getRestartAnswer();
-      this.endGame(answer);
-    }
-  }
-
-  async getRestartAnswer() {
-    const answer = await InputHandler.getRestartAnswer();
-
-    return answer;
-  }
-
-  endGame(answer) {
-    if (answer.toLowerCase() === "n") this.#running = false;
-  }
-}
-
-export default App;
+import App from "./App.js";
 
 const app = new App();
 app.start();
