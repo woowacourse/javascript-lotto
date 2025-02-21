dc3327fc3bf59c6c6745a23b1ab40f9335ff81b1
Ko Daeun <drechsu@gmail.com>
2025-02-22 01:48:25 +0900

    [TEST] : 당첨 내역이 없을 경우의 총수입을 구하는 테스트 추가

--- __tests__/domain/LottoResult.test.js
+++ __tests__/domain/LottoResult.test.js
@@ -17,20 +17,35 @@ describe("LottoResult 클래스 테스트", () => {
     expect(result).toEqual({ 1: 0, 2: 1, 3: 0, 4: 1, 5: 0 });
   });
 
-  test("당첨 내역을 기반으로 총 수입을 구한다.", () => {
-    //given
-    const lottoStatus = [
-      { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
-      { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
-    ];
-    const price = 1000;
-    const lottoResult = new LottoResult(lottoStatus, price);
+  describe("당첨된 내역을 기반으로 총 수입을 구한다.", () => {
+    test("당첨 내역이 있을 경우", () => {
+      //given
+      const lottoStatus = [
+        { RANK: 2, COUNT: 5, REWORD: 30_000_000, IS_BONUS: true },
+        { RANK: 4, COUNT: 4, REWORD: 50_000, IS_BONUS: false },
+      ];
+      const price = 1000;
+      const lottoResult = new LottoResult(lottoStatus, price);
 
-    //when
-    const profit = lottoResult.getTotalProfit();
+      //when
+      const profit = lottoResult.getTotalProfit();
 
-    //then
-    expect(profit).toEqual(30_050_000);
+      //then
+      expect(profit).toEqual(30_050_000);
+    });
+
+    test("당첨 내역이 없을 경우", () => {
+      //given
+      const lottoStatus = [];
+      const price = 1000;
+      const lottoResult = new LottoResult(lottoStatus, price);
+
+      //when
+      const profit = lottoResult.getTotalProfit();
+
+      //then
+      expect(profit).toEqual(0);
+    });
   });
 
   test("당첨된 내역을 기반으로 수익률을 구한다.", () => {
