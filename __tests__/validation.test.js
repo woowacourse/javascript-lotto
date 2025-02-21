cfb5f9807f619b83f76c0adcdb972c9735e90891
spoyodevelop <kjy9505@gmail.com>
2025-02-21 17:24:22 +0900

    refactor: 외부와 상호 작용이 없는 함수 은닉

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,49 +1,49 @@
-import isNumber from '../src/Validation/validateNumber.js';
+import _isNumber from '../src/Validation/validateNumber.js';
 import ERROR_MESSAGE from '../src/Validation/validateNumber.js';
-import isInteger from '../src/Validation/validateNumber.js';
-import isPositive from '../src/Validation/validateNumber.js';
+import _isInteger from '../src/Validation/validateNumber.js';
+import _isPositive from '../src/Validation/validateNumber.js';
 
 describe('인풋 validation 테스트', () => {
-  describe('isNumber Validation test', () => {
+  describe('_isNumber Validation test', () => {
     it(`isNumber는 숫자가 아닐때 ${ERROR_MESSAGE.notANumber}에러를 던져야 한다.`, () => {
       const testCase = 'a';
       expect(() => {
-        isNumber(testCase);
+        _isNumber(testCase);
       }).toThrow(ERROR_MESSAGE.notANumber);
     });
     it('isNumber는 숫자일 때 그 값을 그대로 반환한다.', () => {
       const testCase = '1';
-      expect(isNumber(testCase)).toBe(1);
+      expect(_isNumber(testCase)).toBe(1);
     });
   });
-  describe('isInteger Validation test', () => {
+  describe('_isInteger Validation test', () => {
     it(`isInteger는 정수가 아닐 때 ${ERROR_MESSAGE.notInteger}에러를 던져야 한다.`, () => {
       const testCase = '1.5';
       expect(() => {
-        isInteger(testCase);
+        _isInteger(testCase);
       }).toThrow(ERROR_MESSAGE.notInteger);
     });
 
     it('isInteger는 정수일 때 그 값을 그대로 반환한다.', () => {
       const testCase = '1';
-      expect(isInteger(testCase)).toBe(1);
+      expect(_isInteger(testCase)).toBe(1);
     });
   });
-  describe('isPositive Validation test', () => {
-    it(`isPositive는 양수가 아닐 때 ${ERROR_MESSAGE.notPositive}에러를 던져야 한다.`, () => {
+  describe('_isPositive Validation test', () => {
+    it(`_isPositive는 양수가 아닐 때 ${ERROR_MESSAGE.notPositive}에러를 던져야 한다.`, () => {
       const testCase = '-1';
       expect(() => {
-        isPositive(testCase);
+        _isPositive(testCase);
       }).toThrow(ERROR_MESSAGE.notPositive);
     });
-    it('isPositive는 양수일 때 그 값을 그대로 반환한다.', () => {
+    it('_isPositive는 양수일 때 그 값을 그대로 반환한다.', () => {
       const testCase = '1';
-      expect(isPositive(testCase)).toBe(1);
+      expect(_isPositive(testCase)).toBe(1);
     });
-    it(`isPositive는 0일 때 ${ERROR_MESSAGE.notPositive}에러를 던져야 한다.`, () => {
+    it(`_isPositive는 0일 때 ${ERROR_MESSAGE.notPositive}에러를 던져야 한다.`, () => {
       const testCase = '0';
       expect(() => {
-        isPositive(testCase);
+        _isPositive(testCase);
       }).toThrow(ERROR_MESSAGE.notPositive);
     });
   });
--- src/Validation/validateNumber.js
+++ src/Validation/validateNumber.js
@@ -1,19 +1,19 @@
 import ERROR_MESSAGE from '../settings/ErrorMessage.js';
 
 export default function validateNumber(input) {
-  return isPositive(isInteger(isNumber(input)));
+  return _isPositive(_isInteger(_isNumber(input)));
 }
-export function isNumber(input) {
+export function _isNumber(input) {
   if (Number.isNaN(Number(input))) throw new Error(ERROR_MESSAGE.notANumber);
   return Number(input);
 }
 
-export function isInteger(input) {
+export function _isInteger(input) {
   if (!Number.isInteger(input)) throw new Error(ERROR_MESSAGE.notInteger);
   return input;
 }
 
-export function isPositive(input) {
+export function _isPositive(input) {
   if (input <= 0) throw new Error(ERROR_MESSAGE.notPositive);
   return input;
 }
