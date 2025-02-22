99ac359fc240e054b57107dad418fc30fb291cc1
deokbin <deokbin@naver.com>
2025-02-22 09:08:08 +0900

    refactor(lottoPurchase): 로또 구입 금액 테스트 경계값으로 변경 및 테스트 그룹화

--- __tests__/lottoPurchase.test.js
+++ __tests__/lottoPurchase.test.js
@@ -1,15 +1,35 @@
 import validateLottoPurchase from '../src/Validation/validateLottoPurchase.js';
 import ERROR_MESSAGE from '../src/settings/ErrorMessage.js';
 
-it('로또 구입 최소 금액은 1000원 미만일때 에러를 던진다.', () => {
-  const testCase = 900;
-  expect(() => {
-    validateLottoPurchase(testCase);
-  }).toThrow(ERROR_MESSAGE.notEnoughMoney);
-});
-it('로또 구입 금액은 1000원 단위가 아닐때 에러를 던진다.', () => {
-  const testCase = 1001;
-  expect(() => {
-    validateLottoPurchase(testCase);
-  }).toThrow(ERROR_MESSAGE.notANote);
+describe('로또 구입 금액 유효성 테스트', () => {
+  describe('로또 구입 최소 금액 테스트', () => {
+    it('로또 구입 최소 금액은 1000원 미만일때 에러를 던진다.', () => {
+      const testCase = 999;
+      expect(() => {
+        validateLottoPurchase(testCase);
+      }).toThrow(ERROR_MESSAGE.notEnoughMoney);
+    });
+    it('로또 구입 최소 금액이 1000원 이상일때 에러를 던지지 않는다.', () => {
+      const testCase = 1000;
+      expect(() => {
+        validateLottoPurchase(testCase);
+      }).not.toThrow();
+    });
+
+    describe('로또 구입 금액 1000원 단위인지 확인하는 테스트', () => {
+      it('로또 구입 금액은 1000원 단위가 아닐때 에러를 던진다.', () => {
+        const testCase = 1001;
+        expect(() => {
+          validateLottoPurchase(testCase);
+        }).toThrow(ERROR_MESSAGE.notANote);
+      });
+
+      it('로또 구입 금액은 1000원 단위일 때 에러를 던지지 않는다.', () => {
+        const testCase = 2000;
+        expect(() => {
+          validateLottoPurchase(testCase);
+        }).not.toThrow();
+      });
+    });
+  });
 });
