a22f5fa037f5c75a28dab3a4cc15b826465b0629
hoyyChoi <hoyeon8165@gmail.com>
2025-02-22 17:00:29 +0900

    fix: 랜덤번호 생성 함수 수정 (중복번호 재귀처리)

--- src/domain/createSixRandomNumbers.js
+++ src/domain/createSixRandomNumbers.js
@@ -2,7 +2,7 @@ import pushRandomNumbers from "../utils/pushRandomNumbers.js";
 const createSixRandomNumbers = () => {
   const initialRandomNumbers = [];
   const randomNumbers = pushRandomNumbers(initialRandomNumbers);
-  if (new Set(randomNumbers).size !== randomNumbers.length) createSixRandomNumbers();
+  if (new Set(randomNumbers).size !== randomNumbers.length) return createSixRandomNumbers();
 
   return randomNumbers;
 };
