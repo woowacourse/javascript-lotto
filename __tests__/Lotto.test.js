1c1f311df6ec6c39f96cffd15d9576d27eb75ca1
aydenote <dslgpgh@gmail.com>
2025-02-22 17:27:43 +0900

    refactor: 불필요한 테스트 케이스 삭제

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,15 +1,8 @@
 import { LOTTO_NUMBER_LENGTH } from '../src/constants/common.js';
 import Lotto from '../src/Model/Lotto.js';
 
-describe('로또 ', () => {
-  test('6자리 숫자로 로또를 만든다', () => {
-    const lottoNumbers = Array.from({ length: LOTTO_NUMBER_LENGTH }, (_, i) => i + 1);
-    const lotto = new Lotto(lottoNumbers);
-
-    expect(lotto).toBeDefined();
-  });
-
-  test('로또 번호 숫자 오름차순 정렬 테스트', () => {
+describe('로또', () => {
+  test('6자리 정렬되지 않은 숫자 배열을 오름차순으로 정렬한다.', () => {
     const lottoNumbers = Array.from({ length: LOTTO_NUMBER_LENGTH }, (_, i) => LOTTO_NUMBER_LENGTH - i);
     const lotto = new Lotto(lottoNumbers);
 
