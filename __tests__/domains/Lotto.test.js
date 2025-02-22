29305720eee60f9050c4ab77f8b320491118c2a5
suhwa <tnghk9611@naver.com>
2025-02-22 14:17:38 +0900

    refactor: 반복되는 테스트 케이스를 하나의 each 메서드로 묶어서 검증

--- __tests__/domains/Lotto.test.js
+++ __tests__/domains/Lotto.test.js
@@ -22,24 +22,29 @@ describe('로또 클래스 테스트', () => {
   });
 
   describe('예외 케이스', () => {
-    test('로또 번호가 숫자가 아니면 에러가 발생한다.', () => {
-      const numbers = [null, 1, 2, 3, 4, 5];
-      expect(() => new Lotto(numbers)).toThrow(ERROR_MESSAGE.LOTTO.INVALID_TYPE);
-    });
-
-    test('로또 번호가 6개가 아니면 에러가 발생한다.', () => {
-      const numbers = [1, 2, 3, 4, 5];
-      expect(() => new Lotto(numbers)).toThrow(ERROR_MESSAGE.LOTTO.INVALID_LENGTH);
-    });
-
-    test('로또 번호의 범위가 1~45 사이가 아니면 에러가 발생한다.', () => {
-      const numbers = [0, 2, 3, 4, 5, 46];
-      expect(() => new Lotto(numbers)).toThrow(ERROR_MESSAGE.LOTTO.INVALID_RANGE);
-    });
-
-    test('로또 번호가 중복되면 에러가 발생한다.', () => {
-      const numbers = [1, 2, 3, 4, 5, 5];
-      expect(() => new Lotto(numbers)).toThrow(ERROR_MESSAGE.LOTTO.DUPLICATE);
+    test.each([
+      {
+        description: '로또 번호가 숫자가 아니면',
+        numbers: [null, 1, 2, 3, 4, 5],
+        expectedError: ERROR_MESSAGE.LOTTO.INVALID_TYPE,
+      },
+      {
+        description: '로또 번호가 6개가 아니면',
+        numbers: [1, 2, 3, 4, 5],
+        expectedError: ERROR_MESSAGE.LOTTO.INVALID_LENGTH,
+      },
+      {
+        description: '로또 번호의 범위가 1~45 사이가 아니면',
+        numbers: [0, 2, 3, 4, 5, 46],
+        expectedError: ERROR_MESSAGE.LOTTO.INVALID_RANGE,
+      },
+      {
+        description: '로또 번호가 중복되면',
+        numbers: [1, 2, 3, 4, 5, 5],
+        expectedError: ERROR_MESSAGE.LOTTO.DUPLICATE,
+      },
+    ])('$description 에러가 발생한다', ({ numbers, expectedError }) => {
+      expect(() => new Lotto(numbers)).toThrow(expectedError);
     });
   });
 });
