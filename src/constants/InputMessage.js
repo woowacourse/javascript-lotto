687aaf1816f2f0c610609cc6881a4c2ffd002d37
deokbin <deokbin@naver.com>
2025-02-22 12:52:38 +0900

    refactor(InputMessage, Parsingservice): 사용자 입력 시 메시지 분리

---
+++ src/constants/InputMessage.js
@@ -0,0 +1,8 @@
+const INPUT_MESSAGE = Object.freeze({
+  getPurchasePrice: '> 구입금액을 입력해 주세요.',
+  getWinningNumber: '> 당첨 번호를 입력해 주세요.',
+  getBonusNumber: '> 보너스 번호를 입력해 주세요.',
+  askUserRetry: '> 다시 시작하시겠습니까? (y/n)',
+});
+
+export default INPUT_MESSAGE;
--- src/service/ParsingService.js
+++ src/service/ParsingService.js
@@ -1,5 +1,5 @@
 import Lotto from '../model/Lotto.js';
-import systemSettings from '../settings/systemSettings.js';
+import INPUT_MESSAGE from '../constants/InputMessage.js';
 import readLineAsync from '../View/InputView.js';
 import { printPurchasedAmount, printError } from '../View/OutputView.js';
 import validateBonusNumber from '../Validation/validateBonusNumber.js';
@@ -8,7 +8,7 @@ import validateUserRetry from '../Validation/validateUserRetry.js';
 
 async function getPurchasePrice() {
   try {
-    const purchasePrice = await readLineAsync(systemSettings.getPurchasePrice);
+    const purchasePrice = await readLineAsync(INPUT_MESSAGE.getPurchasePrice);
     const purchaseAmount = validateLottoPurchase(purchasePrice);
     printPurchasedAmount(purchaseAmount);
     return { purchasePrice, purchaseAmount };
@@ -19,7 +19,7 @@ async function getPurchasePrice() {
 }
 async function getWinningNumber() {
   try {
-    const winningNumber = await readLineAsync(systemSettings.getWinningNumber);
+    const winningNumber = await readLineAsync(INPUT_MESSAGE.getWinningNumber);
     const userLotto = new Lotto(
       winningNumber.split(',').map((number) => Number(number)),
     );
@@ -31,7 +31,7 @@ async function getWinningNumber() {
 }
 async function getBonusNumber(userLotto) {
   try {
-    const bonusNumber = await readLineAsync(systemSettings.getBonusNumber);
+    const bonusNumber = await readLineAsync(INPUT_MESSAGE.getBonusNumber);
     const parsedLotto = validateBonusNumber(userLotto, Number(bonusNumber));
     return parsedLotto;
   } catch (error) {
@@ -42,7 +42,7 @@ async function getBonusNumber(userLotto) {
 
 async function getUserRetry() {
   try {
-    const userRetry = await readLineAsync(`${systemSettings.askUserRetry}`);
+    const userRetry = await readLineAsync(`${INPUT_MESSAGE.askUserRetry}`);
     validateUserRetry(userRetry);
     return userRetry;
   } catch (error) {
