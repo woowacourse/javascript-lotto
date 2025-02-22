4aaf2c7815336da511b49e5fbd2f8ae0dc875608
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-22 18:58:22 +0900

    feat(randomLottos): 구매한 로또를 UI로 보여주는 함수 구현

---
+++ src/web/main/randomLottos/randomLottos.js
@@ -0,0 +1,29 @@
+import './randomLottos.css';
+
+export default function randomLottos(playLotto, randomLottosArray) {
+  const purchasedLottoQuantity = document.createElement('p');
+  purchasedLottoQuantity.innerText = `총 ${randomLottosArray.length}개를 구매하였습니다.`;
+  playLotto.appendChild(purchasedLottoQuantity);
+
+  const randomLottos = document.createElement('div');
+  randomLottos.className = 'random-lottos';
+
+  console.log(randomLottosArray);
+
+  randomLottosArray.forEach((randomLotto) => {
+    const oneLotto = document.createElement('div');
+    oneLotto.className = 'one-lotto';
+
+    const lottoImage = document.createElement('img');
+    lottoImage.src = './lotto.png';
+
+    const randomLottoNumbers = document.createElement('p');
+    randomLottoNumbers.innerText = randomLotto;
+
+    oneLotto.appendChild(lottoImage);
+    oneLotto.appendChild(randomLottoNumbers);
+    randomLottos.appendChild(oneLotto);
+  });
+
+  playLotto.appendChild(randomLottos);
+}
