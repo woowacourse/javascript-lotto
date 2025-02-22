f12979f0ec73ea05528d1b2ec6fc223a4028fa9c
thgml05 <aisohii@naver.com>
2025-02-22 22:03:59 +0900

    [test] : 테스트 독립적으로 분리

--- __tests__/LottoPrizeManager.test.js
+++ __tests__/LottoPrizeManager.test.js
@@ -2,25 +2,31 @@ import Lotto from "../src/domain/Lotto.js";
 import LottoComparer from "../src/domain/LottoComparer.js";
 import LottoPrize from "../src/domain/LottoPrize.js";
 
-let lottoComparer;
 let lottoPrize;
 
 beforeEach(() => {
-  const generatedLottos = [
-    new Lotto([1, 2, 3, 4, 5, 6]),
-    new Lotto([1, 3, 4, 5, 6, 10]),
-    new Lotto([1, 3, 4, 5, 6, 7]),
-    new Lotto([1, 4, 5, 6, 7, 8]),
-    new Lotto([1, 5, 6, 7, 8, 9]),
-    new Lotto([1, 6, 7, 8, 9, 10]),
-    new Lotto([1, 7, 8, 9, 10, 11]),
+  const compareResult = [
+    {
+      matchCount: 6,
+      hasBonus: false,
+    },
+    {
+      matchCount: 5,
+      hasBonus: true,
+    },
+    {
+      matchCount: 5,
+      hasBonus: false,
+    },
+    {
+      matchCount: 4,
+      hasBonus: false,
+    },
+    {
+      matchCount: 3,
+      hasBonus: false,
+    },
   ];
-  const winningNumbers = [1, 2, 3, 4, 5, 6];
-  const bonusNumber = 7;
-
-  lottoComparer = new LottoComparer(winningNumbers, bonusNumber);
-
-  const compareResult = lottoComparer.lottoCompareResult(generatedLottos);
   lottoPrize = new LottoPrize(compareResult);
   lottoPrize.calculateTotalPrizeCount();
 });
