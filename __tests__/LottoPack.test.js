6a6fae13599d8d73202fcc08c725d7cb9798e52c
hoyyChoi <hoyeon8165@gmail.com>
2025-02-22 19:37:50 +0900

    refactor : LottoPack 도메인 테스트

--- __tests__/LottoPack.test.js
+++ __tests__/LottoPack.test.js
@@ -1,3 +1,4 @@
+import { BONUS, MATCH_COUNT, WINNING } from "../src/constants/constant";
 import Lotto from "../src/domain/Lotto";
 import LottoPack from "../src/domain/LottoPack";
 
@@ -7,13 +8,38 @@ describe("LottoPack 도메인 테스트", () => {
     [1, 2, 7, 8, 9, 6],
     [2, 3, 13, 4, 5, 6],
   ];
-  test("갯수에 비례해 로또가 발행된다.", () => {
-    const lottoPack = new LottoPack(sampleLottos);
-    expect(lottoPack.lottos.length).toBe(sampleLottos.length);
+  const count = 3;
+  test("LottoPack은 주입된 갯수에 비례해 로또가 발행되어야한다.", () => {
+    const lottoPack = new LottoPack(sampleLottos, count);
+    expect(lottoPack.lottos.length).toBe(count);
   });
 
-  test("Lotto Pack은 Lotto Instance들을 반환해야한다.", () => {
+  test("LottoPack은 Lotto Instance들을 반환해야한다.", () => {
     const lottoPack = new LottoPack(sampleLottos);
     expect(lottoPack.lottos[0]).toBeInstanceOf(Lotto);
+  });
+
+  test("LottoPack은 당첨 번호를 올바르게 카운트해야 한다.", () => {
+    const lottoPack = new LottoPack(sampleLottos, count);
+
+    const answerLotto = {
+      1: WINNING,
+      2: WINNING,
+      3: WINNING,
+      4: WINNING,
+      5: WINNING,
+      6: WINNING,
+      7: BONUS,
+    };
+
+    const result = lottoPack.compareAndReturnResult(answerLotto);
+
+    expect(result).toEqual({
+      [MATCH_COUNT.SIX]: 1,
+      [MATCH_COUNT.FIVE_BONUS]: 0,
+      [MATCH_COUNT.FIVE]: 1,
+      [MATCH_COUNT.FOUR]: 0,
+      [MATCH_COUNT.THREE]: 1,
+    });
   });
 });
