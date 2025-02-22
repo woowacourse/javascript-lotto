bc71a78ace3fdfeafa321c2e6b825f35ffc3abe0
kimyou1102 <yoosk5485@naver.com>
2025-02-22 17:45:48 +0900

    test: 검증 테스트에서 경계값을 고려하여 테스트 데이터 변경

--- __tests__/PriceValidator.test.js
+++ __tests__/PriceValidator.test.js
@@ -9,7 +9,7 @@ describe("입력한 구매 금액에 대한 유효성 검사를 진행한다", (
     expect(() => priceValidator.checkThousandUnit(price)).toThrow("[ERROR]");
   });
 
-  test.each([500, 1000000])(
+  test.each([999, 100001])(
     "1000원 이상 100000 이하가 아닌 경우 에러를 발생시킨다. - %s원인 경우",
     (price) => {
       const priceValidator = new PriceValidator();
--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -19,14 +19,16 @@ describe("입력 값 유효성 테스트를 진행한다.", () => {
     expect(() => validator.number(input)).toThrow("[ERROR]");
   });
 
-  test("범위를 벗어난 입력을 할 경우 오류를 발생시킨다.", () => {
-    // given
-    const input = 0;
-    // when
-    const validator = new Validator();
-    // then
-    expect(() => validator.range({ min: 1, max: 45 }, input)).toThrow(
-      "[ERROR]"
-    );
-  });
+  test.each([0, 46])(
+    "범위를 벗어난 입력을 할 경우 오류를 발생시킨다.",
+    (input) => {
+      // given
+      const min = 1;
+      const max = 45;
+      // when
+      const validator = new Validator();
+      // then
+      expect(() => validator.range({ min, max }, input)).toThrow("[ERROR]");
+    }
+  );
 });
