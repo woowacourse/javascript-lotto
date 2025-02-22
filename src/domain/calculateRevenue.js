fa88d3205ceac07e29b9ed8196da0f864f1873cc
H0ngJu <hongju4385@gmail.com>
2025-02-22 20:47:47 +0900

    refactor(calculateRevenue): 수익률 계산시 최소 일치 개수 3을 상수화

--- src/domain/calculateRevenue.js
+++ src/domain/calculateRevenue.js
@@ -3,7 +3,7 @@ import { LOTTO } from '../constants/messages.js';
 export const calculateRevenue = (matchCounts, purchasePrice) => {
   const totalRevenue = matchCounts
     .map((matchCount, idx) => ({ matchCount, idx }))
-    .filter(({ idx }) => idx >= 3)
+    .filter(({ idx }) => idx >= LOTTO.THREE_MATCH)
     .reduce((acc, { matchCount, idx }) => acc + matchCount * calculateRevenueByMatch(idx), 0);
 
   return Number(((totalRevenue / purchasePrice) * 100).toFixed(1));
