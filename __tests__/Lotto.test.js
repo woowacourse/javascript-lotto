a8be07b6dc2d94378427420ade9ba4c66e6ac3f7
deokbin <deokbin@naver.com>
2025-02-22 09:20:00 +0900

    refactor(getUniqueRandomNumber,Lotto): 테스트 명세 구체적으로 변경

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -9,10 +9,6 @@ describe('Lotto class test', () => {
       new Lotto(testCase);
     }).toThrow(ERROR_MESSAGE.notSixNumbers);
   });
-  it('로또를 생성했을 때 6개의 번호일때는 숫자를 가진다.', () => {
-    const testCase = [1, 2, 3, 4, 5, 6];
-    expect(new Lotto(testCase).numbers).toBe(testCase);
-  });
 
   it('로또를 생성했을 때 중복된 숫자가 포함된 경우 에러를 던진다.', () => {
     const testCase = [1, 2, 3, 4, 5, 5];
@@ -25,5 +21,10 @@ describe('Lotto class test', () => {
     expect(() => {
       validateNumberInRange(testCase);
     }).toThrow(ERROR_MESSAGE.numberOutOfRange);
+  });
+
+  it('유효한 6개의 숫자로 로또 객체가 생성되면 그대로 저장된다.', () => {
+    const testCase = [1, 2, 3, 4, 5, 6];
+    expect(new Lotto(testCase).numbers).toBe(testCase);
   });
 });
--- __tests__/getUniqueRandomNumber.test.js
+++ __tests__/getUniqueRandomNumber.test.js
@@ -1,20 +1,22 @@
 import { getUniqueRandomNumbers } from '../src/util/getUniqueRandomNumbers.js';
 
 describe('랜덤 숫자를 받아오는 함수 테스트', () => {
-  it('랜덤 함수 호출 시 반환되는 값이 1에서 45 사이의 범위여야 한다.', () => {
-    const numberRange = { min: 1, max: 45 };
-    const sets = getUniqueRandomNumbers(numberRange, 6);
+  describe('인자로 최솟값이 1, 최댓값이 45로 주어졌을 때', () => {
+    it('함수에서 반환되는 값이 1에서 45 사이의 범위여야 한다.', () => {
+      const numberRange = { min: 1, max: 45 };
+      const sets = getUniqueRandomNumbers(numberRange, 6);
 
-    sets.forEach((number) => {
-      expect(number).toBeGreaterThanOrEqual(1);
-      expect(number).toBeLessThanOrEqual(45);
+      sets.forEach((number) => {
+        expect(number).toBeGreaterThanOrEqual(1);
+        expect(number).toBeLessThanOrEqual(45);
+      });
     });
-  });
 
-  it('랜덤 함수 호출 시 6개를 가져오면 반환되는 값이 6개 여야한다.', () => {
-    const numberRange = { min: 1, max: 45 };
-    const sets = getUniqueRandomNumbers(numberRange, 6);
+    it('랜덤 함수 호출 시 6개를 가져오면 반환되는 값이 6개 여야한다.', () => {
+      const numberRange = { min: 1, max: 45 };
+      const sets = getUniqueRandomNumbers(numberRange, 6);
 
-    expect(sets.length).toBe(6);
+      expect(sets.length).toBe(6);
+    });
   });
 });
