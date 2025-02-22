a9d171bff9a4f59785d3b515facf68e2f5c44819
aydenote <dslgpgh@gmail.com>
2025-02-23 00:51:16 +0900

    feat: 범위내에 숫자 배열 반환 함수 구현

--- src/util/createNumber.js
+++ src/util/createNumber.js
@@ -1,3 +1,7 @@
 export function getRandomNumberInRange(min = 1, max = 45) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function getLottoNumberArray(startNumber = 1, endNumber = 6) {
+  return Array.from({ length: endNumber }, (_, i) => startNumber + i);
+}
