21086145ce1566cbff110c416c993e73b9893d9c
dlsxjzld <sinjuk1@naver.com>
2025-02-23 16:39:50 +0900

    refactor: Lotto에서 번호를 들고 있는지, 다른 로또와 번호 비교 하는 기능 추가

--- src/Domain/Model/Lotto.js
+++ src/Domain/Model/Lotto.js
@@ -4,8 +4,17 @@ class Lotto {
   constructor(numbers) {
     this.#numbers = numbers;
   }
+
   getNumbers() {
     return this.#numbers;
+  }
+
+  has(number) {
+    return this.#numbers.includes(number);
+  }
+
+  match(targetLotto) {
+    return this.#numbers.filter((number) => targetLotto.has(number)).length;
   }
 }
 
