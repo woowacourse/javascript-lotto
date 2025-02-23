a4a394201748c7fd1e5de29364e215455c68721a
jin123457 <jin_ox@naver.com>
2025-02-23 21:05:15 +0900

    test : getLottoQuantity 함수에 대한 테스트 케이스 추가

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -1,10 +1,19 @@
+import { LOTTO_RULE } from '../src/domain/constants.js';
 import LottoMachine from '../src/domain/model/LottoMachine.js';
 
-test('구입 금액만큼 로또를 발행한다.', () => {
+describe('구입 금액만큼 로또를 발행한다.', () => {
+  let machine;
   const money = 8000;
+  beforeEach(() => {
+    machine = new LottoMachine();
+    machine.createLottos(money);
+  });
 
-  const machine = new LottoMachine();
-  machine.createLottos(money);
+  test('구입 금액만큼 로또를 발행후 머신에서 보관한다.', () => {
+    expect(machine.getLottos()).toHaveLength(money / LOTTO_RULE.PRICE);
+  });
 
-  expect(machine.getLottos()).toHaveLength(8);
+  test('머신에서 보관하고 있는 로또의 개수를 반환한다.', () => {
+    expect(machine.getLottoQuantity()).toBe(money / LOTTO_RULE.PRICE);
+  });
 });
