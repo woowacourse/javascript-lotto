2e7be4f7d4a6c0e564ac0c1b0649663f59914aee
dlsxjzld <sinjuk1@naver.com>
2025-02-23 20:56:52 +0900

    test: 로또 머신에서 숫자 값들만 가져오는 함수 테스트 추가

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -27,3 +27,22 @@ test('로또 1장당 1이상 45이하에서 중복되지 않는 랜덤한 번호
   );
   expect(lottoNumbers.length).toBe(LOTTO_DEFINITION.NUMBER_COUNTS);
 });
+
+describe('로또 4장의 숫자 값들만 2차원 배열로 가져온다.', () => {
+  test('로또 4장이 맞는지 확인한다', () => {
+    const purchaseMoney = 4000;
+    const lottoMachine = new LottoMachine();
+    const lottoCount = lottoMachine.purchaseLotto(purchaseMoney);
+    lottoMachine.makeLottoList(lottoCount);
+    expect(lottoMachine.getLottoNumbersList().length).toBe(4);
+  });
+  test('로또 1장마다 숫자가 6개 있는지 확인한다', () => {
+    const purchaseMoney = 4000;
+    const lottoMachine = new LottoMachine();
+    const lottoCount = lottoMachine.purchaseLotto(purchaseMoney);
+    lottoMachine.makeLottoList(lottoCount);
+    lottoMachine
+      .getLottoNumbersList()
+      .forEach((numbers) => expect(numbers.length).toBe(6));
+  });
+});
