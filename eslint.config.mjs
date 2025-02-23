fe9e4649b1a2b01f5662812125d6c93962d324e5
dlsxjzld <sinjuk1@naver.com>
2025-02-23 15:31:01 +0900

    chore: eslint, prettier 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+
+    rules: {
+      'max-depth': ['error', 1],
+      'max-params': ['error', 2],
+      'no-var': 'error',
+    },
+  },
+  eslintConfigPrettier,
+];
