7942fe686ed9476c9fdb38156d8e45a9ea771fa2
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-23 18:45:44 +0900

    refactor: Lotto 클래스에서 불필요한 초기화 제거

--- src/Model/Lotto.js
+++ src/Model/Lotto.js
@@ -1,7 +1,5 @@
-import { LOTTO_NUMBER_LENGTH } from '../constants/common.js';
-
 class Lotto {
-  #numbers = Array.from({ length: LOTTO_NUMBER_LENGTH }).fill(0);
+  #numbers
 
   constructor(numbers) {
     this.#numbers = numbers.sort((a, b) => a - b);
