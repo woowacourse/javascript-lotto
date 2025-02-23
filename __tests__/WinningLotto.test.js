cc2c50318a384e4f50e9efa8ef5cbc9f5dea66a3
dlsxjzld <sinjuk1@naver.com>
2025-02-23 16:41:37 +0900

    refactor: WinningLotto의 일반 로또와 개수 비교 테스트 문구 더 자세하게 변경

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -11,7 +11,7 @@ test('입력받은 당첨 번호와 보너스 번호를 토대로 당첨 로또�
   expect(lotto.getBonusNumber()).toBe(7);
 });
 
-test('일반 로또 하나와 당첨 로또를 비교해서 번호가 같은 개수를 구한다.', () => {
+test('일반 로또 하나([1, 2, 3, 4, 5, 6])와 당첨 로또([1, 2, 3, 4, 5, 6])를 비교해서 같은 개수가 6개가 나오는지 확인한다.', () => {
   const winningLotto = new WinningLotto(new Lotto([1, 2, 3, 4, 5, 6]), 7);
   const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
   expect(winningLotto.countMatchingNumbers(lotto)).toBe(6);
