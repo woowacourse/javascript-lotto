52d77cbc88dad1d789262b3964727ad7bbc76736
Jeongeun Lee <leeju1009@gmail.com>
2025-02-24 01:30:09 +0900

    feat: 로또 게임 section을 생성하는 함수 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,8 +1,10 @@
+import createGameBox from "./view/layers/createGameBox.js";
 import createHeader from "./view/layers/header.js";
 
 const app = async () => {
   document.addEventListener("DOMContentLoaded", async () => {
     await createHeader();
+    await createGameBox();
   });
 };
 
---
+++ src/view/layers/createGameBox.js
@@ -0,0 +1,12 @@
+import gameBoxContents from './gameBox.html?raw';
+import './gameBox.css';
+
+const createGameBox = async () => {
+  const section = document.createElement('section');
+  section.className = 'gameBox-container';
+  section.innerHTML = gameBoxContents;
+
+  document.getElementById('main-container').prepend(section);
+};
+
+export default createGameBox;
