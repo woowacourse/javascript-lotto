400eac51857356118513caf5534270462f89e2c5
ahhyun <wndkgus0225@naver.com>
2025-02-24 03:49:34 +0900

    refactor: getUniqueNumbersInRange.test.js에서 expect에 상수 대신 직관적인 값을 입력

--- __tests__/getUniqueNumbersInRange.test.js
+++ __tests__/getUniqueNumbersInRange.test.js
@@ -6,29 +6,32 @@ import {
 } from "../src/constants/validateConstants.js";
 
 describe("주어진 범위 안에서 중복되지 않는 숫자를 개수만큼 반환하는 메서드 테스트", () => {
-  // given
-  const min = MIN_LOTTO_NUMBER;
-  const max = MAX_LOTTO_NUMBER;
-  const count = LOTTO_LENGTH;
-
   test("중복되지 않는 숫자를 주어진 개수만큼 반환하는지 확인한다.", () => {
     // when
-    const randomNumbers = getUniqueRandomNumbers(min, max, count);
+    const randomNumbers = getUniqueRandomNumbers(
+      MIN_LOTTO_NUMBER,
+      MAX_LOTTO_NUMBER,
+      LOTTO_LENGTH
+    );
     const uniqueRandomNumbers = Array.from(new Set(randomNumbers));
 
     // then
-    expect(randomNumbers).toHaveLength(count);
-    expect(uniqueRandomNumbers).toHaveLength(count);
+    expect(randomNumbers).toHaveLength(LOTTO_LENGTH);
+    expect(uniqueRandomNumbers).toHaveLength(LOTTO_LENGTH);
   });
 
   test("랜덤으로 반환된 숫자들이 주어진 범위 안에 존재하는지 확인한다.", () => {
     // when
-    const randomNumbers = getUniqueRandomNumbers(min, max, count);
+    const randomNumbers = getUniqueRandomNumbers(
+      MIN_LOTTO_NUMBER,
+      MAX_LOTTO_NUMBER,
+      LOTTO_LENGTH
+    );
 
     // then
     randomNumbers.forEach((number) => {
-      expect(number).toBeLessThanOrEqual(max);
-      expect(number).toBeGreaterThanOrEqual(min);
+      expect(number).toBeLessThanOrEqual(MAX_LOTTO_NUMBER);
+      expect(number).toBeGreaterThanOrEqual(MIN_LOTTO_NUMBER);
     });
   });
 });
