05c5e7fea1b20b60a42c2044dc3be6b12f232d77
hanheel <hanhee630630@gmail.com>
2025-02-24 03:09:10 +0900

    feat : 로또 번호 생성 및 view

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,47 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import { getLottoArray, getLottoCount } from "./service/PurchaseService.js";
+
+const runLotto = () => {
+  document.querySelector(".purchase-button").addEventListener("click", () => {
+    const inputPrice = document.querySelector(".price-input").value;
+    console.log("입력된 값:", inputPrice);
+
+    const lottoCount = getLottoCount(inputPrice);
+    displayCount(lottoCount);
+
+    const lottoArray = getLottoArray(lottoCount);
+    displayLotto(lottoArray);
+  });
+};
+
+const displayLotto = (lottoArray) => {
+  const lottoContainer = document.querySelector(".lotto-numbers-container");
+
+  lottoArray.forEach((lotto) => {
+    const lottoNumbersItem = document.createElement("div");
+    lottoNumbersItem.classList.add("lotto-numbers-item");
+
+    const lottoItem = document.createElement("div");
+    lottoItem.classList.add("lotto-numbers");
+    lottoItem.textContent = lotto.numbers.join(", ");
+
+    const lottoImage = document.createElement("img");
+    lottoImage.classList.add("lotto-image");
+    lottoImage.src = "public/lotto.png";
+    lottoImage.alt = "로또 이미지";
+
+    lottoNumbersItem.appendChild(lottoImage);
+    lottoNumbersItem.appendChild(lottoItem);
+    lottoContainer.appendChild(lottoNumbersItem);
+  });
+};
+
+const displayCount = (lottoCount) => {
+  const countContainer = document.querySelector(".lotto-count");
+  const countItem = document.createElement("div");
+  countItem.classList.add("count");
+  countItem.textContent = `총 ${lottoCount} 개를 구매하였습니다.`;
+
+  countContainer.appendChild(countItem);
+};
+
+runLotto();
