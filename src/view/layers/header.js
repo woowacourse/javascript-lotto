e399dfb45c264993d7878e3fb3535b340a825a86
Jeongeun Lee <leeju1009@gmail.com>
2025-02-23 17:53:29 +0900

    feat: header를 생성하는 함수 생성

---
+++ src/view/layers/header.js
@@ -0,0 +1,12 @@
+const createHeader = async () => {
+  const contents = await fetch("/header.html");
+  const parsedContents = await contents.text();
+
+  const header = document.createElement("header");
+  header.className = `header-container`;
+  header.innerHTML = parsedContents;
+
+  document.getElementById("app").prepend(header);
+};
+
+export default createHeader;
