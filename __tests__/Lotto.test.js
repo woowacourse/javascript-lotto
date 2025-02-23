2089efe62762ace5163791ff9e1bf9ffa426bff8
jin123457 <jin_ox@naver.com>
2025-02-23 16:12:09 +0900

    test : 로또 생성 테스트 케이스 추가 및 그룹화
    [수정된 파일]
    Lotto.test.js :
    * 숫자가 6개가 아닐 경우 에러를 발생 기능 추가
    * 로또의 각 숫자 범위가 기준점보다 낮거나 높을시 에러 발생 기능 추가
    * 로또의 숫자 중 중복됐을 경우 에러 발생 기능 추가
    * 로또의 숫자 범위 그룹화
    * 로또의 숫자의 개수 그룹화

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,26 +1,57 @@
 import Lotto from '../src/domain/model/Lotto.js';
+import { LOTTO_RULE } from '../src/domain/constants.js';
+import { ERROR } from '../src/constants/message.js';
 
-test('숫자 6개를 가진 로또 1장을 발행한다.', () => {
-  // given
-  const lottoNumbers = [1, 2, 3, 4, 5, 6];
+describe(`로또 항상 숫자 ${LOTTO_RULE.LENGTH}개를 가져야 한다.`, () => {
+  test(`숫자 ${LOTTO_RULE.LENGTH}개를 가진 로또 1장을 발행한다.`, () => {
+    // given
+    const lottoNumbers = [1, 2, 3, 4, 5, 6];
 
-  // when
-  const lotto = new Lotto(lottoNumbers);
+    // when
+    const lotto = new Lotto(lottoNumbers);
 
-  // then
-  expect(lotto.getNumbers()).toHaveLength(6);
+    // then
+    expect(lotto.getNumbers()).toHaveLength(LOTTO_RULE.LENGTH);
+  });
+
+  test.each([
+    [1, 2, 3, 4, 5, 6, 7],
+    [1, 2, 3, 4, 5],
+  ])(
+    `숫자 ${LOTTO_RULE.LENGTH}개보다 적거나 많은 숫자를 가진 로또는 에러를 발생한다.`,
+    (lottoNumbers) => {
+      expect(() => {
+        const lotto = new Lotto(lottoNumbers);
+        lotto.getNumbers();
+      }).toThrow(ERROR.LOTTO_NUMBER.QUANTITY);
+    },
+  );
 });
 
-test.each([
-  [[1, 2, 3, 4, 5, 6]], [[40, 41, 42, 43, 44, 45]],
-])('로또의 번호의 숫자 범위는 1 ~ 45이다.', (lottoNumbers) => {
-  // when
-  const lotto = new Lotto(lottoNumbers);
+describe(`로또 번호의 각 숫자 범위는 ${LOTTO_RULE.MIN_RANGE} ~ ${LOTTO_RULE.MAX_RANGE} 사이의 숫자여야한다.`, () => {
+  test.each([[[1, 2, 3, 4, 5, 6]], [[40, 41, 42, 43, 44, 45]]])(
+    `로또 번호의 각 숫자 범위는 ${LOTTO_RULE.MIN_RANGE} ~ ${LOTTO_RULE.MAX_RANGE}이다.`,
+    (lottoNumbers) => {
+      // when
+      const lotto = new Lotto(lottoNumbers);
 
-  // then
-  expect(lotto.getNumbers()).toHaveLength(6);
+      // then
+      expect(lotto.getNumbers()).toHaveLength(LOTTO_RULE.LENGTH);
+    },
+  );
+
+  test.each([[[0, 2, 3, 4, 5, 6]], [[40, 41, 42, 43, 44, 46]]])(
+    `로또 번호의 각 숫자 범위는 ${LOTTO_RULE.MIN_RANGE} ~ ${LOTTO_RULE.MAX_RANGE}가 아닐시 에러를 발생한다.`,
+    (lottoNumbers) => {
+      // when
+
+      // then
+      expect(() => {
+        new Lotto(lottoNumbers);
+      }).toThrow(ERROR.LOTTO_NUMBER.RANGE);
+    },
+  );
 });
-
 
 test('로또 번호는 오름차순이여야 한다.', () => {
   // given
@@ -30,3 +61,12 @@ test('로또 번호는 오름차순이여야 한다.', () => {
   // then
   expect(lotto.getNumbers()).toEqual([1, 2, 5, 9, 24, 35]);
 });
+
+test.each([[[5, 35, 24, 1, 2, 2]], [[35, 35, 24, 1, 2, 3]]])(
+  '로또 번호는 중복된 숫자가 포함되면 안된다.',
+  (lottoNumbers) => {
+    expect(() => {
+      new Lotto(lottoNumbers);
+    }).toThrow(ERROR.LOTTO_NUMBER.DUPLICATION);
+  },
+);
