58219b57fed073eea2c548f2c2ad760724c71005
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-23 16:56:13 +0900

    refactor: 에러 메시지 수정
    - 지폐 단위를 보여주게 수정

--- src/constant/error.js
+++ src/constant/error.js
@@ -4,9 +4,9 @@ import { LOTTO } from "./lotto.js";
 
 const ERROR_MESSAGE = {
   NOT_A_NUMBER: "[ERROR] 숫자를 입력해주세요.",
-  NOT_DIVIDED_1000: `[ERROR] ${PRICE.UNIT}으로 나누어 떨어지는 숫자를 입력해주세요.`,
-  UNDER_MIN_PRICE: `[ERROR] 최소 결제 금액은 ${PRICE.UNIT}원 이상입니다.`,
-  EXCEED_MAX_PRICE: `[ERROR] 최대 결제 금액은 ${PRICE.MAX}원 미만입니다.`,
+  NOT_DIVIDED_1000: `[ERROR] ${PRICE.UNIT.toLocaleString()}원으로 나누어 떨어지는 숫자를 입력해주세요.`,
+  UNDER_MIN_PRICE: `[ERROR] 최소 결제 금액은 ${PRICE.UNIT.toLocaleString()}원 이상입니다.`,
+  EXCEED_MAX_PRICE: `[ERROR] 최대 결제 금액은 ${PRICE.MAX.toLocaleString()}원 미만입니다.`,
   LOTTO_LENGTH: `[ERROR] 당첨 번호는 ${LOTTO.LENGTH}개여야 합니다.`,
   DUPLICATE_WINNING_NUMBER:
     "[ERROR] 당첨 번호는 중복된 번호를 입력할 수 없습니다.",
