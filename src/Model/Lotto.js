2fec37a04eaf107270c5e9b347380df48b296a2c
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-23 19:26:21 +0900

    refactor: Lotto 생성자에서 원본 배열을 변경하지 않도록 수정
    - numbers 배열을 직접 변경하지 않고 복사본([...numbers])을 만들어 정렬

--- src/Model/Lotto.js
+++ src/Model/Lotto.js
@@ -2,7 +2,7 @@ class Lotto {
   #numbers
 
   constructor(numbers) {
-    this.#numbers = numbers.sort((a, b) => a - b);
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   get numbers() {
