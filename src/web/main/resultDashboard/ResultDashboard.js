5888aa28c8b75e32e2230937d9e972855e2ebb25
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 00:14:46 +0900

    feat(resultDashboard): 로또 통계창 닫는 버튼 컴포넌트 구현

--- src/web/main/resultDashboard/ResultDashboard.js
+++ src/web/main/resultDashboard/ResultDashboard.js
@@ -4,12 +4,25 @@ export default function ResultDashboard(playLotto, matchCounts) {
   const resultDashboard = document.createElement('div');
   resultDashboard.className = 'result-dashboard';
 
-  ResultBackground(playLotto);
+  const resultBackground = ResultBackground(playLotto);
+  CloseButton(resultDashboard, resultBackground);
   ResultHeader(resultDashboard);
-
   ResultContainer(resultDashboard, matchCounts);
 
+  playLotto.appendChild(resultBackground);
   playLotto.appendChild(resultDashboard);
+}
+
+function CloseButton(resultDashboard, resultBackground) {
+  const closeButton = document.createElement('button');
+  closeButton.innerText = 'X';
+  closeButton.className = 'close-button';
+  closeButton.addEventListener('click', () => {
+    resultBackground.remove();
+    resultDashboard.remove();
+  });
+
+  resultDashboard.appendChild(closeButton);
 }
 
 function ResultHeader(resultDashboard) {
@@ -30,27 +43,28 @@ function ResultContainer(resultDashboard, matchCounts) {
   resultDashboard.appendChild(resultContainer);
 }
 
-function ResultBackground(playLotto) {
+function ResultBackground() {
   const resultBackground = document.createElement('div');
   resultBackground.className = 'result-background';
-  playLotto.appendChild(resultBackground);
+
+  return resultBackground;
 }
 
 function ResultCols(resultDashboard) {
   const resultCols = document.createElement('div');
   resultCols.className = 'result-rows';
 
-  const typeOfMatch = document.createElement('p');
+  const typeOfMatch = document.createElement('div');
   typeOfMatch.innerText = '일치 갯수';
   typeOfMatch.className = 'result-col';
   resultCols.appendChild(typeOfMatch);
 
-  const winningPrize = document.createElement('p');
+  const winningPrize = document.createElement('div');
   winningPrize.innerText = '당첨금';
   winningPrize.className = 'result-col';
   resultCols.appendChild(winningPrize);
 
-  const matchCounts = document.createElement('p');
+  const matchCounts = document.createElement('div');
   matchCounts.innerText = '당첨 횟수';
   matchCounts.className = 'result-col';
   resultCols.appendChild(matchCounts);
