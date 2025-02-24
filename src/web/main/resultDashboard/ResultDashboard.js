12400c6bc0db8e3d83b79c1ffa6e51dba797f2e9
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 01:16:44 +0900

    feat(resultDashboard): 로또 매칭결과 UI 구현

--- src/web/main/resultDashboard/ResultDashboard.js
+++ src/web/main/resultDashboard/ResultDashboard.js
@@ -39,8 +39,38 @@ function ResultContainer(resultDashboard, matchCounts) {
   ResultCols(resultContainer);
   DividerLine(resultContainer);
 
+  resultContainer.appendChild(ResultRow('3개', '5,000', matchCounts[3]));
+  DividerLine(resultContainer);
+  resultContainer.appendChild(ResultRow('4개', '50,000', matchCounts[4]));
+  DividerLine(resultContainer);
+  resultContainer.appendChild(ResultRow('5개', '1,500,000', matchCounts[5]));
+  DividerLine(resultContainer);
+  resultContainer.appendChild(ResultRow('5개+보너스볼', '30,000,000', matchCounts[7]));
+  DividerLine(resultContainer);
+  resultContainer.appendChild(ResultRow('6개', '2,000,000,000', matchCounts[6]));
+  DividerLine(resultContainer);
+
   resultContainer.className = 'result-container';
   resultDashboard.appendChild(resultContainer);
+}
+
+function ResultRow(type, prize, count) {
+  const resultRow = document.createElement('div');
+  resultRow.className = 'result-rows';
+
+  const typeOfMatch = document.createElement('div');
+  typeOfMatch.innerText = type;
+  resultRow.appendChild(typeOfMatch);
+
+  const winningPrize = document.createElement('div');
+  winningPrize.innerText = prize;
+  resultRow.appendChild(winningPrize);
+
+  const matchCounts = document.createElement('div');
+  matchCounts.innerText = count;
+  resultRow.appendChild(matchCounts);
+
+  return resultRow;
 }
 
 function ResultBackground() {
