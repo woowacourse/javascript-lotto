1ff8bbdc5d668bc6b0421c2c396753ecbc99e453
kimyou1102 <yoosk5485@naver.com>
2025-02-25 00:30:06 +0900

    feat: 공통 Button 컴포넌트 생성

---
+++ src/common/button.js
@@ -0,0 +1,14 @@
+export default class Button {
+  constructor($target, onClick, text) {
+    this.render($target, onClick, text);
+  }
+
+  render($target, onClick, text) {
+    const $button = document.createElement("button");
+    $button.innerText = text;
+    $button.className = "full-button";
+
+    $button.addEventListener("click", onClick);
+    $target.appendChild($button);
+  }
+}
