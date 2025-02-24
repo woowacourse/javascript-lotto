cf44bf26308351c57021ae4c91cad7d376999e2a
kimyou1102 <yoosk5485@naver.com>
2025-02-24 16:47:46 +0900

    feat: index.js에 App컴포넌트 선언 및 Nav, LottoGame 컴포넌트 호출

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,20 @@
+import Nav from "./components/layout/Nav.js";
+import LottoGame from "./components/LottoGame.js";
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+class App {
+  constructor() {
+    this.render();
+  }
+
+  render() {
+    const $body = document.querySelector("body");
+    new Nav();
+    new LottoGame($body);
+  }
+}
+
+new App();
