4dba50814391ae0cd573fad942c299f9f9963aaf
kimyou1102 <yoosk5485@naver.com>
2025-02-24 18:01:09 +0900

    feat: LottoPurchaseHistory 컴포넌트 생성

---
+++ src/components/LottoPurchaseHistory.js
@@ -0,0 +1,31 @@
+import { OUTPUT } from "../constants/message.js";
+import LottoHistoryItem from "./LottoHistoryItem.js";
+
+export default class LottoPurchaseHistory {
+  #countNumber;
+  #lottos;
+
+  constructor($target, countNumber, lottos) {
+    this.#countNumber = countNumber;
+    this.#lottos = lottos;
+
+    this.render($target);
+  }
+
+  render($target) {
+    const $div = document.createElement("div");
+    const $text = document.createElement("p");
+    const $ul = document.createElement("ul");
+    $ul.className = "lotto-history-list";
+
+    $text.innerText = `총 ${this.#countNumber}${OUTPUT.BUY_COUNT}`;
+
+    this.#lottos.map(
+      (lotto) => new LottoHistoryItem($ul, lotto.getLottoNumbers())
+    );
+
+    $div.appendChild($text);
+    $div.appendChild($ul);
+    $target.appendChild($div);
+  }
+}
