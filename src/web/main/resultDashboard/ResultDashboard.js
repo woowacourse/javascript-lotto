e623a7c7697607b2d095b91483b1c491b4f42957
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-24 20:46:52 +0900

    refactor(resultDashboard): ResultBackground 함수 분리

--- src/web/main/resultDashboard/ResultDashboard.js
+++ src/web/main/resultDashboard/ResultDashboard.js
@@ -1,8 +1,16 @@
 import './resultDashboard.css';
 
 export default function ResultDashboard(playLotto) {
+  const resultDashboard = document.createElement('div');
+  resultDashboard.className = 'result-dashboard';
+
+  ResultBackground(resultDashboard);
+
+  playLotto.appendChild(resultDashboard);
+}
+
+function ResultBackground(resultDashboard) {
   const resultBackground = document.createElement('div');
   resultBackground.className = 'result-background';
-
-  playLotto.appendChild(resultBackground);
+  resultDashboard.appendChild(resultBackground);
 }
