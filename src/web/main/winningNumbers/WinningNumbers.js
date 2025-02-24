ec8c7682f845823a67c7a6f29472ec81df09c052
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-24 17:01:00 +0900

    feat(winningNumbers): 당첨번호와 보너스번호 eventListener를 통해 업데이트 로직 구현

--- src/web/main/winningNumbers/WinningNumbers.js
+++ src/web/main/winningNumbers/WinningNumbers.js
@@ -1,9 +1,18 @@
 import './winningNumbers.css';
 
-export default function winningNumbers(playLotto) {
+export default function WinningNumbers(playLotto) {
   const winningNumberContainer = document.createElement('div');
   winningNumberContainer.className = 'winning-number-container';
 
+  WinningNumberHeaders(winningNumberContainer);
+  const { winningNumbersArray, bonusNumber } = WinningNumberInputs(winningNumberContainer);
+
+  playLotto.appendChild(winningNumberContainer);
+
+  return { winningNumbersArray, bonusNumber };
+}
+
+function WinningNumberHeaders(winningNumberContainer) {
   const winningNumberHeaders = document.createElement('div');
   winningNumberHeaders.className = 'winning-number-headers';
 
@@ -17,6 +26,38 @@ export default function winningNumbers(playLotto) {
 
   winningNumberHeaders.appendChild(winningNumberHeader);
   winningNumberHeaders.appendChild(bonusNumberHeader);
+  winningNumberContainer.appendChild(winningNumberHeaders);
+}
 
-  playLotto.appendChild(winningNumberContainer);
+function WinningNumberInputs(winningNumberContainer) {
+  const winningAndBonusInputContainer = document.createElement('div');
+  winningAndBonusInputContainer.className = 'winning-and-bonus-input-container';
+
+  const winningNumbersArray = new Array(6).fill(0);
+  const bonusNumber = { value: 0 };
+  const winningInputs = document.createElement('div');
+  winningInputs.className = 'winning-inputs';
+  Array.from({ length: 6 }, (_, idx) => {
+    const winningNumberInput = document.createElement('input');
+    winningNumberInput.type = 'text';
+    winningNumberInput.className = 'winning-input';
+    winningNumberInput.addEventListener('input', (e) => {
+      winningNumbersArray[idx] = Number(e.target.value);
+    });
+
+    winningInputs.appendChild(winningNumberInput);
+  });
+  winningAndBonusInputContainer.appendChild(winningInputs);
+
+  const bonusNumberInput = document.createElement('input');
+  bonusNumberInput.type = 'text';
+  bonusNumberInput.className = 'bonus-input';
+  bonusNumberInput.addEventListener('input', (e) => {
+    bonusNumber.value = Number(e.target.value);
+  });
+
+  winningAndBonusInputContainer.appendChild(bonusNumberInput);
+
+  winningNumberContainer.appendChild(winningAndBonusInputContainer);
+  return { winningNumbersArray, bonusNumber };
 }
