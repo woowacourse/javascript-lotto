a63b5bb2a10a57a3f480408bc5b19aea435ee398
ahhyun <wndkgus0225@naver.com>
2025-02-24 15:09:35 +0900

    refactor:validateWinningNumbers.test.js에서 로또 번호 유효성 검사를 테스트 유형별로 분리

--- __tests__/validateWinningNumbers.test.js
+++ __tests__/validateWinningNumbers.test.js
@@ -13,6 +13,15 @@ describe("당첨 번호 유효성 테스트", () => {
       input: "1,2,3,4,5,6,7,8,9,10",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.INVALID_COUNT,
     },
+  ])("$description 에러가 발생한다.", ({ input, expectedErrorMessage }) => {
+    // given
+    // when & then
+    expect(() => {
+      validateWinningNumbers(input);
+    }).toThrow(expectedErrorMessage);
+  });
+
+  test.each([
     {
       description: "당첨 번호 중 숫자가 아닌 값이 있는 경우",
       input: "a,b,c,d,e,f",
@@ -23,6 +32,15 @@ describe("당첨 번호 유효성 테스트", () => {
       input: "1.1,2,3,4,5,6",
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.NOT_AN_INTEGER,
     },
+  ])("$description 에러가 발생한다.", ({ input, expectedErrorMessage }) => {
+    // given
+    // when & then
+    expect(() => {
+      validateWinningNumbers(input);
+    }).toThrow(expectedErrorMessage);
+  });
+
+  test.each([
     {
       description: `당첨 번호에서 ${MIN_LOTTO_NUMBER} 미만인 숫자가 있는 경우`,
       input: `1,2,3,4,5,${MIN_LOTTO_NUMBER - 1}`,
@@ -33,6 +51,15 @@ describe("당첨 번호 유효성 테스트", () => {
       input: `1,2,3,4,5,${MAX_LOTTO_NUMBER + 1}`,
       expectedErrorMessage: WINNING_NUMBERS_ERROR_MESSAGES.OUT_OF_RANGE,
     },
+  ])("$description 에러가 발생한다.", ({ input, expectedErrorMessage }) => {
+    // given
+    // when & then
+    expect(() => {
+      validateWinningNumbers(input);
+    }).toThrow(expectedErrorMessage);
+  });
+
+  test.each([
     {
       description: "당첨 번호의 숫자가 중복되는 경우",
       input: "1,2,3,4,4,5",
