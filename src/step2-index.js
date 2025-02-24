f1b0a07cc5a416923b9788ef73d6c302bd3618b1
hoyyChoi <hoyeon8165@gmail.com>
2025-02-24 15:50:10 +0900

    feat: lottoGame 레이아웃 동적 ui 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,15 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lottoGame = document.getElementById("lottoGame");
+
+  if (lottoGame) {
+    const lottoGameTitle = document.createElement("h1"); // `title` 대신 `h1` 사용
+    lottoGameTitle.classList.add("title");
+    lottoGameTitle.innerText = "🎱 내 번호 당첨 확인 🎱";
+
+    lottoGame.appendChild(lottoGameTitle);
+  }
+});
