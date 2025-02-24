6e3066dcc71ec038e5a6ab26f2c7a24e42e8904f
youdame <112458620+youdame@users.noreply.github.com>
2025-02-24 22:05:22 +0900

    test: 경계값 테스트 코드 추가

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,17 +1,6 @@
 import { ERROR_MESSAGE, LOTTO_NUMBERS_ERROR_MESSAGE } from "../src/constants/errorMessage.js";
 import Lotto from "../src/domain/Lotto.js";
 describe("로또 객체 생성 테스트", () => {
-  test("로또 번호는 숫자 배열이 입력되면, 로또 숫자를 필드로 갖는다", () => {
-    // given
-    const numbers = [1, 2, 3, 4, 5, 6];
-
-    // when
-    const lotto = new Lotto(numbers);
-
-    // then
-    expect(lotto.numbers).toEqual(numbers);
-  });
-
   test("로또 번호가 6개가 아닐 경우, 에러를 발생시킨다", () => {
     //given
     const numbers = [1, 2, 3, 4, 5];
@@ -21,12 +10,17 @@ describe("로또 객체 생성 테스트", () => {
   });
 
   test.each([
-    [
-      [1, 3, 5, 6, 7, 50],
-      [-2, 3, 4, 0, 56, 2],
-    ],
-  ])("로또 번호가 1에서 45사이 값이 아닐 경우, 에러를 발생시킨다", (numbers) => {
+    [1, 3, 5, 6, 7, 46],
+    [-2, 3, 4, 0, 56, 2],
+  ])("로또 번호가 1에서 45 사이 값이 아닐 경우, 에러를 발생시킨다", (numbers) => {
     expect(() => new Lotto(numbers)).toThrow(LOTTO_NUMBERS_ERROR_MESSAGE.RANGE);
+  });
+
+  test.each([
+    [1, 3, 5, 6, 7, 45],
+    [10, 15, 20, 25, 30, 45],
+  ])("로또 번호가 1에서 45 사이 값이면, 정상적으로 생성된다", (numbers) => {
+    expect(() => new Lotto(numbers)).not.toThrow();
   });
 
   test.each([
