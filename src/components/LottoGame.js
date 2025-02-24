36080578ef440b6a40504cfb533a75ab16df61f0
kimyou1102 <yoosk5485@naver.com>
2025-02-24 16:46:00 +0900

    feat: LottoGame 컴포넌트 생성

---
+++ src/components/LottoGame.js
@@ -0,0 +1,20 @@
+export default class LottoGame {
+  constructor($target) {
+    this.render($target);
+  }
+
+  render($target) {
+    const $container = document.createElement("div");
+    const $div = document.createElement("div");
+    const $title = document.createElement("p");
+
+    $container.classList = "container";
+    $title.innerText = "🎱 내 번호 당첨 확인 🎱";
+    $title.className = "lotto-winning-result-title";
+    $div.className = "lotto-winning-result-container";
+
+    $div.appendChild($title);
+    $container.appendChild($div);
+    $target.appendChild($container);
+  }
+}
