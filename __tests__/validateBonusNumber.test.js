bdca63d052dee1a7f9e5663bb9c1272af67b00a1
ahhyun <wndkgus0225@naver.com>
2025-02-24 14:38:27 +0900

    refactor:validateBonusNumber.test.js에서 보너스 번호 유효성 검사를 테스트 유형별로 분리

--- __tests__/validateBonusNumber.test.js
+++ __tests__/validateBonusNumber.test.js
@@ -6,57 +6,65 @@ import { BONUS_NUMBER_ERROR_MESSAGES } from "../src/constants/errorConstants.js"
 import validateBonusNumber from "../src/validations/validateBonusNumber.js";
 
 describe("보너스 번호 유효성 테스트", () => {
-  test.each([
-    {
-      description: "숫자가 아닌 경우",
-      input: "숫자아님",
-      expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.NOT_A_NUMBER,
-    },
-    {
-      description: "정수가 아닌 경우",
-      input: "1.1",
-      expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.NOT_AN_INTEGER,
-    },
-    {
-      description: `${MIN_LOTTO_NUMBER} 미만인 경우`,
-      input: `${MIN_LOTTO_NUMBER - 1}`,
-      expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.OUT_OF_RANGE,
-    },
-    {
-      description: `${MAX_LOTTO_NUMBER} 초과인 경우`,
-      input: `${MAX_LOTTO_NUMBER + 1}`,
-      expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.OUT_OF_RANGE,
-    },
-    {
-      description: "당첨 번호와 일치하는 경우",
-      input: "11",
-      expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.DUPLICATE_NUMBER,
-    },
-  ])(
-    "보너스 번호가 $description 에러가 발생한다.",
-    ({ input, expectedErrorMessage }) => {
-      // given
-      // when & then
-      expect(() => {
-        const winningNumbers = [11, 12, 13, 14, 15, 16];
-        validateBonusNumber(input, winningNumbers);
-      }).toThrow(expectedErrorMessage);
-    }
-  );
+  const winningNumbers = [11, 12, 13, 14, 15, 16];
 
-  test.each([
-    {
-      description: "최소값 경계인 경우",
-      input: `${MIN_LOTTO_NUMBER}`,
-    },
-    {
-      description: "최대값 경계인 경우",
-      input: `${MAX_LOTTO_NUMBER}`,
-    },
-  ])("보너스 번호가 $description 정상적으로 통과해야한다.", ({ input }) => {
-    expect(() => {
-      const winningNumbers = [11, 12, 13, 14, 15, 16];
-      validateBonusNumber(input, winningNumbers);
-    }).not.toThrow();
+  describe("잘못된 입력값에 대한 검증", () => {
+    test.each([
+      {
+        description: "숫자가 아닌 경우",
+        input: "숫자아님",
+        expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.NOT_A_NUMBER,
+      },
+      {
+        description: "정수가 아닌 경우",
+        input: "1.1",
+        expectedErrorMessage: BONUS_NUMBER_ERROR_MESSAGES.NOT_AN_INTEGER,
+      },
+    ])(
+      "보너스 번호가 $description 에러가 발생한다.",
+      ({ input, expectedErrorMessage }) => {
+        expect(() => {
+          validateBonusNumber(input, winningNumbers);
+        }).toThrow(expectedErrorMessage);
+      }
+    );
+
+    test.each([
+      {
+        description: `${MIN_LOTTO_NUMBER} 미만인 경우`,
+        input: `${MIN_LOTTO_NUMBER - 1}`,
+      },
+      {
+        description: `${MAX_LOTTO_NUMBER} 초과인 경우`,
+        input: `${MAX_LOTTO_NUMBER + 1}`,
+      },
+    ])("보너스 번호가 $description 에러가 발생한다.", ({ input }) => {
+      expect(() => {
+        validateBonusNumber(input, winningNumbers);
+      }).toThrow(BONUS_NUMBER_ERROR_MESSAGES.OUT_OF_RANGE);
+    });
+
+    test("당첨 번호와 중복된 경우 에러가 발생한다.", () => {
+      expect(() => {
+        validateBonusNumber("11", winningNumbers);
+      }).toThrow(BONUS_NUMBER_ERROR_MESSAGES.DUPLICATE_NUMBER);
+    });
+  });
+
+  describe("정상적인 입력값 검증", () => {
+    test.each([
+      {
+        description: "최소값 경계인 경우",
+        input: `${MIN_LOTTO_NUMBER}`,
+      },
+      {
+        description: "최대값 경계인 경우",
+        input: `${MAX_LOTTO_NUMBER}`,
+      },
+    ])("보너스 번호가 $description 정상적으로 통과해야 한다.", ({ input }) => {
+      expect(() => {
+        validateBonusNumber(input, winningNumbers);
+      }).not.toThrow();
+    });
   });
 });
