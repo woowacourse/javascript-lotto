ab1e089b2727f8db2c60cb99bc75db15b94811ca
deokbin <deokbin@naver.com>
2025-02-24 16:34:52 +0900

    feat: 로또 구입 금액 입력 후 구매 갯수 출력 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,22 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+import { getUIInput } from './service/InputService.js';
+import { getPurchasePrice } from './service/ParsingService.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('purchase-button');
+  button.addEventListener('click', async (event) => {
+    event.preventDefault();
+    try {
+      const { purchaseAmount } = await getPurchasePrice(getUIInput);
+      const result = document.createElement('div');
+      result.classList.add('purchase-result');
+      result.textContent = `${purchaseAmount}개를 구매하였습니다.`;
+      document.querySelector('.input-container').appendChild(result);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  });
+});
