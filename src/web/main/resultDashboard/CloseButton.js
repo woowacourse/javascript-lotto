f20d84a41fbb092e90104a0fa5cc87992860f200
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:58:08 +0900

    refactor(resultDashboard): 로또 통계 모달 닫기 버튼 함수 분리

---
+++ src/web/main/resultDashboard/CloseButton.js
@@ -0,0 +1,11 @@
+import { createElement } from '../../utils/dom';
+
+export default function CloseButton(resultDashboard, resultBackground) {
+  const closeButton = createElement('button', { textContent: 'X', class: 'close-button' });
+  closeButton.addEventListener('click', () => {
+    resultBackground.remove();
+    resultDashboard.remove();
+  });
+
+  resultDashboard.appendChild(closeButton);
+}
