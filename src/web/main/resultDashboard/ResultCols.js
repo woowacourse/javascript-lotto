010ff32ad11da0c581228aaa914ddccd1e7f7161
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 17:00:30 +0900

    refactor(resultDashboard): 로또 통계 열 헤딩 분리

---
+++ src/web/main/resultDashboard/ResultCols.js
@@ -0,0 +1,14 @@
+import { createElement } from '../../utils/dom';
+
+export default function ResultCols(resultDashboard) {
+  const resultCols = createElement('div', { class: 'result-rows' });
+
+  const typeOfMatch = createElement('div', { class: 'result-col', textContent: '일치 갯수' });
+  resultCols.appendChild(typeOfMatch);
+  const winningPrize = createElement('div', { class: 'result-col', textContent: '당첨금' });
+  resultCols.appendChild(winningPrize);
+  const matchCounts = createElement('div', { class: 'result-col', textContent: '당첨 횟수' });
+  resultCols.appendChild(matchCounts);
+
+  resultDashboard.appendChild(resultCols);
+}
