417e7688091045f6e8042383d099a387d46923e8
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 17:01:23 +0900

    refactor(resultDashboard): 로또 통계 결과 행 함수 분리

---
+++ src/web/main/resultDashboard/ResultRow.js
@@ -0,0 +1,14 @@
+import { createElement } from '../../utils/dom';
+
+export default function ResultRow(type, prize, count) {
+  const resultRow = createElement('div', { class: 'result-rows' });
+
+  const typeOfMatch = createElement('div', { class: 'result-col', textContent: type });
+  resultRow.appendChild(typeOfMatch);
+  const winningPrize = createElement('div', { class: 'result-col', textContent: prize });
+  resultRow.appendChild(winningPrize);
+  const matchCounts = createElement('div', { class: 'result-col', textContent: count });
+  resultRow.appendChild(matchCounts);
+
+  return resultRow;
+}
