200d5400f4449e4c59100603cbf61ea974b1dbae
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:59:05 +0900

    refactor(resultDashboard): 로또 재시작 버튼 함수 분리

---
+++ src/web/main/resultDashboard/ReplayLottoButton.js
@@ -0,0 +1,10 @@
+import { createElement } from '../../utils/dom';
+
+export default function ReplayLottoButton(resultDashboard) {
+  const replayLottoButton = createElement('button', { class: 'replay-lotto-button', textContent: '다시 구매하기' });
+  replayLottoButton.addEventListener('click', () => {
+    location.reload();
+  });
+
+  resultDashboard.appendChild(replayLottoButton);
+}
