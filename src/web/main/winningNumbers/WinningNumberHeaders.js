d5e13b3b3fc9456d1a9ba638e3538d428b2c02c1
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 17:31:27 +0900

    refactor(winningNumbers): 당첨 번호 및 보너스 번호 헤더 컴포넌트 함수 분리

---
+++ src/web/main/winningNumbers/WinningNumberHeaders.js
@@ -0,0 +1,11 @@
+import { createElement } from '../../utils/dom';
+
+export default function WinningNumberHeaders(winningNumberContainer) {
+  const winningNumberHeaders = createElement('div', { class: 'winning-number-headers' });
+  const winningNumberHeader = createElement('span', { class: 'header', textContent: '당첨 번호' });
+  const bonusNumberHeader = createElement('span', { class: 'header', textContent: '보너스 번호' });
+
+  winningNumberHeaders.appendChild(winningNumberHeader);
+  winningNumberHeaders.appendChild(bonusNumberHeader);
+  winningNumberContainer.appendChild(winningNumberHeaders);
+}
