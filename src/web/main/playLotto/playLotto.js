7ed3f28047fef01b5efde1ef26e4bd73c32d8c4c
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:25:23 +0900

    refactor(playLotto): 함수로 분리한 컴포넌트들 적용

--- src/web/main/playLotto/playLotto.js
+++ src/web/main/playLotto/playLotto.js
@@ -1,70 +1,39 @@
-import { calculateRevenue } from '../../../domain/calculateRevenue';
-import { getRandomLottos } from '../../../domain/getRandomLottos';
-import { getWinningMatchCount } from '../../../domain/getWinningMatchCount';
-import { LOTTO } from '../../../domain/lottoConstants';
-import { validateBonusNumber, validateWinningNumbers } from '../../../validation/validateLottoNumbers';
-import randomLottos from '../randomLottos/RandomLottos';
-import ResultDashboard from '../resultDashboard/ResultDashboard';
-import { getArrayOfStrings } from '../utils/getArrayOfStrings';
-import WinningNumbers from '../winningNumbers/winningNumbers';
 import './PlayLotto.css';
+import { LOTTO } from '../../../domain/lottoConstants';
+import { getRandomLottos } from '../../../domain/getRandomLottos';
+import { getArrayOfStrings } from '../../utils/getArrayOfStrings';
+import randomLottos from '../randomLottos/RandomLottos';
+import WinningNumbers from '../winningNumbers/winningNumbers';
+import { createElement } from '../../utils/dom';
+import PurchaseForm from './PurchaseForm';
+import LottoResultModal from './LottoResultModal';
 
 export default function PlayLotto() {
-  const playLotto = document.createElement('div');
-  playLotto.className = 'play-lotto';
-  const inputContainer = document.createElement('div');
-  inputContainer.className = 'input-container';
-
-  const purchasePriceHeader = document.createElement('p');
-  purchasePriceHeader.innerText = '구매 금액을 입력해주세요.';
-  purchasePriceHeader.className = 'header';
-  playLotto.appendChild(purchasePriceHeader);
-
-  const priceInput = document.createElement('input');
-  priceInput.type = 'text';
-  priceInput.placeholder = '금액';
-
-  const purchaseButton = document.createElement('button');
-  purchaseButton.innerText = '구매';
-
-  inputContainer.appendChild(priceInput);
-  inputContainer.appendChild(purchaseButton);
-
-  playLotto.appendChild(inputContainer);
+  const playLotto = createElement('div', { class: 'play-lotto' });
+  const { priceInput, purchaseButton } = PurchaseForm(playLotto);
 
   purchaseButton.addEventListener('click', () => {
     try {
       const lottoQuantity = priceInput.value / LOTTO.MIN_PURCHASE_PRICE;
+
       const randomlottos = getRandomLottos(lottoQuantity);
       randomLottos(playLotto, getArrayOfStrings(randomlottos));
 
-      const winningNumberInputHeader = document.createElement('p');
-      winningNumberInputHeader.innerText = '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.';
-      winningNumberInputHeader.className = 'header';
-
+      const winningNumberInputHeader = createElement('span', {
+        class: 'header',
+        textContent: '지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.',
+      });
       playLotto.appendChild(winningNumberInputHeader);
 
       const { winningNumbersArray, bonusNumber } = WinningNumbers(playLotto);
 
-      const resultButton = document.createElement('button');
-      resultButton.innerText = '결과 확인하기';
-      resultButton.className = 'result-button';
-      resultButton.addEventListener('click', () => {
-        try {
-          validateWinningNumbers(winningNumbersArray.join(','));
-          validateBonusNumber(bonusNumber.value, winningNumbersArray);
-          const matchCounts = getWinningMatchCount(randomlottos, {
-            winningNumbers: winningNumbersArray,
-            bonusNumber: bonusNumber.value,
-          });
-
-          const revenue = calculateRevenue(matchCounts, priceInput.value);
-
-          ResultDashboard(playLotto, matchCounts, revenue);
-        } catch (error) {
-          alert(error.message);
-        }
-      });
+      const resultButton = LottoResultModal(
+        { priceInput, playLotto, randomlottos },
+        {
+          winningNumbers: winningNumbersArray,
+          bonusNumber: bonusNumber,
+        },
+      );
 
       playLotto.appendChild(resultButton);
     } catch (error) {
