840cb3caa31e9675e7928b1e6fbb942930966705
Jeongeun Lee <leeju1009@gmail.com>
2025-02-25 14:02:49 +0900

    refactor: winningLottoBox를 감싸는 div를 html 파일에 직접 작성

--- src/view/web/layers/winningLottoBox/createWinningLottoBox.js
+++ src/view/web/layers/winningLottoBox/createWinningLottoBox.js
@@ -1,9 +1,7 @@
 import winningLottoBoxContents from "./winningLottoBox.html?raw";
 
 const createWinningLottoBox = () => {
-  const div = document.createElement("div");
-  div.id = "winningLotto-container";
-  div.className = "purchased-rendered";
+  const div = document.getElementById("winningLotto-container");
   div.innerHTML = winningLottoBoxContents;
 
   document.getElementById("game-container").appendChild(div);
