693e719b294fbaad8d76723cfa82acd21587cd2d
sooyeoniya <tndus1306403@naver.com>
2025-02-25 16:43:54 +0900

    refactor: InputService -> InputController로 변경
    - Service가 아닌 Controller의 역할이 맞다고 판단

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -1,4 +1,4 @@
-import InputService from "../services/InputService.js";
+import InputController from "./InputController.js";
 import issueLottos from "../domains/issueLottos.js";
 import OutputView from "../views/OutputView.js";
 import WinningStatistics from "../domains/WinningStatistics.js";
@@ -7,17 +7,18 @@ import { YES } from "../constants/constants.js";
 class Controller {
   async start() {
     await this.#runLottoGame();
-    const restartConfirm = await InputService.getValidRestartConfirm();
+    const restartConfirm = await InputController.getValidRestartConfirm();
     if (restartConfirm === YES) await this.start();
   }
 
   async #runLottoGame() {
-    const purchaseAmount = await InputService.getValidPurchaseAmount();
+    const purchaseAmount = await InputController.getValidPurchaseAmount();
     const lottos = issueLottos(purchaseAmount);
     OutputView.printIssuedLottos(lottos);
 
-    const winningNumbers = await InputService.getValidWinningNumbers();
-    const bonusNumber = await InputService.getValidBonusNumber(winningNumbers);
+    const winningNumbers = await InputController.getValidWinningNumbers();
+    const bonusNumber =
+      await InputController.getValidBonusNumber(winningNumbers);
 
     const winningStatistics = new WinningStatistics(lottos);
     winningStatistics.calculateWinningResults(winningNumbers, bonusNumber);
--- src/step1-index.js
+++ src/step1-index.js
@@ -3,7 +3,7 @@
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-import Controller from "./controllers/Controller.js";
+import LottoController from "./controllers/LottoController.js";
 
-const controller = new Controller();
-await controller.start();
+const lottoController = new LottoController();
+await lottoController.start();
