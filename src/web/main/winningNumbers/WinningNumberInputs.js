00371f19527f872cb401395b6f89ec354ffc084e
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 17:31:43 +0900

    refactor(winningNumbers): 당첨 번호 및 보너스 번호 입력 컴포넌트 함수 분리

---
+++ src/web/main/winningNumbers/WinningNumberInputs.js
@@ -0,0 +1,37 @@
+import { createElement } from '../../utils/dom';
+
+export default function WinningNumberInputs(winningNumberContainer) {
+  const winningAndBonusInputContainer = createElement('div', { class: 'winning-and-bonus-input-container' });
+
+  const winningNumbersArray = new Array(6).fill(0);
+  const bonusNumber = { value: 0 };
+  const winningInputs = createElement('div', { class: 'winning-inputs' });
+
+  CreateSixWinningInputs(winningAndBonusInputContainer, winningInputs, winningNumbersArray);
+  CreateBonusInput(winningAndBonusInputContainer, bonusNumber);
+
+  winningNumberContainer.appendChild(winningAndBonusInputContainer);
+  return { winningNumbersArray, bonusNumber };
+}
+
+function CreateSixWinningInputs(winningAndBonusInputContainer, winningInputs, winningNumbersArray) {
+  Array.from({ length: 6 }, (_, idx) => {
+    const winningNumberInput = createElement('input', { type: 'text', class: 'winning-input' });
+    winningNumberInput.addEventListener('input', (e) => {
+      winningNumbersArray[idx] = Number(e.target.value);
+    });
+
+    winningInputs.appendChild(winningNumberInput);
+  });
+
+  winningAndBonusInputContainer.appendChild(winningInputs);
+}
+
+function CreateBonusInput(winningAndBonusInputContainer, bonusNumber) {
+  const bonusNumberInput = createElement('input', { type: 'text', class: 'bonus-input' });
+  bonusNumberInput.addEventListener('input', (e) => {
+    bonusNumber.value = Number(e.target.value);
+  });
+
+  winningAndBonusInputContainer.appendChild(bonusNumberInput);
+}
