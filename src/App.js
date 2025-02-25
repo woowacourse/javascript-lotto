75b79950dc41934505513c0b39235a1a515b42ce
eunoia-jaxson <eunoia@handong.ac.kr>
2025-02-25 15:24:42 +0900

    refactor: App에서 입력 검증 로직 View로 분리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { PURCHASE_PRICE, RESTART, SEPARATOR } from './constants/CONFIGURATIONS.js';
+import { RESTART } from './constants/CONFIGURATIONS.js';
 import LottoMachine from './domains/LottoMachine.js';
-import WinningResult from './domains/WinningResult.js';
 import retryUntilValid from './utils/retryUntilValid.js';
-import { BonusNumberValidator } from './validators/BonusNumberValidator.js';
-import { PurchasePriceValidator } from './validators/PurchasePriceValidator.js';
-import RestartValidator from './validators/RestartValidator.js';
-import { WinningNumbersValidator } from './validators/WinningNumbersValidator.js';
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
 
 class App {
   async run() {
     await this.#start();
-    const restartInput = await retryUntilValid(this.#getRestart);
+    const restartInput = await retryUntilValid(InputView.enterRestart);
 
     if (restartInput.toLowerCase() === RESTART.YES) {
       await this.run();
@@ -20,56 +15,37 @@ class App {
   }
 
   async #start() {
-    const [lottoPurchasePrice, lottoMachine] = await this.#processLottoPurchase();
-    const winningResult = await this.#preProcess();
+    const [lottoPurchasePrice, lottoMachine] =
+      await this.#processLottoPurchase();
+    const [winningNumbers, bonusNumber] = await this.#preProcess();
+    const [winningCounts, profitRate] = lottoMachine.calculateResult(
+      {
+        winningNumbers,
+        bonusNumber,
+      },
+      lottoPurchasePrice,
+    );
 
-    const winningCounts = winningResult.calculate(lottoMachine.lottos);
-    const profitRate = winningResult.calculateProfitRate(lottoPurchasePrice, winningCounts);
     OutputView.printResult(winningCounts, profitRate);
   }
 
   async #processLottoPurchase() {
-    const [lottoPurchasePrice, lottoCount] = await retryUntilValid(this.#getPurchasePrice);
-    const lottoMachine = new LottoMachine(lottoCount);
+    const lottoPurchasePrice = await retryUntilValid(
+      InputView.enterPurchasePrice,
+    );
+    const lottoMachine = new LottoMachine(lottoPurchasePrice);
     OutputView.printPurchaseLottos(lottoMachine.lottos);
 
     return [lottoPurchasePrice, lottoMachine];
   }
 
   async #preProcess() {
-    const winningNumbers = await retryUntilValid(this.#getWinningNumbers);
-    const bonusNumber = await retryUntilValid(() => {
-      return this.#getBonusNumber(winningNumbers);
-    });
+    const winningNumbers = await retryUntilValid(InputView.enterWinningNumbers);
+    const bonusNumber = await retryUntilValid(() =>
+      InputView.enterBonusNumber(winningNumbers),
+    );
 
-    return new WinningResult(winningNumbers, bonusNumber);
-  }
-
-  async #getPurchasePrice() {
-    const lottoPurchasePrice = await InputView.enterPurchasePrice();
-    PurchasePriceValidator.validate(Number(lottoPurchasePrice));
-    const lottoCount = lottoPurchasePrice / PURCHASE_PRICE.UNIT;
-    return [lottoPurchasePrice, lottoCount];
-  }
-
-  async #getWinningNumbers() {
-    const winningNumbers = await InputView.enterWinningNumbers();
-    const splittedWinningNumbers = winningNumbers.split(SEPARATOR).map(Number);
-    WinningNumbersValidator.validate(splittedWinningNumbers);
-    return splittedWinningNumbers;
-  }
-
-  async #getBonusNumber(winningNumbers) {
-    const bonusNumber = await InputView.enterBonusNumber();
-    const transformedBonusNumber = Number(bonusNumber);
-    BonusNumberValidator.validate(transformedBonusNumber, winningNumbers);
-    return transformedBonusNumber;
-  }
-
-  async #getRestart() {
-    const restartInput = await InputView.enterRestart();
-    RestartValidator.validate(restartInput);
-    return restartInput;
+    return [winningNumbers, bonusNumber];
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,21 +1,40 @@
+import { SEPARATOR } from '../constants/CONFIGURATIONS.js';
 import { SYSTEM_MESSAGE } from '../constants/MESSAGES.js';
 import readLineAsync from '../utils/readLineAsync.js';
+import { BonusNumberValidator } from '../validators/BonusNumberValidator.js';
+import { PurchasePriceValidator } from '../validators/PurchasePriceValidator.js';
+import RestartValidator from '../validators/RestartValidator.js';
+import { WinningNumbersValidator } from '../validators/WinningNumbersValidator.js';
 
 const InputView = {
   async enterPurchasePrice() {
-    return await readLineAsync(SYSTEM_MESSAGE.ENTER_PURCHASE_PRICE);
+    const purchasePrice = await readLineAsync(
+      SYSTEM_MESSAGE.ENTER_PURCHASE_PRICE,
+    );
+    PurchasePriceValidator.validate(Number(purchasePrice));
+    return purchasePrice;
   },
 
   async enterWinningNumbers() {
-    return await readLineAsync(SYSTEM_MESSAGE.ENTER_WINNING_NUMBERS);
+    const winningNumbers = await readLineAsync(
+      SYSTEM_MESSAGE.ENTER_WINNING_NUMBERS,
+    );
+    const splittedWinningNumbers = winningNumbers.split(SEPARATOR).map(Number);
+    WinningNumbersValidator.validate(splittedWinningNumbers);
+    return splittedWinningNumbers;
   },
 
-  async enterBonusNumber() {
-    return await readLineAsync(SYSTEM_MESSAGE.ENTER_BONUS_NUMBER);
+  async enterBonusNumber(winningNumbers) {
+    const bonusNumber = await readLineAsync(SYSTEM_MESSAGE.ENTER_BONUS_NUMBER);
+    const transformedBonusNumber = Number(bonusNumber);
+    BonusNumberValidator.validate(transformedBonusNumber, winningNumbers);
+    return transformedBonusNumber;
   },
 
   async enterRestart() {
-    return await readLineAsync(SYSTEM_MESSAGE.ENTER_RESTART);
+    const restartInput = await readLineAsync(SYSTEM_MESSAGE.ENTER_RESTART);
+    RestartValidator.validate(restartInput);
+    return restartInput;
   },
 };
 
