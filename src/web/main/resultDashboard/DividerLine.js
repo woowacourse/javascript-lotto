c43289101e6928aedefeb9a90b90d50dc94338b8
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:58:27 +0900

    refactor(resultDashboard): 로또 통계 행 구분선 함수 분리

---
+++ src/web/main/resultDashboard/DividerLine.js
@@ -0,0 +1,6 @@
+import { createElement } from '../../utils/dom';
+
+export default function DividerLine(resultContainer) {
+  const dividerLine = createElement('div', { class: 'divider-line' });
+  resultContainer.appendChild(dividerLine);
+}
