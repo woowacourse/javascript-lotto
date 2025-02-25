8c7d00254d74762ce584ee1da3cbcdef58c89fc3
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 14:46:48 +0900

    refactor(footer): 커스텀 createElement 함수 통해 객체로 속성 적용

--- src/web/footer/Footer.js
+++ src/web/footer/Footer.js
@@ -1,10 +1,9 @@
+import { createElement } from '../utils/dom';
 import './footer.css';
 
 export default function Footer() {
-  const footer = document.createElement('footer');
-  const text = document.createElement('p');
-
-  text.innerText = 'Copyright 2025. woowacourse';
+  const footer = createElement('footer');
+  const text = createElement('span', { textContent: 'Copyright 2025. woowacourse' });
 
   footer.appendChild(text);
   return footer;
