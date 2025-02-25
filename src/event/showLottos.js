25b7b47a2a1138e21283c39390084c20d1ec45ad
Ko Daeun <drechsu@gmail.com>
2025-02-25 17:37:51 +0900

    [FEAT] : 개수대로 로또 발행 및 로또번호 렌더링 기능 구현

--- src/event/showLottos.js
+++ src/event/showLottos.js
@@ -1,10 +1,25 @@
+import Ticket from "../domain/Ticket.js";
 const showLottos = (count) => {
   const whenBuyed = document.querySelector(".whenBuyed");
   const buyedLottos = document.querySelector(".buyedLottos");
   const text = buyedLottos.querySelector(".text");
+  const lottosNumber = buyedLottos.querySelector(".lottosNumber");
 
   whenBuyed.style.display = "block";
   text.textContent = `총 ${count}개를 구매했습니다.`;
+
+  const lottos = Ticket.createLottos(count);
+  const ul = document.createElement("ul");
+  lottosNumber.appendChild(ul);
+
+  for (let i = 0; lottos.length; i++) {
+    const lotto = lottos[i];
+    const li = document.createElement("li");
+    li.textContent = lotto.getLottoNumbers().join(", ");
+    ul.appendChild(li);
+    console.log(li);
+  }
+
   console.log("개수", count);
 };
 
