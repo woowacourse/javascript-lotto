a29ae187f4b5e2f41a698ae0d1ac7bf01d467090
Ko Daeun <drechsu@gmail.com>
2025-02-25 17:27:28 +0900

    [FEAT] : 구매한 로또 개수 렌더링 로직 작성

--- src/event/showLottos.js
+++ src/event/showLottos.js
@@ -1,6 +1,10 @@
 const showLottos = (count) => {
   const whenBuyed = document.querySelector(".whenBuyed");
+  const buyedLottos = document.querySelector(".buyedLottos");
+  const text = buyedLottos.querySelector(".text");
+
   whenBuyed.style.display = "block";
+  text.textContent = `총 ${count}개를 구매했습니다.`;
   console.log("개수", count);
 };
 
