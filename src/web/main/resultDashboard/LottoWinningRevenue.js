50a674c43b935b8748c6ebb55d0cc30220720159
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:58:48 +0900

    refactor(resultDashboard): 로또 통계 수익률 결과 함수 분리

---
+++ src/web/main/resultDashboard/LottoWinningRevenue.js
@@ -0,0 +1,6 @@
+import { createElement } from '../../utils/dom';
+
+export default function LottoWinningRevenue(resultDashboard, revenue) {
+  const revenueTag = createElement('div', { class: 'revenue', textContent: `당신의 총 수익률은 ${revenue}%입니다.` });
+  resultDashboard.appendChild(revenueTag);
+}
