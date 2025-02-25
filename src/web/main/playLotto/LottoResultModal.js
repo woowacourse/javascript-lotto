445cbfbc37f1554bbbd1b9d126890846e58dc279
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 16:24:56 +0900

    refactor(playLotto): 로또 통계 결과 모달 컴포넌트 코드 분리

---
+++ src/web/main/playLotto/LottoResultModal.js
@@ -0,0 +1,30 @@
+import { createElement } from '../../utils/dom';
+import { validateWinningNumbers, validateBonusNumber } from '../../../validation/validateLottoNumbers';
+import { getWinningMatchCount } from '../../../domain/getWinningMatchCount';
+import { calculateRevenue } from '../../../domain/calculateRevenue';
+import ResultDashboard from '../resultDashboard/ResultDashboard';
+
+export default function LottoResultModal({ priceInput, playLotto, randomlottos }, { winningNumbers, bonusNumber }) {
+  const resultButton = createElement('button', { class: 'result-button', textContent: '결과 확인하기' });
+
+  resultButton.addEventListener('click', () => {
+    try {
+      validateWinningAndBonusNumbers(winningNumbers, bonusNumber);
+      const matchCounts = getWinningMatchCount(randomlottos, {
+        winningNumbers,
+        bonusNumber: bonusNumber.value,
+      });
+      const revenue = calculateRevenue(matchCounts, priceInput.value);
+
+      ResultDashboard(playLotto, matchCounts, revenue);
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+  return resultButton;
+}
+
+function validateWinningAndBonusNumbers(winningNumbers, bonusNumber) {
+  validateWinningNumbers(winningNumbers.join(','));
+  validateBonusNumber(bonusNumber.value, winningNumbers);
+}
