1ac754f9fb58d6fbb84b8ec0e2f89791bc330a19
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 14:46:33 +0900

    refactor(header): 커스텀 createElement 함수 통해 객체로 속성 적용

--- src/web/header/Header.js
+++ src/web/header/Header.js
@@ -1,10 +1,9 @@
+import { createElement } from '../utils/dom';
 import './header.css';
 
 export default function Header() {
-  const header = document.createElement('header');
-  const title = document.createElement('h1');
-
-  title.innerText = '🎱행운의 로또';
+  const header = createElement('header');
+  const title = createElement('h1', { textContent: '🎱행운의 로또' });
 
   header.appendChild(title);
   return header;
