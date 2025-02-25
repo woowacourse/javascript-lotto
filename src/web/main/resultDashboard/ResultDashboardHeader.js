6ed8e7ef74eeb04bcd01a856bcfb31c25028d1a5
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-25 17:00:47 +0900

    refactor(resultDashboard): 로또 대시보드 헤더 함수 분리

---
+++ src/web/main/resultDashboard/ResultDashboardHeader.js
@@ -0,0 +1,6 @@
+import { createElement } from '../../utils/dom';
+
+export default function ResultDashboardHeader(resultDashboard) {
+  const resultHeader = createElement('h1', { class: 'result-header', textContent: '🏆 당첨 통계 🏆' });
+  resultDashboard.appendChild(resultHeader);
+}
