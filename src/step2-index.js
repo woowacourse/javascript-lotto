a0cefc9dcecf7d150488156d0c8940bc0dbc12cc
mun-kyeong <horse6953@naver.com>
2025-02-26 16:48:34 +0900

    [feat] : 생성된 로또를 화면에 출력

--- src/step2-index.js
+++ src/step2-index.js
@@ -28,13 +28,39 @@ const printLottoCount = (price) => {
   }개를 구매하였습니다.`;
   lottoContents.appendChild(lottoCountText);
 };
-const printUserLottos = (lottos) => {};
+
+const createLottoObject = (lotto) => {
+  const lottoContainer = document.createElement("div");
+  lottoContainer.className = "lotto-container_lotto";
+  const lottoImage = document.createElement("img");
+  lottoImage.src = "./src/assets/lotto.png";
+  const lottoNumbers = document.createElement("p");
+  lottoNumbers.innerText = lotto.numbers.map((number) => number).join(", ");
+
+  lottoContainer.appendChild(lottoImage);
+  lottoContainer.appendChild(lottoNumbers);
+
+  return lottoContainer;
+};
+
+const printUserLottos = (lottos) => {
+  const lottoContents = document.querySelector(".lotto-contents");
+  const lottosContainer = document.createElement("div");
+
+  const lottoObjects = lottos.map((lotto) => createLottoObject(lotto));
+  lottoObjects.forEach((lottoObject) => {
+    lottosContainer.appendChild(lottoObject);
+  });
+
+  lottoContents.appendChild(lottosContainer);
+};
 
 async function run() {
   while (true) {
     const price = await getPrice();
     printLottoCount(price);
     const lottos = LottoManager.generateLottos(price);
+    printUserLottos(lottos);
   }
 }
 run();
