ddcbe933ad9e997ef5cba92aecf6fd8d872fc05c
kimyou1102 <yoosk5485@naver.com>
2025-02-26 14:27:22 +0900

    chore: 불필요한 주석 제거

--- src/components/LottoHistoryItem.js
+++ src/components/LottoHistoryItem.js
@@ -10,7 +10,6 @@ export default class LottoHistoryItem {
   render($target) {
     const $li = document.createElement("li");
     $li.className = "lotto-history-item";
-    // TODO: 복권 아이콘 이미지로 변경하기
     $li.innerHTML = `
         <span class="lotto-history-item-icon">🎟️</span>
         <span class="lotto-history-item-text">${this.#lottoNumbers.join(
