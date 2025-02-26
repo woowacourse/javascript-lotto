5f9be8d5013f5453269f10b6708b2d7a44841c71
Ko Daeun <drechsu@gmail.com>
2025-02-26 21:41:27 +0900

    [style] : 전체 스타일대로 css 작성

--- src/event/showLottos.js
+++ src/event/showLottos.js
@@ -2,25 +2,24 @@ import Ticket from "../domain/Ticket.js";
 import lottoStore from "../store/lottoStore.js";
 const showLottos = (count) => {
   const whenBuyed = document.querySelector(".whenBuyed");
-
+  whenBuyed.style.display = "flex";
   const buyedLottos = document.querySelector(".buyedLottos");
 
   const text = document.createElement("div");
   text.classList.add("text");
   buyedLottos.appendChild(text);
 
-  const lottosNumber = document.createElement("div");
-  lottosNumber.classList.add("lottosNumber");
-  buyedLottos.appendChild(lottosNumber);
+  const lottosNumbers = document.createElement("div");
+  lottosNumbers.classList.add("lottosNumbers");
+  buyedLottos.appendChild(lottosNumbers);
 
-  whenBuyed.style.display = "block";
-  text.textContent = `총 ${count}개를 구매했습니다.`;
+  text.textContent = `총 ${count}개를 구매하였습니다.`;
 
   const lottos = Ticket.createLottos(count);
   console.log(lottos);
   lottoStore.setLottos(lottos);
   const ul = document.createElement("ul");
-  lottosNumber.appendChild(ul);
+  lottosNumbers.appendChild(ul);
 
   for (let i = 0; i < lottos.length; i++) {
     const lotto = lottos[i];
