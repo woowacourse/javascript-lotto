02edace6102a9f25a115175e4b98bc41857f480c
ahhyun <wndkgus0225@naver.com>
2025-02-27 03:22:34 +0900

    refactor:constants.js 상수 이름 변경

--- src/constants/validateConstants.js
+++ src/constants/validateConstants.js
@@ -6,6 +6,6 @@ export const UPPER_NO = NO.toUpperCase();
 export const MIN_UNIT = 1_000;
 export const MAX_AMOUNT = 100_000;
 
-export const MIN_LOTTO_NUMBER = 1;
-export const MAX_LOTTO_NUMBER = 45;
-export const LOTTO_LENGTH = 6;
+export const LOTTO_NUMBER_MIN = 1;
+export const LOTTO_NUMBER_MAX = 45;
+export const LOTTO_SIZE = 6;
