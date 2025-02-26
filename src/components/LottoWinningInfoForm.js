5a50e1b191a67e686ace14675897c2792c2ba2e0
kimyou1102 <yoosk5485@naver.com>
2025-02-26 16:45:29 +0900

    refactor: "결과 확인하기" 버튼 클릭 이벤트 핸들러에서 콜백 함수 분리
    - handleResultButtonClick 메서드 생성

--- src/components/LottoWinningInfoForm.js
+++ src/components/LottoWinningInfoForm.js
@@ -36,41 +36,38 @@ export default class LottoWinningInfoForm {
     </div>
 `;
 
-    new Button(
-      $form,
-      () => {
-        const $lottoNumbers = document.querySelectorAll(
-          ".lotto-numbers-wrap > .number-input"
-        );
-        const winningNumbers = [];
-        $lottoNumbers.forEach(($lottoNumber) => {
-          winningNumbers.push(Number($lottoNumber.value));
-        });
-
-        try {
-          new Lotto(winningNumbers);
-        } catch (e) {
-          alert(e.message);
-          return;
-        }
-
-        const $bonusNumber = document.querySelector(
-          ".bonus-input-wrap > .number-input"
-        );
-
-        const bonusNumber = Number($bonusNumber.value);
-
-        try {
-          validateBonusNumber(winningNumbers, bonusNumber);
-        } catch (e) {
-          alert(e.message);
-          return;
-        }
-
-        this.#setWinningLottoInfo({ winningNumbers, bonusNumber });
-      },
-      "결과 확인하기"
-    );
+    new Button($form, this.handleResultButtonClick, "결과 확인하기");
     $target.appendChild($form);
   }
+
+  handleResultButtonClick = () => {
+    const $lottoNumbers = document.querySelectorAll(
+      ".lotto-numbers-wrap > .number-input"
+    );
+    const winningNumbers = [];
+    $lottoNumbers.forEach(($lottoNumber) => {
+      winningNumbers.push(Number($lottoNumber.value));
+    });
+
+    try {
+      new Lotto(winningNumbers);
+    } catch (e) {
+      alert(e.message);
+      return;
+    }
+
+    const $bonusNumber = document.querySelector(
+      ".bonus-input-wrap > .number-input"
+    );
+    const bonusNumber = Number($bonusNumber.value);
+
+    try {
+      validateBonusNumber(winningNumbers, bonusNumber);
+    } catch (e) {
+      alert(e.message);
+      return;
+    }
+
+    this.#setWinningLottoInfo({ winningNumbers, bonusNumber });
+  };
 }
