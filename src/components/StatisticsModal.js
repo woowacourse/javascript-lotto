3e8cc87540df72b3548b4a4a328c87847b00413d
장정안 <wjddks96@naver.com>
2025-02-26 22:58:04 +0900

    feat: 당첨 통계 모달 컴포넌트 구현

---
+++ src/components/StatisticsModal.js
@@ -0,0 +1,68 @@
+import { LOTTO_RANK } from "../lib/constants";
+import Component from "./Component";
+
+export default class StatisticsModal extends Component {
+  setEvent() {
+    const { reset } = this.props || {};
+    this.addEvent("click", ".retry-btn", reset.bind(this));
+  }
+
+  template() {
+    return `
+    <dialog class='statistics-dialog'>
+        <div class='statistics-dialog-contents'>
+          <form class='statistics-dialog-close-form'>
+            <button class='dialog-close-btn'>X</button>
+          </form>
+          <h2 class='statistics-title lotto-subtitle'>🏆 당첨 통계 🏆</h2>
+          <div class='statistics-table'>
+            <div class='statistics-label body'>
+              <span class='awards-label'>일치 갯수</span>
+              <span class='prize-label'>당첨금</span>
+              <span class='count-label'>당첨 갯수</span>
+            </div>
+            <ul>
+            ${this.getStatisticsTemplate()}
+            </ul>
+          </div>
+          <div class='earning-rate'>당신의 총 수익률은 ${this.earningRate()}%입니다.
+          </div>
+          <form class='statistics-dialog-retry-form' method="dialog">
+            <button class='retry-btn'>다시 시작하기</button>
+          </form>
+        </div>
+      </dialog>  
+    `;
+  }
+
+  earningRate() {
+    const { lottoResults, lottoList } = this.props;
+    if (!lottoResults) return 0;
+
+    const { profit } = lottoResults;
+    const totalPurchaseAmount = lottoList.lottoList.length * 1000;
+
+    return ((profit / totalPurchaseAmount) * 100).toFixed(1);
+  }
+
+  getStatisticsTemplate() {
+    const { ranks } = this.props.lottoResults || { ranks: [] };
+
+    return Object.entries(LOTTO_RANK)
+      .map(([rank, { winNumber, isBonusNumberRequired, prize }]) => {
+        const matchDescription = isBonusNumberRequired
+          ? `${winNumber}개 + 보너스볼`
+          : `${winNumber}개`;
+
+        const count = ranks ? ranks.filter((r) => r === rank).length : 0;
+
+        return `
+          <li class='statistics'>
+            <span class='awards'>${matchDescription}</span>
+            <span class='prize'>${prize.toLocaleString()}원</span>
+            <span class='count'>${count}개</span>
+          </li>`;
+      })
+      .join("");
+  }
+}
