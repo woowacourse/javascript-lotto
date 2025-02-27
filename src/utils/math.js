3f9f5eb9dd9de46b997ef81f4713f2fdb8a8c72d
Jeongeun Lee <leeju1009@gmail.com>
2025-02-27 16:46:01 +0900

    feat: 숫자를 반올림하는 유틸 함수 추가

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,3 +1,8 @@
 export const getRevenueRate = (revenue, cost) => {
   return (revenue / cost) * 100;
 };
+
+export const roundNumber = (number, roundDigits) => {
+  const fixedNumber = Number(number.toFixed(roundDigits));
+  return fixedNumber;
+};
