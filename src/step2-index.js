688dae84f0dcc04c73e8f2c71a5c743cd374f3a9
deokbin <deokbin@naver.com>
2025-02-27 23:36:31 +0900

    feat: 재시도 시 다시 실행을 위해 eventlistener 제거 및 함수 분리

--- src/step2-index.js
+++ src/step2-index.js
@@ -3,6 +3,7 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
+import clearUIElements from './clearUIElements.js';
 import { PRIZE_MONEY } from './constants/MagicNumber.js';
 import createLottoInput from './createLottoInput.js';
 import createModal from './createModal.js';
@@ -15,11 +16,13 @@ import {
 import {
   getUIBonusNumber,
   getUIPurchasePrice,
+  getUIUserRetry,
   getUIWinningNumber,
 } from './service/InputService.js';
 import {
   getBonusNumber,
   getPurchasePrice,
+  getUserRetry,
   getWinningNumber,
 } from './service/ParsingService.js';
 import showLottoResult from './showLottoResult.js';
@@ -28,46 +31,65 @@ import showPurchaseResult from './showPurchaseResult.js';
 document.addEventListener('DOMContentLoaded', () => {
   const purchaseButton = document.getElementById('purchase-button');
   let lottos = [];
-  purchaseButton.addEventListener('click', async (event) => {
+  let purchasePrice = 0;
+  async function handlePurchase(event) {
     event.preventDefault();
     try {
-      const { purchasePrice, purchaseAmount } = await getPurchasePrice(
-        getUIPurchasePrice,
+      const { purchasePrice: price, purchaseAmount } = await getPurchasePrice(
         getUIPurchasePrice,
       );
+      purchasePrice = price;
 
       showPurchaseResult(purchaseAmount);
-
+      purchaseButton.disabled = true;
       showLottoResult(lottos, purchaseAmount, purchaseButton);
-
       createLottoInput();
 
       const resultButton = document.getElementById('check-result-btn');
 
-      resultButton.addEventListener('click', async (event) => {
-        event.preventDefault();
-
-        const userLotto = await getWinningNumber(getUIWinningNumber);
-        const parsedLotto = await getBonusNumber(userLotto, getUIBonusNumber);
-
-        const winCount = calculateWins(lottos, parsedLotto);
-        const total = calculatePrize(winCount, PRIZE_MONEY);
-        const revenueRate = calculateRevenueRate(total, purchasePrice);
-        resultButton.disabled = true;
-
-        const modalOverlay = createModalOverlay();
-
-        const modal = createModal(winCount, revenueRate, modalOverlay);
-        const closeButton = document.getElementById('close-button');
-        closeButton.addEventListener('click', () => {
-          event.preventDefault();
-          modal.remove();
-          modalOverlay.remove();
-          document.querySelector('.lotto-content').remove();
-        });
-      });
+      resultButton.removeEventListener('click', handleResult);
+      resultButton.addEventListener('click', handleResult);
+      return purchasePrice;
     } catch (error) {
       console.log(error);
     }
-  });
+  }
+
+  async function handleResult(event) {
+    event.preventDefault();
+
+    const userLotto = await getWinningNumber(getUIWinningNumber);
+    const parsedLotto = await getBonusNumber(userLotto, getUIBonusNumber);
+
+    const winCount = calculateWins(lottos, parsedLotto);
+    console.log(winCount);
+    const total = calculatePrize(winCount, PRIZE_MONEY);
+    const revenueRate = calculateRevenueRate(total, purchasePrice);
+
+    const modalOverlay = createModalOverlay();
+    const modal = createModal(winCount, revenueRate, modalOverlay);
+    const closeButton = document.getElementById('close-button');
+    console.log(closeButton);
+
+    const userRetry = await getUserRetry(getUIUserRetry);
+
+    if (userRetry === 'y') {
+      lottos = [];
+      modal.remove();
+      modalOverlay.remove();
+      clearUIElements();
+      purchaseButton.disabled = false;
+      document.querySelector('.lotto-content').innerHTML = '';
+    }
+
+    closeButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      modal.remove();
+      modalOverlay.remove();
+      document.querySelector('.lotto-content').innerHTML = '';
+    });
+  }
+
+  purchaseButton.removeEventListener('click', handlePurchase);
+  purchaseButton.addEventListener('click', handlePurchase);
 });
