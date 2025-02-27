449165c84bb7196925589a0922807c8e1e48a31a
kimyou1102 <yoosk5485@naver.com>
2025-02-27 16:07:28 +0900

    fix: Validator.range 사용시 누락된 매개변수 추가

--- src/validation/validateBonusNumber.js
+++ src/validation/validateBonusNumber.js
@@ -1,11 +1,15 @@
 import { ERROR } from "../constants/message.js";
+import { LOTTO_NUMBER } from "../constants/lotto.js";
 import { throwError } from "../utils/throwError.js";
 import Validator from "./validator.js";
 
 const validateBonusNumber = (enteredLottoNumbers, bonusNumber) => {
   Validator.empty(bonusNumber);
   Validator.number(bonusNumber);
-  Validator.range(bonusNumber);
+  Validator.range(
+    { min: LOTTO_NUMBER.MIN, max: LOTTO_NUMBER.MAX },
+    bonusNumber
+  );
 
   if (enteredLottoNumbers.includes(bonusNumber)) {
     throwError(ERROR.INCLUDE);
