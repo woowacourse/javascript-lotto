a562a18e7a230d3268fd5498f6bdafbe17cc636b
deokbin <deokbin@naver.com>
2025-02-27 11:27:23 +0900

    refactor: 구입한 로또 결과 보여주는 함수 분리

---
+++ src/showLottoResult.js
@@ -0,0 +1,25 @@
+import makeLotto from './service/LottoService';
+
+function showLottoResult(lottos, purchaseAmount, purchaseButton) {
+  lottos = makeLotto(purchaseAmount);
+  lottos.forEach((lotto) => {
+    const lottoWrapper = document.createElement('div');
+    lottoWrapper.classList.add('lotto-wrapper');
+
+    const lottoTicket = document.createElement('div');
+    lottoTicket.classList.add('lotto-ticket');
+    lottoTicket.textContent = '🎟️';
+
+    const lottoNumbers = document.createElement('div');
+    lottoNumbers.classList.add('lotto-numbers');
+    lottoNumbers.textContent = `${lotto.numbers.join(', ')}`;
+
+    lottoWrapper.appendChild(lottoTicket);
+    lottoWrapper.appendChild(lottoNumbers);
+
+    document.querySelector('.lotto-content').appendChild(lottoWrapper);
+    purchaseButton.disabled = true;
+  });
+}
+
+export default showLottoResult;
