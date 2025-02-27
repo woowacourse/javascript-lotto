d1cea3e7b28ae1a0f11d55229e8bba47f0d23265
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-27 16:34:58 +0900

    feat: purchasePrice을 입력하고 나면 다시 값을 입력할 수 없도록 disable하는 로직을 추가

--- src/web/playLotto/playLotto.js
+++ src/web/playLotto/playLotto.js
@@ -8,10 +8,14 @@ export default function PlayLotto() {
   const playLotto = createElement('div', { class: 'play-lotto' });
   const { priceInput, purchaseButton } = PurchaseForm(playLotto);
 
-  purchaseButton.addEventListener('click', () => PlayLottoWithPurchasePrice(playLotto, priceInput));
+  purchaseButton.addEventListener('click', () => {
+    PlayLottoWithPurchasePrice(playLotto, priceInput);
+    priceInput.disabled = true;
+  });
   priceInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
       PlayLottoWithPurchasePrice(playLotto, priceInput);
+      priceInput.disabled = true;
     }
   });
 
