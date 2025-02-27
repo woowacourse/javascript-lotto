d5f6f14f1c33019a03a8f8571cbf9fa7db137a89
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-27 13:25:00 +0900

    chore: merge conflicts 해결 후 필요없는 코드 삭제 및 남은 충돌 처리

--- src/utils/getArrayOfStrings.js
+++ src/utils/getArrayOfStrings.js
@@ -1,10 +1,3 @@
 export const getArrayOfStrings = (arrays) => {
-<<<<<<< HEAD
   return arrays.map((arr) => `[${arr.join(', ')}]`);
-=======
-  return arrays.reduce((acc, cur) => {
-    acc.push(`[${cur.join(', ')}]`);
-    return acc;
-  }, []);
->>>>>>> upstream/eunwoo-levi
 };
