70a6fd44f8721eac2a8b0d3acfa6df24904b85ec
Jeongeun Lee <leeju1009@gmail.com>
2025-02-27 16:46:14 +0900

    test: roundNumber 유틸함수 테스트 추가

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -1,4 +1,4 @@
-import { getRevenueRate } from "./math.js";
+import { getRevenueRate, roundNumber } from "./math.js";
 
 describe("getRevenueRate 함수 테스트", () => {
   test("1000원으로 5만원을 벌면 수익률은 5,000이다.", () => {
@@ -6,5 +6,21 @@ describe("getRevenueRate 함수 테스트", () => {
     const cost = 1_000;
 
     expect(getRevenueRate(revenue, cost)).toBe(5_000);
+  });
+});
+
+describe("roundNumber 함수 테스트", () => {
+  test("0.001을 소수 둘째 자리까지 반올림하면 0이다.", () => {
+    const number = 0.001;
+    const roundDigits = 2;
+
+    expect(roundNumber(number, roundDigits)).toBe(0);
+  });
+
+  test("0.065을 소수 첫째 자리까지 반올림하면 0.1이다.", () => {
+    const number = 0.065;
+    const roundDigits = 1;
+
+    expect(roundNumber(number, roundDigits)).toBe(0.1);
   });
 });
