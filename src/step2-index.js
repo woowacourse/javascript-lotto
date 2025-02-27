52c33da6c3722f920400b7fa4877eadc4fe041ac
minji2219 <aabb0219@naver.com>
2025-02-27 14:20:33 +0900

    feat: 구입 버튼 눌렀을 때, print 로직 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,22 +1,36 @@
 import LottoGame from "./models/LottoGame.js";
 import { getLottoPrice } from "./uiView/input.js";
+import { printLottoCount, printLottoNumbers } from "./uiView/output.js";
 
 // /**
 //  * step 2의 시작점이 되는 파일입니다.
 //  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
 //  */
+const lottoGame = new LottoGame();
+let lottos;
 
-const init = () => {
-  const lottoGame = new LottoGame();
-
-  const price = document
+const init = async () => {
+  document
     .querySelector(".purchase button")
     .addEventListener("click", handlePurchaseClick);
 };
 
 const handlePurchaseClick = (e) => {
   const price = getLottoPrice();
-  if (price) e.target.disabled = true;
+  if (price) {
+    e.target.disabled = true;
+    lottos = lottoGame.generateLottos(price);
+
+    printLottoCount(lottos.length);
+
+    const lottoListEl = document.createElement("ul");
+    lottoListEl.classList.add("lotto-list");
+    document.querySelector(".purchase-history .count").after(lottoListEl);
+
+    lottos.forEach((lotto) => printLottoNumbers(lotto.numbers));
+
+    document.querySelector(".winning-lotto").classList.add("active");
+  }
 };
 
 init();
