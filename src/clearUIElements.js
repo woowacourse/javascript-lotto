3aff5b7ea6adb1060ac0e61ee2c75eea3eb85667
deokbin <deokbin@naver.com>
2025-02-27 23:34:13 +0900

    feat: 재시도 시 모든 요소 제거하는 함수 추가

---
+++ src/clearUIElements.js
@@ -0,0 +1,37 @@
+function clearUIElements() {
+  const purchasePriceInput = document.getElementById('purchase-price');
+  if (purchasePriceInput.value) {
+    purchasePriceInput.value = '';
+  }
+
+  const purchaseResult = document.querySelector('.purchase-result');
+  if (purchaseResult) {
+    purchaseResult.remove();
+  }
+
+  const lottoInputContainer = document.getElementsByClassName(
+    'lotto-input-container',
+  );
+  console.log(lottoInputContainer);
+  if (lottoInputContainer) {
+    Array.from(lottoInputContainer).forEach((element) => {
+      element.remove();
+    });
+  }
+
+  const resultButton = document.getElementById('check-result-btn');
+  if (resultButton) {
+    resultButton.disabled = false;
+  }
+
+  const modalOverlay = document.querySelector('.modal-overlay');
+  if (modalOverlay) {
+    modalOverlay.remove();
+  }
+  const modal = document.querySelector('.modal');
+  if (modal) {
+    modal.remove();
+  }
+}
+
+export default clearUIElements;
