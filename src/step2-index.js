8d34ee4ff174ea8e0f14891e72493fa3ed9b465c
eunoia-jaxson <eunoia@handong.ac.kr>
2025-02-28 00:29:49 +0900

    refactor: selector 상수화

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,5 +1,6 @@
 import Main from './components/Main/Main.js';
+import { SELECTORS } from './constants/WinningResultModalConstants.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Main('#main').render();
+  new Main(SELECTORS.MAIN).render();
 });
