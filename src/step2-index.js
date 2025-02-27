65ec40e13a5ddbb7c1027d3319bb8717f04554e0
H0ngJu <hongju4385@gmail.com>
2025-02-27 15:03:46 +0900

    feat: 로또 출력

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,7 @@
+import { getLottos } from './domain/getLottos.js';
+
 let purchasePrice = 0;
+let lottos = [];
 
 const getPurchasePrice = () => {
   const purchaseForm = document.getElementById('purchase-form');
@@ -23,6 +26,48 @@ const printQuantity = (purchasePrice) => {
   result.appendChild(div);
 
   printLottos(quantity);
+};
+
+const printLottos = (quantity) => {
+  // 기존 로또 출력 초기화
+  const existingContainer = document.getElementById('lottoContainer');
+  if (existingContainer) {
+    existingContainer.remove();
+  }
+
+  const container = document.createElement('div');
+  container.id = 'lottoContainer';
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  container.style.gap = '10px';
+
+  lottos = getLottos(quantity);
+
+  lottos.forEach((lotto) => {
+    container.appendChild(printLotto(lotto));
+  });
+
+  result.appendChild(container);
+};
+
+const printLotto = (lotto) => {
+  const container = document.createElement('div');
+  container.style.display = 'flex';
+  container.style.alignItems = 'center';
+  container.style.gap = '10px';
+
+  const img = new Image();
+  img.src = '../images/lottoImage.png';
+  img.alt = '로또 이미지';
+  img.style.width = '30px';
+
+  const numbersDiv = document.createElement('div');
+  numbersDiv.innerHTML = lotto.getNumbers().join(', ');
+
+  container.appendChild(img);
+  container.appendChild(numbersDiv);
+
+  return container;
 };
 
 getPurchasePrice();
