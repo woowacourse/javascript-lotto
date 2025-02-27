40483710157efd616d3adb81e2e3fc7b80193e79
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-27 13:59:22 +0900

    refactor: 상수 객체에서 value와 function 구분하여 가독성 향상

--- src/view/output.js
+++ src/view/output.js
@@ -2,7 +2,7 @@ import { LOTTO } from '../domain/lottoConstants.js';
 import { OUTPUT } from './viewConstants.js';
 
 export const printPurchasedQuantity = (quantity) => {
-  console.log(OUTPUT.PURCHASED_QUANTITY(quantity));
+  console.log(OUTPUT.FUNCTION.PURCHASED_QUANTITY(quantity));
 };
 
 export const printRandomLottos = (randomLottos) => {
@@ -12,12 +12,11 @@ export const printRandomLottos = (randomLottos) => {
 };
 
 export const printStatistics = (matchCounts, revenue) => {
-  console.log(OUTPUT.STATISTICS_HEADER);
-
-  console.log(OUTPUT.MATCH.TRHEE_MATCH(matchCounts[LOTTO.THREE_MATCH]));
-  console.log(OUTPUT.MATCH.FOUR_MATCH(matchCounts[LOTTO.FOUR_MATCH]));
-  console.log(OUTPUT.MATCH.FIVE_MATCH(matchCounts[LOTTO.FIVE_MATCH]));
-  console.log(OUTPUT.MATCH.FIVE_WITH_BONUS_MATCH(matchCounts[LOTTO.FIVE_WITH_BONUS_MATCH_IDX]));
-  console.log(OUTPUT.MATCH.SIX_MATCH(matchCounts[LOTTO.FIVE_MATCH]));
-  console.log(OUTPUT.TOTAL_REVENUE(revenue));
+  console.log(OUTPUT.VALUE.STATISTICS_HEADER);
+  console.log(OUTPUT.FUNCTION.MATCH.TRHEE_MATCH(matchCounts[LOTTO.THREE_MATCH]));
+  console.log(OUTPUT.FUNCTION.MATCH.FOUR_MATCH(matchCounts[LOTTO.FOUR_MATCH]));
+  console.log(OUTPUT.FUNCTION.MATCH.FIVE_MATCH(matchCounts[LOTTO.FIVE_MATCH]));
+  console.log(OUTPUT.FUNCTION.MATCH.FIVE_WITH_BONUS_MATCH(matchCounts[LOTTO.FIVE_WITH_BONUS_MATCH_IDX]));
+  console.log(OUTPUT.FUNCTION.MATCH.SIX_MATCH(matchCounts[LOTTO.FIVE_MATCH]));
+  console.log(OUTPUT.FUNCTION.TOTAL_REVENUE(revenue));
 };
