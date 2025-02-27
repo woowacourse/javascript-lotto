d27251ae41a3bf4ea19d8d0b8a96fcbefaac3bac
Jeongeun Lee <leeju1009@gmail.com>
2025-02-27 16:29:07 +0900

    refactor: 당첨 통계 내역이 형식에 맞게 출력되도록 수정

--- src/constants/web.js
+++ src/constants/web.js
@@ -1,4 +1,19 @@
+import FrozenMap from "../FrozenMap.js";
+import { LOTTO_MATCHED_NUMBER_COUNT } from "./lotto.js";
+
 export const WINNING_TABLE = {
   HEADERS: ["일치갯수", "당첨금", "당첨갯수"],
   LOTTO_UNIT: "개",
 };
+
+export const LOTTO_RESULT_TABLE_MAP = Array.from(
+  LOTTO_MATCHED_NUMBER_COUNT,
+).reduce((matchedTemplate, [matchedCount, matchKey]) => {
+  if (matchKey === "5개 일치+보너스") {
+    matchedTemplate.set(matchedCount, "5개+보너스볼");
+    return matchedTemplate;
+  }
+
+  matchedTemplate.set(matchedCount, `${matchedCount}개`);
+  return matchedTemplate;
+}, new FrozenMap());
--- src/view/web/layers/modal/createPrizeTable.js
+++ src/view/web/layers/modal/createPrizeTable.js
@@ -2,14 +2,23 @@ import {
   LOTTO_MATCHED_NUMBER_COUNT,
   LOTTO_PRIZE_MONEY,
 } from "../../../../constants/lotto.js";
-import { WINNING_TABLE } from "../../../../constants/web.js";
+import {
+  LOTTO_RESULT_TABLE_MAP,
+  WINNING_TABLE,
+} from "../../../../constants/web.js";
 import { setHeader, setRow } from "../../templates/table.js";
 
 const createPrizeTable = (result) => {
   const rows = Array.from(result).map(([matchedCount, matchedLottos]) => {
     const prizeKey = LOTTO_MATCHED_NUMBER_COUNT.get(matchedCount);
-    const prize = LOTTO_PRIZE_MONEY.get(prizeKey);
-    return [matchedCount, prize, matchedLottos];
+    const matchedTemplate = LOTTO_RESULT_TABLE_MAP.get(matchedCount);
+    const prize = LOTTO_PRIZE_MONEY.get(prizeKey).toLocaleString();
+
+    return [
+      matchedTemplate,
+      prize,
+      `${matchedLottos}${WINNING_TABLE.LOTTO_UNIT}`,
+    ];
   });
 
   const headerTemplate = setHeader([...WINNING_TABLE.HEADERS]);
