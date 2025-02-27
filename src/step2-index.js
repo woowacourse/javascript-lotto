9b80bb290a53e59cba54dcf33762472250f21549
sanghee01 <sang6299@naver.com>
2025-02-27 13:38:51 +0900

    feat: 구입 금액 입력시 로또 번호 생성

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,42 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+import LottoMaker from './domain/LottoMaker.js';
+import validatePurchaseMoney from './validations/validate/PurchaseMoneyValidate.js';
+
+const $purchaseForm = document.getElementById('purchase-form');
+const $purchaseInput = document.getElementById('purchase-form__input');
+const $purchaseBtn = document.getElementById('purchase-form__btn');
+const $purchaseFormError = document.getElementById('purchase-form__error');
+
+const $afterPurchaseWrap = document.getElementById('after-purchase-wrap');
+const $lottoList = document.getElementById('lotto-list');
+const $lottoCount = document.getElementById('lotto-count');
+
+$purchaseInput.addEventListener('input', () => {
+  if ($purchaseInput.value) {
+    $purchaseBtn.disabled = false;
+    return;
+  }
+  $purchaseBtn.disabled = true;
+});
+
+$purchaseForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const purchaseMoney = $purchaseInput.valueAsNumber;
+  validatePurchaseMoney(purchaseMoney);
+  $afterPurchaseWrap.classList.remove('hidden');
+
+  const lottoMaker = new LottoMaker(purchaseMoney);
+  $purchaseFormError.textContent = '';
+  $purchaseBtn.disabled = true;
+  $purchaseInput.disabled = true;
+
+  lottoMaker.lottoList.forEach((lotto) => {
+    const li = document.createElement('li');
+    li.textContent = '🎟️ ' + lotto.numbers;
+    $lottoList.appendChild(li);
+  });
+  $lottoCount.textContent = lottoMaker.lottoList.length;
+});
--- src/validations/validate/PurchaseMoneyValidate.js
+++ src/validations/validate/PurchaseMoneyValidate.js
@@ -2,20 +2,28 @@ import { PURCHASE_NUMBER_ERROR_MESSAGES } from '../../constants/constants.js';
 import runValidators from '../../utils/runValidators.js';
 import { PurchaseMoneyValidator } from '../validator/PurchaseMoneyValidator.js';
 
+const $purchaseFormError = document.getElementById('purchase-form__error');
+
 const validatePurchaseMoneyInteger = (input) => {
   if (!PurchaseMoneyValidator.isInteger(input)) {
+    $purchaseFormError.textContent = PURCHASE_NUMBER_ERROR_MESSAGES.INTIGER;
+
     throw new Error(PURCHASE_NUMBER_ERROR_MESSAGES.INTIGER);
   }
 };
 
 const validateUnit = (input) => {
   if (!PurchaseMoneyValidator.isValidUnit(input)) {
+    $purchaseFormError.textContent = PURCHASE_NUMBER_ERROR_MESSAGES.UNIT;
+
     throw new Error(PURCHASE_NUMBER_ERROR_MESSAGES.UNIT);
   }
 };
 
 const validateRange = (input) => {
   if (!PurchaseMoneyValidator.isValidRange(input)) {
+    $purchaseFormError.textContent = PURCHASE_NUMBER_ERROR_MESSAGES.MIN;
+
     throw new Error(PURCHASE_NUMBER_ERROR_MESSAGES.MIN);
   }
 };
