326529bb06698569e00888264f500940485a10f5
minji2219 <aabb0219@naver.com>
2025-02-27 14:53:31 +0900

    feat: 당첨번호, 보너스 번호 입력 받기 구현

--- src/uiView/input.js
+++ src/uiView/input.js
@@ -1,9 +1,28 @@
 import retryUntilValid from "../utils/retryUntilValidUI.js";
 import validatePrice from "../validations/validatePrice.js";
+import {
+  validateBonusNumber,
+  validateWinningNumbers,
+} from "../validations/validateWinningNumbers.js";
 
 export const getLottoPrice = () => {
   return retryUntilValid(
     document.querySelector(".purchase input").value,
     validatePrice
+  );
+};
+
+export const getWinningNumbers = () => {
+  const winningNumbersEl = document.querySelectorAll(".winning-number");
+  const winningNumbers = [...winningNumbersEl].map((winningNumber) => {
+    return winningNumber.value;
+  });
+  return retryUntilValid(winningNumbers.join(","), validateWinningNumbers);
+};
+
+export const getBonusNumber = (winningNumbers) => {
+  const bonusNumber = document.querySelector(".bonus-number").value;
+  return retryUntilValid(bonusNumber, (bonusNumber) =>
+    validateBonusNumber(bonusNumber, winningNumbers)
   );
 };
