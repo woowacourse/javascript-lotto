5de321dbd33c41e6b19bc9309406250524b0d02d
kimyou1102 <yoosk5485@naver.com>
2025-02-27 22:02:55 +0900

    refactor: customCreateElement 유틸함수로 요소 만드는 것으로 변경
    - innerHTML 제거

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,15 +1,20 @@
+import customCreateElement from "../../utils/customElement.js";
+
 export default class Nav {
   constructor($target) {
     this.render($target);
   }
 
   render($target) {
-    const $nav = document.createElement("nav");
-    const $title = document.createElement("h1");
-
-    $title.innerHTML = "🎱 행운의 로또";
-    $title.className = "title";
-    $nav.className = "nav";
+    const $nav = customCreateElement({
+      tagName: "nav",
+      className: "nav",
+    });
+    const $title = customCreateElement({
+      tagName: "h1",
+      className: "title",
+      text: "🎱 행운의 로또",
+    });
 
     $nav.appendChild($title);
     $target.appendChild($nav);
