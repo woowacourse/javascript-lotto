ee60968e93a96ba69774f9a241029fdc573bcad3
H0ngJu <hongju4385@gmail.com>
2025-02-27 15:01:42 +0900

    feat: 로또 수량 계산 및 출력

--- src/step2-index.js
+++ src/step2-index.js
@@ -13,4 +13,16 @@ const getPurchasePrice = () => {
   });
 };
 
+const printQuantity = (purchasePrice) => {
+  const quantity = Math.floor(purchasePrice / 1000);
+
+  const result = document.getElementById('result');
+  result.innerHTML = ''; // 기존 내용 초기화
+  const div = document.createElement('div');
+  div.textContent = `총 ${quantity}개를 구매하였습니다.`;
+  result.appendChild(div);
+
+  printLottos(quantity);
+};
+
 getPurchasePrice();
