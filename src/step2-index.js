461df6fdee7ebd2e23002c519708d40c3d1ea84e
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-28 18:22:45 +0900

    feat: 로또 구입 기능 및 UI 업데이트 추가
    - 구입 금액 입력 후 로또 번호를 생성하는 기능 추가
    - 입력값 검증 후 유효하지 않은 경우 alert로 에러 메시지 출력
    - 로또 개수 및 번호 리스트 UI 동적 업데이트 (`updateLottoUI()` 추가)
    - 구입 후 UI (`.lotto-display-container`)가 보이도록 처리
    - 입력 후 입력 칸 초기화 (`#purchase-input.value = '';`)
    - 에러 발생 시 `.lotto-display-container`를 숨기도록 처리

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,59 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import LottoMachine from './Model/LottoMachine.js'
+import Validate from './Model/Validate.js';
+
+const lottoMachine = new LottoMachine();
+let lottos = [];
+document.querySelector("#purchase-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    const price = document.querySelector("#purchase-input").value;
+    Validate.validatePrice(price);
+    lottos = lottoMachine.generateLotto(price);
+
+    console.log("구매한 로또:", lottos);
+    updateLottoUI(lottos);
+    document.querySelector(".lotto-display-container").style.display = "block";
+    document.querySelector("#purchase-input").value = '';
+  } catch (error) {
+    document.querySelector(".lotto-display-container").style.display = "none";
+    document.querySelector("#purchase-input").value = '';
+    alert(error.message);
+  }
+});
+
+function updateLottoUI(lottos) {
+  document.querySelector(".purchase-count-message span").textContent = lottos.length;
+
+  const lottoList = document.querySelector(".lotto-tickets");
+  lottoList.innerHTML = "";
+
+  lottos.forEach((lotto) => {
+    const lottoItem = document.createElement("li");
+
+    const ticketIcon = document.createElement("span");
+    ticketIcon.classList.add("lotto-title");
+    ticketIcon.textContent = "🎟️";
+
+    const lottoNumbers = document.createElement("span");
+    lottoNumbers.classList.add("lotto-body");
+    lottoNumbers.textContent = lotto.numbers.join(", ");
+
+    lottoItem.appendChild(ticketIcon);
+    lottoItem.appendChild(lottoNumbers);
+    lottoList.appendChild(lottoItem);
+  });
+}
+
+
+const showBtn = document.querySelector('.show-result-btn');
+const resultModal = document.querySelector('.dialog');
+const closeBtn = document.querySelector('.close-btn');
+
+showBtn.addEventListener('click', () => {
+  resultModal.showModal();
+})
+
+closeBtn.addEventListener('click', () => {
+  resultModal.close();
+});
