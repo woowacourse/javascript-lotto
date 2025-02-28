aad2210a911d316ea399408388203ebd700071c7
suhwa <tnghk9611@naver.com>
2025-02-28 16:52:25 +0900

    fix: web 버전에서는 inputProcessor를 사용하지 않는 방식으로 수정

--- src/controllers/WebController.js
+++ src/controllers/WebController.js
@@ -1,32 +1,32 @@
-import { getPurchasePrice, getWiningAndBonusNumbers } from '../domains/InputProcessor.js';
 import LottoMachine from '../domains/LottoMachine.js';
 import WinningResult from '../domains/WinningResult.js';
-import retryUntilValidWithAlert from '../utils/retryUntilValidWithAlert.js';
 import OutputView from '../views/web/OutputView.js';
+import InputView from '../views/web/InputView.js';
+import retryUntilValidWithAlert from '../utils/retryUntilValidWithAlert.js';
 
 const WebController = {
   async start() {
-    const { lottoPurchasePrice, lottos } = await this.processLottoPurchase();
+    const { purchasePrice, lottos } = await this.processLottoPurchase();
     const winningResult = await this.generateWinningResult();
     const winningCounts = winningResult.calculate(lottos);
-    const profitRate = winningResult.calculateProfitRate(lottoPurchasePrice, winningCounts);
+    const profitRate = winningResult.calculateProfitRate(purchasePrice, winningCounts);
 
     OutputView.showModal(winningCounts, profitRate);
   },
 
   async processLottoPurchase() {
-    const { lottoPurchasePrice, lottoCount } = await retryUntilValidWithAlert(() =>
-      getPurchasePrice('web'),
+    const { purchasePrice, lottoCount } = await retryUntilValidWithAlert(
+      async () => await InputView.enterPurchasePrice(),
     );
     const lottoMachine = new LottoMachine(lottoCount);
     OutputView.printPurchaseLottos(lottoCount, lottoMachine.lottos);
 
-    return { lottoPurchasePrice, lottos: lottoMachine.lottos };
+    return { purchasePrice, lottos: lottoMachine.lottos };
   },
 
   async generateWinningResult() {
-    const { winningNumbers, bonusNumber } = await retryUntilValidWithAlert(() =>
-      getWiningAndBonusNumbers(),
+    const { winningNumbers, bonusNumber } = await retryUntilValidWithAlert(
+      async () => await InputView.enterWinningAndBonusNumber(),
     );
     return new WinningResult(winningNumbers, bonusNumber);
   },
--- src/domains/InputProcessor.js
+++ src/domains/InputProcessor.js
@@ -3,44 +3,31 @@ import { BonusNumberValidator } from '../validators/BonusNumberValidator.js';
 import { PurchasePriceValidator } from '../validators/PurchasePriceValidator.js';
 import RestartValidator from '../validators/RestartValidator.js';
 import { WinningNumbersValidator } from '../validators/WinningNumbersValidator.js';
-import * as ConsoleInputView from '../views/console/InputView.js';
-import * as WebInputView from '../views/web/InputView.js';
+import InputView from '../views/console/InputView.js';
 
-const CONSOLE = 'console';
-const getView = (type) => (type === CONSOLE ? ConsoleInputView.default : WebInputView.default);
-
-export async function getPurchasePrice(type = CONSOLE) {
-  const inputView = getView(type);
-  const lottoPurchasePrice = await inputView.enterPurchasePrice();
+export async function getPurchasePrice() {
+  const lottoPurchasePrice = await InputView.enterPurchasePrice();
   PurchasePriceValidator.validate(Number(lottoPurchasePrice));
   const lottoCount = lottoPurchasePrice / PURCHASE_PRICE.UNIT;
   return { lottoPurchasePrice, lottoCount };
 }
 
 export async function getWinningNumbers() {
-  const winningNumbers = await ConsoleInputView.default.enterWinningNumbers();
+  const winningNumbers = await InputView.enterWinningNumbers();
   const splittedWinningNumbers = winningNumbers.split(SEPARATOR).map(Number);
   WinningNumbersValidator.validate(splittedWinningNumbers);
   return splittedWinningNumbers;
 }
 
 export async function getBonusNumber(winningNumbers) {
-  const bonusNumber = await ConsoleInputView.default.enterBonusNumber();
+  const bonusNumber = await InputView.enterBonusNumber();
   const transformedBonusNumber = Number(bonusNumber);
   BonusNumberValidator.validate(transformedBonusNumber, winningNumbers);
   return transformedBonusNumber;
 }
 
-export async function getWiningAndBonusNumbers() {
-  const { winningNumbers, bonusNumber } = await WebInputView.default.enterWinningAndBonusNumber();
-  WinningNumbersValidator.validate(winningNumbers);
-  BonusNumberValidator.validate(bonusNumber, winningNumbers);
-  return { winningNumbers, bonusNumber };
-}
-
-export async function getRestart(type = CONSOLE) {
-  const inputView = getView(type);
-  const restartInput = await inputView.enterRestart();
+export async function getRestart() {
+  const restartInput = await InputView.enterRestart();
   RestartValidator.validate(restartInput);
   return restartInput;
 }
