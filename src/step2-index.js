55fce6763077f3380e953090768a46c0743896a6
yeji0214 <1225yj50@naver.com>
2025-02-28 17:48:03 +0900

    feat: 구입 금액 유효성 검사에 따른 헬퍼텍스트 출력 및 버튼 활성화 여부 결정

--- src/step2-index.js
+++ src/step2-index.js
@@ -4,8 +4,24 @@
  */
 
 import { purchase } from "./LottoStore.js";
+import { validatePurchaseAmount } from "./util/validate.js";
 
 document.getElementById("button-purchase").addEventListener("click", async () => {
     await purchase();
 });
 
+document.getElementById("input-purchase-amount").addEventListener("input", (event) => {
+    const inputValue = event.target.value;
+    const button = document.getElementById("button-purchase");
+    const helperText = document.getElementById("purchase-amount-helper-text");
+
+    try {
+        validatePurchaseAmount(inputValue);
+        button.disabled = false;
+        helperText.style.display = "none";
+    } catch (error) {
+        button.disabled = true;
+        helperText.innerText = error.message.slice(8);
+        helperText.style.display = "block";
+    }
+});
