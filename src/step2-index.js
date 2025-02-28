501188269acbe10403a082f6a1348b639192e0f8
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-02-28 17:54:51 +0900

    feat: 사용자 입력 및 출력 처리 모듈 추가 및 UI 개선

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,111 +1,41 @@
-/* eslint-disable max-lines-per-function */
-// /**
-//  * step 2의 시작점이 되는 파일입니다.
-//  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
-// */
+import calculateRevenueRate from './domain/model/calculateRevenueRate.js';
+import createLottos from './domain/model/createLottos.js';
+import LottoStatistics from './domain/model/LottoStatistics.js';
 
-import calculateRevenueRate from './domain/model/calculateRevenueRate';
-import createLottos from './domain/model/createLottos';
-import LottoStatistics from './domain/model/LottoStatistics';
+import InputView from './view/ui/InputView.js';
+import OutputView from './view/ui/OutputView.js';
 
-import { validateBonus, validateLottoNumber, validateMoney } from './domain/validation';
-
-import normalizeErrorMessage from './view/utils/normalizeErrorMessage';
-
+let userLottos;
 let userMoney;
-let userLottos = [];
 const lottoStatistics = new LottoStatistics();
 
-function toggleClassName(element, className) {
-  if (element.classList.contains(className)) {
-    return element.classList.remove(className);
-  }
-  return element.classList.add(className);
-}
-
-function toggleModal() {
-  const $modal = document.querySelector('.modal');
-  const $modalDimmed = document.querySelector('.modal-dimmed');
-  toggleClassName($modal, 'modal-close');
-  toggleClassName($modalDimmed, 'modal-close');
-}
-
-function createElement(type, text) {
-  const element = document.createElement(type);
-  element.innerText = text;
-  return element;
-}
-
 // 1. 로또 구매하기
-function printUserLottos() {
-  userLottos.forEach((userLotto) => {
-    const parent = document.querySelector('.lotto-item-container');
-    const element = createElement('p', `🎟️ ${userLotto.getNumbers().join(', ')}`);
-    parent.appendChild(element);
-  });
-}
-
 document.getElementById('purchase-button').addEventListener('click', () => {
-  userMoney = document.querySelector('#user-money').value;
-
-  try {
-    validateMoney(userMoney);
-    userLottos = createLottos(userMoney);
-    printUserLottos();
-  } catch (error) {
-    // eslint-disable-next-line no-alert
-    alert(normalizeErrorMessage(error.message));
+  userMoney = InputView.readMoney();
+  if (!userMoney) {
+    return;
   }
+  userLottos = createLottos(userMoney);
+  OutputView.printUserLottos(userLottos);
 });
 
-// 2-1. 로또 결과 확인하기 - 수익률
-function printRevenueRate(revenueRate) {
-  const $boldText = document.querySelector('.bold-text');
-  const element = createElement('p', `당신의 총 수익률은 ${revenueRate}% 입니다`);
-  $boldText.appendChild(element);
-  element.classList.add('modal-items');
-}
-
-// 2-2. 로또 결과 확인하기 - 통계 결과
-function printStatisticsResult(rankResult) {
-  toggleModal();
-  Object.keys(rankResult).forEach((key) => {
-    const { name, price, count } = rankResult[key];
-
-    const parent = document.querySelector('.modal-item-container');
-    const child = createElement('tr', '');
-    child.classList.add('modal-items');
-    parent.appendChild(child);
-    let elementName = createElement('td', `${name}개`);
-
-    if (name === '5+1') {
-      elementName = createElement('td', '5개+보너스볼');
-    }
-
-    child.appendChild(elementName);
-    const elementPrice = createElement('td', `${price.toLocaleString()}원`);
-    child.appendChild(elementPrice);
-    const elementCount = createElement('td', `${count}개`);
-    child.appendChild(elementCount);
-  });
-
+function getRevenueRate() {
   const profit = lottoStatistics.calculateProfit();
   const revenueRate = calculateRevenueRate(profit, userMoney);
-  printRevenueRate(revenueRate);
+  return revenueRate;
 }
 
+// 2. 로또 결과 확인하기
 document.getElementById('result-button').addEventListener('click', () => {
-  const bonusNumber = Number(document.querySelector('#input-bonus-number').value);
-  const winningNumbers = [...document.querySelectorAll('.input-winning-number')].map((element) => Number(element.value));
-  const winningLotto = { bonusNumber, lottoNumber: winningNumbers };
-  try {
-    validateLottoNumber(winningNumbers);
-    validateBonus(bonusNumber, winningNumbers);
-    const rankResult = lottoStatistics.compareLottos(userLottos, winningLotto);
-    printStatisticsResult(rankResult);
-  } catch (error) {
-    alert(normalizeErrorMessage(error.message));
+  const winningNumbers = InputView.readWinningNumbers();
+  const bonusNumber = InputView.readBonusNumber(winningNumbers);
+  if (!userLottos || !winningNumbers || !bonusNumber) {
+    return;
   }
+  const winningLotto = { bonusNumber, lottoNumber: winningNumbers };
+  const rankResult = lottoStatistics.compareLottos(userLottos, winningLotto);
+  OutputView.printStatisticsResult(rankResult);
+  OutputView.printRevenueRate(getRevenueRate());
 });
 
 // 3. 로또 초기화하기
@@ -119,5 +49,5 @@ document.getElementById('reset-close-button').addEventListener('click', () => {
     element.remove();
   });
   lottoStatistics.init();
-  toggleModal();
+  OutputView.toggleModal();
 });
---
+++ src/view/ui/OutputView.js
@@ -0,0 +1,60 @@
+/* eslint-disable max-lines-per-function */
+function createElement(type, text) {
+  const element = document.createElement(type);
+  element.innerText = text;
+  return element;
+}
+
+function toggleClassName(element, className) {
+  if (element.classList.contains(className)) {
+    return element.classList.remove(className);
+  }
+  return element.classList.add(className);
+}
+
+const OutputView = {
+  toggleModal() {
+    const $modal = document.querySelector('.modal');
+    const $modalDimmed = document.querySelector('.modal-dimmed');
+    toggleClassName($modal, 'modal-close');
+    toggleClassName($modalDimmed, 'modal-close');
+  },
+  printUserLottos(userLottos) {
+    userLottos.forEach((userLotto) => {
+      const $parent = document.querySelector('.lotto-item-container');
+      const $element = createElement('p', `🎟️ ${userLotto.getNumbers().join(', ')}`);
+      $parent.appendChild($element);
+    });
+  },
+  printStatisticsResult(rankResult) {
+    this.toggleModal();
+
+    Object.keys(rankResult).forEach((key) => {
+      const { name, price, count } = rankResult[key];
+
+      const parent = document.querySelector('.modal-item-container');
+      const child = createElement('tr', '');
+      child.classList.add('modal-items');
+      parent.appendChild(child);
+      let elementName = createElement('td', `${name}개`);
+
+      if (name === '5+1') {
+        elementName = createElement('td', '5개+보너스볼');
+      }
+
+      child.appendChild(elementName);
+      const elementPrice = createElement('td', `${price.toLocaleString()}원`);
+      child.appendChild(elementPrice);
+      const elementCount = createElement('td', `${count}개`);
+      child.appendChild(elementCount);
+    });
+  },
+  printRevenueRate(revenueRate) {
+    const $boldText = document.querySelector('.bold-text');
+    const $element = createElement('p', `당신의 총 수익률은 ${revenueRate}% 입니다`);
+    $boldText.appendChild($element);
+    $element.classList.add('modal-items');
+  },
+};
+
+export default OutputView;
