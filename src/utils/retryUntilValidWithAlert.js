ce81f31b760a014c7c738c89fa20cce9658d9fc6
suhwa <tnghk9611@naver.com>
2025-02-28 17:10:57 +0900

    refactor: alert문 제거

--- src/utils/retryUntilValidWithAlert.js
+++ src/utils/retryUntilValidWithAlert.js
@@ -2,7 +2,6 @@ const retryUntilValidWithAlert = async (func, ...arg) => {
   try {
     return await func(...arg);
   } catch (error) {
-    alert(`${error.message}\n`);
     return retryUntilValidWithAlert(func, ...arg);
   }
 };
