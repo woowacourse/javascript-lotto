db93b21a701cc672f90f8ac58b15857f99f7ee2f
Jeongeun Lee <leeju1009@gmail.com>
2025-02-28 13:40:59 +0900

    fix: 클래스 속성이 잘못된 방식으로 부여된 문제 수정

--- src/view/web/layers/winningLottoBox/createNumbersInput.js
+++ src/view/web/layers/winningLottoBox/createNumbersInput.js
@@ -3,7 +3,7 @@ import { LOTTO_RULE } from "../../../../constants/lotto.js";
 const createNumbersInput = () => {
   return Array.from({ length: LOTTO_RULE.LOTTO_LENGTH })
     .map(() => {
-      return `<input type="text" className="winning-numbers" />`;
+      return `<input type="text" class="winning-numbers" />`;
     })
     .join("");
 };
