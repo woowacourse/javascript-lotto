687763e6b3f12082ae40127632701c4e0fe21428
jin123457 <jin_ox@naver.com>
2025-03-01 18:59:32 +0900

    refactor: 구매 금액 입력 필드에 최소 및 최대 값 설정 및 placeholder 문구 수정

--- src/components/lottoHeader/purchaseForm/purchaseFormInput/purchaseFormInput.js
+++ src/components/lottoHeader/purchaseForm/purchaseFormInput/purchaseFormInput.js
@@ -4,7 +4,9 @@ const $purchaseFormInput = () => {
   const purchaseFormInput = createDomElement('input', {
     type: 'number',
     name: 'money',
-    placeholder: '금액',
+    min: 1000,
+    max: 100000,
+    placeholder: '1,000원 ~ 100,000원 구매가 가능합니다.',
   });
 
   return purchaseFormInput;
