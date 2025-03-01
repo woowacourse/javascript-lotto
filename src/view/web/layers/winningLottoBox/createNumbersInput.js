52cb93579591e931de08b16a33dd01b0437ff7f5
Jeongeun Lee <leeju1009@gmail.com>
2025-03-01 15:26:25 +0900

    feat: input 태그에 사용자 입력값의 조건을 제한하는 속성 추가

--- src/view/web/layers/winningLottoBox/createNumbersInput.js
+++ src/view/web/layers/winningLottoBox/createNumbersInput.js
@@ -3,7 +3,13 @@ import { LOTTO_RULE } from "../../../../constants/lotto.js";
 const createNumbersInput = () => {
   return Array.from({ length: LOTTO_RULE.LOTTO_LENGTH })
     .map(() => {
-      return `<input type="text" class="winning-numbers" />`;
+      return `<input
+        type="number"
+        class="winning-numbers"
+        min="${LOTTO_RULE.MIN_LOTTO_NUMBER}"
+        max="${LOTTO_RULE.MAX_LOTTO_NUMBER}"
+        autocomplete="off"
+      />`;
     })
     .join("");
 };
