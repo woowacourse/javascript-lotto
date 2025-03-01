095714f463dd8517538b0c8bcae461e53d2f42c9
jin123457 <jin_ox@naver.com>
2025-03-01 19:50:35 +0900

    refactor: SVG 닫기 버튼 생성 및 DOM 요소 생성 함수 수정

--- src/components/modal/content/closeButton/closeButton.js
+++ src/components/modal/content/closeButton/closeButton.js
@@ -1,12 +1,23 @@
 import createDomElement from '../../../../utils/createDomElement';
 
-const $closeButton = () => {
-  const modalCloseButton = createDomElement('p', {
-    className: 'close_btn',
-    id: 'closeButton',
-    textContent: 'X',
+const $closeButtonPath = () => {
+  const pathElement = createDomElement('path', {
+    d: 'M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z',
+    fill: 'black',
   });
 
-  return modalCloseButton;
+  return pathElement;
+};
+
+const $closeButton = () => {
+  const closeButton = createDomElement('svg', {
+    class: 'close_button',
+    id: 'closeButton',
+    xmlns: 'http://www.w3.org/2000/svg',
+  });
+
+  closeButton.appendChild($closeButtonPath());
+
+  return closeButton;
 };
 export default $closeButton;
--- src/utils/createDomElement.js
+++ src/utils/createDomElement.js
@@ -1,4 +1,11 @@
 const createDomElement = (tag, props) => {
+  if (tag === 'svg' || tag === 'path') {
+    const element = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    Object.entries(props).forEach(([key, value]) => {
+      element.setAttribute(`${key}`, value);
+    });
+    return element;
+  }
   const element = document.createElement(tag);
   Object.entries(props).forEach(([key, value]) => {
     element[key] = value;
