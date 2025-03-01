eb3c193aa749878d1d9935034b944317eb042174
Jeongeun Lee <leeju1009@gmail.com>
2025-03-02 03:42:00 +0900

    refactor: 모달 외 영역을 클릭했을 때 닫히도록 리팩토링

--- src/view/web/modules/closeResultModal.js
+++ src/view/web/modules/closeResultModal.js
@@ -2,7 +2,10 @@ const closeResultModal = () => {
   const resultModal = document.querySelector(".prize-result-modal");
 
   resultModal.addEventListener("click", (event) => {
-    if (event.target.closest(".close-button")) {
+    if (
+      event.target.closest(".close-button") ||
+      event.target.nodeName === "DIALOG"
+    ) {
       resultModal.close();
     }
   });
