a45ec13a169e1fec9f3972e8647b7e4d1f99d9a3
jin123457 <jin_ox@naver.com>
2025-03-02 00:44:50 +0900

    refactor: 구매 버튼 상태 관리 로직 추가 및 입력 필드 옵션 리팩토링

--- src/components/lottoHeader/purchaseForm/purchaseFormButton/purchaseFormButton.js
+++ src/components/lottoHeader/purchaseForm/purchaseFormButton/purchaseFormButton.js
@@ -3,8 +3,10 @@ import createDomElement from '../../../../utils/createDomElement.js';
 const $purchaseFormButton = () => {
   const purchaseFormButton = createDomElement('button', {
     type: 'submit',
+    className: 'disabled_button',
     id: 'buyButton',
     textContent: '구입',
+    disabled: true,
   });
 
   return purchaseFormButton;
--- src/components/lottoHeader/purchaseForm/purchaseFormInput/purchaseFormInput.js
+++ src/components/lottoHeader/purchaseForm/purchaseFormInput/purchaseFormInput.js
@@ -1,23 +1,39 @@
 import createDomElement from '../../../../utils/createDomElement.js';
 import { validateMoney } from '../../../../domain/validation.js';
 
+const moneyInputOption = {
+  type: 'number',
+  name: 'money',
+  min: 1000,
+  max: 100000,
+  placeholder: '1,000원 ~ 100,000원 구매가 가능합니다.',
+};
+
+const buyButtonState = (state, error) => {
+  if (state) {
+    document.getElementById('lottoBuyError').textContent = '';
+    document.getElementById('lottoBuyError').classList.remove('show');
+    document.getElementById('buyButton').classList.remove('disabled_button');
+    document.getElementById('buyButton').disabled = false;
+
+    return;
+  }
+
+  document.getElementById('lottoBuyError').textContent = error.message;
+  document.getElementById('lottoBuyError').classList.add('show');
+  document.getElementById('buyButton').classList.add('disabled_button');
+  document.getElementById('buyButton').disabled = true;
+};
+
 const $purchaseFormInput = () => {
-  const purchaseFormInput = createDomElement('input', {
-    type: 'number',
-    name: 'money',
-    min: 1000,
-    max: 100000,
-    placeholder: '1,000원 ~ 100,000원 구매가 가능합니다.',
-  });
+  const purchaseFormInput = createDomElement('input', moneyInputOption);
 
   purchaseFormInput.addEventListener('input', () => {
     try {
       validateMoney(purchaseFormInput.value);
-      document.getElementById('lottoBuyError').textContent = '';
-      document.getElementById('lottoBuyError').classList.remove('show');
+      buyButtonState(true);
     } catch (error) {
-      document.getElementById('lottoBuyError').textContent = error.message;
-      document.getElementById('lottoBuyError').classList.add('show');
+      buyButtonState(false, error);
     }
   });
   return purchaseFormInput;
