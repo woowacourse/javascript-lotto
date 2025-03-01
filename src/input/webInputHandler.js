9d4e084959b1dcf2f1c84c659f36192a47141675
마 <126178440+shuyeon@users.noreply.github.com>
2025-03-02 05:17:40 +0900

    fix : 잘못된 당첨 번호 입력 시 보너스 번호 유효성 검사x

--- src/input/webInputHandler.js
+++ src/input/webInputHandler.js
@@ -31,6 +31,10 @@ const webInputHandler = {
   },
 
   bonusNumber(winningNumbersLotto){
+    if(winningNumbersLotto===null){
+      return null
+    }
+    
     try {
         DOM.bonusErrorText.textContent = ""
         const bonusNumber =  Number(DOM.bonusInput.value);
