2340b5e0466ce92e10397f41374d082a5897d775
Jeongeun Lee <leeju1009@gmail.com>
2025-03-01 22:33:54 +0900

    refactor: dialog 태그를 정적으로 생성하도록 코드 수정
    dialog 태그의 안정적인 생성을 위해 코드 수정

--- src/view/web/layers/modal/createPrizeResultModal.js
+++ src/view/web/layers/modal/createPrizeResultModal.js
@@ -8,7 +8,7 @@ const createPrizeResultModal = (result, revenueRate) => {
   const revenueRateMessage = createRevenueRateMessage(revenueRate);
 
   document
-    .querySelector("#app")
+    .querySelector(".prize-result-modal")
     .insertAdjacentHTML("beforeend", prizeResultContents);
 
   document
