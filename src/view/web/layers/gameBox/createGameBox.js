ff82b4e5100f84b0f3e3900f7228c7e8482a7765
Jeongeun Lee <leeju1009@gmail.com>
2025-03-01 15:47:26 +0900

    refactor: 로또 구입 금액 input 태그를 동적으로 생성하도록 리팩토링
    input 태그에 동적으로 속성을 부여하기 위해 리팩토링함

--- src/view/web/layers/gameBox/createGameBox.js
+++ src/view/web/layers/gameBox/createGameBox.js
@@ -1,10 +1,14 @@
 import gameBoxContents from "./gameBox.html?raw";
+import createPriceInput from "./createPriceInput";
 import "./gameBox.css";
 
 const createGameBox = () => {
   const main = document.querySelector("main");
-
   main.insertAdjacentHTML("beforeend", gameBoxContents);
+
+  const priceInput = createPriceInput();
+  const inputContainer = document.querySelector(".purchase-container");
+  inputContainer.insertAdjacentHTML("afterbegin", priceInput);
 };
 
 export default createGameBox;
---
+++ src/view/web/layers/gameBox/createPriceInput.js
@@ -0,0 +1,16 @@
+import { LOTTO_RULE } from "../../../../constants/lotto";
+
+const createPriceInput = () => {
+  return `<input
+      type="number"
+      id="price"
+      placeholder="금액"
+      onfocus="this.placeholder = ''"
+      onblur="this.placeholder = '금액'"
+      min="${LOTTO_RULE.MIN_PRICE}"
+      max="${LOTTO_RULE.MAX_PRICE}"
+      autocomplete="off"
+    />`;
+};
+
+export default createPriceInput;
