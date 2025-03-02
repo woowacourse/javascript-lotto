65af1613224bad0b0313b6db5b538cefc73ad75b
thgml05 <aisohii@naver.com>
2025-03-02 13:54:27 +0900

    [fix] : purchaseButton 부르는 방식 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -23,7 +23,9 @@ const state = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  elements.purchaseButton.addEventListener("click", purchase);
+  const purchaseButton = document.querySelector("#purchase-button");
+  purchaseButton.addEventListener("click", purchase);
+  // elements.purchaseButton.addEventListener("click", purchase);
   elements.resultButton.addEventListener("click", checkResult);
   elements.modalCloseButton.addEventListener("click", closeModal);
   elements.restartButton.addEventListener("click", restartLotto);
