552c43ea9c5f9d165cad724e775d972e1f59e852
H0ngJu <hongju4385@gmail.com>
2025-03-02 23:48:49 +0900

    refactor: 핸들러 분리에 따른 파일 수정

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,70 +1,23 @@
-import { calculateRevenue } from './domain/calculateRevenue.js';
-import { generateLottos } from './domain/generateLottos.js';
-import { getWinningMatchCount } from './domain/getWinningMatchCount.js';
-import WinningLotto from './domain/WinningLotto.js';
-import { validateBonusNumber, validateWinningNumbers } from './validation/validateLottoNumbers.js';
 import { DOM } from './DOM/dom.js';
-import { showLottos, showPurchaseResult, showResultsModal } from './view/web/outputHandler.js';
-import { handlePurchase } from './view/web/inputHandler.js';
-import { errorHandler } from './utils/errorHandler.js';
+import { handleBonusNumberInput, handlePurchaseSubmit, handleWinningNumberInput } from './view/web/inputHandler.js';
+import { handleRestartButtonClick, handleModalCloseClick, handleResultButtonClick } from './view/web/buttonHandler.js';
 
-let purchasePrice = 0;
-let lottos = [];
-let bonusNumber = 0;
-let winningNumbers = [];
-
-DOM.purchaseForm.addEventListener('submit', (event) => {
-  handlePurchase(event, (price) => {
-    purchasePrice = price;
-    const quantity = Math.floor(purchasePrice / 1000);
-    showPurchaseResult(quantity);
-    lottos = generateLottos(quantity);
-    showLottos(lottos);
-    DOM.systemMessage.style.display = 'flex';
-  });
-});
+DOM.purchaseForm.addEventListener('submit', handlePurchaseSubmit);
 
 DOM.winningInputs.forEach((input, index) => {
   input.addEventListener('input', (event) => {
-    const value = Number(event.target.value);
-    winningNumbers[index] = value;
+    handleWinningNumberInput(index, event.target.value);
   });
 });
 
 DOM.bonusInput.addEventListener('input', (event) => {
-  const value = Number(event.target.value);
-  bonusNumber = value;
+  handleBonusNumberInput(event.target.value);
 });
 
 document.addEventListener('click', (event) => {
-  try {
-    if (event.target && event.target.id === 'result-button') {
-      validateWinningNumbers(winningNumbers);
-      validateBonusNumber(winningNumbers)(bonusNumber);
-      DOM.bonusInput.disabled = true;
-      DOM.bonusInput.style.cursor = 'not-allowed';
-      DOM.winningInputs.forEach((input) => {
-        input.disabled = true;
-        input.style.cursor = 'not-allowed';
-      });
+  if (event.target && event.target.id === 'result-button') handleResultButtonClick();
 
-      DOM.modal.style.display = 'flex';
+  if (event.target && event.target.classList.contains('modal-close')) handleModalCloseClick();
 
-      const lottoNumbers = new WinningLotto(winningNumbers, bonusNumber);
-      const matchCounts = getWinningMatchCount(lottos, lottoNumbers);
-      const revenue = calculateRevenue(matchCounts, purchasePrice);
-
-      showResultsModal(matchCounts, revenue);
-    }
-  } catch (e) {
-    errorHandler(e);
-  }
-
-  if (event.target && event.target.classList.contains('modal-close')) {
-    DOM.modal.style.display = 'none';
-  }
-
-  if (event.target && event.target.id === 'restart-button') {
-    location.reload();
-  }
+  if (event.target && event.target.id === 'restart-button') handleRestartButtonClick();
 });
