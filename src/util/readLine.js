392fd1ecd49a523936fb7b831d3dafdbf8bb09b3
yeji0214 <1225yj50@naver.com>
2025-03-02 18:06:04 +0900

    fix: readline import 삭제

--- src/util/readLine.js
+++ src/util/readLine.js
@@ -1,25 +1,25 @@
 // import readline from "readline";
 
-function readLineAsync(query) {
-  return new Promise((resolve, reject) => {
-    if (arguments.length !== 1) {
-      reject(new Error("arguments must be 1"));
-    }
+// function readLineAsync(query) {
+//   return new Promise((resolve, reject) => {
+//     if (arguments.length !== 1) {
+//       reject(new Error("arguments must be 1"));
+//     }
 
-    if (typeof query !== "string") {
-      reject(new Error("query must be string"));
-    }
+//     if (typeof query !== "string") {
+//       reject(new Error("query must be string"));
+//     }
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
 
-    rl.question(query, (input) => {
-      rl.close();
-      resolve(input);
-    });
-  });
-}
+//     rl.question(query, (input) => {
+//       rl.close();
+//       resolve(input);
+//     });
+//   });
+// }
 
-export default readLineAsync;
+// export default readLineAsync;
