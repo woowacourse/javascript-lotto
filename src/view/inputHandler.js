62adcd6fc298e78d255f8fc3f1493c69541e36d3
H0ngJu <hongju4385@gmail.com>
2025-03-02 19:23:55 +0900

    refactor: handlePurchase 분리

---
+++ src/view/inputHandler.js
@@ -0,0 +1,16 @@
+import { DOM } from '../DOM/dom.js';
+import { validatePurchasePrice } from '../validation/validatePurchasePrice.js';
+import { errorHandler } from '../utils/errorHandler.js';
+
+export const handlePurchase = (event, callback) => {
+  event.preventDefault();
+  const formData = new FormData(DOM.purchaseForm);
+  const purchasePrice = Number(formData.get('purchase-input'));
+
+  try {
+    validatePurchasePrice(purchasePrice);
+    callback(purchasePrice);
+  } catch (error) {
+    errorHandler(error.message);
+  }
+};
