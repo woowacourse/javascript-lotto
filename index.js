82a2c8b3355476a303d2ae31aa01f9c05582da52
Jeongeun Lee <leeju1009@gmail.com>
2025-03-02 21:34:51 +0900

    refactor: 구입 금액 input 비활성화에 따른 불필요한 예외 처리 제거

--- index.js
+++ index.js
@@ -81,21 +81,19 @@ const checkWinningLotto = (winningNumbers, bonusNumber) => {
 
 const rendererUsingWinningLotto = (winningNumbers, bonusNumber) => {
   const price = getPrice();
-  if (checkPrice(price)) {
-    const lottoNumbers = getLottoNumbers();
+  const lottoNumbers = getLottoNumbers();
 
-    const result = calculatePrizeResult(
-      lottoNumbers,
-      winningNumbers,
-      bonusNumber,
-    );
+  const result = calculatePrizeResult(
+    lottoNumbers,
+    winningNumbers,
+    bonusNumber,
+  );
 
-    const totalPrizeMoney = getTotalPrizeMoney(result);
-    const revenueRate = getRevenueRate(totalPrizeMoney, price);
+  const totalPrizeMoney = getTotalPrizeMoney(result);
+  const revenueRate = getRevenueRate(totalPrizeMoney, price);
 
-    createPrizeResultModal(result, revenueRate);
-    handleResultModal(".prize-result-modal");
-  }
+  createPrizeResultModal(result, revenueRate);
+  handleResultModal(".prize-result-modal");
 };
 
 const handleResultModal = (targetSelector) => {
