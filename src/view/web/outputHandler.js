111a629fcc50477552a724bb8ed05847f39fb8a4
H0ngJu <hongju4385@gmail.com>
2025-03-03 00:46:57 +0900

    refactor: 경로에서 public 삭제

--- src/view/web/outputHandler.js
+++ src/view/web/outputHandler.js
@@ -35,7 +35,7 @@ const showLotto = (lotto) => {
 
   const img = new Image();
   img.id = 'lotto-image';
-  img.src = 'public/lottoImage.png';
+  img.src = '/lottoImage.png';
   img.alt = 'lotto-image';
 
   const numbersDiv = document.createElement('div');
