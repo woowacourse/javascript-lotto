86c3f55ba4faec5d571c9ad8096765587dd30310
jin123457 <jin_ox@naver.com>
2025-03-02 13:56:22 +0900

    refactor: 로또 입력 필드의 수량을 상수로 변경

--- src/components/lottoContent/inputList/inputFormInputBox/inputFormInputBox.js
+++ src/components/lottoContent/inputList/inputFormInputBox/inputFormInputBox.js
@@ -2,14 +2,14 @@ import createDomElement from '../../../../utils/createDomElement.js';
 import $bonusInput from './bonusInput/bonusInput.js';
 import $inputFormButton from './inputFormButton/inputFormButton.js';
 import $winningInputs from './winningInputs/winningInputs.js';
+import { LOTTO_RULE } from '../../../../domain/constants.js';
 
 const $lottoFormInputBox = () => {
   const inputFormInputBox = createDomElement('form', {
     className: 'lotto_input_box',
     id: 'winningNumberInputForm',
   });
-
-  inputFormInputBox.appendChild($winningInputs(6));
+  inputFormInputBox.appendChild($winningInputs(LOTTO_RULE.LENGTH));
   inputFormInputBox.appendChild($bonusInput());
   inputFormInputBox.appendChild($inputFormButton());
 
