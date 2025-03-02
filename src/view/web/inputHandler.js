ad6af0e25d43bf01a96f21c0977dbb887bf14ad0
H0ngJu <hongju4385@gmail.com>
2025-03-02 23:48:15 +0900

    refactor: input handler 분리

--- src/view/web/inputHandler.js
+++ src/view/web/inputHandler.js
@@ -1,16 +1,36 @@
 import { DOM } from '../../DOM/dom.js';
 import { validatePurchasePrice } from '../../validation/validatePurchasePrice.js';
 import { errorHandler } from '../../utils/errorHandler.js';
+import { LottoGame } from './LottoGame.js';
+import { showLottos, showPurchaseResult } from './outputHandler.js';
+import { generateLottos } from '../../domain/generateLottos.js';
+import { LOTTO_SYSTEM } from '../../constants/LottoSystem.js';
 
-export const handlePurchase = (event, callback) => {
+export const handlePurchaseSubmit = (event) => {
   event.preventDefault();
-  const formData = new FormData(DOM.purchaseForm);
-  const purchasePrice = Number(formData.get('purchase-input'));
-
   try {
+    const purchasePrice = DOM.purchaseInput.value;
     validatePurchasePrice(purchasePrice);
-    callback(purchasePrice);
+    LottoGame.purchasePrice = purchasePrice;
+    disablePurchaseInputs();
+    purchaseLottos();
   } catch (error) {
-    errorHandler(error.message);
+    errorHandler(error);
   }
+};
+
+const purchaseLottos = () => {
+  const quantity = Math.floor(LottoGame.purchasePrice / LOTTO_SYSTEM.MIN_PURCHASE_PRICE);
+  showPurchaseResult(quantity);
+  LottoGame.lottos = generateLottos(quantity);
+  showLottos(LottoGame.lottos);
+  DOM.systemMessage.style.display = 'flex';
+};
+
+export const handleWinningNumberInput = (index, value) => {
+  LottoGame.winningNumbers[index] = Number(value);
+};
+
+export const handleBonusNumberInput = (value) => {
+  LottoGame.bonusNumber = Number(value);
 };
