821f638575d6318d6d974b5b11b581c322cbd3fa
dev-dino22 <wlsskdud02@naver.com>
2025-03-03 06:11:57 +0900

    fix: readline 사용하는 파일 잠시 주석처리

--- src/util/InputHandler.js
+++ src/util/InputHandler.js
@@ -1,58 +1,58 @@
-import ERROR from "../constant/Error.js";
-import Validator from "../domain/Validator.js";
-import Output from "../view/Output.js";
-import { throwError } from "./util.js";
-import readline from "readline";
+// import ERROR from "../constant/Error.js";
+// import Validator from "../domain/Validator.js";
+// import Output from "../view/Output.js";
+// import { throwError } from "./util.js";
+// import readline from "readline";
 
-export async function inputHandler({
-  promptMessage,
-  parser,
-  validatorMethod,
-  errorName,
-}) {
-  try {
-    const userInput = await userInputEmptyHandler(promptMessage);
-    const parsedUserInput = parser ? parser(userInput) : userInput;
-    const parsedUserInputError = validatorMethod(parsedUserInput);
-    Output.printErrorResults(parsedUserInputError, errorName);
-    throwError(parsedUserInputError);
-    return parsedUserInput;
-  } catch (error) {
-    return inputHandler({
-      promptMessage,
-      parser,
-      validatorMethod,
-      errorName,
-    });
-  }
-}
+// export async function inputHandler({
+//   promptMessage,
+//   parser,
+//   validatorMethod,
+//   errorName,
+// }) {
+//   try {
+//     const userInput = await userInputEmptyHandler(promptMessage);
+//     const parsedUserInput = parser ? parser(userInput) : userInput;
+//     const parsedUserInputError = validatorMethod(parsedUserInput);
+//     Output.printErrorResults(parsedUserInputError, errorName);
+//     throwError(parsedUserInputError);
+//     return parsedUserInput;
+//   } catch (error) {
+//     return inputHandler({
+//       promptMessage,
+//       parser,
+//       validatorMethod,
+//       errorName,
+//     });
+//   }
+// }
 
-async function userInputEmptyHandler(promptMessage) {
-  const userInput = await readLineAsync(promptMessage);
-  const userInputError = Validator.userInput(userInput);
-  Output.printErrorResults(userInputError, ERROR.USER_INPUT);
-  throwError(userInputError);
-  return userInput;
-}
+// async function userInputEmptyHandler(promptMessage) {
+//   const userInput = await readLineAsync(promptMessage);
+//   const userInputError = Validator.userInput(userInput);
+//   Output.printErrorResults(userInputError, ERROR.USER_INPUT);
+//   throwError(userInputError);
+//   return userInput;
+// }
 
-export async function readLineAsync(query) {
-  return new Promise((resolve, reject) => {
-    if (arguments.length !== 1) {
-      reject(new Error("arguments must be 1"));
-    }
+// export async function readLineAsync(query) {
+//   return new Promise((resolve, reject) => {
+//     if (arguments.length !== 1) {
+//       reject(new Error("arguments must be 1"));
+//     }
 
-    if (typeof query !== "string") {
-      reject(new Error("query must be string"));
-    }
+//     if (typeof query !== "string") {
+//       reject(new Error("query must be string"));
+//     }
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
 
-    rl.question(query, (input) => {
-      rl.close();
-      resolve(input);
-    });
-  });
-}
+//     rl.question(query, (input) => {
+//       rl.close();
+//       resolve(input);
+//     });
+//   });
+// }
