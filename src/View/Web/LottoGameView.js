436dfa74a5fed73912bcfc235d4559ce32817cda
Oliy <eunsoa0310@gmail.com>
2025-03-02 22:54:09 +0900

    fix: import 문 오타 수정

--- src/View/Web/LottoGameView.js
+++ src/View/Web/LottoGameView.js
@@ -1,4 +1,4 @@
-import { LOTTO_PRIZE_DEFINITION } from '../../Domain/Constant/definition.js';
+import { LOTTO_PRIZE_DEFINITION } from '../../Domain/Constant/Definition.js';
 
 class LottoGameView {
   #state;
