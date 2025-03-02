9c487af85b3f27946027bd4e855ee436886c9595
deokbin <deokbin@naver.com>
2025-03-02 13:51:28 +0900

    refactor: 에러 핸들러 UI 와 로또 인풋 에러 여부에 따라 분리

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -1,10 +1,19 @@
 import { printError } from '../View/OutputView.js';
+import clearLottoInputs from '../View/clear/clearLottoInputs.js';
 import clearUIElements from '../View/clear/clearUIElements.js';
 
-export const uiErrorHandler = (error) => {
-  printError(error.message);
+export const priceErrorHandler = (error) => {
   clearUIElements();
+  printError(error.message);
   alert(error.message);
+  throw error;
+};
+
+export const lottoInputErrorHandler = (error) => {
+  clearLottoInputs();
+  printError(error.message);
+  alert(error.message);
+  throw error;
 };
 
 export const defaultErrorHandler = (error) => {
