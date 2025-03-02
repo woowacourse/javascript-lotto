f6502b62519d882f02a629b19e419cc240a259bd
Jeongeun Lee <leeju1009@gmail.com>
2025-03-02 16:52:20 +0900

    chore: 불필요한 코드 제거

--- src/view/web/modules/restartGame.js
+++ src/view/web/modules/restartGame.js
@@ -1,6 +1,5 @@
 const restartGame = () => {
   const restartButton = document.querySelector(".restart-button");
-  const body = document.querySelector("body");
 
   restartButton.addEventListener("click", () => {
     location.replace(location.href);
