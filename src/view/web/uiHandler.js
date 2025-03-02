af136549d121c21c09d911fe296f65db8f6855d9
H0ngJu <hongju4385@gmail.com>
2025-03-02 23:47:42 +0900

    refactor: UI handler 분리

---
+++ src/view/web/uiHandler.js
@@ -0,0 +1,22 @@
+import { DOM } from '../../DOM/dom.js';
+
+export const disableWinningInputs = () => {
+  DOM.bonusInput.disabled = true;
+  DOM.bonusInput.style.cursor = 'not-allowed';
+  DOM.winningInputs.forEach((input) => {
+    input.disabled = true;
+    input.style.cursor = 'not-allowed';
+  });
+};
+
+export const disablePurchaseInputs = () => {
+  DOM.purchaseButton.disabled = true;
+  DOM.purchaseButton.style.cursor = 'not-allowed';
+  DOM.purchaseInput.disabled = true;
+  DOM.purchaseInput.style.cursor = 'not-allowed';
+};
+
+export const handleModalCloseClick = () => {
+  DOM.modal.style.display = 'none';
+  document.body.style.overflow = '';
+};
