6afed9d05495a6f27da4e123b06d6e3cfceda96a
aydenote <dslgpgh@gmail.com>
2025-03-03 16:41:52 +0900

    feat: 로또 구매 수량 UI 에 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,6 +2,29 @@ import Validate from './Model/Validate.js';
 
 const input_price = document.querySelector('.input_price');
 
+// 로또 구매 수량 요소 생성
+export function createLottoCountElement(count) {
+  const buy_count = document.createElement('p');
+  buy_count.innerText = `총 ${count}개를 구매하였습니다.`;
+  buy_count.classList.add('buy_count');
+  return buy_count;
+}
+
+// 로또 컨테이너 생성
+function createLottoContainer() {
+  const lotto_container = document.createElement('section');
+  lotto_container.classList.add('lotto_container');
+  return lotto_container;
+}
+
+// 로또 구매 수량 UI 생성
+function paintLottoCount(lottos) {
+  const main_container = document.getElementById('main_container');
+
+  main_container.appendChild(createLottoCountElement(lottos.length));
+  main_container.appendChild(createLottoContainer());
+}
+
 // 로또 생성
 function createLotto() {
   const lottoMachine = new LottoMachine();
@@ -17,6 +40,7 @@ function handleLottoPurchase() {
     Validate.checkPriceRange(input_price.value);
 
     const lottos = createLotto();
+    paintLottoCount(lottos);
   } catch (error) {
     alert(error.message);
   }
