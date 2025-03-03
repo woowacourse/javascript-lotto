2569480d13455c9373e85fd734bdbe5c29e80d3f
yeongipark <yeongi0111@naver.com>
2025-03-03 14:15:09 +0900

    refactor(step2-index.js): 필요없는 함수 호출 제거

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,9 +1,3 @@
 import { addBuyEventHandler } from "./eventHandler/buyHandler.js";
-import { addModalCloseEventHandler } from "./eventHandler/modalHandler.js";
-import { addModalRestartEventHandler } from "./eventHandler/restartHandler.js";
-import { addResultEventHandler } from "./eventHandler/resultHandler.js";
 
 addBuyEventHandler();
-addResultEventHandler();
-addModalRestartEventHandler();
-addModalCloseEventHandler();
