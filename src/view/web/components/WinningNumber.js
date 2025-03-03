f50072aa525306b44d5eb48c5c11111d3505a980
bunju20 <ghkd2271@naver.com>
2025-03-03 16:25:37 +0900

    refactor(WinningNumber) : 이벤트 위임, 검증로직 분리 - forEach 대신 이벤트 위임 사용해서 개선 - 검증 로직  validateWinningNumbers 함수로 분리

--- src/view/web/components/WinningNumber.js
+++ src/view/web/components/WinningNumber.js
@@ -7,9 +7,13 @@ export default class WinningNumbers {
     this.winningNumberInputs = DomHelper.querySelectorAll(
       ".target__lottos .one__numbers__input",
     );
+    this.winningNumberContainer = DomHelper.querySelector(".target__lottos");
+
     this.bonusNumberInput = DomHelper.querySelector(
       ".bonus__number .one__numbers__input",
     );
+
+    this.bonusNumberContainer = DomHelper.querySelector(".bonus__number");
     this.resultButton = DomHelper.querySelector(".result__btn");
     this.onResult = onResult;
 
@@ -20,44 +24,51 @@ export default class WinningNumbers {
     this.middleSection.style.display = "none";
     this.resultButton.addEventListener("click", this.handleResult.bind(this));
 
-    this.winningNumberInputs.forEach((input, index) => {
-      input.addEventListener("keydown", (event) => {
-        this.handleInputKeydown(event, index);
-      });
-    });
+    this.winningNumberContainer.addEventListener(
+      "keydown",
+      this.handleWinningInputKeydown.bind(this),
+    );
 
-    this.bonusNumberInput.addEventListener("keydown", (event) => {
-      this.handleBonusInputKeydown(event);
-    });
-
-    
+    this.bonusNumberContainer.addEventListener(
+      "keydown",
+      this.handleBonusInputKeydown.bind(this),
+    );
   }
 
-  handleInputKeydown(event, index) {
+  handleWinningInputKeydown(event) {
+    if (!event.target.classList.contains("one__numbers__input")) return;
+
     if (
-      event.key === "Enter" ||
-      event.key === "ArrowRight" ||
-      event.key === " "
-    ) {
-      event.preventDefault();
-      if (index < this.winningNumberInputs.length - 1) {
-        this.winningNumberInputs[index + 1].focus();
-      } else {
-        this.bonusNumberInput.focus();
-      }
+      event.key !== "Enter" &&
+      event.key !== "ArrowRight" &&
+      event.key !== " "
+    )
+      return;
+
+    event.preventDefault();
+
+    const inputs = Array.from(this.winningNumberInputs);
+    const currentIndex = inputs.indexOf(event.target);
+
+    if (currentIndex < this.winningNumberInputs.length - 1) {
+      this.winningNumberInputs[currentIndex + 1].focus();
+    } else {
+      this.bonusNumberInput.focus();
     }
   }
 
   handleBonusInputKeydown(event) {
+    if (!event.target.classList.contains("one__numbers__input")) return;
     if (
-      event.key === "Enter" ||
-      event.key === "ArrowRight" ||
-      event.key === " "
-    ) {
-      event.preventDefault();
+      event.key !== "Enter" &&
+      event.key !== "ArrowRight" &&
+      event.key !== " "
+    )
+      return;
 
-      this.resultButton.focus();
-    }
+    event.preventDefault();
+
+    this.resultButton.focus();
   }
 
   show() {
@@ -78,23 +89,7 @@ export default class WinningNumbers {
         throw new Error("로또를 먼저 구매해주세요.");
       }
 
-      const winningNumbers = [];
-
-      this.winningNumberInputs.forEach((input) => {
-        if (!input.value) {
-          throw new Error("당첨 번호를 모두 입력해주세요.");
-        }
-        winningNumbers.push(Number(input.value));
-      });
-
-      Validator.isTargetNumber(winningNumbers.join(", "));
-
-      if (!this.bonusNumberInput.value) {
-        throw new Error("보너스 번호를 입력해주세요.");
-      }
-
-      const bonusNumber = Number(this.bonusNumberInput.value);
-      Validator.isBonusNumber(bonusNumber, winningNumbers);
+      const { winningNumbers, bonusNumber } = this.validateWinningNumbers();
 
       this.onResult(winningNumbers, bonusNumber);
     } catch (error) {
@@ -108,5 +103,27 @@ export default class WinningNumbers {
     });
     this.bonusNumberInput.value = "";
     this.hide();
+  }
+
+  validateWinningNumbers() {
+    const winningNumbers = [];
+
+    this.winningNumberInputs.forEach((input) => {
+      if (!input.value) {
+        throw new Error("당첨 번호를 모두 입력해주세요.");
+      }
+      winningNumbers.push(Number(input.value));
+    });
+
+    Validator.isTargetNumber(winningNumbers.join(", "));
+
+    if (!this.bonusNumberInput.value) {
+      throw new Error("보너스 번호를 입력해주세요.");
+    }
+
+    const bonusNumber = Number(this.bonusNumberInput.value);
+    Validator.isBonusNumber(bonusNumber, winningNumbers);
+
+    return { winningNumbers, bonusNumber };
   }
 }
