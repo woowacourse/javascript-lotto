4293f36c17ed1866e8fa94473e61f882be0e1200
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-07 02:25:26 +0900

    test(Validate): 유효성 검사 테스트 리팩터링 및 구조 개선
    - validateWinningNumbers() 와validateBonusNumber() 제거 후, 개별 검증 함수 기반으로 테스트 재작성

--- __tests__/Validate.test.js
+++ __tests__/Validate.test.js
@@ -2,13 +2,10 @@ import {
   MIN_PRICE,
   MAX_PRICE,
   LOTTO_NUMBER_LENGTH,
-  MIN_LOTTO_NUMBER,
-  MAX_LOTTO_NUMBER,
 } from '../src/constants/common.js';
 import Validate from '../src/Model/Validate.js';
 
 describe('Validate 유효성 검사 테스트', () => {
-
   describe('validatePrice() - 구입 금액 입력 검증', () => {
     test.each(['', '   '])('공백 입력 예외처리', (price) => {
       expect(() => {
@@ -36,75 +33,93 @@ describe('Validate 유효성 검사 테스트', () => {
     });
   });
 
-  describe('validateWinningNumbers() - 당첨 번호 입력 검증', () => {
-    test.each(['', '   '])('공백 입력 예외처리', (winningNumbersInput) => {
+  describe('checkIsEmpty() - 공백 입력 검증', () => {
+    test.each(['', '   '])('공백 입력 예외처리', (input) => {
       expect(() => {
-        Validate.validateWinningNumbers(winningNumbersInput);
+        Validate.checkIsEmpty(input);
       }).toThrow('[ERROR] 공백 입력이 되었습니다.');
     });
 
-    test.each(['1,2,3,4,5', '1,2,3,4,5,6,7'])(
-      '당첨 번호 6개 미만/초과 입력 예외처리',
-      (winningNumbersInput) => {
-        expect(() => {
-          Validate.validateWinningNumbers(winningNumbersInput);
-        }).toThrow(`[ERROR] ${LOTTO_NUMBER_LENGTH}개의 숫자를 입력해주세요.`);
-      }
-    );
-
-
-    test('쉼표만 입력된 경우 예외처리', () => {
-      const winningNumbersInput = ',,,,,'; // 공백 값만 포함
+    test('공백이 아닌 정상 입력 (예외 발생하지 않음)', () => {
       expect(() => {
-        Validate.validateWinningNumbers(winningNumbersInput);
-      }).toThrow('[ERROR] 공백 입력이 되었습니다.');
-    });
-
-    test.each(['1,2,3,4,5,0', '1,2,3,4,5,46'])(
-      '1~45 범위 미만, 초과 값 예외처리',
-      (winningNumbersInput) => {
-        expect(() => {
-          Validate.validateWinningNumbers(winningNumbersInput);
-        }).toThrow();
-      }
-    );
-
-    test('중복된 당첨 번호 입력 예외처리', () => {
-      const duplicatedWinningNumbers = '1,2,3,4,5,5';
-      expect(() => {
-        Validate.validateWinningNumbers(duplicatedWinningNumbers);
-      }).toThrow('[ERROR] 당첨 번호가 중복 입력되었습니다.');
+        Validate.checkIsEmpty('123');
+      }).not.toThrow();
     });
   });
 
-  describe('validateBonusNumber() - 보너스 번호 검증', () => {
-    test.each(['', '   '])('공백 입력 예외처리', (bonusNumber) => {
+  describe('checkIsNumber() - 숫자 입력 검증', () => {
+    test.each(['abc', '천원'])('숫자가 아닌 입력 예외처리', (input) => {
       expect(() => {
-        Validate.validateBonusNumber(bonusNumber, '1,2,3,4,5,6');
-      }).toThrow('[ERROR] 공백 입력이 되었습니다.');
-    });
-
-    test.each(['a', '!', '3a'])('숫자가 아닌 입력 예외처리', (bonusNumber) => {
-      expect(() => {
-        Validate.validateBonusNumber(bonusNumber, '1,2,3,4,5,6');
+        Validate.checkIsNumber(input);
       }).toThrow('[ERROR] 숫자 이외의 입력입니다.');
     });
 
-    test.each([`${MIN_LOTTO_NUMBER - 1}`, `${MAX_LOTTO_NUMBER + 1}`])(
-      '1 ~ 45 범위 초과 값 예외처리',
-      (bonusNumber) => {
-        expect(() => {
-          Validate.validateBonusNumber(bonusNumber, '1,2,3,4,5,6');
-        }).toThrow('[ERROR] 로또 숫자의 범위는 1 ~ 45 입니다.');
-      }
-    );
-
-    test('보너스 번호가 당첨 번호와 중복 입력된 경우 예외처리', () => {
-      const bonusNumber = '6';
-      const winningNumbers = '1,2,3,4,5,6';
+    test.each(['123', '1000', '45'])('숫자 입력 (예외 발생하지 않음)', (input) => {
       expect(() => {
-        Validate.validateBonusNumber(bonusNumber, winningNumbers);
+        Validate.checkIsNumber(input);
+      }).not.toThrow();
+    });
+  });
+
+  describe('checkWinningNumberCount() - 당첨 번호 개수 검증', () => {
+    test('당첨 번호가 부족할 때 예외처리', () => {
+      expect(() => {
+        Validate.checkWinningNumberCount([1, 2, 3, 4, 5]);
+      }).toThrow(`[ERROR] ${LOTTO_NUMBER_LENGTH}개의 숫자를 입력해주세요.`);
+    });
+
+    test('당첨 번호가 초과할 때 예외처리', () => {
+      expect(() => {
+        Validate.checkWinningNumberCount([1, 2, 3, 4, 5, 6, 7]);
+      }).toThrow(`[ERROR] ${LOTTO_NUMBER_LENGTH}개의 숫자를 입력해주세요.`);
+    });
+
+    test('당첨 번호가 정확히 6개일 때 정상 처리', () => {
+      expect(() => {
+        Validate.checkWinningNumberCount([1, 2, 3, 4, 5, 6]);
+      }).not.toThrow();
+    });
+  });
+
+  describe('checkLottoNumberRange() - 로또 번호 범위 검증', () => {
+    test.each([0, 46, -1, 100])('범위를 벗어난 숫자 예외처리', (input) => {
+      expect(() => {
+        Validate.checkLottoNumberRange(input);
+      }).toThrow('[ERROR] 로또 숫자의 범위는 1 ~ 45 입니다.');
+    });
+
+    test.each([1, 10, 45])('1~45 범위 내 숫자는 정상 처리', (input) => {
+      expect(() => {
+        Validate.checkLottoNumberRange(input);
+      }).not.toThrow();
+    });
+  });
+
+  describe('checkWinningNumberDuplicate() - 당첨 번호 중복 검증', () => {
+    test('중복된 번호가 있을 때 예외처리', () => {
+      expect(() => {
+        Validate.checkWinningNumberDuplicate([1, 2, 3, 4, 5, 5]);
+      }).toThrow('[ERROR] 당첨 번호가 중복 입력되었습니다.');
+    });
+
+    test('중복 없이 6개 숫자 입력 시 정상 처리', () => {
+      expect(() => {
+        Validate.checkWinningNumberDuplicate([1, 2, 3, 4, 5, 6]);
+      }).not.toThrow();
+    });
+  });
+
+  describe('checkBonusNumberDuplicate() - 보너스 번호 중복 검증', () => {
+    test('보너스 번호가 당첨 번호와 중복될 때 예외처리', () => {
+      expect(() => {
+        Validate.checkBonusNumberDuplicate([1, 2, 3, 4, 5, 6], 5);
       }).toThrow('[ERROR] 당첨 번호와 중복 입력입니다.');
+    });
+
+    test('보너스 번호가 당첨 번호와 다를 때 정상 처리', () => {
+      expect(() => {
+        Validate.checkBonusNumberDuplicate([1, 2, 3, 4, 5, 6], 7);
+      }).not.toThrow();
     });
   });
 
