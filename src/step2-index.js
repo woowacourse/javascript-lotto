ffdb580572e8901277191735a5f11b547f3846c3
sooyeoniya <tndus1306403@naver.com>
2025-03-08 21:01:21 +0900

    refactor(styles): 본 프로젝트에 더 효율적인 스타일 로드 방식 적용
    - CSS 내부의 @import 방식은 link 방식보다 로드 시간 긺 (link 방식은 여러 CSS 파일을 병렬 처리)
    - JS 파일 내부에서 global.css 를 import 로 호출하는 것보다 link 태그를 사용하는 것이 FOUC 문제 방지 가능
    - link 태그는 HTML에서 CSS 함께 로드하기 때문에 스타일 빠르게 적용 가능
    - link 태그방식은 CSS 가 모든 페이지에 로드되기 때문에 전역 스타일 적용에 적합
    
    결론: global.css, reset.css 를 모두 link 태그 방식으로 변경함

--- src/step2-index.js
+++ src/step2-index.js
@@ -3,7 +3,6 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-import "./styles/global.css";
 import LottoDomain from "./domains/web/LottoDomain.js";
 import View from "./views/web/View.js";
 import LottoController from "./controllers/web/LottoController.js";
