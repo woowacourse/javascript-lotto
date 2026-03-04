8e92530b96a422b2e71b6c1e2b2989d1d6928c26
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-04 17:13:17 +0900

    fix(InputConsole): 에러메시지 출력 기능 추가

--- src/Console/InputConsole.js
+++ src/Console/InputConsole.js
@@ -2,9 +2,17 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const InputConsole = {
   async readPurchasePrice() {
-    const purchasePrice =
-      await MissionUtils.Console.readLineAsync("> 구입금액을 입력해 주세요.");
-    return purchasePrice;
+    while (true) {
+      try {
+        const purchasePrice =
+          await MissionUtils.Console.readLineAsync(
+            "> 구입금액을 입력해 주세요.",
+          );
+        return purchasePrice;
+      } catch (e) {
+        Console.print(e.message);
+      }
+    }
   },
 };
 
