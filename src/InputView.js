61b3ce1b87f7fd355c488bd6d6624027920d0462
박건규 <geongyu09@gmail.com>
2026-03-04 20:20:28 +0900

    refactor(InputView): InputView 객체를 클래스로 마이그레이션

--- src/InputView.js
+++ src/InputView.js
@@ -1,36 +1,36 @@
 import readline from "readline/promises";
 
-async function getUserInput(message) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+class InputView {
+  async getUserInput(message) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  const input = await rl.question(message);
-  rl.close();
-  return input;
-}
+    const input = await rl.question(message);
+    rl.close();
+    return input;
+  }
 
-const InputView = {
-  askAmount: async () => {
+  async askAmount() {
     const MESSAGE = "> 구입금액을 입력해 주세요. ";
-    return await getUserInput(MESSAGE);
-  },
+    return await this.getUserInput(MESSAGE);
+  }
 
-  askWinningNumbers: async () => {
+  async askWinningNumbers() {
     const MESSAGE = "> 당첨 번호를 입력해 주세요. ";
-    return await getUserInput(MESSAGE);
-  },
+    return await this.getUserInput(MESSAGE);
+  }
 
-  askBonusNumber: async () => {
+  async askBonusNumber() {
     const MESSAGE = "> 보너스 번호를 입력해 주세요. ";
-    return await getUserInput(MESSAGE);
-  },
+    return await this.getUserInput(MESSAGE);
+  }
 
-  askRetry: async () => {
+  async askRetry() {
     const MESSAGE = "> 다시 시작하시겠습니까? (y/n)";
-    return await getUserInput(MESSAGE);
-  },
-};
+    return await this.getUserInput(MESSAGE);
+  }
+}
 
 export default InputView;
