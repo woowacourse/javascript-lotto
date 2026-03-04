4361db47acdaf6c2624db2a2df4528605aafc55d
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-04 18:35:59 +0900

    test: 구입 개수 만큼 로또 생성 테스트 추가
    Co-authored-by: TH_97 <hood3392@gmail.com>

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -23,4 +23,20 @@ describe('로또 머신 테스트', () => {
         lottoMachine = new LottoMachine(7000)
         expect(lottoMachine.purchaseCount).toBe(7);
     });
+
+    test('구입 개수 만큼 로또 생성 테스트', () =>{
+        pickNumberInRange
+        .mockReturnValueOnce([1, 4, 5, 6, 8, 9])
+        .mockReturnValueOnce([3, 17, 19, 21, 23, 31])
+        .mockReturnValueOnce([35, 37, 39, 41, 42, 45]);
+        const lottoMachine = new LottoMachine(3000);
+        
+        expect(lottoMachine.getLotto()).toEqual(
+            [
+                [1, 4, 5, 6, 8, 9],
+                [3, 17, 19, 21, 23, 31],
+                [35, 37, 39, 41, 42, 45],
+            ]
+        );
+    });
 });
