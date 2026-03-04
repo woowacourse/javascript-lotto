c4ccdd211e44f5e96898e3621bbf0ec0ca44abf3
박건규 <geongyu09@gmail.com>
2026-03-04 20:48:36 +0900

    test(InputView.test): validateWinningNumbers 메서드 테스트 작성

--- __tests__/InputView.test.js
+++ __tests__/InputView.test.js
@@ -20,4 +20,26 @@ describe("InputView 클래스 유닛 테스트", () => {
       expect(() => inputView.validateAmount(invalidAmounts)).toThrow();
     });
   });
+
+  describe("validateWinningNumbers 메서드 유닛 테스트", () => {
+    test("입력값은 ,로 구분된 숫자여야한다.", () => {
+      // given
+      const inputView = new InputView();
+      const input = "1,2,3,4,5,6";
+
+      // when & then
+      expect(() => inputView.validateWinningNumbers(input)).not.toThrow();
+    });
+
+    test("숫자와 구분자 외 다른 문자가 포함되면 예외를 반환한다.", () => {
+      // given
+      const inputView = new InputView();
+      const invalidWinningNumbers = "1%2,3/4,5,6";
+
+      // when & then
+      expect(() =>
+        inputView.validateWinningNumbers(invalidWinningNumbers),
+      ).toThrow();
+    });
+  });
 });
