01d365de0912657a664d8052c7f48622f21a1426
박건규 <geongyu09@gmail.com>
2026-03-04 18:55:07 +0900

    test(utils.test): generateUniqueRandomNumbers 테스트 작성

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,11 +1,13 @@
-import { generateRandomNumber } from "../src/utils";
-
-const mockMathRandom = jest.spyOn(Math, "random").mockReturnValue(0.5);
+import {
+  generateRandomNumber,
+  generateUniqueRandomNumbers,
+} from "../src/utils";
 
 describe("utils 유닛테스트", () => {
   describe("generateRandomNumber", () => {
     test("1부터 주어진 값까지의 랜덤한 양의 정수를 반환한다.", () => {
       // given
+      jest.spyOn(Math, "random").mockReturnValueOnce(0.5);
       const to = 45;
 
       // when
@@ -16,7 +18,52 @@ describe("utils 유닛테스트", () => {
     });
   });
 
-  afterAll(() => {
-    mockMathRandom.mockRestore();
+  describe("generateUniqueRandomNumbers", () => {
+    test("입력받은 길이의 1부터 주어진 값까지의 랜덤한 양의 정수 배열을 반환한다.", () => {
+      // given
+      jest
+        .spyOn(Math, "random")
+        .mockReturnValueOnce(0.1)
+        .mockReturnValueOnce(0.3)
+        .mockReturnValueOnce(0.5)
+        .mockReturnValueOnce(0.7)
+        .mockReturnValueOnce(0.9)
+        .mockReturnValueOnce(0.2);
+      const to = 45;
+      const length = 6;
+
+      // when
+      const result = generateUniqueRandomNumbers(to, length);
+
+      // then
+      expect(result.length).toBe(6);
+    });
+
+    test("입력받은 길이의 1부터 주어진 값까지의 랜덤한 중복되지 않은 숫자 배열을 반환한다.", () => {
+      // given
+      jest
+        .spyOn(Math, "random")
+        .mockReturnValueOnce(0.1)
+        .mockReturnValueOnce(0.3)
+        .mockReturnValueOnce(0.5)
+        .mockReturnValueOnce(0.7)
+        .mockReturnValueOnce(0.9)
+        .mockReturnValueOnce(0.9)
+        .mockReturnValueOnce(0.2);
+
+      const to = 45;
+      const length = 6;
+
+      // when
+      const result = generateUniqueRandomNumbers(to, length);
+      const set = new Set([...result]);
+
+      // then
+      expect(set.size).toBe(6);
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 });
