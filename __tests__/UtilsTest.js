3249f13e6df195c9d301599bcadf1a8def6ca202
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-04 15:09:48 +0900

    feat: 두 수 사이의 정수 난수 배열 반환 기능 추가
    Co-authored-by: TH_97 <hood3392@gmail.com>

---
+++ __tests__/UtilsTest.js
@@ -0,0 +1,16 @@
+import { pickNumberInRange } from '../src/step1/Utils';
+
+
+describe('유틸 함수 테스트', () => {
+    test('범위내 랜덤값 반환 테스트', () => {
+        jest.spyOn(global.Math, 'random')
+        .mockReturnValueOnce(0.1)
+        .mockReturnValueOnce(0.2)
+        .mockReturnValueOnce(0.3)
+        .mockReturnValueOnce(0.4)
+        .mockReturnValueOnce(0.5)
+        .mockReturnValueOnce(0.6);
+
+        expect(pickNumberInRange(1, 10, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+});
---
+++ src/step1/Utils.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * 두 수를 포함하는 두 수 사이의 정수 난수 배열 반환.
+ * @param {Number} min 
+ * @param {Number} max 
+ * @param {Number} range 
+ * @returns {number[]}
+ */
+export function pickNumberInRange(min, max, range) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    const numbers = new Set();
+    while (true) {
+        if (numbers.size === range) {
+            break;
+        }
+        const randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+        numbers.add(randomNumber);
+    }
+
+    return [...numbers];
+}
