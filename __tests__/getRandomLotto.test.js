82c2b62a0a988aeeb5ba7f6c9ee1598701e45f69
eunnn <ehddms0517@naver.com>
2026-03-04 17:45:27 +0900

    test: 로또 객체 배열 반환 테스트 코드 작성

--- __tests__/getRandomLotto.test.js
+++ __tests__/getRandomLotto.test.js
@@ -1,8 +1,16 @@
-import { getRandomLotto } from "../src/service/getRandomLotto.js";
+import { getRandomLotto, getLottos } from "../src/service/getRandomLotto.js";
 
 describe("getRandomLotto", () => {
   test("랜덤 로또 번호 6개를 반환한다.", () => {
     const result = getRandomLotto();
     expect(result.length).toBe(6);
+  });
+});
+
+describe("getLottos", () => {
+  test("구입 금액만큼 랜덤으로 발급받은 로또 객체를 반환한다.", () => {
+    const count = 2;
+    const result = getLottos(count);
+    expect(result.length).toBe(2);
   });
 });
