f3a8b604a89ac6a58c5e242f1f6208b6ed37d8d0
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-04 17:33:38 +0900

    feat: 구입 금액 입력 기능 추가
    - 구입 금액 입력 검증 추가
    
    Co-authored-by: TH_97 <hood3392@gmail.com>

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,28 +1,36 @@
-import readline from 'readline';
+import { readLine } from '../src/step1/Utils.js';
 import App from '../src/step1/App.js';
 
+jest.mock('../src/step1/Utils.js', () => ({
+    readLine: jest.fn(),
+    read: { close: jest.fn() },
+}));
+const logSpy = jest.spyOn(console, 'log');
 
 describe('구입 금액 입력예외 테스트', () => {
-    test('1000원 단위가 아닌 입력예외 테스트', () => {
-        jest.spyOn(readline, 'createInterface').mockImplementationOnce(() => {
-            return ['600'];
+    test('1000원 단위가 아닌 입력예외 테스트', async () => {
+        const expectedAnswer = ['600', '1000'];
+        expectedAnswer.forEach((answer) => {
+            readLine.mockImplementationOnce(() => {
+                return answer;
+            });
         });
-        const logSpy = jest.spyOn(console, 'log');
 
-        const app = App();
-        app.run();
+        const app = new App();
+        await app.run();
 
         expect(logSpy).toHaveBeenCalledWith('[ERROR] 1000원 단위만 입력 가능합니다.');
     });
 
-    test('정수가 아닌 입력 예외 테스트', () => {
-        jest.spyOn(readline, 'createInterface').mockImplementationOnce(() => {
-            return ['ㅁ'];
+    test('정수가 아닌 입력 예외 테스트', async () => {
+        const expectedAnswer = ['ㅁ', '1000'];
+        expectedAnswer.forEach((answer) => {
+            readLine.mockImplementationOnce(() => {
+                return answer;
+            });
         });
-        const logSpy = jest.spyOn(console, 'log');
-
-        const app = App();
-        app.run();
+        const app = new App();
+        await app.run();
 
         expect(logSpy).toHaveBeenCalledWith('[ERROR] 숫자만 입력해 주세요.');
     });
--- src/step1/App.js
+++ src/step1/App.js
@@ -1,6 +1,26 @@
-class App {
-    run() {
+import { readLine, read } from './Utils.js';
 
+class App {
+    async run() {
+        const amount = await this.amount();
+    }
+
+    async amount(){
+        while (true) {
+            try{
+                const answer = await readLine('구입금액을 입력해 주세요.');
+                if (!Number.isInteger(Number(answer))) {
+                    throw new Error('숫자만 입력해 주세요.');
+                }
+                if (Number(answer) % 1000 !== 0) {
+                    throw new Error('1000원 단위만 입력 가능합니다.');
+                }
+                read.close();
+                return answer;
+            } catch(err){
+                console.log(`[ERROR] ${err.message}`);
+            }
+        }
     }
 }
 
