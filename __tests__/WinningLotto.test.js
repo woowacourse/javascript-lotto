cf0ee3fad8c8e0e72dbc9ee8dfca8268803e918e
eunnn <ehddms0517@naver.com>
2026-03-05 11:07:19 +0900

    test: 당첨 번호와 보너스 번호 매개변수 수정

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -2,9 +2,10 @@ import WinningLotto from "../src/domain/WinningLotto.js";
 
 describe("WinningLotto", () => {
   test("당첨 번호와 보너스 번호를 받는 객체를 생성한다.", () => {
-    const number = { "당첨 번호": [1, 2, 3, 4, 5, 6], "보너스 번호": 7 };
+    const winningNumber = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
 
-    const lotto = new WinningLotto(number);
+    const lotto = new WinningLotto(winningNumber, bonusNumber);
 
     expect(lotto.getWinningNumber()).toStrictEqual([1, 2, 3, 4, 5, 6]);
     expect(lotto.getBonusNumber()).toStrictEqual(7);
