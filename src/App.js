5239a4fa35150a30932e10dd67c0fd3d09ed4db1
stardusty-lab <startdusty.lab@gmail.com>
2026-03-05 17:13:07 +0900

    feat: 가격, 당첨 번호, 보너스 번호 유효성 검사 로직

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import LottoGame from "./Model/LottoGame.js";
 import LottoList from "./Model/LottoList.js";
 import Rate from "./Model/Rate.js";
 import { parsingNumbers } from "./utils/parsing.js";
+import Validator from "./Validator.js";
 import InputView from "./View/InputView.js";
 import OutputView from "./View/OutputView.js";
 
 class App {
-  async run() {
-    const inputView = new InputView();
-    const price = await inputView.readPrice();
-    const lottoNumbers = await inputView.readLottoNumbers();
-    const bonusNumber = await inputView.readBonusNumber();
+  #validator;
+  #inputView;
+  #outputView;
 
-    const outputView = new OutputView();
-    outputView.printAmount(price);
+  constructor() {
+    this.#validator = new Validator();
+    this.#inputView = new InputView();
+    this.#outputView = new OutputView();
+  }
+
+  async run() {
+    const price = await this.#inputView.readPrice();
+    this.#validator.validatePrice(price);
 
     const lottoList = new LottoList(price / 1000);
-    outputView.printLottos(lottoList.getLottoList());
+    this.#outputView.printAmount(price);
+    this.#outputView.printLottos(lottoList.getLottoList());
 
+    const lottoNumbers = await this.#inputView.readLottoNumbers();
     const winningNumbers = parsingNumbers(lottoNumbers);
+    this.#validator.validateLottoNumbers(winningNumbers);
 
-    
+    const bonusNumber = await this.#inputView.readBonusNumber();
+    this.#validator.validateBonusNumber(bonusNumber);
 
     const lottoGame = new LottoGame(winningNumbers, bonusNumber);
-
     const statistics = lottoGame.getStatistics(lottoList);
-
     const rate = new Rate(statistics, price);
 
-    outputView.printStatistics(statistics);
-    outputView.printRate(rate.getRate());
+    this.#outputView.printStatistics(statistics);
+    this.#outputView.printRate(rate.getRate());
 
     // const isRetry = await inputView.readIsRetry();
   }
