c20a9e6510919608990047232210e267fc1ec9ce
박건규 <geongyu09@gmail.com>
2026-03-05 18:34:20 +0900

    test(Lotto.test): matchCount 메서드 테스트 작성

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -39,4 +39,18 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(result).toBe(false);
     });
   });
+
+  describe("matchCount", () => {
+    test("로또 번호와 당첨 번호 사이에 일치하는 번호 개수를 반환한다.", () => {
+      // given
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningNumbers = [1, 2, 3, 7, 8, 9];
+
+      // when
+      const result = lotto.matchCount(winningNumbers);
+
+      // then
+      expect(result).toBe(3);
+    });
+  });
 });
