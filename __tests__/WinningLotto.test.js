0e0f2776687d09ee0a9ddbb2aa0b3e5268756188
eunnn <ehddms0517@naver.com>
2026-03-05 11:17:59 +0900

    test: winningNumber 정수형 배열 테스트

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -2,7 +2,7 @@ import WinningLotto from "../src/domain/WinningLotto.js";
 
 describe("WinningLotto", () => {
   test("당첨 번호와 보너스 번호를 받는 객체를 생성한다.", () => {
-    const winningNumber = [1, 2, 3, 4, 5, 6];
+    const winningNumber = "1,2,3,4,5,6";
     const bonusNumber = 7;
 
     const lotto = new WinningLotto(winningNumber, bonusNumber);
