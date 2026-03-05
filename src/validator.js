1825dc17a682a2402a4da273cf8dbda416abef1e
hjkim0905 <eax9952@gmail.com>
2026-03-05 19:08:00 +0900

    refactor: refactor validation functions

--- src/validator.js
+++ src/validator.js
@@ -13,48 +13,50 @@ export const validateRange = (number) => {
   if (number < 1) throw new Error("[ERROR]");
   return number;
 };
-export const validateNoDuplicate = (numbers) => {
-  if (numbers.length !== new Set(numbers).size) throw new Error("[ERROR]");
-  return numbers;
+export const validateNoDuplicate = (lottoNumbers) => {
+  if (lottoNumbers.length !== new Set(lottoNumbers).size)
+    throw new Error("[ERROR]");
+  return lottoNumbers;
 };
-export const validateCount = (numbers) => {
-  if (numbers.length !== 6) throw new Error("[ERROR]");
-  return numbers;
+export const validateCount = (lottoNumbers) => {
+  if (lottoNumbers.length !== 6) throw new Error("[ERROR]");
+  return lottoNumbers;
 };
 export const validatePositive = (number) => {
   if (number < 0) throw new Error("[ERROR]");
   return number;
 };
-export const validateUnit = (number) => {
-  if (number % 1000 !== 0) throw new Error("[ERROR]");
-  return number;
+export const validateUnit = (purchaseAmount) => {
+  if (purchaseAmount % 1000 !== 0) throw new Error("[ERROR]");
+  return purchaseAmount;
 };
 export const validateYesNo = (string) => {
   if (string !== "y" && string !== "n") throw new Error("[ERROR]");
 };
+
 export const validatePurchaseAmount = (number) => {
-  const b = validateNumber(number);
-  const c = validatePositive(b);
-  const d = validateUnit(c);
-  return d;
+  validateNumber(number);
+  validatePositive(number);
+  validateUnit(number);
+  return number;
 };
-export const validateLottoNumbers = (numbers) => {
-  const a = validateNoDuplicate(numbers);
-  const b = validateCount(a);
-  b.forEach((number) => {
-    validateNumber(number);
-    validateRange(number);
+export const validateLottoNumbers = (lottoNumbers) => {
+  validateNoDuplicate(lottoNumbers);
+  validateCount(lottoNumbers);
+  lottoNumbers.forEach((lottoNumber) => {
+    validateNumber(lottoNumber);
+    validateRange(lottoNumber);
   });
-  return numbers;
+  return lottoNumbers;
 };
 export const validateBonusNumber = (bonusNumber, winningNumbers) => {
-  const a = validateNumber(bonusNumber);
-  const b = validateRange(a);
-  if (winningNumbers.includes(b)) throw new Error("[ERROR]");
+  validateNumber(bonusNumber);
+  validateRange(bonusNumber);
+  if (winningNumbers.includes(bonusNumber)) throw new Error("[ERROR]");
   return bonusNumber;
 };
 export const validateRestartInput = (yn) => {
-  const a = validateEmpty(yn);
-  validateYesNo(a);
+  validateEmpty(yn);
+  validateYesNo(yn);
   return yn;
 };
