4a7f8a5739780079913c31aea5fc51258989fd27
stardusty-lab <startdusty.lab@gmail.com>
2026-03-05 11:35:38 +0900

    refactor: test 코드를 each로 변경

--- __tests__/RateTest.js
+++ __tests__/RateTest.js
@@ -1,19 +1,23 @@
 import Rate from "../src/Model/Rate.js";
 
 describe("수익률 계산 검사 테스트", () => {
-  test("등수와 금액을 받아서 수익률을 계산한다", () => {
-    const statistics = {
-      1: 1,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      0: 1,
-    };
-    const price = 2000;
-
+  test.each([
+    [
+      "등수와 금액을 받아서 수익률을 계산한다",
+      {
+        1: 1,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        0: 1,
+      },
+      2000,
+      100000000.0,
+    ],
+  ])("%s", (description, statistics, price, result) => {
     const rate = new Rate(statistics, price);
 
-    expect(rate.getRate()).toBe(100000000.0);
+    expect(rate.getRate()).toBe(result);
   });
 });
