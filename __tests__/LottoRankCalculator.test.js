cfbdd66564ac324b0bca896a6f977b308eee385f
박건규 <geongyu09@gmail.com>
2026-03-05 18:43:57 +0900

    refactor(LottoRankCalculator): calculateLottoRank 메서드가 단일 lotto 인스턴스를 받도록 수정

--- __tests__/LottoRankCalculator.test.js
+++ __tests__/LottoRankCalculator.test.js
@@ -5,19 +5,19 @@ describe("LottoRankCalculator 클래스 유닛 테스트", () => {
   describe("calculateLottoRank", () => {
     test("당첨 번호와 일치하는 번호의 개수 + 보너스 번호의 포함 여부에 따라 올바른 등수를 반환해야 한다", () => {
       // given
-      const lottos = [new Lotto([1, 2, 3, 4, 5, 6])];
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
       const winningNumbers = [1, 2, 3, 4, 5, 6];
       const bonusNumber = 7;
 
       // when
       const rank = LottoRankCalculator.calculateLottoRank(
-        lottos,
+        lotto,
         winningNumbers,
         bonusNumber,
       );
 
       // then
-      expect(rank).toEqual({ 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
+      expect(rank).toEqual({ 1: 1, 2: 0, 3: 0, 4: 0, 5: 0 });
     });
   });
 });
--- src/LottoRankCalculator.js
+++ src/LottoRankCalculator.js
@@ -1,27 +1,19 @@
 class LottoRankCalculator {
-  static calculateLottoRank(lottos, winningNumbers, bonusNumber) {
-    // {1등: 6개 일치, 2등: 5개 + 보너스 번호 일치, 3등: 5개 일치, 4등: 4개 일치, 5등: 3개 일치, 꽝: 그 외}
-    const rank = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
+  static calculateLottoRank(lotto, winningNumbers, bonusNumber) {
+    const rank = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
 
-    lottos.forEach((lotto) => {
-      const matchCount = lotto.matchCount(winningNumbers);
-      if (matchCount === 6) {
-        return rank[1]++;
-      }
-      if (matchCount === 5 && lotto.includes(bonusNumber)) {
-        return rank[2]++;
-      }
-      if (matchCount === 5) {
-        return rank[3]++;
-      }
-      if (matchCount === 4) {
-        return rank[4]++;
-      }
-      if (matchCount === 3) {
-        return rank[5]++;
-      }
-      rank[6]++;
-    });
+    const matchCount = lotto.matchCount(winningNumbers);
+    if (matchCount === 6) {
+      rank[1]++;
+    } else if (matchCount === 5 && lotto.includes(bonusNumber)) {
+      rank[2]++;
+    } else if (matchCount === 5) {
+      rank[3]++;
+    } else if (matchCount === 4) {
+      rank[4]++;
+    } else if (matchCount === 3) {
+      rank[5]++;
+    }
 
     return rank;
   }
