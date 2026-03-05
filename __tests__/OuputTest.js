9740a9edfb89169675e72df8bd6cedbfb1d06bc8
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-05 11:02:25 +0900

    test: 로또 번호 출력 테스트 추가
    Co-authored-by: TH_97 <hood3392@gmail.com>

---
+++ __tests__/OuputTest.js
@@ -0,0 +1,28 @@
+import { LottoMachine } from '../src/step1/LottoMachine'
+import { Output } from '../src/step1/Output'
+
+const logSpy = jest.spyOn(console, 'log');
+
+describe('출력 테스트', () =>{
+    test('로또 번호 출력 테스트',() =>{
+
+        const lottoMachine = new LottoMachine(5000);
+
+        jest
+        .spyOn(lottoMachine, 'createLotto')
+        .mockReturnValueOnce([1, 2, 3, 4, 5, 6])
+        .mockReturnValueOnce([15, 11, 13, 12, 14, 10])
+        .mockReturnValueOnce([6, 5, 4, 3, 2, 1])
+        .mockReturnValueOnce([44, 43, 33, 32, 45, 30])
+        .mockReturnValueOnce([20, 25, 30, 10, 15, 40]);
+
+        Output.printLottos(lottos);
+        
+        expect(logSpy)
+        .toHaveBeenCalledWith('[1, 2, 3, 4, 5, 6]')
+        .toHaveBeenCalledWith('[10, 11, 12, 13, 14, 15]')
+        .toHaveBeenCalledWith('[1, 2, 3, 4, 5, 6]')
+        .toHaveBeenCalledWith('[30, 32, 33, 43, 44, 45]')
+        .toHaveBeenCalledWith('[10, 15, 20, 25, 30, 40]')
+    });
+});
---
+++ src/step1/Output.js
@@ -0,0 +1,5 @@
+export const Output ={
+    printLottos(lottos){
+        
+    }
+}
