6c659d76205e9bb2c80ab951201726f188268fff
jebiyeon02 <jhje5595@naver.com>
2026-03-05 17:38:41 +0900

    test: 중복되지 않는 무작위 정수 6개 생성 테스트

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -86,7 +86,7 @@ describe("유틸 함수 테스트", () => {
     expect(randomNumber >= 1 && randomNumber <= 45).toEqual(true);
   });
 
-  test("1~45 사이의 무작위 숫자 6개를 반환해야 한다", () => {
+  test("1~45 사이의 중복되지 않는 무작위 숫자 6개를 반환해야 한다", () => {
     // given
     const count = 6;
 
@@ -95,5 +95,6 @@ describe("유틸 함수 테스트", () => {
 
     // then 나중에 모킹 해놓기
     expect(randomNumbers.length).toEqual(count);
+    expect(randomNumbers.length === new Set(randomNumbers).size).toEqual(true);
   });
 });
