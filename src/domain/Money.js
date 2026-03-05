6f90267af5a1073e3537c4f2231210d7b5c3d9db
inaemin <inaemin@gmail.com>
2026-03-05 18:38:19 +0900

    🟢 feat: Money 도메인 구현
    Co-authored-by: less <lejin0906@snu.ac.kr>
    Co-authored-by: iftype <iftype@users.noreply.github.com>

---
+++ src/domain/Money.js
@@ -0,0 +1,21 @@
+class Money {
+  static UNIT = 1000;
+  #amount;
+
+  constructor(amount) {
+    this.#validate(Number(amount));
+    this.#amount = Number(amount);
+  }
+
+  #validate(amount) {
+    if (amount < Money.UNIT) {
+      throw new Error("[ERROR] 로또를 구매할 수 없습니다");
+    }
+  }
+
+  purchaseLotto() {
+    return Math.floor(this.#amount / Money.UNIT);
+  }
+}
+
+export default Money;
