b16024b03317180b9a9544944bdab8a8da73d4dc
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-05 11:11:35 +0900

    feat(Validator): 당첨번호 검증 기능 추가
    Co-authored-by: binggwa <qudrhks317@gmail.com>

--- src/Utils/Validator.js
+++ src/Utils/Validator.js
@@ -67,6 +67,31 @@ const Validator = {
 
     return command;
   },
+
+  validateWinningNumbers(input) {
+    if (!input.includes(","))
+      throw new Error("[ERROR] 쉼표를 기준으로 구분하지 않았습니다!");
+
+    const winningNumbers = input
+      .split(",")
+      .map((numStr) => Number(numStr.trim()));
+
+    const hasNaN = winningNumbers.some((n) => Number.isNaN(n));
+    if (hasNaN) {
+      throw new Error("[ERROR] 각 번호가 숫자가 아닙니다!");
+    }
+
+    if (winningNumbers.length !== 6) {
+      throw new Error(`[ERROR] 당첨 번호는 6개여야 합니다!`);
+    }
+
+    const isOutRange = winningNumbers.some((num) => num < 1 || num > 45);
+    if (isOutRange) {
+      throw new Error(`[ERROR] 당첨 번호는 1~45 범위여야 합니다!`);
+    }
+
+    return winningNumbers;
+  },
 };
 
 export default Validator;
