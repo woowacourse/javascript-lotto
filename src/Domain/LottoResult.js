4ae0f193d6c301df4ddcd7787b053339d4b1f987
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-05 21:18:59 +0900

    feat(LottoResult): 총 수익률 계산 기능 추가
    Co-Authored-By: binggwa <199538015+binggwa@users.noreply.github.com>

--- src/Domain/LottoResult.js
+++ src/Domain/LottoResult.js
@@ -1,16 +1,32 @@
+import { PRIZE } from '../Utils/Constants.js';
+
 class LottoResult {
   calculateWinningResult(lottos, winningNumbersObj) {
     const result = { FIRST: 0, SECOND: 0, THIRD: 0, FOURTH: 0, FIFTH: 0 };
     for (let lotto of lottos) {
       const match = lotto.countMatches(winningNumbersObj.winningNumbers());
       const hasBonus = lotto.hasBonus(winningNumbersObj.bonusNumber());
-      if (match === 6) result['FIRST']++;
-      else if (match === 5 && hasBonus) result['SECOND']++;
-      else if (match === 5) result['THIRD']++;
-      else if (match === 4) result['FOURTH']++;
-      else if (match === 3) result['FIFTH']++;
+      if (match === 6) result["FIRST"]++;
+      else if (match === 5 && hasBonus) result["SECOND"]++;
+      else if (match === 5) result["THIRD"]++;
+      else if (match === 4) result["FOURTH"]++;
+      else if (match === 3) result["FIFTH"]++;
     }
     return result;
+  }
+
+  calculateProfitRate(winningResult,purchasePrice) {
+    const totalPrize = this.calculateTotalPrize(winningResult);
+    const profitRate = (totalPrize / purchasePrice) * 100;
+
+    return Number(profitRate.toFixed(1));
+  }
+
+  calculateTotalPrize(winningResult) {
+    return Object.entries(winningResult).reduce(
+      (sum, [key, count]) => sum + PRIZE[key] * count,
+      0,
+    );
   }
 }
 
