b77f20d0282940e00b236e461644868fc21154cf
stardusty-lab <startdusty.lab@gmail.com>
2026-03-05 11:04:14 +0900

    test: 수익률 계산 기능 테스트

---
+++ __tests__/RateTest.js
@@ -0,0 +1,22 @@
+import Rate from "../src/Rate.js";
+
+describe("수익률 계산 검사 테스트", () => {
+  test("", () => {
+    expect().toBe();
+  });
+  test("등수와 금액을 받아서 수익률을 계산한다", () => {
+    const statistics = {
+      1: 1,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      0: 1,
+    };
+    const price = 2000;
+
+    const rate = new Rate(statistics, price);
+
+    expect(rate.getRate()).toBe(50);
+  });
+});
