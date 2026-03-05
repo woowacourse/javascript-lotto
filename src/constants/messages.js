381dfdae7bf20362aef2b21d4566bc9773ad3fa2
inaemin <inaemin@gmail.com>
2026-03-05 20:54:17 +0900

    chore: 입출력 메시지 상수 추가
    Co-authored-by: less <lejin0906@snu.ac.kr>
    Co-authored-by: iftype <iftype@users.noreply.github.com>

---
+++ src/constants/messages.js
@@ -0,0 +1,8 @@
+export const INFO = {
+  PURCHASE_AMOUNT: "> 구입금액을 입력해 주세요.",
+  WINNING_NUMBERS: "> 당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER: "> 보너스 번호를 입력해 주세요.",
+  ASK_RETRY: "> 다시 시작하시겠습니까? (y/n) ",
+};
+
+export const ERROR_MESSAGES = {};
