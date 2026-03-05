2e335a56e896f1c4a13c7e70338ee472e1d24e26
stardusty-lab <startdusty.lab@gmail.com>
2026-03-05 17:41:35 +0900

    feat: 입력할때 올바른 값이 들어올 때까지 반복해서 입력받는  기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import LottoGame from "./Model/LottoGame.js";
 import LottoList from "./Model/LottoList.js";
 import Rate from "./Model/Rate.js";
-import { parsingNumbers } from "./utils/parsing.js";
+import { parsingNumbers, stringToNumber } from "./utils/parsing.js";
 import Validator from "./Validator.js";
 import InputView from "./View/InputView.js";
 import OutputView from "./View/OutputView.js";
@@ -18,19 +18,50 @@ class App {
   }
 
   async run() {
-    const price = await this.#inputView.readPrice();
-    this.#validator.validatePrice(price);
+    let price;
+    let winningNumbers;
+    let bonusNumber;
+
+    while (true) {
+      try {
+        price = await this.#inputView.readPrice();
+        this.#validator.validatePrice(price);
+
+        break;
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
     const lottoList = new LottoList(price / 1000);
     this.#outputView.printAmount(price);
     this.#outputView.printLottos(lottoList.getLottoList());
 
-    const lottoNumbers = await this.#inputView.readLottoNumbers();
-    const winningNumbers = parsingNumbers(lottoNumbers);
-    this.#validator.validateLottoNumbers(winningNumbers);
+    while (true) {
+      try {
+        const lottoNumbers = await this.#inputView.readLottoNumbers();
+        winningNumbers = parsingNumbers(lottoNumbers);
+        this.#validator.validateLottoNumbers(winningNumbers);
 
-    const bonusNumber = await this.#inputView.readBonusNumber();
-    this.#validator.validateBonusNumber(bonusNumber);
+        break;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    while (true) {
+      try {
+        bonusNumber = await this.#inputView.readBonusNumber();
+        this.#validator.validateBonusNumber(
+          winningNumbers,
+          stringToNumber(bonusNumber),
+        );
+
+        break;
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
     const lottoGame = new LottoGame(winningNumbers, bonusNumber);
     const statistics = lottoGame.getStatistics(lottoList);
