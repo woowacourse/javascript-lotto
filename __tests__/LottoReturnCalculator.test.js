f82d7452d0f4f035368599fb7633403b6a9005d2
박건규 <geongyu09@gmail.com>
2026-03-05 20:40:51 +0900

    test(LottoReturnCalculator.test): calculateReturnRate 테스트 작성

--- __tests__/LottoReturnCalculator.test.js
+++ __tests__/LottoReturnCalculator.test.js
@@ -2,7 +2,7 @@ import LottoReturnCalculator from "../src/LottoReturnCalculator";
 
 describe("LottoReturnCalculator 클래스 유닛테스트", () => {
   describe("calculateReturnAmount", () => {
-    test("등수에 따른 수익률을 계산한다.", () => {
+    test("등수에 따른 수익 금액을 계산한다.", () => {
       // given
       const rank = { 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
 
@@ -11,6 +11,23 @@ describe("LottoReturnCalculator 클래스 유닛테스트", () => {
 
       // then
       expect(returnAmount).toBe(2000000000);
+    });
+  });
+
+  describe("calculateReturnRate", () => {
+    test("수익률을 계산한다.", () => {
+      // given
+      const returnAmount = 2000000000;
+      const purchaseAmount = 10000;
+
+      // when
+      const returnRate = LottoReturnCalculator.calculateReturnRate(
+        returnAmount,
+        purchaseAmount,
+      );
+
+      // then
+      expect(returnRate).toBe(20000000);
     });
   });
 });
