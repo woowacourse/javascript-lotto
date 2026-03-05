8aa9d3ab5cda8ccd7dccfb518009289c9993f85e
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-05 16:59:43 +0900

    feat(LottoMachine): 로또 발행 기능 추가
    Co-Authored-By: binggwa <199538015+binggwa@users.noreply.github.com>

---
+++ src/Domain/LottoMachine.js
@@ -0,0 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
+class LottoMachine {
+  issueLottos(purchasePrice) {
+    const ticketsCount = purchasePrice / 1000;
+
+    return Array.from({ length: ticketsCount }, () => this.#createLotto());
+  }
+
+  #createLotto() {
+    const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
+      1,
+      45,
+      6,
+    ).sort((a, b) => a - b);
+
+    return new Lotto(lottoNumbers);
+  }
+}
+
+export default LottoMachine;
