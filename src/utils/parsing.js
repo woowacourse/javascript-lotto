838dd595e9873dfff22a8c894eb542d9be61538b
stardusty-lab <startdusty.lab@gmail.com>
2026-03-05 17:40:05 +0900

    fix: 당첨 번호 파싱 로직 수정 및 보너스 번호 타입 변환 기능 추가

--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -1,3 +1,12 @@
 export const parsingNumbers = (numberString) => {
-  return numberString.split(",");
+  const numberStringArray = numberString.split(",");
+  const numberArray = numberStringArray.map((numberString) =>
+    parseInt(numberString, 10),
+  );
+  return numberArray;
+};
+
+export const stringToNumber = (string) => {
+  const number = parseInt(string, 10);
+  return number;
 };
