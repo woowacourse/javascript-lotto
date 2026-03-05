5d0d6724a86d3b1aebe351a09ff339eb9a7432bc
hjkim0905 <eax9952@gmail.com>
2026-03-05 09:53:02 +0900

    refactor: convert magic string into constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const LOTTO_INTO = Object.freeze({
+  LOTTO_NUMBER_COUNT: 6,
+  LOTTO_MAX_NUMBER: 45,
+  LOTTO_MIN_NUMBER: 1,
+});
+
+export const PRIZE_PER_RANK = Object.freeze({
+  1: 2000000000,
+  2: 30000000,
+  3: 1500000,
+  4: 50000,
+  5: 5000,
+});
