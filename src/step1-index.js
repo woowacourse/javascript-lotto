5b5bfeb9b5b32e361f81b583542c0dfe03823273
박건규 <geongyu09@gmail.com>
2026-03-05 20:48:17 +0900

    feat(OutputView): 로또 결과 출력 로직 구현

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,26 +1,43 @@
 import InputView from "./InputView.js";
+import OutputView from "./OutputView.js";
 import LottoStore from "./LottoStore.js";
 import WinningNumbersAndBonusNumberBuilder from "./WinningNumbersAndBonusNumberBuilder.js";
+import LottoRankCalculator from "./LottoRankCalculator.js";
+import LottoReturnCalculator from "./LottoReturnCalculator.js";
 
 async function main() {
   const inputView = new InputView();
+  const outputView = new OutputView();
 
-  const amount = await inputHandler(() => inputView.askAmount());
-  const lottos = LottoStore.purchaseLottos(amount);
+  const purchaseAmount = await inputHandler(() => inputView.askAmount());
+  const lottos = LottoStore.purchaseLottos(purchaseAmount);
 
   const builder = new WinningNumbersAndBonusNumberBuilder();
 
-  const winningNumbers = await inputHandler(() => {
-    inputView.askWinningNumbers();
+  await inputHandler(async () => {
+    const winningNumbers = await inputView.askWinningNumbers();
     builder.setWinningNumbers(winningNumbers);
   });
 
-  const bonusNumber = await inputHandler(() => {
-    inputView.askBonusNumber();
+  await inputHandler(async () => {
+    const bonusNumber = await inputView.askBonusNumber();
     builder.setBonusNumber(bonusNumber);
   });
 
-  builder.build();
+  const { winningNumbers, bonusNumber } = builder.build();
+
+  const rank = LottoRankCalculator.calculateLottoRanks(
+    lottos,
+    winningNumbers,
+    bonusNumber,
+  );
+  const returnAmount = LottoReturnCalculator.calculateReturnAmount(rank);
+  const returnRate = LottoReturnCalculator.calculateReturnRate(
+    returnAmount,
+    purchaseAmount,
+  );
+
+  outputView.printLottoResult(rank, returnRate);
 }
 
 async function inputHandler(inputFn) {
