b5cc3d6f368bb3c3e4d26053461b441cdc91f99b
kojesung <js95112345@gmail.com>
2026-03-05 20:39:07 +0900

    fix: generateRandomNumbers 함수 난수 생성 최솟값 지정 문제 해결

--- src/generateRandomNumbers.js
+++ src/generateRandomNumbers.js
@@ -3,8 +3,10 @@ import { LOTTO_INTO } from "./constants";
 export const generateRandomNumbers = () => {
   const randomSet = new Set();
   while (randomSet.size < LOTTO_INTO.LOTTO_NUMBER_COUNT) {
-    randomSet.add(Math.floor(Math.random() * LOTTO_INTO.LOTTO_MAX_NUMBER)) +
-      LOTTO_INTO.LOTTO_MIN_NUMBER;
+    randomSet.add(
+      Math.floor(Math.random() * LOTTO_INTO.LOTTO_MAX_NUMBER) +
+        LOTTO_INTO.LOTTO_MIN_NUMBER
+    );
   }
   return Array.from(randomSet);
 };
