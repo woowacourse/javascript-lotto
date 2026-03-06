3188218c9fd4dd3a115685e74b21109c3ffe4f37
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-06 15:31:14 +0900

    refactor: 헬퍼함수를 통해 ExceptionTest에서 반복되는 패턴 분리
    Co-authored-by: TH_97 <hood3392@gmail.com>

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -10,144 +10,91 @@ jest.mock('../src/step1/Utils.js', () => ({
 
 const logSpy = jest.spyOn(console, 'log');
 
-describe('구입 금액 입력예외 테스트', () => {
-    test('1000원 단위가 아닌 입력예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['600', '1000', '1,2,3,4,5,6', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
+const runException = async (inputs, expectedMessage) => {
+
+    inputs.forEach((input) => {
+        readLine.mockImplementationOnce(() => {
+                return input;
             });
-        });
+    });
 
-        const app = new App();
-        await app.run();
+    const app = new App();
+    await app.run();
 
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 1000원 단위만 입력 가능합니다.');
+    expect(logSpy).toHaveBeenCalledWith(expectedMessage);
+};
+
+describe('구입 금액 입력예외 테스트', () => {
+    beforeEach(() => {
+        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
+    });
+
+    test('1000원 단위가 아닌 입력예외 테스트', async () => {
+        runException(
+            ['600', '1000', '1,2,3,4,5,6', '10', 'n'],
+            '[ERROR] 1000원 단위만 입력 가능합니다.',
+        );
     });
 
     test('정수가 아닌 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['ㅁ', '1000', '1,2,3,4,5,6', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 숫자만 입력해 주세요.');
+        runException(
+            ['ㅁ', '1000', '1,2,3,4,5,6', '10', 'n'],
+            '[ERROR] 숫자만 입력해 주세요.',
+        );
     });
 });
 
 describe('당첨 번호 입력 예외 테스트', () =>{
     test('1 ~ 45가 아닌 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', '1,2,3,5,6,46', '1,2,3,4,5,6', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 1 ~ 45 이내 숫자만 입력 가능합니다.');
+        runException(
+            ['1000', '1,2,3,5,6,46', '1,2,3,4,5,6', '10', 'n'],
+            '[ERROR] 1 ~ 45 이내 숫자만 입력 가능합니다.',
+        );
     });
 
     test('겹치는 당첨 번호 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', '1,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 중복 당첨 번호 입력은 불가 합니다.');
+        runException(
+            ['1000', '1,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'],
+            '[ERROR] 중복 당첨 번호 입력은 불가 합니다.',
+        );
     });
 
     test('정수가 아닌 당첨 번호 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 당첨 번호는 숫자만 입력 가능합니다.');
+        runException(
+            ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'],
+            '[ERROR] 당첨 번호는 숫자만 입력 가능합니다.',
+        );
     });
 });
 
 describe('보너스 번호 입력 예외 테스트', () => {
     test('1 ~ 45가 아닌 보너스 번호 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', '1,2,3,4,5,6', '46', '10', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 보너스 번호는 1 ~ 45 이내 숫자여야 합니다.');
+        runException(
+            ['1000', '1,2,3,4,5,6', '46', '10', 'n'],
+            '[ERROR] 보너스 번호는 1 ~ 45 이내 숫자여야 합니다.',
+        );
     });
 
     test('당첨 번호와 동일한 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', '1,2,3,4,5,6', '3', '41', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+        runException(
+            ['1000', '1,2,3,4,5,6', '3', '41', 'n'],
+            '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+        );
     });
 
     test('정수가 아닌 보너스 번호 입력 예외 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', 'z', '21', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 보너스 번호는 숫자여야 합니다.');
+        runException(
+            ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', 'z', '21', 'n'],
+            '[ERROR] 보너스 번호는 숫자여야 합니다.',
+        );
     });
 });
 
 describe('재시작 입력 예외 테스트', () => {
     test('y 또는 n외 입력 테스트', async () => {
-        pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
-        const expectedAnswer = ['1000', '1,2,3,4,5,6', '7', 'zzz', 'n'];
-        expectedAnswer.forEach((answer) => {
-            readLine.mockImplementationOnce(() => {
-                return answer;
-            });
-        });
-
-        const app = new App();
-        await app.run();
-
-        expect(logSpy).toHaveBeenCalledWith('[ERROR] 다시시작 입력은 y 또는 n 만 입력 가능합니다.');
+        runException(
+            ['1000', '1,2,3,4,5,6', '7', 'zzz', 'n'],
+            '[ERROR] 다시시작 입력은 y 또는 n 만 입력 가능합니다.',
+        );
     });
 });
