e3084378bb3025d45a161fc88dcf064b006dcc7a
박건규 <geongyu09@gmail.com>
2026-03-06 16:13:08 +0900

    test(LottoRankCalculator.test): 통계 계산 시 등수별 개별 테스트 작성

--- __tests__/LottoRankCalculator.test.js
+++ __tests__/LottoRankCalculator.test.js
@@ -3,7 +3,7 @@ import Lotto from "../src/Lotto";
 
 describe("LottoRankCalculator 클래스 유닛 테스트", () => {
   describe("calculateLottoRank", () => {
-    test("당첨 번호와 일치하는 번호의 개수 + 보너스 번호의 포함 여부에 따라 올바른 등수를 반환해야 한다.", () => {
+    test("당첨 번호와 일치하는 번호의 개수가 6이면 1을 반환한다.", () => {
       // given
       const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
       const winningNumbers = [1, 2, 3, 4, 5, 6];
@@ -18,6 +18,74 @@ describe("LottoRankCalculator 클래스 유닛 테스트", () => {
 
       // then
       expect(rank).toBe(1);
+    });
+
+    test("당첨 번호와 일치하는 번호의 개수가 5이고 보너스 번호를 포함하면 2를 반환한다.", () => {
+      // given
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningNumbers = [1, 2, 3, 4, 5, 7];
+      const bonusNumber = 6;
+
+      // when
+      const rank = LottoRankCalculator.calculateLottoRank({
+        lotto,
+        winningNumbers,
+        bonusNumber,
+      });
+
+      // then
+      expect(rank).toBe(2);
+    });
+
+    test("당첨 번호와 일치하는 번호의 개수가 5이면 3을 반환한다.", () => {
+      // given
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningNumbers = [1, 2, 3, 4, 5, 7];
+      const bonusNumber = 8;
+
+      // when
+      const rank = LottoRankCalculator.calculateLottoRank({
+        lotto,
+        winningNumbers,
+        bonusNumber,
+      });
+
+      // then
+      expect(rank).toBe(3);
+    });
+
+    test("당첨 번호와 일치하는 번호의 개수가 4이면 4를 반환한다.", () => {
+      // given
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningNumbers = [1, 2, 3, 4, 7, 8];
+      const bonusNumber = 9;
+
+      // when
+      const rank = LottoRankCalculator.calculateLottoRank({
+        lotto,
+        winningNumbers,
+        bonusNumber,
+      });
+
+      // then
+      expect(rank).toBe(4);
+    });
+
+    test("당첨 번호와 일치하는 번호의 개수가 3이면 5를 반환한다.", () => {
+      // given
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningNumbers = [1, 2, 3, 7, 8, 9];
+      const bonusNumber = 10;
+
+      // when
+      const rank = LottoRankCalculator.calculateLottoRank({
+        lotto,
+        winningNumbers,
+        bonusNumber,
+      });
+
+      // then
+      expect(rank).toBe(5);
     });
   });
 
