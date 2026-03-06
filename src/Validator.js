bcfd454b40f94fd460981c32aa158151e3099110
stardusty-lab <startdusty.lab@gmail.com>
2026-03-06 13:57:47 +0900

    refactor: 에러 메세지를 상수로 변경

--- src/Validator.js
+++ src/Validator.js
@@ -1,26 +1,30 @@
+import { ERROR_MESSAGE } from "./constants/errorMessage";
+
 class Validator {
   validatePrice(price) {
     if (isNaN(price)) {
-      throw new Error("가격 예외 발생");
+      throw new Error(ERROR_MESSAGE.NOT_NUMBER);
     }
     if (price % 1000 !== 0 || price <= 0) {
-      throw new Error("가격 예외 발생");
+      throw new Error(ERROR_MESSAGE.INVALID_AMOUNT);
     }
   }
   validateLottoNumber(number) {
-    if (number < 1 || number > 45) throw new Error("번호 예외 발생");
-    if (!Number.isInteger(number)) throw new Error("번호 예외 발생");
-    if (isNaN(number)) throw new Error("번호 예외 발생");
+    if (number < 1 || number > 45)
+      throw new Error(ERROR_MESSAGE.INVALID_NUMBER_RANGE);
+    if (!Number.isInteger(number))
+      throw new Error(ERROR_MESSAGE.MUST_BE_INTEGER);
+    if (isNaN(number)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
   }
   validateLottoNumbers(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("번호 예외 발생");
+      throw new Error(ERROR_MESSAGE.INVALID_NUMBER_LENGTH);
     }
 
     const set = new Set(numbers);
 
     if (set.size !== numbers.length) {
-      throw new Error("번호 예외 발생");
+      throw new Error(ERROR_MESSAGE.MUST_BE_NOT_DUPLICATE);
     }
 
     numbers.forEach((number) => {
@@ -30,7 +34,8 @@ class Validator {
 
   validateBonusNumber(lottoNumbers, bonusNumber) {
     this.validateLottoNumber(bonusNumber);
-    if (lottoNumbers.includes(bonusNumber)) throw new Error("번호 예외 발생");
+    if (lottoNumbers.includes(bonusNumber))
+      throw new Error(ERROR_MESSAGE.MUST_BE_NOT_DUPLICATE_WITH_WINNINGNUMBER);
   }
 }
 
