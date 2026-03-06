9e91536f527e954a871e09e006ae6a5819cb31b0
TH_97 <hood3392@gmail.com>
2026-03-06 16:58:45 +0900

    refactor: readRetry에서 불필요한 무한루프 제거
    - Co-authored-by: Antoliny0919 <antoliny0919@gmail.com>

--- src/step1/Input.js
+++ src/step1/Input.js
@@ -1,13 +1,12 @@
-import { Lotto, WinningLotto } from './Lotto.js';
-import { readLine } from './Utils.js';
-import Validator from './Validator.js';
+import { Lotto, WinningLotto } from "./Lotto.js";
+import { readLine } from "./Utils.js";
+import Validator from "./Validator.js";
 
 const Input = {
-
   async reRead(func, funcArgs) {
     while (true) {
       try {
-        const answer = await func(funcArgs)
+        const answer = await func(funcArgs);
         return answer;
       } catch (err) {
         console.log(`[ERROR] ${err.message}`);
@@ -16,34 +15,28 @@ const Input = {
   },
 
   async readPurchaseAmount() {
-    const answer = await readLine('> 구입금액을 입력해 주세요. ');
+    const answer = await readLine("> 구입금액을 입력해 주세요. ");
     Validator.purchaseAmountValidator(answer);
     return answer;
   },
 
   async readWinningLottoNumber() {
-    const answer = await readLine('> 당첨 번호를 입력해 주세요. ');
-    const lotto = new Lotto(answer.split(','));
+    const answer = await readLine("> 당첨 번호를 입력해 주세요. ");
+    const lotto = new Lotto(answer.split(","));
     return lotto;
   },
 
   async readBonusNumber(winningLottoNumber) {
-    const bonusNumber = await readLine('> 보너스 번호를 입력해 주세요. ');
+    const bonusNumber = await readLine("> 보너스 번호를 입력해 주세요. ");
     const winningLotto = new WinningLotto(winningLottoNumber, bonusNumber);
     return winningLotto;
   },
 
   async readRetry() {
-    while (true) {
-      try {
-        const answer = await readLine('> 다시 시작하시겠습니까? (y/n) ');
-        Validator.retryValidator(answer);
-        return answer;
-      } catch (err) {
-        console.log(`[ERROR] ${err.message}`);
-      }
-    }
+    const answer = await readLine("> 다시 시작하시겠습니까? (y/n) ");
+    Validator.retryValidator(answer);
+    return answer;
   },
-}
+};
 
 export default Input;
