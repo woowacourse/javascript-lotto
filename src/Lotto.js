7966942e537c11568f83a76b4a984e1dba2dc5e9
hjkim0905 <eax9952@gmail.com>
2026-03-06 16:27:18 +0900

    feat: apply domain rules & validation

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,10 +1,20 @@
 class Lotto {
   constructor(lottoNumberList) {
-    this.numbers = lottoNumberList;
+    this.#validate(lottoNumberList);
+    this.numbers = [...lottoNumberList].sort((a, b) => a - b);
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6)
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (new Set(numbers).size !== 6)
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    if (numbers.some((n) => n < 1 || n > 45))
+      throw new Error("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
   }
 
   getNumbers() {
-    return this.numbers;
+    return [...this.numbers];
   }
 }
 
