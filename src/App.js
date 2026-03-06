f981ef49745a36c9c7d95e0fea163232c946abab
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-06 15:53:27 +0900

    feat(App): 로또 실행 로직 추가
    Co-Authored-By: binggwa <199538015+binggwa@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import InputConsole from "./Console/InputConsole.js";
+import OutputConsole from "./Console/OutputConsole.js";
+import LottoMachine from "./Domain/LottoMachine.js";
+import LuckyNumbers from "./Domain/LuckyNumbers.js";
+import LottoResult from "./Domain/LottoResult.js";
+import LuckyNumbers from "./Domain/LuckyNumbers.js";
 
 class App {
   async run() {
+    while(true){
     // 구입할 로또 금액 입력받기
     const purchasePrice = await InputConsole.readPurchasePrice();
 
+    // 발행된 로또 목록 출력하기
+    const lottos = new LottoMachine.issueLottos(purchasePrice);
+    OutputConsole.printLottoList(lottos);
+
+    // 당첨 번호 입력받기
     const winningNumbers = await InputConsole.readWinningNumbers();
+
+    // 보너스 번호 입력받기
+    const bonusNumber = await InputConsole.readBonusNumber(winningNumbers);
+
+    // 당첨 통계 출력하기
+    const luckyNumbers = new LuckyNumbers(winningNumbers, bonusNumber);
+    const lottoResult = new LottoResult();
+    const winningResult = lottoResult.calculateWinningResult(lottos, luckyNumbers);
+    OutputConsole.printMatchResult(winningResult);
+
+    // 총 수익률 출력하기
+    const profitRate = lottoResult.calculateProfitRate(winningResult,purchasePrice);
+    OutputConsole.printProfitRate(profitRate);
+
+    // 재시작 로직 출력
+    const restartCommand = await InputConsole.readRestart();
+    if(restartCommand==="n") break;
+    }
   }
 }
 
