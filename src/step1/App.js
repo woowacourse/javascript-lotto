0461678575fedd06bd3a5d7be8bac6163814a0fd
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-06 09:56:07 +0900

    refactor: 입력로직 분리
    Co-authored-by: TH_97 <hood3392@gmail.com>

--- src/step1/App.js
+++ src/step1/App.js
@@ -1,74 +1,25 @@
-import { readLine, read } from './Utils.js';
-import Validator from './Validator.js';
-import { Lotto, WinningLotto } from './Lotto.js';
+import { read } from './Utils.js';
+import Input from './Input.js';
 import { LottoMachine } from './LottoMachine.js';
 import { Output } from './Output.js';
 
 class App {
     async run() {
         while(true) {
-            const amount = await this.amount();
+            const amount = await Input.reRead(Input.readPurchaseAmount);
             const lottoMachine = new LottoMachine(amount)
             Output.printPurchaseLottoCount(lottoMachine.purchaseCount);
             Output.printLottos(lottoMachine.lottos);
-            const winningLottoNumber = (await this.winningLotto()).getLottoNumber();
-            const winningLotto = await this.bonusNumber(winningLottoNumber);
+            const winningLottoNumber = (await Input.reRead(Input.readWinningLottoNumber)).getLottoNumber();
+            const winningLotto = await Input.reRead(Input.readBonusNumber, winningLottoNumber);
             lottoMachine.calculateMatchResult(
                 winningLotto.getLottoNumber(), winningLotto.getBonusNumber()
             );
             Output.printResult(lottoMachine);
-            const restart = await this.restart();
+            const restart = await this.reRead(Input.readRetry);
             if (restart === 'n') {
                 read.close();
                 break;
-            }
-        }
-    }
-
-    async amount(){
-        while (true) {
-            try{
-                const answer = await readLine('구입금액을 입력해 주세요.');
-                Validator.purchaseAmountValidator(answer);
-                return answer;
-            } catch(err){
-                console.log(`[ERROR] ${err.message}`);
-            }
-        }
-    }
-
-    async winningLotto() {
-        while (true) {
-            try{
-                const answer = await readLine('당첨 번호를 입력해 주세요.');
-                const lotto = new Lotto(answer.split(','));
-                return lotto;
-            } catch(err){
-                console.log(`[ERROR] ${err.message}`);
-            }
-        }
-    }
-
-    async bonusNumber(winningLottoNumber) {
-        while (true) {
-            try{
-                const bonusNumber = await readLine('보너스 번호를 입력해 주세요.');
-                const winningLotto = new WinningLotto(winningLottoNumber, bonusNumber);
-                return winningLotto;
-            } catch(err){
-                console.log(`[ERROR] ${err.message}`);
-            }
-        }
-    }
-
-    async restart() {
-        while (true) {
-            try {
-                const answer = await readLine('다시 시작하시겠습니까? (y/n)');
-                Validator.purchaseAmountValidator(answer);
-                return answer;
-            } catch (err) {
-                console.log(`[ERROR] ${err.message}`);
             }
         }
     }
---
+++ src/step1/Input.js
@@ -0,0 +1,48 @@
+import { Lotto, WinningLotto } from './Lotto.js';
+import Validator from './Validator.js';
+
+const Input = {
+
+    async reRead(func, funcArgs) {
+        while (true) {
+            try {
+                const answer = await func(funcArgs)
+                return answer;
+            } catch (err) {
+                console.log(`[ERROR] ${err.message}`);
+            }
+        }
+    },
+
+    async readPurchaseAmount() {
+        const answer = await readLine('구입금액을 입력해 주세요.');
+        Validator.purchaseAmountValidator(answer);
+        return answer;
+    },
+
+    async readWinningLottoNumber() {
+        const answer = await readLine('당첨 번호를 입력해 주세요.');
+        const lotto = new Lotto(answer.split(','));
+        return lotto;
+    },
+
+    async readBonusNumber(winningLottoNumber) {
+        const bonusNumber = await readLine('보너스 번호를 입력해 주세요.');
+        const winningLotto = new WinningLotto(winningLottoNumber, bonusNumber);
+        return winningLotto;
+    },
+
+    async readRetry() {
+        while (true) {
+            try {
+                const answer = await readLine('다시 시작하시겠습니까? (y/n)');
+                Validator.retryValidator(answer);
+                return answer;
+            } catch (err) {
+                console.log(`[ERROR] ${err.message}`);
+            }
+        }
+    },
+}
+
+export default Input;
