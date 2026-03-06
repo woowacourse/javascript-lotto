0324a802b7c61131a2f236c3901a433bcbfa11ff
stardusty-lab <startdusty.lab@gmail.com>
2026-03-06 13:54:09 +0900

    feat: 에러 메세지 상수화

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,14 @@
+export const ERROR_MESSAGE = Object.freeze({
+  NOT_NUMBER: "숫자가 아닙니다.",
+  INVALID_AMOUNT: "1000원 단위가 아닙니다.",
+
+  INVALID_NUMBER_RANGE: "모든 번호는 1부터 45사이의 숫자여야 합니다.",
+  MUST_BE_INTEGER: "모든 번호는 정수여야 합니다.",
+
+  INVALID_NUMBER_LENGTH:
+    "로또 번호는 6개의 숫자가 존재하고 콤마로 구분되어야 합니다.",
+  MUST_BE_NOT_DUPLICATE: "로또 번호는 중복될 수 없습니다.",
+
+  MUST_BE_NOT_DUPLICATE_WITH_WINNINGNUMBER:
+    "보너스 번호는 당첨 번호와 중복될 수 없습니다.",
+});
