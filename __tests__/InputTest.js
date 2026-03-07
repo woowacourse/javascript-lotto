9663d5475671805437a53c7ca40c9d31812049f8
hjkim0905 <eax9952@gmail.com>
2026-03-07 23:28:40 +0900

    test: change test description name

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -12,31 +12,31 @@ jest.mock("../src/view/input.js", () => ({
 }));
 
 describe("입력 처리 테스트", () => {
-  test("구입 금액 입력 테스트", async () => {
+  test("구입 금액 입력 테스트 (빈 문자열 입력 시 -> 에러)", async () => {
     Console.input.mockResolvedValue("");
     await expect(inputPurchaseAmount()).rejects.toThrow("[ERROR]");
   });
-  test("구입 금액 입력 테스트", async () => {
+  test("구입 금액 입력 테스트 (8000 입력 시 -> 정상)", async () => {
     Console.input.mockResolvedValue("8000");
     expect(await inputPurchaseAmount()).toBe("8000");
   });
-  test("당첨 번호 입력 테스트", async () => {
+  test("당첨 번호 입력 테스트 (1,2,3,4,5,6 입력시 -> 정상)", async () => {
     Console.input.mockResolvedValue("1,2,3,4,5,6");
     expect(await inputWinningNumbers()).toBe("1,2,3,4,5,6");
   });
-  test("당첨 번호 입력 테스트", async () => {
+  test("당첨 번호 입력 테스트 (빈 문자열 입력 시 -> 에러)", async () => {
     Console.input.mockResolvedValue("");
     await expect(inputWinningNumbers()).rejects.toThrow("[ERROR]");
   });
-  test("보너스 번호 입력 테스트", async () => {
+  test("보너스 번호 입력 테스트 (7 입력 시 -> 정상)", async () => {
     Console.input.mockResolvedValue("7");
     expect(await inputBonusNumber()).toBe("7");
   });
-  test("보너스 번호 입력 테스트", async () => {
+  test("보너스 번호 입력 테스트 (빈 문자열 입력 시 -> 에러", async () => {
     Console.input.mockResolvedValue("");
     await expect(inputBonusNumber()).rejects.toThrow("[ERROR]");
   });
-  test("재시작/종료 입력 테스트", async () => {
+  test("재시작/종료 입력 테스트 (y 입력 시 -> 정상)", async () => {
     Console.input.mockResolvedValue("y");
     expect(await inputYesNo()).toBe("y");
   });
