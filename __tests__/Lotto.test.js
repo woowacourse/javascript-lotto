3827e43d211496916b15945028339e680f389f49
gustn99 <azerosong@gmail.com>
2026-03-08 10:36:26 +0900

    test(Lotto.test): includes 메서드 테스트 설명 수정

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,9 +1,9 @@
-import Lotto from "../src/Lotto";
-import LottoNumber from "../src/LottoNumber";
+import Lotto from '../src/Lotto';
+import LottoNumber from '../src/LottoNumber';
 
-describe("Lotto 클래스 유닛 테스트", () => {
-  describe("getNumbers", () => {
-    test("로또 객체의 숫자를 반환한다.", () => {
+describe('Lotto 클래스 유닛 테스트', () => {
+  describe('getNumbers', () => {
+    test('로또 객체의 숫자를 반환한다.', () => {
       // given
       const numbers = [1, 2, 3, 4, 5, 6];
 
@@ -22,8 +22,8 @@ describe("Lotto 클래스 유닛 테스트", () => {
     });
   });
 
-  describe("validateLotto", () => {
-    test("당첨 번호는 1부터 45까지의 정수여야한다.", () => {
+  describe('validateLotto', () => {
+    test('당첨 번호는 1부터 45까지의 정수여야한다.', () => {
       // given
       const numbers = [1, 2, 3, 4, 5, 6];
 
@@ -31,7 +31,7 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(() => new Lotto(numbers)).not.toThrow();
     });
 
-    test("로또 번호가 정수가 아니면 에러를 반환한다.", () => {
+    test('로또 번호가 정수가 아니면 에러를 반환한다.', () => {
       // given
       const numbers = [1, 2, 3, 4, 5, 6.1];
 
@@ -39,7 +39,7 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(() => new Lotto(numbers)).toThrow();
     });
 
-    test("당첨 번호가 1보다 작으면 에러를 반환한다.", () => {
+    test('당첨 번호가 1보다 작으면 에러를 반환한다.', () => {
       // given
       const numbers = [-1, 2, 3, 4, 5, 6];
 
@@ -47,7 +47,7 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(() => new Lotto(numbers)).toThrow();
     });
 
-    test("당첨 번호가 45보다 크면 에러를 반환한다.", () => {
+    test('당첨 번호가 45보다 크면 에러를 반환한다.', () => {
       // given
       const numbers = [1, 2, 3, 4, 5, 46];
 
@@ -55,7 +55,7 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(() => new Lotto(numbers)).toThrow();
     });
 
-    test("당첨 번호가 중복되면 에러를 반환한다.", () => {
+    test('당첨 번호가 중복되면 에러를 반환한다.', () => {
       // given
       const numbers = [1, 2, 3, 4, 5, 5];
 
@@ -64,8 +64,8 @@ describe("Lotto 클래스 유닛 테스트", () => {
     });
   });
 
-  describe("includes", () => {
-    test("전달받은 값이 로또 번호 안에 포함되면 true를 반환한다.", () => {
+  describe('includes', () => {
+    test('전달받은 값이 로또 번호 안에 포함되면 true를 반환한다.', () => {
       // given
       const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
       const bonusNumber = new LottoNumber(1);
@@ -77,7 +77,7 @@ describe("Lotto 클래스 유닛 테스트", () => {
       expect(result).toBe(true);
     });
 
-    test("전달받은 값이 로또 번호 안에 포함되면 false를 반환한다.", () => {
+    test('전달받은 값이 로또 번호 안에 포함되지 않으면 false를 반환한다.', () => {
       // given
       const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
       const bonusNumber = new LottoNumber(7);
@@ -90,8 +90,8 @@ describe("Lotto 클래스 유닛 테스트", () => {
     });
   });
 
-  describe("matchCount", () => {
-    test("로또 번호와 당첨 번호 사이에 일치하는 번호 개수를 반환한다.", () => {
+  describe('matchCount', () => {
+    test('로또 번호와 당첨 번호 사이에 일치하는 번호 개수를 반환한다.', () => {
       // given
       const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
       const winningNumbers = [
