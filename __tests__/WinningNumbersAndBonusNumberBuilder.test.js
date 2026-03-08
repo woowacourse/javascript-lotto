fee4eec034a7b1088630d3158abb6f14611993c6
gustn99 <azerosong@gmail.com>
2026-03-08 11:17:07 +0900

    test(WinningNumbersAndBonusNumberBuilder.test): 당첨 번호 자체(integer, range)에 대한 검증 테스트 추가

--- __tests__/WinningNumbersAndBonusNumberBuilder.test.js
+++ __tests__/WinningNumbersAndBonusNumberBuilder.test.js
@@ -12,6 +12,36 @@ describe('WinningNumbersAndBonusNumberBuilder 클래스 유닛 테스트', () =>
       ).not.toThrow();
     });
 
+    // test('당첨 번호가 정수가 아니면 에러를 반환한다.', () => {
+    //   // given
+    //   const builder = new WinningNumbersAndBonusNumberBuilder();
+    //
+    //   // when & then
+    //   expect(() =>
+    //     builder.validateWinningNumbers([1, 2, 3, 4, 5, 6.1]),
+    //   ).toThrow('당첨 번호는 정수여야 합니다.');
+    // });
+    //
+    // test('당첨 번호가 1보다 작으면 에러를 반환한다.', () => {
+    //   // given
+    //   const builder = new WinningNumbersAndBonusNumberBuilder('당첨 번호는 1부터 45 사이여야 합니다.');
+    //
+    //   // when & then
+    //   expect(() =>
+    //     builder.validateWinningNumbers([-1, 2, 3, 4, 5, 6]),
+    //   ).toThrow('당첨 번호는 1부터 45 사이여야 합니다.');
+    // });
+    //
+    // test('당첨 번호가 45보다 크면 에러를 반환한다.', () => {
+    //   // given
+    //   const builder = new WinningNumbersAndBonusNumberBuilder();
+    //
+    //   // when & then
+    //   expect(() =>
+    //     builder.validateWinningNumbers([1, 2, 3, 4, 5, 46]),
+    //   ).toThrow('당첨 번호는 1부터 45 사이여야 합니다.');
+    // });
+
     test('당첨 번호가 6개 미만이면 에러를 반환한다.', () => {
       // given
       const builder = new WinningNumbersAndBonusNumberBuilder();
