a03c8928294bafbd9c64af7ac9c1a83bf856ef08
gustn99 <azerosong@gmail.com>
2026-03-08 11:09:49 +0900

    test(LottoNumber.test): 구체적인 에러 메시지 검증 로직 추가

--- __tests__/LottoNumber.test.js
+++ __tests__/LottoNumber.test.js
@@ -1,8 +1,8 @@
-import LottoNumber from "../src/LottoNumber";
+import LottoNumber from '../src/LottoNumber';
 
-describe("LottoNumber 클래스 유닛테스트", () => {
-  describe("validateNumber", () => {
-    test("로또 번호는 1부터 45까지의 정수여야한다.", () => {
+describe('LottoNumber 클래스 유닛테스트', () => {
+  describe('validateNumber', () => {
+    test('로또 번호는 1부터 45까지의 정수여야한다.', () => {
       // given
       const number = 1;
 
@@ -10,28 +10,28 @@ describe("LottoNumber 클래스 유닛테스트", () => {
       expect(() => new LottoNumber(number)).not.toThrow();
     });
 
-    test("로또 번호가 정수가 아니면 에러를 반환한다.", () => {
+    test('로또 번호가 정수가 아니면 에러를 반환한다.', () => {
       // given
       const number = 1.5;
 
       // when & then
-      expect(() => new LottoNumber(number)).toThrow();
+      expect(() => new LottoNumber(number)).toThrow('로또 번호는 정수여야 합니다.');
     });
 
-    test("로또 번호가 1보다 작으면 에러를 반환한다.", () => {
+    test('로또 번호가 1보다 작으면 에러를 반환한다.', () => {
       // given
       const number = -1;
 
       // when & then
-      expect(() => new LottoNumber(number)).toThrow();
+      expect(() => new LottoNumber(number)).toThrow('로또 번호는 1부터 45 사이여야 합니다.');
     });
 
-    test("로또 번호가 45보다 크면 에러를 반환한다.", () => {
+    test('로또 번호가 45보다 크면 에러를 반환한다.', () => {
       // given
       const number = 46;
 
       // when & then
-      expect(() => new LottoNumber(number)).toThrow();
+      expect(() => new LottoNumber(number)).toThrow('로또 번호는 1부터 45 사이여야 합니다.');
     });
   });
 });
