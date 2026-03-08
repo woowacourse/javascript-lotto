e7a7701af55407a7fd7e1b629295c29832d1c7a8
JuHyeong424 <wkdwngud963@gmail.com>
2026-03-08 20:31:57 +0900

    feat: MyLotto 클래스 객체 생성

---
+++ src/domain/MyLotto.js
@@ -0,0 +1,19 @@
+class MyLotto {
+  #money;
+  #randomLotto;
+
+  constructor(money, randomLotto) {
+    this.#money = money;
+    this.#randomLotto = randomLotto;
+  }
+
+  getMoney() {
+    return this.#money;
+  }
+
+  getRandomLotto() {
+    return this.#randomLotto;
+  }
+}
+
+export default MyLotto;
