2e0b345315904a6d7921c37a49ce8c98c551b522
gustn99 <azerosong@gmail.com>
2026-03-08 10:57:20 +0900

    refactor(WinningNumbersAndBonusNumberBuilder): validateBonusNumber 메서드 테스트 given/when/then 구조 변경

--- __tests__/WinningNumbersAndBonusNumberBuilder.test.js
+++ __tests__/WinningNumbersAndBonusNumberBuilder.test.js
@@ -47,22 +47,18 @@ describe('WinningNumbersAndBonusNumberBuilder 클래스 유닛 테스트', () =>
     test('보너스 번호는 당첨 번호에 포함되지 않은 1부터 45까지의 정수여야한다.', () => {
       // given
       const builder = new WinningNumbersAndBonusNumberBuilder();
-
-      // when
       builder.setWinningNumbers([1, 2, 3, 4, 5, 6]);
 
-      // then
+      // when & then
       expect(() => builder.validateBonusNumber(7)).not.toThrow();
     });
 
     test('당첨 번호에 이미 포함된 번호면 에러를 반환한다.', () => {
       // given
       const builder = new WinningNumbersAndBonusNumberBuilder();
-
-      // when
       builder.setWinningNumbers([1, 2, 3, 4, 5, 6]);
 
-      // then
+      // when & then
       expect(() => builder.validateBonusNumber(6)).toThrow();
     });
   });
