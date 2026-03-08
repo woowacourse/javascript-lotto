c7160eea8cc2f843b267b31078ed6cc349de53d9
gustn99 <azerosong@gmail.com>
2026-03-08 11:08:11 +0900

    test(InputView.test): 구체적인 에러 메시지 검증 로직 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,5 @@
-import readline from "readline/promises";
-import ERROR_MESSAGE from "./constants/errorMessage.js";
+import readline from 'readline/promises';
+import ERROR_MESSAGE from './constants/errorMessage.js';
 
 class InputView {
   async getUserInput(message) {
@@ -14,28 +14,28 @@ class InputView {
   }
 
   async askAmount() {
-    const MESSAGE = "> 구입금액을 입력해 주세요. ";
+    const MESSAGE = '> 구입금액을 입력해 주세요. ';
     const input = await this.getUserInput(MESSAGE);
     this.validateAmount(input);
     return Number(input);
   }
 
   async askWinningNumbers() {
-    const MESSAGE = "> 당첨 번호를 입력해 주세요. ";
+    const MESSAGE = '> 당첨 번호를 입력해 주세요. ';
     const input = await this.getUserInput(MESSAGE);
     this.validateWinningNumbers(input);
-    return input.split(",").map(Number);
+    return input.split(',').map(Number);
   }
 
   async askBonusNumber() {
-    const MESSAGE = "> 보너스 번호를 입력해 주세요. ";
+    const MESSAGE = '> 보너스 번호를 입력해 주세요. ';
     const input = await this.getUserInput(MESSAGE);
     this.validateBonusNumber(input);
     return Number(input);
   }
 
   async askRetry() {
-    const MESSAGE = "> 다시 시작하시겠습니까? (y/n) ";
+    const MESSAGE = '> 다시 시작하시겠습니까? (y/n) ';
     const input = await this.getUserInput(MESSAGE);
     this.validateRetry(input);
     return input;
@@ -49,9 +49,9 @@ class InputView {
   }
 
   validateWinningNumbers(userInput) {
-    const winningNumbers = userInput.split(",").map(Number);
+    const winningNumbers = userInput.split(',').map(Number);
     if (winningNumbers.some((number) => Number.isNaN(number))) {
-      throw new Error(ERROR_MESSAGE.WINNING_NUMBER.NUMBER);
+      throw new Error(ERROR_MESSAGE.WINNING_NUMBERS.NUMBER);
     }
   }
 
@@ -63,7 +63,7 @@ class InputView {
   }
 
   validateRetry(userInput) {
-    if (userInput !== "y" && userInput !== "n") {
+    if (userInput !== 'y' && userInput !== 'n') {
       throw new Error(ERROR_MESSAGE.RETRY.INPUT);
     }
   }
