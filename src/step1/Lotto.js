2120ebeb5296f32398969b5d49b9dcf9b9068437
TH_97 <hood3392@gmail.com>
2026-03-08 18:06:27 +0900

    fix: 로또 번호 범위 검증 버그 수정

--- src/step1/Lotto.js
+++ src/step1/Lotto.js
@@ -1,55 +1,56 @@
 export class Lotto {
-	#numbers;
+  #numbers;
 
-	constructor(numbers) {
-		this.#validate(numbers);
-		this.#numbers = numbers;
-	}
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
 
-	#validate(numbers) {
-		if (new Set(numbers).size !== 6) {
-			throw new Error('중복 당첨 번호 입력은 불가 합니다.');
-		}
-		numbers.forEach(number => {
-			number = Number(number);
-			if (!Number.isInteger(number)) {
-				throw new Error('당첨 번호는 숫자만 입력 가능합니다.');
-			}
-			if (number > 45 || number < 0) {
-				throw new Error('1 ~ 45 이내 숫자만 입력 가능합니다.');
-			}
-		});
-	}
+  #validate(numbers) {
+    if (new Set(numbers).size !== 6) {
+      throw new Error("중복 당첨 번호 입력은 불가 합니다.");
+    }
+    numbers.forEach((number) => {
+      number = Number(number);
+      if (!Number.isInteger(number)) {
+        throw new Error("당첨 번호는 숫자만 입력 가능합니다.");
+      }
+      const MIN = 1;
+      const MAX = 45;
+      if (number < MIN || number > MAX) {
+        throw new Error("1 ~ 45 이내 숫자만 입력 가능합니다.");
+      }
+    });
+  }
 
-	getLottoNumber() {
-		return [...this.#numbers];
-	}
+  getLottoNumber() {
+    return [...this.#numbers];
+  }
 }
 
-
 export class WinningLotto extends Lotto {
-	#bonusNumber;
+  #bonusNumber;
 
-	constructor(numbers, bonusNumber) {
-		super(numbers.map((lottoNumber) => Number(lottoNumber)));
-		this.#validateBonusNumber(bonusNumber);
-		this.#bonusNumber = Number(bonusNumber)
-	}
+  constructor(numbers, bonusNumber) {
+    super(numbers.map((lottoNumber) => Number(lottoNumber)));
+    this.#validateBonusNumber(bonusNumber);
+    this.#bonusNumber = Number(bonusNumber);
+  }
 
-	#validateBonusNumber(bonusNumber) {
-		bonusNumber = Number(bonusNumber);
-		if (!Number.isInteger(bonusNumber)) {
-			throw new Error('보너스 번호는 숫자여야 합니다.');
-		}
-		if (bonusNumber > 45 || bonusNumber < 0) {
-			throw new Error('보너스 번호는 1 ~ 45 이내 숫자여야 합니다.');
-		}
-		if (new Set([...this.getLottoNumber(), bonusNumber]).size !== 7) {
-			throw new Error('보너스 번호는 당첨 번호와 중복될 수 없습니다.');
-		}
-	}
+  #validateBonusNumber(bonusNumber) {
+    bonusNumber = Number(bonusNumber);
+    if (!Number.isInteger(bonusNumber)) {
+      throw new Error("보너스 번호는 숫자여야 합니다.");
+    }
+    if (bonusNumber > 45 || bonusNumber < 0) {
+      throw new Error("보너스 번호는 1 ~ 45 이내 숫자여야 합니다.");
+    }
+    if (new Set([...this.getLottoNumber(), bonusNumber]).size !== 7) {
+      throw new Error("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+  }
 
-	getBonusNumber() {
-		return this.#bonusNumber;
-	}
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
 }
