d780dda2f3429a4fd143451c45b652cb61e9ff32
TH_97 <hood3392@gmail.com>
2026-03-09 16:09:22 +0900

    refactor: run() 메서드 가독성 향상을 위해 메서드로 분리

--- src/step1/App.js
+++ src/step1/App.js
@@ -1,27 +1,55 @@
-import { read } from './Utils.js';
-import Input from './Input.js';
-import { LottoMachine } from './LottoMachine.js';
-import { Output } from './Output.js';
+import { read } from "./Utils.js";
+import Input from "./Input.js";
+import { LottoMachine } from "./LottoMachine.js";
+import { Output } from "./Output.js";
 
 class App {
   async run() {
     while (true) {
       const amount = await Input.reRead(Input.readPurchaseAmount);
+
       const lottoMachine = new LottoMachine(amount);
-      Output.printPurchaseLottoCount(lottoMachine.getPurchaseCount());
-      Output.printLottos(lottoMachine.getLottos());
-      const winningLottoNumber = (await Input.reRead(Input.readWinningLottoNumber)).getLottoNumber();
-      const winningLotto = await Input.reRead(Input.readBonusNumber, winningLottoNumber);
-      lottoMachine.calculateMatchResult(
-        winningLotto.getLottoNumber(), winningLotto.getBonusNumber()
-      );
-      Output.printResult(lottoMachine.getMatchResult(), lottoMachine.getRateOfReturn());
+
+      this.showPurchaseLotto(lottoMachine);
+
+      const winningLotto = await this.getWinningLotto();
+
+      this.showMatchResult(lottoMachine, winningLotto);
+
       const restart = await Input.reRead(Input.readRetry);
-      if (restart === 'n') {
+      if (restart === "n") {
         read.close();
         break;
       }
     }
+  }
+
+  async showPurchaseLotto(lottoMachine) {
+    Output.printPurchaseLottoCount(lottoMachine.getPurchaseCount());
+    Output.printLottos(lottoMachine.getLottos());
+  }
+
+  showMatchResult(lottoMachine, winningLotto) {
+    lottoMachine.calculateMatchResult(
+      winningLotto.getLottoNumber(),
+      winningLotto.getBonusNumber(),
+    );
+    Output.printResult(
+      lottoMachine.getMatchResult(),
+      lottoMachine.getRateOfReturn(),
+    );
+  }
+
+  async getWinningLotto() {
+    const winningLottoNumber = (
+      await Input.reRead(Input.readWinningLottoNumber)
+    ).getLottoNumber();
+    const winningLotto = await Input.reRead(
+      Input.readBonusNumber,
+      winningLottoNumber,
+    );
+
+    return winningLotto;
   }
 }
 
