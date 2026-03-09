7d842d8aa1cc3a1a1e92f1c6c2d2926be0b8f5ff
binggwa <qudrhks317@gmail.com>
2026-03-09 18:24:31 +0900

    test(ValidationTest) : 리팩토링에 따른 테스트코드 변경

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,29 +1,25 @@
-import Validator from "../src/Utils/Validator.js";
+import LottoMachine from "../src/Domain/LottoMachine.js";
+import Lotto from "../src/Domain/Lotto.js";
+import LuckyNumbers from "../src/Domain/LuckyNumbers.js";
 
-describe("Validator 테스트", () => {
-  test.each([
-    { input: "1000 ", expected: "1000" },
-    { input: "7500", expected: "7500" },
-  ])("구입 금액 유효성 검증 테스트: ", ({ input, expected }) => {
-    return expect(Validator.validatePurchasePrice(input)).toBe(expected);
-  });
-
+describe("Validation 테스트", () => {
   test.each([
     { input: "abcde", message: "숫자 아님" },
     { input: "999", message: "1000원 이상 체크" },
     { input: "", message: "빈 문자열" },
-    { input: "    ", message: "공백만 있는 경우" },
   ])("구입 금액 예외 테스트: ", ({ input }) => {
-    return expect(() => Validator.validatePurchasePrice(input)).toThrow(
+    const random = () => [1, 2, 3, 4, 5, 6]
+    return expect(() => LottoMachine.issueLottos(input, random)).toThrow(
       /^\[ERROR\]/,
     );
   });
 
   test.each([
     { input: "1,2,3,4,5,6", expected: [1, 2, 3, 4, 5, 6] },
-    { input: "40,41,42,43,44,45", expected: [40, 41, 42, 43, 44, 45] },
   ])("당첨 번호 정상 테스트: $input", ({ input, expected }) => {
-    return expect(Validator.validateWinningNumbers(input)).toEqual(expected);
+    const numbers = input.split(",");
+    const lotto = new Lotto(numbers);
+    return expect(lotto.getNumbers()).toEqual(expected);
   });
 
   test.each([
@@ -32,16 +28,10 @@ describe("Validator 테스트", () => {
     { input: "1,2,3,4,5,46", message: "당첨 번호 상한 범위 이탈" },
     { input: "a,b,c,d,e,f", message: "당첨 번호 숫자 아님" },
   ])('당첨 번호 예외 테스트: "$input", $message', ({ input }) => {
-    return expect(() => Validator.validateWinningNumbers(input)).toThrow(
+    const numbers = input.split(",");
+    return expect(() => new Lotto(numbers)).toThrow(
       /^\[ERROR\]/,
     );
-  });
-
-  test.each([
-    { input: '1', winningNumbers: [2, 3, 4, 5, 6, 7], expected: 1 },
-    { input: '45', winningNumbers: [1, 2, 3, 4, 5, 6], expected: 45 },
-  ])("보너스 번호 정상 테스트: $input", ({ input, winningNumbers, expected }) => {
-    return expect(Validator.validateBonusNumber(input, winningNumbers)).toEqual(expected);
   });
 
   test.each([
@@ -50,7 +40,7 @@ describe("Validator 테스트", () => {
     { input: '46', winningNumbers: [1, 2, 3, 4, 5, 6], message: '보너스 번호 상한 이탈' },
     { input: '1', winningNumbers: [1, 2, 3, 4, 5, 6], message: '당첨 번호와 중복' },
   ])('보너스 번호 예외 테스트: "$input", $message', ({ input, winningNumbers }) => {
-    return expect(() => Validator.validateBonusNumber(input, winningNumbers)).toThrow(
+    return expect(() => new LuckyNumbers(winningNumbers, input)).toThrow(
       /^\[ERROR\]/,
     );
   });
