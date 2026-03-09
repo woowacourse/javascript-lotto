bb10bbad179423d96cf442a9d5771602783580ba
TH_97 <hood3392@gmail.com>
2026-03-09 16:25:13 +0900

    fix: pickNumberInRange 메서드 수정
    - range 값이 6 초과 시 무한 루프 수정
    - max값을 포함하게 수정

--- src/step1/Utils.js
+++ src/step1/Utils.js
@@ -1,22 +1,22 @@
-import readline from 'readline';
+import readline from "readline";
 
 /**
- * 
+ *
  * 두 수를 포함하는 두 수 사이의 정수 난수 배열 반환.
- * @param {Number} min 
- * @param {Number} max 
- * @param {Number} range 
+ * @param {Number} min
+ * @param {Number} max
+ * @param {Number} range
  * @returns {number[]}
  */
 export function pickNumberInRange(min, max, range) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
+
   const numbers = new Set();
-  while (true) {
-    if (numbers.size === range) {
-      break;
-    }
-    const randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  while (numbers.size < range) {
+    const randomNumber = Math.floor(
+      Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+    );
     numbers.add(randomNumber);
   }
 
@@ -25,7 +25,7 @@ export function pickNumberInRange(min, max, range) {
 
 export const read = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 export function readLine(query) {
