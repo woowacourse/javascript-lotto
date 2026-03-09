7e7c113f87c6d549b17ee820898b65ce5b41569a
stardusty-lab <startdusty.lab@gmail.com>
2026-03-09 19:06:05 +0900

    test: expect 매개변수로 lotto 인스턴스를 만드는 함수 넘기도록 수정

--- __tests__/Model/Lotto.test.js
+++ __tests__/Model/Lotto.test.js
@@ -17,27 +17,24 @@ describe("로또 검사 테스트", () => {
 
   test("로또가 가진 번호가 6개가 아닌 경우", () => {
     const randomNumbers = [1, 2, 3, 4, 5];
-    const lotto = new Lotto(randomNumbers);
 
-    expect(lotto.getNumbers().length).toThrow(
+    expect(() => new Lotto(randomNumbers)).toThrow(
       ERROR_MESSAGE.INVALID_NUMBER_LENGTH,
     );
   });
 
   test("로또 숫자들의 범위가 1에서 45 범위를 초과한 경우", () => {
     const randomNumbers = [1, 2, 3, 4, 5, 50];
-    const lotto = new Lotto(randomNumbers);
 
-    expect(lotto.getNumbers().length).toThrow(
+    expect(() => new Lotto(randomNumbers)).toThrow(
       ERROR_MESSAGE.INVALID_NUMBER_RANGE,
     );
   });
 
   test("로또 숫자들이 중복된 경우", () => {
     const randomNumbers = [1, 2, 3, 4, 5, 5];
-    const lotto = new Lotto(randomNumbers);
 
-    expect(lotto.getNumbers().length).toThrow(
+    expect(() => new Lotto(randomNumbers)).toThrow(
       ERROR_MESSAGE.MUST_BE_NOT_DUPLICATE,
     );
   });
