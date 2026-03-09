d24014013e45a393b29faf1645695ba1a69cb259
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-09 15:14:59 +0900

    feat: LottoList 테스트 코드에 LottoList의 책임(로또 번호는 오름차순으로 6개이며, 1~45 사이의 범위이며 중복되지 않음) 기능 검증 테스트코드 추가

--- __tests__/LottoList.test.js
+++ __tests__/LottoList.test.js
@@ -2,31 +2,99 @@ import LottoList from "../src/Model/LottoList.js";
 import random from "../src/utils/random.js";
 
 const mockRandoms = (numbers) => {
-  random.randomArray = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, random.randomArray);
+  jest.spyOn(random, "randomArray");
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    random.randomArray
+  );
 };
 
 describe("로또 리스트 검사 테스트", () => {
-  test("로또 리스트 객체는 로또 개수를 받아 그 개수만큼 로또 배열을 가진다.", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("금액이 5000원이라면 로또 5장이 발행된다.", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+      [19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30],
+    ]);
     const amount = 5;
     const lottoList = new LottoList(amount);
 
     expect(lottoList.getLottoList().length).toBe(5);
   });
-  test("로또 객체는 중복되지 않은 번호들 오름차순으로 가진다.", () => {
+  test("각 로또 번호는 총 6개이다.", () => {
     const amount = 2;
-    const randoms = [
-      [1, 2, 3, 4, 5, 6],
-      [7, 8, 9, 10, 11, 12],
-    ];
-
-    mockRandoms(randoms);
     const lottoList = new LottoList(amount);
 
-    expect(lottoList.getLottoList()[0].getNumbers()).toEqual([
-      1, 2, 3, 4, 5, 6,
+    const isLottoHasSixNumbers = lottoList
+      .getLottoList()
+      .every((lotto) => lotto.getNumbers().length === 6);
+
+    expect(isLottoHasSixNumbers).toBe(true);
+  });
+
+  test("모든 로또 번호는 1~45 범위이다.", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+      [19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30],
     ]);
+    const amount = 5;
+    const lottoList = new LottoList(amount);
+
+    const isNumbersInRange = lottoList
+      .getLottoList()
+      .every((lotto) =>
+        lotto.getNumbers().every((number) => number >= 1 && number <= 45)
+      );
+
+    expect(isNumbersInRange).toBe(true);
+  });
+
+  test("모든 로또에 대해 한 장 안에서 번호들은 중복되지 않는다.", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+      [19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30],
+    ]);
+    const amount = 5;
+    const lottoList = new LottoList(amount);
+
+    const isAllLottoNumbersUnique = lottoList
+      .getLottoList()
+      .every(
+        (lotto) =>
+          new Set(lotto.getNumbers()).size === lotto.getNumbers().length
+      );
+
+    expect(isAllLottoNumbersUnique).toBe(true);
+  });
+
+  test("로또 한 장 번호들은 오름차순으로 정렬된다.", () => {
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [4, 5, 6, 7, 8, 9],
+    ]);
+    const amount = 2;
+
+    const lottoList = new LottoList(amount);
+
+    const isAscending = lottoList.getLottoList().every((lotto) => {
+      const numbers = lotto.getNumbers();
+      return numbers.every(
+        (number, index) => index === 0 || numbers[index - 1] <= number
+      );
+    });
+
+    expect(isAscending).toBe(true);
   });
 });
