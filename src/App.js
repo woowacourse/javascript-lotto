2472c1e70feb84786f14c1b16fb34276287575e7
장혜원 <127714800+janghw0126@users.noreply.github.com>
2026-03-09 15:11:57 +0900

    refactor: 입력 검증 실패 시 해당 단계부터 재입력하도록 구조 개선

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
 import InputConsole from "./Console/InputConsole.js";
 import OutputConsole from "./Console/OutputConsole.js";
 import LottoMachine from "./Domain/LottoMachine.js";
 import LottoResult from "./Domain/LottoResult.js";
 import Validator from "./Utils/Validator.js";
-
 class App {
   async run() {
     while (true) {
       // 구입할 로또 금액 입력받기
-      const purchasePrice = Validator.validatePurchasePrice(
-        await InputConsole.readPurchasePrice(),
-      );
+      const purchasePrice = await this.#readPurchasePrice();
 
       // 발행된 로또 목록 출력하기
       const lottoMachine = new LottoMachine();
@@ -18,15 +16,10 @@ class App {
       OutputConsole.printLottoList(lottos);
 
       // 당첨 번호 입력받기
-      const winningNumbers = Validator.validateWinningNumbers(
-        await InputConsole.readWinningNumbers(),
-      );
+      const winningNumbers = await this.#readWinningNumbers();
 
       // 보너스 번호 입력받기
-      const bonusNumber = Validator.validateBonusNumber(
-        await InputConsole.readBonusNumber(),
-        winningNumbers,
-      );
+      const bonusNumber = await this.#readBonusNumber();
 
       // 당첨 통계 출력하기
       const luckyNumbers = {
@@ -50,6 +43,39 @@ class App {
       // 재시작 로직 출력하기
       const restartCommand = await InputConsole.readRestart();
       if (restartCommand === "n") break;
+    }
+  }
+
+  async #readPurchasePrice() {
+    while (true) {
+      try {
+        const input = await InputConsole.readPurchasePrice();
+        return Validator.validatePurchasePrice(input);
+      } catch (e) {
+        Console.print(e.message);
+      }
+    }
+  }
+
+  async #readWinningNumbers() {
+    while (true) {
+      try {
+        const inputs = await InputConsole.readWinningNumbers();
+        return Validator.validateWinningNumbers(inputs);
+      } catch (e) {
+        Console.print(e.message);
+      }
+    }
+  }
+
+  async #readBonusNumber() {
+    while (true) {
+      try {
+        const inputs = await InputConsole.readBonusNumber();
+        return Validator.validateBonusNumber(inputs);
+      } catch (e) {
+        Console.print(e.message);
+      }
     }
   }
 }
