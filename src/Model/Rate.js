aa1dcbff8655d1ca20187f79ce749b7e84f1a8b2
stardusty-lab <startdusty.lab@gmail.com>
2026-03-09 23:12:39 +0900

    refactor: Rate 의 property를 private로 변경

--- src/Model/Rate.js
+++ src/Model/Rate.js
@@ -2,18 +2,18 @@ import { PRIZE_MONEY } from "../constants/lottoConstants.js";
 
 class Rate {
   constructor(statistics, price) {
-    this.statistics = statistics;
-    this.price = price;
+    this.#statistics = statistics;
+    this.#price = price;
   }
   #getTotal() {
-    return Object.entries(this.statistics).reduce((acc, [grade, count]) => {
+    return Object.entries(this.#statistics).reduce((acc, [grade, count]) => {
       acc += PRIZE_MONEY[grade] * count;
       return acc;
     }, 0);
   }
   getRate() {
     const total = this.#getTotal();
-    const rate = (total / this.price) * 100;
+    const rate = (total / this.#price) * 100;
 
     return Math.round(rate * 10) / 10;
   }
