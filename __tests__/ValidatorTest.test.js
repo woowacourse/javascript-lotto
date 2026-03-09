dc71e370d033d8ea1566f1a6c4deeeeca4b5e3f3
jebiyeon02 <jhje5595@naver.com>
2026-03-09 13:44:34 +0900

    test: 누락된 검증 테스트 작성

--- __tests__/ValidatorTest.test.js
+++ __tests__/ValidatorTest.test.js
@@ -2,7 +2,7 @@ import { ERROR_MESSAGE } from "../src/constants";
 import Validator from "../src/Validator";
 
 describe("유효성 검증 테스트", () => {
-  test("입력된 문자열이 빈 값이면 에러를 발생시켜야 한다", () => {
+  test("[예외] 입력된 문자열이 빈 값이면 에러를 발생시켜야 한다", () => {
     // given
     const wrongInput = "";
 
@@ -11,7 +11,40 @@ describe("유효성 검증 테스트", () => {
       ERROR_MESSAGE.PREFIX,
     );
   });
-  test("입력된 문자열이 숫자가 아니면 에러를 발생시켜야 한다", () => {
+
+  test.each([0, -1])(
+    "[예외] 숫자가 양의 정수가 아니면 에러를 발생시켜야 한다 (%s)",
+    (wrongNumber) => {
+      // when & then
+      expect(() => Validator.validatePositiveNumber(wrongNumber)).toThrow(
+        ERROR_MESSAGE.PREFIX,
+      );
+    },
+  );
+
+  test("[예외] 숫자가 기준값보다 크면 에러를 발생시켜야 한다", () => {
+    // given
+    const upper = 45;
+    const wrongNumber = 46;
+
+    // when & then
+    expect(() => Validator.validateNumberUpper(upper, wrongNumber)).toThrow(
+      ERROR_MESSAGE.PREFIX,
+    );
+  });
+
+  test("[예외] 숫자가 기준값보다 작으면 에러를 발생시켜야 한다", () => {
+    // given
+    const lower = 1;
+    const wrongNumber = 0;
+
+    // when & then
+    expect(() => Validator.validateNumberLower(lower, wrongNumber)).toThrow(
+      ERROR_MESSAGE.PREFIX,
+    );
+  });
+
+  test("[예외] 입력된 문자열이 숫자가 아니면 에러를 발생시켜야 한다", () => {
     // given
     const wrongInput = "NoNumber";
 
@@ -21,7 +54,7 @@ describe("유효성 검증 테스트", () => {
     );
   });
 
-  test("숫자가 특정 값으로 나누어 떨어지지 않으면 에러를 발생시켜야 한다", () => {
+  test("[예외] 숫자가 특정 값으로 나누어 떨어지지 않으면 에러를 발생시켜야 한다", () => {
     // given
     const divideNumber = 1000;
     const wrongInput = 1500;
@@ -32,7 +65,7 @@ describe("유효성 검증 테스트", () => {
     ).toThrow(ERROR_MESSAGE.PREFIX);
   });
 
-  test("배열에 중복된 원소가 존재하면 에러를 발생시켜야 한다", () => {
+  test("[예외] 배열에 중복된 원소가 존재하면 에러를 발생시켜야 한다", () => {
     // given
     const wrongArray = [1, 1, 2, 3, 4, 5];
 
@@ -42,7 +75,7 @@ describe("유효성 검증 테스트", () => {
     );
   });
 
-  test("배열의 길이가 올바르지 않으면 에러를 발생시켜야 한다", () => {
+  test("[예외] 배열의 길이가 올바르지 않으면 에러를 발생시켜야 한다", () => {
     // given
     const wrongArray = [1, 2, 3, 4, 5];
 
@@ -50,5 +83,16 @@ describe("유효성 검증 테스트", () => {
     expect(() => Validator.validateArrayLength(wrongArray, 6)).toThrow(
       ERROR_MESSAGE.PREFIX,
     );
+  });
+
+  test("[예외] 원하는 값이 배열에 포함되어 있지 않으면 예외를 발생시켜야 한다", () => {
+    // given
+    const targetElement = "a";
+    const array = ["b", "c"];
+
+    // when & then
+    expect(() =>
+      Validator.validateIncludeElement(targetElement, array),
+    ).toThrow(ERROR_MESSAGE.PREFIX);
   });
 });
