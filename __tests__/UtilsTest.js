490b17a9a61e3991bf2af1a95f08bdc98c9cbf3d
TH_97 <hood3392@gmail.com>
2026-03-09 16:53:01 +0900

    test: 유틸 테스트 케이스 추가 및 삭제

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -1,16 +1,13 @@
-import { pickNumberInRange } from '../src/step1/Utils.js';
+import { pickNumberInRange } from "../src/step1/Utils.js";
 
-
-describe('유틸 함수 테스트', () => {
-  test('범위내 랜덤값 반환 테스트', () => {
-    jest.spyOn(global.Math, 'random')
-      .mockReturnValueOnce(0.1)
-      .mockReturnValueOnce(0.2)
-      .mockReturnValueOnce(0.3)
-      .mockReturnValueOnce(0.4)
-      .mockReturnValueOnce(0.5)
-      .mockReturnValueOnce(0.6);
-
-    expect(pickNumberInRange(1, 10, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+describe("유틸 함수 테스트", () => {
+  test("반환된 배열의 길이가 range와 일치한다", () => {
+    const result = pickNumberInRange(1, 45, 7);
+    expect(result).toHaveLength(7);
+  });
+  test("max 경계값(45)이 반환될 수 있다", () => {
+    jest.spyOn(global.Math, "random").mockReturnValue(0.9999);
+    const result = pickNumberInRange(1, 45, 1);
+    expect(result).toContain(45);
   });
 });
