428da2a04d727a002cf1efef533df5bdaad54bc1
binggwa <qudrhks317@gmail.com>
2026-03-09 16:05:06 +0900

    refactor(LuckyNumbers,Validator) : 당첨번호와 보너스번호 검증 기능을 LuckyNumbers로 이관, Validator 최종삭제

--- src/Domain/LuckyNumbers.js
+++ src/Domain/LuckyNumbers.js
@@ -1,23 +1,29 @@
 class LuckyNumbers {
-  #winningNumbers;
+  #winningLotto;
   #bonusNumber;
 
   constructor(winningNumbers, bonusNumber) {
-    this.#winningNumbers = winningNumbers;
+    this.#winningLotto = new Lotto(winningNumbers);
+
+    this.#validateBonusNumber(bonusNumber);
     this.#bonusNumber = bonusNumber;
   }
 
-  winningNumbers() { return this.#winningNumbers; }
+  #validateBonusNumber(bonusNumber) {
+    const bonus = Number(bonusNumber.trim());
+
+    if (Number.isNaN(bonus) || bonus < 1 || bonus > 45) {
+      throw new Error("[ERROR] 보너스 번호는 1~45 범위의 숫자여야 합니다!");
+    }
+    if (this.#winningLotto.getNumbers().includes(bonus)) {
+      throw new Error("[ERROR] 보너스 번호가 당첨번호와 중복됩니다!");
+    }
+
+    return bonus;
+  }
+
+  winningNumbers() { return this.#winningLotto.getNumbers(); }
   bonusNumber() { return this.#bonusNumber; }
-
-  setWinningNumbers(winningNumbers) {
-    this.#winningNumbers = winningNumbers;
-  }
-
-  setBonusNumber(bonusNumber) {
-    this.#bonusNumber = bonusNumber;
-  }
-
 }
 
 export default LuckyNumbers;
