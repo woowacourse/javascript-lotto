d39cdd2ea3faaf59a72429435b98f3de7e12d9e5
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-09 15:41:37 +0900

    feat: parsing 테스트에 공백이 포함된 경우, 구분자 형식이 오류난 경우를 추가 & test.each로 중복 코드 제거

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -2,11 +2,37 @@ import { ERROR_MESSAGE } from "../src/constants/errorMessage.js";
 import { parsingNumbers } from "../src/utils/parsing.js";
 
 describe("문자열 파싱 테스트", () => {
-  test("로또 번호 파싱 시의 유효성 검사", () => {
-    const numberString = "1,2,3,4m,5,6";
-
-    expect(() => parsingNumbers(numberString)).toThrow(
+  test.each([
+    [
+      "정상적으로 처리하는 경우 파싱한다.",
+      "1,2,3,4,5,6",
+      [1, 2, 3, 4, 5, 6],
+      null,
+    ],
+    [
+      "숫자가 공백을 포함한 경우 공백을 제거한 후 파싱한다.",
+      "1,2,     3     , 4 ,5 , 6",
+      [1, 2, 3, 4, 5, 6],
+      null,
+    ],
+    [
+      "숫자와 문자가 같이 입력된 경우 NOT_NUMBER 에러를 발생시킨다.",
+      "1,2,3,4m,5,6",
+      null,
       ERROR_MESSAGE.NOT_NUMBER,
-    );
+    ],
+    [
+      "구분자 형식이 오류가 난 경우 INVALID_NUMBER_LENGTH 에러를 발생시킨다.",
+      "1,2,3,4,5 6",
+      null,
+      ERROR_MESSAGE.INVALID_NUMBER_LENGTH,
+    ],
+  ])("%s", (_, numberString, expected, expectedError) => {
+    if (expectedError) {
+      expect(() => parsingNumbers(numberString)).toThrow(expectedError);
+      return;
+    }
+
+    expect(parsingNumbers(numberString)).toEqual(expected);
   });
 });
--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -3,6 +3,11 @@ import { ERROR_MESSAGE } from "../constants/errorMessage.js";
 export const parsingNumbers = (numberString) => {
   const numberStringArray = numberString.split(",");
 
+  if (numberStringArray.length !== 6)
+    throw new Error(ERROR_MESSAGE.INVALID_NUMBER_LENGTH);
+
+  console.log(numberStringArray);
+
   numberStringArray.forEach((numberString) => {
     const number = Number(numberString);
     if (isNaN(number)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
