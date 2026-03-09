122dd2748667747d6a6597301148e57c500fcd25
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-09 11:25:22 +0900

    fix: random 모듈의 randomArray 메서드에서 범위가 항상 0부터인 문제 수정 및 방어로직 추가

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,9 +1,13 @@
 const random = {
   randomArray: (startNum, endNum, count) => {
+    // 무한루프 방지 로직
+    if (count > endNum - startNum + 1) return;
+
     // 랜덤 숫자 뽑기
     let randomArray = [];
     for (let i = 0; i < count; i++) {
-      const randomNum = Math.floor(Math.random() * (endNum - startNum + 1));
+      const randomNum =
+        Math.floor(Math.random() * (endNum - startNum + 1)) + startNum;
       if (randomArray.indexOf(randomNum) === -1) randomArray.push(randomNum);
       else i--;
     }
