bff6e6408af16f2b34452f93cc8d6a5812204017
stardusty-lab <startdusty.lab@gmail.com>
2026-03-09 18:38:51 +0900

    fix: 로또 번호들(lotto) 검증 순서 변경
    - 각 로또 번호 검사 ->  length 검사 -> 중복 여부 검사

--- src/Validator.js
+++ src/Validator.js
@@ -17,6 +17,10 @@ class Validator {
       throw new Error(ERROR_MESSAGE.INVALID_NUMBER_RANGE);
   }
   validateLottoNumbers(numbers) {
+    numbers.forEach((number) => {
+      this.validateLottoNumber(number);
+    });
+
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGE.INVALID_NUMBER_LENGTH);
     }
@@ -26,10 +30,6 @@ class Validator {
     if (set.size !== numbers.length) {
       throw new Error(ERROR_MESSAGE.MUST_BE_NOT_DUPLICATE);
     }
-
-    numbers.forEach((number) => {
-      this.validateLottoNumber(number);
-    });
   }
 
   validateBonusNumber(lottoNumbers, bonusNumber) {
