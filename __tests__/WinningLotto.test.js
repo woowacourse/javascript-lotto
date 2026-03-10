64987546192a7f2c9fea544bd75790fad976f446
gustn99 <azerosong@gmail.com>
2026-03-10 14:37:10 +0900

    feat(WinningLotto): 등수를 계산하는 기능 추가

--- __tests__/WinningLotto.test.js
+++ __tests__/WinningLotto.test.js
@@ -12,4 +12,81 @@ describe('WinningLotto 클래스 유닛 테스트', () => {
       expect(() => new WinningLotto(winningLotto, bonusNumber)).toThrow('당첨 번호와 보너스 번호는 중복될 수 없습니다.');
     });
   });
+
+  describe('calculateLottoRank', () => {
+    test('당첨 번호와 일치하는 번호의 개수가 6이면 1을 반환한다.', () => {
+      // given
+      const userLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const bonusNumber = 7;
+
+      const winningLottoInstance = new WinningLotto(winningLotto, bonusNumber);
+
+      // when
+      const rank = winningLottoInstance.calculateRank(userLotto);
+
+      // then
+      expect(rank).toBe(1);
+    });
+
+    test('당첨 번호와 일치하는 번호의 개수가 5이고 보너스 번호를 포함하면 2를 반환한다.', () => {
+      // given
+      const userLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningLotto = new Lotto([1, 2, 3, 4, 5, 7]);
+      const bonusNumber = 6;
+
+      const winningLottoInstance = new WinningLotto(winningLotto, bonusNumber);
+
+      // when
+      const rank = winningLottoInstance.calculateRank(userLotto);
+
+      // then
+      expect(rank).toBe(2);
+    });
+
+    test('당첨 번호와 일치하는 번호의 개수가 5이면 3을 반환한다.', () => {
+      // given
+      const userLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningLotto = new Lotto([1, 2, 3, 4, 5, 7]);
+      const bonusNumber = 8;
+
+      const winningLottoInstance = new WinningLotto(winningLotto, bonusNumber);
+
+      // when
+      const rank = winningLottoInstance.calculateRank(userLotto);
+
+      // then
+      expect(rank).toBe(3);
+    });
+
+    test('당첨 번호와 일치하는 번호의 개수가 4이면 4를 반환한다.', () => {
+      // given
+      const userLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningLotto = new Lotto([1, 2, 3, 4, 7, 8]);
+      const bonusNumber = 5;
+
+      const winningLottoInstance = new WinningLotto(winningLotto, bonusNumber);
+
+      // when
+      const rank = winningLottoInstance.calculateRank(userLotto);
+
+      // then
+      expect(rank).toBe(4);
+    });
+
+    test('당첨 번호와 일치하는 번호의 개수가 3이면 5를 반환한다.', () => {
+      // given
+      const userLotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const winningLotto = new Lotto([1, 2, 3, 7, 8, 9]);
+      const bonusNumber = 4;
+
+      const winningLottoInstance = new WinningLotto(winningLotto, bonusNumber);
+
+      // when
+      const rank = winningLottoInstance.calculateRank(userLotto);
+
+      // then
+      expect(rank).toBe(5);
+    });
+  });
 });
--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -17,6 +17,21 @@ class WinningLotto {
     }
   }
 
+  calculateRank(userLotto) {
+    const matchCount = userLotto.matchCount(this.#winningLotto);
+    if (matchCount === 6) {
+      return 1;
+    } else if (matchCount === 5 && userLotto.includes(this.#bonusNumber)) {
+      return 2;
+    } else if (matchCount === 5) {
+      return 3;
+    } else if (matchCount === 4) {
+      return 4;
+    } else if (matchCount === 3) {
+      return 5;
+    }
+    return 6;
+  }
 }
 
 export default WinningLotto;
