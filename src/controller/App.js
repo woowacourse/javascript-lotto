9cf35baed54531e826611b1facc1c89bf4eb1d13
김상윤 <ksy151020@gmail.com>
2026-03-10 09:45:28 +0900

    refactor: App 컨트롤러 리펙토링

--- src/controller/App.js
+++ src/controller/App.js
@@ -7,28 +7,44 @@ import Lotto from "../model/Lotto.js";
 class App {
   async run() {
     while (true) {
-      const purchasedPrice = await retry(() => InputView.inputPrice());
+      const lottoController = await this.#prepareLottoGame();
 
-      const lottoCount = purchasedPrice / 1000;
+      await this.#playLottoGame(lottoController);
 
-      const lottoController = new LottoController(lottoCount);
-      const purchasedLottos = lottoController.issueLottos();
-      OutPutView.printLotto(purchasedLottos);
+      this.#showGameResult(lottoController);
 
-      const winningNums = await retry(() => InputView.inputWinningNums());
-      const winningLotto = new Lotto(winningNums);
-
-      const bonusNum = await retry(() => InputView.inputBonusNum(winningLotto.getNumbers()));
-      lottoController.updateWinningResult(winningLotto, bonusNum);
-
-      const { rankCount, profitRate } = lottoController.getWinningResult();
-      OutPutView.printResult(rankCount, profitRate);
-
-      const restartAnswer = await retry(() => InputView.inputRestartAnswer());
-      if (restartAnswer === "n") break;
+      if (await this.#retryGame()) break;
     }
-
     InputView.close();
+  }
+
+  async #prepareLottoGame() {
+    const purchasedPrice = await retry(() => InputView.inputPrice());
+    const lottoCount = purchasedPrice / 1000;
+    const lottoController = new LottoController(lottoCount);
+
+    const purchasedLottos = lottoController.issueLottos();
+    OutPutView.printLotto(purchasedLottos);
+
+    return lottoController;
+  }
+
+  async #playLottoGame(lottoController) {
+    const winningNums = await retry(() => InputView.inputWinningNums());
+    const winningLotto = new Lotto(winningNums);
+
+    const bonusNum = await retry(() => InputView.inputBonusNum(winningLotto.getNumbers()));
+    lottoController.updateWinningResult(winningLotto, bonusNum);
+  }
+
+  #showGameResult(lottoController) {
+    const { rankCount, profitRate } = lottoController.getWinningResult();
+    OutPutView.printResult(rankCount, profitRate);
+  }
+
+  async #retryGame() {
+    const restartAnswer = await retry(() => InputView.inputRestartAnswer());
+    return restartAnswer === "n"
   }
 }
 
