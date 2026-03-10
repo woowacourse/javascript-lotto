a68ec6addcdcb60af78be3090293eadadb77ac87
gustn99 <azerosong@gmail.com>
2026-03-10 14:03:45 +0900

    feat(WinningLotto.test): WinningLotto 클래스 생성자 구현

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -1,5 +1,20 @@
+import ERROR_MESSAGE from './constants/errorMessage.js';
+import LottoNumber from './LottoNumber.js';
+
 class WinningLotto {
+  #winningLotto;
+  #bonusNumber;
+
   constructor(winningLotto, bonusNumber) {
+    this.#winningLotto = winningLotto;
+    this.validateBonusNumber(bonusNumber);
+    this.#bonusNumber = new LottoNumber(bonusNumber);
+  }
+
+  validateBonusNumber(bonusNumber) {
+    if (this.#winningLotto.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGE.BONUS_NUMBER.DUPLICATE);
+    }
   }
 
 }
