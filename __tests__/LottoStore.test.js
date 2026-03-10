a89de67acdfa805f4b84fc1a22013cc047facff6
gustn99 <azerosong@gmail.com>
2026-03-10 14:50:47 +0900

    test(LottoStore.test): purchaseLotto 시 반환하는 로또 인스턴스 개수 검증

--- __tests__/LottoStore.test.js
+++ __tests__/LottoStore.test.js
@@ -1,9 +1,9 @@
-import LottoStore from "../src/LottoStore.js";
-import Lotto from "../src/Lotto.js";
+import LottoStore from '../src/LottoStore.js';
+import Lotto from '../src/Lotto.js';
 
-describe("LottoStore 클래스 유닛 테스트", () => {
-  describe("purchaseLotto", () => {
-    test("구입 금액을 받아서 로또 인스턴스를 반환한다.", () => {
+describe('LottoStore 클래스 유닛 테스트', () => {
+  describe('purchaseLotto', () => {
+    test('구입 금액을 받아서 로또 인스턴스를 반환한다.', () => {
       // given
       const amount = 1000;
 
@@ -15,10 +15,21 @@ describe("LottoStore 클래스 유닛 테스트", () => {
         expect(lotto).toBeInstanceOf(Lotto);
       });
     });
+
+    test('1000원에 1개씩 로또 인스턴스를 반환한다.', () => {
+      // given
+      const amount = 3000;
+
+      // when
+      const lottos = LottoStore.purchaseLottos(amount);
+
+      // then
+      expect(lottos.length).toBe(3);
+    });
   });
 
-  describe("createRandomLotto", () => {
-    test("랜덤한 로또 번호를 가지는 로또 인스턴스를 반환한다.", () => {
+  describe('createRandomLotto', () => {
+    test('랜덤한 로또 번호를 가지는 로또 인스턴스를 반환한다.', () => {
       // when
       const lotto = LottoStore.createRandomLotto();
 
