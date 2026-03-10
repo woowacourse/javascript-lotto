0def3649e93d4b18e9c39e76f9c747510955b126
TH_97 <hood3392@gmail.com>
2026-03-10 18:17:50 +0900

    test: [ERROR]라는 공통 키워드로 테스트 기준 변경

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,17 +1,16 @@
-import { readLine } from '../src/step1/Utils.js';
-import { pickNumberInRange } from '../src/step1/Utils.js';
-import App from '../src/step1/App.js';
+import { readLine } from "../src/step1/Utils.js";
+import { pickNumberInRange } from "../src/step1/Utils.js";
+import App from "../src/step1/App.js";
 
-jest.mock('../src/step1/Utils.js', () => ({
+jest.mock("../src/step1/Utils.js", () => ({
   readLine: jest.fn(),
   read: { close: jest.fn() },
   pickNumberInRange: jest.fn(),
 }));
 
-const logSpy = jest.spyOn(console, 'log');
+const logSpy = jest.spyOn(console, "log");
 
-const runException = async (inputs, expectedMessage) => {
-
+const runException = async (inputs) => {
   inputs.forEach((input) => {
     readLine.mockImplementationOnce(() => {
       return input;
@@ -21,84 +20,56 @@ const runException = async (inputs, expectedMessage) => {
   const app = new App();
   await app.run();
 
-  expect(logSpy).toHaveBeenCalledWith(expectedMessage);
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
 };
 
-describe('구입 금액 입력예외 테스트', () => {
+describe("구입 금액 입력예외 테스트", () => {
   beforeEach(() => {
     pickNumberInRange.mockReturnValue([1, 2, 3, 4, 5, 6]);
   });
 
-  test('1000원 단위가 아닌 입력예외 테스트', async () => {
-    runException(
-      ['600', '1000', '1,2,3,4,5,6', '10', 'n'],
-      '[ERROR] 1000원 단위만 입력 가능합니다.',
-    );
+  test("1000원 단위가 아닌 입력예외 테스트", async () => {
+    runException(["600", "1000", "1,2,3,4,5,6", "10", "n"]);
   });
 
-  test.each([
-    ['ㅁ'],
-    ['1.5'],
-    ['NaN'],
-  ])('정수가 아닌 입력 예외 테스트 - %s', async (input) => {
-    runException(
-      [input, '1000', '1,2,3,4,5,6', '10', 'n'],
-      '[ERROR] 숫자만 입력해 주세요.',
-    );
+  test.each([["ㅁ"], ["1.5"], ["NaN"]])(
+    "정수가 아닌 입력 예외 테스트 - %s",
+    async (input) => {
+      runException([input, "1000", "1,2,3,4,5,6", "10", "n"]);
+    },
+  );
+});
+
+describe("당첨 번호 입력 예외 테스트", () => {
+  test("1 ~ 45가 아닌 입력 예외 테스트", async () => {
+    runException(["1000", "1,2,3,5,6,46", "1,2,3,4,5,6", "10", "n"]);
+  });
+
+  test("겹치는 당첨 번호 입력 예외 테스트", async () => {
+    runException(["1000", "1,1,2,3,4,5", "1,2,3,4,5,6", "10", "n"]);
+  });
+
+  test("정수가 아닌 당첨 번호 입력 예외 테스트", async () => {
+    runException(["1000", "a,1,2,3,4,5", "1,2,3,4,5,6", "10", "n"]);
   });
 });
 
-describe('당첨 번호 입력 예외 테스트', () => {
-  test('1 ~ 45가 아닌 입력 예외 테스트', async () => {
-    runException(
-      ['1000', '1,2,3,5,6,46', '1,2,3,4,5,6', '10', 'n'],
-      '[ERROR] 1 ~ 45 이내 숫자만 입력 가능합니다.',
-    );
+describe("보너스 번호 입력 예외 테스트", () => {
+  test("1 ~ 45가 아닌 보너스 번호 입력 예외 테스트", async () => {
+    runException(["1000", "1,2,3,4,5,6", "46", "10", "n"]);
   });
 
-  test('겹치는 당첨 번호 입력 예외 테스트', async () => {
-    runException(
-      ['1000', '1,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'],
-      '[ERROR] 중복 당첨 번호 입력은 불가 합니다.',
-    );
+  test("당첨 번호와 동일한 입력 예외 테스트", async () => {
+    runException(["1000", "1,2,3,4,5,6", "3", "41", "n"]);
   });
 
-  test('정수가 아닌 당첨 번호 입력 예외 테스트', async () => {
-    runException(
-      ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', '10', 'n'],
-      '[ERROR] 당첨 번호는 숫자만 입력 가능합니다.',
-    );
+  test("정수가 아닌 보너스 번호 입력 예외 테스트", async () => {
+    runException(["1000", "a,1,2,3,4,5", "1,2,3,4,5,6", "z", "21", "n"]);
   });
 });
 
-describe('보너스 번호 입력 예외 테스트', () => {
-  test('1 ~ 45가 아닌 보너스 번호 입력 예외 테스트', async () => {
-    runException(
-      ['1000', '1,2,3,4,5,6', '46', '10', 'n'],
-      '[ERROR] 보너스 번호는 1 ~ 45 이내 숫자여야 합니다.',
-    );
-  });
-
-  test('당첨 번호와 동일한 입력 예외 테스트', async () => {
-    runException(
-      ['1000', '1,2,3,4,5,6', '3', '41', 'n'],
-      '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
-    );
-  });
-
-  test('정수가 아닌 보너스 번호 입력 예외 테스트', async () => {
-    runException(
-      ['1000', 'a,1,2,3,4,5', '1,2,3,4,5,6', 'z', '21', 'n'],
-      '[ERROR] 보너스 번호는 숫자여야 합니다.',
-    );
-  });
-});
-
-describe('재시작 입력 예외 테스트', () => {
-  test('y 또는 n외 입력 테스트', async () => {
-    runException(
-      ['1000', '1,2,3,4,5,6', '7', 'zzz', 'n'],
-      '[ERROR] 다시시작 입력은 y 또는 n 만 입력 가능합니다.',
-    );
+describe("재시작 입력 예외 테스트", () => {
+  test("y 또는 n외 입력 테스트", async () => {
+    runException(["1000", "1,2,3,4,5,6", "7", "zzz", "n"]);
   });
 });
