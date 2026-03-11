be9143125f06b52edf4eddcc3e9a0e32b103245a
boyeon <boyeon6331@gmail.com>
2026-03-11 23:47:47 +0900

    feat: 구매한 로또 렌더링 기능 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,19 +1,48 @@
+import { LOTTO_PRICE } from "./constants/lottoInfo";
+import LottoController from "./controller/LottoController";
 import Validator from "./utils/Validator";
 
 const purchaseForm = document.querySelector("form");
-const priceInput = document.querySelector("#purchase-price");
+const purchasePriceInput = document.querySelector("#purchase-price");
 
-purchaseForm.addEventListener("submit", (event) => {
+const handlePurchase = (event) => {
   event.preventDefault();
 
   try {
-    const price = Number(priceInput.value);
+    const purchasedPrice = Number(purchasePriceInput.value);
 
-    Validator.validateNumber(price);
-    Validator.validatePurchaseUnit(price);
+    Validator.validateNumber(purchasedPrice);
+    Validator.validatePurchaseUnit(purchasedPrice);
 
     purchaseForm.reset();
+    const lottoCount = purchasedPrice / LOTTO_PRICE;
+    const lottoController = new LottoController(lottoCount);
+    const purchasedLottos = lottoController.issueLottos();
+    renderLottoTicket(purchasedLottos);
   } catch (error) {
     alert(error.message);
   }
-});
+};
+
+purchaseForm.addEventListener("submit", handlePurchase);
+
+const lottoTicketContainer = document.querySelector(".lotto-ticket-container");
+
+const lottoTicket = (lotto) => {
+  const numbers = lotto.join(", ");
+
+  return `
+    <li class="lotto-ticket">
+      <span class="lotto-icon">🎟️</span>
+      <span class="lotto-numbers">${numbers}</span>
+    </li>
+  `;
+};
+
+const renderLottoTicket = (lottos) => {
+  lottoTicketContainer.innerHTML = `
+  <p>총 ${lottos.length}개를 구매하였습니다.</p>
+  <ul>
+    ${lottos.map((lotto) => lottoTicket(lotto)).join("")}
+  </ul>`;
+};
