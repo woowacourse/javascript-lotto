a234d10856f9cda340e35252d61f6186dad79fe9
hjkim0905 <eax9952@gmail.com>
2026-03-11 14:59:45 +0900

    refactor: add loadnestedComponents() to load nested components

--- src/ui/loadComponent.js
+++ src/ui/loadComponent.js
@@ -1,7 +1,23 @@
 const loadComponent = (id, path) => {
-  fetch(path)
+  return fetch(path)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById(id).insertAdjacentHTML("beforeend", data);
+      return loadNestedComponents(document.getElementById(id));
     });
+};
+
+const loadNestedComponents = (root) => {
+  const targetComponents = root.querySelectorAll("[data-component]");
+  const promises = [...targetComponents].map((element) => {
+    const elementPath = element.dataset.component;
+    element.removeAttribute("data-component");
+    return fetch(elementPath)
+      .then((response) => response.text())
+      .then((html) => {
+        element.insertAdjacentHTML("beforeend", html);
+        return loadNestedComponents(element);
+      });
+  });
+  return Promise.all(promises);
 };
