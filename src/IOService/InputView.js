a427ddfacfd4b784b435331fa1b311e2720a64de
박건규 <geongyu09@gmail.com>
2026-03-11 16:50:20 +0900

    chore: IO 관련 코드를 /IOService 디렉토리로 이동

--- src/IOService/InputView.js
+++ src/IOService/InputView.js
@@ -1,5 +1,5 @@
 import readline from "readline/promises";
-import ERROR_MESSAGE from "./constants/errorMessage.js";
+import ERROR_MESSAGE from "../constants/errorMessage.js";
 
 class InputView {
   async getUserInput(message) {
