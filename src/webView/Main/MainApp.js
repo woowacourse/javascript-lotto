b060e53b065369231698cad4a348f1a31cd15c83
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-03-11 17:38:34 +0900

    feat: 구입한 로또 아이콘과 번호 렌더링

--- src/webView/Main/MainApp.js
+++ src/webView/Main/MainApp.js
@@ -1,6 +1,20 @@
 const html = String.raw;
 
+const mockLottoCount = 3;
+const mockLottos = [
+  [1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 10, 11, 12],
+  [13, 14, 15, 16, 17, 18],
+];
+
 class MainApp extends HTMLElement {
+  #isShowLottos;
+
+  constructor() {
+    super();
+    this.#isShowLottos = false;
+  }
+
   connectedCallback() {
     this.render();
   }
@@ -8,8 +22,10 @@ class MainApp extends HTMLElement {
   render() {
     this.innerHTML = html`<div class="main-container">
       <div class="card-container">
+        <!-- 1. 헤더 -->
         <div class="card-header">🎱 내 번호 당첨 확인 🎱</div>
 
+        <!--  2. 구입 금액 입력 폼 -->
         <div class="card-input-container">
           <label class="input-label" for="purchase-amount">
             구입할 금액을 입력해주세요.
@@ -19,8 +35,32 @@ class MainApp extends HTMLElement {
             <button class="input-button">구입</button>
           </div>
         </div>
+
+        <!-- 3. 로또 및 로또 번호 & 보너스 번호 입력 폼 -->
+        <div class="card-hidden-section" ${this.#isShowLottos ? "" : "hidden"}>
+          <div class="lottos-container">
+            <div class="lottos-container-header">
+              총 ${mockLottoCount}개를 구매하셨습니다.
+            </div>
+            ${mockLottos
+              .map(
+                (lotto) =>
+                  // <!-- 하나의 로또 라인 -->
+                  html`<div class="lotto-line">
+                    <div class="lotto-line-icon">🎟️</div>
+                    ${lotto.join(", ")}
+                  </div>`
+              )
+              .join("")}
+          </div>
+        </div>
       </div>
     </div>`;
+
+    this.querySelector(".input-button").addEventListener("click", () => {
+      this.#isShowLottos = true;
+      this.render();
+    });
   }
 }
 
