0a22d9acbd89b0c90c6b0bac4dc9ecac2d1e94d1
jebiyeon02 <jhje5595@naver.com>
2026-03-12 23:02:12 +0900

    feat: 로또 구매 수량과 발행한 로또 번호 렌더링

--- src/step2-index.js
+++ src/step2-index.js
@@ -4,16 +4,45 @@
  */
 
 import { LOTTO } from "./constants";
+import LottoGenerator from "./LottoGenerator";
 import Validator from "./Validator";
 import WebView from "./View/WebView";
 
 const purchaseForm = document.querySelector(".purchase-form");
 const purchaseInput = document.querySelector(".purchase-form__input");
 
-function validateMoney(money) {
+const ticketList = document.querySelector(".ticket-list");
+const winningForm = document.querySelector(".winning-form");
+
+const validateMoney = (money) => {
   Validator.numberDivided(money, LOTTO.PRICE);
   Validator.positiveNumber(money);
-}
+};
+
+const setPurchaseLottoCount = (purchaseLottoCount) => {
+  const ticketListSummary = document.querySelector(".ticket-list__summary");
+  console.log(ticketListSummary);
+  ticketListSummary.innerText = `총 ${purchaseLottoCount}개를 구매하였습니다.`;
+};
+
+const getTicket = (lottoNumbers) => {
+  const ticket = document.createElement("div");
+  ticket.className = "ticket-list__item";
+  // 티켓 이미지 생성
+  const ticketIcon = document.createElement("img");
+  ticketIcon.className = "ticket-list__icon";
+  ticketIcon.src = "./assets//lotto_ticket.png";
+  // 로또 번호 span 태그 생성
+  const lottoNumberSpan = document.createElement("span");
+  lottoNumberSpan.className = "ticket-list__numbers";
+  const numberText = document.createTextNode(lottoNumbers.join(", "));
+  lottoNumberSpan.appendChild(numberText);
+
+  ticket.appendChild(ticketIcon);
+  ticket.appendChild(lottoNumberSpan);
+
+  return ticket;
+};
 
 // 이벤트 달기
 purchaseForm.addEventListener("submit", (e) => {
@@ -22,8 +51,22 @@ purchaseForm.addEventListener("submit", (e) => {
     const money = WebView.readMoney();
     validateMoney(money);
 
-    console.log();
-    alert(money);
+    // 구입 성공한 이후
+    const purchaseLottoCount = money / LOTTO.PRICE;
+    setPurchaseLottoCount(purchaseLottoCount);
+
+    // 로또 객체 생성
+    const lottos = LottoGenerator.makeLottos(purchaseLottoCount);
+    const ticketListContainer = document.querySelector(
+      ".ticket-list__container",
+    );
+
+    lottos.map((lotto) =>
+      ticketListContainer.appendChild(getTicket(lotto.getNumbers())),
+    );
+
+    ticketList.style.visibility = "visible";
+    winningForm.style.visibility = "visible";
   } catch (error) {
     alert(error.message);
     purchaseInput.focus();
