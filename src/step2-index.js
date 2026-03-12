d0c8a7c79e1e39e17fb4e44d2f254257b7f0972d
kojesung <js95112345@gmail.com>
2026-03-12 15:47:26 +0900

    feat: 로또 구매 후 구매한 로또 목록 확인 기능 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,34 @@
+import { parseStringToNumber } from "./utils/parser.js";
+import { generateRandomNumbers } from "./generateRandomNumbers.js";
+import WebLottoManager from "./WebLottoManager.js";
+
 /**
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+
+const purchaseBtn = document.getElementById("purchase-amount-button");
+const purchaseInput = document.getElementById("purchase-amount-input");
+const purchasedLottoSection = document.getElementById(
+  "purchased-lotto-section"
+);
+
+const webLottoManager = new WebLottoManager(generateRandomNumbers);
+
+purchaseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const amount = parseStringToNumber(purchaseInput.value);
+
+  const { count, lottos } = webLottoManager.purchase(amount);
+  purchasedLottoSection.innerHTML = `
+  <p>총 ${count}개를 구매하였습니다.</p>
+  ${lottos
+    .map(
+      (lotto) =>
+        `<p class='lotto-number-container'><span class='lotto-emoji'>🎟️</span><span class='lotto-numbers'>${lotto
+          .getNumbers()
+          .join(", ")}</span></p>`
+    )
+    .join("")}
+  `;
+});
