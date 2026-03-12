b2e685e9a3f2657ad7bc3b81cbdc07d7f678b138
jebiyeon02 <jhje5595@naver.com>
2026-03-13 01:22:37 +0900

    feat: 당첨 통계를 표시하고, 다시 시작 기능 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -6,6 +6,7 @@
 import { LOTTO } from "./constants";
 import LottoGenerator from "./LottoGenerator";
 import WinningLotto from "./Model/WinningLotto";
+import ScoreBoard from "./ScoreBoard";
 import Validator from "./Validator";
 import WebView from "./View/WebView";
 
@@ -16,16 +17,49 @@ const purchaseSubmitButton = document.querySelector(
 );
 
 const ticketList = document.querySelector(".ticket-list");
+const ticketListContainer = document.querySelector(".ticket-list__container");
 
 const winningForm = document.querySelector(".winning-form");
 const resultModal = document.querySelector(".result-modal");
 const resultModalCloseButton = document.querySelector(
   ".result-modal__close-btn",
 );
+const resultModalProfit = document.querySelector(".result-modal__profit");
+const resultModalSubmitButton = document.querySelector(
+  ".result-modal__submit-btn",
+);
+
+const lottos = [];
+let money;
 
 const validateMoney = (money) => {
   Validator.numberDivided(money, LOTTO.PRICE);
   Validator.positiveNumber(money);
+};
+
+const initLotttGame = () => {
+  lottos.length = 0;
+  ticketListContainer.innerHTML = "";
+
+  purchaseForm.reset();
+  winningForm.reset();
+
+  purchaseInput.disabled = false;
+  purchaseInput.style.cursor = "";
+  purchaseSubmitButton.disabled = false;
+  purchaseSubmitButton.style.cursor = "pointer";
+
+  ticketList.style.visibility = "hidden";
+  winningForm.style.visibility = "hidden";
+
+  const winningNumberInputNodes = document.querySelectorAll(
+    ".winning-form__input",
+  );
+
+  winningNumberInputNodes.forEach((node) => {
+    node.disabled = false;
+    node.style.cursor = "";
+  });
 };
 
 const setPurchaseLottoCount = (purchaseLottoCount) => {
@@ -39,7 +73,7 @@ const getTicket = (lottoNumbers) => {
   // 티켓 이미지 생성
   const ticketIcon = document.createElement("img");
   ticketIcon.className = "ticket-list__icon";
-  ticketIcon.src = "./assets//lotto_ticket.png";
+  ticketIcon.src = "./assets/lotto_ticket.png";
   // 로또 번호 span 태그 생성
   const lottoNumberSpan = document.createElement("span");
   lottoNumberSpan.className = "ticket-list__numbers";
@@ -52,13 +86,25 @@ const getTicket = (lottoNumbers) => {
   return ticket;
 };
 
-const lottos = [];
+const setScore = (allRankCount) => {
+  const tableDataFifth = document.querySelector(".result-modal__td--fifth");
+  const tableDataFourth = document.querySelector(".result-modal__td--fourth");
+  const tableDataThird = document.querySelector(".result-modal__td--third");
+  const tableDataSecond = document.querySelector(".result-modal__td--second");
+  const tableDataFirst = document.querySelector(".result-modal__td--first");
+
+  tableDataFifth.innerText = `${allRankCount.FIFTH}개`;
+  tableDataFourth.innerText = `${allRankCount.FOURTH}개`;
+  tableDataThird.innerText = `${allRankCount.THIRD}개`;
+  tableDataSecond.innerText = `${allRankCount.SECOND}개`;
+  tableDataFirst.innerText = `${allRankCount.FIRST}개`;
+};
 
 // 이벤트 달기
 purchaseForm.addEventListener("submit", (e) => {
   e.preventDefault(); // 폼 새로고침 방지
   try {
-    const money = WebView.readMoney();
+    money = WebView.readMoney();
     validateMoney(money);
 
     // 구입 성공한 이후
@@ -71,9 +117,6 @@ purchaseForm.addEventListener("submit", (e) => {
 
     // 로또 객체 생성
     lottos.push(...LottoGenerator.makeLottos(purchaseLottoCount));
-    const ticketListContainer = document.querySelector(
-      ".ticket-list__container",
-    );
 
     lottos.map((lotto) =>
       ticketListContainer.appendChild(getTicket(lotto.getNumbers())),
@@ -95,8 +138,13 @@ winningForm.addEventListener("submit", (e) => {
     const bonusNumber = WebView.readBonusNumber();
     const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
     const winningNumberInputNodes = document.querySelectorAll(
-      ".winning-form__input ",
+      ".winning-form__input",
     );
+    const allRankCount = ScoreBoard.makeAllRankCount(lottos, winningLotto);
+    setScore(allRankCount);
+    const profitRate = ScoreBoard.getProfitRate(allRankCount, money);
+    resultModalProfit.innerText = `당신의 총 수익률은 ${profitRate}%입니다.`;
+
     winningNumberInputNodes.forEach((node) => {
       node.disabled = true;
       node.style.cursor = "not-allowed";
@@ -110,4 +158,9 @@ winningForm.addEventListener("submit", (e) => {
 
 resultModalCloseButton.addEventListener("click", (e) => {
   resultModal.close();
+});
+
+resultModalSubmitButton.addEventListener("click", () => {
+  resultModal.close();
+  initLotttGame();
 });
