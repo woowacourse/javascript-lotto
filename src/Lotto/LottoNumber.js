509c0b0be567f7b9846efbbb2a94ca5b50534e0f
박건규 <geongyu09@gmail.com>
2026-03-12 15:55:08 +0900

    feat(step2-index): 구입 금액을 form/input 요소로 입력받아 로또를 발행하는 로직 구현

--- src/Lotto/LottoNumber.js
+++ src/Lotto/LottoNumber.js
@@ -1,4 +1,4 @@
-import ERROR_MESSAGE from "./constants/errorMessage.js";
+import ERROR_MESSAGE from "../constants/errorMessage.js";
 
 class LottoNumber {
   #number;
--- src/step2-index.js
+++ src/step2-index.js
@@ -1,6 +1,16 @@
-import App from "./App.js";
-import { render } from "./service/index.js";
+import LottoStore from "./Lotto/LottoStore.js";
 
-const app = document.querySelector("#app");
+const purchaseForm = document.querySelector(".lotto-purchase-form");
 
-render(app, App());
+let lottos = [];
+
+purchaseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(purchaseForm);
+  const purchaseAmount = parseInt(formData.get("purchase-amount"), 10); // TODO: 상수화 하기
+  lottos = LottoStore.purchaseLottos(purchaseAmount);
+
+  drawLottoList();
+});
+
+function drawLottoList() {}
