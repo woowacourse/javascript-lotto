303f66c49d1473fb68df75e02e8eb7155506767e
박건규 <geongyu09@gmail.com>
2026-03-12 16:03:43 +0900

    feat(step2-index): 로또 발행 시 당첨 번호/보너스 번호 입력 폼이 나타나도록 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -11,6 +11,7 @@ purchaseForm.addEventListener("submit", (event) => {
   lottos = LottoStore.purchaseLottos(purchaseAmount);
 
   drawLottoList();
+  drawWinningNumbersAndBonusNumber();
 });
 
 function drawLottoList() {
@@ -18,7 +19,7 @@ function drawLottoList() {
 
   const lottoListInfo = document.createElement("p");
   lottoListInfo.classList.add("lotto-list__info");
-  lottoListInfo.textContent = `총 ${lottos.length}개를 구매하였습니다.`;
+  lottoListInfo.innerText = `총 ${lottos.length}개를 구매하였습니다.`;
   lottoListBlock.appendChild(lottoListInfo);
 
   const lottoListItems = document.createElement("ul");
@@ -31,4 +32,76 @@ function drawLottoList() {
     lottoListItem.innerHTML = `<img src="/assets/Lotto.png" alt="로또" width="34px" /> ${lotto.parseNumbers().join(", ")}`;
     lottoListItems.appendChild(lottoListItem);
   });
+}
+
+function drawWinningNumbersAndBonusNumber() {
+  const winningNumbersAndBonusNumberBlock = document.querySelector(
+    ".lotto-winning-bonus-number",
+  );
+
+  const winningNumbersAndBonusNumberInfo = document.createElement("p");
+  winningNumbersAndBonusNumberInfo.innerText =
+    "지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요.";
+  winningNumbersAndBonusNumberBlock.appendChild(
+    winningNumbersAndBonusNumberInfo,
+  );
+
+  const winningNumbersAndBonusNumberInputs = document.createElement("div");
+  winningNumbersAndBonusNumberInputs.classList.add(
+    "lotto-winning-bonus-number__inputs",
+  );
+  winningNumbersAndBonusNumberBlock.appendChild(
+    winningNumbersAndBonusNumberInputs,
+  );
+
+  const winningNumbersInput = document.createElement("div");
+  winningNumbersInput.classList.add("lotto-winning-bonus-number__winning");
+  winningNumbersInput.innerHTML = `
+    <label for="">당첨 번호</label>
+    <div class="lotto-winning-bonus-number__winning-inputs">
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+      <input
+        type="number"
+        name="winning-number"
+        class="lotto-winning-bonus-number__input"
+      />
+    </div>
+  `;
+  winningNumbersAndBonusNumberInputs.appendChild(winningNumbersInput);
+
+  const bonusNumberInput = document.createElement("div");
+  bonusNumberInput.classList.add("lotto-winning-bonus-number__bonus");
+  bonusNumberInput.innerHTML = `
+    <label for="">보너스 번호</label>
+    <input
+      type="number"
+      name="bonus-number"
+      class="lotto-winning-bonus-number__input"
+    />
+  `;
+  winningNumbersAndBonusNumberInputs.appendChild(bonusNumberInput);
 }
