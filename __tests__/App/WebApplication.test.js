9b86393f0fa3e23f63a20a5ebb90c6b5e92d9a35
stardusty-lab <startdusty.lab@gmail.com>
2026-03-12 17:01:49 +0900

    test(WebApplication): 렌더링 후 element 찾도록 코드 순서 수정

--- __tests__/App/WebApplication.test.js
+++ __tests__/App/WebApplication.test.js
@@ -35,39 +35,47 @@ describe("웹 로또 앱 테스트", () => {
 
     mockRandoms(lottoListNumbers);
 
-    const priceInput = document.querySelector("#price");
+    // 금액 입력 후 구입 버튼 누르기
+    const priceInput = document.querySelector("#price input");
     const buyButton = document.querySelector("#buy-button");
-    const amountText = document.querySelector("#amount-text");
-    const lottoList = document.querySelector("#lotto-list");
-    const lottoListItem = lottoList.querySelectorAll("li");
-    const winningLottos = document.querySelector("#winning-lottos");
-    const winningLottosPin = winningLottos.querySelectorAll(".ui-pin");
-    const bonusLottoPin = document.querySelector("#bonus-lotto");
-    const resultButton = document.querySelector("#result-button");
-    const resultStaticstic = document.querySelector("#result-staticstic");
-    const rateText = document.querySelector("#rate-text");
 
     priceInput.value = 2000;
     buyButton.click();
 
+    // 결과 확인: 로또 구매 개수
+    const amountText = document.querySelector("#amount-text");
     expect(amountText.textContent).toBe("총 2개를 구매하였습니다.");
 
+    // 결과 확인: 번호 확인
+    const lottoList = document.querySelector("#lotto-list");
+    const lottoListItem = lottoList.querySelectorAll("li");
     lottoListItem.forEach((lottoList, i) => {
       expect(lottoList.textContent).toBe(
         `🎟️ ${lottoListNumbers[i].join(", ")}`,
       );
     });
 
+    // 당첨 번호 입력
     const winningNumbers = [1, 2, 3, 7, 8, 9];
+
+    const winningLottos = document.querySelector("#winning-lottos");
+    const winningLottosPin = winningLottos.querySelectorAll(".ui-pin");
+
     winningLottosPin.forEach((winningLottoPin, i) => {
       winningLottoPin.querySelector("input").value = winningNumbers[i];
     });
 
+    // 보너스 번호 입력
     const bonusNumber = 20;
+
+    const bonusLottoPin = document.querySelector("#bonus-lotto");
     bonusLottoPin.querySelector("input").value = bonusNumber;
 
+    // 결과 확인하기 버튼 클릭
+    const resultButton = document.querySelector("#result-button");
     resultButton.click();
 
+    // 결과 학인: 당첨 통계
     const staticstic = {
       1: 0,
       2: 0,
@@ -76,13 +84,20 @@ describe("웹 로또 앱 테스트", () => {
       5: 1,
       0: 0,
     };
+
+    const resultStaticstic = document.querySelector("#result-staticstic");
     resultStaticstic.querySelectorAll("tbody tr").forEach((tr, i) => {
       // 5등 부터 보여줌
       const grade = 5 - i;
-      expect(tr.querySelector("td:last-child")).toBe(`${staticstic[grade]}개`);
+      const lastTd = tr.querySelector("td:last-child");
+
+      expect(lastTd.textContent).toBe(`${staticstic[grade]}개`);
     });
 
-    const rate = 500;
+    // 결과 학인: 수익률
+    const rate = 250;
+    const rateText = document.querySelector("#rate-text");
+
     expect(rateText.textContent).toBe(`당신의 총 수익률은 ${rate}%입니다.`);
   });
 });
