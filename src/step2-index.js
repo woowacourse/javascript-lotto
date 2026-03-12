5bfb6b7007dd55ba5ec0af889544c601ced4ee96
inaemin <inaemin@gmail.com>
2026-03-12 19:27:34 +0900

    feat: step2 웹 엔트리포인트 연결
    - WebInputView, WebOutputView를 App에 주입하여 실행

--- src/step2-index.js
+++ src/step2-index.js
@@ -2,3 +2,8 @@
  * step 2의 시작점이 되는 파일입니다.
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
+import App from "./controller/App.js";
+import WebInputView from "./web/WebInputView.js";
+import WebOutputView from "./web/WebOutputView.js";
+
+new App({ inputView: WebInputView, outputView: WebOutputView }).run();
