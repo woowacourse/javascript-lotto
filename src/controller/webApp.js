71aab784a43c5ee1b283bbfe93cb151ed4c50f9d
김상윤 <ksy151020@gmail.com>
2026-03-12 23:09:00 +0900

    refactor: 관심사 분리를 위한 WebView 및 WebApp 구조 개선

--- src/controller/webApp.js
+++ src/controller/webApp.js
@@ -5,42 +5,79 @@ import Validator from "../utils/Validator.js";
 
 class WebApp {
   #lottoController;
+
   constructor() {
     this.view = new WebView();
   }
 
   init() {
-    this.view.$purchaseForm.addEventListener("submit", (e) => {
+    this.view.purchase.$form.addEventListener("submit", (e) => {
       e.preventDefault();
       this.#handlePurchase();
     });
 
-    this.view.$winningForm.addEventListener("submit", (e) => {
+    this.view.winning.$form.addEventListener("submit", (e) => {
       e.preventDefault();
       this.#handleResult();
     });
 
+    this.view.lotto.$displayMoreBtn.addEventListener('click', () => {
+      this.view.lotto.$container.classList.remove('collapsed');
+      this.view.lotto.$displayMoreBtn.classList.add('hidden');
+      this.view.lotto.$hideBtn.classList.remove('hidden');
+    });
+
+    this.view.lotto.$hideBtn.addEventListener('click', () => {
+      this.view.lotto.$container.classList.add('collapsed');
+      this.view.lotto.$displayMoreBtn.classList.remove('hidden');
+      this.view.lotto.$hideBtn.classList.add('hidden');
+    });
+
     window.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        if (this.view.isModalVisible()) {
-          e.preventDefault();
-          location.reload();
-        }
+      if (e.key === "Enter" && this.view.isModalVisible()) {
+        e.preventDefault();
+        location.reload();
       }
     });
+  }
 
-    this.view.$displayMoreBtn.addEventListener('click', () => {
-      this.view.$purchasedLottoContainer.classList.remove('collapsed');
-      this.view.$displayMoreBtn.classList.add('hidden');
-      this.view.$hideBtn.classList.remove('hidden');
-    });
+  #handlePurchase() {
+    try {
+      const purchasedPrice = Number(this.view.getPurchaseAmount());
+      this.#validatePurchase(purchasedPrice);
 
-    this.view.$hideBtn.addEventListener('click', () => {
-      this.view.$purchasedLottoContainer.classList.add('collapsed');
-      this.view.$displayMoreBtn.classList.remove('hidden');
-      this.view.$hideBtn.classList.add('hidden');
-    });
+      const lottoCount = purchasedPrice / 1000;
+      this.#lottoController = new LottoController(lottoCount);
+      const purchasedLottos = this.#lottoController.issueLottos();
 
+      this.view.renderLottoCount(lottoCount);
+      this.view.renderLottosContainer(purchasedLottos);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  #handleResult() {
+    try {
+      const winningNumbers = this.view.getWinningNumbers();
+      const bonusNumber = this.view.getBonusNumber();
+
+      this.#validateWinningNum(winningNumbers);
+      this.#validateBonusNum(winningNumbers, bonusNumber);
+
+      winningNumbers.forEach(num => {
+        Validator.validateNumber(num);
+        Validator.validateLottoNumRange(num);
+      });
+
+      const winningLotto = new Lotto(winningNumbers);
+      this.#lottoController.updateWinningResult(winningLotto, bonusNumber);
+
+      const { rankCount, profitRate } = this.#lottoController.getWinningResult();
+      this.view.renderResultTable(rankCount, profitRate);
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   #validatePurchase(price) {
@@ -57,51 +94,6 @@ class WebApp {
     Validator.validateNumber(bonusNumber);
     Validator.validateLottoNumRange(bonusNumber);
     Validator.validateDuplicateBonusNum(winningNumbers, bonusNumber);
-
-  }
-
-  #handlePurchase() {
-    try {
-      const purchasedPrice = Number(this.view.getPurchaseAmount());
-
-      this.#validatePurchase(purchasedPrice);
-
-      const lottoCount = purchasedPrice / 1000;
-      this.#lottoController = new LottoController(lottoCount);
-      const purchasedLottos = this.#lottoController.issueLottos();
-
-      this.view.renderLottoCount(lottoCount);
-      this.view.renderLottosContainer(purchasedLottos);
-
-    } catch (error) {
-
-      alert(error.message);
-      return;
-    }
-  }
-
-
-  #handleResult() {
-    try {
-      const winningNumbers = this.view.getWinningNumbers();
-      const bonusNumber = this.view.getBonusNumber();
-      this.#validateWinningNum(winningNumbers);
-      this.#validateBonusNum(winningNumbers, bonusNumber)
-
-      winningNumbers.forEach(num => {
-        Validator.validateNumber(num);
-        Validator.validateLottoNumRange(num);
-
-      });
-      const winningLotto = new Lotto(winningNumbers);
-      this.#lottoController.updateWinningResult(winningLotto, bonusNumber);
-
-      const { rankCount, profitRate } = this.#lottoController.getWinningResult();
-      this.view.renderResultTable(rankCount, profitRate);
-    } catch (error) {
-      alert(error.message);
-      return;
-    }
   }
 }
 
--- src/view/WebView.js
+++ src/view/WebView.js
@@ -1,82 +1,84 @@
 class WebView {
-    constructor() {
-        this.$purchaseForm = document.querySelector(".purchase-form");
-        this.$purchaseInput = document.querySelector(".purchase-input");
+  constructor() {
+    this.purchase = {
+      $form: document.querySelector(".purchase-form"),
+      $input: document.querySelector(".purchase-input"),
+    };
 
-        this.$lottoCount = document.querySelector(".lotto-count");
+    this.lotto = {
+      $section: document.querySelector(".purchased-lotto"),
+      $count: document.querySelector(".lotto-count"),
+      $container: document.querySelector(".purchased-lotto-container"),
+      $displayMoreBtn: document.querySelector(".display-more-lottos-btn"),
+      $hideBtn: document.querySelector(".hide-lottos-btn"),
+    };
 
-        this.$purchasedLottoSection = document.querySelector(".purchased-lotto");
-        this.$purchasedLottoContainer = document.querySelector(".purchased-lotto-container");
+    this.winning = {
+      $section: document.querySelector(".winningnum-bonusnum"),
+      $form: document.querySelector(".winningnum-bonusnum-form"),
+      $inputs: document.querySelectorAll(".winning-number"), 
+      $bonusInput: document.querySelector(".bonus-number"),
+    };
 
-        this.$displayMoreBtn = document.querySelector(".display-more-lottos-btn"); 
-        this.$hideBtn = document.querySelector(".hide-lottos-btn");
+    this.modal = {
+      $section: document.querySelector(".modal-result-screen"),
+      $rankCounts: document.querySelectorAll(".rank-count"),
+      $profitRate: document.querySelector(".profitrate-view"),
+    };
+  }
 
-        this.$winningSection = document.querySelector(".winningnum-bonusnum");
-        this.$winningForm = document.querySelector(".winningnum-bonusnum-form");
+  getPurchaseAmount() {
+    return this.purchase.$input.value;
+  }
 
-        this.$modalSection = document.querySelector(".modal-result-screen");
-        this.$rankCounts = document.querySelectorAll(".rank-count");
+  renderLottoCount(count) {
+    this.lotto.$count.innerText = count;
+    this.lotto.$section.classList.remove("hidden");
+    this.winning.$section.classList.remove("hidden");
+  }
 
-        this.$profitRate = document.querySelector(".profitrate-view");
-    }
+  renderLottosContainer(purchasedLottos) {
+    this.lotto.$count.innerText = purchasedLottos.length;
 
-
-    getPurchaseAmount() {
-        return this.$purchaseInput.value;
-    }
-
-    renderLottoCount(count) {
-        this.$lottoCount.innerText = count;
-        this.$purchasedLottoSection.classList.remove("hidden");
-        this.$winningSection.classList.remove("hidden");
-    }
-
-    renderLottosContainer(purchasedLottos) {
-        this.$lottoCount.innerText = purchasedLottos.length;
-        const lottoHTML = purchasedLottos
-            .map((lotto) => `
+    const lottoHTML = purchasedLottos
+      .map((lotto) => `
         <div class="lotto-item">
           <span>🎟️</span>
           <span class="lotto-numbers">${lotto.toString()}</span>
         </div>
       `).join("");
-        this.$purchasedLottoContainer.innerHTML = lottoHTML;
-        const lottoItemCount = this.$purchasedLottoContainer.querySelectorAll('.lotto-item').length;
 
-        if (lottoItemCount > 10) {
-            this.$displayMoreBtn.classList.remove('hidden');
-            this.$purchasedLottoContainer.classList.add('collapsed');
-        } else {
-            this.$displayMoreBtn.classList.add('hidden');
-            this.$hideBtn.classList.add('hidden');
-        }
+    this.lotto.$container.innerHTML = lottoHTML;
+
+    const isOverTen = purchasedLottos.length > 10;
+    this.lotto.$displayMoreBtn.classList.toggle('hidden', !isOverTen);
+    this.lotto.$container.classList.toggle('collapsed', isOverTen);
+  }
+
+  getWinningNumbers() {
+    return Array.from(this.winning.$inputs).map(input => Number(input.value));
+  }
+
+  getBonusNumber() {
+    return Number(this.winning.$bonusInput.value);
+  }
+
+  renderResultTable(rankCount, profitRate) {
+    this.modal.$section.classList.remove("hidden");
+
+    this.modal.$rankCounts.forEach(($td) => {
+      const rank = $td.dataset.rank;
+      $td.innerText = `${rankCount[rank] || 0}개`;
+    });
+
+    if (this.modal.$profitRate) {
+      this.modal.$profitRate.innerText = `당신의 총 수익률은 ${profitRate}%입니다.`;
     }
+  }
 
-    getWinningNumbers() {
-        const $winningInputs = document.querySelectorAll(".winning-number");
-        return Array.from($winningInputs).map(input => Number(input.value));
-    }
-
-    getBonusNumber() {
-        return Number(document.querySelector(".bonus-number").value);
-    }
-
-    renderResultTable(rankCount, profitRate) {
-        this.$modalSection.classList.remove("hidden");
-
-        this.$rankCounts.forEach(($td) => {
-            const rank = $td.dataset.rank;
-            $td.innerText = `${rankCount[rank] || 0}개`;
-        });
-
-        if (this.$profitRate) {
-            this.$profitRate.innerText = `당신의 총 수익률은 ${profitRate}%입니다.`;
-        }
-    }
-
-    isModalVisible() {
-        return !this.$modalSection.classList.contains("hidden");
-    }
-};
+  isModalVisible() {
+    return !this.modal.$section.classList.contains("hidden");
+  }
+}
 
 export default WebView;
