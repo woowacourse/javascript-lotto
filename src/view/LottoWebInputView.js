7dae8775cb451204aac495c82ece29939b51cf9b
eunnn <ehddms0517@naver.com>
2026-03-12 16:13:39 +0900

    feat: keydown event 추가

--- src/view/LottoWebInputView.js
+++ src/view/LottoWebInputView.js
@@ -1,9 +1,15 @@
 const purchaseButton = document.querySelector("#purchase");
 const moneyInput = document.querySelector("#money");
+const purchaseForm = document.querySelector("#purchase-form");
 
 export const LottoWebInputView = {
   bindPurchase(handler) {
     purchaseButton.addEventListener("click", handler);
+
+    purchaseForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      handler();
+    });
   },
 
   getPurchaseMoney() {
