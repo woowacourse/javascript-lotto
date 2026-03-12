d3bbeab48421c945756ba582665243fa3c28ca0f
kojesung <js95112345@gmail.com>
2026-03-12 14:56:28 +0900

    feat: WebLottoManager 테스트 통과 로직 구현

---
+++ src/WebLottoManager.js
@@ -0,0 +1,50 @@
+import PurchasedLotto from "./domain/PurchasedLotto";
+import WinningLotto from "./domain/WinningLotto";
+import { validatePurchaseAmount } from "./utils/validator";
+import { getReturnRate } from "./utils/getReturnRate";
+
+class WebLottoManager {
+  #purchasedLotto = null;
+  #purchaseAmount = 0;
+  #generateRandomNumbers;
+
+  constructor(generateRandomNumbers) {
+    this.#generateRandomNumbers = generateRandomNumbers;
+  }
+
+  purchase(amount) {
+    validatePurchaseAmount(amount);
+    this.#purchaseAmount = amount;
+    const numbers = Array.from(
+      { length: amount / 1000 },
+      this.#generateRandomNumbers
+    );
+    this.#purchasedLotto = new PurchasedLotto(numbers);
+    return {
+      count: this.#purchaseAmount / 1000,
+      lottos: this.#purchasedLotto.getLottos(),
+    };
+  }
+
+  getResult(winningNumbers, bonusNumber) {
+    if (this.#purchaseAmount === 0 || this.#purchasedLotto === null)
+      throw new Error("[ERROR]");
+    const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    const prizeList = [0, 0, 0, 0, 0, 0];
+    this.#purchasedLotto.getLottos().forEach((lotto) => {
+      const rank = winningLotto.getRank(lotto);
+      if (rank !== null && rank >= 1 && rank <= 5) prizeList[rank] += 1;
+    });
+    return {
+      prizeList: prizeList,
+      roi: getReturnRate(prizeList, this.#purchaseAmount),
+    };
+  }
+
+  reset() {
+    this.#purchasedLotto = null;
+    this.#purchaseAmount = 0;
+  }
+}
+
+export default WebLottoManager;
