f3f146d2082d1232ac954be20777c4883ac64116
박건규 <geongyu09@gmail.com>
2026-03-12 15:54:25 +0900

    chore: 더 이상 사용하지 않는 app 파일 제거

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import Header from "./components/Header/index.js";
-import { createElement } from "./service/index.js";
-
-const App = () => {
-  return createElement("main", {}, Header());
-};
-
-export default App;
