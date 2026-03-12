8d88b04253fecc8c53661f84cde9e8e314f7d554
박건규 <geongyu09@gmail.com>
2026-03-12 15:55:54 +0900

    feat(step2-index): 발행된 로또의 개수와 번호를 화면에 출력하는 로직 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -13,4 +13,22 @@ purchaseForm.addEventListener("submit", (event) => {
   drawLottoList();
 });
 
-function drawLottoList() {}
+function drawLottoList() {
+  const lottoListBlock = document.querySelector(".lotto-list");
+
+  const lottoListInfo = document.createElement("p");
+  lottoListInfo.classList.add("lotto-list__info");
+  lottoListInfo.textContent = `총 ${lottos.length}개를 구매하였습니다.`;
+  lottoListBlock.appendChild(lottoListInfo);
+
+  const lottoListItems = document.createElement("ul");
+  lottoListItems.classList.add("lotto-list__items");
+  lottoListBlock.appendChild(lottoListItems);
+
+  lottos.forEach((lotto) => {
+    const lottoListItem = document.createElement("li");
+    lottoListItem.classList.add("lotto-list__item");
+    lottoListItem.innerHTML = `<img src="/assets/Lotto.png" alt="로또" width="34px" /> ${lotto.parseNumbers().join(", ")}`;
+    lottoListItems.appendChild(lottoListItem);
+  });
+}
