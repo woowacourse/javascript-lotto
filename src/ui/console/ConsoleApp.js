67886589c4bd3d66521ea30c7e71441467d4abae
iftype <conditionaltype@gmail.com>
2026-03-13 00:11:33 +0900

    feat: 로또 결과 출력 및 다시하기 기능 추가
    - 결과 프로세스 구현 (통계, 수익률)
    - Y와 N은 UI의 책임으로 판단

--- src/ui/console/ConsoleApp.js
+++ src/ui/console/ConsoleApp.js
@@ -1,8 +1,12 @@
 import Lotto from "../../domain/Lotto.js";
 import WinningUseCase from "../../features/winning/WinningUseCase.js";
+import LottoStatisticsUseCase from "../../features/statistics/LottoStatisticsUseCase.js";
+import { calculateProfit } from "../../features/statistics/lottoStatistics.js";
 import { isNumber, isNumberArray } from "../../utils/inputValidator.js";
 import { toSplitComma, toNumber } from "../../utils/parser.js";
 export default class ConsoleApp {
+  static ANSWER = { YES: "y", NO: "n" };
+
   #purchaseLottoUseCase;
   #ui;
 
@@ -17,24 +21,26 @@ export default class ConsoleApp {
 
   async #lottoGame() {
     do {
-      const { lottoNumbers, purchasedAmount } = await this.#retry(() =>
+      const { lottos, purchasedAmount } = await this.#retry(() =>
         this.#processPurchase(),
       );
       const numbers = await this.#retry(() => this.#processWinningNumber());
       const winningNumber = await this.#retry(() =>
         this.#processWinningBonus(numbers),
       );
-    } while (false);
+      const totalPrize = await this.#processStatistics(lottos, winningNumber);
+      this.#processProfit(purchasedAmount, totalPrize);
+    } while (await this.#retry(() => this.#processAskRetry()));
   }
 
   async #processPurchase() {
     const rawAmount = await this.#ui.readAmount();
     const amount = isNumber(toNumber(rawAmount));
 
-    const { lottoNumbers, purchasedAmount } =
+    const { lottos, lottoNumbers, purchasedAmount } =
       this.#purchaseLottoUseCase.execute(amount);
     this.#ui.printLottos(lottoNumbers);
-    return { lottoNumbers, purchasedAmount };
+    return { lottos, purchasedAmount };
   }
 
   async #processWinningNumber() {
@@ -48,6 +54,27 @@ export default class ConsoleApp {
     const rawBonus = await this.#ui.readBonusNumber();
     const bonus = isNumber(toNumber(rawBonus));
     return WinningUseCase.execute(numbers, bonus);
+  }
+
+  async #processStatistics(lottos, winningNumber) {
+    const { lottosResult, totalPrize } =
+      LottoStatisticsUseCase.statisticsLottos(lottos, winningNumber);
+    this.#ui.printStatistics(lottosResult);
+    return totalPrize;
+  }
+
+  async #processProfit(purchasedAmount, totalPrize) {
+    const profit = calculateProfit(purchasedAmount, totalPrize);
+    this.#ui.printProfit(profit);
+  }
+
+  async #processAskRetry() {
+    const { YES, NO } = ConsoleApp.ANSWER;
+    const rawAnswer = await this.#ui.readAskRetry();
+    const answer = rawAnswer.toLowerCase();
+    if (answer === YES) return true;
+    if (answer === NO) return false;
+    throw new Error("y나 n을 입력해주세요");
   }
 
   async #retry(fn) {
