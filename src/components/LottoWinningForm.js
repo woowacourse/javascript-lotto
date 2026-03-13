e193359ba0f70518cc47890f60e4a82fd979e2a4
boyeon <boyeon6331@gmail.com>
2026-03-13 16:19:45 +0900

    fix: 라벨 연결 이슈 해결

--- src/components/LottoWinningForm.js
+++ src/components/LottoWinningForm.js
@@ -1,25 +1,27 @@
-const NumberInput = (name) => `
+const NumberInput = (name, ariaLabel = "", id = "") => `
   <input 
     type="number" 
     name="${name}" 
+    ${id ? `id="${id}"` : ""}
+    ${ariaLabel ? `aria-label="${ariaLabel}"` : ""}
     class="number"
   />
 `;
 
 export const WinningNumberInput = () => `
   <div class="winning-numbers-container">
-    <label>당첨 번호</label>
+    <span class="input-label">당첨 번호</span>
     <div class="winning-number-wrapper">
-      ${Array.from({ length: 6 }, (_, i) => NumberInput("winning-number")).join(
-        "",
-      )}
+      ${Array.from({ length: 6 }, (_, i) =>
+        NumberInput("winning-number", `당첨 번호 ${i + 1}번`),
+      ).join("")}
     </div>
   </div>
 `;
 
 export const BonusNumberInput = () => `
   <div class="bonus-number-container">
-    <label for="bonus-number">보너스 번호</label>
-    ${NumberInput("bonus-number")}
+    <label for="bonus-number" class="input-label">보너스 번호</label>
+    ${NumberInput("bonus-number", "보너스 번호", "bonus-number")}
   </div>
 `;
