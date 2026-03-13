b94df2e106c83e3225a66474417006a5b9fd3bd8
bel1c10ud <mail@bellclou.de>
2026-03-13 10:00:28 +0900

    test: 누락된 테스트 환경 추가

--- __tests__/output.test.js
+++ __tests__/output.test.js
@@ -1,5 +1,35 @@
 import WebOutput from "../src/view/WebOutput.js";
 import ConsoleOutput from "../src/view/ConsoleOutput.js";
+beforeEach(() => {
+  document.body.innerHTML = `
+    <div id="app">
+      <header class="header">
+        <div class="header__wrap">
+        </div>
+      </header>
+      <div class="main">
+        <div class="main__container">
+          <header class="main__container__header">
+          </header>
+          <div class="main__container__body">
+          </div>
+          <footer class="main__container__footer">
+          </footer>
+        </div>
+      </div>
+      <footer class="footer">
+      </footer>
+      <div class="overlay hidden">
+        <div class="modal">
+          <header class="modal__header"></header>
+          <div class="modal__body"></div>
+          <footer class="modal__footer"></footer>
+        </div>
+      </div>
+    </div>
+  `;
+});
+
 
 describe("Output 테스트", () => {
   test("WebOutput, ConsoleOutput은 모두 공통된 인스턴스 메서드를 가진다.", () => {
