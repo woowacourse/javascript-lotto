48b0acc11aed14083a909a08ac9215734a4ba3e1
장혜원 <janghw232625@gmail.com>
2026-03-13 17:34:05 +0900

    feat: 로또 구매 수 출력 UI 구현

--- src/step2-index.js
+++ src/step2-index.js
@@ -3,6 +3,8 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 import Validator from "./Utils/Validator.js";
+import LottoMachine from "./Domain/LottoMachine.js";
+import OutputView from "./View/OutputView.js";
 
 const inputPrice = document.querySelector("#inputPrice");
 const purchaseButton = document.querySelector("#purchaseButton");
@@ -10,6 +12,11 @@ const purchaseButton = document.querySelector("#purchaseButton");
 purchaseButton.addEventListener("click", () => {
   try {
     const purchasePrice = Validator.validatePurchasePrice(inputPrice.value);
+
+    const lottoMachine = new LottoMachine();
+    const lottos = lottoMachine.issueLottos(purchasePrice);
+
+    OutputView.printLottoList(lottos);
   } catch (e) {
     window.alert(e.message);
   }
