ad4af51460bb7e530691c4eb255eafbc8919b4d5
kojesung <js95112345@gmail.com>
2026-03-13 19:55:37 +0900

    feat: 이벤트 헨들러로 도메인 로직 및 모달 연결

--- src/step2-index.js
+++ src/step2-index.js
@@ -12,11 +12,22 @@ const purchaseInput = document.getElementById("purchase-amount-input");
 const purchasedLottoSection = document.getElementById(
   "purchased-lotto-section"
 );
+const winningSection = document.getElementById("winning-section");
+
+const winningNumberInputs = document.querySelectorAll(
+  ".winning-number-input-container .number-each"
+);
+const bonusNumberInput = document.getElementById("bonus-number");
+
+const resultBtn = document.getElementById("result-btn");
+
+const modalOverlay = document.getElementById("modal-overlay");
 
 const webLottoManager = new WebLottoManager(generateRandomNumbers);
 
 purchaseBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  winningSection.classList.remove("hidden");
   const amount = parseStringToNumber(purchaseInput.value);
 
   const { count, lottos } = webLottoManager.purchase(amount);
@@ -32,4 +43,26 @@ purchaseBtn.addEventListener("click", (e) => {
     )
     .join("")}
   </div>`;
+});
+
+resultBtn.addEventListener("click", () => {
+  const winningNumbers = [...winningNumberInputs].map((input) =>
+    parseStringToNumber(input.value)
+  );
+  const bonusNumber = parseStringToNumber(bonusNumberInput.value);
+
+  const { prizeList, roi } = webLottoManager.getResult(
+    winningNumbers,
+    bonusNumber
+  );
+  [1, 2, 3, 4, 5].forEach((rank) => {
+    document.getElementById(
+      `stat-${rank}`
+    ).textContent = `${prizeList[rank]}개`;
+  });
+  document.getElementById(
+    "roi-text"
+  ).textContent = `당신의 총 수익률은 ${roi}%입니다.`;
+
+  modalOverlay.classList.remove("hidden");
 });
