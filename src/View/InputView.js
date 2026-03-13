54dbe5bef8d8b35137b6f1484f8e8109f8c31a0a
장혜원 <janghw232625@gmail.com>
2026-03-13 15:33:09 +0900

    refactor: 웹 기반 UI에 맞게 Console 파일명을 View로 변경

---
+++ src/View/InputView.js
@@ -0,0 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const InputView = {
+  async readPurchasePrice() {
+    const input =
+      await MissionUtils.Console.readLineAsync("> 구입금액을 입력해 주세요.");
+    return input;
+  },
+
+  async readWinningNumbers() {
+    const inputs =
+      await MissionUtils.Console.readLineAsync("> 당첨 번호를 입력해 주세요. ");
+    return inputs;
+  },
+
+  async readBonusNumber() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "\n> 보너스 번호를 입력해 주세요. ",
+    );
+    return input;
+  },
+
+  async readRestart() {
+    const restartCommand = await MissionUtils.Console.readLineAsync(
+      "\n> 다시 시작하시겠습니까? (y/n) ",
+    );
+    return restartCommand;
+  },
+};
+
+export default InputView;
