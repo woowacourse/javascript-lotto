e927eef3b437be08fc0596cfa9e0e65f4b4ac737
gustn99 <azerosong@gmail.com>
2026-03-14 15:05:17 +0900

    feat: 헤더 퍼블리싱

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,9 +1,11 @@
 import LottoGameCard from './step-2/LottoGameCard/LottoGameCard.js';
 import LottoResultModal from './step-2/LottoResultModal/LottoResultModal.js';
+import LottoHeader from './step-2/LottoHeader/LottoHeader.js';
 
 function main() {
   const app = document.getElementById('app');
 
+  LottoHeader.render(app);
   LottoGameCard.render(app);
   LottoResultModal.render(app);
 }
