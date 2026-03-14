b82ae29be047c1da202a2e7bdbf4cef1bdb4bf20
gustn99 <azerosong@gmail.com>
2026-03-14 15:29:42 +0900

    feat: 반응형 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,12 +1,15 @@
 import LottoGameCard from './step-2/LottoGameCard/LottoGameCard.js';
 import LottoResultModal from './step-2/LottoResultModal/LottoResultModal.js';
 import LottoHeader from './step-2/LottoHeader/LottoHeader.js';
+import LottoFooter from './step-2/LottoFooter/LottoFooter.js';
 
 function main() {
   const app = document.getElementById('app');
 
   LottoHeader.render(app);
   LottoGameCard.render(app);
+  LottoFooter.render(app);
+  
   LottoResultModal.render(app);
 }
 
