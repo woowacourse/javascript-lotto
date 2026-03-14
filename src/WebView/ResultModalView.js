814f66988ccb4a35c14c40cc4efe6faf88b0592a
jebiyeon02 <jhje5595@naver.com>
2026-03-14 17:51:59 +0900

    refactor(resultModal): 당첨 정보를 정적 렌더링에서 동적으로 렌더링 하도록 변경

--- src/WebView/ResultModalView.js
+++ src/WebView/ResultModalView.js
@@ -1,12 +1,14 @@
 class ResultModalView {
   #modal;
   #closeButton;
+  #tbody;
   #profit;
   #submitButton;
 
   constructor() {
     this.#modal = document.querySelector(".result-modal");
     this.#closeButton = document.querySelector(".result-modal__close-btn");
+    this.#tbody = document.querySelector(".result-modal__tbody");
     this.#profit = document.querySelector(".result-modal__profit");
     this.#submitButton = document.querySelector(".result-modal__submit-btn");
   }
@@ -26,6 +28,39 @@ class ResultModalView {
 
   setProfitRate(profitRate) {
     this.#profit.textContent = `당신의 총 수익률은 ${profitRate}%입니다.`;
+  }
+
+  setScore(scoreData) {
+    this.#tbody.innerHTML = "";
+    const fragment = new DocumentFragment();
+
+    scoreData.forEach(({ matchCount, mustHaveBonus, price, winCount }) => {
+      const tr = document.createElement("tr");
+      tr.classList.add("result-modal__tr", "body");
+      const tdMatchCount = document.createElement("td");
+      const tdPrice = document.createElement("td");
+      const tdWinCount = document.createElement("td");
+
+      tdMatchCount.className = "result-modal__td";
+      tdMatchCount.textContent = `${matchCount}개`;
+      if (mustHaveBonus) {
+        tdMatchCount.textContent += "+보너스볼";
+      }
+
+      tdPrice.className = "result-modal__td";
+      tdPrice.textContent = price.toLocaleString();
+
+      tdWinCount.className = "result-modal__td";
+      tdWinCount.textContent = `${winCount}개`;
+
+      tr.append(tdMatchCount);
+      tr.append(tdPrice);
+      tr.append(tdWinCount);
+
+      fragment.prepend(tr);
+    });
+
+    this.#tbody.append(fragment);
   }
 
   open() {
--- src/step2-index.js
+++ src/step2-index.js
@@ -3,10 +3,11 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-import { LOTTO } from "./constants";
+import { LOTTO, RANK } from "./constants";
 import LottoGenerator from "./LottoGenerator";
 import WinningLotto from "./Model/WinningLotto";
 import ScoreBoard from "./ScoreBoard";
+import Validator from "./Validator";
 import PurchaseView from "./WebView/PurchaseView";
 import ResultModalView from "./WebView/ResultModalView";
 import TicketListView from "./WebView/TicketList";
@@ -32,53 +33,11 @@ class App {
   }
 
   run() {
-    this.#purchaseView.bindSubmitButton(this.#successPurchase);
-    this.#winningLottoView.bindSubmitButton(this.#successCreateWinningLotto);
+    this.#purchaseView.bindSubmitButton(this.#handleSubmitPurchase);
+    this.#winningLottoView.bindSubmitButton(this.#handleSubmitWinningLotto);
     this.#resultModalView.bindCloseButton();
     this.#resultModalView.bindSubmitButton(this.#initLotttGame);
   }
-
-  #successPurchase = (money) => {
-    this.#money = money;
-    const purchaseLottoCount = money / LOTTO.PRICE;
-    this.#ticketListView.setPurchaseLottoCount(purchaseLottoCount);
-
-    // 로또 객체 생성
-    this.#lottos.push(...LottoGenerator.makeLottos(purchaseLottoCount));
-    const allLottoNumbers = this.#lottos.map((lotto) => lotto.getNumbers());
-    this.#ticketListView.setAllTickets(allLottoNumbers);
-
-    this.#ticketListView.show();
-    this.#winningLottoView.show();
-  };
-
-  #successCreateWinningLotto = (winningNumbers, bonusNumber) => {
-    this.#winningLotto = new WinningLotto(winningNumbers, bonusNumber);
-    const allRankCount = ScoreBoard.makeAllRankCount(
-      this.#lottos,
-      this.#winningLotto,
-    );
-    this.#setScore(allRankCount);
-    const profitRate = ScoreBoard.getProfitRate(allRankCount, this.#money);
-    this.#resultModalView.setProfitRate(profitRate);
-
-    this.#resultModalView.open();
-  };
-
-  // 반복 코드 - 리팩토링 하기
-  #setScore = (allRankCount) => {
-    const tableDataFifth = document.querySelector(".result-modal__td--fifth");
-    const tableDataFourth = document.querySelector(".result-modal__td--fourth");
-    const tableDataThird = document.querySelector(".result-modal__td--third");
-    const tableDataSecond = document.querySelector(".result-modal__td--second");
-    const tableDataFirst = document.querySelector(".result-modal__td--first");
-
-    tableDataFifth.innerText = `${allRankCount.FIFTH}개`;
-    tableDataFourth.innerText = `${allRankCount.FOURTH}개`;
-    tableDataThird.innerText = `${allRankCount.THIRD}개`;
-    tableDataSecond.innerText = `${allRankCount.SECOND}개`;
-    tableDataFirst.innerText = `${allRankCount.FIRST}개`;
-  };
 
   #initLotttGame = () => {
     this.#lottos = [];
@@ -87,6 +46,80 @@ class App {
     this.#purchaseView.init();
     this.#winningLottoView.init();
   };
+
+  #handleSubmitPurchase = () => {
+    try {
+      const money = this.#purchaseView.readMoney();
+      Validator.numberDivided(money, LOTTO.PRICE);
+      Validator.positiveNumber(money);
+
+      this.#money = money;
+      this.#purchaseView.disableForm();
+      this.#setTickets();
+
+      this.#ticketListView.show();
+      this.#winningLottoView.show();
+    } catch (error) {
+      alert(error.message);
+      this.#purchaseView.removeInputValue();
+      this.#purchaseView.focusInput();
+    }
+  };
+
+  #handleSubmitWinningLotto = () => {
+    try {
+      const winningNumbers = this.#winningLottoView.readWinningNumbers();
+      winningNumbers.forEach((number) => {
+        this.#validateLottoNumber(number);
+      });
+      Validator.notDuplicated(winningNumbers);
+      Validator.arrayLength(winningNumbers, LOTTO.COUNT);
+
+      const bonusNumber = this.#winningLottoView.readBonusNumber();
+      this.#validateLottoNumber(bonusNumber);
+
+      this.#winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+      this.#winningLottoView.disableInputs();
+
+      this.#setLottoResult();
+      this.#resultModalView.open();
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  #setTickets() {
+    const purchaseLottoCount = this.#money / LOTTO.PRICE;
+    this.#ticketListView.setPurchaseLottoCount(purchaseLottoCount);
+
+    this.#lottos.push(...LottoGenerator.makeLottos(purchaseLottoCount));
+    const allLottoNumbers = this.#lottos.map((lotto) => lotto.getNumbers());
+    this.#ticketListView.setAllTickets(allLottoNumbers);
+  }
+
+  #setLottoResult() {
+    const allRankCount = ScoreBoard.makeAllRankCount(
+      this.#lottos,
+      this.#winningLotto,
+    );
+    const scoreData = Object.entries(RANK).map(([_, rank]) => {
+      return {
+        matchCount: rank.MATCH_COUNT,
+        mustHaveBonus: rank.MUST_HAVE_BONUS,
+        price: rank.PRICE,
+        winCount: allRankCount[rank.DISPLAY],
+      };
+    });
+    this.#resultModalView.setScore(scoreData);
+    const profitRate = ScoreBoard.getProfitRate(allRankCount, this.#money);
+    this.#resultModalView.setProfitRate(profitRate);
+  }
+
+  #validateLottoNumber(number) {
+    Validator.positiveNumber(number);
+    Validator.numberLower(LOTTO.LOWER, number);
+    Validator.numberUpper(LOTTO.UPPER, number);
+  }
 }
 
 const app = new App();
