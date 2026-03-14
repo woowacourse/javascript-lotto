5a3376a0c38e8f32adf5024e8fd76f00c79f9cd3
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-14 23:36:52 +0900

    fix: readLine이 WebApp에서 사용되지 않도록 분리

--- src/step1/Utils.js
+++ src/step1/Utils.js
@@ -1,5 +1,3 @@
-// import readline from 'readline';
-
 /**
  * 
  * 두 수를 포함하는 두 수 사이의 정수 난수 배열 반환.
@@ -26,18 +24,6 @@ export function pickNumberInRange(min, max, range) {
   return [...numbers];
 }
 
-// export const read = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// export function readLine(query) {
-//   return new Promise((resolve) => {
-//     read.question(query, (answer) => {
-//       resolve(answer);
-//     });
-//   });
-// }
 
 export async function reReadUntilSuccess(read) {
   while (true) {
