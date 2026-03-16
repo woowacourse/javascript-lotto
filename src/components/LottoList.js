7f7e3f40d0757bd2de9ed873acb18e122f0aa3ab
박건규 <geongyu09@gmail.com>
2026-03-16 17:34:16 +0900

    fix: 배포된 사이트에서 에셋이 깨지던 버그 수정
    - assets을 배포하기 위하여 public 폴더로 이동
    - 배포된 사이트 주소를 url로 사용하도록 import.meta.env.BASE_URL 추가

--- src/components/LottoList.js
+++ src/components/LottoList.js
@@ -2,7 +2,7 @@ const LottoList = (lottos) => {
   return `
     <p class="lotto-list__info">총 ${lottos.length}개를 구매하였습니다.</p>
     <ul class="lotto-list__items">
-      ${lottos.map((lotto) => `<li class="lotto-list__item"><img src="/assets/Lotto.png" alt="로또" width="34px" /> ${lotto.parseNumbers().join(", ")}</li>`).join("")}
+      ${lottos.map((lotto) => `<li class="lotto-list__item"><img src="${import.meta.env.BASE_URL}assets/Lotto.png" alt="로또" width="34px" /> ${lotto.parseNumbers().join(", ")}</li>`).join("")}
     </ul>
   `;
 };
--- src/components/LottoResult.js
+++ src/components/LottoResult.js
@@ -15,7 +15,7 @@ const LottoResult = (rank, returnRate) => {
       <div class="lotto-result__dimmed"></div>
       <div class="lotto-result__content">
         <button class="lotto-result__close-button">
-          <img src="/assets/Close.png" alt="닫기" width="14px" />
+          <img src="${import.meta.env.BASE_URL}assets/Close.png" alt="닫기" width="14px" />
         </button>
         <div class="lotto-result__title-wrapper">
           <h2 class="lotto-result__title">🏆 당첨 통계 🏆</h2>
