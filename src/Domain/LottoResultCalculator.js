b875772b2b639058774e9151a9eea7e82ca6d648
장혜원 <janghw232625@gmail.com>
2026-03-16 11:00:28 +0900

    fix: 수익률 계산 로직을 총 당첨금 기준으로 수정

--- src/Domain/LottoResultCalculator.js
+++ src/Domain/LottoResultCalculator.js
@@ -17,7 +17,7 @@ class LottoResultCalculator {
 
   calculateProfitRate(winningResult, purchasePrice) {
     const totalPrize = this.calculateTotalPrize(winningResult);
-    const profitRate = ((totalPrize - purchasePrice) / purchasePrice) * 100;
+    const profitRate = (totalPrize / purchasePrice) * 100;
 
     return Number(profitRate.toFixed(1));
   }
