b6cb8bfcef922320f0028af90175a78b0818ca21
박건규 <geongyu09@gmail.com>
2026-03-16 20:44:32 +0900

    feat(render): selector에 해당하는 요소를 찾지 못한 경우 에러를 던지도록 로직 추가

--- src/service/render.js
+++ src/service/render.js
@@ -1,5 +1,8 @@
 const render = (selector, content) => {
   const element = document.querySelector(selector);
+  if (!element) {
+    throw new Error(`Selector ${selector}에 해당하는 요소가 없습니다.`);
+  }
   element.innerHTML = content;
 };
 
