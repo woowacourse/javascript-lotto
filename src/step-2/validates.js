97583d39cb24df4290bd90f39aa82ea5a23d8b7c
gustn99 <azerosong@gmail.com>
2026-03-18 13:35:28 +0900

    refactor: 에러 메시지 상수화

--- src/step-2/validates.js
+++ src/step-2/validates.js
@@ -1,32 +1,34 @@
+import ERROR_MESSAGE from '../constants/errorMessage.js';
+
 export const validatePurchaseAmountInput = (userInput) => {
   if (userInput.trim() === '') {
-    throw new Error('구입 금액은 필수 입력값입니다.');
+    throw new Error(ERROR_MESSAGE.AMOUNT.REQUIRED);
   }
 
   const amountNumber = Number(userInput);
   if (Number.isNaN(amountNumber) || amountNumber <= 0) {
-    throw new Error('구입 금액은 양수여야 합니다.');
+    throw new Error(ERROR_MESSAGE.AMOUNT.POSITIVE);
   }
 };
 
 export const validateWinningNumber = (userInput) => {
   if (userInput.trim() === '') {
-    throw new Error('당첨 번호는 필수 입력값입니다.');
+    throw new Error(ERROR_MESSAGE.WINNING_NUMBERS.REQUIRED);
   }
 
   const winningNumber = Number(userInput);
   if (Number.isNaN(winningNumber)) {
-    throw new Error('당첨 번호는 숫자여야 합니다.');
+    throw new Error(ERROR_MESSAGE.WINNING_NUMBERS.NUMBER);
   }
 };
 
 export const validateBonusNumber = (userInput) => {
   if (userInput.trim() === '') {
-    throw new Error('보너스 번호는 필수 입력값입니다.');
+    throw new Error(ERROR_MESSAGE.BONUS_NUMBER.REQUIRED);
   }
 
   const bonusNumber = Number(userInput);
   if (Number.isNaN(bonusNumber)) {
-    throw new Error('보너스 번호는 숫자여야 합니다.');
+    throw new Error(ERROR_MESSAGE.BONUS_NUMBER.NUMBER);
   }
 };
